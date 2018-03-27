0069706541e6712d10227f20c9627e4da7bb8f84
Ben Baron <ben@einsteinx2.com>
2018-03-27 18:49:22 -0500

    Implemented updateTransactionStatus API

--- src/model/api.js
+++ src/model/api.js
@@ -4,17 +4,7 @@ export async function postRequest(url, bodyJson) {
     console.log(`url: ${url}`);
     console.log(`bodyJson: ${bodyJson}`);
     try {
-        const response = await axios.post(url, bodyJson);
-
-        // const response = await fetch(url, {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: bodyJson,
-        // });
-        console.log(`status: ${response.status} data: ${JSON.stringify(response.data)}`);
-        return response.data;
+        return await axios.post(url, bodyJson);
     } catch (error) {
         console.error(error);
         return null;
@@ -33,17 +23,29 @@ export async function updateDeviceDetails(bridgeDomain, sessionToken, fcmToken, 
         }),
     });
 
-    const responseJson = await postRequest(`${bridgeDomain}/update-device-details`, bodyJson);
-    console.log(`updateConnectionDetails response ${JSON.stringify(responseJson)}`);
-    return responseJson.deviceUuid;
+    const response = await postRequest(`${bridgeDomain}/update-device-details`, bodyJson);
+    console.log(`updateConnectionDetails response status: ${response.status} data: ${JSON.stringify(response.data)}`);
+    return response.data.deviceUuid;
 }
 
 // TODO: First parse the push notification to get this info, and call this API to get the actual transaction data
-// The data returned is the encryptedPayload for the transaction, which I decrypt using my privkey from the server
+// The data returned is the encryptedPayload for the transaction, which I decrypt using my privkey
 // that payload contains all info needed to construct a transaction
 // export async function getTransactionDetails(deviceUuid, transactionUuid) {}
 
-// TODO: After attempting to send the transaction to the blockchain, call this API to report the success
-// and if successful the transaction hash. Format is {deviceUuid, transactionUuid, encryptedPayload: {success: boolean, transactionHash: string}}
-// Encrypted payload should be stringified and encrypted using the Dapp's pubkey
-// export asunc function updateTransactionStatus(deviceUuid, transactionUuid, transactionHash, success) {}
+// After attempting to send the transaction to the blockchain, call this API to report the success
+// TODO: Encrypted payload should be stringified and encrypted using the Dapp's pubkey
+export async function updateTransactionStatus(bridgeDomain, deviceUuid, transactionUuid, success, transactionHash) {
+    const bodyJson = JSON.stringify({
+        deviceUuid,
+        transactionUuid,
+        encryptedPayload: JSON.stringify({
+            success,
+            transactionHash,
+        }),
+    });
+
+    const response = await postRequest(`${bridgeDomain}/update-transaction-status`, bodyJson);
+    console.log(`updateTransactionStatus response status: ${response.status} data: ${JSON.stringify(response.data)}`);
+    return response.status;
+}
