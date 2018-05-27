d0a59544ad9772eba62e1b008887a1bfef2b4151
Pedro Gomes <pedrogomes94@gmail.com>
2018-05-26 23:47:35 -0300

    fix missing styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const colors = {
-  accent: 'rgb(18, 184, 120)',
-  contrast: 'rgb(255, 255, 255)',
-};
-
 const Label = styled.Text`
   color: #ffffff;
   font-size: 18px;
@@ -14,13 +9,13 @@ const Label = styled.Text`
   letter-spacing: 0.4px;
   align-self: center;
   text-align: center;
-  padding: 16px 0 0 0;
+  padding: 17px 0 0 0;
 `;
 
 const ButtonContainer = styled.TouchableHighlight`
   position: absolute;
   bottom: 48px;
-  background-color: #00B371;
+  background-color: #00b371;
   width: 347px;
   height: 59px;
   border-radius: 14px;
