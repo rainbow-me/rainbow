b1a961a3cefb316c51123a4c06026c8c62f864f9
Jesse Weigel <jesseweigel@gmail.com>
2018-06-16 18:57:50 -0400

    feat: add DappCard component

--- src/screens/QRScannerScreen.js
+++ src/screens/QRScannerScreen.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import Card from '../components/Card';
+import DappCard from '../components/DappCard';
 import { walletConnectInit, walletConnectSendSession } from '../model/walletconnect';
 
 class QRScannerScreen extends Component {
@@ -35,40 +36,11 @@ class QRScannerScreen extends Component {
           onRead={this.onSuccess}
         />
         <Card style={{ flex: 1 }}>
-          <Image
-            style={{
-              width: 74,
-              height: 46,
-              resizeMode: 'contain',
-              alignSelf: 'center',
-            }}
-            source={require('../assets/walletconnect-logo-blue.png')}
-          />
+          <Image style={styles.connectLogo} source={require('../assets/walletconnect-logo-blue.png')} />
           <Text style={[styles.centerText, styles.title]}>WalletConnect</Text>
-          <Text style={styles.centerText}>Scan a QR code to connect to a web dapp</Text>
-          <ScrollView horizontal>
-            <View
-              style={{
-                borderColor: '#E8E6E6',
-                borderStyle: 'solid',
-                borderWidth: 0.5,
-                padding: 8,
-                backgroundColor: '#FCFCFC',
-                borderRadius: 8,
-              }}
-            >
-              <Text style={styles.dappName}>CryptoKitties.co</Text>
-              <Image
-                style={{
-                  width: 68,
-                  height: 84,
-                  resizeMode: 'contain',
-                  alignSelf: 'center',
-                }}
-                source={require('../assets/landing-kitty03.png')}
-              />
-              <Text style={styles.dappName}>Tokenized cat trading</Text>
-            </View>
+          <Text style={[styles.centerText, styles.textDefault]}>Scan a QR code to connect to a web dapp</Text>
+          <ScrollView horizontal contentContainerStyle={{ justifyContent: 'center', alignItems: 'center' }}>
+            {dappInfo.map(dapp => <DappCard key={dapp.name} dappName={dapp.name} dappImage={dapp.image} dappText={dapp.text} />)}
           </ScrollView>
         </Card>
       </View>
@@ -87,12 +59,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#eeeeee',
   },
-
+  connectLogo: {
+    width: 74,
+    height: 46,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
   centerText: {
     flexWrap: 'wrap',
     textAlign: 'center',
+  },
+  textDefault: {
     fontSize: 16,
-    paddingTop: 32,
+    paddingTop: 24,
     color: 'rgba(0,0,0,0.54)',
   },
   title: {
@@ -100,12 +79,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     paddingTop: 16,
   },
-  dappName: {
-    fontSize: 12,
-    color: '#3B99FC',
-    paddingTop: 0,
-  },
-
   scannerTop: {
     flex: 0,
     height: 0,
@@ -129,5 +102,23 @@ const styles = StyleSheet.create({
     color: 'rgb(0,122,255)',
   },
 });
+
+const dappInfo = [
+  {
+    name: 'CryptoKitties.co',
+    image: require('../assets/landing-kitty03.png'),
+    text: 'Tokenised cat trading',
+  },
+  {
+    name: 'OpenSea.io',
+    image: require('../assets/opensea.png'),
+    text: 'Trade unique tokens',
+  },
+  {
+    name: 'Balance.io',
+    image: require('../assets/balance.png'),
+    text: 'Token based banking',
+  },
+];
 
 export default QRScannerScreen;
