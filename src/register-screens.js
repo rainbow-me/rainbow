1079c7262f6d15d6331c0c1bf09cecbf736f3fca
Michael Demarais <m.demarais@gmail.com>
2018-07-12 21:46:51 -0400

    attempt react-native upgrade

--- src/register-screens.js
+++ src/register-screens.js
@@ -1,5 +1,6 @@
-import { Navigation, ScreenVisibilityListener } from 'react-native-navigation';
+import { Navigation } from 'react-native-navigation';
 
+import IntroScreen from './onboarding/IntroScreen';
 import POCScreen from './screens/POCScreen';
 import QRScannerScreen from './screens/QRScannerScreen';
 import SettingsScreenWithData from './settings/SettingsScreenWithData';
@@ -8,6 +9,7 @@ import WalletScreenWithData from './wallet/WalletScreenWithData';
 
 // register all screens of the app (including internal ones)
 export function registerScreens() {
+  Navigation.registerComponent('BalanceWallet.IntroScreen', () => IntroScreen);
   Navigation.registerComponent('BalanceWallet.POCScreen', () => POCScreen);
   Navigation.registerComponent('BalanceWallet.QRScannerScreen', () => QRScannerScreen);
   Navigation.registerComponent('BalanceWallet.SettingsScreen', () => SettingsScreenWithData);
@@ -15,19 +17,19 @@ export function registerScreens() {
   Navigation.registerComponent('BalanceWallet.WalletScreen', () => WalletScreenWithData);
 }
 
-export function registerScreenVisibilityListener() {
-  new ScreenVisibilityListener({
-    willAppear: ({ screen }) => console.log(`Displaying screen ${screen}`),
-    didAppear: ({
-      screen, startTime, endTime, commandType,
-    }) =>
-      console.log('screenVisibility', `Screen ${screen} displayed in ${endTime - startTime} millis [${commandType}]`),
-    willDisappear: ({ screen }) => console.log(`Screen will disappear ${screen}`),
-    didDisappear: ({ screen }) => console.log(`Screen disappeared ${screen}`),
-  }).register();
-}
+// export function registerScreenVisibilityListener() {
+//   new ScreenVisibilityListener({
+//     willAppear: ({ screen }) => console.log(`Displaying screen ${screen}`),
+//     didAppear: ({
+//       screen, startTime, endTime, commandType,
+//     }) =>
+//       console.log('screenVisibility', `Screen ${screen} displayed in ${endTime - startTime} millis [${commandType}]`),
+//     willDisappear: ({ screen }) => console.log(`Screen will disappear ${screen}`),
+//     didDisappear: ({ screen }) => console.log(`Screen disappeared ${screen}`),
+//   }).register();
+// }
 
 export default function initializeScreens() {
   registerScreens();
-  registerScreenVisibilityListener();
+  // registerScreenVisibilityListener();
 }
