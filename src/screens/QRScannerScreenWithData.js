1b6c114862f2c7cc12e795d16c25ee9834855859
jinchung <jin.chung17@gmail.com>
2018-07-30 22:30:43 -0400

    replaced old firebase library with new and fixes for fcm token if not available yet

--- src/screens/QRScannerScreenWithData.js
+++ src/screens/QRScannerScreenWithData.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { walletConnectInit } from '../model/walletconnect';
 import QRScannerScreen from './QRScannerScreen';
+import { AlertIOS } from 'react-native';
 
 class QRScannerScreenWithData extends Component {
   static propTypes = {
@@ -21,11 +22,12 @@ class QRScannerScreenWithData extends Component {
         await walletConnectInit(accountAddress, data.domain, data.sessionId, data.sharedKey, data.dappName);
         navigation.navigate('WalletScreen');
       } catch (error) {
-        // TODO error handling
+        AlertIOS.alert('Error initializing with WalletConnect', error);
         console.log('error initializing wallet connect', error);
         setTimeout(() => this.qrCodeScanner.reactivate(), 1000);
       }
     } else {
+      console.log('resetting qr scanner');
       setTimeout(() => this.qrCodeScanner.reactivate(), 1000);
     }
   }
