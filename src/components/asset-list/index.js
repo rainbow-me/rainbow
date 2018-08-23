e570f9b95254d9b31be1fdf6e653c43436ca2faa
Michael Demarais <m.demarais@gmail.com>
2018-08-23 17:46:08 -0400

    Create UniqueTokenImage component

--- src/components/asset-list/index.js
+++ src/components/asset-list/index.js
@@ -3,4 +3,5 @@ export { default as AssetListFooter } from './AssetListFooter';
 export { default as AssetListHeader } from './AssetListHeader';
 export { default as AssetListItem } from './AssetListItem';
 export { default as UniqueTokenCard } from './UniqueTokenCard';
+export { default as UniqueTokenImage } from './UniqueTokenImage';
 export { default as UniqueTokenRow } from './UniqueTokenRow';
