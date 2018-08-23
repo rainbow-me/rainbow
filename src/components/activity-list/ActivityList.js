64760e4d7c5fd0457ebaa67ffef7188db991ecf5
Michael Demarais <m.demarais@gmail.com>
2018-08-23 17:50:31 -0400

    Create ActivityList

---
+++ src/components/activity-list/ActivityList.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { buildTransactionsSections } from '../../helpers/transactions';
+import { CoinRow, TransactionCoinRow } from '../coin-row';
+import { SectionList } from '../list';
+import ActivityListHeader from './ActivityListHeader';
+
+const getItemLayout = (data, index) => ({
+  index,
+  length: CoinRow.height,
+  offset: CoinRow.height * index,
+});
+
+const ActivityList = ({
+  accountAddress,
+  fetchingTransactions,
+  hasPendingTransaction,
+  onPressBack,
+  safeAreaInset,
+  transactions,
+}) => (
+  <SectionList
+    contentContainerStyle={{ paddingBottom: 40 }}
+    extraData={{ hasPendingTransaction }}
+    getItemLayout={getItemLayout}
+    initialNumToRender={75}
+    keyExtractor={({ hash }) => hash}
+    removeClippedSubviews
+    renderSectionHeader={({ section }) => <ActivityListHeader {...section} />}
+    sections={buildTransactionsSections({
+      accountAddress,
+      renderItem: TransactionCoinRow,
+      transactions,
+    })}
+  />
+);
+
+ActivityList.propTypes = {
+  accountAddress: PropTypes.string,
+  hasPendingTransaction: PropTypes.bool,
+  transactions: PropTypes.array,
+};
+
+export default ActivityList;
---
+++ src/components/activity-list/index.js
@@ -0,0 +1,2 @@
+export { default as ActivityList } from './ActivityList';
+export { default as ActivityListHeader } from './ActivityListHeader';
