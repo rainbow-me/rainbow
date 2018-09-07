84e239c83b43a5a02e28f42e2eb162e91c9f9f92
Michael Demarais <m.demarais@gmail.com>
2018-09-07 17:41:19 -0400

    create withAccountAddress HOC

--- src/hoc/index.js
+++ src/hoc/index.js
@@ -1,3 +1,4 @@
+export { default as withAccountAddress } from './withAccountAddress';
 export { default as withHideSplashScreen } from './withHideSplashScreen';
 export { default as withHideSplashScreenOnMount } from './withHideSplashScreenOnMount';
 export { default as withSafeAreaViewInsetValues } from './withSafeAreaViewInsetValues';
