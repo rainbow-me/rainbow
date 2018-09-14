65129cff76f4df92cd820c5087e5eb69023db8b8
Michael Demarais <m.demarais@gmail.com>
2018-09-13 22:52:01 -0400

    Create Badge component

--- src/components/header/ActivityHeaderButton.js
+++ src/components/header/ActivityHeaderButton.js
@@ -2,20 +2,20 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { withNavigation } from 'react-navigation';
 import { compose, withHandlers } from 'recompact';
-import Icon from '../icons/Icon';
 import { colors } from '../../styles';
+import { Badge } from '../badge';
+import Icon from '../icons/Icon';
+import { Centered } from '../layout';
 import HeaderButton from './HeaderButton';
 
-const ActivityHeaderButton = ({ onPress }) => {
-  return (
-    <HeaderButton onPress={onPress}>
-      <Icon
-        color={colors.dark}
-        name="clock"
-      />
-    </HeaderButton>
-  );
-};
+const ActivityHeaderButton = ({ onPress }) => (
+  <HeaderButton onPress={onPress}>
+    <Centered>
+      <Icon color={colors.dark} name="clock" />
+      <Badge delay={2500} value={5} />
+    </Centered>
+  </HeaderButton>
+);
 
 ActivityHeaderButton.propTypes = {
   onPress: PropTypes.func,
