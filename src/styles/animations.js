915a83297cb401fff70fed0c272a722c112400a7
Michael Demarais <m.demarais@gmail.com>
2018-09-13 22:52:01 -0400

    Create Badge component

--- src/styles/animations.js
+++ src/styles/animations.js
@@ -1,4 +1,8 @@
 const keyframes = {
+  badge: {
+    from: { scale: 0, translateY: 1 },
+    to: { scale: 1, translateY: 0 },
+  },
   button: {
     from: { scale: 1, translateY: 0 },
     to: { scale: 0.80, translateY: 1 },
@@ -6,6 +10,10 @@ const keyframes = {
 };
 
 const spring = {
+  badge: {
+    friction: 13,
+    tension: 145,
+  },
   default: {
     friction: 44,
     tension: 1000,
