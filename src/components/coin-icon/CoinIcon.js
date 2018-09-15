1e827713f115450ecc7104bcf2b71f6add36042e
Michael Demarais <m.demarais@gmail.com>
2018-09-14 21:42:31 -0400

    Create RequestCoinRow

--- src/components/coin-icon/CoinIcon.js
+++ src/components/coin-icon/CoinIcon.js
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { css } from 'styled-components/primitives';
 import ReactCoinIcon, { FallbackIcon } from 'react-coin-icon';
-import { hoistStatics, onlyUpdateForKeys } from 'recompact';
-import { colors, fonts, position, shadow } from '../styles';
-import { ShadowStack } from './shadow-stack';
+import { onlyUpdateForKeys } from 'recompact';
+import { borders, colors, fonts, shadow } from '../../styles';
+import { ShadowStack } from '../shadow-stack';
 
-const CoinIconHeight = 40;
+const CoinIconSize = 40;
 
 const fallbackTextStyles = css`
   font-family: ${fonts.family.SFMono};
@@ -20,10 +20,10 @@ const CoinIconFallback = fallbackProps => (
   />
 );
 
-const CoinIcon = ({ size, symbol }) => (
+const enhance = onlyUpdateForKeys(['symbol']);
+const CoinIcon = enhance(({ size, symbol }) => (
   <ShadowStack
-    {...position.sizeAsObject(size)}
-    borderRadius={size / 2}
+    {...borders.buildCircleAsObject(size)}
     shadows={[
       shadow.buildString(0, 4, 6, colors.alpha(colors.purple, 0.12)),
       shadow.buildString(0, 1, 3, colors.alpha(colors.purple, 0.24)),
@@ -35,7 +35,7 @@ const CoinIcon = ({ size, symbol }) => (
       symbol={symbol}
     />
   </ShadowStack>
-);
+));
 
 CoinIcon.propTypes = {
   symbol: PropTypes.string,
@@ -43,9 +43,9 @@ CoinIcon.propTypes = {
 };
 
 CoinIcon.defaultProps = {
-  size: CoinIconHeight,
+  size: CoinIconSize,
 };
 
-CoinIcon.height = CoinIconHeight;
+CoinIcon.size = CoinIconSize;
 
-export default hoistStatics(onlyUpdateForKeys(['symbol']))(CoinIcon);
+export default CoinIcon;
---
+++ src/components/coin-row/RequestCoinRow.js
@@ -0,0 +1,77 @@
+import { addHours, differenceInMinutes, subMinutes } from 'date-fns';
+import React from 'react';
+import { compose, onlyUpdateForKeys, withProps } from 'recompact';
+import { css } from 'styled-components/primitives';
+import { colors } from '../../styles';
+import { Button } from '../buttons';
+import { Text } from '../text';
+import CoinName from './CoinName';
+import CoinRow from './CoinRow';
+
+import { RequestCoinIcon } from '../coin-icon';
+
+const getPercentageOfTimeElapsed = (startDate, endDate) => {
+  const originalDifference = differenceInMinutes(endDate, startDate);
+  const currentDifference = differenceInMinutes(endDate, Date.now());
+
+  return Math.floor((currentDifference * 100) / originalDifference);
+};
+
+const buttonContainerStyles = css`
+  border-radius: 18;
+  height: 36;
+  padding-left: 12;
+  padding-right: 12;
+`;
+
+const RequestCoinRowButton = () => (
+  <Button
+    bgColor={colors.primaryBlue}
+    containerStyles={buttonContainerStyles}
+    onPress={() => console.log('XXX TODO: HOOK THIS UP')}
+    size="small"
+    textProps={{ size: 'smedium' }}
+  >
+    Open
+  </Button>
+);
+
+const RequestCoinRow = ({
+  expirationColor,
+  expiresAt,
+  item,
+  ...props
+}) => (
+  <CoinRow
+    {...item}
+    {...props}
+    bottomRowRender={({ name }) => <CoinName>{name}</CoinName>}
+    coinIconRender={RequestCoinIcon}
+    expirationColor={expirationColor}
+    topRowRender={() => (
+      <Text color={expirationColor} weight="semibold">
+        Expires in {differenceInMinutes(expiresAt, Date.now())}m
+      </Text>
+    )}
+  >
+    <RequestCoinRowButton />
+  </CoinRow>
+);
+
+export default compose(
+  withProps(() => {
+    // XXX TODO: HOOK THIS UP
+    const createdAt = subMinutes(Date.now(), 48);
+    // XXX TODO: HOOK THIS UP
+    const expiresAt = addHours(createdAt, 1);
+    const percentElapsed = getPercentageOfTimeElapsed(createdAt, expiresAt);
+
+    return {
+      createdAt,
+      expirationColor: (percentElapsed > 25) ? colors.primaryBlue : colors.orangeMedium,
+      expiresAt,
+      percentElapsed,
+    };
+  }),
+  onlyUpdateForKeys(['expirationColor', 'percentElapsed']),
+)(RequestCoinRow);
