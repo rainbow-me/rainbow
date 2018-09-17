a5fc1b3c3fa720b9339032e7d0a46fcb17fe0942
Michael Demarais <m.demarais@gmail.com>
2018-09-17 06:50:50 -0400

    Add iOS rasterization optimization to AcitivyListItems

--- src/components/activity-list/ActivityListHeader.js
+++ src/components/activity-list/ActivityListHeader.js
@@ -1,7 +1,8 @@
-import { compose, onlyUpdateForKeys, pickProps } from 'recompact';
+import { compose, onlyUpdateForKeys, pickProps, withProps } from 'recompact';
 import { ListHeader } from '../list';
 
 export default compose(
   pickProps(Object.keys(ListHeader.propTypes)),
+  withProps({ shouldRasterizeIOS: true }),
   onlyUpdateForKeys(['title']),
 )(ListHeader);
