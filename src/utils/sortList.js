14158b13f3abf6d28537950320984a11ab04021a
jinchung <jin.chung17@gmail.com>
2018-09-25 15:11:06 -0400

    fixes for pending txns

--- src/utils/sortList.js
+++ src/utils/sortList.js
@@ -1,9 +1,9 @@
 import { get } from 'lodash';
 
-export default (array = [], sortByKey) =>
+export default (array = [], sortByKey, defaultValue = '') =>
   array.slice(0).sort((a, b) => {
-    const itemA = sortByKey ? get(a, sortByKey) : a;
-    const itemB = sortByKey ? get(b, sortByKey) : b;
+    const itemA = sortByKey ? get(a, sortByKey, defaultValue) : a;
+    const itemB = sortByKey ? get(b, sortByKey, defaultValue) : b;
 
     if (typeof itemA === 'string' && typeof itemB === 'string') {
       if (itemA.toLowerCase() < itemB.toLowerCase()) return -1;
