60dd5e560a244c23d96c96b1f0d1bd9f733227f0
Michael Demarais <m.demarais@gmail.com>
2018-10-23 18:03:42 -0400

    Create WalletConnectList & related components

---
+++ src/components/walletconnect-list/WalletConnectListItem.js
@@ -0,0 +1,73 @@
+import { distanceInWordsStrict } from 'date-fns';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { compose, hoistStatics, onlyUpdateForKeys, withHandlers } from 'recompact';
+import styled from 'styled-components/primitives';
+import { colors, padding } from '../../styles';
+import { Button } from '../buttons';
+import { RequestVendorLogoIcon } from '../coin-icon';
+import { Column, Row } from '../layout';
+import { Text, TruncatedText } from '../text';
+
+const ContainerPadding = 15;
+const VendorLogoIconSize = 50;
+
+const Container = styled(Row).attrs({ align: 'center' })`
+  ${padding(ContainerPadding)}
+`;
+
+const Content = styled(Column)`
+  ${padding(0, 18, 0, 12)}
+`;
+
+const DisconnectButton = styled(Button)`
+  ${padding(8, 12.5, 10, 12.5)}
+`;
+
+const ExpiresText = styled(Text).attrs({ size: 'medium' })`
+  color: ${colors.alpha(colors.blueGreyDark, 0.60)};
+  margin-top: 3.5;
+`;
+
+const WalletConnectListItem = ({
+  dappName,
+  expires,
+  onDisconnect,
+}) => (
+  <Container>
+    <RequestVendorLogoIcon
+      dappName={dappName}
+      size={VendorLogoIconSize}
+    />
+    <Content flex={1}>
+      <TruncatedText color="dark" size="lmedium">
+        {dappName}
+      </TruncatedText>
+      <ExpiresText>
+        Expires in {distanceInWordsStrict(Date.now(), expires)}
+      </ExpiresText>
+    </Content>
+    <DisconnectButton
+      bgColor={colors.primaryBlue}
+      onPress={onDisconnect}
+      textProps={{ size: 'smedium' }}
+    >
+      Connected
+    </DisconnectButton>
+  </Container>
+);
+
+WalletConnectListItem.propTypes = {
+  dappName: PropTypes.string.isRequired,
+  expires: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  onDisconnect: PropTypes.func.isRequired,
+};
+
+WalletConnectListItem.height = VendorLogoIconSize + (ContainerPadding * 2);
+
+export default hoistStatics(compose(
+  onlyUpdateForKeys(['expires']),
+  withHandlers({
+    onDisconnect: ({ _sessionId, onDisconnect }) => () => onDisconnect(_sessionId),
+  }),
+))(WalletConnectListItem);
