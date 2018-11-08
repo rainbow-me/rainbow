00eb421d5ebf916e4296ab29d2839adf41dbe11b
Michael Demarais <m.demarais@gmail.com>
2018-11-07 21:13:11 -0500

    Create getEthereumAddressFromQRCodeData utility function

---
+++ src/utils/address.js
@@ -0,0 +1,20 @@
+import { isValidAddress } from 'balance-common';
+
+/**
+ * @desc get ethereum address from raw QR Code data
+ * @param  {String}  data - qr code data
+ * @return {String}  address - ethereum address
+ */
+export const getEthereumAddressFromQRCodeData = (data) => {
+  if (!data) return null;
+
+  const parts = data.split(':');
+
+  if (parts[0] === 'ethereum' && isValidAddress(parts[1])) {
+    return parts[1];
+  } else if (isValidAddress(parts[0])) {
+    return parts[0];
+  }
+
+  return null;
+};
