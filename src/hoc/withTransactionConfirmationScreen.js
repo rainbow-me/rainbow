8f5f0fc130c7a93d81eaa331f73e03ae5d980e25
jinchung <jin.chung17@gmail.com>
2018-11-16 17:15:02 -0500

    setup of wallet specific nonce logic

--- src/hoc/withTransactionConfirmationScreen.js
+++ src/hoc/withTransactionConfirmationScreen.js
@@ -1,13 +1,19 @@
 import { accountUpdateHasPendingTransaction, accountUpdateTransactions } from 'balance-common';
 import { connect } from 'react-redux';
 import { removeTransaction } from '../redux/transactionsToApprove';
+import { updateTransactionCountNonce } from '../redux/nonce';
 
-const mapStateToProps = ({ walletconnect: { walletConnectors } }) => ({
+const mapStateToProps = ({
+  walletconnect: { walletConnectors },
+  nonce: { transactionCountNonce },
+}) => ({
   walletConnectors,
+  transactionCountNonce,
 });
 
 export default Component => connect(mapStateToProps, {
   accountUpdateHasPendingTransaction,
   accountUpdateTransactions,
   removeTransaction,
+  updateTransactionCountNonce,
 })(Component);
