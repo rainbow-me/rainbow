b70c9e3e099e7031beb0645675870fb72a119d56
Michael Demarais <m.demarais@gmail.com>
2018-12-16 22:21:57 -0500

    Make Divider configurablely horizontal/vertical

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -1,40 +1,65 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { pure } from 'recompact';
-import styled from 'styled-components/primitives';
+import styled, { css } from 'styled-components/primitives';
 import { Row } from './layout';
 import { borders, colors, position } from '../styles';
 
+const horizontalBorderLineStyles = ({ insetLeft, insetRight }) => css`
+  ${insetLeft ? borders.buildRadius('left', 2) : null};
+  ${insetRight ? borders.buildRadius('right', 2) : null};
+  left: ${insetLeft || 0};
+  right: ${insetRight || 0};
+`;
+
+const verticalBorderLineStyles = ({ insetBottom, insetTop }) => css`
+  ${insetBottom ? borders.buildRadius('bottom', 2) : null};
+  ${insetTop ? borders.buildRadius('top', 2) : null};
+  bottom: ${insetBottom || 0};
+  top: ${insetTop || 0};
+`;
+
 const BorderLine = styled.View`
-  ${borders.buildRadius('left', 2)};
   ${position.cover};
-  background-color: ${colors.lightGrey};
-  left: ${({ insetLeft }) => (insetLeft || 0)};
-  right: ${({ insetRight }) => (insetRight || 0)};
+  ${({ horizontal, ...props }) => (
+    horizontal
+      ? horizontalBorderLineStyles(props)
+      : verticalBorderLineStyles(props)
+  )}
+  background-color: ${({ color }) => color};
+  bottom: 0;
+  top: 0;
 `;
 
 const Container = styled(Row)`
   background-color: ${colors.white};
   flex-shrink: 0;
-  height: 2;
-  width: 100%;
+  height: ${({ horizontal }) => (horizontal ? '2' : '100%')};
+  width: ${({ horizontal }) => (horizontal ? '100%' : '2')};
 `;
 
-const Divider = ({ insetLeft, insetRight, ...props }) => (
-  <Container {...props}>
+const Divider = ({ color, horizontal, ...props }) => (
+  <Container {...props} horizontal={horizontal}>
     <BorderLine
-      insetLeft={insetLeft}
-      insetRight={insetRight}
+      {...props}
+      color={color}
+      horizontal={horizontal}
     />
   </Container>
 );
 
 Divider.propTypes = {
+  color: PropTypes.string,
+  horizontal: PropTypes.bool,
+  insetBottom: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
   insetLeft: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
   insetRight: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+  insetTop: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
 };
 
 Divider.defaultProps = {
+  color: colors.lightGrey,
+  horizontal: true,
   insetLeft: 19,
   insetRight: false,
 };
