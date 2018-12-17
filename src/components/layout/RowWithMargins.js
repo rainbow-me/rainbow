f974c8753edfb243266029f95e4553b35ac21683
Michael Demarais <m.demarais@gmail.com>
2018-12-16 22:20:21 -0500

    Create LayoutWithMargin components

---
+++ src/components/layout/RowWithMargins.js
@@ -0,0 +1,11 @@
+import { compose, defaultProps, setDisplayName, withProps } from 'recompact';
+import LayoutWithMargins from './LayoutWithMargins';
+
+export default compose(
+  setDisplayName('RowWithMargins'),
+  defaultProps({ margin: 19 }),
+  withProps({
+    direction: 'row',
+    marginKey: 'marginRight',
+  }),
+)(LayoutWithMargins);
