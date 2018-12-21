99dc72f785d755ae25c0cc2876d9e6768938380f
jinchung <jin.chung17@gmail.com>
2018-12-20 19:13:53 -0800

    add tracking to profile screen

--- src/screens/ProfileScreenWithData.js
+++ src/screens/ProfileScreenWithData.js
@@ -1,13 +1,16 @@
 import { compose, shouldUpdate, withHandlers } from 'recompact';
+import { setDisplayName } from 'recompose';
 import {
   withAccountAddress,
   withAccountTransactions,
   withBlurTransitionProps,
   withRequests,
+  withTrackingScreen,
 } from '../hoc';
 import ProfileScreen from './ProfileScreen';
 
 export default compose(
+  setDisplayName('ProfileScreen'),
   withAccountAddress,
   withAccountTransactions,
   withBlurTransitionProps,
@@ -16,6 +19,7 @@ export default compose(
     onPressBackButton: ({ navigation }) => () => navigation.navigate('WalletScreen'),
     onPressSettings: ({ navigation }) => () => navigation.navigate('SettingsModal'),
   }),
+  withTrackingScreen,
   /*
   shouldUpdate((props, { isScreenActive, ...nextProps }) => {
     if (!isScreenActive) return false;
