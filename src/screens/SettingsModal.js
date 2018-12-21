b7bd000d07d26987f623b52127d5cf3ebe1183b3
Michael Demarais <m.demarais@gmail.com>
2018-12-20 21:07:42 -0500

    Cleanup Backup page in Settings Modal

--- src/screens/SettingsModal.js
+++ src/screens/SettingsModal.js
@@ -1,8 +1,7 @@
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { compose, withHandlers, withProps } from 'recompact';
-import styled from 'styled-components/primitives';
+import React, { createElement } from 'react';
+import { compose, onlyUpdateForKeys, withHandlers, withProps } from 'recompact';
 import { Column } from '../components/layout';
 import { Modal, ModalHeader } from '../components/modal';
 import { AnimatedPager } from '../components/pager';
@@ -14,64 +13,58 @@ import {
 } from '../components/settings-menu';
 
 const SettingsPages = {
-  backup: 'Backup',
-  currency: 'Currency',
-  default: 'Settings',
-  language: 'Language',
-  security: 'Security',
-};
-
-const Content = styled(Column)`
-  flex: 1;
-  overflow: hidden;
-`;
-
-const renderActiveSettingsSection = currentPage => {
-  switch (currentPage) {
-    case SettingsPages.language:
-      return <LanguageSection />;
-
-    case SettingsPages.currency:
-      return <CurrencySection />;
-
-    case SettingsPages.backup:
-      return <BackupSection />;
-
-    case SettingsPages.default:
-    default:
-      return null;
-  }
+  backup: {
+    title: 'Backup',
+    component: BackupSection,
+  },
+  currency: {
+    title: 'Currency',
+    component: CurrencySection,
+  },
+  default: {
+    title: 'Settings',
+    component: null,
+  },
+  language: {
+    title: 'Language',
+    component: LanguageSection,
+  },
 };
 
 const SettingsModal = ({
   currentSettingsPage,
+  navigation,
   onCloseModal,
   onPressBack,
   onPressSection,
-}) => (
-  <Modal onCloseModal={onCloseModal}>
-    <Content>
-      <ModalHeader
-        onPressBack={onPressBack}
-        onPressClose={onCloseModal}
-        showBackButton={currentSettingsPage !== SettingsPages.default}
-        title={currentSettingsPage}
-      />
-      <AnimatedPager
-        isOpen={currentSettingsPage !== SettingsPages.default}
-        style={{ top: ModalHeader.height }}
-      >
-        <SettingsSection
-          onPressBackup={onPressSection(SettingsPages.backup)}
-          onPressCurrency={onPressSection(SettingsPages.currency)}
-          onPressLanguage={onPressSection(SettingsPages.language)}
-          onPressSecurity={onPressSection(SettingsPages.security)}
+}) => {
+  const { component, title } = currentSettingsPage;
+  const isDefaultPage = title === SettingsPages.default.title;
+
+  return (
+    <Modal onCloseModal={onCloseModal}>
+      <Column flex={1}>
+        <ModalHeader
+          onPressBack={onPressBack}
+          onPressClose={onCloseModal}
+          showBackButton={!isDefaultPage}
+          title={title}
         />
-        {renderActiveSettingsSection(currentSettingsPage)}
-      </AnimatedPager>
-    </Content>
-  </Modal>
-);
+        <AnimatedPager
+          isOpen={!isDefaultPage}
+          style={{ top: ModalHeader.height }}
+        >
+          <SettingsSection
+            onPressBackup={onPressSection(SettingsPages.backup)}
+            onPressCurrency={onPressSection(SettingsPages.currency)}
+            onPressLanguage={onPressSection(SettingsPages.language)}
+          />
+          {component && createElement(component, { navigation })}
+        </AnimatedPager>
+      </Column>
+    </Modal>
+  );
+};
 
 SettingsModal.propTypes = {
   currentSettingsPage: PropTypes.oneOf(Object.values(SettingsPages)),
@@ -90,4 +83,5 @@ export default compose(
     onPressBack: ({ navigation }) => () => navigation.setParams({ section: SettingsPages.default }),
     onPressSection: ({ navigation }) => (section) => () => navigation.setParams({ section }),
   }),
+  onlyUpdateForKeys(['currentSettingsPage']),
 )(SettingsModal);
