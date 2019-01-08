ca220ff5174f0ec5c561135291420d97d8531e79
Michael Demarais <m.demarais@gmail.com>
2019-01-07 23:13:42 -0500

    create buildTextStyles utility

--- src/components/text/Text.js
+++ src/components/text/Text.js
@@ -1,22 +1,9 @@
 import { withProps } from 'recompact';
 import styled from 'styled-components/primitives';
-import { colors, fonts } from '../../styles';
+import { buildTextStyles } from '../../styles';
 
 const Text = styled.Text`
-  ${({ align }) => (
-    align
-      ? `text-align: ${align};`
-      : ''
-  )}
-  ${({ lineHeight }) => (
-    lineHeight
-      ? `line-height: ${fonts.lineHeight[lineHeight]};`
-      : ''
-  )}
-  color: ${({ color }) => (colors.get(color) || colors.dark)}
-  font-family: ${({ family }) => fonts.family[family || 'SFProText']};
-  font-size: ${({ size }) => fonts.size[size || 'medium']};
-  font-weight: ${({ weight }) => fonts.weight[weight || 'regular']};
+  ${buildTextStyles}
 `;
 
 export default withProps({ allowFontScaling: false })(Text);
