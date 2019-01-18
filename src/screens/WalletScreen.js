9297f04f7421247241440ee2640f4be63f62a173
jinchung <jin.chung17@gmail.com>
2019-01-18 11:18:19 -0500

    eslint fixes for changed files

--- src/screens/WalletScreen.js
+++ src/screens/WalletScreen.js
@@ -16,7 +16,7 @@ import BlurOverlay from '../components/BlurOverlay';
 import { FabWrapper } from '../components/fab';
 import { CameraHeaderButton, Header, ProfileHeaderButton } from '../components/header';
 import { Page } from '../components/layout';
-import { buildWalletSectionsSelector } from '../helpers/buildWalletSections';
+import buildWalletSectionsSelector from '../helpers/buildWalletSections';
 import { getShowShitcoinsSetting, updateShowShitcoinsSetting } from '../model/localstorage';
 import {
   withAccountRefresh,
@@ -68,7 +68,6 @@ class WalletScreen extends Component {
       assets,
       assetsTotal,
       isScreenActive,
-      onHideSplashScreen,
       trackingDate,
       uniqueTokens,
       updateTrackingDate,
