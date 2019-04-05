e493029b33e5550dea460fafac446483e6c1d13b
Michael Demarais <m.demarais@gmail.com>
2019-04-05 17:49:45 -0400

    Clean up OfflineBadge

---
+++ src/hoc/withNetInfo.js
@@ -0,0 +1,16 @@
+import NetInfo from '@react-native-community/netinfo';
+import { compose, lifecycle, withHandlers, withState } from 'recompact';
+
+const withNetInfo = ComponentToWrap => compose(
+  withState('isConnected', 'setIsConnected', true),
+  lifecycle({
+    componentDidMount() {
+      NetInfo.isConnected.addEventListener('connectionChange', this.props.setIsConnected);
+    },
+    componentWillUnmount() {
+      NetInfo.isConnected.removeEventListener('connectionChange', this.props.setIsConnected);
+    },
+  }),
+)(ComponentToWrap);
+
+export default withNetInfo;
