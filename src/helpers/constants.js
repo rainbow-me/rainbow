97e61ffef2671f44b255c012be8bf44edb8f213e
jinchung <jin.chung17@gmail.com>
2019-04-23 18:43:29 -0400

    cleanup

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,13 +1,3 @@
-import {
-  Platform, StatusBar, Dimensions, NativeModules,
-} from 'react-native';
-
-export const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
-
-export const WINDOW_WIDTH = Dimensions.get('window').width;
-
-export const WINDOW_HEIGHT = Dimensions.get('window').height;
-
-export const WINDOW_HEIGHT_NO_STATUS_BAR = STATUS_BAR_HEIGHT ? WINDOW_HEIGHT - STATUS_BAR_HEIGHT : WINDOW_HEIGHT;
+import { Platform, NativeModules } from 'react-native';
 
 export const DEVICE_LANGUAGE = Platform.OS === 'ios' ? NativeModules.SettingsManager.settings.AppleLocale : NativeModules.I18nManager.localeIdentifier;
