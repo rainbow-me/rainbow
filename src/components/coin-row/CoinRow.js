de8cc8a828d653a53665e48cfc0602d88c82925f
osdnk <micosa97@gmail.com>
2019-05-08 09:08:53 +0200

    Add highlight

--- src/components/coin-row/CoinRow.js
+++ src/components/coin-row/CoinRow.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
 import React, { createElement } from 'react';
-import { compose, pure } from 'recompact';
+import { StyleSheet } from 'react-native';
+import { compose, omitProps, onlyUpdateForKeys, pure, withProps } from 'recompact';
 import styled from 'styled-components/primitives';
+import connect from 'react-redux/es/connect/connect';
 import { withAccountSettings } from '../../hoc';
 import { colors, padding } from '../../styles';
 import { CoinIcon } from '../coin-icon';
 import { Column, Row } from '../layout';
+import Flex from '../layout/Row';
+import selectedWithFab from '../../redux/selectedWithFab';
 
 const CoinRowPaddingVertical = 12;
 
@@ -16,12 +20,33 @@ const Container = styled(Row)`
 `;
 
 const Content = styled(Column)`
-  background-color: ${colors.white};
   height: ${CoinIcon.size};
   margin-left: ${CoinRowPaddingVertical};
 `;
 
+const Highlight = styled(Flex)`
+ left: 0;
+ top: 0;
+ right: 0;
+ bottom: 0;
+  position: absolute;
+  margin: 7px;
+  background-color: ${colors.lightestGrey};
+  border-radius: 10;
+`;
+
+const mapStateToProps = ({
+                           selectedWithFab: {
+                             selectedId,
+                           },
+                         }) => ({
+  selectedId,
+});
+
 const enhance = compose(
+  connect(mapStateToProps),
+  withProps(({ selectedId, uniqueId }) => ({ highlight: selectedId === uniqueId })),
+  omitProps('selectedId'),
   withAccountSettings,
   pure,
 );
@@ -32,12 +57,15 @@ const CoinRow = enhance(({
   coinIconRender,
   containerStyles,
   contentStyles,
+  highlight,
+  selectedId,
   onPress,
   symbol,
   topRowRender,
   ...props
 }) => (
-  <Container align="center" css={containerStyles}>
+  <Container align="center" css={containerStyles} color="red">
+    {highlight && <Highlight/>}
     {createElement(coinIconRender, { symbol, ...props })}
     <Content flex={1} justify="space-between" css={contentStyles}>
       <Row align="center" justify="space-between">
@@ -70,5 +98,6 @@ CoinRow.defaultProps = {
 };
 
 CoinRow.height = CoinIcon.size + (CoinRowPaddingVertical * 2);
+
 
 export default CoinRow;
