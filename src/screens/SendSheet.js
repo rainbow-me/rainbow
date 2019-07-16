9e21fc688988c626148e51616fb96c7919d6ac28
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2019-07-16 13:47:56 +0200

    add interface to contact list in send list

--- src/screens/SendSheet.js
+++ src/screens/SendSheet.js
@@ -215,6 +215,7 @@ class SendSheet extends Component {
               isValid={isValidAddress}
               onChangeAddressInput={sendUpdateRecipient}
               recipient={recipient}
+              onPressPaste={sendUpdateRecipient}
             />
             {showEmptyState && <SendContactList allAssets={this.state.contacts} />}
             {showAssetList && (
