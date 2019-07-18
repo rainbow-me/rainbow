0eb600e945932be284722a978593382519625f96
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2019-07-18 15:47:02 +0200

    make header list button independet of clipboard

--- src/components/buttons/AddContactButton.js
+++ src/components/buttons/AddContactButton.js
@@ -12,40 +12,11 @@ class AddContactButton extends PureComponent {
     onPress: PropTypes.func.isRequired,
   }
 
-  state = { clipboardContents: null }
-
-  componentDidMount() {
-    this.getClipboardContents();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.appState === 'background' && this.props.appState === 'active') {
-      this.getClipboardContents();
-    }
-  }
-
-  getClipboardContents = async () => Clipboard.getString().then(this.setClipboardContents)
-
-  handlePress = () => {
-    if (this.state.clipboardContents) {
-      this.props.onPress(this.state.clipboardContents);
-    }
-  }
-
-  setClipboardContents = async (clipboardContents) => {
-    if (await isValidAddress(clipboardContents)) {
-      this.setState({ clipboardContents });
-    }
-  }
-
   render() {
-    const { clipboardContents } = this.state;
-
     return (
       <Button
-        backgroundColor={clipboardContents ? colors.sendScreen.brightBlue : '#D2D3D7'}
-        disabled={!clipboardContents}
-        onPress={this.handlePress}
+        backgroundColor={colors.sendScreen.brightBlue}
+        onPress={this.props.onPress}
         size="small"
         type="pill"
       >
