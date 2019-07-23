53fb19e54485d76474a7d83fbbaac50a07ab8190
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2019-07-23 16:48:04 +0200

    change send lins input to detect nicknames of the contacts

--- src/components/fields/AddressField.js
+++ src/components/fields/AddressField.js
@@ -30,84 +30,78 @@ const PlaceholderText = styled(Label)`
   opacity: 0.45;
 `;
 
-const HeaderNameText = styled(Label)`
-  margin-top: -0.5px;
-  opacity: 1;
-  background-color: white;
-  z-index: 100;
-  width: 300px;
-  color: ${colors.appleBlue};
-  height: 22px;
-  line-height: 23px;
-`;
-
 const formatValue = value => (
   (isHexString(value) && (value.length === addressUtils.maxLength))
     ? abbreviations.address(value)
     : value
 );
 
-let input;
-
 export default class AddressField extends PureComponent {
   static propTypes = {
     address: PropTypes.string,
     autoFocus: PropTypes.bool,
     onChange: PropTypes.func.isRequired,
+    contacts: PropTypes.array,
+    currentContact: PropTypes.object,
   }
 
   state = {
+    currentContact: false,
+    inputValue: '',
     address: '',
     isValid: false,
     focused: true,
     forceShowNickname: false,
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    // Validate 'address' whenever its value changes
-    if (isNewValueForPath(this.state, prevState, 'address')) {
-      this.validateAddress(this.state.address);
-    }
-
-    // Allow component state to be overwritten by parent component through the
-    // use of the 'address' prop. Assume that 'address' is valid because redux handles that for us.
-    if (this.props.address && !this.state.address) {
+  componentDidUpdate(props) {
+    if (this.props.currentContact.nickname !== props.currentContact.nickname ) {
+      this.setState({ 
+        currentContact: this.props.currentContact,
+        inputValue: this.props.currentContact.nickname ? this.props.currentContact.nickname : this.props.address,
+        address: this.props.address,
+        isValid: true,
+      });
+    } else if (this.props.address && !this.state.address) {
       this.setState({
+        inputValue: this.props.currentContact.nickname ? this.props.currentContact.nickname : this.props.address,
         address: this.props.address,
         isValid: true,
       });
     }
   }
 
-  componentWillReceiveProps(props) {
-    if (props.headerName !== this.props.headerName) {
-      this.setState({forceShowNickname: true});
+  onChange = ({ nativeEvent }) => {
+    const contact = this.findContactByNickname(nativeEvent.text);
+    if(contact) {
+      this.props.onChange(contact.address);
+      this.validateAddress(contact.address);
+      return true;
     }
+    this.props.onChange(nativeEvent.text);
+    this.validateAddress(nativeEvent.text);
+    return this.setState({ address: nativeEvent.text });
   }
 
-  onChange = ({ nativeEvent }) => this.props.onChange(nativeEvent.text)
+  onChangeText = inputValue => this.setState({ inputValue });
 
-  onChangeText = address => this.setState({ address })
-
-  validateAddress = async (inputValue) => {
-    const isValid = await isValidAddress(inputValue);
+  validateAddress = async (address) => {
+    const isValid = await isValidAddress(address);
     return this.setState({ isValid });
   }
 
-  onBlur = () => {
-    this.setState({ focused: false });
-  }
-  onPressNickName = () => {
-    this.setState({ focused: true, forceShowNickname: false });
-    setTimeout(() => {
-      input.focus();
-    }, 50);
+  findContactByNickname = (nickname) => {
+    for (let i = 0; i < this.props.contacts.length; i++) {
+      if (this.props.contacts[i].nickname == nickname) {
+        return this.props.contacts[i];
+      }
+    }
   }
 
   render() {
-    const { autoFocus, headerName, ...props } = this.props;
-    const { address, isValid } = this.state;
-
+    const { autoFocus, ...props } = this.props;
+    const { inputValue, isValid } = this.state;
+    
     return (
       <Row flex={1}>
         <AddressInput
@@ -121,23 +115,16 @@ export default class AddressField extends PureComponent {
           onChange={this.onChange}
           onChangeText={this.onChangeText}
           selectTextOnFocus={true}
-          value={formatValue(address)}
+          value={formatValue(inputValue)}
           onBlur={this.onBlur}
         />
-        {!address && (
+        {!inputValue && (
           <Placeholder>
             <TouchableWithoutFeedback onPress={this.onPressNickName} >
               <PlaceholderText>ENS or Address (</PlaceholderText>
             </TouchableWithoutFeedback>
             <PlaceholderText family="SFMono">0x</PlaceholderText>
             <PlaceholderText>...)</PlaceholderText>
-          </Placeholder>
-        )}
-        {headerName.length > 0 && (!this.state.focused || this.state.forceShowNickname) && (
-          <Placeholder>
-            <TouchableWithoutFeedback onPress={this.onPressNickName} >
-              <HeaderNameText>{headerName}</HeaderNameText>
-            </TouchableWithoutFeedback>
           </Placeholder>
         )}
       </Row>
