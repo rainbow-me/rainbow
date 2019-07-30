c2a830408a569c791bc799f46cc4ae189a8649ad
Michael Demarais <m.demarais@gmail.com>
2019-07-30 03:39:15 -0700

    Create CoolButton component lol please lets rename this tho idk

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -3,3 +3,4 @@ export { default as Button } from './Button';
 export { default as PasteAddressButton } from './PasteAddressButton';
 export { default as HoldToAuthorizeButton } from './HoldToAuthorizeButton';
 // export { default as LongPressButton } from './LongPressButton';
+export { default as CoolButton } from './CoolButton';
