86eb71c4a7a93ac77330ce0bed19a5e2bb0b312b
jinchung <jin.chung17@gmail.com>
2019-08-20 22:31:47 -0700

    setup for hooking in precision estimation calcuation

--- src/helpers/__tests__/utilities.test.js
+++ src/helpers/__tests__/utilities.test.js
@@ -1,22 +1,22 @@
-import { determinePrecisionToDisplay } from '../utilities';
+import { updatePrecisionToDisplay } from '../utilities';
 
-test('determinePrecisionToDisplay', () => {
-  const result = determinePrecisionToDisplay('0.123456789', '3.00');
+test('updatePrecisionToDisplay', () => {
+  const result = updatePrecisionToDisplay('0.123456789', '3.00');
   expect(result).toBe('0.123');
 });
 
-test('determinePrecisionToDisplay', () => {
-  const result = determinePrecisionToDisplay('0.123456789', '32.04');
+test('updatePrecisionToDisplay', () => {
+  const result = updatePrecisionToDisplay('0.123456789', '32.04');
   expect(result).toBe('0.1235');
 });
 
-test('determinePrecisionToDisplay', () => {
-  const result = determinePrecisionToDisplay('0.123456789', '132.00');
+test('updatePrecisionToDisplay', () => {
+  const result = updatePrecisionToDisplay('0.123456789', '132.00');
   expect(result).toBe('0.12346');
 });
 
-test('determinePrecisionToDisplay', () => {
-  const result = determinePrecisionToDisplay('0.123456789', '1320.00');
+test('updatePrecisionToDisplay', () => {
+  const result = updatePrecisionToDisplay('0.123456789', '1320.00');
   expect(result).toBe('0.123457');
 });
 
