c0e7f8079fdcc65642aaa61c7f8ac4fa9bcd7d6d
jinchung <jin.chung17@gmail.com>
2019-08-31 00:51:12 -0400

    setup for hooking in gas prices into exchange

---
+++ src/hoc/withGas.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { compose } from 'recompact';
+import { gasUpdateGasPrice, gasUpdateGasPriceOption } from '../redux/gas';
+
+const mapStateToProps = ({ gas }) => ({
+  gasLimit: gas.gasLimit,
+  gasPrices: gas.gasPrices,
+  isSufficientGas: gas.isSufficientGas,
+  selectedGasPrice: gas.selectedGasPrice,
+  selectedGasPriceOption: gas.selectedGasPriceOption,
+  txFees: gas.txFees,
+});
+
+export default Component => compose(
+  connect(mapStateToProps, {
+    gasUpdateGasPrice,
+    gasUpdateGasPriceOption,
+  }),
+)(Component);
