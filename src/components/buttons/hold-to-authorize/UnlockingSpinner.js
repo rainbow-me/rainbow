e1a6cffb1ffc87d4749089ec00b683ff79b85880
jinchung <jin.chung17@gmail.com>
2019-11-18 21:24:37 -0500

    Add countdown animation for unlocking spinner

--- src/components/buttons/hold-to-authorize/UnlockingSpinner.js
+++ src/components/buttons/hold-to-authorize/UnlockingSpinner.js
@@ -1,33 +1,58 @@
+import AnimateNumber from '@bankify/react-native-animate-number';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { colors } from '../../../styles';
 import { ColumnWithMargins, RowWithMargins } from '../../layout';
 import Spinner from '../../Spinner';
 import { Text } from '../../text';
 
-const UnlockingSpinner = ({ timeRemaining }) => (
-  <ColumnWithMargins align="center" justify="center" margin={2}>
-    <RowWithMargins margin={8}>
-      <Spinner duration={1200} />
-      <Text color="white" lineHeight="loose" size="large" weight="semibold">
-        Unlocking
-      </Text>
-    </RowWithMargins>
-    {timeRemaining && (
-      <Text
-        color={colors.alpha(colors.white, 0.4)}
-        lineHeight="tight"
-        size="smedium"
-        weight="medium"
-      >
-        {`~ ${timeRemaining} Remaining`}
-      </Text>
-    )}
-  </ColumnWithMargins>
+const renderCountdownText = animatedTimeRemaining => (
+  <Text
+    color={colors.alpha(colors.white, 0.4)}
+    lineHeight="tight"
+    size="smedium"
+    weight="medium"
+  >
+    {`~ ${animatedTimeRemaining}s Remaining`}
+  </Text>
 );
 
+const UnlockingSpinner = ({ interval, timeRemaining }) => {
+  const formatter = useCallback(
+    animatedNumber =>
+      Math.max(Math.ceil((timeRemaining - animatedNumber) / interval), 0),
+    [interval, timeRemaining]
+  );
+
+  return (
+    <ColumnWithMargins align="center" justify="center" margin={2}>
+      <RowWithMargins margin={8}>
+        <Spinner duration={1200} />
+        <Text color="white" lineHeight="loose" size="large" weight="semibold">
+          Unlocking
+        </Text>
+      </RowWithMargins>
+      {timeRemaining && (
+        <AnimateNumber
+          formatter={formatter}
+          interval={interval}
+          renderContent={renderCountdownText}
+          steps={timeRemaining / interval}
+          timing="linear"
+          value={timeRemaining}
+        />
+      )}
+    </ColumnWithMargins>
+  );
+};
+
 UnlockingSpinner.propTypes = {
-  timeRemaining: PropTypes.string,
+  interval: PropTypes.number,
+  timeRemaining: PropTypes.number,
+};
+
+UnlockingSpinner.defaultProps = {
+  interval: 1000,
 };
 
 export default React.memo(UnlockingSpinner);
