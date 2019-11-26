b18e47fba48fce799fd351fc3ad21bf83c2ec08a
blokboy <proxxist@gmail.com>
2019-11-25 22:35:13 -0500

    finishing up animation and making sure the compound math makes sense

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -17,13 +17,12 @@ export const COMPOUND_MARKET_QUERY = gql`
   }
 `;
 
-
-// Create Queries for DAI and for USDC
 export const COMPOUND_DAI_ACCOUNT_TOKEN_QUERY = gql`
-  query accountCToken($address: String!){
-    accountCToken(where: { id: $address }) {
+  query accountCToken($addr: String!) {
+    accountCToken(where: { id: $addr }) {
+      cTokenBalance 
       id 
-      lifetimeSupplyInterestAccrued 
+      lifetimeSupplyInterestAccrued
       market {
         blockTimestamp
         id 
@@ -40,8 +39,24 @@ export const COMPOUND_DAI_ACCOUNT_TOKEN_QUERY = gql`
   }
 `
 
-// export const  COMPOUND_USDC_ACCOUNT_TOKEN_QUERY = gql`
-//   query accountCToken() {
-//     accountCToken()
-//   }
-// `
+export const COMPOUND_USDC_ACCOUNT_TOKEN_QUERY = gql`
+  query accountCToken {
+    accountCToken(id: "0x39aa39c021dfbae8fac545936693ac917d5e7563-0xf0f21ab2012731542731df194cff6c77d29cb31a") {
+      cTokenBalance
+      id 
+      lifetimeSupplyInterestAccrued 
+      market {
+        blockTimestamp
+        id 
+        exchangeRate 
+        interestRateModelAddress 
+        name 
+        supplyRate  
+        underlyingAddress 
+        underlyingDecimals
+        underlyingPriceUSD
+        underlyingPrice
+      }
+    }
+ }
+`
--- src/components/investment-cards/CompoundInvestmentCard.js
+++ src/components/investment-cards/CompoundInvestmentCard.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import { floor } from 'lodash';
 import { withProps, withHandlers, compose } from 'recompact';
 import { withOpenInvestmentCards, withAccountSettings, withAccountData } from '../../hoc';
 import { Text } from '../text';
 import InvestmentCard from './InvestmentCard';
-import { colors, padding } from '../../styles';
+import { colors } from '../../styles';
 import { graphql } from '@apollo/react-hoc';
-import { COMPOUND_MARKET_QUERY, COMPOUND_DAI_ACCOUNT_TOKEN_QUERY } from '../../apollo/queries';
-import Divider from '../Divider';
+import { COMPOUND_DAI_ACCOUNT_TOKEN_QUERY, COMPOUND_USDC_ACCOUNT_TOKEN_QUERY } from '../../apollo/queries';
+
+import BigNumber from 'bignumber.js';
+import AnimateNumber from '@bankify/react-native-animate-number';
+
+/*
+  TODO: 
+   - Convert graphQL HoCs to useSavingsAccount hook 
+   - Finish animating number for DAI/USDC supply rates 
+   - Clean  up the component
+   - Look into moving enhance up a level so that we can use hooks inside of these functions
+*/
 
 const CompoundInvestmentCardHeight =  114;
 
@@ -21,20 +31,19 @@ const AssetLabel = withProps({
 const enhance = compose(
     withAccountSettings,
     withAccountData,
-    graphql(COMPOUND_MARKET_QUERY, {
-      props: ({ data }) => {
-        return data
-      }
+    graphql(COMPOUND_USDC_ACCOUNT_TOKEN_QUERY, {
+        //options: (props) => ({ variables: { addr: `0x39aa39c021dfbae8fac545936693ac917d5e7563-${props.accountAddress.toLocaleLowerCase('en')}` }}),
+        props: ({ data }) => {
+          console.log('usdc ', data);
+          return data;
+        }
     }),
     graphql(COMPOUND_DAI_ACCOUNT_TOKEN_QUERY, {
-        options: (props) => ({
-          variables: {
-            address: props.accountAddress ? `0xf5dce57282a584d2746faf1593d3121fcac444dc-${props.accountAddress}` : "0xf5dce57282a584d2746faf1593d3121fcac444dc-0x00000000af5a61acaf76190794e3fdf1289288a1"
-          },  
-        }),
-        props: ({ data }) => {
-          return data
-        }
+      //options: (props) => ({ variables: { addr: `0xf5dce57282a584d2746faf1593d3121fcac444dc-${props.accountAddress.toLocaleLowerCase('en')}` }}),
+      props: ({ data }) => {
+        console.log('dai ', data);
+        return data;
+      }
     }),
     withHandlers({
         onPressContainer: ({ item, onPress }) => () => {
@@ -47,15 +56,20 @@ const enhance = compose(
 
 const CompoundInvestmentCard = enhance(
 ({ 
-   isCollapsible, 
-   markets,
-   nativeCurrency, 
-   onPress, 
-   onPressContainer, 
-   openInvestmentCards, 
-   ...props 
+   accountCToken,
+   isCollapsible,
+   nativeCurrency,
+   onPress,
+   onPressContainer,
+   openInvestmentCards,
+   ...props
 }) => {
-  console.log('props ', props);
+
+  console.log(`props `, props)
+  const interestRate = BigNumber(accountCToken.market.supplyRate / 2104400).toString();
+  //const val = `${(Number(accountCToken.cTokenBalance) * Number(accountCToken.market.exchangeRate))}`
+  const val = `$${ (Number(accountCToken.cTokenBalance) * accountCToken.market.exchangeRate).toFixed(2) } $${ Number(accountCToken.lifetimeSupplyInterestAccrued).toFixed(8) }`
+  console.log('interest rate ', interestRate)
   return (
   <InvestmentCard
    {...props}
@@ -65,18 +79,20 @@ const CompoundInvestmentCard = enhance(
    headerProps={{
        color: colors.limeGreen,
        emoji: 'money_mouth_face',
-       title: '',
-       titleColor: '#D040FF',
-       value: markets.length 
-              ? markets[1].exchangeRate // Set names for markets and  use those  as  keys instead. We don't have a balance. But this will become cToken balance * exchangeRate for the token with LifetimeSupplyAccrued on the opposite end.
-              : 1
+       value: <AnimateNumber value={accountCToken.cTokenBalance} countBy={interestRate} timing={(interval= 1, progress) => { return interval * (1 - Math.sin(Math.PI*interestRate) )*10}}/>,
    }}
-        height={CompoundInvestmentCardHeight}
-      >
-
-      </InvestmentCard>
+    height={CompoundInvestmentCardHeight}
+    >
+    </InvestmentCard>
   );
-})
+});
+
+CompoundInvestmentCard.propTypes = {
+  accountCToken: PropTypes.object,
+  nativeCurrency: PropTypes.string,
+  onPress: PropTypes.func,
+  onPressContainer: PropTypes.func,
+};
 
 export default compose(
   withOpenInvestmentCards,
