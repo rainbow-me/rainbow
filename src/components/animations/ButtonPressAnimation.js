b1a4993cd74b4c4a2cc67159c82aafda0c0b1ddf
osdnk <micosa97@gmail.com>
2019-12-12 16:44:35 +0100

    rewrite button press animation

--- src/components/animations/ButtonPressAnimation.js
+++ src/components/animations/ButtonPressAnimation.js
@@ -1,41 +1,33 @@
-import { omit, pick } from 'lodash';
 import PropTypes from 'prop-types';
-import React, { Fragment, Component } from 'react';
+import React from 'react';
 import { InteractionManager } from 'react-native';
 import {
   createNativeWrapper,
   PureNativeButton,
-  State,
 } from 'react-native-gesture-handler';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 import Animated, { Easing } from 'react-native-reanimated';
-import { transformOrigin as transformOriginUtil } from 'react-native-redash';
 import stylePropType from 'react-style-proptype';
-import { animations, colors } from '../../styles';
+import { animations } from '../../styles';
 import { directionPropType } from '../../utils';
-import { contains, interpolate, timing } from './procs';
+import { transformOrigin as transformOriginUtil } from './procs';
 
 const {
+  neq,
   and,
+  timing,
+  startClock,
   block,
   call,
-  Clock,
+  eq,
+  or,
+  set,
   cond,
   createAnimatedComponent,
-  divide,
-  eq,
   event,
-  greaterThan,
-  lessThan,
   onChange,
-  or,
-  proc,
-  set,
   stopClock,
-  Value,
 } = Animated;
-
-const { ACTIVE, CANCELLED, END, FAILED, UNDETERMINED } = State;
 
 const AnimatedRawButton = createNativeWrapper(
   createAnimatedComponent(PureNativeButton),
@@ -44,14 +36,6 @@ const AnimatedRawButton = createNativeWrapper(
     shouldCancelWhenOutside: true,
   }
 );
-
-const AnimatedRawButtonPropBlacklist = [
-  'onLongPress',
-  'onPress',
-  'onPressStart',
-];
-
-const NOOP = () => undefined;
 
 const HapticFeedbackTypes = {
   impactHeavy: 'impactHeavy',
@@ -63,277 +47,267 @@ const HapticFeedbackTypes = {
   selection: 'selection',
 };
 
-const isBetweenProc = proc(
-  (scaleTo, defaultScale, lessThanCondition, greaterThanCondition) =>
-    or(
-      and(lessThan(scaleTo, defaultScale), lessThanCondition),
-      and(greaterThan(scaleTo, defaultScale), greaterThanCondition)
-    )
-);
+function useAnimatedValue(initialValue) {
+  const animatedRef = React.useRef(null);
 
-export default class ButtonPressAnimation extends Component {
-  static propTypes = {
-    activeOpacity: PropTypes.number,
-    children: PropTypes.any,
-    defaultScale: PropTypes.number,
-    disabled: PropTypes.bool,
-    duration: PropTypes.number,
-    easing: PropTypes.func,
-    enableHapticFeedback: PropTypes.bool,
-    exclusive: PropTypes.bool,
-    hapticType: PropTypes.oneOf(Object.keys(HapticFeedbackTypes)),
-    isInteraction: PropTypes.bool,
-    minLongPressDuration: PropTypes.number,
-    onLongPress: PropTypes.func,
-    onPress: PropTypes.func,
-    onPressStart: PropTypes.func,
-    scaleTo: PropTypes.number,
-    style: stylePropType,
-    tapRef: PropTypes.object,
-    transformOrigin: directionPropType,
-  };
-
-  static defaultProps = {
-    activeOpacity: 1,
-    defaultScale: animations.keyframes.button.from.scale,
-    duration: 200,
-    easing: Easing.bezier(0.25, 0.46, 0.45, 0.94),
-    enableHapticFeedback: true,
-    exclusive: true,
-    hapticType: HapticFeedbackTypes.selection,
-    minLongPressDuration: 500,
-    scaleTo: animations.keyframes.button.to.scale,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.clock = new Clock();
-    this.clockReversed = new Clock();
-    this.delayClock = new Clock();
-    this.gestureState = new Value(UNDETERMINED);
-    this.handle = undefined;
-    this.longPressDetected = false;
-    this.longPressTimeout = undefined;
-    this.scale = new Value(1);
-    this.shouldSpring = new Value(-1);
-
-    this.state = {
-      height: 0,
-      width: 0,
-    };
-
-    this.onGestureEvent = event([
-      {
-        nativeEvent: {
-          state: this.gestureState,
-        },
-      },
-    ]);
+  if (animatedRef.current == null) {
+    animatedRef.current = new Animated.Value(initialValue);
   }
 
-  componentWillUnmount = () => {
-    this.reset();
-  };
+  return animatedRef.current;
+}
 
-  clearInteraction = () => {
-    if (this.props.isInteraction && this.handle) {
-      InteractionManager.clearInteractionHandle(this.handle);
-      this.handle = undefined;
+function useAnimatedClock() {
+  const animatedRef = React.useRef(null);
+
+  if (animatedRef.current == null) {
+    animatedRef.current = new Animated.Clock();
+  }
+
+  return animatedRef.current;
+}
+
+export default function ButtonPressAnimation({
+  style,
+  children,
+  onPress,
+  scaleTo,
+  duration,
+  onPressStart,
+  easing,
+  transformOrigin,
+  isInteraction,
+  activeOpacity,
+  disabled,
+  onLongPress,
+  minLongPressDuration,
+  enableHapticFeedback,
+  hapticType,
+}) {
+  const gestureState = useAnimatedValue(0);
+  const scaleValue = useAnimatedValue(1);
+  const finished = useAnimatedValue(0);
+  const frameTime = useAnimatedValue(0);
+  const time = useAnimatedValue(0);
+  const toValue = useAnimatedValue(0.5);
+  const durationVal = useAnimatedValue(duration);
+  const animationState = useAnimatedValue(3);
+
+  const inteactionHandle = React.useRef();
+  const createHandle = React.useCallback(() => {
+    inteactionHandle.current = InteractionManager.createInteractionHandle();
+  });
+
+  const removeHandle = React.useCallback(() => {
+    if (inteactionHandle.current) {
+      InteractionManager.clearInteractionHandle(inteactionHandle.current);
+      inteactionHandle.current = null;
     }
-  };
+  });
 
-  clearLongPressListener = () => {
-    if (this.longPressTimeout) {
-      clearTimeout(this.longPressTimeout);
+  const longPressHandle = React.useRef();
+
+  const createLongPressHandle = React.useCallback(() => {
+    longPressHandle.current = setTimeout(() => {
+      onLongPress();
+      if (enableHapticFeedback) {
+        ReactNativeHapticFeedback.trigger(hapticType);
+      }
+    }, minLongPressDuration);
+  });
+
+  const removeLongPressHandle = React.useCallback(() => {
+    if (inteactionHandle.current) {
+      clearTimeout(longPressHandle.current);
+      longPressHandle.current = null;
     }
-  };
+  });
 
-  createInteraction = () => {
-    if (this.props.isInteraction && !this.handle) {
-      this.handle = InteractionManager.createInteractionHandle();
+  React.useEffect(() => () => {
+    removeLongPressHandle();
+    removeHandle();
+  });
+
+  const [layout, setLayout] = React.useState({ height: 0, width: 0 });
+
+  const onLayout = React.useCallback(
+    ({
+      nativeEvent: {
+        layout: { width, height },
+      },
+    }) => {
+      if (transformOrigin && !Object.values(layout).reduce((a, b) => a + b)) {
+        console.log(width, height, transformOrigin);
+        setLayout({ height, width });
+      }
     }
-  };
-
-  createLongPressListener = () => {
-    this.longPressDetected = false;
-    const { minLongPressDuration, onLongPress } = this.props;
-    if (onLongPress) {
-      this.longPressTimeout = setTimeout(
-        this.handleDetectedLongPress,
-        minLongPressDuration
-      );
-    }
-  };
-
-  handleHaptic = () => {
-    const { enableHapticFeedback, hapticType } = this.props;
-    if (enableHapticFeedback) {
-      ReactNativeHapticFeedback.trigger(hapticType);
-    }
-  };
-
-  handleLayout = ({ nativeEvent: { layout } }) => {
-    // only setState if height+width dont already exist
-    if (!Object.values(this.state).reduce((a, b) => a + b)) {
-      this.setState(prevState => pick(layout, Object.keys(prevState)));
-    }
-  };
-
-  handleDetectedLongPress = () => {
-    this.longPressDetected = true;
-    clearTimeout(this.longPressTimeout);
-    this.handleHaptic();
-    this.props.onLongPress();
-  };
-
-  handlePress = () => {
-    if (!this.longPressDetected && this.props.onPress) {
-      this.handleHaptic();
-      this.props.onPress();
-    }
-  };
-
-  handlePressStart = () => {
-    if (this.props.onPressStart) {
-      this.props.onPressStart();
-    }
-  };
-
-  reset = () => {
-    this.clearInteraction();
-    this.clearLongPressListener();
-  };
-
-  render = () => {
-    const {
-      activeOpacity,
-      children,
-      disabled,
-      exclusive,
-      style,
-      tapRef,
-      transformOrigin,
-      ...props
-    } = this.props;
-
-    const { height, width } = this.state;
-
+  );
+  const { offsetX, offsetY } = React.useMemo(() => {
     let offsetX = 0;
     let offsetY = 0;
 
     if (transformOrigin === 'left' || transformOrigin === 'right') {
-      offsetX = Math.floor(width / 2) * (transformOrigin === 'left' ? -1 : 1);
+      offsetX =
+        Math.floor(layout.width / 2) * (transformOrigin === 'left' ? -1 : 1);
     } else if (transformOrigin === 'bottom' || transformOrigin === 'top') {
-      offsetY = Math.floor(height / 2) * (transformOrigin === 'top' ? -1 : 1);
+      offsetY =
+        Math.floor(layout.height / 2) * (transformOrigin === 'top' ? -1 : 1);
     }
+    console.log(offsetX, offsetY);
+    return { offsetX, offsetY };
+  }, [layout.height, layout.width, transformOrigin]);
 
-    const scaleDiff =
-      this.props.defaultScale -
-      (this.props.defaultScale - this.props.scaleTo) / 2;
+  const zoomClock = useAnimatedClock();
 
-    const opacity =
-      this.props.scaleTo > this.props.defaultScale
-        ? activeOpacity
-        : interpolate(divide(this.scale, this.props.defaultScale), {
-            inputRange: [this.props.scaleTo, this.props.defaultScale],
-            outputRange: [activeOpacity, 1],
-          });
+  const onGestureEvent = React.useRef(
+    event([
+      {
+        nativeEvent: {
+          state: gestureState,
+        },
+      },
+    ]),
+    [gestureState]
+  ).current;
 
-    return (
-      <Fragment>
-        <AnimatedRawButton
-          {...omit(props, AnimatedRawButtonPropBlacklist)}
-          backgroundColor={colors.transparent}
-          enabled={!disabled}
-          exclusive={exclusive}
-          onGestureEvent={this.onGestureEvent}
-          onHandlerStateChange={this.onGestureEvent}
-          ref={tapRef}
-        >
-          <Animated.View
-            accessible
-            onLayout={transformOrigin ? this.handleLayout : NOOP}
-            style={[
-              style,
-              {
-                opacity,
-                transform: transformOriginUtil(offsetX, offsetY, {
-                  scale: this.scale,
-                }),
-              },
-            ]}
-          >
-            {children}
-          </Animated.View>
-        </AnimatedRawButton>
-        <Animated.Code
-          exec={block([
-            cond(eq(this.gestureState, ACTIVE), [
-              set(this.shouldSpring, 1),
-              stopClock(this.clockReversed),
-            ]),
-            cond(contains([FAILED, CANCELLED, END], this.gestureState), [
-              cond(
-                isBetweenProc(
-                  this.props.scaleTo,
-                  this.props.defaultScale,
-                  lessThan(this.scale, scaleDiff),
-                  greaterThan(this.scale, scaleDiff)
-                ),
-                block([stopClock(this.clock), set(this.shouldSpring, 0)])
-              ),
-              call([], this.reset),
-            ]),
+  const scale = React.useRef(
+    block([
+      cond(and(eq(gestureState, 4), eq(animationState, 3)), [
+        set(animationState, 0),
+      ]),
+      ...(onLongPress
+        ? [
             onChange(
-              this.gestureState,
+              eq(gestureState, 4),
+              call([gestureState], ([gs]) => {
+                if (gs === 4) {
+                  createLongPressHandle();
+                } else {
+                  removeLongPressHandle();
+                }
+              })
+            ),
+          ]
+        : []),
+      ...(isInteraction
+        ? [
+            onChange(
+              eq(gestureState, 4),
+              call([gestureState], ([gs]) => {
+                if (gs === 4) {
+                  createHandle();
+                } else {
+                  removeHandle();
+                }
+              })
+            ),
+          ]
+        : []),
+      ...(onPress
+        ? [
+            onChange(
+              gestureState,
               cond(
-                eq(this.gestureState, ACTIVE),
-                [
-                  call([], this.createInteraction),
-                  call([], this.createLongPressListener),
-                  call([], this.handlePressStart),
-                ],
-                cond(eq(this.gestureState, END), [call([], this.handlePress)])
-              )
-            ),
-            cond(
-              eq(this.shouldSpring, 1),
-              set(
-                this.scale,
-                timing({
-                  clock: this.clock,
-                  duration: this.props.duration,
-                  easing: this.props.easing,
-                  from: this.scale,
-                  to: this.props.scaleTo,
+                eq(gestureState, 5),
+                call([], () => {
+                  if (enableHapticFeedback) {
+                    ReactNativeHapticFeedback.trigger(hapticType);
+                  }
+                  onPress();
                 })
               )
             ),
-            cond(
-              and(
-                eq(this.shouldSpring, 0),
-                isBetweenProc(
-                  this.props.scaleTo,
-                  this.props.defaultScale,
-                  lessThan(this.scale, this.props.defaultScale),
-                  greaterThan(this.scale, this.props.defaultScale)
-                )
-              ),
-              set(
-                this.scale,
-                timing({
-                  clock: this.clockReversed,
-                  duration: this.props.duration,
-                  easing: this.props.easing,
-                  from: this.scale,
-                  to: this.props.defaultScale,
-                })
-              )
+          ]
+        : []),
+      ...(onPressStart
+        ? [
+            onChange(
+              eq(gestureState, 4),
+              cond(eq(gestureState, 4), call([], onPressStart))
             ),
-          ])}
-        />
-      </Fragment>
-    );
-  };
+          ]
+        : []),
+      cond(eq(animationState, 0), [
+        startClock(zoomClock),
+        set(finished, 0),
+        set(animationState, 1),
+        set(frameTime, 0),
+        set(time, 0),
+        set(toValue, scaleTo),
+      ]),
+      cond(and(eq(animationState, 1), neq(gestureState, 4), finished), [
+        set(finished, 0),
+        set(animationState, 2),
+        set(frameTime, 0),
+        set(time, 0),
+        set(toValue, 1),
+      ]),
+
+      cond(and(eq(animationState, 2), finished), [
+        set(animationState, 3),
+        stopClock(zoomClock),
+      ]),
+      cond(
+        or(eq(animationState, 1), eq(animationState, 2)),
+        timing(
+          zoomClock,
+          { finished, frameTime, position: scaleValue, time },
+          { duration: durationVal, easing, toValue }
+        )
+      ),
+      scaleValue,
+    ])
+  ).current;
+
+  return (
+    <AnimatedRawButton
+      onHandlerStateChange={onGestureEvent}
+      enabled={!disabled}
+    >
+      <Animated.View
+        onLayout={onLayout}
+        accessible
+        style={[
+          style,
+          {
+            opacity: activeOpacity,
+            transform: transformOriginUtil(offsetX, offsetY, {
+              scale,
+            }),
+          },
+        ]}
+      >
+        {children}
+      </Animated.View>
+    </AnimatedRawButton>
+  );
 }
+
+ButtonPressAnimation.propTypes = {
+  activeOpacity: PropTypes.number,
+  children: PropTypes.any,
+  disabled: PropTypes.bool,
+  duration: PropTypes.number,
+  easing: PropTypes.func,
+  enableHapticFeedback: PropTypes.bool,
+  hapticType: PropTypes.oneOf(Object.keys(HapticFeedbackTypes)),
+  isInteraction: PropTypes.bool,
+  minLongPressDuration: PropTypes.number,
+  onLongPress: PropTypes.func,
+  onPress: PropTypes.func,
+  onPressStart: PropTypes.func,
+  scaleTo: PropTypes.number,
+  style: stylePropType,
+  transformOrigin: directionPropType,
+};
+
+ButtonPressAnimation.defaultProps = {
+  activeOpacity: 1,
+  defaultScale: animations.keyframes.button.from.scale,
+  duration: 200,
+  easing: Easing.bezier(0.25, 0.46, 0.45, 0.94),
+  enableHapticFeedback: true,
+  hapticType: HapticFeedbackTypes.selection,
+  minLongPressDuration: 500,
+  scaleTo: animations.keyframes.button.to.scale,
+};
