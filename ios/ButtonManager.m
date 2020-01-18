30c0ba5626445be267afb5b0048288ba31eb9e0b
Alexey Kureev <a.g.kureev@gmail.com>
2020-01-18 00:48:14 +0000

    ButtonPressAnimation interface ported to NativeButton component

--- ios/ButtonManager.m
+++ ios/ButtonManager.m
@@ -10,5 +10,13 @@
 @interface RCT_EXTERN_MODULE(ButtonManager, RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(onPress, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onPressStart, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onLongPress, RCTBubblingEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(disabled, BOOL)
+RCT_EXPORT_VIEW_PROPERTY(duration, NSTimeInterval)
+RCT_EXPORT_VIEW_PROPERTY(enableHapticFeedback, BOOL)
+RCT_EXPORT_VIEW_PROPERTY(hapticType, NSString)
+RCT_EXPORT_VIEW_PROPERTY(minLongPressDuration, NSTimeInterval)
+RCT_EXPORT_VIEW_PROPERTY(scaleTo, CGFloat)
 
 @end 
--- ios/Extensions.swift
+++ ios/Extensions.swift
@@ -4,12 +4,48 @@
 //
 //  Created by Alexey Kureev on 13/01/2020.
 //
+
+fileprivate func generateHapticFeedback(_ hapticEffect: String) {
+  switch hapticEffect {
+  case "error":
+    let generator = UINotificationFeedbackGenerator()
+    generator.notificationOccurred(.error)
+    
+  case "success":
+    let generator = UINotificationFeedbackGenerator()
+    generator.notificationOccurred(.success)
+    
+  case "warning":
+    let generator = UINotificationFeedbackGenerator()
+    generator.notificationOccurred(.warning)
+    
+  case "light":
+      let generator = UIImpactFeedbackGenerator(style: .light)
+      generator.impactOccurred()
+
+  case "medium":
+      let generator = UIImpactFeedbackGenerator(style: .medium)
+      generator.impactOccurred()
+
+  case "heavy":
+      let generator = UIImpactFeedbackGenerator(style: .heavy)
+      generator.impactOccurred()
+  default:
+    let generator = UISelectionFeedbackGenerator()
+    generator.selectionChanged()
+  }
+}
+
 extension UIView {
   static func fromNib<T: UIView>() -> T {
     return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
   }
   
-  func animateQuickTap(duration: TimeInterval = 0.15, options: UIView.AnimationOptions = .curveEaseInOut, scale: CGFloat = 0.97) {
+  func animateQuickTap(
+    duration: TimeInterval = 0.15,
+    options: UIView.AnimationOptions = .curveEaseInOut,
+    scale: CGFloat = 0.97
+  ) {
     UIView.animate(withDuration: duration, delay: 0, options: options, animations: {
       self.transform = CGAffineTransform(scaleX: scale, y: scale)
     }, completion: { _ in
@@ -19,7 +55,15 @@ extension UIView {
     })
   }
   
-  func animateTapStart(duration: TimeInterval = 0.15, options: UIView.AnimationOptions = .curveEaseInOut, scale: CGFloat = 0.97) {
+  func animateTapStart(
+    duration: TimeInterval = 0.15,
+    options: UIView.AnimationOptions = .curveEaseInOut,
+    scale: CGFloat = 0.97,
+    useHaptic: String? = nil
+  ) {
+    if useHaptic != nil {
+      generateHapticFeedback(useHaptic!)
+    }
     UIView.animate(withDuration: duration, delay: 0, options: options, animations: {
       self.transform = CGAffineTransform(scaleX: scale, y: scale)
     })
