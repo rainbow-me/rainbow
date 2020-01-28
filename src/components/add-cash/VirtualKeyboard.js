afd0416f2b54abc09121a822da60d743b89e120d
jinchung <jin.chung17@gmail.com>
2020-01-28 14:22:26 -0500

    Wyre integration

--- src/components/add-cash/VirtualKeyboard.js
+++ src/components/add-cash/VirtualKeyboard.js
@@ -1,13 +1,10 @@
-'use strict';
-
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { Text, View, ViewPropTypes } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ViewPropTypes } from 'react-native';
 import styled from 'styled-components/primitives';
-import { colors } from '../../styles';
+import { colors, fonts } from '../../styles';
 import { ButtonPressAnimation } from '../animations';
 import { Icon } from '../icons';
-import styles from './VirtualKeyboardStyle';
 
 const Backspace = styled(Icon).attrs({
   align: 'center',
@@ -20,105 +17,92 @@ const Backspace = styled(Icon).attrs({
 
 const NumberColor = colors.alpha(colors.blueGreyDark, 0.8);
 
-class VirtualKeyboard extends Component {
-  static propTypes = {
-    decimal: PropTypes.bool,
-    onPress: PropTypes.func.isRequired,
-    pressMode: PropTypes.oneOf(['string', 'char']),
-    rowStyle: ViewPropTypes.style,
+const styles = StyleSheet.create({
+  backspace: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    width: 80,
+  },
+  container: {
+    alignItems: 'center',
+  },
+  number: {
+    fontFamily: fonts.family.SFProRounded,
+    fontSize: 44,
+    fontWeight: fonts.weight.bold,
+    lineHeight: 64,
+    textAlign: 'center',
+    width: 80,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+});
+
+const VirtualKeyboard = ({ decimal = true, onPress, rowStyle }) => {
+  const BackspaceButton = () => (
+    <ButtonPressAnimation
+      duration={35}
+      onPress={() => {
+        onPress('back');
+      }}
+      scaleTo={1.6}
+      style={styles.backspace}
+      transformOrigin="keyboard"
+    >
+      <Backspace />
+    </ButtonPressAnimation>
+  );
+
+  const Row = numbersArray => {
+    let cells = numbersArray.map(val => Cell(val));
+    return <View style={[styles.row, rowStyle]}>{cells}</View>;
   };
 
-  static defaultProps = {
-    decimal: true,
-    pressMode: 'string',
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: '',
-    };
-  }
-
-  Backspace() {
-    return (
-      <ButtonPressAnimation
-        duration={35}
-        onPress={() => {
-          this.onPress('back');
-        }}
-        scaleTo={1.6}
-        style={styles.backspace}
-        transformOrigin="keyboard"
+  const Cell = symbol => (
+    <ButtonPressAnimation
+      duration={35}
+      key={symbol}
+      onPress={() => {
+        onPress(symbol.toString());
+      }}
+      scaleTo={1.6}
+      transformOrigin="keyboard"
+      width={80}
+    >
+      <Text
+        style={[
+          styles.number,
+          { color: NumberColor },
+          { transform: [{ scale: 0.5 }] },
+        ]}
       >
-        <Backspace />
-      </ButtonPressAnimation>
-    );
-  }
+        {symbol}
+      </Text>
+    </ButtonPressAnimation>
+  );
 
-  Row(numbersArray) {
-    let cells = numbersArray.map(val => this.Cell(val));
-    return <View style={[styles.row, this.props.rowStyle]}>{cells}</View>;
-  }
-
-  Cell(symbol) {
-    return (
-      <ButtonPressAnimation
-        duration={35}
-        key={symbol}
-        onPress={() => {
-          this.onPress(symbol.toString());
-        }}
-        scaleTo={1.6}
-        transformOrigin="keyboard"
-        width={80}
-      >
-        <Text
-          style={[
-            styles.number,
-            { color: NumberColor },
-            { transform: [{ scale: 0.5 }] },
-          ]}
-        >
-          {symbol}
-        </Text>
-      </ButtonPressAnimation>
-    );
-  }
-
-  onPress(val) {
-    if (this.props.pressMode === 'string') {
-      let curText = this.state.text;
-      if (isNaN(val)) {
-        if (val === 'back') {
-          curText = curText.slice(0, -1);
-        } else if (curText < 1500) {
-          curText += val;
-        }
-      } else if (curText + val <= 1500) {
-        curText += val;
-      }
-      this.setState({ text: curText });
-      this.props.onPress(curText);
-    } /* if (props.pressMode == 'char')*/ else {
-      this.props.onPress(val);
-    }
-  }
-
-  render() {
-    return (
-      <View style={[styles.container, this.props.style]}>
-        {this.Row([1, 2, 3])}
-        {this.Row([4, 5, 6])}
-        {this.Row([7, 8, 9])}
-        <View style={[styles.row, this.props.rowStyle]}>
-          {this.props.decimal ? this.Cell('.') : <View style={{ flex: 1 }} />}
-          {this.Cell(0)}
-          {this.Backspace()}
-        </View>
+  return (
+    <View style={[styles.container]}>
+      {Row([1, 2, 3])}
+      {Row([4, 5, 6])}
+      {Row([7, 8, 9])}
+      <View style={[styles.row, rowStyle]}>
+        {decimal ? Cell('.') : <View style={{ flex: 1 }} />}
+        {Cell(0)}
+        {BackspaceButton()}
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
-module.exports = VirtualKeyboard;
+VirtualKeyboard.propTypes = {
+  decimal: PropTypes.bool,
+  onPress: PropTypes.func.isRequired,
+  rowStyle: ViewPropTypes.style,
+};
+
+export default VirtualKeyboard;
