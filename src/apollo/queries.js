11406a1c4888bee5e50a17de1e7904abb2d17c2c
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2020-02-18 18:30:27 +0100

    add missing queries

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -108,3 +108,49 @@ export const DIRECTORY_QUERY = gql`
     }
   }
 `;
+
+export const COMPOUND_ACCOUNT_QUERY = gql`
+  query account($id: ID!) {
+    account(id: $id) {
+      id
+      health
+      tokens(first: 15) {
+        borrowBalanceUnderlying
+        cTokenBalance
+        id
+        lifetimeSupplyInterestAccrued
+        supplyBalanceUnderlying
+        symbol
+        totalUnderlyingSupplied
+      }
+      totalBorrowValueInEth
+      totalCollateralValueInEth
+    }
+  }
+`;
+
+export const COMPOUND_ALL_MARKETS_QUERY = gql`
+  query markets {
+    markets(first: 15) {
+      blockTimestamp
+      exchangeRate
+      id
+      interestRateModelAddress
+      name
+      supplyRate
+      underlyingAddress
+      underlyingDecimals
+      underlyingPrice
+      underlyingPriceUSD
+    }
+  }
+`;
+
+export const COMPOUND_SUPPLY_RATE = gql`
+  query markets($cTokenAddress: ID!) {
+    markets(where: { id: $cTokenAddress }) {
+      id
+      supplyRate
+    }
+  }
+`;
