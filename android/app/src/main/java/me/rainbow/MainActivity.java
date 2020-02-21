fd47918b46f7f1a4e8f1cdc765a0df1bd646fd97
Bruno Barbieri <brunobar79@gmail.com>
2020-02-21 17:02:03 -0500

    fix splash screen color

--- android/app/src/main/java/me/rainbow/MainActivity.java
+++ android/app/src/main/java/me/rainbow/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
+        SplashScreen.show(this, true);
         super.onCreate(savedInstanceState);
   }
 
