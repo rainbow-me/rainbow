8ba3370434d36acfec195af8df420570ac8ca494
jinchung <jin.chung17@gmail.com>
2020-02-26 18:37:00 -0500

    Removing unused savings components

--- src/components/investment-cards/index.js
+++ src/components/investment-cards/index.js
@@ -2,4 +2,3 @@ export { default as InvestmentCard } from './InvestmentCard';
 export { default as InvestmentCardHeader } from './InvestmentCardHeader';
 export { default as InvestmentCardPill } from './InvestmentCardPill';
 export { default as UniswapInvestmentCard } from './UniswapInvestmentCard';
-export { default as CompoundInvestmentCard } from './CompoundInvestmentCard';
--- src/components/savings/index.js
+++ src/components/savings/index.js
@@ -2,4 +2,3 @@ export { default as APRPill } from './APRPill';
 export { default as SavingsSheetEmptyState } from './SavingsSheetEmptyState';
 export { default as SavingsPredictionStepper } from './SavingsPredictionStepper';
 export { default as SavingsSheetHeader } from './SavingsSheetHeader';
-export { default as SavingsWrapper } from './SavingsWrapper';
