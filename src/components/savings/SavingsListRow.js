4d5252e27969c4485dc1d399ce8f40a04a79e82c
Bruno Barbieri <brunobar79@gmail.com>
2020-02-26 13:55:49 -0500

    @brunobar79/link to savings sheet (#385)
    * link to savings sheet

--- src/components/savings/SavingsListRow.js
+++ src/components/savings/SavingsListRow.js
@@ -23,107 +23,113 @@ const SavingsListRow = ({ APY, currency, value, onPress }) => (
         [0, 6, 10, colors.dark, 0.14],
       ]}
     >
-      <Row
-        style={{
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          marginLeft: 5,
-          padding: 10,
-        }}
-      >
+      <ButtonPressAnimation onPress={onPress} scaleTo={value ? 0.92 : 1}>
         <Row
           style={{
             alignItems: 'center',
             justifyContent: 'space-between',
+            marginLeft: 5,
+            padding: 10,
           }}
         >
-          {currency ? (
-            <CoinIcon symbol={currency} size={26} style={{ marginRight: 7 }} />
-          ) : null}
-          {value ? (
-            <GradientText
-              colors={['#000000', '#2CCC00']}
-              steps={[0.6, 1]}
-              end={{ x: 0.9, y: 0.5 }}
-              style={{
-                color: colors.blueGreyDark,
-                fontSize: 16,
-                fontWeight: fonts.weight.semibold,
-                marginRight: 10,
-              }}
-            >
-              ${Number(value)}
-            </GradientText>
-          ) : (
-            <>
-              <Text
+          <Row
+            style={{
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            {currency ? (
+              <CoinIcon
+                symbol={currency}
+                size={26}
+                style={{ marginRight: 7 }}
+              />
+            ) : null}
+            {value ? (
+              <GradientText
+                colors={['#000000', '#2CCC00']}
+                steps={[0.6, 1]}
+                end={{ x: 0.9, y: 0.5 }}
                 style={{
                   color: colors.blueGreyDark,
                   fontSize: 16,
                   fontWeight: fonts.weight.semibold,
                   marginRight: 10,
-                  opacity: 0.5,
                 }}
               >
-                $0.00
-              </Text>
-              <ButtonPressAnimation onPress={onPress} scaleTo={0.92}>
-                <ShadowStack
-                  width={97}
-                  height={30}
-                  borderRadius={25}
-                  paddingHorizontal={8}
-                  backgroundColor="#575CFF"
-                  alignItems="center"
-                  flexDirection="row"
-                  shadows={[
-                    [0, 3, 5, colors.dark, 0.2],
-                    [0, 6, 10, colors.dark, 0.14],
-                  ]}
+                ${Number(value)}
+              </GradientText>
+            ) : (
+              <>
+                <Text
+                  style={{
+                    color: colors.blueGreyDark,
+                    fontSize: 16,
+                    fontWeight: fonts.weight.semibold,
+                    marginRight: 10,
+                    opacity: 0.5,
+                  }}
                 >
-                  <Icon name="plusCircled" color={colors.white} height={16} />
-                  <Text
-                    style={{
-                      color: colors.white,
-                      fontSize: 15,
-                      fontWeight: fonts.weight.semibold,
-                      marginLeft: -7.5,
-                      paddingHorizontal: 10,
-                    }}
+                  $0.00
+                </Text>
+                <ButtonPressAnimation onPress={onPress} scaleTo={0.92}>
+                  <ShadowStack
+                    width={97}
+                    height={30}
+                    borderRadius={25}
+                    paddingHorizontal={8}
+                    backgroundColor="#575CFF"
+                    alignItems="center"
+                    flexDirection="row"
+                    shadows={[
+                      [0, 3, 5, colors.dark, 0.2],
+                      [0, 6, 10, colors.dark, 0.14],
+                    ]}
                   >
-                    Deposit
-                  </Text>
-                </ShadowStack>
-              </ButtonPressAnimation>
-            </>
-          )}
-        </Row>
-        <Centered
-          style={{
-            height: 30,
-          }}
-        >
-          <LinearGradient
-            borderRadius={17}
-            overflow="hidden"
-            colors={['#2CCC00', '#FEBE44']}
-            end={{ x: 1, y: 1 }}
-            pointerEvents="none"
-            start={{ x: 0, y: 0 }}
-            opacity={0.12}
-            style={position.coverAsObject}
-          />
-          <GradientText
+                    <Icon name="plusCircled" color={colors.white} height={16} />
+                    <Text
+                      style={{
+                        color: colors.white,
+                        fontSize: 15,
+                        fontWeight: fonts.weight.semibold,
+                        marginLeft: -7.5,
+                        paddingHorizontal: 10,
+                      }}
+                    >
+                      Deposit
+                    </Text>
+                  </ShadowStack>
+                </ButtonPressAnimation>
+              </>
+            )}
+          </Row>
+          <Centered
             style={{
-              fontSize: 16,
-              fontWeight: fonts.weight.semibold,
-              paddingHorizontal: 10,
+              height: 30,
             }}
           >
-            {String(APY)}% APY
-          </GradientText>
-        </Centered>
-      </Row>
+            <LinearGradient
+              borderRadius={17}
+              overflow="hidden"
+              colors={['#2CCC00', '#FEBE44']}
+              end={{ x: 1, y: 1 }}
+              pointerEvents="none"
+              start={{ x: 0, y: 0 }}
+              opacity={0.12}
+              style={position.coverAsObject}
+            />
+            <GradientText
+              style={{
+                fontSize: 16,
+                fontWeight: fonts.weight.semibold,
+                paddingHorizontal: 10,
+              }}
+            >
+              {String(APY)}% APY
+            </GradientText>
+          </Centered>
+        </Row>
+      </ButtonPressAnimation>
     </ShadowStack>
   </Centered>
 );
@@ -138,6 +144,11 @@ SavingsListRow.propTypes = {
 export default compose(
   withNavigation,
   withHandlers({
-    onPress: ({ navigation }) => () => navigation.navigate('SavingsSheet'),
+    onPress: ({ navigation, value, currency }) => () => {
+      navigation.navigate('SavingsSheet', {
+        currency,
+        isEmpty: value ? false : true,
+      });
+    },
   })
 )(SavingsListRow);
