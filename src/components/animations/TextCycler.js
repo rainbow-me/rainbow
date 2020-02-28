4334d810fdaf1327605dd99a66290d6f64103ee4
Michael Demarais <m.demarais@gmail.com>
2020-02-28 02:51:57 -0500

    Cycle the TextCycler when pressed

--- src/components/animations/TextCycler.js
+++ src/components/animations/TextCycler.js
@@ -6,10 +6,11 @@ import React, {
   useRef,
   useState,
 } from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import Animated, { Easing } from 'react-native-reanimated';
 import { useTimingTransition } from 'react-native-redash';
 import { position } from '../../styles';
-import { isNewValueForObjectPaths } from '../../utils';
+import { magicMemo } from '../../utils';
 import { Centered } from '../layout';
 import { Text } from '../text';
 
@@ -17,11 +18,7 @@ const TextCyclerItem = ({ children, duration, renderer, selected }) => (
   <Animated.View
     style={{
       ...position.coverAsObject,
-      opacity: useTimingTransition(
-        selected,
-        duration,
-        Easing.inOut(Easing.ease)
-      ),
+      opacity: useTimingTransition(selected, duration, Easing.out(Easing.ease)),
     }}
   >
     {createElement(renderer, { children })}
@@ -30,7 +27,6 @@ const TextCyclerItem = ({ children, duration, renderer, selected }) => (
 
 const TextCycler = ({
   defaultSelectedIndex,
-  duration,
   height,
   interval,
   items,
@@ -38,54 +34,78 @@ const TextCycler = ({
   ...props
 }) => {
   const intervalRef = useRef();
+  const timeoutRef = useRef();
   const [selectedIndex, setSelectedIndex] = useState(defaultSelectedIndex);
 
   const cycleText = useCallback(() => {
     setSelectedIndex(index => (index + 1 > items.length - 1 ? 0 : index + 1));
   }, [items]);
 
-  useEffect(() => {
+  const startInterval = useCallback(() => {
     intervalRef.current = setInterval(cycleText, interval);
-    return () => clearInterval(intervalRef.current);
   }, [cycleText, interval]);
 
+  const stopInterval = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+  };
+
+  const stopTimeout = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  };
+
+  useEffect(() => {
+    startInterval();
+    return () => {
+      stopInterval();
+      stopTimeout();
+    };
+  }, [startInterval]);
+
+  const handlePress = useCallback(() => {
+    stopInterval();
+    stopTimeout();
+    cycleText();
+    timeoutRef.current = setTimeout(startInterval, interval);
+  }, [cycleText, interval, startInterval]);
+
   return (
-    <Centered height={height} width="100%" {...props}>
-      {items.map(item =>
-        createElement(TextCyclerItem, {
-          children: item,
-          duration,
-          key: item,
-          renderer,
-          selected: item === items[selectedIndex],
-        })
-      )}
-    </Centered>
+    <TouchableOpacity activeOpacity={0.69} onPress={handlePress}>
+      <Centered height={height} width="100%" {...props}>
+        {items.map(item =>
+          createElement(TextCyclerItem, {
+            children: item,
+            duration: interval,
+            key: item,
+            renderer,
+            selected: item === items[selectedIndex],
+          })
+        )}
+      </Centered>
+    </TouchableOpacity>
   );
 };
 
 TextCycler.propTypes = {
   defaultSelectedIndex: PropTypes.number,
-  duration: PropTypes.number,
   height: PropTypes.number.isRequired,
-  interval: PropTypes.number,
+  interval: PropTypes.number.isRequired,
   items: PropTypes.arrayOf(PropTypes.string),
   renderer: PropTypes.func,
 };
 
 TextCycler.defaultProps = {
   defaultSelectedIndex: 0,
-  duration: 250,
-  interval: 2500,
+  interval: 3000,
   renderer: Text,
 };
 
-const arePropsEqual = (...props) =>
-  !isNewValueForObjectPaths(...props, [
-    'duration',
-    'height',
-    'interval',
-    'items',
-  ]);
-
-export default React.memo(TextCycler, arePropsEqual);
+export default magicMemo(TextCycler, [
+  'duration',
+  'height',
+  'interval',
+  'items',
+]);
