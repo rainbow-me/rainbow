5938c7944b984336837beaa72c1499ca9cd20810
Michał Osadnik <micosa97@gmail.com>
2020-03-01 01:42:23 +0100

    revert experimental.js

--- src/config/experimental.js
+++ src/config/experimental.js
@@ -1,10 +1,10 @@
 /**
- * This fine contains flags for enabling features which are still in development.
+ * This file contains flags for enabling features which are still in development.
  * While adding new functionality, please verify it against these features
  * keeping in mind that they should never be broken
  */
 
-export const discoverSheetAvailable = true;
+export const discoverSheetAvailable = false;
 export const chartExpandedAvailable = false;
 export const nativeTransactionListAvailable = false;
 export const nativeButtonPressAnimationAvailable = false;
