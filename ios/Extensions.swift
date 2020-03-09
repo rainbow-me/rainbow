176da8bbbfe1259ccc0779ca1a67366bafd4620f
Alexey Kureev <a.g.kureev@gmail.com>
2020-03-09 23:06:23 +0000

    Use haptics on touchEnd

--- ios/Extensions.swift
+++ ios/Extensions.swift
@@ -40,16 +40,11 @@ extension UIView {
   static func fromNib<T: UIView>() -> T {
     return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
   }
-
+  
   func animateTapStart(
     duration: TimeInterval = 0.1,
-    scale: CGFloat = 0.97,
-    useHaptic: String? = nil
+    scale: CGFloat = 0.97
   ) {
-    if useHaptic != nil {
-      generateHapticFeedback(useHaptic!)
-    }
-    
     let timingFunction = CAMediaTimingFunction(controlPoints: 0.25, 0.46, 0.45, 0.94)
     
     CATransaction.begin()
@@ -62,7 +57,10 @@ extension UIView {
     CATransaction.commit()
   }
   
-  func animateTapEnd(duration: TimeInterval = 0.1) {
+  func animateTapEnd(duration: TimeInterval = 0.1, useHaptic: String? = nil) {
+    if useHaptic != nil {
+      generateHapticFeedback(useHaptic!)
+    }
     let timingFunction = CAMediaTimingFunction(controlPoints: 0.25, 0.46, 0.45, 0.94)
     
     CATransaction.begin()
