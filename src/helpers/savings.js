b14ef43f6fe06c82eb33de40b8ed75e360f9ffac
Bruno Barbieri <brunobar79@gmail.com>
2020-03-11 22:15:06 -0400

    calculate estimate earnings

--- src/helpers/savings.js
+++ src/helpers/savings.js
@@ -1,16 +1,31 @@
+const APROX_BLOCK_TIME = 15;
+
 const calculateAPY = supplyRate => {
   if (!supplyRate) return (0).toFixed(2);
-  const approxBlockTime = 15;
-  let blocksPerYear = (60 / approxBlockTime) * 60 * 24 * 365;
+  let blocksPerYear = (60 / APROX_BLOCK_TIME) * 60 * 24 * 365;
   const periodicRate = (supplyRate * 1) / blocksPerYear;
   return ((Math.pow(1 + periodicRate, blocksPerYear - 1) - 1) * 100).toFixed(2);
 };
 
 const calculateCompoundInterestPerBlock = (amount, apy) => {
-  const approxBlockTime = 15;
-  let blocksPerYear = (60 / approxBlockTime) * 60 * 24 * 365;
+  let blocksPerYear = (60 / APROX_BLOCK_TIME) * 60 * 24 * 365;
   const totalInterest = amount * (apy / 100);
   return (1 / blocksPerYear) * totalInterest;
 };
 
-export { calculateAPY, calculateCompoundInterestPerBlock };
+const calculateEarningsInDays = (amount, apr, days) => {
+  const apy = calculateAPY(apr);
+  const compoundInterestPerBlock = calculateCompoundInterestPerBlock(
+    amount,
+    apy
+  );
+  const blocksPerMinute = 60 / APROX_BLOCK_TIME;
+  return compoundInterestPerBlock * blocksPerMinute * 60 * 24 * days;
+};
+
+export {
+  calculateAPY,
+  calculateCompoundInterestPerBlock,
+  calculateEarningsInDays,
+  APROX_BLOCK_TIME,
+};
