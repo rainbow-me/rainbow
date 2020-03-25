408f5d35eec3009c6831f25612bb23fd8bf28942
Bruno Barbieri <brunobar79@gmail.com>
2020-03-25 02:04:42 -0400

    fix spinner animation when in bg

--- ios/Extensions.swift
+++ ios/Extensions.swift
@@ -143,3 +143,27 @@ extension UIView {
     layer.anchorPoint = point
   }
 }
+
+
+extension CALayer {
+    func pause() {
+        if self.isPaused() == false {
+            let pausedTime: CFTimeInterval = self.convertTime(CACurrentMediaTime(), from: nil)
+            self.speed = 0.0
+            self.timeOffset = pausedTime
+        }
+    }
+
+    func isPaused() -> Bool {
+        return self.speed == 0.0
+    }
+
+    func resume() {
+        let pausedTime: CFTimeInterval = self.timeOffset
+        self.speed = 1.0
+        self.timeOffset = 0.0
+        self.beginTime = 0.0
+        let timeSincePause: CFTimeInterval = self.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        self.beginTime = timeSincePause
+    }
+}
