12453ff3910705673ed274d58f52eaff6956d989
Bruno Barbieri <brunobar79@gmail.com>
2020-03-27 18:39:56 -0400

    fix

--- src/raps/actions/unlock.js
+++ src/raps/actions/unlock.js
@@ -3,8 +3,6 @@ import { greaterThan, isZero } from '../../helpers/utilities';
 import transactionStatusTypes from '../../helpers/transactionStatusTypes';
 import transactionTypes from '../../helpers/transactionTypes';
 import store from '../../redux/store';
-import transactionStatusTypes from '../../helpers/transactionStatusTypes';
-import transactionTypes from '../../helpers/transactionTypes';
 import { dataAddNewTransaction } from '../../redux/data';
 import { rapsAddOrUpdate } from '../../redux/raps';
 import { contractUtils, gasUtils } from '../../utils';
