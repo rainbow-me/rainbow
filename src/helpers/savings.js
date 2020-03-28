e134a4645010a83c2a2aaea095f21c2a1daba1a7
Christian Baroni <c@baroni.co>
2020-03-28 06:04:49 -0400

    Show more decimals

--- src/helpers/savings.js
+++ src/helpers/savings.js
@@ -1,7 +1,7 @@
 import BigNumber from 'bignumber.js';
 
 const APPROX_BLOCK_TIME = 15;
-const MAX_DECIMALS_TO_SHOW = 9;
+const MAX_DECIMALS_TO_SHOW = 10;
 const BLOCKS_PER_YEAR = (60 / APPROX_BLOCK_TIME) * 60 * 24 * 365;
 
 const calculateAPY = supplyRate => {
