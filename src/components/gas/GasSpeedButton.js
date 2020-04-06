15b695ce71b01fea539bfd58ba58f7b4b0cd9551
jinchung <jin.chung17@gmail.com>
2020-04-06 16:19:45 -0400

    Transform GasSpeedButton to use hooks

--- src/components/gas/GasSpeedButton.js
+++ src/components/gas/GasSpeedButton.js
@@ -1,12 +1,12 @@
 import AnimateNumber from '@bankify/react-native-animate-number';
-import analytics from '@segment/analytics-react-native';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { LayoutAnimation } from 'react-native';
-import { compose, mapProps, onlyUpdateForKeys, withProps } from 'recompact';
+import { useDispatch } from 'react-redux';
+import { withProps } from 'recompact';
 import ExchangeModalTypes from '../../helpers/exchangeModalTypes';
-import { withAccountSettings, withGas } from '../../hoc';
+import { useAccountSettings, useGas } from '../../hooks';
 import { colors, padding } from '../../styles';
 import { gasUtils } from '../../utils';
 import { ButtonPressAnimation } from '../animations';
@@ -35,25 +35,38 @@ const renderGasPriceText = animatedNumber => (
   </Text>
 );
 
-class GasSpeedButton extends PureComponent {
-  static propTypes = {
-    estimatedTimeUnit: PropTypes.string,
-    estimatedTimeValue: PropTypes.string,
-    gasPrices: PropTypes.object,
-    gasUpdateGasPriceOption: PropTypes.func,
-    nativeCurrencySymbol: PropTypes.string,
-    price: PropTypes.string,
-    selectedGasPriceOption: PropTypes.oneOf(gasUtils.GasSpeedOrder),
-    type: PropTypes.oneOf(Object.values(ExchangeModalTypes)),
-  };
+const getActionLabel = type => {
+  switch (type) {
+    case ExchangeModalTypes.deposit:
+      return 'Deposits in';
+    case ExchangeModalTypes.withdrawal:
+      return 'Withdraws in';
+    default:
+      return 'Swaps in';
+  }
+};
 
-  handlePress = () => {
-    const {
-      gasPrices,
-      gasUpdateGasPriceOption,
-      selectedGasPriceOption,
-    } = this.props;
+const GasSpeedButton = ({ type }) => {
+  const dispatch = useDispatch();
+  const { nativeCurrencySymbol } = useAccountSettings();
+  const {
+    gasUpdateGasPriceOption,
+    selectedGasPrice,
+    selectedGasPriceOption,
+  } = useGas();
 
+  const estimatedTime = get(
+    selectedGasPrice,
+    'estimatedTime.display',
+    ''
+  ).split(' ');
+
+  const gasPrice = get(selectedGasPrice, 'txFee.native.value.amount');
+  const estimatedTimeUnit = estimatedTime[1] || 'min';
+  const estimatedTimeValue = estimatedTime[0] || 0;
+  const price = isNaN(gasPrice) ? '0.00' : gasPrice;
+
+  const handlePress = () => {
     LayoutAnimation.easeInEaseOut();
 
     const currentSpeedIndex = gasUtils.GasSpeedOrder.indexOf(
@@ -63,93 +76,58 @@ class GasSpeedButton extends PureComponent {
       (currentSpeedIndex + 1) % gasUtils.GasSpeedOrder.length;
 
     const nextSpeed = gasUtils.GasSpeedOrder[nextSpeedIndex];
-    const nextSpeedGweiValue = get(gasPrices, `[${nextSpeed}].value.display`);
 
-    gasUpdateGasPriceOption(nextSpeed);
-    analytics.track('Updated Gas Price', { gasPrice: nextSpeedGweiValue });
+    dispatch(gasUpdateGasPriceOption(nextSpeed));
   };
 
-  formatAnimatedGasPrice = animatedPrice => {
+  const formatAnimatedGasPrice = animatedPrice => {
     const formattedPrice = parseFloat(animatedPrice).toFixed(3);
-    return `${this.props.nativeCurrencySymbol}${formattedPrice}`;
+    return `${nativeCurrencySymbol}${formattedPrice}`;
   };
 
-  getActionLabel = () => {
-    const { type } = this.props;
-    switch (type) {
-      case ExchangeModalTypes.deposit:
-        return 'Deposits in';
-      case ExchangeModalTypes.withdrawal:
-        return 'Withdraws in';
-      default:
-        return 'Swaps in';
-    }
-  };
-
-  formatAnimatedEstimatedTime = estimatedTime => {
-    const actionLabel = this.getActionLabel();
+  const formatAnimatedEstimatedTime = estimatedTime => {
+    const actionLabel = getActionLabel(type);
     const time = parseFloat(estimatedTime || 0).toFixed(0);
-    return `${actionLabel} ~ ${time} ${this.props.estimatedTimeUnit}`;
+    return `${actionLabel} ~ ${time} ${estimatedTimeUnit}`;
   };
 
-  render = () => (
+  return (
     <ButtonPressAnimation
       hapticType="impactHeavy"
-      onPress={this.handlePress}
+      onPress={handlePress}
       scaleTo={0.99}
       width="100%"
     >
       <Column css={padding(15, 19, 0)} width="100%">
         <Row align="center" justify="space-between">
           <AnimateNumber
-            formatter={this.formatAnimatedGasPrice}
+            formatter={formatAnimatedGasPrice}
             interval={6}
             renderContent={renderGasPriceText}
             steps={6}
             timing="linear"
-            value={this.props.price}
+            value={price}
           />
-          <GasSpeedLabelPager label={this.props.selectedGasPriceOption} />
+          <GasSpeedLabelPager label={selectedGasPriceOption} />
         </Row>
         <Row align="center" justify="space-between">
           <Label>Fee</Label>
           <AnimateNumber
-            formatter={this.formatAnimatedEstimatedTime}
+            formatter={formatAnimatedEstimatedTime}
             interval={1}
             renderContent={renderEstimatedTimeText}
             steps={6}
             timing="linear"
-            value={this.props.estimatedTimeValue}
+            value={estimatedTimeValue}
           />
         </Row>
       </Column>
     </ButtonPressAnimation>
   );
-}
+};
 
-export default compose(
-  withAccountSettings,
-  withGas,
-  mapProps(({ selectedGasPrice, ...props }) => {
-    const estimatedTime = get(
-      selectedGasPrice,
-      'estimatedTime.display',
-      ''
-    ).split(' ');
+GasSpeedButton.propTypes = {
+  type: PropTypes.oneOf(Object.values(ExchangeModalTypes)),
+};
 
-    const gasPrice = get(selectedGasPrice, 'txFee.native.value.amount');
-
-    return {
-      estimatedTimeUnit: estimatedTime[1] || 'min',
-      estimatedTimeValue: estimatedTime[0] || 0,
-      price: isNaN(gasPrice) ? '0.00' : gasPrice,
-      ...props,
-    };
-  }),
-  onlyUpdateForKeys([
-    'estimatedTimeUnit',
-    'estimatedTimeValue',
-    'price',
-    'selectedGasPriceOption',
-  ])
-)(GasSpeedButton);
+export default GasSpeedButton;
