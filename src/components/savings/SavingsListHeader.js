27d18de654b174a3a30af7a70715ff3bc0407001
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2020-04-07 11:32:39 +0200

    reverse savings list header changes to resolve reload issue

--- src/components/savings/SavingsListHeader.js
+++ src/components/savings/SavingsListHeader.js
@@ -3,11 +3,13 @@ import React from 'react';
 import FastImage from 'react-native-fast-image';
 import Animated, { Easing } from 'react-native-reanimated';
 import { toRad, useTimingTransition } from 'react-native-redash';
+import { compose } from 'recompact';
 import CaretImageSource from '../../assets/family-dropdown-arrow.png';
 import { ButtonPressAnimation, interpolate } from '../animations';
 import Highlight from '../Highlight';
 import { Row, RowWithMargins } from '../layout';
 import { Emoji, Text, TruncatedText } from '../text';
+import withOpenSavings from '../../hoc/withOpenSavings';
 
 const AnimatedFastImage = Animated.createAnimatedComponent(FastImage);
 
@@ -17,19 +19,19 @@ const TokenFamilyHeaderHeight = 44;
 const SavingsListHeader = ({
   emoji,
   highlight,
-  isOpen,
-  onPress,
+  openSavings,
   savingsSumValue,
+  setOpenSavings,
 }) => {
-  const animation = useTimingTransition(isOpen, {
+  const animation = useTimingTransition(openSavings, {
     duration: TokenFamilyHeaderAnimationDuration,
     easing: Easing.bezier(0.25, 0.1, 0.25, 1),
   });
 
   return (
     <ButtonPressAnimation
-      key={`${emoji}_${isOpen}`}
-      onPress={onPress}
+      key={`${emoji}_${openSavings}`}
+      onPress={() => setOpenSavings(!openSavings)}
       scaleTo={1.05}
     >
       <Row
@@ -52,25 +54,23 @@ const SavingsListHeader = ({
           </TruncatedText>
         </RowWithMargins>
         <RowWithMargins align="center" margin={13}>
-          {savingsSumValue && (
-            <Animated.View
-              style={{
-                opacity: interpolate(animation, {
-                  inputRange: [0, 1],
-                  outputRange: [1, 0],
-                }),
-              }}
+          <Animated.View
+            style={{
+              opacity: interpolate(animation, {
+                inputRange: [0, 1],
+                outputRange: [1, 0],
+              }),
+            }}
+          >
+            <Text
+              align="right"
+              color="dark"
+              size="large"
+              style={{ marginBottom: 1 }}
             >
-              <Text
-                align="right"
-                color="dark"
-                size="large"
-                style={{ marginBottom: 1 }}
-              >
-                ${Number(savingsSumValue).toFixed(2)}
-              </Text>
-            </Animated.View>
-          )}
+              ${Number(savingsSumValue).toFixed(2)}
+            </Text>
+          </Animated.View>
           <AnimatedFastImage
             resizeMode={FastImage.resizeMode.contain}
             source={CaretImageSource}
@@ -104,13 +104,13 @@ SavingsListHeader.height = TokenFamilyHeaderHeight;
 SavingsListHeader.propTypes = {
   emoji: PropTypes.string,
   highlight: PropTypes.bool,
-  isOpen: PropTypes.bool,
-  onPress: PropTypes.func,
+  openSavings: PropTypes.bool,
   savingsSumValue: PropTypes.number,
+  setOpenSavings: PropTypes.func,
 };
 
 SavingsListHeader.defaultProps = {
   emoji: 'sunflower',
 };
 
-export default SavingsListHeader;
+export default compose(withOpenSavings)(SavingsListHeader);
