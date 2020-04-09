2fc0d8270e1cb766e2b0e77280888630d86cea92
jinchung <jin.chung17@gmail.com>
2020-04-09 14:36:08 -0400

    Create add cash redux and limit hook

---
+++ src/hooks/useAddCashLimits.js
@@ -0,0 +1,51 @@
+import { differenceInDays, differenceInYears } from 'date-fns';
+import { findIndex, sumBy, take } from 'lodash';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+
+const DEFAULT_DAILY_LIMIT = 250;
+const DEFAULT_YEARLY_LIMIT = 1500;
+
+const findRemainingAmount = (limit, purchaseTransactions, index) => {
+  const transactionsInTimeline =
+    index >= 0 ? take(purchaseTransactions, index) : purchaseTransactions;
+  const purchasedAmount = sumBy(transactionsInTimeline, txn =>
+    Number(txn.sourceAmount)
+  );
+  return limit - purchasedAmount;
+};
+
+export default function useAddCashLimits() {
+  const purchaseTransactions = useSelector(
+    ({ addCash: { purchaseTransactions } }) => purchaseTransactions
+  );
+
+  const limits = useMemo(() => {
+    const now = Date.now();
+
+    const firstIndexBeyondToday = findIndex(
+      purchaseTransactions,
+      txn => differenceInDays(now, txn.timestamp) > 1
+    );
+    const dailyRemainingLimit = findRemainingAmount(
+      DEFAULT_DAILY_LIMIT,
+      purchaseTransactions,
+      firstIndexBeyondToday
+    );
+
+    const firstIndexBeyondThisYear = findIndex(
+      purchaseTransactions,
+      txn => differenceInYears(now, txn.timestamp) > 1,
+      Math.max(firstIndexBeyondToday, 0)
+    );
+    const yearlyRemainingLimit = findRemainingAmount(
+      DEFAULT_YEARLY_LIMIT,
+      purchaseTransactions,
+      firstIndexBeyondThisYear
+    );
+
+    return { dailyRemainingLimit, yearlyRemainingLimit };
+  }, [purchaseTransactions]);
+
+  return limits;
+}
