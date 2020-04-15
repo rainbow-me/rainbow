757aed4d9f0aed567c55e32e5ae9f53e136d3787
Bruno Barbieri <brunobar79@gmail.com>
2020-04-15 17:24:38 -0400

    support ens and fix address formatting

--- src/hoc/withAccountSettings.js
+++ src/hoc/withAccountSettings.js
@@ -10,8 +10,22 @@ import {
 } from '../redux/settings';
 
 const mapStateToProps = ({
-  settings: { accountAddress, chainId, language, nativeCurrency, network },
-}) => ({ accountAddress, chainId, language, nativeCurrency, network });
+  settings: {
+    accountAddress,
+    accountENS,
+    chainId,
+    language,
+    nativeCurrency,
+    network,
+  },
+}) => ({
+  accountAddress,
+  accountENS,
+  chainId,
+  language,
+  nativeCurrency,
+  network,
+});
 
 const languageSelector = state => state.language;
 const nativeCurrencySelector = state => state.nativeCurrency;
