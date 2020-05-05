e1a93fd780d72d7e369a42ba26459d75a0a6c194
Bruno Barbieri <brunobar79@gmail.com>
2020-05-04 21:29:16 -0400

    added backup & restore methods

--- src/model/keychain.js
+++ src/model/keychain.js
@@ -1,10 +1,18 @@
 import { captureException } from '@sentry/react-native';
+import DeviceInfo from 'react-native-device-info';
 import {
+  ACCESS_CONTROL,
+  ACCESSIBLE,
+  AUTHENTICATION_TYPE,
+  canImplyAuthentication,
+  getAllInternetCredentials,
   getInternetCredentials,
   resetInternetCredentials,
   setInternetCredentials,
 } from 'react-native-keychain';
 import { logger } from '../utils';
+
+const RAINBOW_BACKUP_KEY = 'rainbowBackup';
 
 // NOTE: implement access control for iOS keychain
 export async function saveString(key, value, accessControlOptions) {
@@ -27,6 +35,17 @@ export async function loadString(key, authenticationPrompt) {
     logger.log(`Keychain: string does not exist for key: ${key}`);
   } catch (err) {
     logger.log(`Keychain: failed to load string for key: ${key} error: ${err}`);
+    captureException(err);
+  }
+  return null;
+}
+
+export async function loadAllKeys(authenticationPrompt) {
+  try {
+    const { results } = await getAllInternetCredentials(authenticationPrompt);
+    return results;
+  } catch (err) {
+    logger.log(`Keychain: failed to load all items`, err);
     captureException(err);
   }
   return null;
@@ -62,4 +81,76 @@ export async function remove(key) {
     );
     captureException(err);
   }
+}
+
+export async function backupToIcloud() {
+  const results = await loadAllKeys();
+  const parsedResults = results.map(result => {
+    let password = result.password;
+    try {
+      password = JSON.parse(password);
+      // eslint-disable-next-line no-empty
+    } catch (e) {}
+    return {
+      ...result,
+      password,
+    };
+  });
+  const backup = JSON.stringify(parsedResults);
+  try {
+    await saveString(RAINBOW_BACKUP_KEY, backup, {
+      ACCESSIBLE: ACCESSIBLE.WHEN_UNLOCKED,
+      synchronizable: true,
+    });
+    console.log('DONE BACKING UP TO ICLOUD');
+  } catch (e) {
+    console.log('Error while backing up', e);
+  }
+}
+
+export async function restoreBackup() {
+  const results = await getInternetCredentials(RAINBOW_BACKUP_KEY, {
+    synchronizable: true,
+  });
+
+  const backedUpData = JSON.parse(results.password);
+  const publicAccessControlOptions = {
+    accessible: ACCESSIBLE.ALWAYS_THIS_DEVICE_ONLY,
+  };
+
+  let privateAccessControlOptions = {};
+  const canAuthenticate = await canImplyAuthentication({
+    authenticationType: AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
+  });
+
+  let isSimulator = false;
+
+  if (canAuthenticate) {
+    isSimulator = __DEV__ && (await DeviceInfo.isEmulator());
+  }
+  if (canAuthenticate && !isSimulator) {
+    privateAccessControlOptions = {
+      accessControl: ACCESS_CONTROL.USER_PRESENCE,
+      accessible: ACCESSIBLE.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+    };
+  }
+
+  backedUpData.forEach(async item => {
+    let accessControl = publicAccessControlOptions;
+    if (
+      item.username.indexOf('rainbowSeedPhrase') !== -1 ||
+      item.username.indexOf('rainbowPrivateKey') !== -1
+    ) {
+      accessControl = privateAccessControlOptions;
+    }
+
+    if (typeof item.password === 'string') {
+      await saveString(item.username, item.password, accessControl);
+    } else {
+      await saveObject(item.username, item.password, accessControl);
+    }
+    console.log('RESTORED KEY ', item.username);
+  });
+
+  return results;
 }
