2d3f67bb03baa5a335223c7ae0a801e6843942d8
jinchung <jin.chung17@gmail.com>
2020-05-05 22:22:45 -0400

    Support OpenSea for Rinkeby testnet

--- src/handlers/opensea-api.js
+++ src/handlers/opensea-api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import NetworkTypes from '../helpers/networkTypes';
 import { parseAccountUniqueTokens } from '../parsers/uniqueTokens';
 import { logger } from '../utils';
 
@@ -6,19 +7,18 @@ export const UNIQUE_TOKENS_LIMIT_PER_PAGE = 50;
 export const UNIQUE_TOKENS_LIMIT_TOTAL = 350;
 
 const api = axios.create({
-  baseURL: 'https://api.opensea.io/api/v1',
   headers: {
     Accept: 'application/json',
   },
   timeout: 20000, // 20 secs
 });
 
-export const apiGetAccountUniqueTokens = async (address, page) => {
+export const apiGetAccountUniqueTokens = async (network, address, page) => {
   try {
+    const networkPrefix = network === NetworkTypes.mainnet ? '' : `${network}-`;
     const offset = page * UNIQUE_TOKENS_LIMIT_PER_PAGE;
-    const data = await api.get(
-      `/assets?exclude_currencies=true&owner=${address}&limit=${UNIQUE_TOKENS_LIMIT_PER_PAGE}&offset=${offset}`
-    );
+    const url = `https://${networkPrefix}api.opensea.io/api/v1/assets?exclude_currencies=true&owner=${address}&limit=${UNIQUE_TOKENS_LIMIT_PER_PAGE}&offset=${offset}`;
+    const data = await api.get(url);
     return parseAccountUniqueTokens(data);
   } catch (error) {
     logger.log('Error getting unique tokens', error);
