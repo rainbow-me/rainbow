d73dc91d3bdbf3943bf119b759d45dd47601215b
jinchung <jin.chung17@gmail.com>
2020-05-11 18:47:21 -0400

    Fix unlock asset check that was comparing raw vs non-raw amounts

--- src/utils/__tests__/contract.test.js
+++ src/utils/__tests__/contract.test.js
@@ -1,14 +1,14 @@
 import { greaterThan } from '../../helpers/utilities';
 import Contract from '../contract';
 
-const { getAllowance } = Contract;
+const { getRawAllowance } = Contract;
 
 const accountAddress = '0x1492004547FF0eFd778CC2c14E794B26B4701105';
 
-it('getAllowanceZrx', async () => {
+it('getRawAllowanceZrx', async () => {
   const exchangeAddress = '0xaE76c84C9262Cdb9abc0C2c8888e62Db8E22A0bF';
   const tokenAddress = '0xe41d2489571d322189246dafa5ebde1f4699f498';
-  const allowance = await getAllowance(
+  const allowance = await getRawAllowance(
     accountAddress,
     { address: tokenAddress, decimals: 18 },
     exchangeAddress
@@ -17,10 +17,10 @@ it('getAllowanceZrx', async () => {
   expect(result).toBeTruthy();
 });
 
-it('getAllowanceMkr', async () => {
+it('getRawAllowanceMkr', async () => {
   const exchangeAddress = '0x2C4Bd064b998838076fa341A83d007FC2FA50957';
   const tokenAddress = '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2';
-  const allowance = await getAllowance(
+  const allowance = await getRawAllowance(
     accountAddress,
     { address: tokenAddress, decimals: 18 },
     exchangeAddress
@@ -29,11 +29,10 @@ it('getAllowanceMkr', async () => {
   expect(result).toBeTruthy();
 });
 
-// TODO
-xit('getAllowanceBatNotApproved', async () => {
+xit('getRawAllowanceBatNotApproved', async () => {
   const exchangeAddress = '0x2E642b8D59B45a1D8c5aEf716A84FF44ea665914';
   const tokenAddress = '0x0d8775f648430679a709e98d2b0cb6250d2887ef';
-  const allowance = await getAllowance(
+  const allowance = await getRawAllowance(
     accountAddress,
     { address: tokenAddress, decimals: 18 },
     exchangeAddress
