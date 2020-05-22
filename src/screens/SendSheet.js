511ee55251e5b2b670daad1705a2078dae1ab0a0
Wojciech Stanisz <42337257+wojtus7@users.noreply.github.com>
2020-05-22 14:41:20 +0200

    Add ActionSheet and fix send sheet for android (#692)
    * add action sheet for android
    
    * remodel show transaction speed to work on ondroid
    
    * unlink action sheet

--- src/screens/SendSheet.js
+++ src/screens/SendSheet.js
@@ -6,6 +6,7 @@ import {
   InteractionManager,
   Keyboard,
   KeyboardAvoidingView,
+  Platform,
   StatusBar,
 } from 'react-native';
 import { getStatusBarHeight, isIphoneX } from 'react-native-iphone-x-helper';
@@ -394,7 +395,10 @@ const SendSheet = ({ setAppearListener, ...props }) => {
   return (
     <SheetContainer>
       <StatusBar barStyle="light-content" />
-      <KeyboardAvoidingView behavior="padding">
+      <KeyboardAvoidingView
+        enabled={Platform.OS !== 'android'}
+        behavior="padding"
+      >
         <Container align="center">
           <SendHeader
             contacts={contacts}
