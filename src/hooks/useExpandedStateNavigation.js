0cbbd118d6f26d95f80c714f49e61d14e23d0fbd
jinchung <jin.chung17@gmail.com>
2020-05-24 20:45:57 -0400

    Pass along default input to swap from expanded state

--- src/hooks/useExpandedStateNavigation.js
+++ src/hooks/useExpandedStateNavigation.js
@@ -1,15 +1,29 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { Alert, InteractionManager } from 'react-native';
 import { useNavigation, useNavigationState } from 'react-navigation-hooks';
+import AssetInputTypes from '../helpers/assetInputTypes';
 import useAsset from './useAsset';
 import useWallets from './useWallets';
 
-export default function useExpandedStateNavigation() {
+export default function useExpandedStateNavigation(inputType) {
   const { goBack, navigate } = useNavigation();
   const { params } = useNavigationState();
   const { isReadOnlyWallet } = useWallets();
 
   const asset = useAsset(params.asset);
+
+  const navigationPayload = useMemo(() => {
+    switch (inputType) {
+      case AssetInputTypes.in:
+        return {
+          inputAsset: asset,
+        };
+      case AssetInputTypes.out:
+        return { outputAsset: asset };
+      default:
+        return { asset };
+    }
+  }, [asset, inputType]);
 
   return useCallback(
     route => {
@@ -18,9 +32,9 @@ export default function useExpandedStateNavigation() {
       return isReadOnlyWallet
         ? Alert.alert(`You need to import the wallet in order to do this`)
         : InteractionManager.runAfterInteractions(() =>
-            navigate(route, { asset })
+            navigate(route, navigationPayload)
           );
     },
-    [asset, goBack, isReadOnlyWallet, navigate]
+    [goBack, isReadOnlyWallet, navigate, navigationPayload]
   );
 }
--- src/screens/SwapModal.js
+++ src/screens/SwapModal.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import { useNavigationParam } from 'react-navigation-hooks';
 import ExchangeModalTypes from '../helpers/exchangeModalTypes';
 import createUnlockAndSwapRap, {
   estimateUnlockAndSwap,
 } from '../raps/unlockAndSwap';
 import ExchangeModal from './ExchangeModal';
 
-const SwapModal = (props, ref) => (
-  <ExchangeModal
-    createRap={createUnlockAndSwapRap}
-    estimateRap={estimateUnlockAndSwap}
-    inputHeaderTitle="Swap"
-    showOutputField
-    ref={ref}
-    type={ExchangeModalTypes.swap}
-    {...props}
-  />
-);
+const SwapModal = (props, ref) => {
+  const defaultInputAsset = useNavigationParam('inputAsset');
+  const defaultOutputAsset = useNavigationParam('outputAsset');
+
+  return (
+    <ExchangeModal
+      createRap={createUnlockAndSwapRap}
+      defaultInputAsset={defaultInputAsset}
+      defaultOutputAsset={defaultOutputAsset}
+      estimateRap={estimateUnlockAndSwap}
+      inputHeaderTitle="Swap"
+      ref={ref}
+      showOutputField
+      type={ExchangeModalTypes.swap}
+      {...props}
+    />
+  );
+};
+
 export default React.forwardRef(SwapModal);
