eba89386045081d2c2f8e9180dcda9b3d12fe7de
osdnk <micosa97@gmail.com>
2020-06-12 15:13:20 +0200

    Make it faster :rocket:

--- ios/SafeStoreReview.m
+++ ios/SafeStoreReview.m
@@ -9,26 +9,135 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTBridge.h>
 #import <StoreKit/SKStoreReviewController.h>
+#import <objc/runtime.h>
+
+@interface MonitorObject:NSObject
+
+@property (nonatomic, weak) UIWindow* owner;
+
+-(id)init:(UIWindow*)owner;
+
+@end
+
+
+@interface UIWindow (DismissNotification)
+
++ (void)load;
+
+@end
+
+@implementation MonitorObject
+
+
+-(id)init:(UIWindow*)owner
+{
+  NSLog(@"@#$@#inti$@");
+  self = [super init];
+  self.owner = owner;
+  [[NSNotificationCenter defaultCenter] postNotificationName:UIWindowDidBecomeVisibleNotification object:self];
+  return self;
+  
+}
+
+@end
+
+
+
+@implementation UIWindow (DismissNotification)
+
+static NSString* monitorObjectKey = @"monitorKey";
+static NSString* partialDescForStoreReviewWindow =  @"SKStore";
++ (void)load
+{
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    Class class = [self class];
+    
+    SEL originalSelector = @selector(setWindowLevel:);
+    SEL swizzledSelector = @selector(setWindowLevel_startMonitor:);
+    
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+    
+    BOOL didAddMethod =
+    class_addMethod(class,
+                    originalSelector,
+                    method_getImplementation(swizzledMethod),
+                    method_getTypeEncoding(swizzledMethod));
+    
+    if (didAddMethod) {
+      class_replaceMethod(class,
+                          swizzledSelector,
+                          method_getImplementation(originalMethod),
+                          method_getTypeEncoding(originalMethod));
+    } else {
+      method_exchangeImplementations(originalMethod, swizzledMethod);
+    }
+  });
+}
+
+
+#pragma mark - Method Swizzling
+
+- (void)setWindowLevel_startMonitor:(int)level{
+  [self setWindowLevel_startMonitor:level];
+  
+  if([self.description containsString:partialDescForStoreReviewWindow])
+  {
+    MonitorObject *monObj = [[MonitorObject alloc] init:self];
+    objc_setAssociatedObject(self, &monitorObjectKey, monObj, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    
+  }
+}
+
+@end
+
+
 
 @interface RainbowRequestReviewManager : NSObject <RCTBridgeModule>
 @end
 
-@implementation RainbowRequestReviewManager
+@implementation RainbowRequestReviewManager {
+  RCTResponseSenderBlock _callback;
+}
 
 - (dispatch_queue_t)methodQueue {
   return dispatch_get_main_queue();
 }
 
+- (instancetype)init
+{
+  self = [super init];
+  if (self) {
+     [[NSNotificationCenter defaultCenter] addObserver:self
+                                                selector:@selector(windowDidBecomeVisibleNotification:)
+                                                    name:UIWindowDidBecomeVisibleNotification
+                                                  object:nil];
+  }
+  return self;
+}
+
+- (void)windowDidBecomeVisibleNotification:(NSNotification *)notification
+{
+  if (_callback != nil) {
+    _callback(@[[[NSNumber alloc] initWithBool:YES]]);
+    _callback = nil;
+  }
+}
+
+
 RCT_EXPORT_MODULE(RainbowRequestReview);
 
 RCT_EXPORT_METHOD(requestReview:(RCTResponseSenderBlock)callback) {
-  NSUInteger windowCount = [UIApplication sharedApplication].windows.count;
   [SKStoreReviewController requestReview];
-  NSUInteger noSecondsToWait = 1; // TODO verify on devices
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, noSecondsToWait * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-    NSUInteger newWindowCount = [UIApplication sharedApplication].windows.count;
-    callback(@[[[NSNumber alloc] initWithBool:newWindowCount != windowCount]]);
+  _callback = callback;
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.01 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+    if (_callback != nil) {
+      _callback(@[[[NSNumber alloc] initWithBool:NO]]);
+      _callback = nil;
+    }
   });
+  
 }
 
 @end
