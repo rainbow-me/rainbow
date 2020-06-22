74280629972ee459a02037b3725a706376c7a3dd
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2020-06-22 19:01:00 +0200

    add Share button

--- src/screens/ReceiveModal.js
+++ src/screens/ReceiveModal.js
@@ -1,37 +1,32 @@
 import { toLower } from 'lodash';
 import React from 'react';
 import { Platform, Share } from 'react-native';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+// import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components/primitives';
-import Divider from '../components/Divider';
 import QRCodeDisplay from '../components/QRCodeDisplay';
-import { FloatingEmojis } from '../components/floating-emojis';
-import { Column } from '../components/layout';
-import {
-  Modal,
-  ModalFooterButton,
-  ModalFooterButtonsRow,
-  ModalHeader,
-} from '../components/modal';
-import { Br, Monospace, Text } from '../components/text';
-import { useClipboard } from '../hooks';
-import { useNavigation } from '../navigation/Navigation';
-import { colors } from '../styles';
-import { haptics } from '../utils';
+import { ButtonPressAnimation } from '../components/animations';
+// import { FloatingEmojis } from '../components/floating-emojis';
+import { Icon } from '../components/icons';
+import { Centered, Column } from '../components/layout';
+import { Text } from '../components/text';
+// import { useClipboard } from '../hooks';
+// import { useNavigation } from '../navigation/Navigation';
+import { colors, fonts, position } from '../styles';
+// import { haptics } from '../utils';
 
-const statusBarHeight = getStatusBarHeight(true);
+// const statusBarHeight = getStatusBarHeight(true);
 const QRCodeSize = Platform.OS === 'ios' ? 250 : 190;
 
-const AddressText = styled(Monospace).attrs({
-  color: colors.alpha(colors.blueGreyDark, 0.6),
-})`
-  font-size: 13.86;
-  letter-spacing: null;
-  line-height: 19;
-  text-align: justify;
-  width: 100%;
-`;
+// const AddressText = styled(Monospace).attrs({
+//   color: colors.alpha(colors.blueGreyDark, 0.6),
+// })`
+//   font-size: 13.86;
+//   letter-spacing: null;
+//   line-height: 19;
+//   text-align: justify;
+//   width: 100%;
+// `;
 
 const TopHandle = styled.View`
   width: 35px;
@@ -40,15 +35,37 @@ const TopHandle = styled.View`
   background-color: ${colors.white};
 `;
 
+const ButtonWrapper = styled(Centered)`
+  width: 123px;
+  height: 56px;
+  border-radius: 28px;
+  border: 0.5px solid rgba(0, 0, 0, 0.06);
+  box-shadow: 0px 10px 30px rgba(37, 41, 46, 0.2);
+  background-color: #25292e;
+  justify-content: center;
+  padding-bottom: 2px;
+`;
+
+const ButtonIcon = styled(Icon)`
+  ${position.maxSize('110%')};
+  margin-right: 9;
+`;
+
+const IconContainer = styled(Centered).attrs({
+  grow: 0,
+  shrink: 0,
+})`
+  ${position.size(18)};
+`;
+
 const ReceiveModal = () => {
-  const { setClipboard } = useClipboard();
-  const { goBack } = useNavigation();
+  // const { setClipboard } = useClipboard();
+  // const { goBack } = useNavigation();
   const accountAddress = useSelector(({ settings: { accountAddress } }) =>
     toLower(accountAddress)
   );
 
   return (
-    // <Modal height={360} radius={39} marginBottom={statusBarHeight} onCloseModal={goBack} margin={20}>
     <Column align="center">
       <TopHandle />
       <Column
@@ -61,8 +78,24 @@ const ReceiveModal = () => {
       >
         <QRCodeDisplay size={QRCodeSize} value={accountAddress} />
       </Column>
+      <ButtonPressAnimation
+        onPress={() =>
+          Share.share({
+            message: accountAddress,
+            title: 'My account address:',
+          })
+        }
+      >
+        <ButtonWrapper>
+          <IconContainer>
+            <ButtonIcon color="white" name="share" />
+          </IconContainer>
+          <Text color="white" size={fonts.size.larger} weight="semibold">
+            Share
+          </Text>
+        </ButtonWrapper>
+      </ButtonPressAnimation>
     </Column>
-    //   <ModalFooterButtonsRow>
     //     <FloatingEmojis
     //       distance={250}
     //       duration={500}
@@ -84,18 +117,6 @@ const ReceiveModal = () => {
     //         />
     //       )}
     //     </FloatingEmojis>
-    //     <ModalFooterButton
-    //       icon="share"
-    //       label="Share"
-    //       onPress={() =>
-    //         Share.share({
-    //           message: accountAddress,
-    //           title: 'My account address:',
-    //         })
-    //       }
-    //     />
-    //   </ModalFooterButtonsRow>
-    // </Modal>
   );
 };
 
