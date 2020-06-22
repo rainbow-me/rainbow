2ffee1480f1898e48be0585a84aa25279b2ab1cb
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2020-06-22 18:03:44 +0200

    implement new QR layout WIP

--- src/components/QRCodeDisplay.js
+++ src/components/QRCodeDisplay.js
@@ -2,9 +2,20 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import QRCode from 'react-native-qrcode-svg';
 import { onlyUpdateForPropTypes } from 'recompact';
+import image from '../assets/icon-pixel.png';
 
 const QRCodeDisplay = ({ size, value, ...props }) => (
-  <QRCode {...props} size={size} value={value} />
+  <QRCode
+    {...props}
+    size={size}
+    value={value}
+    logo={image}
+    logoSize={84}
+    logoMargin={-5}
+    logoBackgroundColor="transparent"
+    quietZone={100}
+    ecl="Q"
+  />
 );
 
 QRCodeDisplay.propTypes = {
