a24ac78d244067f61b6d3cc3ebe2bdcf9686dc8f
Michał Osadnik <micosa97@gmail.com>
2020-06-23 19:07:13 +0200

    Disable safari debugging (#812)

--- ios/Rainbow/AppDelegate.m
+++ ios/Rainbow/AppDelegate.m
@@ -92,7 +92,7 @@ static void InitializeFlipper(UIApplication *application) {
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
   #if DEBUG
-    return [NSURL URLWithString:[[[[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil] absoluteString] stringByAppendingString:@"&inlineSourceMap=true" ]];
+    return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   #else
     return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
   #endif
