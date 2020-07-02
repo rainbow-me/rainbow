dc181510b69baf36cbd4123ebd4e636247894abc
Michał Osadnik <micosa97@gmail.com>
2020-07-02 12:26:10 +0200

    Uncomment copy seeds (#847)

--- src/components/settings-menu/DevSection.js
+++ src/components/settings-menu/DevSection.js
@@ -1,9 +1,8 @@
 import AsyncStorage from '@react-native-community/async-storage';
+import Clipboard from '@react-native-community/clipboard';
 import React, { useCallback, useContext } from 'react';
 import { ScrollView } from 'react-native';
-// uncomment after adding DEV_SEEDS to env
-// import Clipboard from '@react-native-community/clipboard';
-// import { DEV_SEEDS } from 'react-native-dotenv';
+import { DEV_SEEDS } from 'react-native-dotenv';
 import { DevContext } from '../../helpers/DevContext';
 import { ListFooter, ListItem } from '../list';
 import { RadioListItem } from '../radio-list';
@@ -33,10 +32,10 @@ const DevSection = () => {
         label="🤷 Restore default experimental config"
         onPress={() => AsyncStorage.removeItem('experimentalConfig')}
       />
-      {/*<ListItem*/}
-      {/*  label="‍💻 Copy dev seeds"*/}
-      {/*  onPress={() => Clipboard.setString(DEV_SEEDS)}*/}
-      {/*/>*/}
+      <ListItem
+        label="‍💻 Copy dev seeds"
+        onPress={() => Clipboard.setString(DEV_SEEDS)}
+      />
       <ListFooter />
 
       {Object.keys(config).map(key => (
