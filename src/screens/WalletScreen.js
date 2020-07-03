51cd80461e8f33c50b8312448ba4ec931cc78b7f
Bruno Barbieri <brunobar79@gmail.com>
2020-07-03 04:46:54 -0400

    remove console.log (#852)

--- src/screens/WalletScreen.js
+++ src/screens/WalletScreen.js
@@ -30,8 +30,8 @@ import {
   useWalletSectionsData,
 } from '../hooks';
 import { sheetVerticalOffset } from '../navigation/effects';
-import { usePortal } from 'react-native-cool-modals/Portal';
 import { position } from '@rainbow-me/styles';
+import { usePortal } from 'react-native-cool-modals/Portal';
 
 const HeaderOpacityToggler = styled(OpacityToggler).attrs(({ isVisible }) => ({
   endingOpacity: 0.4,
