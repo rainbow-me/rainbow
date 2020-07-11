129addfb2fefafd32310a5e15084a40b1498e90f
Bruno Barbieri <brunobar79@gmail.com>
2020-07-11 12:01:37 -0400

    show codepush version under settings (#892)

--- src/components/AppVersionStamp.js
+++ src/components/AppVersionStamp.js
@@ -1,21 +1,55 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// eslint-disable-next-line import/default
+import CodePush from 'react-native-code-push';
 import VersionNumber from 'react-native-version-number';
 import { Text } from './text';
 import { colors } from '@rainbow-me/styles';
 
-const AppVersionStamp = ({ color, ...props }) => (
-  <Text
-    align="center"
-    color={color || colors.alpha(colors.blueGreyDark, 0.2)}
-    lineHeight="normal"
-    size="small"
-    weight="bold"
-    {...props}
-  >
-    {`${VersionNumber.appVersion} (${VersionNumber.buildVersion})`}
-  </Text>
-);
+async function getAppVersion() {
+  const [{ appVersion }, update] = await Promise.all([
+    CodePush.getConfiguration(),
+    CodePush.getUpdateMetadata(),
+  ]);
+
+  if (!update) {
+    return `${appVersion} (${VersionNumber.buildVersion})`;
+  }
+
+  const label = update.label.substring(1);
+  return `${appVersion} (${VersionNumber.buildVersion}) rev.${label}`;
+}
+
+const AppVersionStamp = ({ color, ...props }) => {
+  const [appVersion, setAppVersion] = useState(
+    `${VersionNumber.appVersion} (${VersionNumber.buildVersion})`
+  );
+
+  // Try to get the codepush version number
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const v = await getAppVersion();
+        setAppVersion(v);
+        // eslint-disable-next-line no-empty
+      } catch (e) {}
+    };
+    init();
+  }, []);
+
+  return (
+    <Text
+      align="center"
+      color={color || colors.alpha(colors.blueGreyDark, 0.2)}
+      lineHeight="normal"
+      size="small"
+      weight="bold"
+      {...props}
+    >
+      {`${appVersion}`}
+    </Text>
+  );
+};
 
 AppVersionStamp.propTypes = {
   color: PropTypes.string,
