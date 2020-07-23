047f2b8687e025161f6c4d8ec71d505bddebfd5d
Wojciech Stanisz <42337257+wojtus7@users.noreply.github.com>
2020-07-23 23:19:36 +0200

    Add modifiers to touchX variable in charts (#925)
    * add modifiers to touchX variable in charts
    
    * Switch to interpolation for calculatedTouchX (#931)
    
    Co-authored-by: Christian Baroni <christianbaroni@me.com>

--- src/components/value-chart/ValueChart.js
+++ src/components/value-chart/ValueChart.js
@@ -18,23 +18,23 @@ const formatDate = date => format(new Date(date), 'MMM dd hh:mm aa');
 
 const {
   and,
-  or,
-  eq,
-  sub,
-  onChange,
   block,
-  event,
-  interpolate,
-  cond,
   call,
-  set,
-  neq,
-  greaterOrEq,
-  lessThan,
-  greaterThan,
-  stopClock,
-  multiply,
+  cond,
+  eq,
+  event,
   Extrapolate,
+  greaterOrEq,
+  greaterThan,
+  interpolate,
+  lessThan,
+  multiply,
+  neq,
+  onChange,
+  or,
+  set,
+  stopClock,
+  sub,
 } = Animated;
 
 let allSegmentDividers = [];
@@ -424,8 +424,24 @@ export default class Chart extends PureComponent {
               currentData={currentData}
               currentValue={currentValue}
               setCurrentPath={path => {
-                const length = interpolate(this.touchX, {
-                  extrapolate: Extrapolate.EXTEND,
+                const magneticPadding = 50;
+                const calculatedTouchX = interpolate(this.touchX, {
+                  extrapolate: Extrapolate.CLAMP,
+                  inputRange: [
+                    magneticPadding / 2,
+                    magneticPadding,
+                    width - magneticPadding,
+                    width - magneticPadding / 2,
+                  ],
+                  outputRange: [
+                    0,
+                    magneticPadding,
+                    width - magneticPadding,
+                    width,
+                  ],
+                });
+                const length = interpolate(calculatedTouchX, {
+                  extrapolate: Extrapolate.CLAMP,
                   inputRange: path.p0x,
                   outputRange: path.start,
                 });
