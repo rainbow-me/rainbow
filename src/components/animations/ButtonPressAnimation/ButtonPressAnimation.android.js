9bb5fef911a223a0cadaf096a8e36c966339b9be
osdnk <micosa97@gmail.com>
2020-07-30 15:05:14 +0200

    use touchables from RNGH

--- src/components/animations/ButtonPressAnimation/ButtonPressAnimation.android.js
+++ src/components/animations/ButtonPressAnimation/ButtonPressAnimation.android.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function ButtonPressAnimation({
   children,
