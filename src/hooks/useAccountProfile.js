d751b5ef7f93109055bea6d7ba2c16ea6001d84e
Bruno Barbieri <brunobar79@gmail.com>
2020-08-01 17:17:08 -0400

    remove deadbeef logs (#973)

--- src/hooks/useAccountProfile.js
+++ src/hooks/useAccountProfile.js
@@ -1,11 +1,9 @@
-import { captureMessage } from '@sentry/react-native';
 import GraphemeSplitter from 'grapheme-splitter';
 import { get, toUpper } from 'lodash';
 import { removeFirstEmojiFromString } from '../helpers/emojiHandler';
 import { address } from '../utils/abbreviations';
 import useAccountSettings from './useAccountSettings';
 import useWallets from './useWallets';
-import logger from 'logger';
 
 export default function useAccountProfile() {
   const wallets = useWallets();
@@ -15,23 +13,14 @@ export default function useAccountProfile() {
   const { accountAddress } = settings;
 
   if (!selectedWallet) {
-    logger.sentry('redux settings', settings);
-    logger.sentry('redux wallets', wallets);
-    captureMessage('DEADBEEF - no selectedWallet');
     return {};
   }
 
   if (!accountAddress) {
-    logger.sentry('redux settings', settings);
-    logger.sentry('redux wallets', wallets);
-    captureMessage('DEADBEEF - no accountAddress');
     return {};
   }
 
   if (!selectedWallet?.addresses?.length) {
-    logger.sentry('redux settings', settings);
-    logger.sentry('redux wallets', wallets);
-    captureMessage('DEADBEEF - no addresses');
     return {};
   }
 
@@ -42,9 +31,6 @@ export default function useAccountProfile() {
   );
 
   if (!selectedAccount) {
-    logger.sentry('redux settings', settings);
-    logger.sentry('redux wallets', wallets);
-    captureMessage('DEADBEEF - no selectedAccount');
     return {};
   }
 
