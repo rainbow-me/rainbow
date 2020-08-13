6f1928395466c5e977ac7d86a71e3ccb98f047aa
Bruno Barbieri <brunobar79@gmail.com>
2020-08-13 12:58:04 -0400

    use enum and remove fake types

--- src/helpers/walletTypes.ts
+++ src/helpers/walletTypes.ts
@@ -1,14 +1,15 @@
-import { EthereumWalletType } from '../model/wallet';
-
-const walletTypes: AllWalletTypes = {
-  mnemonic: 'mnemonic',
-  privateKey: 'privateKey',
-  readOnly: 'readOnly',
-  seed: 'seed',
-};
-
-export default walletTypes;
-
-export interface AllWalletTypes {
-  [key: string]: EthereumWalletType;
+export enum EthereumWalletType {
+  mnemonic = 'mnemonic',
+  privateKey = 'privateKey',
+  readOnly = 'readOnly',
+  seed = 'seed',
 }
+
+// We need to keep this one until
+// we have typescript everywhere
+export default {
+  mnemonic: 'mnemonic' as EthereumWalletType,
+  privateKey: 'privateKey' as EthereumWalletType,
+  readOnly: 'readOnly' as EthereumWalletType,
+  seed: 'seed' as EthereumWalletType,
+};
