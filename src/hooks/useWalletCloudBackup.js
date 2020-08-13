6882536b160d501a6f62a4f0cb7be80128ba691c
Bruno Barbieri <brunobar79@gmail.com>
2020-08-13 16:41:59 -0400

    fix params on useWalletCloudBackup

--- src/hooks/useWalletCloudBackup.js
+++ src/hooks/useWalletCloudBackup.js
@@ -9,58 +9,56 @@ import { setIsWalletLoading, setWalletBackedUp } from '../redux/wallets';
 import useWallets from './useWallets';
 import logger from 'logger';
 
-export default function useWalletCloudBackup({
-  walletId,
-  password,
-  latestBackup,
-  onError,
-}) {
+export default function useWalletCloudBackup() {
   const dispatch = useDispatch();
   const { wallets } = useWallets();
   const { goBack } = useNavigation();
 
-  const walletCloudBackup = useCallback(async () => {
-    try {
-      dispatch(setIsWalletLoading(walletLoadingStates.BACKING_UP_WALLET));
+  const walletCloudBackup = useCallback(
+    async ({ walletId, password, latestBackup, onError }) => {
+      try {
+        dispatch(setIsWalletLoading(walletLoadingStates.BACKING_UP_WALLET));
 
-      let backupFile;
-      if (!latestBackup) {
-        logger.log(
-          'walletCloudBackup:: backing up to icloud',
-          wallets[walletId]
-        );
+        let backupFile;
+        if (!latestBackup) {
+          logger.log(
+            'walletCloudBackup:: backing up to icloud',
+            wallets[walletId]
+          );
 
-        backupFile = await backupWalletToCloud(password, wallets[walletId]);
-      } else {
-        logger.log(
-          'walletCloudBackup:: adding to icloud backup',
-          wallets[walletId],
-          latestBackup
-        );
-        backupFile = await addWalletToCloudBackup(
-          password,
-          wallets[walletId],
-          latestBackup
-        );
+          backupFile = await backupWalletToCloud(password, wallets[walletId]);
+        } else {
+          logger.log(
+            'walletCloudBackup:: adding to icloud backup',
+            wallets[walletId],
+            latestBackup
+          );
+          backupFile = await addWalletToCloudBackup(
+            password,
+            wallets[walletId],
+            latestBackup
+          );
+        }
+        if (backupFile) {
+          logger.log('walletCloudBackup:: saving backup password');
+          await saveBackupPassword(password);
+          logger.log('walletCloudBackup:: saved');
+
+          logger.log('walletCloudBackup:: backup completed!', backupFile);
+          await dispatch(
+            setWalletBackedUp(walletId, walletBackupTypes.cloud, backupFile)
+          );
+          logger.log('walletCloudBackup:: backup saved everywhere!');
+          goBack();
+        } else {
+          onError();
+        }
+      } catch (e) {
+        onError(e);
       }
-      if (backupFile) {
-        logger.log('walletCloudBackup:: saving backup password');
-        await saveBackupPassword(password);
-        logger.log('walletCloudBackup:: saved');
-
-        logger.log('walletCloudBackup:: backup completed!', backupFile);
-        await dispatch(
-          setWalletBackedUp(walletId, walletBackupTypes.cloud, backupFile)
-        );
-        logger.log('walletCloudBackup:: backup saved everywhere!');
-        goBack();
-      } else {
-        onError();
-      }
-    } catch (e) {
-      onError(e);
-    }
-  }, [walletId, wallets, dispatch, latestBackup, password, goBack, onError]);
+    },
+    [wallets, dispatch, goBack]
+  );
 
   return walletCloudBackup;
 }
