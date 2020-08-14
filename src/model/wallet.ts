e64ed9e097d0334c7050dcc4cd68f311a182987c
Bruno Barbieri <brunobar79@gmail.com>
2020-08-14 15:52:44 -0400

    Fix wrong key in allWallets (#1000)
    * Update wallet.ts
    
    * Format

--- src/model/wallet.ts
+++ src/model/wallet.ts
@@ -502,11 +502,7 @@ export const createWallet = async (
     // Get all wallets
     const allWalletsResult = await getAllWallets();
     logger.sentry('[createWallet] - getAllWallets');
-    const allWallets: AllRainbowWallets = get(
-      allWalletsResult,
-      'allWallets',
-      {}
-    );
+    const allWallets: AllRainbowWallets = get(allWalletsResult, 'wallets', {});
 
     let existingWalletId = null;
     if (isImported) {
