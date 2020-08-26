a6f37e7f8ca79b11720e33dc1e430c03815c3531
osdnk <micosa97@gmail.com>
2020-08-26 19:34:39 +0200

    Fix example

---
+++ src/react-native-animated-charts/Example/metro.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  watchFolders: [path.resolve(__dirname, '..')],
+};
