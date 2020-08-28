dc09adea633305b17807d30e67c3f595661cdcd0
osdnk <micosa97@gmail.com>
2020-08-29 01:04:58 +0200

    Remove bezier while progressing

--- src/react-native-animated-charts/src/smoothSVG.js
+++ src/react-native-animated-charts/src/smoothSVG.js
@@ -22,7 +22,12 @@ const controlPoint = (current, previous, next, reverse, smoothing) => {
   return [x, y];
 };
 
-export const svgBezierPath = (points, smoothing, strategy = 'complex') => {
+export const svgBezierPath = (
+  points,
+  smoothing,
+  strategy = 'complex',
+  progress
+) => {
   'worklet';
   const traversed = points.map(p => [p.x, p.y]);
   // build the d attributes by looping over the points
@@ -52,12 +57,14 @@ export const svgBezierPath = (points, smoothing, strategy = 'complex') => {
         const y1 = p1[1];
         const x = point[0];
         const y = point[1];
-        const cp1x = (2 * x0 + x1) / 3;
-        const cp1y = (2 * y0 + y1) / 3;
-        const cp2x = (x0 + 2 * x1) / 3;
-        const cp2y = (y0 + 2 * y1) / 3;
-        const cp3x = (x0 + 4 * x1 + x) / 6;
-        const cp3y = (y0 + 4 * y1 + y) / 6;
+        const factor =
+          (-Math.max(Math.abs(0.5 - progress.value) - 0.3, 0) + 0.2) * 5;
+        const cp1x = ((2 * x0 + x1) / 3) * (1 - factor) + factor * x1;
+        const cp1y = ((2 * y0 + y1) / 3) * (1 - factor) + factor * y1;
+        const cp2x = ((x0 + 2 * x1) / 3) * (1 - factor) + factor * x;
+        const cp2y = ((y0 + 2 * y1) / 3) * (1 - factor) + factor * y;
+        const cp3x = ((x0 + 4 * x1 + x) / 6) * (1 - factor) + factor * x;
+        const cp3y = ((y0 + 4 * y1 + y) / 6) * (1 - factor) + factor * y;
         if (i === a.length - 1) {
           return `${acc} C ${cp1x},${cp1y} ${cp2x},${cp2y} ${cp3x},${cp3y} C${x},${y} ${x},${y} ${x},${y}`;
         }
