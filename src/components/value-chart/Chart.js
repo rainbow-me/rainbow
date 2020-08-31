f8a66f42acb376178d1e377b8fdcf38acd4aae0e
Christian Baroni <christianbaroni@me.com>
2020-08-31 02:38:16 -0600

    Add custom chart spinner (#1030)
    * Add custom chart spinner
    
    * Move animation configs

--- src/components/value-chart/Chart.js
+++ src/components/value-chart/Chart.js
@@ -1,11 +1,18 @@
 import { debounce, invert } from 'lodash';
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Dimensions } from 'react-native';
-import { useSharedValue } from 'react-native-reanimated';
+import FastImage from 'react-native-fast-image';
+import Animated, {
+  NewEasing,
+  repeat,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import styled from 'styled-components/native';
-import ActivityIndicator from '../ActivityIndicator';
+import Spinner from '../../assets/chartSpinner.png';
 import { ChartExpandedStateHeader } from '../expanded-state/chart';
-import { Centered, Column } from '../layout';
+import { Column } from '../layout';
 import Labels from './ExtremeLabels';
 import TimespanSelector from './TimespanSelector';
 import ChartTypes from '@rainbow-me/helpers/chartTypes';
@@ -29,6 +36,15 @@ const ChartTimespans = [
 
 const ChartContainer = styled.View`
   margin-vertical: ${({ showChart }) => (showChart ? '17px' : '0px')};
+`;
+
+const ChartSpinner = styled(FastImage).attrs(({ color }) => ({
+  resizeMode: FastImage.resizeMode.contain,
+  source: Spinner,
+  tintColor: color,
+}))`
+  height: 28;
+  width: 28;
 `;
 
 const Container = styled(Column)`
@@ -55,26 +71,23 @@ const Dot = styled(ChartDot)`
 
 const HEIGHT = 146.5;
 
-const Overlay = styled(Centered).attrs({
+const Overlay = styled(Animated.View).attrs({
   pointerEvents: 'none',
 })`
   ${position.cover};
-  background-color: ${colors.alpha(colors.white, 0.69)};
+  align-items: center;
+  background-color: ${colors.alpha(colors.white, 0.9)};
+  justify-content: center;
 `;
 
-function useShowLoadingState(isFetching) {
-  const [isShow, setIsShow] = useState(false);
-  const timeout = useRef();
-  useEffect(() => {
-    if (isFetching) {
-      timeout.current = setTimeout(() => setIsShow(isFetching), 1000);
-    } else {
-      clearTimeout(timeout.current);
-      setIsShow(isFetching);
-    }
-  }, [isFetching]);
-  return isShow;
-}
+const rotationConfig = {
+  duration: 500,
+  easing: NewEasing.linear,
+};
+
+const timingConfig = {
+  duration: 300,
+};
 
 export default function ChartWrapper({
   chartType,
@@ -112,6 +125,61 @@ export default function ChartWrapper({
   }, [nativePoints, fetchingCharts, points, debouncedSetThrottledData]);
 
   const chartTimeSharedValue = useSharedValue('');
+  const spinnerRotation = useSharedValue(0);
+  const spinnerScale = useSharedValue(0);
+
+  function useShowLoadingState(isFetching) {
+    const [isShow, setIsShow] = useState(false);
+    const timeout = useRef();
+    useEffect(() => {
+      if (isFetching) {
+        timeout.current = setTimeout(() => setIsShow(isFetching), 500);
+      } else {
+        clearTimeout(timeout.current);
+        setIsShow(isFetching);
+      }
+    }, [isFetching]);
+    return isShow;
+  }
+
+  const showLoadingState = useShowLoadingState(fetchingCharts);
+
+  const spinnerTimeout = useRef();
+  useEffect(() => {
+    if (showLoadingState) {
+      clearTimeout(spinnerTimeout.current);
+      spinnerRotation.value = 0;
+      spinnerRotation.value = repeat(
+        withTiming(360, rotationConfig),
+        -1,
+        false
+      );
+      spinnerScale.value = withTiming(1, timingConfig);
+    } else {
+      spinnerScale.value = withTiming(0, timingConfig);
+      spinnerTimeout.current = setTimeout(
+        () => (spinnerRotation.value = 0),
+        timingConfig.duration
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [showLoadingState]);
+
+  const overlayStyle = useAnimatedStyle(() => {
+    return {
+      opacity: spinnerScale.value,
+    };
+  });
+
+  const spinnerStyle = useAnimatedStyle(() => {
+    return {
+      opacity: spinnerScale.value,
+      transform: [
+        { rotate: `${spinnerRotation.value}deg` },
+        { scale: spinnerScale.value },
+      ],
+    };
+  });
 
   const timespan = invert(ChartTypes)[chartType];
   const formattedTimespan =
@@ -127,8 +195,6 @@ export default function ChartWrapper({
         (chartTimeSharedValue.value = `Past ${formattedTimespan}`);
     }
   }, [chartTimeSharedValue, chartType, formattedTimespan]);
-
-  const showLoadingState = useShowLoadingState(fetchingCharts);
 
   return (
     <Container>
@@ -151,8 +217,6 @@ export default function ChartWrapper({
                   minDurationMs: 60,
                 }}
                 stroke={color}
-                strokeLinecap="round"
-                strokeLinejoin="round"
                 strokeWidth={3.5}
                 strokeWidthSelected={3}
                 width={WIDTH}
@@ -160,13 +224,13 @@ export default function ChartWrapper({
               <Dot color={colors.alpha(color, 0.03)} size={65}>
                 <InnerDot color={color} />
               </Dot>
+              <Overlay style={overlayStyle}>
+                <Animated.View style={spinnerStyle}>
+                  <ChartSpinner color={color} />
+                </Animated.View>
+              </Overlay>
             </>
           )}
-          {showLoadingState ? (
-            <Overlay>
-              <ActivityIndicator color={color} />
-            </Overlay>
-          ) : null}
         </ChartContainer>
       </ChartProvider>
       {showChart ? (
