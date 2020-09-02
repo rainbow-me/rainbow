c05ee141b7c5954f1a621c2da8d020e682ac4fb7
Michael Demarais <m.demarais@gmail.com>
2020-09-02 15:15:00 -0400

    Utilize reselect instead of manual memoization inside of useWallets hook

--- src/hooks/useWallets.js
+++ src/hooks/useWallets.js
@@ -1,21 +1,32 @@
-import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
 import { findLatestBackUp } from '../model/backup';
 import WalletTypes from '@rainbow-me/helpers/walletTypes';
 
-export default function useWallets() {
-  const { isWalletLoading, selectedWallet, walletNames, wallets } = useSelector(
-    ({ wallets: { isWalletLoading, selected, walletNames, wallets } }) => ({
-      isWalletLoading,
-      selectedWallet: selected || {},
-      walletNames,
-      wallets,
-    })
-  );
+const walletSelector = createSelector(
+  ({ wallets: { isWalletLoading, selected = {}, walletNames, wallets } }) => ({
+    isWalletLoading,
+    selectedWallet: selected,
+    walletNames,
+    wallets,
+  }),
+  ({ isWalletLoading, selectedWallet, walletNames, wallets }) => ({
+    isWalletLoading,
+    latestBackup: findLatestBackUp(wallets) || false,
+    selectedWallet,
+    walletNames,
+    wallets,
+  })
+);
 
-  const latestBackup = useMemo(() => {
-    return findLatestBackUp(wallets) || false;
-  }, [wallets]);
+export default function useWallets() {
+  const {
+    isWalletLoading,
+    latestBackup,
+    selectedWallet,
+    walletNames,
+    wallets,
+  } = useSelector(walletSelector);
 
   return {
     isReadOnlyWallet: selectedWallet.type === WalletTypes.readOnly,
