59eca5ebf9d223accf6ddb47b6c8206dd46867fd
Michał Osadnik <micosa97@gmail.com>
2020-09-18 16:16:57 +0300

    Add moving marqueeList (#800)

--- src/components/list/MarqueeList.js
+++ src/components/list/MarqueeList.js
@@ -1,51 +1,174 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { FlatList } from 'react-native-gesture-handler';
-import styled from 'styled-components/primitives';
-import { TopMoverCoinRow } from '../coin-row';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+import Animated, { Clock, decay, Value } from 'react-native-reanimated';
+import { useMemoOne } from 'use-memo-one';
+import { measureTopMoverCoinRow, TopMoverCoinRow } from '../coin-row';
 
-const contentContainerStyle = { paddingVertical: 8 };
-const keyExtractor = ({ address }) => `MarqueeList-${address}`;
-const renderItem = ({ item }) => (
-  <TopMoverCoinRow {...item} key={item?.address} />
-);
+const {
+  set,
+  cond,
+  eq,
+  add,
+  modulo,
+  startClock,
+  stopClock,
+  clockRunning,
+  sub,
+  event,
+} = Animated;
 
-const MarqueeFlatList = styled(FlatList).attrs({
-  contentContainerStyle,
-  directionalLockEnabled: true,
-  horizontal: true,
-  initialNumToRender: 8,
-  keyExtractor,
-  removeClippedSubviews: true,
-  renderItem,
-  scrollEventThrottle: 32,
-  showsHorizontalScrollIndicator: false,
-  windowSize: 11,
-})`
-  width: 100%;
-`;
+const DECCELERATION = 0.998;
 
-const MarqueeList = ({ items = [], measureItem, onLayout, ...props }) => {
-  const listRef = useRef();
-  const [itemWidths, setItemWidths] = useState([]);
+function runDecay(clock, value, velocity) {
+  const state = {
+    finished: new Value(0),
+    position: value,
+    time: new Value(0),
+    velocity: velocity,
+  };
+
+  const config = { deceleration: DECCELERATION };
+
+  return [
+    cond(clockRunning(clock), 0, [set(state.time, 0), startClock(clock)]),
+    decay(clock, state, config),
+    state.position,
+  ];
+}
+
+const useReanimatedValue = initialValue => {
+  const value = useRef();
+
+  if (!value.current) {
+    value.current = new Value(initialValue);
+  }
+
+  return value.current;
+};
+
+const SwipeableList = ({ components, speed }) => {
+  const dragX = useReanimatedValue(0);
+  const state = useReanimatedValue(-1);
+  const dragVX = useReanimatedValue(0);
+
+  const onGestureEvent = useMemoOne(
+    () =>
+      event([
+        {
+          nativeEvent: { state, translationX: dragX, velocityX: dragVX },
+        },
+      ]),
+    [dragVX, dragVX, state]
+  );
+
+  const transX = useReanimatedValue(0);
+  const prevDragX = useReanimatedValue(0);
+
+  const clock = useMemoOne(() => new Clock(), []);
+
+  const transXWrapped = useMemoOne(() =>
+    cond(
+      eq(state, State.ACTIVE),
+      [
+        stopClock(clock),
+        set(transX, add(transX, sub(dragX, prevDragX))),
+        set(prevDragX, dragX),
+        transX,
+      ],
+      [
+        set(prevDragX, 0),
+        set(transX, runDecay(clock, transX, dragVX)),
+        set(transX, add(transX, speed)),
+      ]
+    )
+  );
+
+  const sumWidth = useMemoOne(
+    () => components.reduce((acc, { width }) => acc + width, 0),
+    [components]
+  );
+
+  return (
+    <PanGestureHandler
+      maxPointers={1}
+      activeOffsetX={4}
+      activeOffsetY={1000}
+      failOffsetY={[-10, 10]}
+      onGestureEvent={onGestureEvent}
+      onHandlerStateChange={onGestureEvent}
+    >
+      <Animated.View style={{ height: 78, width: '100%' }}>
+        <Animated.View
+          style={{
+            flexDirection: 'row',
+          }}
+        >
+          {components.map(({ view, offset, width }) => (
+            <Animated.View
+              key={offset}
+              style={{
+                position: 'absolute',
+                transform: [
+                  {
+                    translateX: sub(
+                      modulo(add(transXWrapped, offset, width), sumWidth || 0),
+                      width
+                    ),
+                  },
+                ],
+              }}
+            >
+              {view}
+            </Animated.View>
+          ))}
+        </Animated.View>
+      </Animated.View>
+    </PanGestureHandler>
+  );
+};
+
+const MarqueeList = ({ items = [], speed }) => {
+  const [itemWidths, setItemWidths] = useState(null);
 
   const updateItemWidths = useCallback(async () => {
-    const widths = await Promise.all(items.map(measureItem));
+    const widths = await Promise.all(items.map(measureTopMoverCoinRow));
     setItemWidths(widths);
-  }, [items, measureItem]);
+  }, [items]);
 
   useEffect(() => {
     updateItemWidths();
   }, [updateItemWidths]);
 
-  console.log('itemWidths', itemWidths);
+  const renderItemCallback = useCallback(
+    ({ item }) => <TopMoverCoinRow {...item} key={item?.address} />,
+    []
+  );
+
+  const offsets = useMemoOne(
+    () =>
+      itemWidths
+        ?.reduce(([first, ...tail], width) => [width + first, first, ...tail], [
+          0,
+        ])
+        .reverse(),
+    [itemWidths]
+  );
+
+  if (itemWidths === null) {
+    return null;
+  }
 
   return (
-    <MarqueeFlatList
-      {...props}
-      data={items}
-      onLayout={onLayout}
-      ref={listRef}
-    />
+    <>
+      <SwipeableList
+        speed={speed}
+        components={items.map((item, idx) => ({
+          offset: offsets[idx],
+          view: renderItemCallback({ item }),
+          width: itemWidths[idx],
+        }))}
+      />
+    </>
   );
 };
 
