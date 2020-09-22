e06d191d69f0c61db9ad75f5f428238b697b1d7d
osdnk <micosa97@gmail.com>
2020-09-22 16:06:07 +0300

    Remove experimentals that are no longer experimental

--- src/config/experimental.js
+++ src/config/experimental.js
@@ -8,23 +8,11 @@
 export const chartExpandedAvailable = true;
 
 export const AVATAR_PICKER = 'avatarPicker';
-export const CHARTS_EXAMPLE = 'chartsExample';
 export const DISCOVER_SHEET = 'discoverSheet';
-export const NATIVE_BPA = 'nativeButtonPressAnimation';
-export const NATIVE_TRANSACTION_LIST = 'nativeTransactionList';
 export const NEW_ONBOARDING = 'newOnboardingFlow';
-export const NEW_SPLASH_SCREEN = 'newSplashScreen';
-export const RAINBOW_TEXT = 'rainbowText';
-export const RED_GREEN_PRICE_CHANGE = 'redGreenPriceChange';
 
 export const defaultConfig = {
   [AVATAR_PICKER]: false,
-  [CHARTS_EXAMPLE]: false,
   [DISCOVER_SHEET]: false,
-  [NATIVE_BPA]: true,
-  [NATIVE_TRANSACTION_LIST]: true,
   [NEW_ONBOARDING]: false,
-  [NEW_SPLASH_SCREEN]: true,
-  [RAINBOW_TEXT]: true,
-  [RED_GREEN_PRICE_CHANGE]: true,
 };
