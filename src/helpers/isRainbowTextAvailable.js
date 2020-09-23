c25d4dfd6225b369b9238a893443dacdef2c73dc
Michał Osadnik <micosa97@gmail.com>
2020-09-23 07:45:32 +0300

    Remove experimentals that are no longer experimental (#1119)
    * Remove experimentals that are no longer experimental
    
    * Remove   CHARTS_EXAMPLE: 'ChartsExampleScreen',

--- src/helpers/isRainbowTextAvailable.js
+++ src/helpers/isRainbowTextAvailable.js
@@ -1,15 +1,7 @@
 import { UIManager } from 'react-native';
-import useExperimentalFlag, { RAINBOW_TEXT } from '../config/experimentalHooks';
 import isNativeStackAvailable from '../helpers/isNativeStackAvailable';
 
-const useNativeButtonAvailable = () => {
-  const isRainbowTextAvailable = useExperimentalFlag(RAINBOW_TEXT);
-
-  return (
-    isRainbowTextAvailable &&
-    isNativeStackAvailable &&
-    !!UIManager.getViewManagerConfig('RainbowText')
-  );
-};
+const useNativeButtonAvailable = () =>
+  isNativeStackAvailable && !!UIManager.getViewManagerConfig('RainbowText');
 
 export default useNativeButtonAvailable;
