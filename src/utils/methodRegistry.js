65b8a71c9fd12e7f576f7c3f2828ce56961ba257
Christian Baroni <c@baroni.co>
2020-09-27 14:11:40 -0400

    Fix ethers import, add @ethersproject/solidity

--- src/utils/methodRegistry.js
+++ src/utils/methodRegistry.js
@@ -1,4 +1,4 @@
-import { ethers } from 'ethers';
+import { Contract } from '@ethersproject/contracts';
 import { web3Provider } from '../handlers/web3';
 import namesOverrides from '../references/method-names-overrides.json';
 import methodRegistryABI from '../references/method-registry-abi.json';
@@ -6,7 +6,7 @@ import methodRegistryABI from '../references/method-registry-abi.json';
 const METHOD_REGISTRY_ADDRESS = '0x44691B39d1a75dC4E0A0346CBB15E310e6ED1E86';
 
 export const methodRegistryLookupAndParse = async methodSignatureBytes => {
-  const registry = new ethers.Contract(
+  const registry = new Contract(
     METHOD_REGISTRY_ADDRESS,
     methodRegistryABI,
     web3Provider
