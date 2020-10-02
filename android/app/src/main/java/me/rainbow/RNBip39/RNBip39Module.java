c393daf4842d59a74fcf0e0b62318f577d3921fe
Bruno Barbieri <brunobar79@gmail.com>
2020-10-01 20:18:58 -0400

    create RNBip39 in Java to do mnemonicToSeed natively

---
+++ android/app/src/main/java/me/rainbow/RNBip39/RNBip39Module.java
@@ -0,0 +1,47 @@
+package me.rainbow.RNBip39;
+
+import android.util.Log;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import android.util.Base64;
+
+
+import io.github.novacrypto.bip39.*;
+
+public class RNBip39Module extends ReactContextBaseJavaModule {
+    public static final String TAG = "RNBip39";
+    private final ReactApplicationContext reactContext;
+
+    public RNBip39Module(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "RNBip39";
+    }
+
+
+    @ReactMethod
+    public void mnemonicToSeed(ReadableMap options, final Promise promise) {
+
+        try {
+            String passphrase = options.getString("passphrase");
+            if(passphrase == null){
+                passphrase = "";
+            }
+            String mnemonic = options.getString("mnemonic");
+            byte[] seed = new SeedCalculator().calculateSeed(mnemonic, passphrase);
+            String base64String = Base64.encodeToString(seed, Base64.NO_WRAP);
+            promise.resolve(base64String);
+        } catch(Exception e){
+            promise.reject(e);
+        }
+    }
+
+}
---
+++ android/app/src/main/java/me/rainbow/RNBip39/RNBip39Package.java
@@ -0,0 +1,31 @@
+package me.rainbow.RNBip39;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class RNBip39Package implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new RNBip39Module(reactContext));
+        return modules;
+    }
+
+    // deprecated >= RN 0.47.0
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<ViewManager>asList();
+    }
+}
