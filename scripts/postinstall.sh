d29f849e93ad4528ef48637ea588a039f4aeebeb
osdnk <micosa97@gmail.com>
2020-10-03 12:09:18 +0300

    Fix reanimated refresh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -7,7 +7,7 @@ echo "✅ All patches applied"
 
 # Specifying ONLY the node packages that we need to install via browserify
 # (because those aren't available in react native) and some of our deps require them.
-# If we don't specify which packages, rn-nodeify  will look into each package.json 
+# If we don't specify which packages, rn-nodeify  will look into each package.json
 # and install everything including a bunch devDeps that we don't need like "console"
 rn-nodeify --install --hack 'crypto,buffer,react-native-randombytes,vm,stream,http,https,os,url,net,fs,process'
 echo "✅ rn-nodeify packages hacked succesfully"
@@ -28,3 +28,4 @@ else
   echo "Please make sure the file exists and it's located in the root of the project"
 fi
 
+cp patches/reanimated/* node_modules/react-native-reanimated/android
