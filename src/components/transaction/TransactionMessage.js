1928ff0e9ef3441653bc26ec11082637f1ad3a8b
Bruno Barbieri <brunobar79@gmail.com>
2020-10-06 10:22:05 -0400

    WC Fixes (#1170)

--- src/components/transaction/TransactionMessage.js
+++ src/components/transaction/TransactionMessage.js
@@ -3,6 +3,7 @@ import { ScrollView } from 'react-native-gesture-handler';
 import { pure } from 'recompact';
 import styled from 'styled-components';
 import { SIGN_TYPED_DATA } from '../../utils/signingMethods';
+import { Row } from '../layout';
 import { Text } from '../text';
 import { colors, padding } from '@rainbow-me/styles';
 import { deviceUtils } from '@rainbow-me/utils';
@@ -10,20 +11,26 @@ import { deviceUtils } from '@rainbow-me/utils';
 const deviceWidth = deviceUtils.dimensions.width;
 const horizontalPadding = 24;
 
+const Container = styled(Row)`
+  max-height: ${({ maxHeight }) => maxHeight};
+  min-height: ${({ minHeight }) => minHeight};
+`;
 const MessageWrapper = styled(ScrollView)`
   border-color: ${colors.alpha(colors.blueGreyDark, 0.2)};
   border-radius: 20;
   border-width: 1;
   margin-bottom: 14;
-  max-height: ${({ maxHeight }) => maxHeight};
   ${padding(14)}
   min-width: ${deviceWidth - horizontalPadding * 2};
 `;
 
-const TransactionMessage = ({ maxHeight = 100, message, method }) => {
+const TransactionMessage = ({ maxHeight = 150, message, method }) => {
   let msg = message;
   let maximumHeight = maxHeight;
+  let minimumHeight = 150;
   if (method === SIGN_TYPED_DATA) {
+    maximumHeight = 200;
+    minimumHeight = 200;
     try {
       msg = JSON.parse(message);
       // eslint-disable-next-line no-empty
@@ -32,11 +39,13 @@ const TransactionMessage = ({ maxHeight = 100, message, method }) => {
   }
 
   return (
-    <MessageWrapper maxHeight={maximumHeight}>
-      <Text color={colors.alpha(colors.blueGreyDark, 0.6)} size="lmedium">
-        {msg}
-      </Text>
-    </MessageWrapper>
+    <Container maxHeight={maximumHeight} minHeight={minimumHeight}>
+      <MessageWrapper>
+        <Text color={colors.alpha(colors.blueGreyDark, 0.6)} size="lmedium">
+          {msg}
+        </Text>
+      </MessageWrapper>
+    </Container>
   );
 };
 
