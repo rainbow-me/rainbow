9677d72a5c907d77481e651b4245f6ec604a65cd
osdnk <micosa97@gmail.com>
2020-10-12 15:24:14 +0300

    restore ripple effect

--- src/navigation/SwipeNavigator.js
+++ src/navigation/SwipeNavigator.js
@@ -20,7 +20,7 @@ export function SwipeNavigator() {
     <Swipe.Navigator
       initialLayout={deviceUtils.dimensions}
       initialRouteName={Routes.WALLET_SCREEN}
-      pager={renderPager}
+      {...(ios && { pager: renderPager })}
       position={scrollPosition}
       swipeEnabled={!isCoinListEdited}
       tabBar={renderTabBar}
