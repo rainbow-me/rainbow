3a588723f50b2a8ae209b879332dafcfdb24def0
skylarbarrera <skylar.barrera@gmail.com>
2020-10-19 17:45:49 -0600

    flakiness fixes

--- e2e/ganacheTransactionFlow.spec.js
+++ e2e/ganacheTransactionFlow.spec.js
@@ -10,13 +10,13 @@ describe('Ganache Transaction Flow', () => {
 
   it('Should show the "Restore Sheet" after tapping on "I already have a wallet"', async () => {
     await Helpers.tap('already-have-wallet-button');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.checkIfExists('restore-sheet');
   });
 
   it('show the "Import Sheet" when tapping on "Restore with a recovery phrase or private key"', async () => {
     await Helpers.tap('restore-with-key-button');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.checkIfExists('import-sheet');
   });
 
@@ -41,20 +41,20 @@ describe('Ganache Transaction Flow', () => {
   it('Should navigate to the Profile screen after swiping right', async () => {
     await Helpers.delay(1000);
     await Helpers.swipe('wallet-screen', 'right', 'slow');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.checkIfVisible('profile-screen');
     await Helpers.delay(3000);
   });
 
   it('Should navigate to Settings Modal after tapping Settings Button', async () => {
     await Helpers.tap('settings-button');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.checkIfVisible('settings-modal');
   });
 
   it('Should navigate to Developer Settings after tapping Developer Section', async () => {
     await Helpers.tap('developer-section');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.checkIfVisible('developer-settings-modal');
   });
 
@@ -63,17 +63,17 @@ describe('Ganache Transaction Flow', () => {
     await Helpers.delay(30000);
     await Helpers.checkIfVisible('testnet-toast-Ganache');
     await Helpers.swipe('profile-screen', 'left', 'slow');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
   });
 
   //SwapSheet Transactions
   it('Should open and complete SwapSheet for ETH -> ERC20', async () => {
     await Helpers.tap('exchange-fab');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.typeText('exchange-modal-input', '0.01', true);
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('exchange-modal-output-selection-button');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.typeText('currency-select-search-input', 'DAI', true);
     await Helpers.delay(3000);
     await Helpers.tap('exchange-coin-row-DAI');
@@ -84,17 +84,17 @@ describe('Ganache Transaction Flow', () => {
   });
 
   it('Should open and complete SwapSheet for ERC20 -> ERC20', async () => {
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('exchange-fab');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('exchange-modal-input-selection-button');
     await Helpers.delay(3000);
     await Helpers.tap('exchange-coin-row-BAT');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.typeText('exchange-modal-input', '5', true);
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('exchange-modal-output-selection-button');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('exchange-coin-row-ZRX');
     await Helpers.delay(3000);
     await Helpers.tapAndLongPressByText('Hold to Swap');
@@ -103,19 +103,19 @@ describe('Ganache Transaction Flow', () => {
   });
 
   it('Should open and complete SwapSheet for ERC20 -> ETH', async () => {
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('exchange-fab');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('exchange-modal-input-selection-button');
     await Helpers.delay(3000);
     await Helpers.tap('exchange-coin-row-USDC');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.typeText('exchange-modal-input', '2', true);
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('exchange-modal-output-selection-button');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.typeText('currency-select-search-input', 'ETH', true);
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('exchange-coin-row-ETH');
     await Helpers.delay(3000);
     await Helpers.tapAndLongPressByText('Hold to Swap');
@@ -124,29 +124,14 @@ describe('Ganache Transaction Flow', () => {
   });
 
   it('Should open and complete Send Sheet for cSAI', async () => {
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('send-fab');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.typeText('send-asset-form-field', 'poopcoin.eth', false);
     await Helpers.delay(3000);
     await Helpers.tap('send-savings-cSAI');
-    await Helpers.delay(2000);
-    await Helpers.typeText('selected-asset-field-input', '1', true);
-    await Helpers.delay(1000);
-    await Helpers.tapAndLongPressByText('Hold to Send');
-    await Helpers.delay(6000);
-    await Helpers.swipe('profile-screen', 'left', 'slow');
-  });
-
-  it('Should open and complete Send Sheet for NFT', async () => {
-    await Helpers.delay(2000);
-    await Helpers.tap('send-fab');
-    await Helpers.delay(2000);
-    await Helpers.typeText('send-asset-form-field', 'poopcoin.eth', false);
     await Helpers.delay(3000);
-    await Helpers.tapByText('CryptoKitties');
-    await Helpers.delay(2000);
-    await Helpers.tapByText('Arun Cattybinky');
+    await Helpers.typeText('selected-asset-field-input', '1', true);
     await Helpers.delay(3000);
     await Helpers.tapAndLongPressByText('Hold to Send');
     await Helpers.delay(6000);
@@ -161,14 +146,29 @@ describe('Ganache Transaction Flow', () => {
     await Helpers.swipe('profile-screen', 'left', 'slow');
   });
 
-  it('Should open and complete Send Sheet for ERC20', async () => {
-    await Helpers.delay(2000);
+  it('Should open and complete Send Sheet for NFT', async () => {
+    await Helpers.delay(3000);
     await Helpers.tap('send-fab');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
+    await Helpers.typeText('send-asset-form-field', 'poopcoin.eth', false);
+    await Helpers.delay(3000);
+    await Helpers.tapByText('CryptoKitties');
+    await Helpers.delay(3000);
+    await Helpers.tapByText('Arun Cattybinky');
+    await Helpers.delay(3000);
+    await Helpers.tapAndLongPressByText('Hold to Send');
+    await Helpers.delay(6000);
+    await Helpers.swipe('profile-screen', 'left', 'slow');
+  });
+
+  it('Should open and complete Send Sheet for ERC20', async () => {
+    await Helpers.delay(3000);
+    await Helpers.tap('send-fab');
+    await Helpers.delay(3000);
     await Helpers.typeText('send-asset-form-field', 'poopcoin.eth', false);
     await Helpers.delay(3000);
     await Helpers.tap('send-asset-BAT');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.typeText('selected-asset-field-input', '2', true);
     await Helpers.delay(3000);
     await Helpers.tapAndLongPressByText('Hold to Send');
@@ -177,13 +177,13 @@ describe('Ganache Transaction Flow', () => {
   });
 
   it('Should open and complete Send Sheet for ETH', async () => {
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.tap('send-fab');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.typeText('send-asset-form-field', 'poopcoin.eth', false);
     await Helpers.delay(3000);
     await Helpers.tap('send-asset-ETH');
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     await Helpers.typeText('selected-asset-field-input', '.01', true);
     await Helpers.delay(3000);
     await Helpers.tapAndLongPressByText('Hold to Send');
@@ -191,7 +191,7 @@ describe('Ganache Transaction Flow', () => {
   });
 
   it('Should show completed transaction - Swap ERC20 -> ERC20', async () => {
-    await Helpers.delay(2000);
+    await Helpers.delay(3000);
     try {
       await Helpers.checkIfVisible('Swapped-Basic Attention Token');
     } catch (e) {
