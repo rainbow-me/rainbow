d29e3e622d9f729a4659bb412ea2582f497573dd
Michał Osadnik <micosa97@gmail.com>
2020-10-27 12:54:45 +0100

    Fix fonts in Text inputs (#1224)

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,6 +1,6 @@
 export { default as borders } from './borders';
 export { default as buildFlexStyles } from './buildFlexStyles';
-export { default as buildTextStyles } from './buildTextStyles';
+export { default as buildTextStyles, fontWithWidth } from './buildTextStyles';
 export { default as calcDirectionToDegrees } from './calcDirectionToDegrees';
 export { default as colors } from './colors';
 export { default as fonts } from './fonts';
