a820bbc91e8c6455757d556f64d0c82c591085f6
Michał Osadnik <micosa97@gmail.com>
2020-10-29 11:48:16 +0100

    Fix border radius for ripple animation in Minibutton on Android (#1245)

--- src/components/buttons/MiniButton.js
+++ src/components/buttons/MiniButton.js
@@ -40,6 +40,7 @@ export default function MiniButton({
     <ButtonPressAnimation
       disabled={disabled}
       onPress={onPress}
+      radiusAndroid={borderRadius}
       scaleTo={scaleTo}
       {...props}
     >
