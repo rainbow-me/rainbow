c551714a9642ffb8c4c0239d22fb2baa9d437a10
Michał Osadnik <micosa97@gmail.com>
2020-11-02 23:02:12 +0100

    Use reanimated version from npm (#1266)

--- src/components/expanded-state/chart/chart-data-labels/ChartChangeDirectionArrow.js
+++ src/components/expanded-state/chart/chart-data-labels/ChartChangeDirectionArrow.js
@@ -34,26 +34,18 @@ export default function ChartChangeDirectionArrow() {
     [],
     'ChartChangeDirectionArrowRatioColor'
   );
-  const arrowWrapperStyle = useAnimatedStyle(
-    () => {
-      return {
-        opacity: ratio.value === 1 ? 0 : 1,
-        transform: [{ rotate: ratio.value < 1 ? '180deg' : '0deg' }],
-      };
-    },
-    [],
-    'ChartChangeDirectionArrowRatioWrapperStyle'
-  );
+  const arrowWrapperStyle = useAnimatedStyle(() => {
+    return {
+      opacity: ratio.value === 1 ? 0 : 1,
+      transform: [{ rotate: ratio.value < 1 ? '180deg' : '0deg' }],
+    };
+  });
 
-  const arrowStyle = useAnimatedStyle(
-    () => {
-      return {
-        backgroundColor: arrowColor.value,
-      };
-    },
-    [],
-    'ChartChangeDirectionArrowRatioStyle'
-  );
+  const arrowStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: arrowColor.value,
+    };
+  });
 
   return hidden ? null : (
     <Animated.View style={arrowWrapperStyle}>
