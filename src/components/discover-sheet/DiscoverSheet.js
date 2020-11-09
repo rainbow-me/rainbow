b1eb9bcd20f3006e5407755901a066e9231c70d3
Michał Osadnik <micosa97@gmail.com>
2020-11-09 15:51:04 +0100

    Fix TopMoversSection + Discover skeleton (#1274)
    Co-authored-by: Jin <jin.chung17@gmail.com>

--- src/components/discover-sheet/DiscoverSheet.js
+++ src/components/discover-sheet/DiscoverSheet.js
@@ -1,17 +1,14 @@
 import { useIsFocused } from '@react-navigation/native';
-import React, { Fragment } from 'react';
-import { Platform } from 'react-native';
+import React, { useMemo, useRef } from 'react';
+import { findNodeHandle, NativeModules, Platform } from 'react-native';
 import Animated from 'react-native-reanimated';
 import { useSafeArea } from 'react-native-safe-area-context';
 // eslint-disable-next-line import/no-unresolved
 import SlackBottomSheet from 'react-native-slack-bottom-sheet';
 
 import BottomSheet from 'reanimated-bottom-sheet';
-import { ColumnWithMargins } from '../layout';
-import { SlackSheet } from '../sheet';
-import DiscoverSheetHeader from './DiscoverSheetHeader';
-import TopMoversSection from './TopMoversSection';
-import { position } from '@rainbow-me/styles';
+import DiscoverSheetContent from './DiscoverSheetContent';
+import DiscoverSheetContext from './DiscoverSheetContext';
 
 // eslint-disable-next-line import/no-named-as-default-member
 const { SpringUtils } = Animated;
@@ -27,47 +24,53 @@ const discoverSheetSpring = SpringUtils.makeConfigFromBouncinessAndSpeed({
   toss: 6,
 });
 
-const DiscoverSheetContent = () => (
-  <Fragment>
-    <DiscoverSheetHeader />
-    <ColumnWithMargins flex={1} margin={42}>
-      <TopMoversSection />
-    </ColumnWithMargins>
-  </Fragment>
-);
-
 export default function DiscoverSheet() {
   const insets = useSafeArea();
   const isFocused = useIsFocused();
+  const ref = useRef();
+  const value = useMemo(
+    () => ({
+      jumpToShort() {
+        const screen = findNodeHandle(ref.current);
+        if (screen) {
+          NativeModules.ModalView.jumpTo(false, screen);
+        }
+      },
+    }),
+    []
+  );
 
   // noinspection JSConstructorReturnsPrimitive
-  return Platform.OS === 'ios' ? (
-    <SlackBottomSheet
-      allowsDragToDismiss={false}
-      allowsTapToDismiss={false}
-      backgroundOpacity={0}
-      blocksBackgroundTouches={false}
-      cornerRadius={30}
-      initialAnimation={false}
-      interactsWithOuterScrollView
-      isHapticFeedbackEnabled={false}
-      presentGlobally={false}
-      scrollsToTopOnTapStatusBar={isFocused}
-      showDragIndicator={false}
-      topOffset={insets.top}
-      unmountAnimation={false}
-    >
-      <SlackSheet contentOffset={position.current}>
-        <DiscoverSheetContent />
-      </SlackSheet>
-    </SlackBottomSheet>
-  ) : (
-    <BottomSheet
-      borderRadius={20}
-      overdragResistanceFactor={0}
-      renderContent={DiscoverSheetContent}
-      snapPoints={[300, 744]}
-      springConfig={discoverSheetSpring}
-    />
+  return (
+    <DiscoverSheetContext.Provider value={value}>
+      {Platform.OS === 'ios' ? (
+        <SlackBottomSheet
+          allowsDragToDismiss={false}
+          allowsTapToDismiss={false}
+          backgroundOpacity={0}
+          blocksBackgroundTouches={false}
+          cornerRadius={30}
+          initialAnimation={false}
+          interactsWithOuterScrollView
+          isHapticFeedbackEnabled={false}
+          presentGlobally={false}
+          ref={ref}
+          scrollsToTopOnTapStatusBar={isFocused}
+          showDragIndicator={false}
+          topOffset={insets.top}
+          unmountAnimation={false}
+        >
+          <DiscoverSheetContent />
+        </SlackBottomSheet>
+      ) : (
+        <BottomSheet
+          borderRadius={20}
+          overdragResistanceFactor={0}
+          renderContent={DiscoverSheetContent}
+          snapPoints={[300, 744]}
+          springConfig={discoverSheetSpring}
+        />
+      )}
+    </DiscoverSheetContext.Provider>
   );
 }
--- src/components/sheet/SlackSheet.js
+++ src/components/sheet/SlackSheet.js
@@ -1,9 +1,11 @@
 import React, { useMemo } from 'react';
-import { ScrollView } from 'react-native-gesture-handler';
+import { View } from 'react-native';
+import Animated from 'react-native-reanimated';
 import { useSafeArea } from 'react-native-safe-area-context';
 import styled from 'styled-components/primitives';
 import { useDimensions } from '../../hooks';
 import { Centered } from '../layout';
+import { useReanimatedValue } from '../list/MarqueeList';
 import SheetHandleFixedToTop, {
   SheetHandleFixedToTopHeight,
 } from './SheetHandleFixedToTop';
@@ -18,10 +20,22 @@ const Container = styled(Centered).attrs({ direction: 'column' })`
   right: 0;
 `;
 
-const Content = styled(ScrollView).attrs({
+const { event } = Animated;
+
+const Content = styled(Animated.ScrollView).attrs(({ y }) => ({
   directionalLockEnabled: true,
   keyboardShouldPersistTaps: 'always',
-})`
+  onScroll: event([
+    {
+      nativeEvent: {
+        contentOffset: {
+          y,
+        },
+      },
+    },
+  ]),
+  scrollEventThrottle: 16,
+}))`
   background-color: ${({ backgroundColor }) => backgroundColor};
   ${({ contentHeight, deviceHeight }) =>
     contentHeight ? `height: ${deviceHeight + contentHeight}` : null};
@@ -42,9 +56,11 @@ export default function SlackSheet({
   children,
   contentHeight,
   hideHandle = false,
+  renderHeader,
   scrollEnabled = true,
   ...props
 }) {
+  const yPosition = useReanimatedValue(0);
   const { height: deviceHeight } = useDimensions();
   const insets = useSafeArea();
   const bottomInset = useMemo(
@@ -70,23 +86,28 @@ export default function SlackSheet({
   return (
     <Container backgroundColor={backgroundColor} {...props}>
       {!hideHandle && <SheetHandleFixedToTop showBlur={scrollEnabled} />}
-      <Content
-        backgroundColor={backgroundColor}
-        contentContainerStyle={scrollEnabled && contentContainerStyle}
-        contentHeight={contentHeight}
-        deviceHeight={deviceHeight}
-        directionalLockEnabled
-        scrollEnabled={scrollEnabled}
-        scrollIndicatorInsets={scrollIndicatorInsets}
-      >
-        {children}
-        {!scrollEnabled && (
-          <Whitespace
-            backgroundColor={backgroundColor}
-            deviceHeight={deviceHeight}
-          />
-        )}
-      </Content>
+      <View style={{ backgroundColor, height: '100%', width: '100%' }}>
+        {renderHeader?.(yPosition)}
+
+        <Content
+          backgroundColor={backgroundColor}
+          contentContainerStyle={scrollEnabled && contentContainerStyle}
+          contentHeight={contentHeight}
+          deviceHeight={deviceHeight}
+          directionalLockEnabled
+          scrollEnabled={scrollEnabled}
+          scrollIndicatorInsets={scrollIndicatorInsets}
+          y={yPosition}
+        >
+          {children}
+          {!scrollEnabled && (
+            <Whitespace
+              backgroundColor={backgroundColor}
+              deviceHeight={deviceHeight}
+            />
+          )}
+        </Content>
+      </View>
     </Container>
   );
 }
