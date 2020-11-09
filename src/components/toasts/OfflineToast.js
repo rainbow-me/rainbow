311d7eb8cb64bed3e66036e23471d409aa48e5f2
Michał Osadnik <micosa97@gmail.com>
2020-11-09 22:20:32 +0100

    Connect two toasts if displayed in the same time (#1255)
    Co-authored-by: Skylar Barrera <skylar.barrera@gmail.com>

--- src/components/toasts/OfflineToast.js
+++ src/components/toasts/OfflineToast.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { useInternetStatus } from '../../hooks';
+import { web3Provider } from '../../handlers/web3';
+import networkTypes from '../../helpers/networkTypes';
+import { useAccountSettings, useInternetStatus } from '../../hooks';
 import Toast from './Toast';
 
 const OfflineToast = () => {
   const isConnected = useInternetStatus();
-  return <Toast icon="offline" isVisible={!isConnected} text="Offline" />;
+  const { network } = useAccountSettings();
+  const providerUrl = web3Provider?.connection?.url;
+  const isMainnet =
+    network === networkTypes.mainnet && !providerUrl?.startsWith('http://');
+  return (
+    <Toast
+      icon="offline"
+      isVisible={!isConnected && isMainnet}
+      text="Offline"
+    />
+  );
 };
 
 const neverRerender = () => true;
