8c9c33b490c84507698c86dd06eb632043cf7d51
Bruno Barbieri <brunobar79@gmail.com>
2020-11-11 21:09:12 -0500

    fix wyre webview header

--- src/screens/WyreWebview.js
+++ src/screens/WyreWebview.js
@@ -1,38 +1,46 @@
 import { useRoute } from '@react-navigation/core';
-import React, { Fragment, useEffect, useState } from 'react';
+import { HeaderBackButton } from '@react-navigation/stack';
+import React, { Fragment, useCallback, useEffect, useState } from 'react';
 import { WebView } from 'react-native-webview';
 import styled from 'styled-components/primitives';
 import Spinner from '../components/Spinner';
-import { BackButton } from '../components/header';
 import { Centered, Row } from '../components/layout';
 import { Text } from '../components/text';
 import { reserveWyreOrder } from '../handlers/wyre';
 import { useAccountSettings } from '../hooks';
 import { colors } from '../styles';
+import { useNavigation } from '@rainbow-me/navigation';
 
 const HeaderTitle = styled(Text).attrs({
-  align: 'center',
-  color: colors.blueGreyDark,
+  align: 'left',
+  color: colors.black,
   letterSpacing: 'roundedMedium',
   lineHeight: 'loose',
   opacity: 0.8,
   size: 'large',
   weight: 'bold',
-})``;
+})`
+  margin-left: 15;
+`;
 
 const Header = styled(Row).attrs({
   align: 'center',
-  justify: 'space-between',
+  backgroundColor: colors.white,
 })`
   height: 42;
   margin-bottom: 42;
-  top: 24;
+  top: 42;
   width: 100%;
 `;
 export default function WyreWebview() {
   const { params } = useRoute();
+  const { goBack } = useNavigation();
   const [url, setUrl] = useState(null);
   const { accountAddress, network } = useAccountSettings();
+
+  const handleBackButton = useCallback(() => {
+    goBack();
+  }, [goBack]);
 
   useEffect(() => {
     const getReservationId = async () => {
@@ -53,10 +61,8 @@ export default function WyreWebview() {
   return (
     <Fragment>
       <Header>
-        <BackButton />
-        <Centered cover>
-          <HeaderTitle>Add Cash</HeaderTitle>
-        </Centered>
+        <HeaderBackButton onPress={handleBackButton} />
+        <HeaderTitle>Add Cash</HeaderTitle>
       </Header>
       {url ? (
         <WebView
