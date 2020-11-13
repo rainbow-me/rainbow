887eb6f8d19ab39e3e54da75d79b86bb86ce9db6
skylarbarrera <skylar.barrera@gmail.com>
2020-11-12 21:29:28 -0700

    clean log - FAB

--- src/components/fab/FloatingActionButton.js
+++ src/components/fab/FloatingActionButton.js
@@ -55,8 +55,6 @@ const FloatingActionButton = ({
       useLateHaptic={false}
       {...props}
     >
-      {console.log(`FAB: ${testID}`)}
-
       <ShadowStack
         {...borders.buildCircleAsObject(size)}
         hideShadow={disabled}
