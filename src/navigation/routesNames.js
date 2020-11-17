2a0fd9dcdcb1f15260a17e02796c49ecebc29a34
Bruno Barbieri <brunobar79@gmail.com>
2020-11-17 00:20:30 -0500

    Update src/navigation/routesNames.js

--- src/navigation/routesNames.js
+++ src/navigation/routesNames.js
@@ -73,7 +73,9 @@ const RoutesWithNativeStackAvailability = {
   IMPORT_SEED_PHRASE_FLOW: isNativeStackAvailable
     ? Routes.IMPORT_SEED_PHRASE_SHEET_NAVIGATOR
     : Routes.IMPORT_SEED_PHRASE_SHEET,
-  SEND_FLOW: Routes.SEND_SHEET_NAVIGATOR,
+  SEND_FLOW: isNativeStackAvailable || android
+    ? Routes.SEND_SHEET_NAVIGATOR
+    : Routes.SEND_SHEET,
 };
 
 export default RoutesWithNativeStackAvailability;
