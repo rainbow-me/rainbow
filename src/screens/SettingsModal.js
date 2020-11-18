f6e7908e4b2595f54873f26f533d3a02e135b0c5
Bruno Barbieri <brunobar79@gmail.com>
2020-11-17 21:32:07 -0500

    fix settings modal navbar

--- src/screens/SettingsModal.js
+++ src/screens/SettingsModal.js
@@ -18,6 +18,7 @@ import SettingsBackupView from '../components/settings-menu/BackupSection/Settin
 import ShowSecretView from '../components/settings-menu/BackupSection/ShowSecretView';
 import WalletSelectionView from '../components/settings-menu/BackupSection/WalletSelectionView';
 import DevSection from '../components/settings-menu/DevSection';
+import { Text } from '../components/text';
 import WalletTypes from '../helpers/walletTypes';
 import { useDimensions, useWallets } from '../hooks';
 import { settingsOptions } from '../navigation/config';
@@ -91,12 +92,29 @@ const SettingsPages = {
   },
 };
 
+const EmptyButtonPlaceholder = styled.View`
+  flex: 1;
+`;
 const Container = styled.View`
   flex: 1;
   overflow: hidden;
 `;
 
 const Stack = createStackNavigator();
+
+const SettingsTitle = ({ children }) => {
+  return (
+    <Text
+      align="center"
+      color={colors.dark}
+      letterSpacing="roundedMedium"
+      size="large"
+      weight="bold"
+    >
+      {children}
+    </Text>
+  );
+};
 
 export default function SettingsModal() {
   const { goBack, navigate } = useNavigation();
@@ -167,16 +185,25 @@ export default function SettingsModal() {
     >
       <Container>
         <Stack.Navigator
-          screenOptions={{ ...settingsOptions, headerRight: renderHeaderRight }}
+          screenOptions={{
+            ...settingsOptions,
+            headerRight: renderHeaderRight,
+            ...(android && {
+              // eslint-disable-next-line react/display-name
+              headerRight: () => <EmptyButtonPlaceholder />,
+              // eslint-disable-next-line react/display-name
+              headerTitle: props => <SettingsTitle {...props} />,
+            }),
+          }}
         >
           <Stack.Screen
             name="SettingsSection"
             options={{
+              title: 'Settings',
               ...(android && {
                 // eslint-disable-next-line react/display-name
                 headerLeft: () => <HeaderBackButton onPress={goBack} />,
               }),
-              title: 'Settings',
             }}
           >
             {() => (
