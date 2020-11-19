a80db6897e9905a1b15eb7a083192e0fff257760
osdnk <micosa97@gmail.com>
2020-11-19 19:50:54 +0200

    additionalTopPadding={android

--- src/components/expanded-state/ChartExpandedState.js
+++ src/components/expanded-state/ChartExpandedState.js
@@ -167,7 +167,7 @@ export default function ChartExpandedState({ asset }) {
 
   return (
     <SlackSheet
-      additionalTopPadding
+      additionalTopPadding={android}
       contentHeight={ChartExpandedStateSheetHeight}
       scrollEnabled={false}
     >
