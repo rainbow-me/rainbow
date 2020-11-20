8023233ed446b70cef775100c11bacf2e5ae0d4b
Bruno Barbieri <brunobar79@gmail.com>
2020-11-20 00:58:11 -0500

    adjust chart expanded state height

--- src/components/expanded-state/ChartExpandedState.js
+++ src/components/expanded-state/ChartExpandedState.js
@@ -31,7 +31,7 @@ import { useNavigation } from '@rainbow-me/navigation';
 
 import { ModalContext } from 'react-native-cool-modals/NativeStackView';
 
-const heightWithChart = 606;
+const heightWithChart = 630;
 const heightWithNoChart = 309;
 
 const ActionRowAndroid = styled.View`
