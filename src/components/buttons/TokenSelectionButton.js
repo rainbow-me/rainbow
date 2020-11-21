beaa6c169cfc43dab0a2d4d1902fa3647d46c7df
Michael Demarais <m.demarais@gmail.com>
2020-11-20 23:31:07 -0500

    hook up exchange inputs to asset color

--- src/components/buttons/TokenSelectionButton.js
+++ src/components/buttons/TokenSelectionButton.js
@@ -1,16 +1,27 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useMemo } from 'react';
 import FastImage from 'react-native-fast-image';
 import styled from 'styled-components/primitives';
-import CaretImageSource from '../../assets/family-dropdown-arrow.png';
-import { magicMemo } from '../../utils';
 import { ButtonPressAnimation } from '../animations';
 import { InnerBorder, RowWithMargins } from '../layout';
 import { Text } from '../text';
+import CaretImageSource from '@rainbow-me/assets/family-dropdown-arrow.png';
+import { useColorForAsset } from '@rainbow-me/hooks';
 import { colors, padding, position } from '@rainbow-me/styles';
 import ShadowStack from 'react-native-shadow-stack';
 
-const Content = styled(RowWithMargins).attrs({ align: 'center', margin: 7 })`
+const TokenSelectionButtonHeight = 46;
+
+const Button = styled(ButtonPressAnimation).attrs({
+  contentContainerStyle: {
+    height: TokenSelectionButtonHeight,
+  },
+  overflowMargin: 30,
+})``;
+
+const Content = styled(RowWithMargins).attrs({
+  align: 'center',
+  margin: 7,
+})`
   ${padding(11, 14, 14, 16)};
   height: 46;
   z-index: 1;
@@ -26,73 +37,54 @@ const CaretIcon = styled(FastImage).attrs({
   width: 8;
 `;
 
-const ButtonShadows = styled(ShadowStack).attrs(({ symbol }) => ({
-  shadows: [
-    [0, 10, 30, colors.dark, 0.2],
-    [0, 5, 15, symbol ? colors.dark : colors.appleBlue, 0.4],
-  ],
-}))``;
-
-const Button = styled(ButtonPressAnimation).attrs({
-  overflowMargin: 30,
-})``;
-
-const TokenSelectionButton = ({
-  borderRadius,
+export default function TokenSelectionButton({
+  address,
+  borderRadius = 30,
   onPress,
-  shadows,
   symbol,
   testID,
-}) => (
-  <Button
-    contentContainerStyle={{
-      height: 46,
-    }}
-    onPress={onPress}
-    overflowMargin={30}
-    radiusAndroid={borderRadius}
-    testID={testID}
-    throttle
-  >
-    <ButtonShadows
-      {...position.coverAsObject}
-      backgroundColor={symbol ? colors.dark : colors.appleBlue}
-      borderRadius={borderRadius}
-      elevation={ios ? 0 : 8}
-      height={46}
-      shadows={shadows}
-      symbol={symbol}
-    />
-    <Content>
-      <Text
-        align="center"
-        color={colors.white}
-        size="large"
-        testID={testID + '-text'}
-        weight="bold"
-      >
-        {symbol || 'Choose Token'}
-      </Text>
-      <CaretIcon />
-    </Content>
-    <InnerBorder radius={borderRadius} />
-  </Button>
-);
+}) {
+  const colorForAsset = useColorForAsset(
+    { address },
+    address ? undefined : colors.appleBlue
+  );
+  const buttonShadows = useMemo(
+    () => [
+      [0, 10, 30, colors.dark, 0.2],
+      [0, 5, 15, colorForAsset, 0.4],
+    ],
+    [colorForAsset]
+  );
 
-TokenSelectionButton.propTypes = {
-  borderRadius: PropTypes.number,
-  onPress: PropTypes.func,
-  shadows: PropTypes.arrayOf(PropTypes.array),
-  showLockIcon: PropTypes.bool,
-  symbol: PropTypes.string,
-};
-
-TokenSelectionButton.defaultProps = {
-  borderRadius: 30,
-};
-
-export default magicMemo(TokenSelectionButton, [
-  'onPress',
-  'showLockIcon',
-  'symbol',
-]);
+  return (
+    <Button
+      onPress={onPress}
+      radiusAndroid={borderRadius}
+      testID={testID}
+      throttle
+    >
+      <ShadowStack
+        {...position.coverAsObject}
+        backgroundColor={colorForAsset}
+        borderRadius={borderRadius}
+        elevation={8}
+        height={TokenSelectionButtonHeight}
+        shadows={buttonShadows}
+        symbol={symbol}
+      />
+      <Content>
+        <Text
+          align="center"
+          color={colors.white}
+          size="large"
+          testID={testID + '-text'}
+          weight="bold"
+        >
+          {symbol || 'Choose Token'}
+        </Text>
+        <CaretIcon />
+      </Content>
+      <InnerBorder radius={borderRadius} />
+    </Button>
+  );
+}
--- src/components/exchange/ExchangeField.js
+++ src/components/exchange/ExchangeField.js
@@ -6,6 +6,7 @@ import { CoinIcon, CoinIconSize } from '../coin-icon';
 import { Row, RowWithMargins } from '../layout';
 import { EnDash } from '../text';
 import ExchangeInput from './ExchangeInput';
+import { useColorForAsset } from '@rainbow-me/hooks';
 import { borders, colors } from '@rainbow-me/styles';
 
 const ExchangeFieldHeight = android ? 64 : 38;
@@ -59,6 +60,7 @@ const ExchangeField = (
   },
   ref
 ) => {
+  const colorForAsset = useColorForAsset({ address });
   const handleFocusField = useCallback(() => ref?.current?.focus(), [ref]);
   useEffect(() => {
     autoFocus && handleFocusField();
@@ -73,6 +75,7 @@ const ExchangeField = (
             <CoinIconSkeleton />
           )}
           <Input
+            color={colorForAsset}
             editable={!!symbol}
             onBlur={onBlur}
             onChangeText={setAmount}
@@ -88,6 +91,7 @@ const ExchangeField = (
       </TouchableWithoutFeedback>
       {!disableCurrencySelection && (
         <TokenSelectionButton
+          address={address}
           onPress={onPressSelectCurrency}
           symbol={symbol}
           testID={testID + '-selection-button'}
