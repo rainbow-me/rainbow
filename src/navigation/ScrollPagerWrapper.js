a84c2674e4d1b8c1851825e77e0caceb7877b198
osdnk <micosa97@gmail.com>
2020-11-24 00:20:39 +0200

    use another pager

--- src/navigation/ScrollPagerWrapper.js
+++ src/navigation/ScrollPagerWrapper.js
@@ -1,20 +1,14 @@
 import React, { forwardRef } from 'react';
 import { Platform } from 'react-native';
 import { Value } from 'react-native-reanimated';
-import ViewPagerAdapter from 'react-native-tab-view-viewpager-adapter';
+import Pager from 'react-native-tab-view/lib/module/Pager';
 import ScrollPager from '../helpers/ScrollPager';
 
 export const scrollPosition = new Value(1);
 
 export default forwardRef(function ScrollPagerWrapper(props, ref) {
   return Platform.select({
-    android: (
-      <ViewPagerAdapter
-        {...props}
-        keyboardDismissMode="none"
-        overScrollMode="never"
-      />
-    ),
+    android: <Pager {...props} />,
     ios: <ScrollPager {...props} overscroll={false} ref={ref} />,
   });
 });
