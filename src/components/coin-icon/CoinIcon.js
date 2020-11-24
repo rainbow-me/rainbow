0754f789949dc75216863743fce5f29802111711
Michael Demarais <m.demarais@gmail.com>
2020-11-24 09:17:50 -0500

    always give CoinIcon a background-color for shadow perf reasons

--- src/components/coin-icon/CoinIcon.js
+++ src/components/coin-icon/CoinIcon.js
@@ -6,18 +6,30 @@ import CoinIconFallback from './CoinIconFallback';
 import CoinIconIndicator from './CoinIconIndicator';
 import { useColorForAsset, useTokenMetadata } from '@rainbow-me/hooks';
 import { RAINBOW_TOKEN_LIST } from '@rainbow-me/references/uniswap';
-import { colors } from '@rainbow-me/styles';
 import { isETH, magicMemo } from '@rainbow-me/utils';
 
 export const CoinIconSize = 40;
 
 const StyledCoinIcon = styled(ReactCoinIcon)`
+  background-color: ${({ color }) => {
+    // We have to give the outermost wrapper a background color
+    // or else React Native complains about shadow performance (and probably
+    // our perf is actually effected too).
+    // Read more about details of RN shadow / background requirements:
+    // 📎️ https://github.com/facebook/react-native/commit/e4c53c28aea7e067e48f5c8c0100c7cafc031b06
+    return color;
+  }};
   opacity: ${({ isHidden }) => (isHidden ? 0.4 : 1)};
 `;
 
+// Create a flattened array containing all tokenAddresses and tokenSymbols for
+// the tokens with nice SVG icons from our `react-coin-icons` package
 const tokensWithIcons = filter(RAINBOW_TOKEN_LIST.tokens, 'extensions.color')
   .map(({ address, symbol }) => [address, symbol])
   .flat();
+
+const isIconAvailable = addressOrSymbol =>
+  tokensWithIcons.includes(addressOrSymbol);
 
 const CoinIcon = ({
   address,
@@ -28,14 +40,13 @@ const CoinIcon = ({
   ...props
 }) => {
   const tokenMetadata = useTokenMetadata(address);
-  const color = useColorForAsset({ address }, colors.dark);
-  const shadowColor = tokenMetadata?.shadowColor || color;
+  const color = useColorForAsset({ address });
 
   const forceFallback = useMemo(
     () =>
       !isETH(address)
-        ? tokensWithIcons.includes(address)
-        : tokensWithIcons.includes(symbol) && !isETH(symbol),
+        ? isIconAvailable(address)
+        : isIconAvailable(symbol) && !isETH(symbol),
     [address, symbol]
   );
 
@@ -45,9 +56,10 @@ const CoinIcon = ({
       <StyledCoinIcon
         {...props}
         address={address}
+        color={color}
         fallbackRenderer={CoinIconFallback}
         forceFallback={forceFallback}
-        shadowColor={shadowColor}
+        shadowColor={tokenMetadata?.shadowColor || color}
         size={size}
         symbol={symbol}
       />
