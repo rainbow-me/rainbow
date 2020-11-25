1c34e1181ce20ded52fd3e5d90597e1cb24154c9
jinchung <jin.chung17@gmail.com>
2020-11-25 16:01:31 -0500

    Tidy up rainbow-me path imports

--- src/redux/explorer.js
+++ src/redux/explorer.js
@@ -1,8 +1,6 @@
 import { concat, get, isNil, keys, map, toLower } from 'lodash';
 import { DATA_API_KEY, DATA_ORIGIN } from 'react-native-dotenv';
 import io from 'socket.io-client';
-import { disableCharts, forceFallbackProvider } from '../config/debug';
-import NetworkTypes from '../helpers/networkTypes';
 import { assetChartsReceived, DEFAULT_CHART_TYPE } from './charts';
 import {
   addressAssetsReceived,
@@ -15,6 +13,8 @@ import {
   fallbackExplorerClearState,
   fallbackExplorerInit,
 } from './fallbackExplorer';
+import { disableCharts, forceFallbackProvider } from '@rainbow-me/config/debug';
+import NetworkTypes from '@rainbow-me/helpers/networkTypes';
 import logger from 'logger';
 
 // -- Constants --------------------------------------- //
