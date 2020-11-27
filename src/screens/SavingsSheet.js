65fc896c1a683e2fa25d2be1b288e7930cb21331
Christian Baroni <c@baroni.co>
2020-11-26 19:48:01 -0500

    Fix savings coin icons

--- src/screens/SavingsSheet.js
+++ src/screens/SavingsSheet.js
@@ -77,6 +77,7 @@ const SavingsSheet = () => {
 
   const savingsRowItem = useMemo(
     () => ({
+      address: underlying.address,
       lifetimeSupplyInterestAccrued,
       name: underlying.name,
       supplyBalanceUnderlying,
@@ -87,6 +88,7 @@ const SavingsSheet = () => {
       lifetimeSupplyInterestAccrued,
       supplyBalanceUnderlying,
       supplyRate,
+      underlying.address,
       underlying.name,
       underlying.symbol,
     ]
