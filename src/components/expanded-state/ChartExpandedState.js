6ba6a5a6c7874c3ab06478f69b5d2ead29b1cb87
Skylar Barrera <skylar.barrera@gmail.com>
2020-11-30 13:53:39 -0700

    fix sheet height for no data on charts - android (#1359)
    * fix noDataCharts
    
    * fix no price charts on android

--- src/components/expanded-state/ChartExpandedState.js
+++ src/components/expanded-state/ChartExpandedState.js
@@ -33,8 +33,9 @@ import { useNavigation } from '@rainbow-me/navigation';
 
 import { ModalContext } from 'react-native-cool-modals/NativeStackView';
 
-const heightWithChart = android ? 630 : 606;
-const heightWithNoChart = 309;
+//add's StatusBar height to android
+const heightWithChart = 606 + (android && 24);
+const heightWithNoChart = 309 + (android && 24);
 
 const ActionRowAndroid = styled.View`
   flex-direction: row;
@@ -94,7 +95,7 @@ function useJumpingForm(isLong) {
   }, [isLong, setOptions, jumpToShort, jumpToLong]);
 }
 
-export const ChartExpandedStateSheetHeight =
+export const initialChartExpandedStateSheetHeight =
   heightWithChart + (android ? 40 : 0);
 
 export default function ChartExpandedState({ asset }) {
@@ -168,6 +169,9 @@ export default function ChartExpandedState({ asset }) {
   if (duration.current === 0) {
     duration.current = 300;
   }
+  const ChartExpandedStateSheetHeight =
+    (ios || showChart ? heightWithChart : heightWithNoChart) +
+    (android ? 40 : 0);
 
   return (
     <SlackSheet
