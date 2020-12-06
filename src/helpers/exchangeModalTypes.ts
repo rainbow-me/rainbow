fb3b4bff4e1e38ec3ce95a537e8b4d5c4a697128
jinchung <jin.chung17@gmail.com>
2020-12-06 13:45:20 -0500

    Update exchangeModalTypes to typescript

---
+++ src/helpers/exchangeModalTypes.ts
@@ -0,0 +1,17 @@
+export enum ExchangeModalTypes {
+  addLiquidity = 'addLiquidity',
+  deposit = 'deposit',
+  removeLiquidity = 'removeLiquidity',
+  swap = 'swap',
+  withdrawal = 'withdrawal',
+}
+
+// We need to keep this one until
+// we have typescript everywhere
+export default {
+  addLiquidity: 'addLiquidity',
+  deposit: 'deposit',
+  removeLiquidity: 'removeLiquidity',
+  swap: 'swap',
+  withdrawal: 'withdrawal',
+};
