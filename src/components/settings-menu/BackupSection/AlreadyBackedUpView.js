cae9f18017ecffe0118b08efca3d8fb6be2cc780
Christian Baroni <c@baroni.co>
2020-12-09 16:22:32 -0500

    Improve cloud backup copy, remove unnecessary $

--- src/components/settings-menu/BackupSection/AlreadyBackedUpView.js
+++ src/components/settings-menu/BackupSection/AlreadyBackedUpView.js
@@ -142,14 +142,14 @@ export default function AlreadyBackedUpView() {
   }, [walletId]);
 
   const manageCloudBackups = useCallback(() => {
-    const buttons = [`Delete all ${cloudPlatform} backups`, 'Cancel'];
+    const buttons = [`Delete All ${cloudPlatform} Backups`, 'Cancel'];
 
     showActionSheetWithOptions(
       {
         cancelButtonIndex: 1,
         destructiveButtonIndex: 0,
         options: buttons,
-        title: `Manage ${cloudPlatform} backups`,
+        title: `Manage ${cloudPlatform} Backups`,
       },
       buttonIndex => {
         if (buttonIndex === 0) {
@@ -158,8 +158,8 @@ export default function AlreadyBackedUpView() {
             {
               cancelButtonIndex: 1,
               destructiveButtonIndex: 0,
-              message: `Are you sure you want to delete all ${cloudPlatform} Backups`,
-              options: [`YES, Delete All ${cloudPlatform} Backups`, 'Cancel'],
+              message: `Are you sure you want to delete your ${cloudPlatform} wallet backups?`,
+              options: [`Confirm and Delete Backups`, 'Cancel'],
             },
             async buttonIndex => {
               if (buttonIndex === 0) {
@@ -276,7 +276,7 @@ export default function AlreadyBackedUpView() {
               size="large"
               weight="semibold"
             >
-              􀙶 Back up {cloudPlatform}
+              􀙶 Back up to {cloudPlatform}
             </Text>
           </ButtonPressAnimation>
         </Footer>
@@ -290,7 +290,7 @@ export default function AlreadyBackedUpView() {
               size="lmedium"
               weight="semibold"
             >
-              􀍢 Manage {cloudPlatform} backups
+              􀍢 Manage {cloudPlatform} Backups
             </Text>
           </ButtonPressAnimation>
         </Footer>
