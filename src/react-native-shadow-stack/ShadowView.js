b55a3c23129b4229d874d2dbefe7650f12a651de
osdnk <micosa97@gmail.com>
2020-12-10 23:31:02 +0200

    else

--- src/react-native-shadow-stack/ShadowView.js
+++ src/react-native-shadow-stack/ShadowView.js
@@ -4,14 +4,13 @@ import { StyleSheet, View } from 'react-native';
 const ShadowView = props => {
   if (ios || props.elevation > 0) {
     return <View {...props} />;
-  } else {
-    return (
-      <View
-        {...props}
-        elevation={Math.min(StyleSheet.flatten(props.style).shadowRadius, 5)}
-      />
-    );
   }
+  return (
+    <View
+      {...props}
+      elevation={Math.min(StyleSheet.flatten(props.style).shadowRadius, 5)}
+    />
+  );
 };
 
 export default ShadowView;
