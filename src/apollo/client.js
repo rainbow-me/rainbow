c3ad8c94887d4b804e94dbd3615876d3e1a44bf1
osdnk <micosa97@gmail.com>
2020-12-15 17:44:50 +0200

    Remove apollo caching

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,19 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
+const defaultOptions = {
+  query: {
+    errorPolicy: 'all',
+    fetchPolicy: 'no-cache',
+  },
+  watchQuery: {
+    errorPolicy: 'ignore',
+    fetchPolicy: 'no-cache',
+  },
+};
+
 export const compoundClient = new ApolloClient({
   cache: new InMemoryCache(),
+  defaultOptions,
   link: new HttpLink({
     uri: 'https://api.thegraph.com/subgraphs/name/graphprotocol/compound-v2',
   }),
@@ -9,6 +21,7 @@ export const compoundClient = new ApolloClient({
 
 export const uniswapClient = new ApolloClient({
   cache: new InMemoryCache(),
+  defaultOptions,
   link: new HttpLink({
     uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
   }),
