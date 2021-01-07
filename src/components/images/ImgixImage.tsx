ad928d22c99a896cecc713c9b47686ae40ac12ee
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2021-01-07 18:27:07 +0000

    fix: ensure signing in ImgixImage class component

--- src/components/images/ImgixImage.tsx
+++ src/components/images/ImgixImage.tsx
@@ -11,9 +11,36 @@ export type ImgixImageProps = FastImageProps & {
 // Animated.createAnimatedComponent. We cannot render an AnimatedFastImage
 // instead because this introduces a non-conform interface to callers,
 // resulting in rendering issues (specifically on the SavingsListHeader).
-class ImgixImage extends React.PureComponent<ImgixImageProps> {
+class ImgixImage extends React.PureComponent<ImgixImageProps, ImgixImageProps> {
+  constructor(props: ImgixImageProps) {
+    super(props);
+    const { source } = props;
+    this.state = {
+      source:
+        !!source && typeof source === 'object'
+          ? maybeSignSource(source)
+          : source,
+    };
+  }
+  componentDidUpdate(prevProps: ImgixImageProps) {
+    const { source: prevSource } = prevProps;
+    const { source } = this.props;
+    // Has the source changed?
+    if (prevSource !== source) {
+      // If the source has changed and looks signable, attempt to sign it.
+      if (!!source && typeof source === 'object') {
+        Object.assign(this.state, { source: maybeSignSource(source) });
+      } else {
+        // Else propagate the source as normal.
+        Object.assign(this.state, { source });
+      }
+    }
+  }
   render() {
-    const { Component: maybeComponent, source, ...props } = this.props;
+    const { Component: maybeComponent, ...props } = this.props;
+    // Use the local state as the signing source, as opposed to the prop directly.
+    // (The source prop may point to an untrusted URL.)
+    const { source } = this.state;
     const Component = maybeComponent || FastImage;
     return <Component {...props} source={source} />;
   }
