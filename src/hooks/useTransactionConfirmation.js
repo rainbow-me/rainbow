0216dc01eef6264163ffb22fc3e9b916c077526e
Bruno Barbieri <brunobar79@gmail.com>
2021-01-08 13:37:01 -0500

    remove nonce reducer

--- src/hooks/useTransactionConfirmation.js
+++ src/hooks/useTransactionConfirmation.js
@@ -1,20 +1,11 @@
-import { useSelector } from 'react-redux';
 import { dataAddNewTransaction } from '../redux/data';
-import { updateTransactionCountNonce } from '../redux/nonce';
 import { removeRequest } from '../redux/requests';
 import { walletConnectSendStatus } from '../redux/walletconnect';
 
 export default function useTransactionConfirmation() {
-  const { transactionCountNonce } = useSelector(
-    ({ nonce: { transactionCountNonce } }) => ({
-      transactionCountNonce,
-    })
-  );
   return {
     dataAddNewTransaction,
     removeRequest,
-    transactionCountNonce,
-    updateTransactionCountNonce,
     walletConnectSendStatus,
   };
 }
