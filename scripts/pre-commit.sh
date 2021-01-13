3ecb26422376fa55627de4d050cb366f3c480969
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2021-01-13 23:17:16 +0000

    chore: ignore Internals.swift changes

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yarn lint-staged;
 
-# Ignore any tracked changes to internals.
+# Ignore any potential tracked changes to mutable Internals.
 git update-index --assume-unchanged "ios/Internals/ios/Internals.h"
 git update-index --assume-unchanged "ios/Internals/ios/Internals.m"
+git update-index --assume-unchanged "ios/Internals/ios/Internals.swift"
