08209589f8fc776938473d91abf0cd07718d8ee5
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2021-01-13 18:25:11 +0000

    chore: update pre-commit to ignore modifications to the internals directory

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 yarn lint-staged;
+
+RAINBOW_INTERNALS=(`find ios/Internals -type f`)
+RAINBOW_INTERNALS_IGNORED=(`git ls-files -o ios/Internals`)
+
+echo $RAINBOW_INTERNALS_IGNORED
+
+# Ignore any tracked changes to internals.
+for i in "${RAINBOW_INTERNALS[@]}"
+do
+  # Only ignore changes to files which are permitted to be tracked,
+  # i.e. they are *not* already specified within a .gitignore pattern.
+  if [[ ! " ${RAINBOW_INTERNALS_IGNORED[@]} " =~ " $i " ]]; then
+     git update-index --assume-unchanged "$i"&
+  fi
+done
