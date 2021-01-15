fd93c7fda074426424fd1ed88ba96fe3a218be35
Skylar Barrera <skylar.barrera@gmail.com>
2021-01-15 00:55:07 -0700

    remove ganache from android for CI testing

--- e2e/ganacheTransactionFlow.spec.js
+++ e2e/ganacheTransactionFlow.spec.js
@@ -15,73 +15,78 @@ describe('Ganache Transaction Flow', () => {
     await Helpers.checkIfVisible('welcome-screen');
   });
 
-  it('Should show the "Restore Sheet" after tapping on "I already have a wallet"', async () => {
-    await Helpers.tap('already-have-wallet-button');
-    await Helpers.delay(3000);
-    await Helpers.checkIfExists('restore-sheet');
-  });
+  if (device.getPlatform() === 'ios') {
+    it('Should show the "Restore Sheet" after tapping on "I already have a wallet"', async () => {
+      await Helpers.tap('already-have-wallet-button');
+      await Helpers.delay(3000);
+      await Helpers.checkIfExists('restore-sheet');
+    });
 
-  it('show the "Import Sheet" when tapping on "Restore with a recovery phrase or private key"', async () => {
-    await Helpers.tap('restore-with-key-button');
-    await Helpers.delay(3000);
-    await Helpers.checkIfExists('import-sheet');
-  });
+    it('show the "Import Sheet" when tapping on "Restore with a recovery phrase or private key"', async () => {
+      await Helpers.tap('restore-with-key-button');
+      await Helpers.delay(3000);
+      await Helpers.checkIfExists('import-sheet');
+    });
 
-  it('Should show the "Add wallet modal" after tapping import with a valid seed"', async () => {
-    await Helpers.typeText('import-sheet-input', process.env.TEST_SEEDS, false);
-    await Helpers.delay(1500);
-    await Helpers.checkIfElementHasString(
-      'import-sheet-button-label',
-      'Import'
-    );
-    await Helpers.tap('import-sheet-button');
-    await Helpers.checkIfVisible('wallet-info-modal');
-  });
+    it('Should show the "Add wallet modal" after tapping import with a valid seed"', async () => {
+      await Helpers.typeText(
+        'import-sheet-input',
+        process.env.TEST_SEEDS,
+        false
+      );
+      await Helpers.delay(1500);
+      await Helpers.checkIfElementHasString(
+        'import-sheet-button-label',
+        'Import'
+      );
+      await Helpers.tap('import-sheet-button');
+      await Helpers.checkIfVisible('wallet-info-modal');
+    });
 
-  it('Should navigate to the Wallet screen after tapping on "Import Wallet"', async () => {
-    await Helpers.delay(2000);
-    await Helpers.tap('wallet-info-submit-button');
-    if (device.getPlatform() === 'android') {
-      await Helpers.checkIfVisible('pin-authentication-screen');
-      // Set the pin
-      await Helpers.authenticatePin('1234');
-      // Confirm it
-      await Helpers.authenticatePin('1234');
-    }
-    await Helpers.delay(3000);
-    await Helpers.checkIfVisible('wallet-screen');
-    await Helpers.delay(5000);
-  });
+    it('Should navigate to the Wallet screen after tapping on "Import Wallet"', async () => {
+      await Helpers.delay(2000);
+      await Helpers.tap('wallet-info-submit-button');
+      if (device.getPlatform() === 'android') {
+        await Helpers.checkIfVisible('pin-authentication-screen');
+        // Set the pin
+        await Helpers.authenticatePin('1234');
+        // Confirm it
+        await Helpers.authenticatePin('1234');
+      }
+      await Helpers.delay(3000);
+      await Helpers.checkIfVisible('wallet-screen');
+      await Helpers.delay(5000);
+    });
 
-  it('Should navigate to the Profile screen after swiping right', async () => {
-    await Helpers.delay(1000);
-    await Helpers.swipe('wallet-screen', 'right', 'slow');
-    await Helpers.delay(3000);
-    await Helpers.checkIfVisible('profile-screen');
-    await Helpers.delay(3000);
-  });
+    it('Should navigate to the Profile screen after swiping right', async () => {
+      await Helpers.delay(1000);
+      await Helpers.swipe('wallet-screen', 'right', 'slow');
+      await Helpers.delay(3000);
+      await Helpers.checkIfVisible('profile-screen');
+      await Helpers.delay(3000);
+    });
 
-  it('Should navigate to Settings Modal after tapping Settings Button', async () => {
-    await Helpers.tap('settings-button');
-    await Helpers.delay(3000);
-    await Helpers.checkIfVisible('settings-modal');
-  });
+    it('Should navigate to Settings Modal after tapping Settings Button', async () => {
+      await Helpers.tap('settings-button');
+      await Helpers.delay(3000);
+      await Helpers.checkIfVisible('settings-modal');
+    });
 
-  it('Should navigate to Developer Settings after tapping Developer Section', async () => {
-    await Helpers.tap('developer-section');
-    await Helpers.delay(3000);
-    await Helpers.checkIfVisible('developer-settings-modal');
-  });
+    it('Should navigate to Developer Settings after tapping Developer Section', async () => {
+      await Helpers.tap('developer-section');
+      await Helpers.delay(3000);
+      await Helpers.checkIfVisible('developer-settings-modal');
+    });
 
-  it('Should show Ganache Toast after pressing Connect To Ganache', async () => {
-    await Helpers.tap('ganache-section');
-    await Helpers.delay(13000);
-    await Helpers.checkIfVisible('testnet-toast-Ganache');
-    await Helpers.swipe('profile-screen', 'left', 'slow');
-    await Helpers.delay(3000);
-  });
+    it('Should show Ganache Toast after pressing Connect To Ganache', async () => {
+      await Helpers.tap('ganache-section');
+      await Helpers.delay(13000);
+      await Helpers.checkIfVisible('testnet-toast-Ganache');
+      await Helpers.swipe('profile-screen', 'left', 'slow');
+      await Helpers.delay(3000);
+    });
 
-  /*
+    /*
   it('Should swap ETH -> ERC20 (DAI)', async () => {
     await Helpers.tap('exchange-fab');
     await Helpers.delay(3000);
@@ -138,28 +143,28 @@ describe('Ganache Transaction Flow', () => {
     await Helpers.swipe('profile-screen', 'left', 'slow');
   });*/
 
-  it('Should send ERC20 (cSAI)', async () => {
-    await Helpers.delay(3000);
-    await Helpers.tap('send-fab');
-    await Helpers.delay(3000);
-    await Helpers.typeText('send-asset-form-field', 'poopcoin.eth\n', false);
-    await Helpers.delay(3000);
-    await Helpers.tap('send-savings-cSAI');
-    await Helpers.delay(3000);
-    await Helpers.typeText('selected-asset-field-input', '1', true);
-    await Helpers.delay(5000);
-    await Helpers.tapAndLongPress('send-sheet-confirm');
-    if (device.getPlatform() === 'android') {
-      await Helpers.tapAlertWithButton('Slow');
-      await Helpers.delay(4000);
-      await Helpers.authenticatePin('1234');
-    }
+    it('Should send ERC20 (cSAI)', async () => {
+      await Helpers.delay(3000);
+      await Helpers.tap('send-fab');
+      await Helpers.delay(3000);
+      await Helpers.typeText('send-asset-form-field', 'poopcoin.eth\n', false);
+      await Helpers.delay(3000);
+      await Helpers.tap('send-savings-cSAI');
+      await Helpers.delay(3000);
+      await Helpers.typeText('selected-asset-field-input', '1', true);
+      await Helpers.delay(5000);
+      await Helpers.tapAndLongPress('send-sheet-confirm');
+      if (device.getPlatform() === 'android') {
+        await Helpers.tapAlertWithButton('Slow');
+        await Helpers.delay(4000);
+        await Helpers.authenticatePin('1234');
+      }
 
-    await Helpers.delay(10000);
-    await Helpers.swipe('profile-screen', 'left', 'slow');
-  });
+      await Helpers.delay(10000);
+      await Helpers.swipe('profile-screen', 'left', 'slow');
+    });
 
-  /*it('Should show completed swap ETH -> ERC20 (DAI)', async () => {
+    /*it('Should show completed swap ETH -> ERC20 (DAI)', async () => {
     try {
       await Helpers.checkIfVisible('Swapped-Ethereum');
     } catch (e) {
@@ -168,66 +173,66 @@ describe('Ganache Transaction Flow', () => {
     await Helpers.swipe('profile-screen', 'left', 'slow');
   });*/
 
-  it('Should send (Cryptokitties)', async () => {
-    await Helpers.delay(3000);
-    await Helpers.tap('send-fab');
-    await Helpers.delay(3000);
-    await Helpers.typeText('send-asset-form-field', 'poopcoin.eth\n', false);
-    await Helpers.delay(3000);
-    await Helpers.tap('CryptoKitties-family-header');
-    await Helpers.delay(2000);
-    await Helpers.tapByText('Arun Cattybinky');
-    await Helpers.delay(3000);
-    await Helpers.tapAndLongPress('send-sheet-confirm');
-    if (device.getPlatform() === 'android') {
-      await Helpers.tapAlertWithButton('Slow');
-      await Helpers.delay(4000);
-      await Helpers.authenticatePin('1234');
-    }
-    await Helpers.delay(10000);
-    await Helpers.swipe('profile-screen', 'left', 'slow');
-  });
+    it('Should send (Cryptokitties)', async () => {
+      await Helpers.delay(3000);
+      await Helpers.tap('send-fab');
+      await Helpers.delay(3000);
+      await Helpers.typeText('send-asset-form-field', 'poopcoin.eth\n', false);
+      await Helpers.delay(3000);
+      await Helpers.tap('CryptoKitties-family-header');
+      await Helpers.delay(2000);
+      await Helpers.tapByText('Arun Cattybinky');
+      await Helpers.delay(3000);
+      await Helpers.tapAndLongPress('send-sheet-confirm');
+      if (device.getPlatform() === 'android') {
+        await Helpers.tapAlertWithButton('Slow');
+        await Helpers.delay(4000);
+        await Helpers.authenticatePin('1234');
+      }
+      await Helpers.delay(10000);
+      await Helpers.swipe('profile-screen', 'left', 'slow');
+    });
 
-  it('Should send ERC20 (BAT)', async () => {
-    await Helpers.delay(3000);
-    await Helpers.tap('send-fab');
-    await Helpers.delay(3000);
-    await Helpers.typeText('send-asset-form-field', 'poopcoin.eth\n', false);
-    await Helpers.delay(3000);
-    await Helpers.tap('send-asset-BAT');
-    await Helpers.delay(3000);
-    await Helpers.typeText('selected-asset-field-input', '2', true);
-    await Helpers.delay(3000);
-    await Helpers.tapAndLongPress('send-sheet-confirm');
-    if (device.getPlatform() === 'android') {
-      await Helpers.tapAlertWithButton('Slow');
-      await Helpers.delay(4000);
-      await Helpers.authenticatePin('1234');
-    }
-    await Helpers.delay(10000);
-    await Helpers.swipe('profile-screen', 'left', 'slow');
-  });
+    it('Should send ERC20 (BAT)', async () => {
+      await Helpers.delay(3000);
+      await Helpers.tap('send-fab');
+      await Helpers.delay(3000);
+      await Helpers.typeText('send-asset-form-field', 'poopcoin.eth\n', false);
+      await Helpers.delay(3000);
+      await Helpers.tap('send-asset-BAT');
+      await Helpers.delay(3000);
+      await Helpers.typeText('selected-asset-field-input', '2', true);
+      await Helpers.delay(3000);
+      await Helpers.tapAndLongPress('send-sheet-confirm');
+      if (device.getPlatform() === 'android') {
+        await Helpers.tapAlertWithButton('Slow');
+        await Helpers.delay(4000);
+        await Helpers.authenticatePin('1234');
+      }
+      await Helpers.delay(10000);
+      await Helpers.swipe('profile-screen', 'left', 'slow');
+    });
 
-  it('Should send ETH', async () => {
-    await Helpers.delay(3000);
-    await Helpers.tap('send-fab');
-    await Helpers.delay(3000);
-    await Helpers.typeText('send-asset-form-field', 'poopcoin.eth\n', false);
-    await Helpers.delay(3000);
-    await Helpers.tap('send-asset-ETH');
-    await Helpers.delay(3000);
-    await Helpers.typeText('selected-asset-field-input', '.001', true);
-    await Helpers.delay(3000);
-    await Helpers.tapAndLongPress('send-sheet-confirm');
-    if (device.getPlatform() === 'android') {
-      await Helpers.tapAlertWithButton('Slow');
-      await Helpers.delay(4000);
-      await Helpers.authenticatePin('1234');
-    }
-    await Helpers.delay(10000);
-  });
+    it('Should send ETH', async () => {
+      await Helpers.delay(3000);
+      await Helpers.tap('send-fab');
+      await Helpers.delay(3000);
+      await Helpers.typeText('send-asset-form-field', 'poopcoin.eth\n', false);
+      await Helpers.delay(3000);
+      await Helpers.tap('send-asset-ETH');
+      await Helpers.delay(3000);
+      await Helpers.typeText('selected-asset-field-input', '.001', true);
+      await Helpers.delay(3000);
+      await Helpers.tapAndLongPress('send-sheet-confirm');
+      if (device.getPlatform() === 'android') {
+        await Helpers.tapAlertWithButton('Slow');
+        await Helpers.delay(4000);
+        await Helpers.authenticatePin('1234');
+      }
+      await Helpers.delay(10000);
+    });
 
-  /*
+    /*
   it('Should show completed swap ERC20 (BAT) -> ERC20 (ZRX)', async () => {
     await Helpers.delay(3000);
     try {
@@ -249,37 +254,38 @@ describe('Ganache Transaction Flow', () => {
       await Helpers.checkIfVisible('Sending-Compound Sai');
     }
   });*/
-  it('Should show completed send ERC20 (cSAI)', async () => {
-    try {
-      await Helpers.checkIfVisible('Sent-Compound SAI');
-    } catch (e) {
-      await Helpers.checkIfVisible('Sending-Compound SAI');
-    }
-  });
+    it('Should show completed send ERC20 (cSAI)', async () => {
+      try {
+        await Helpers.checkIfVisible('Sent-Compound SAI');
+      } catch (e) {
+        await Helpers.checkIfVisible('Sending-Compound SAI');
+      }
+    });
 
-  it('Should show completed send NFT (Cryptokitties)', async () => {
-    try {
-      await Helpers.checkIfVisible('Sent-Arun Cattybinky');
-    } catch (e) {
-      await Helpers.checkIfVisible('Sending-Arun Cattybinky');
-    }
-  });
+    it('Should show completed send NFT (Cryptokitties)', async () => {
+      try {
+        await Helpers.checkIfVisible('Sent-Arun Cattybinky');
+      } catch (e) {
+        await Helpers.checkIfVisible('Sending-Arun Cattybinky');
+      }
+    });
 
-  it('Should show completed send ERC20 (BAT)', async () => {
-    try {
-      await Helpers.checkIfVisible('Sent-Basic Attention Token');
-    } catch (e) {
-      await Helpers.checkIfVisible('Sending-Basic Attention Token');
-    }
-  });
+    it('Should show completed send ERC20 (BAT)', async () => {
+      try {
+        await Helpers.checkIfVisible('Sent-Basic Attention Token');
+      } catch (e) {
+        await Helpers.checkIfVisible('Sending-Basic Attention Token');
+      }
+    });
 
-  it('Should show completed send ETH', async () => {
-    try {
-      await Helpers.checkIfVisible('Sent-Ethereum');
-    } catch (e) {
-      await Helpers.checkIfVisible('Sending-Ethereum');
-    }
-  });
+    it('Should show completed send ETH', async () => {
+      try {
+        await Helpers.checkIfVisible('Sent-Ethereum');
+      } catch (e) {
+        await Helpers.checkIfVisible('Sending-Ethereum');
+      }
+    });
+  }
 
   afterAll(async () => {
     // Reset the app state
