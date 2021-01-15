37d1365d2c2bf48462b18f6a138b1b2ef129b7bd
jinchung <jin.chung17@gmail.com>
2021-01-15 14:53:47 -0700

    Remove unused withCoinCurrentAction hoc

--- src/hoc/index.js
+++ src/hoc/index.js
@@ -2,7 +2,6 @@ export { default as withAccountInfo } from './withAccountInfo';
 export { default as withAccountSettings } from './withAccountSettings';
 export { default as withActionSheetManager } from './withActionSheetManager';
 export { default as withAppState } from './withAppState';
-export { default as withCoinCurrentAction } from './withCoinCurrentAction';
 export { default as withCoinListEdited } from './withCoinListEdited';
 export { default as withCoinRecentlyPinned } from './withCoinRecentlyPinned';
 export { default as withEditOptions } from './withEditOptions';
