cf1d21a796e01ed10fb9a54d949e062bb4244a19
jinchung <jin.chung17@gmail.com>
2021-01-15 14:57:27 -0700

    Remove unused actionSheetManager redux and hoc

--- src/hoc/index.js
+++ src/hoc/index.js
@@ -1,6 +1,5 @@
 export { default as withAccountInfo } from './withAccountInfo';
 export { default as withAccountSettings } from './withAccountSettings';
-export { default as withActionSheetManager } from './withActionSheetManager';
 export { default as withAppState } from './withAppState';
 export { default as withCoinListEdited } from './withCoinListEdited';
 export { default as withCoinRecentlyPinned } from './withCoinRecentlyPinned';
