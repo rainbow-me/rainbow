7ebce8bdc2d6c1968c8e556552e2af07efb8e19e
Michael Demarais <m.demarais@gmail.com>
2021-01-16 22:48:37 -0500

    cleaup SwapDetails related components

--- src/components/expanded-state/swap-details/CurrencyTile.js
+++ src/components/expanded-state/swap-details/CurrencyTile.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import RadialGradient from 'react-native-radial-gradient';
 import styled from 'styled-components/primitives';
 import { CoinIcon } from '../../coin-icon';
-import { Centered, ColumnWithMargins } from '../../layout';
-import { TruncatedText } from '../../text';
-import { useColorForAsset } from '@rainbow-me/hooks';
+import { Centered, ColumnWithMargins, Row } from '../../layout';
+import { Text, TruncatedText } from '../../text';
+import {
+  convertAmountAndPriceToNativeDisplay,
+  updatePrecisionToDisplay,
+} from '@rainbow-me/helpers/utilities';
+import { useAccountSettings, useColorForAsset } from '@rainbow-me/hooks';
 import { colors, position } from '@rainbow-me/styles';
 
-const CurrencyTileHeight = 143;
+export const CurrencyTileHeight = 143;
+
+const AmountText = styled(Text).attrs({
+  color: colors.blueGreyDark80,
+  letterSpacing: 'roundedTight',
+  size: 'smedium',
+  weight: 'semibold',
+})``;
 
 const Container = styled(Centered).attrs({
   direction: 'column',
@@ -31,15 +42,35 @@ const NativePriceText = styled(TruncatedText).attrs({
   weight: 'heavy',
 })``;
 
+const TruncatedAmountText = styled(AmountText).attrs({
+  as: TruncatedText,
+})`
+  flex-grow: 0;
+  flex-shrink: 1;
+`;
+
 export default function CurrencyTile({
-  amountDisplay,
+  amount,
   asset,
+  priceValue,
   isHighSlippage,
   type = 'input',
   ...props
 }) {
   const { address, symbol } = asset;
+  const { nativeCurrency } = useAccountSettings();
   const colorForAsset = useColorForAsset(asset);
+
+  const { amountDisplay, priceDisplay } = useMemo(() => {
+    const data = [amount, priceValue || 0];
+    return {
+      amountDisplay: updatePrecisionToDisplay(...data, true),
+      priceDisplay: convertAmountAndPriceToNativeDisplay(
+        ...data,
+        nativeCurrency
+      ).display,
+    };
+  }, [amount, nativeCurrency, priceValue]);
 
   return (
     <Container {...props}>
@@ -48,19 +79,17 @@ export default function CurrencyTile({
         <CoinIcon address={address} size={50} symbol={symbol} />
         <ColumnWithMargins centered margin={4} paddingHorizontal={8}>
           <NativePriceText>
-            $123.45
+            {priceDisplay}
             {isHighSlippage && (
               <NativePriceText color={colors.warning}>{` 􀇿`}</NativePriceText>
             )}
           </NativePriceText>
-          <TruncatedText
-            color={colors.blueGreyDark80}
-            letterSpacing="roundedTight"
-            size="smedium"
-            weight="semibold"
-          >
-            {`${type === 'output' ? '~' : ''}${amountDisplay} ${symbol}`}
-          </TruncatedText>
+          <Row align="center">
+            <TruncatedAmountText>
+              {`${type === 'output' ? '~' : ''}${amountDisplay}`}
+            </TruncatedAmountText>
+            <AmountText>{` ${symbol}`}</AmountText>
+          </Row>
         </ColumnWithMargins>
       </ColumnWithMargins>
     </Container>
