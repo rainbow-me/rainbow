5ff84a094f6e06f15ab48c33e560ed4a99bc4ee7
Michael Demarais <m.demarais@gmail.com>
2021-01-17 07:59:26 -0500

    continue cleanup SwapDetails related components

---
+++ src/components/expanded-state/swap-details/SwapDetailsContent.js
@@ -0,0 +1,68 @@
+import { useRoute } from '@react-navigation/native';
+import React from 'react';
+import styled from 'styled-components/primitives';
+import { ColumnWithMargins } from '../../layout';
+import SwapDetailsContractRow from './SwapDetailsContractRow';
+import SwapDetailsPriceRow from './SwapDetailsPriceRow';
+import SwapDetailsRow, {
+  SwapDetailsRowHeight,
+  SwapDetailsValue,
+} from './SwapDetailsRow';
+import SwapDetailsUniswapRow from './SwapDetailsUniswapRow';
+import { useSlippageDetails } from '@rainbow-me/hooks';
+import { padding } from '@rainbow-me/styles';
+import { isETH } from '@rainbow-me/utils';
+
+const contentRowMargin = 24;
+export const SwapDetailsContentMinHeight =
+  (SwapDetailsRowHeight + contentRowMargin) * 4;
+
+const Container = styled(ColumnWithMargins).attrs({
+  flex: 1,
+  margin: contentRowMargin,
+})`
+  ${({ isHighSlippage }) => padding(isHighSlippage ? 24 : 30, 19, 30)};
+`;
+
+export default function SwapDetailsContent({
+  onCopySwapDetailsText,
+  ...props
+}) {
+  const {
+    params: { inputCurrency, outputCurrency, slippage },
+  } = useRoute();
+
+  const {
+    color: slippageColor,
+    isHighSlippage,
+    percentDisplay,
+  } = useSlippageDetails(slippage);
+
+  return (
+    <Container
+      isHighSlippage={isHighSlippage}
+      testID="swap-details-state"
+      {...props}
+    >
+      <SwapDetailsRow label="Price impact">
+        <SwapDetailsValue color={slippageColor} letterSpacing="roundedTight">
+          {`${percentDisplay}%`}
+        </SwapDetailsValue>
+      </SwapDetailsRow>
+      <SwapDetailsPriceRow />
+      {!isETH(inputCurrency?.address) && (
+        <SwapDetailsContractRow
+          asset={inputCurrency}
+          onCopySwapDetailsText={onCopySwapDetailsText}
+        />
+      )}
+      {!isETH(outputCurrency?.address) && (
+        <SwapDetailsContractRow
+          asset={outputCurrency}
+          onCopySwapDetailsText={onCopySwapDetailsText}
+        />
+      )}
+      <SwapDetailsUniswapRow />
+    </Container>
+  );
+}
