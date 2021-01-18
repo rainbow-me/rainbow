59d9323b9d1ee2cd1572a9f1787389913f20ce60
Michael Demarais <m.demarais@gmail.com>
2021-01-18 05:36:18 -0500

    Cleanup and animate ExchangeDetailsRow / SlippageWarning components

--- src/components/exchange/ExchangeDetailsRow.js
+++ src/components/exchange/ExchangeDetailsRow.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Animated, {
+  NewEasing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import styled from 'styled-components/primitives';
-import { ButtonPressAnimation } from '../animations';
 import { Centered, Row } from '../layout';
-import { Text } from '../text';
+import ExchangeDetailsButton from './ExchangeDetailsButton';
 import SlippageWarning from './SlippageWarning';
-import { colors, padding } from '@rainbow-me/styles';
+import { padding, position } from '@rainbow-me/styles';
 
-const ExchangeDetailsButtonLabel = styled(Text).attrs({
-  color: colors.white,
-  size: 'large',
-  weight: 'bold',
-})`
-  ${padding(9)};
+const defaultSlippageScale = 1.15;
+const timingConfig = {
+  duration: 200,
+  easing: NewEasing.bezier(0.76, 0, 0.24, 1),
+};
+
+const Container = styled(Centered)`
+  height: 60;
+  width: 100%;
 `;
 
 const ExchangeDetailsButtonRow = styled(Row).attrs({
@@ -19,22 +27,13 @@ const ExchangeDetailsButtonRow = styled(Row).attrs({
   justify: 'space-between',
 })`
   ${padding(10)};
+  ${position.cover};
   width: 100%;
 `;
 
-function ExchangeDetailsButton({ children, disabled, onPress, ...props }) {
-  return (
-    <ButtonPressAnimation
-      {...props}
-      disabled={disabled}
-      onPress={onPress}
-      opacity={disabled ? 0.4 : 1}
-      scaleTo={1.0666}
-    >
-      <ExchangeDetailsButtonLabel>{children}</ExchangeDetailsButtonLabel>
-    </ButtonPressAnimation>
-  );
-}
+const AnimatedExchangeDetailsButtonRow = Animated.createAnimatedComponent(
+  ExchangeDetailsButtonRow
+);
 
 export default function ExchangeDetailsRow({
   isSlippageWarningVisible,
@@ -44,23 +43,58 @@ export default function ExchangeDetailsRow({
   slippage,
   ...props
 }) {
+  const detailsRowOpacity = useSharedValue(1);
+  const slippageOpacity = useSharedValue(0);
+  const slippageScale = useSharedValue(defaultSlippageScale);
+
+  const detailsRowStyles = useAnimatedStyle(() => ({
+    opacity: detailsRowOpacity.value,
+  }));
+
+  const slippageStyles = useAnimatedStyle(() => ({
+    opacity: slippageOpacity.value,
+    transform: [{ scale: slippageScale.value }],
+  }));
+
+  useEffect(() => {
+    if (isSlippageWarningVisible) {
+      detailsRowOpacity.value = withTiming(0, timingConfig);
+      slippageOpacity.value = withTiming(1, timingConfig);
+      slippageScale.value = withTiming(1, timingConfig);
+    } else {
+      detailsRowOpacity.value = withTiming(1, timingConfig);
+      slippageOpacity.value = withTiming(0, timingConfig);
+      slippageScale.value = withTiming(defaultSlippageScale, timingConfig);
+    }
+  }, [
+    detailsRowOpacity,
+    isSlippageWarningVisible,
+    slippageOpacity,
+    slippageScale,
+  ]);
+
   return (
-    <Centered {...props}>
-      {isSlippageWarningVisible ? (
-        <SlippageWarning slippage={slippage} />
-      ) : (
-        <ExchangeDetailsButtonRow>
-          <ExchangeDetailsButton onPress={onFlipCurrencies}>
-            􀄬 Flip
-          </ExchangeDetailsButton>
-          <ExchangeDetailsButton
-            disabled={!showDetailsButton}
-            onPress={onPressViewDetails}
-          >
-            􀕹 View Details
-          </ExchangeDetailsButton>
-        </ExchangeDetailsButtonRow>
-      )}
-    </Centered>
+    <Container {...props}>
+      <SlippageWarning
+        onPress={onPressViewDetails}
+        pointerEvents={isSlippageWarningVisible ? 'auto' : 'none'}
+        slippage={slippage}
+        style={slippageStyles}
+      />
+      <AnimatedExchangeDetailsButtonRow
+        pointerEvents={isSlippageWarningVisible ? 'none' : 'auto'}
+        style={detailsRowStyles}
+      >
+        <ExchangeDetailsButton onPress={onFlipCurrencies}>
+          􀄬 Flip
+        </ExchangeDetailsButton>
+        <ExchangeDetailsButton
+          disabled={!showDetailsButton}
+          onPress={onPressViewDetails}
+        >
+          􀕹 View Details
+        </ExchangeDetailsButton>
+      </AnimatedExchangeDetailsButtonRow>
+    </Container>
   );
 }
--- src/components/exchange/index.js
+++ src/components/exchange/index.js
@@ -2,6 +2,7 @@ export { default as ConfirmExchangeButton } from './ConfirmExchangeButton';
 export { default as CurrencySelectionList } from './CurrencySelectionList';
 export { default as CurrencySelectModalHeader } from './CurrencySelectModalHeader';
 export { default as ExchangeAssetList } from './ExchangeAssetList';
+export { default as ExchangeDetailsButton } from './ExchangeDetailsButton';
 export { default as ExchangeDetailsRow } from './ExchangeDetailsRow';
 export { default as ExchangeInput } from './ExchangeInput';
 export { default as ExchangeInputField } from './ExchangeInputField';
