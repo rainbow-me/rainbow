4ee9163e19dec61bb940c0b07a6e15f4360d4b93
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2021-01-20 22:39:13 +0000

    feat: add anisotropic transform

---
+++ metro.transform.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-commonjs */
+const { merge } = require('lodash');
+const { transform } = require('metro-plugin-anisotropic-transform');
+
+module.exports.transform = function applyRainbowTransform({
+  src,
+  filename,
+  options,
+}) {
+  const opts = merge(options, {
+    customTransformOptions: {
+      'metro-plugin-anisotropic-transform': {
+        globalScopeFilter: {
+          //'@react-native-community/clipboard': {},
+          'react-native-keychain': {},
+        },
+      },
+    },
+  });
+  return transform({ filename, options: opts, src });
+};
