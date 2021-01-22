6004b0b97f525d8a98233f7e402c736e6b3651e0
jinchung <jin.chung17@gmail.com>
2021-01-22 05:02:43 -0700

    Uniswap pool quote and deposit handler

--- src/handlers/uniswapLiquidity.ts
+++ src/handlers/uniswapLiquidity.ts
@@ -1,20 +1,73 @@
 import { arrayify } from '@ethersproject/bytes';
 import { Contract } from '@ethersproject/contracts';
+import { ChainId, Pair, Token, WETH } from '@uniswap/sdk';
 import { fill, join } from 'lodash';
 import { addHexPrefix, web3Provider } from './web3';
+import { getQuote } from './zeroEx';
 import { ZeroExPayload } from '@rainbow-me/entities';
-import { ZAP_IN_ABI, ZapInAddress, ZERO_ADDRESS } from '@rainbow-me/references';
+import {
+  ETH_ADDRESS,
+  WETH_ADDRESS,
+  ZAP_IN_ABI,
+  ZapInAddress,
+  ZERO_ADDRESS,
+} from '@rainbow-me/references';
 import { ethereumUtils } from '@rainbow-me/utils';
 import logger from 'logger';
 
-export const depositToZap = async (
+const determineBuyToken = (
+  tokenA: Token,
+  tokenB: Token,
+  chainId: ChainId
+): string => {
+  if (tokenA.equals(WETH[chainId]) || tokenB.equals(WETH[chainId]))
+    return WETH_ADDRESS;
+  return tokenB.address;
+};
+
+export const depositToPool = async (
+  fromTokenAddress: string,
+  tokenA: Token,
+  tokenB: Token,
+  chainId: ChainId,
+  fromAmount: string,
+  network: string
+): Promise<string | null> => {
+  const buyToken = determineBuyToken(tokenA, tokenB, chainId);
+
+  const firstSwapDetails = await getQuote(
+    network,
+    fromTokenAddress,
+    buyToken,
+    fromAmount
+  );
+
+  if (!firstSwapDetails) return null;
+
+  const pairAddress = Pair.getAddress(tokenA, tokenB);
+  const minPoolTokens = '1';
+  const lpPurchased = await executeDepositZap(
+    fromTokenAddress,
+    fromAmount,
+    pairAddress,
+    firstSwapDetails,
+    minPoolTokens
+  );
+  return lpPurchased;
+};
+
+export const executeDepositZap = async (
+  fromTokenAddress: string,
+  fromAmount: string,
   pairAddress: string,
   firstSwapDetails: ZeroExPayload,
-  minPoolTokens: string,
-  ethValue: string,
-  fromAddress = ZERO_ADDRESS,
-  fromAmount = '0'
+  minPoolTokens: string
 ) => {
+  const fromAddress =
+    fromTokenAddress === ETH_ADDRESS ? ZERO_ADDRESS : fromTokenAddress;
+  const sellTokenAmount = fromTokenAddress === ETH_ADDRESS ? '0' : fromAmount;
+  const ethValue = fromTokenAddress === ETH_ADDRESS ? fromAmount : '0';
+
   const { swapTarget, allowanceTarget, swapPayload } = firstSwapDetails;
   const rawSwapPayload = ethereumUtils.removeHexPrefix(swapPayload);
 
@@ -27,7 +80,10 @@ export const depositToZap = async (
 
   const rawSwapData = `${offset}${dataLength}${rawSwapPayload}`;
   const remainingPaddingLength = 64 - (rawSwapData.length % 64);
-  const remainingPadding = join(fill(Array(remainingPaddingLength), '0'), '');
+  const remainingPadding =
+    remainingPaddingLength === 64
+      ? join(fill(Array(remainingPaddingLength), '0'), '')
+      : '';
   const swapData = `${rawSwapData}${remainingPadding}`;
   const swapDataBytes = arrayify(addHexPrefix(swapData));
 
@@ -36,7 +92,7 @@ export const depositToZap = async (
     const lpTokensBought = await zapInContract.callStatic.ZapIn(
       fromAddress,
       pairAddress,
-      fromAmount,
+      sellTokenAmount,
       minPoolTokens,
       allowanceTarget,
       swapTarget,
