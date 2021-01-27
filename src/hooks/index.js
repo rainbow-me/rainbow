7346d582d161f4f5f780bc224758a9733b158551
Jin <jin.chung17@gmail.com>
2021-01-27 16:19:19 -0700

    Move swap input values to redux (#1636)

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -63,6 +63,7 @@ export { default as useShowcaseTokens } from './useShowcaseTokens';
 export { default as useSwapDetails } from './useSwapDetails';
 export { default as useSwapInputRefs } from './useSwapInputRefs';
 export { default as useSwapInputs } from './useSwapInputs';
+export { default as useSwapInputValues } from './useSwapInputValues';
 export { default as useSwapInputOutputTokens } from './useSwapInputOutputTokens';
 export { default as useTimeout } from './useTimeout';
 export { default as useTopMovers } from './useTopMovers';
