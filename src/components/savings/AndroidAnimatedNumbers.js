6019cbcb5f0bd4d5740e5b9c3582712889ed39cb
osdnk <micosa97@gmail.com>
2021-01-28 14:52:02 +0200

    darkMode in savings

--- src/components/savings/AndroidAnimatedNumbers.js
+++ src/components/savings/AndroidAnimatedNumbers.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { findNodeHandle, NativeModules, TextInput } from 'react-native';
 import styled from 'styled-components';
+import { useTheme } from '../../context/ThemeContext';
 import { isSymbolStablecoin } from '../../helpers/savings';
 import { fonts, fontWithWidth } from '@rainbow-me/styles';
 
@@ -34,15 +35,18 @@ function formatter(symbol, val) {
 
 export default function AndroidText({ style, animationConfig }) {
   const isStable = isSymbolStablecoin(animationConfig.symbol);
+  const { isDarkMode } = useTheme();
+
   const ref = useRef();
   useEffect(() => {
     const screen = findNodeHandle(ref.current.getNativeRef());
     NativeModules.RNTextAnimator.animate(screen, {
       ...animationConfig,
+      darkMode: isDarkMode,
       isStable,
     });
     return () => NativeModules.RNTextAnimator.stop(screen);
-  }, [animationConfig, isStable]);
+  }, [animationConfig, isStable, isDarkMode]);
 
   return (
     <Row style={style}>
