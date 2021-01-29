7d96c9d406f3d73792a08e1d756dd51c3cd97166
osdnk <micosa97@gmail.com>
2021-01-29 14:47:44 +0200

    ProfileMasthead.js

--- src/components/profile/ProfileMasthead.js
+++ src/components/profile/ProfileMasthead.js
@@ -1,6 +1,5 @@
 import Clipboard from '@react-native-community/clipboard';
 import analytics from '@segment/analytics-react-native';
-import { find } from 'lodash';
 import React, { useCallback, useRef } from 'react';
 import ImagePicker from 'react-native-image-crop-picker';
 import { useDispatch } from 'react-redux';
@@ -108,14 +107,19 @@ export default function ProfileMasthead({
   const isAvatarImagePickerEnabled = true;
 
   const onRemovePhoto = useCallback(async () => {
-    const newWallets = { ...wallets };
-    const newWallet = newWallets[selectedWallet.id];
-    const account = find(newWallet.addresses, ['address', accountAddress]);
+    const newWallets = {
+      ...wallets,
+      [selectedWallet.id]: {
+        ...wallets[selectedWallet.id],
+        addresses: wallets[selectedWallet.id].addresses.map(account =>
+          account.address === accountAddress
+            ? { ...account, image: null }
+            : account
+        ),
+      },
+    };
 
-    account.image = null;
-    newWallet.addresses[account.index] = account;
-
-    dispatch(walletsSetSelected(newWallet));
+    dispatch(walletsSetSelected(newWallets[selectedWallet.id]));
     await dispatch(walletsUpdate(newWallets));
   }, [dispatch, selectedWallet, accountAddress, wallets]);
 
@@ -126,15 +130,22 @@ export default function ProfileMasthead({
         if (isAvatarImagePickerEnabled) {
           const processPhoto = image => {
             const stringIndex = image?.path.indexOf('/tmp');
-            const newWallets = { ...wallets };
-            const walletId = selectedWallet.id;
-            newWallets[walletId].addresses.some((account, index) => {
-              newWallets[walletId].addresses[index].image = ios
-                ? `~${image?.path.slice(stringIndex)}`
-                : image?.path;
-              dispatch(walletsSetSelected(newWallets[walletId]));
-              return true;
-            });
+            const newWallets = {
+              ...wallets,
+              [selectedWallet.id]: {
+                ...wallets[selectedWallet.id],
+                addresses: wallets[selectedWallet.id].addresses.map(account =>
+                  account.address === accountAddress
+                    ? {
+                        ...account,
+                        image: `~${image?.path.slice(stringIndex)}`,
+                      }
+                    : account
+                ),
+              },
+            };
+
+            dispatch(walletsSetSelected(newWallets[selectedWallet.id]));
             dispatch(walletsUpdate(newWallets));
           };
 
