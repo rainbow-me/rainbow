9aa05defb49c55b0a0b528cb49f142c0f0ecda04
Bruno Barbieri <brunobar79@gmail.com>
2021-02-08 12:20:48 -0500

    uniswap pools

--- src/components/discover-sheet/UniswapPoolsSection.js
+++ src/components/discover-sheet/UniswapPoolsSection.js
@@ -1,37 +1,260 @@
-import React from 'react';
-import { UniswapInvestmentRow } from '../investment-cards';
-import { Column } from '../layout';
+/* eslint-disable sort-keys */
+import React, { useCallback, useRef, useState } from 'react';
+import { FlatList } from 'react-native';
+import styled from 'styled-components';
+import { ButtonPressAnimation } from '../animations';
+import UniswapLogo from '../icons/UniswapLogo';
+import { UniswapPoolListRow } from '../investment-cards';
+import { Column, Row } from '../layout';
 import { Text } from '../text';
+import EdgeFade from './EdgeFade';
 
-const sample = {
-  ethBalance: 0.0017,
-  nativeDisplay: '$0.714',
-  percentageOwned: '0.002791633932773133',
-  pricePerShare: '$1,195.02',
-  tokenAddress: '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359',
-  tokenBalance: 0.3457,
-  tokenName: 'Sai',
-  tokenSymbol: 'SAI',
-  totalBalanceAmount: '1.4281594544978260747632',
-  totalNativeDisplay: '$1.43',
-  uniBalance: 0.001195,
-  uniqueId: 'uniswap_0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359',
-  uniTotalSupply: 42.8096419,
-};
+const PoolsSample = [
+  {
+    name: 'Uniswap V2 DAI-ETH',
+    symbol: 'univ2DAIETH',
+    decimals: 18,
+    type: 'uniswap-v2',
+    icon_url:
+      'https://token-icons.s3.amazonaws.com/0xa478c2975ab1ea89e8196811f51a7b7ade33eb11.png',
+    price: {
+      value: 106.36118385853452,
+      relative_change_24h: 2.390657195442647,
+      changed_at: 0,
+    },
+    is_displayable: true,
+    is_verified: true,
+    address: '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11',
+    uniqueId: '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11',
+    balance: {
+      amount: '0.178987293103995993',
+      display: '0.179 univ2DAIETH',
+    },
+    native: {
+      balance: {
+        amount: '19.03730039017552561387822893217836',
+        display: '$19.04',
+      },
+      change: '2.39%',
+      price: {
+        amount: 106.36118385853452,
+        display: '$106.36',
+      },
+    },
+    tokens: [
+      {
+        balance: '9.48727670028411709413',
+        address: '0x6b175474e89094c44da98b954eedeac495271d0f',
+        decimals: 18,
+        name: 'Dai',
+        symbol: 'DAI',
+        uniqueId: '0x6b175474e89094c44da98b954eedeac495271d0f',
+        color: '#F0B340',
+        isRainbowCurated: true,
+        isVerified: true,
+        value: '9.487',
+      },
+      {
+        balance: '0.00558983898186401602',
+        address: 'eth',
+        decimals: 18,
+        name: 'Ethereum',
+        symbol: 'ETH',
+        isRainbowCurated: true,
+        isVerified: true,
+        uniqueId: 'eth',
+        value: '0.00559',
+      },
+    ],
+    totalSupply: '1434286.784696692925766469',
+    percentageOwned: '0.000012479184429064',
+    pricePerShare: '$106.36',
+    tokenNames: 'DAI-ETH',
+    totalBalancePrice: '19.03730039017552561387822893217836',
+    totalNativeDisplay: '$19.04',
+    uniBalance: '0.179',
+  },
+  {
+    name: 'Uniswap AAVEETH Pool',
+    symbol: 'UNI-V2',
+    decimals: 18,
+    type: 'uniswap-v2',
+    icon_url: 'https://token-icons.s3.us-east-1.amazonaws.com/uniswap-v2.png',
+    price: {
+      value: 2078.434201464036,
+      relative_change_24h: 3.185022468962506,
+      changed_at: 0,
+    },
+    is_displayable: true,
+    is_verified: true,
+    address: '0xdfc14d2af169b0d36c4eff567ada9b2e0cae044f',
+    uniqueId: '0xdfc14d2af169b0d36c4eff567ada9b2e0cae044f',
+    balance: {
+      amount: '0.03055668468469635',
+      display: '0.0306 UNI-V2',
+    },
+    native: {
+      balance: {
+        amount: '63.5100585320251968742141054686',
+        display: '$63.51',
+      },
+      change: '3.19%',
+      price: {
+        amount: 2078.434201464036,
+        display: '$2,078.43',
+      },
+    },
+    tokens: [
+      {
+        balance: '0.06209617825576930376',
+        address: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
+        decimals: 18,
+        name: 'Aave',
+        symbol: 'AAVE',
+        uniqueId: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
+        color: '#7285B2',
+        isRainbowCurated: true,
+        isVerified: true,
+        value: '0.0621',
+      },
+      {
+        balance: '0.01862736200640838583',
+        address: 'eth',
+        decimals: 18,
+        name: 'Ethereum',
+        symbol: 'ETH',
+        isRainbowCurated: true,
+        isVerified: true,
+        uniqueId: 'eth',
+        value: '0.0186',
+      },
+    ],
+    totalSupply: '31724.402250801184727386',
+    percentageOwned: '0.000096319181818232',
+    pricePerShare: '$2,078.43',
+    tokenNames: 'AAVE-ETH',
+    totalBalancePrice: '63.5100585320251968742141054686',
+    totalNativeDisplay: '$63.51',
+    uniBalance: '0.0306',
+  },
+];
 
-const renderUniswapInvestmentRow = item => (
-  <UniswapInvestmentRow assetType="uniswap" item={item} key={item.uniqueId} />
+const PoolListButton = styled(ButtonPressAnimation).attrs({
+  scaleTo: 0.96,
+})`
+  margin-right: 16px;
+  ${({ selected, theme: { colors } }) =>
+    selected
+      ? `
+        background-color: ${colors.alpha(colors.appleBlue, 0.06)};
+        border-radius: 12px;
+        height: 30px;
+        padding-horizontal: 8px;
+        padding-top: ${ios ? 6 : 4}px;
+      `
+      : `
+        padding-top: ${ios ? 6 : 4}px;
+      `}
+`;
+
+const ListName = styled(Text)`
+  margin-left: 3px;
+  margin-top: ${ios ? -4.5 : 0}px;
+`;
+
+const renderUniswapPoolListRow = item => (
+  <UniswapPoolListRow assetType="uniswap" item={item} key={item.uniqueId} />
 );
 
+const listData = [
+  {
+    id: 'anualized_fees',
+    name: 'Anualized Fees',
+  },
+  {
+    id: 'liquidity',
+    name: 'Liquidity',
+  },
+  {
+    id: '30d_profit',
+    name: '30d profit',
+  },
+  { id: '24h_volume', name: '24h volume' },
+];
+
 export default function UniswapPools() {
+  const listRef = useRef(null);
+  const { colors } = useTheme();
+  const [selectedList, setSelectedList] = useState(listData[0].id);
+  const handleSwitchList = useCallback((id, index) => {
+    setSelectedList(id);
+    listRef.current?.scrollToIndex({
+      animated: true,
+      index,
+      viewPosition: 0.5,
+    });
+  }, []);
+
+  const renderItem = useCallback(
+    ({ item: list, index }) => (
+      <PoolListButton
+        key={`list-${list.id}`}
+        onPress={() => handleSwitchList(list.id, index)}
+        selected={selectedList === list.id}
+      >
+        <Row>
+          <ListName
+            color={
+              selectedList === list.id
+                ? colors.alpha(colors.appleBlue, 0.8)
+                : colors.alpha(colors.blueGreyDark, 0.5)
+            }
+            lineHeight="paragraphSmall"
+            size="lmedium"
+            weight="bold"
+          >
+            {list.name} {selectedList === list.id ? '􀄨' : ''}
+          </ListName>
+        </Row>
+      </PoolListButton>
+    ),
+    [colors, handleSwitchList, selectedList]
+  );
+
   return (
-    <Column paddingHorizontal={12}>
-      <Text size="larger" weight="bold">
-        🦄 Uniswap Pools
-      </Text>
-      {[sample, sample, sample, sample, sample, sample].map(
-        renderUniswapInvestmentRow
-      )}
+    <Column marginTop={20}>
+      <Row marginBottom={10} paddingHorizontal={22}>
+        <UniswapLogo
+          borderRadius={7.5}
+          height={22}
+          imageStyle={{ width: 15, height: 17.5 }}
+          marginRight={7}
+          marginTop={2}
+          width={22}
+        />
+        <Text marginLeft={10} size="larger" weight="bold">
+          Uniswap Pools
+        </Text>
+      </Row>
+      <Column>
+        <Column>
+          <FlatList
+            contentContainerStyle={{
+              paddingBottom: 19,
+              paddingHorizontal: 19,
+              paddingTop: 0,
+            }}
+            data={listData}
+            horizontal
+            keyExtractor={item => item.id}
+            ref={listRef}
+            renderItem={renderItem}
+            showsHorizontalScrollIndicator={false}
+          />
+          <EdgeFade />
+        </Column>
+      </Column>
+      {PoolsSample.map(renderUniswapPoolListRow)}
     </Column>
   );
 }
---
+++ src/components/investment-cards/UniswapPoolListRow.js
@@ -0,0 +1,113 @@
+import React, { Fragment, useCallback } from 'react';
+import { View } from 'react-native';
+import styled from 'styled-components';
+import { useTheme } from '../../context/ThemeContext';
+import { convertAmountToPercentageDisplay } from '../../helpers/utilities';
+import { ButtonPressAnimation } from '../animations';
+import { BottomRowText, CoinRow } from '../coin-row';
+import BalanceText from '../coin-row/BalanceText';
+import CoinName from '../coin-row/CoinName';
+import { initialLiquidityPoolExpandedStateSheetHeight } from '../expanded-state/LiquidityPoolExpandedState';
+import { FlexItem, Row } from '../layout';
+import { useNavigation } from '@rainbow-me/navigation';
+import Routes from '@rainbow-me/routes';
+
+const formatPercentageString = percentString =>
+  percentString ? percentString.toString().split('-').join('- ') : '-';
+
+const PercentageText = styled(BottomRowText).attrs({
+  align: 'right',
+})`
+  color: ${({ isPositive, theme: { colors } }) =>
+    isPositive ? colors.green : colors.alpha(colors.blueGreyDark, 0.5)};
+`;
+
+const Content = styled(ButtonPressAnimation)`
+  top: 0;
+`;
+
+const BottomRowContainer = ios
+  ? Fragment
+  : styled(Row).attrs({ marginBottom: 10, marginTop: -10 })``;
+
+const TopRowContainer = ios
+  ? Fragment
+  : styled(Row).attrs({
+      align: 'flex-start',
+      justify: 'flex-start',
+      marginTop: 0,
+    })``;
+
+const PriceContainer = ios
+  ? View
+  : styled(View)`
+      margin-top: -3;
+      margin-bottom: 3;
+    `;
+
+const BottomRow = ({ price, type, uniBalance }) => {
+  const relativeChange = price?.relative_change_24h;
+  const percentageChangeDisplay = relativeChange
+    ? formatPercentageString(convertAmountToPercentageDisplay(relativeChange))
+    : '-';
+  const isPositive =
+    relativeChange && percentageChangeDisplay.charAt(0) !== '-';
+
+  const tokenType = type === 'uniswap' ? 'UNI-V1' : 'UNI-V2';
+  const balanceLabel = `${uniBalance} ${tokenType}`;
+
+  return (
+    <BottomRowContainer>
+      <FlexItem flex={1}>
+        <BottomRowText>{balanceLabel}</BottomRowText>
+      </FlexItem>
+      <View>
+        <PercentageText isPositive={isPositive}>
+          {percentageChangeDisplay}
+        </PercentageText>
+      </View>
+    </BottomRowContainer>
+  );
+};
+
+const TopRow = ({ tokenNames, totalNativeDisplay }) => {
+  const { colors } = useTheme();
+  return (
+    <TopRowContainer>
+      <FlexItem flex={1}>
+        <CoinName color={colors.dark}>{tokenNames}</CoinName>
+      </FlexItem>
+      <PriceContainer>
+        <BalanceText color={colors.blueGreyLight} numberOfLines={1}>
+          {totalNativeDisplay}
+        </BalanceText>
+      </PriceContainer>
+    </TopRowContainer>
+  );
+};
+
+export default function UniswapPoolListRow({ assetType, item, ...props }) {
+  const { navigate } = useNavigation();
+
+  const handleOpenExpandedState = useCallback(() => {
+    navigate(Routes.EXPANDED_ASSET_SHEET, {
+      asset: item,
+      cornerRadius: 10,
+      longFormHeight: initialLiquidityPoolExpandedStateSheetHeight,
+      type: assetType,
+    });
+  }, [assetType, item, navigate]);
+
+  return (
+    <Content onPress={handleOpenExpandedState} scaleTo={0.96}>
+      <CoinRow
+        bottomRowRender={BottomRow}
+        isPool
+        onPress={handleOpenExpandedState}
+        topRowRender={TopRow}
+        {...item}
+        {...props}
+      />
+    </Content>
+  );
+}
