392370d814a4be4b4b4d26e841313097b2f78364
osdnk <micosa97@gmail.com>
2021-02-08 17:31:44 +0200

    remove unwanted file

--- x.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DEBUGFILE=src/config/debug.js
-if test -f "$DEBUGFILE"; then
-    echo "$DEBUGFILE exists."
-else
-    echo "$DEBUGFILE does not exist. You use default debug settings."
-    cp src/config/defaultDebug.js $FILE
-fi
