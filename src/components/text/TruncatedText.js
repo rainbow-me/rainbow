eafbca9d17eda4a0e1b459efa6d6b118d280cbfb
Michael Demarais <m.demarais@gmail.com>
2021-02-09 23:13:36 -0500

    fix eslint

--- src/components/text/TruncatedText.js
+++ src/components/text/TruncatedText.js
@@ -2,7 +2,13 @@ import styled from 'styled-components';
 import Text from './Text';
 
 const TruncatedText = styled(Text).attrs(
-  ({ ellipsizeMode = 'tail', numberOfLines = 1 }) => ({
+  ({
+    ellipsizeMode = 'tail',
+    numberOfLines = 1,
+    color,
+    theme: { colors },
+  }) => ({
+    color: color ? color : colors.dark,
     ellipsizeMode,
     numberOfLines,
   })
