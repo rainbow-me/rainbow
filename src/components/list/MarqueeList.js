2974cc9f076f5d378eb5275581b56f7055bd1d50
osdnk <micosa97@gmail.com>
2021-02-20 16:04:29 +0200

    Make MarqueeList safe

--- src/components/list/MarqueeList.js
+++ src/components/list/MarqueeList.js
@@ -43,7 +43,7 @@ const SingleElement = ({
         translateX:
           ((((transX.value + offset + width) % sumWidth) + sumWidth) %
             sumWidth) -
-          width,
+            width || 0,
       },
     ],
   }));
