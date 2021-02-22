cb2a5494e04e5ab73a0f8a42afc36fd723ac7103
Michael Demarais <m.demarais@gmail.com>
2021-02-22 14:40:12 -0500

    fix broken BottomRowTextt styles after darkmode

--- src/components/coin-row/BottomRowText.js
+++ src/components/coin-row/BottomRowText.js
@@ -4,7 +4,7 @@ import { TruncatedText } from '../text';
 const BottomRowText = styled(TruncatedText).attrs(
   ({ align = 'left', color, theme: { colors } }) => ({
     align,
-    color: color ? color : colors.alpha(colors.blueGreyDark, 0.5),
+    color: color ?? colors.alpha(colors.blueGreyDark, 0.5),
     size: 'smedium',
   })
 )``;
