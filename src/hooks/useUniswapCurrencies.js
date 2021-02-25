56192e07dc1fd930a2437bcd8b1e8f037a00b959
jinchung <jin.chung17@gmail.com>
2021-02-25 00:20:10 -0700

    Cleanup

--- src/hooks/useUniswapCurrencies.js
+++ src/hooks/useUniswapCurrencies.js
@@ -251,7 +251,6 @@ export default function useUniswapCurrencies({
   ]);
 
   return {
-    defaultInputAddress,
     flipCurrencies,
     navigateToSelectInputCurrency,
     navigateToSelectOutputCurrency,
