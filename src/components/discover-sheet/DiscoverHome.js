b5ea913804749053dbb413fac6aeaf7cf0037192
Bruno Barbieri <brunobar79@gmail.com>
2021-02-26 15:50:03 -0500

    prevent android crash new user flow

--- src/components/discover-sheet/DiscoverHome.js
+++ src/components/discover-sheet/DiscoverHome.js
@@ -5,15 +5,17 @@ import PulseIndex from './PulseIndexSection';
 // import Strategies from './StrategiesSection';
 import TopMoversSection from './TopMoversSection';
 import UniswapPools from './UniswapPoolsSection';
+import { useAccountSettings } from '@rainbow-me/hooks';
 
 export default function DiscoverHome() {
+  const { accountAddress } = useAccountSettings();
   return (
     <React.Fragment>
       <TopMoversSection />
       <PulseIndex />
       <Lists />
       {/* <Strategies /> */}
-      <UniswapPools />
+      {accountAddress ? <UniswapPools /> : null}
       <BottomSpacer />
     </React.Fragment>
   );
