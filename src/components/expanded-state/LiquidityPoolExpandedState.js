1b84ae31dc00a33a26b25937b08e5c9444fe44b5
Skylar Barrera <skylar.barrera@gmail.com>
2021-02-26 11:47:46 -0700

    Discover - fix android LP charts & withdraw button textColor (#1781)
    * withdraw button fix
    
    * fix LP charts on android
    
    * remove import
    
    * poolValueText android

--- src/components/expanded-state/LiquidityPoolExpandedState.js
+++ src/components/expanded-state/LiquidityPoolExpandedState.js
@@ -17,7 +17,7 @@ import {
 import { Chart } from '../value-chart';
 import { ChartPathProvider } from '@rainbow-me/animated-charts';
 import { toChecksumAddress } from '@rainbow-me/handlers/web3';
-import { useChartThrottledPoints, useDimensions } from '@rainbow-me/hooks';
+import { useChartThrottledPoints } from '@rainbow-me/hooks';
 import { magicMemo } from '@rainbow-me/utils';
 
 const heightWithoutChart = 373 + (android ? 20 - getSoftMenuBarHeight() : -153);
@@ -62,15 +62,12 @@ const LiquidityPoolExpandedState = ({ asset }) => {
   const tokenAddresses = useMemo(() => {
     return tokens.map(token => formatTokenAddress(token.address));
   }, [tokens]);
-  const { height: screenHeight } = useDimensions();
 
   return (
     <SlackSheet
       additionalTopPadding={android}
       contentHeight={liquidityPoolExpandedStateSheetHeight}
-      {...(ios
-        ? { height: '100%' }
-        : { additionalTopPadding: true, contentHeight: screenHeight - 80 })}
+      {...(ios && { height: '100%' })}
       scrollEnabled
     >
       <ChartPathProvider data={throttledData}>
