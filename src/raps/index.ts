2967c181e906e0a01994977b39ea76b83470b749
jinchung <jin.chung17@gmail.com>
2021-03-06 16:55:14 -0700

    Cleanup raps index

--- src/raps/index.ts
+++ src/raps/index.ts
@@ -1,14 +1,1 @@
-export {
-  createSwapAndDepositCompoundRap,
-  estimateSwapAndDepositCompound,
-} from './swapAndDepositCompound';
-export { createUnlockAndSwapRap, estimateUnlockAndSwap } from './unlockAndSwap';
-export {
-  createDepositUniswapRap,
-  estimateDepositUniswap,
-} from './unlockAndDepositUniswap';
-export {
-  estimateWithdrawFromCompound,
-  createWithdrawFromCompoundRap,
-} from './withdrawFromCompound';
 export { executeRap, getRapEstimationByType } from './common';
