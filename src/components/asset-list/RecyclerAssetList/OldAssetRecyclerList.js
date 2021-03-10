eb764f2e458ded0b6f23d61d8d1e6879cafaa802
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2021-03-10 15:52:36 +0000

    chore: initial componentDidUpdate extraction

--- src/components/asset-list/RecyclerAssetList/OldAssetRecyclerList.js
+++ src/components/asset-list/RecyclerAssetList/OldAssetRecyclerList.js
@@ -18,6 +18,132 @@ const StyledRecyclerListView = styled(RecyclerListView)`
   min-height: 1;
 `;
 
+const shouldComponentDidUpdate = ({
+  nextSections,
+  lastSections,
+  nextOpenFamilyTabs,
+  lastOpenFamilyTabs,
+  dataProvider,
+  scrollToOffset,
+  paddingBottom,
+  nextIsCoinListEdited,
+  lastIsCoinListEdited,
+}) => {
+  let collectibles = {};
+  let prevCollectibles = {};
+
+  nextSections.forEach(section => {
+    if (section.collectibles) {
+      collectibles = section;
+    }
+  });
+
+  lastSections.forEach(section => {
+    if (section.collectibles) {
+      prevCollectibles = section;
+    }
+  });
+
+  const bottomHorizonOfScreen =
+    ((RecyclerAssetListSharedState.rlv &&
+      RecyclerAssetListSharedState.rlv.getCurrentScrollOffset()) ||
+      0) + RecyclerAssetListSharedState.globalDeviceDimensions;
+
+  // Auto-scroll to opened family logic 👇
+  if (nextOpenFamilyTabs !== lastOpenFamilyTabs && collectibles.data) {
+    let i = 0;
+    while (i < collectibles.data.length) {
+      if (
+        nextOpenFamilyTabs[collectibles.data[i].familyName] === true &&
+        !lastOpenFamilyTabs[collectibles.data[i].familyName]
+      ) {
+        const safeIndex = i;
+        const safeCollectibles = collectibles;
+        const familyIndex = findIndex(
+          dataProvider.getAllData(),
+          function (data) {
+            return (
+              data.item?.familyName ===
+              safeCollectibles.data[safeIndex].familyName
+            );
+          }
+        );
+
+        const focusedFamilyItem = dataProvider.getAllData()[familyIndex].item;
+        const focusedFamilyHeight = ViewTypes.UNIQUE_TOKEN_ROW.calculateHeight({
+          amountOfRows: Math.ceil(Number(focusedFamilyItem.childrenAmount) / 2),
+          isFirst: false,
+          isHeader: true,
+          isOpen: true,
+        });
+
+        const startOfDesiredComponent =
+          RecyclerAssetListSharedState.rlv.getLayout(familyIndex).y -
+          AssetListHeaderHeight;
+
+        if (
+          focusedFamilyHeight <
+          RecyclerAssetListSharedState.globalDeviceDimensions
+        ) {
+          const endOfDesiredComponent =
+            startOfDesiredComponent +
+            focusedFamilyHeight +
+            AssetListHeaderHeight;
+
+          if (endOfDesiredComponent > bottomHorizonOfScreen) {
+            scrollToOffset(
+              endOfDesiredComponent -
+                RecyclerAssetListSharedState.globalDeviceDimensions,
+              true
+            );
+          }
+        } else {
+          scrollToOffset(startOfDesiredComponent, true);
+        }
+
+        break;
+      }
+      i++;
+    }
+  }
+
+  // Auto-scroll to end of the list if something was closed/disappeared 👇
+  if (
+    RecyclerAssetListSharedState.rlv &&
+    RecyclerAssetListSharedState.rlv.getContentDimension().height <
+      bottomHorizonOfScreen +
+        ViewTypes.FOOTER.calculateHeight({
+          paddingBottom: paddingBottom || 0,
+        }) &&
+    RecyclerAssetListSharedState.rlv.getCurrentScrollOffset() > 0 &&
+    (!nextIsCoinListEdited || (!lastIsCoinListEdited && nextIsCoinListEdited))
+  ) {
+    requestAnimationFrame(() => {
+      RecyclerAssetListSharedState.rlv &&
+        RecyclerAssetListSharedState.rlv.scrollToEnd({ animated: true });
+    });
+  }
+
+  // Auto-scroll to showcase family if something was added/removed 👇
+  if (
+    collectibles.data &&
+    prevCollectibles.data &&
+    collectibles.data[0]?.familyName === 'Showcase' &&
+    (collectibles.data[0]?.childrenAmount !==
+      prevCollectibles.data[0]?.childrenAmount ||
+      prevCollectibles.data[0]?.familyName !== 'Showcase')
+  ) {
+    const familyIndex = findIndex(dataProvider.getAllData(), function (data) {
+      return data.item?.familyName === 'Showcase';
+    });
+
+    const startOfDesiredComponent =
+      RecyclerAssetListSharedState.rlv.getLayout(familyIndex).y -
+      AssetListHeaderHeight;
+    scrollToOffset(startOfDesiredComponent, true);
+  }
+};
+
 export default class RecyclerAssetList extends Component {
   static propTypes = {
     externalScrollView: PropTypes.any,
@@ -52,180 +178,71 @@ export default class RecyclerAssetList extends Component {
   };
 
   componentDidUpdate(prevProps) {
-    const { openFamilyTabs, sections } = this.props;
-
-    //if (nativeCurrency !== prevProps.nativeCurrency) {
-    //  requestAnimationFrame(
-    //    () =>
-    //      RecyclerAssetListSharedState.rlv &&
-    //      RecyclerAssetListSharedState.rlv.scrollToTop(false)
-    //  );
-    //}
-
-    let collectibles = {};
-    let prevCollectibles = {};
-
-    sections.forEach(section => {
-      if (section.collectibles) {
-        collectibles = section;
-      }
+    const {
+      isCoinListEdited: nextIsCoinListEdited,
+      sections: nextSections,
+      openFamilyTabs: nextOpenFamilyTabs,
+      dataProvider,
+      scrollToOffset,
+      paddingBottom,
+    } = this.props;
+    const {
+      isCoinListEdited: lastIsCoinListEdited,
+      sections: lastSections,
+      openFamilyTabs: lastOpenFamilyTabs,
+    } = prevProps;
+    return shouldComponentDidUpdate({
+      dataProvider,
+      lastIsCoinListEdited,
+      lastOpenFamilyTabs,
+      lastSections,
+      nextIsCoinListEdited,
+      nextOpenFamilyTabs,
+      nextSections,
+      paddingBottom,
+      scrollToOffset,
     });
-
-    prevProps.sections.forEach(section => {
-      if (section.collectibles) {
-        prevCollectibles = section;
-      }
-    });
-
-    const bottomHorizonOfScreen =
-      ((RecyclerAssetListSharedState.rlv &&
-        RecyclerAssetListSharedState.rlv.getCurrentScrollOffset()) ||
-        0) + RecyclerAssetListSharedState.globalDeviceDimensions;
-
-    // Auto-scroll to opened family logic 👇
-    if (openFamilyTabs !== prevProps.openFamilyTabs && collectibles.data) {
-      let i = 0;
-      while (i < collectibles.data.length) {
-        if (
-          openFamilyTabs[collectibles.data[i].familyName] === true &&
-          !prevProps.openFamilyTabs[collectibles.data[i].familyName]
-        ) {
-          const safeIndex = i;
-          const safeCollectibles = collectibles;
-          const familyIndex = findIndex(
-            this.props.dataProvider.getAllData(),
-            function (data) {
-              return (
-                data.item?.familyName ===
-                safeCollectibles.data[safeIndex].familyName
-              );
-            }
-          );
-
-          const focusedFamilyItem = this.props.dataProvider.getAllData()[
-            familyIndex
-          ].item;
-          const focusedFamilyHeight = ViewTypes.UNIQUE_TOKEN_ROW.calculateHeight(
-            {
-              amountOfRows: Math.ceil(
-                Number(focusedFamilyItem.childrenAmount) / 2
-              ),
-              isFirst: false,
-              isHeader: true,
-              isOpen: true,
-            }
-          );
-
-          const startOfDesiredComponent =
-            RecyclerAssetListSharedState.rlv.getLayout(familyIndex).y -
-            AssetListHeaderHeight;
-
-          if (
-            focusedFamilyHeight <
-            RecyclerAssetListSharedState.globalDeviceDimensions
-          ) {
-            const endOfDesiredComponent =
-              startOfDesiredComponent +
-              focusedFamilyHeight +
-              AssetListHeaderHeight;
-
-            if (endOfDesiredComponent > bottomHorizonOfScreen) {
-              this.props.scrollToOffset(
-                endOfDesiredComponent -
-                  RecyclerAssetListSharedState.globalDeviceDimensions,
-                true
-              );
-            }
-          } else {
-            this.props.scrollToOffset(startOfDesiredComponent, true);
-          }
-
-          break;
-        }
-        i++;
-      }
-    }
-
-    // Auto-scroll to end of the list if something was closed/disappeared 👇
-    if (
-      RecyclerAssetListSharedState.rlv &&
-      RecyclerAssetListSharedState.rlv.getContentDimension().height <
-        bottomHorizonOfScreen +
-          ViewTypes.FOOTER.calculateHeight({
-            paddingBottom: this.props.paddingBottom || 0,
-          }) &&
-      RecyclerAssetListSharedState.rlv.getCurrentScrollOffset() > 0 &&
-      (!this.props.isCoinListEdited ||
-        (!prevProps.isCoinListEdited && this.props.isCoinListEdited))
-    ) {
-      requestAnimationFrame(() => {
-        RecyclerAssetListSharedState.rlv &&
-          RecyclerAssetListSharedState.rlv.scrollToEnd({ animated: true });
-      });
-    }
-
-    // Auto-scroll to showcase family if something was added/removed 👇
-    if (
-      collectibles.data &&
-      prevCollectibles.data &&
-      collectibles.data[0]?.familyName === 'Showcase' &&
-      (collectibles.data[0]?.childrenAmount !==
-        prevCollectibles.data[0]?.childrenAmount ||
-        prevCollectibles.data[0]?.familyName !== 'Showcase')
-    ) {
-      const familyIndex = findIndex(
-        this.props.dataProvider.getAllData(),
-        function (data) {
-          return data.item?.familyName === 'Showcase';
-        }
-      );
-
-      const startOfDesiredComponent =
-        RecyclerAssetListSharedState.rlv.getLayout(familyIndex).y -
-        AssetListHeaderHeight;
-      this.props.scrollToOffset(startOfDesiredComponent, true);
-    }
   }
 
-// TODO: Needs to be introduced.
-//  getStableId = index => {
-//    const { dataProvider } = this.state;
-//    const row = get(dataProvider.getAllData(), index);
-//
-//    if (row.item && row.item.familyName) {
-//      return `family_${row.item.familyName}_${row.item.familyId}`;
-//    }
-//
-//    if (row.isHeader && (!row.item || !row.item.familyName)) {
-//      return `header_${row.title}`;
-//    }
-//
-//    if (row.item && row.item.address) {
-//      return `balance_${row.item.address}`;
-//    }
-//
-//    if (row.item && row.item.uniqueId) {
-//      return `pool_${row.item.uniqueId}`;
-//    }
-//
-//    if (row.item && row.item.smallBalancesContainer) {
-//      return `smallBalancesContainer`;
-//    }
-//
-//    if (row.item && row.item.coinDivider) {
-//      return `coinDivider`;
-//    }
-//
-//    if (row.item && row.item.savingsContainer) {
-//      return `savingsContainer`;
-//    }
-//
-//    if (index === dataProvider.getAllData().length - 1) {
-//      return 'footer';
-//    }
-//
-//    return index;
-//  };
+  // TODO: Needs to be introduced.
+  //  getStableId = index => {
+  //    const { dataProvider } = this.state;
+  //    const row = get(dataProvider.getAllData(), index);
+  //
+  //    if (row.item && row.item.familyName) {
+  //      return `family_${row.item.familyName}_${row.item.familyId}`;
+  //    }
+  //
+  //    if (row.isHeader && (!row.item || !row.item.familyName)) {
+  //      return `header_${row.title}`;
+  //    }
+  //
+  //    if (row.item && row.item.address) {
+  //      return `balance_${row.item.address}`;
+  //    }
+  //
+  //    if (row.item && row.item.uniqueId) {
+  //      return `pool_${row.item.uniqueId}`;
+  //    }
+  //
+  //    if (row.item && row.item.smallBalancesContainer) {
+  //      return `smallBalancesContainer`;
+  //    }
+  //
+  //    if (row.item && row.item.coinDivider) {
+  //      return `coinDivider`;
+  //    }
+  //
+  //    if (row.item && row.item.savingsContainer) {
+  //      return `savingsContainer`;
+  //    }
+  //
+  //    if (index === dataProvider.getAllData().length - 1) {
+  //      return 'footer';
+  //    }
+  //
+  //    return index;
+  //  };
 
   handleListRef = ref => {
     RecyclerAssetListSharedState.rlv = ref;
