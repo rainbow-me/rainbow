b481a39d25522beda2e68dbf9301cfaf1e9456de
Bruno Barbieri <brunobar79@gmail.com>
2021-03-10 00:14:37 -0500

    add webview to metro transform

--- metro.transform.js
+++ metro.transform.js
@@ -15,6 +15,7 @@ module.exports.transform = function applyRainbowTransform({
           '@react-native-community/clipboard': {},
           'react-native-keychain': {},
           'react-native-video': {},
+          'react-native-webview': {},
         },
         madge: {
           tsConfig: require.resolve('./tsconfig.json'),
