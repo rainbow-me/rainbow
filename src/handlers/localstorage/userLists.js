2e76e0b67f61423f0ab9d09ff2205ba16cd5d640
Bruno Barbieri <brunobar79@gmail.com>
2021-03-15 17:07:02 -0400

    CR: clean userLists localstorage

--- src/handlers/localstorage/userLists.js
+++ src/handlers/localstorage/userLists.js
@@ -1,11 +1,9 @@
-import { DefaultTokenLists } from '../../references';
 import { getGlobal, saveGlobal } from './common';
 
 const USER_LISTS = 'userLists';
 const USER_LISTS_SELECTED_LIST = 'userListsSelectedList';
 
-export const getUserLists = network =>
-  getGlobal(USER_LISTS, DefaultTokenLists[network]);
+export const getUserLists = () => getGlobal(USER_LISTS, null);
 
 export const saveUserLists = lists => saveGlobal(USER_LISTS, lists);
 
