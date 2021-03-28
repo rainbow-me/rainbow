eec08c9927a1c0a7bc9e0612c0dcb4aa7cabb70c
osdnk <micosa97@gmail.com>
2021-03-28 23:39:06 +0300

    Revert "revert visual changes for LP pools"
    This reverts commit 600ed70f

--- src/components/expanded-state/LiquidityPoolExpandedState.js
+++ src/components/expanded-state/LiquidityPoolExpandedState.js
@@ -1,8 +1,13 @@
 import { useRoute } from '@react-navigation/core';
 import { map, toLower } from 'lodash';
-import React, { Fragment } from 'react';
+import React, { Fragment, useMemo } from 'react';
 import { getSoftMenuBarHeight } from 'react-native-extra-dimensions-android';
+import styled from 'styled-components';
 import { UniBalanceHeightDifference } from '../../hooks/charts/useChartThrottledPoints';
+import useNativeCurrencyToUSD from '../../hooks/useNativeCurrencyToUSD';
+import { useUsersPositions } from '../../hooks/useUsersPositions';
+import { renderPoolValue } from '../investment-cards/renderPoolValue';
+
 import {
   DepositActionButton,
   SheetActionButtonRow,
@@ -18,13 +23,18 @@ import {
 } from '../token-info';
 import { Chart } from '../value-chart';
 import { ChartPathProvider } from '@rainbow-me/animated-charts';
+import { useTheme } from '@rainbow-me/context';
 import { toChecksumAddress } from '@rainbow-me/handlers/web3';
 import chartTypes from '@rainbow-me/helpers/chartTypes';
-import { useChartThrottledPoints } from '@rainbow-me/hooks';
+import {
+  useAccountSettings,
+  useChartThrottledPoints,
+  usePoolDetails,
+} from '@rainbow-me/hooks';
 import { ETH_ADDRESS } from '@rainbow-me/references';
 import { magicMemo } from '@rainbow-me/utils';
 
-const heightWithoutChart = 392 + (android ? 20 - getSoftMenuBarHeight() : 0);
+const heightWithoutChart = 452 + (android ? 20 - getSoftMenuBarHeight() : 0);
 const heightWithChart = heightWithoutChart + 293;
 
 export const initialLiquidityPoolExpandedStateSheetHeight = heightWithChart;
@@ -36,12 +46,52 @@ const formatTokenAddress = address => {
   return toChecksumAddress(address);
 };
 
-const LiquidityPoolExpandedState = ({ asset }) => {
+const APYWrapper = styled.View`
+  padding-top: 5;
+  flex: 1;
+`;
+
+const Carousel = styled.ScrollView.attrs({
+  horizontal: true,
+})``;
+
+const LiquidityPoolExpandedState = () => {
   const { params } = useRoute();
+  const { asset } = params;
   const { tokenNames, tokens, totalNativeDisplay, type, uniBalance } = asset;
 
   const tokenType = type === 'uniswap' ? 'UNI-V1' : 'UNI-V2';
   const uniBalanceLabel = `${uniBalance} ${tokenType}`;
+  const { nativeCurrency } = useAccountSettings();
+
+  const positions = useUsersPositions();
+
+  const totalFeeEarned = useMemo(
+    () =>
+      nativeCurrency === 'USD'
+        ? positions
+            ?.find(({ pair: { id } }) => id === asset.address)
+            ?.fees.sum.toLocaleString('en-US', {
+              currency: 'USD',
+              style: 'currency',
+            })
+        : '',
+    [asset.address, nativeCurrency, positions]
+  );
+
+  useNativeCurrencyToUSD();
+
+  const details = usePoolDetails(asset.address);
+  const { annualized_fees: fee, oneDayVolumeUSD } = details || {};
+
+  const volume = useMemo(
+    () =>
+      oneDayVolumeUSD?.toLocaleString('en-US', {
+        currency: 'USD',
+        style: 'currency',
+      }),
+    [oneDayVolumeUSD]
+  );
 
   const {
     chart,
@@ -66,7 +116,7 @@ const LiquidityPoolExpandedState = ({ asset }) => {
     (uniBalance ? 0 : UniBalanceHeightDifference);
 
   const tokenAddresses = useMemo(() => {
-    return tokens.map(token => formatTokenAddress(token.address));
+    return tokens?.map(token => formatTokenAddress(token.address));
   }, [tokens]);
 
   const chartDataLabels = useMemo(() => {
@@ -77,6 +127,8 @@ const LiquidityPoolExpandedState = ({ asset }) => {
     }
     return initialChartDataLabels;
   }, [chartType, initialChartDataLabels, params?.asset?.profit30d]);
+
+  const { colors } = useTheme();
 
   return (
     <SlackSheet
@@ -126,20 +178,57 @@ const LiquidityPoolExpandedState = ({ asset }) => {
                 {totalNativeDisplay}
               </TokenInfoItem>
             </TokenInfoRow>
+            <Carousel>
+              <TokenInfoItem hidden={!fee} title="Annualized fees">
+                <APYWrapper>
+                  {renderPoolValue(
+                    'annualized_fees',
+                    Number(fee),
+                    nativeCurrency,
+                    colors
+                  )}
+                </APYWrapper>
+              </TokenInfoItem>
+              <TokenInfoItem
+                hidden={!totalFeeEarned}
+                title="Total fee earned"
+                weight="bold"
+              >
+                {totalFeeEarned} 🤑
+              </TokenInfoItem>
+              <TokenInfoItem
+                hidden={!totalFeeEarned}
+                title="Pool volume 24h"
+                weight="bold"
+              >
+                {volume}
+              </TokenInfoItem>
+              <TokenInfoItem
+                hidden={!totalFeeEarned}
+                title="30d profit"
+                weight="bold"
+              >
+                {volume}
+              </TokenInfoItem>
+            </Carousel>
           </TokenInfoSection>
           <SheetActionButtonRow>
-            <WithdrawActionButton
-              symbol={tokenNames}
-              token1Address={tokenAddresses[0]}
-              token2Address={tokenAddresses[1]}
-              weight="bold"
-            />
-            <DepositActionButton
-              symbol={tokenNames}
-              token1Address={tokenAddresses[0]}
-              token2Address={tokenAddresses[1]}
-              weight="bold"
-            />
+            {tokenAddresses?.length > 0 && (
+              <WithdrawActionButton
+                symbol={tokenNames}
+                token1Address={tokenAddresses[0]}
+                token2Address={tokenAddresses[1]}
+                weight="bold"
+              />
+            )}
+            {tokenAddresses?.length > 0 && (
+              <DepositActionButton
+                symbol={tokenNames}
+                token1Address={tokenAddresses[0]}
+                token2Address={tokenAddresses[1]}
+                weight="bold"
+              />
+            )}
           </SheetActionButtonRow>
         </Fragment>
       ) : (
