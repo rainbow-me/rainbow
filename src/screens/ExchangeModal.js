52ac6a417e84fe6a7186cebcf9497d4d5e1f3a09
Jin <jin.chung17@gmail.com>
2021-04-15 13:16:20 -0600

    Fix stuck on authorizing bug in swap (#1966)
    * Parse ethers errors in swap and present better error message
    
    This also fixes the stuck on authorizing on failures in swap.
    
    * Add sentry logging for Ethers error code as it often gets cut off
    
    * Switch default value

--- src/screens/ExchangeModal.js
+++ src/screens/ExchangeModal.js
@@ -7,7 +7,7 @@ import React, {
   useRef,
   useState,
 } from 'react';
-import { Keyboard } from 'react-native';
+import { Alert, Keyboard } from 'react-native';
 import { useAndroidBackHandler } from 'react-navigation-backhandler';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
@@ -305,10 +305,14 @@ export default function ExchangeModal({
           return;
         }
 
-        const callback = () => {
+        const callback = (success = false, errorMessage = null) => {
           setIsAuthorizing(false);
-          setParams({ focused: false });
-          navigate(Routes.PROFILE_SCREEN);
+          if (success) {
+            setParams({ focused: false });
+            navigate(Routes.PROFILE_SCREEN);
+          } else if (errorMessage) {
+            Alert.alert(errorMessage);
+          }
         };
         logger.log('[exchange - handle submit] rap');
         const swapParameters = {
