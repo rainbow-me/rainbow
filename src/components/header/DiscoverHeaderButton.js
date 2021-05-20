7d1e4020f091656039801fa785d06b406f40f6f2
osdnk <micosa97@gmail.com>
2021-05-20 15:31:14 +0300

    Fix DiscoverHeaderButton

--- src/components/header/DiscoverHeaderButton.js
+++ src/components/header/DiscoverHeaderButton.js
@@ -40,7 +40,7 @@ const DiscoverButtonContent = styled(RowWithMargins).attrs({
 })`
   align-items: center;
   justify-content: center;
-  ${padding(0, 10, 7.5)};
+  ${padding(ios ? 6 : 0, 10, 7.5)};
   height: 34;
   z-index: 2;
 `;
