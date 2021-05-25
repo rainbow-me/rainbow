fc88bda7dff997f4909bcad747a2a8953aa30193
osdnk <micosa97@gmail.com>
2021-05-25 16:17:26 +0300

    Revert SavingsSheet.js

--- src/screens/SavingsSheet.js
+++ src/screens/SavingsSheet.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
+import { useRoute } from '@react-navigation/native';
+import analytics from '@segment/analytics-react-native';
+import React, { Fragment, useCallback, useEffect, useMemo } from 'react';
+import { Alert, StatusBar } from 'react-native';
 import { getSoftMenuBarHeight } from 'react-native-extra-dimensions-android';
 import { useSafeArea } from 'react-native-safe-area-context';
 import styled from 'styled-components';
-import { Centered } from '../components/layout';
-import { SlackSheet } from '../components/sheet';
-import { useDimensions } from '@rainbow-me/hooks';
+import Divider from '../components/Divider';
+import { SavingsCoinRow } from '../components/coin-row';
+import {
+  FloatingEmojis,
+  FloatingEmojisTapHandler,
+} from '../components/floating-emojis';
+import { Centered, Column } from '../components/layout';
+import {
+  SavingsPredictionStepper,
+  SavingsSheetEmptyState,
+  SavingsSheetHeader,
+} from '../components/savings';
+import {
+  SheetActionButton,
+  SheetActionButtonRow,
+  SlackSheet,
+} from '../components/sheet';
+import { enableActionsOnReadOnlyWallet } from '@rainbow-me/config/debug';
+import { isSymbolStablecoin } from '@rainbow-me/helpers/savings';
+import { convertAmountToNativeDisplay } from '@rainbow-me/helpers/utilities';
+import {
+  useAccountSettings,
+  useDimensions,
+  useWallets,
+} from '@rainbow-me/hooks';
+import { useNavigation } from '@rainbow-me/navigation';
+import Routes from '@rainbow-me/routes';
 import { position } from '@rainbow-me/styles';
 
 export const SavingsSheetEmptyHeight = 313;
@@ -20,20 +46,178 @@ const Container = styled(Centered).attrs({ direction: 'column' })`
 `;
 
 const SavingsSheet = () => {
+  const { colors, isDarkMode } = useTheme();
   const { height: deviceHeight } = useDimensions();
+  const { navigate } = useNavigation();
+  const { params } = useRoute();
   const insets = useSafeArea();
+  const { isReadOnlyWallet } = useWallets();
+  const { nativeCurrency, nativeCurrencySymbol } = useAccountSettings();
+  const cTokenBalance = params['cTokenBalance'];
+  const isEmpty = params['isEmpty'];
+  const underlyingBalanceNativeValue = params['underlyingBalanceNativeValue'];
+  const underlying = params['underlying'];
+  const lifetimeSupplyInterestAccrued = params['lifetimeSupplyInterestAccrued'];
+  const lifetimeSupplyInterestAccruedNative =
+    params['lifetimeSupplyInterestAccruedNative'];
+  const supplyBalanceUnderlying = params['supplyBalanceUnderlying'];
+  const supplyRate = params['supplyRate'];
+
+  const balance = nativeCurrencySymbol + underlyingBalanceNativeValue;
+  const lifetimeAccruedInterest = convertAmountToNativeDisplay(
+    lifetimeSupplyInterestAccruedNative,
+    nativeCurrency
+  );
+
+  const savingsRowItem = useMemo(
+    () => ({
+      address: underlying.address,
+      lifetimeSupplyInterestAccrued,
+      name: underlying.name,
+      supplyBalanceUnderlying,
+      supplyRate,
+      symbol: underlying.symbol,
+    }),
+    [
+      lifetimeSupplyInterestAccrued,
+      supplyBalanceUnderlying,
+      supplyRate,
+      underlying.address,
+      underlying.name,
+      underlying.symbol,
+    ]
+  );
+
+  useEffect(() => {
+    return () => {
+      analytics.track('Closed Savings Sheet', {
+        category: 'savings',
+        empty: isEmpty,
+        label: underlying.symbol,
+      });
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onWithdraw = useCallback(() => {
+    if (!isReadOnlyWallet || enableActionsOnReadOnlyWallet) {
+      navigate(Routes.SAVINGS_WITHDRAW_MODAL, {
+        cTokenBalance,
+        defaultInputAsset: underlying,
+        supplyBalanceUnderlying,
+      });
+
+      analytics.track('Navigated to SavingsWithdrawModal', {
+        category: 'savings',
+        label: underlying.symbol,
+      });
+    } else {
+      Alert.alert(`You need to import the wallet in order to do this`);
+    }
+  }, [
+    cTokenBalance,
+    isReadOnlyWallet,
+    navigate,
+    supplyBalanceUnderlying,
+    underlying,
+  ]);
+
+  const onDeposit = useCallback(() => {
+    if (!isReadOnlyWallet || enableActionsOnReadOnlyWallet) {
+      navigate(Routes.SAVINGS_DEPOSIT_MODAL, {
+        params: {
+          params: {
+            defaultInputAsset: underlying,
+          },
+          screen: Routes.MAIN_EXCHANGE_SCREEN,
+        },
+        screen: Routes.MAIN_EXCHANGE_NAVIGATOR,
+      });
+
+      analytics.track('Navigated to SavingsDepositModal', {
+        category: 'savings',
+        empty: isEmpty,
+        label: underlying.symbol,
+      });
+    } else {
+      Alert.alert(`You need to import the wallet in order to do this`);
+    }
+  }, [isEmpty, isReadOnlyWallet, navigate, underlying]);
 
   return (
     <Container
       deviceHeight={deviceHeight}
-      height={SavingsSheetHeight}
+      height={isEmpty ? SavingsSheetEmptyHeight : SavingsSheetHeight}
       insets={insets}
     >
       <StatusBar barStyle="light-content" />
       <SlackSheet
         additionalTopPadding={android}
-        contentHeight={SavingsSheetHeight}
-      />
+        contentHeight={isEmpty ? SavingsSheetEmptyHeight : SavingsSheetHeight}
+      >
+        {isEmpty ? (
+          <SavingsSheetEmptyState
+            isReadOnlyWallet={isReadOnlyWallet}
+            supplyRate={supplyRate}
+            underlying={underlying}
+          />
+        ) : (
+          <Fragment>
+            <SavingsSheetHeader
+              balance={balance}
+              lifetimeAccruedInterest={lifetimeAccruedInterest}
+            />
+            <SheetActionButtonRow>
+              <SheetActionButton
+                color={isDarkMode ? colors.darkModeDark : colors.dark}
+                label="􀁏 Withdraw"
+                onPress={onWithdraw}
+                radiusAndroid={24}
+                weight="bold"
+              />
+              <SheetActionButton
+                color={colors.swapPurple}
+                label="􀁍 Deposit"
+                onPress={onDeposit}
+                radiusAndroid={24}
+                weight="bold"
+              />
+            </SheetActionButtonRow>
+            <Divider color={colors.rowDividerLight} zIndex={0} />
+            <FloatingEmojis
+              disableHorizontalMovement
+              distance={600}
+              duration={600}
+              emojis={['money_with_wings']}
+              opacityThreshold={0.86}
+              scaleTo={0.3}
+              size={40}
+              wiggleFactor={0}
+            >
+              {({ onNewEmoji }) => (
+                <FloatingEmojisTapHandler onNewEmoji={onNewEmoji}>
+                  <Column paddingBottom={9} paddingTop={4}>
+                    <SavingsCoinRow
+                      item={savingsRowItem}
+                      key={underlying.address}
+                    />
+                  </Column>
+                </FloatingEmojisTapHandler>
+              )}
+            </FloatingEmojis>
+            <Divider color={colors.rowDividerLight} zIndex={0} />
+            <SavingsPredictionStepper
+              asset={underlying}
+              balance={
+                isSymbolStablecoin(underlying.symbol)
+                  ? underlyingBalanceNativeValue
+                  : supplyBalanceUnderlying
+              }
+              interestRate={supplyRate}
+            />
+          </Fragment>
+        )}
+      </SlackSheet>
     </Container>
   );
 };
