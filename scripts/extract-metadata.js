065b737b2904fddaf7d22b3ee1d2dc6d49bb7fc1
Bruno Barbieri <brunobar79@gmail.com>
2021-05-26 16:23:37 -0400

    add offline metadata script

---
+++ scripts/extract-metadata.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-commonjs */
+const { readdirSync, writeFileSync } = require('fs');
+const sizeOf = require('image-size');
+const {
+  default: makeColorMoreChill,
+  isBlackOrWhite,
+} = require('make-color-more-chill');
+const Vibrant = require('node-vibrant');
+
+const start = Date.now();
+
+const args = process.argv.slice(2);
+const assets_dir = args[0];
+console.log('Extracting metadata...');
+
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+
+const tokenAddresses = getDirectories(assets_dir);
+//.slice(0, 1);
+let allItems = {};
+const init = async () => {
+  for (let i = 0; i < tokenAddresses.length; i++) {
+    const address = tokenAddresses[i];
+    // Promise
+    const imageUrl = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`;
+    let metadata = {};
+    try {
+      const filePath = `${assets_dir}/${address}/logo.png`;
+      const { width, height } = sizeOf(filePath);
+      metadata.dimensions = {
+        height,
+        isSquare: height === width,
+        width,
+      };
+
+      const palette = await Vibrant.from(filePath).getPalette();
+      const background = palette.Muted.hex;
+      const secondary = palette.LightMuted.hex;
+      let color = makeColorMoreChill(secondary);
+      if (isBlackOrWhite(background)) {
+        color = makeColorMoreChill(secondary);
+      }
+      if (isBlackOrWhite(secondary)) {
+        color = makeColorMoreChill(background);
+      }
+      if (color) {
+        metadata.color = color;
+      }
+    } catch (e) {
+      // console.log('Error with', address, e);
+    } finally {
+      allItems[imageUrl] = metadata;
+    }
+  }
+
+  const data = JSON.stringify(allItems);
+  writeFileSync('tokens-metadata.json', data);
+
+  const end = Date.now();
+  const time = (end - start) / 1000;
+  console.log(`Processed ${tokenAddresses.length} images`);
+  console.log(`Took ${time} seconds`);
+};
+
+init();
