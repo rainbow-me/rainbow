1e1f738b7c5c037cc3be7640b4132f9daa2a26c1
Christian Lewis <1657236+ctjlewis@users.noreply.github.com>
2021-05-26 03:28:40 -0500

    feat(postinstall): support Linux, ensure global NPM deps are available

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,32 +1,56 @@
 #!/bin/bash
-
-# If not using MacOS, gracefully skip.
-if [ "$(uname -s)" != "Darwin" ]; then
-  echo "Non-Darwin platform detected. Skipping postinstall."
-  exit 0
-fi
-
 set -eo pipefail
 
-if [ -e .env ]
+# Detect the platform.
+PLATFORM=$(uname -s)
+
+# Specify the required global NPM packages.
+GLOBAL_NPM_SCRIPTS=( "patch-package" "rn-nodeify" )
+GLOBAL_NPM_MISSING=()
+
+# For each package, add it to the GLOBAL_NPM_MISSING array if it is not globally
+# available using the canonical cross-platform method.
+for script in ${GLOBAL_NPM_SCRIPTS[@]}
+do
+  if [ ! -x "$(command -v $script)" ]; then
+    GLOBAL_NPM_MISSING+=($script)
+  fi
+done
+
+# If there are missing global NPM packages, install them.
+if [ ${#GLOBAL_NPM_MISSING[@]} -gt 0 ]
 then
-  cat .env | grep "BRANCH" | sed 's/=/ = /g' > ./ios/debug.xcconfig
-  cat .env | grep "BRANCH" | sed 's/=/ = /g' > ./ios/release.xcconfig
-  cat .env | grep "BRANCH" | sed 's/=/ = /g' > ./ios/localrelease.xcconfig
-  cat .env | grep "BRANCH" | sed 's/=/ = /g' > ./ios/staging.xcconfig
-  
-  cat .env | grep "CODEPUSH" | sed 's/=/ = /g' >> ./ios/debug.xcconfig
-  cat .env | grep "CODEPUSH" | sed 's/=/ = /g' >> ./ios/release.xcconfig
-  cat .env | grep "CODEPUSH" | sed 's/=/ = /g' >> ./ios/localrelease.xcconfig
-  cat .env | grep "CODEPUSH" | sed 's/=/ = /g' >> ./ios/staging.xcconfig
+  echo ""
+  echo "Some global NPM packages are required. Installing:"
+  echo "${GLOBAL_NPM_MISSING[@]}"
+  echo ""
+  yarn global add ${GLOBAL_NPM_MISSING[@]}
+fi
 
-  cat .env | grep "GOOGLE" | sed 's/=/ = /g' >> ./ios/debug.xcconfig
-  cat .env | grep "GOOGLE" | sed 's/=/ = /g' >> ./ios/release.xcconfig
-  cat .env | grep "GOOGLE" | sed 's/=/ = /g' >> ./ios/localrelease.xcconfig
-  cat .env | grep "GOOGLE" | sed 's/=/ = /g' >> ./ios/staging.xcconfig
+# Set up the environment.
+if [ -e .env ]; then
 
-  echo "✅ .xcconfig files created"
+  # For MacOS, copy xconfig files.
+  if [ $PLATFORM == "Darwin" ]; then
+    cat .env | grep "BRANCH" | sed 's/=/ = /g' > ./ios/debug.xcconfig
+    cat .env | grep "BRANCH" | sed 's/=/ = /g' > ./ios/release.xcconfig
+    cat .env | grep "BRANCH" | sed 's/=/ = /g' > ./ios/localrelease.xcconfig
+    cat .env | grep "BRANCH" | sed 's/=/ = /g' > ./ios/staging.xcconfig
+    
+    cat .env | grep "CODEPUSH" | sed 's/=/ = /g' >> ./ios/debug.xcconfig
+    cat .env | grep "CODEPUSH" | sed 's/=/ = /g' >> ./ios/release.xcconfig
+    cat .env | grep "CODEPUSH" | sed 's/=/ = /g' >> ./ios/localrelease.xcconfig
+    cat .env | grep "CODEPUSH" | sed 's/=/ = /g' >> ./ios/staging.xcconfig
 
+    cat .env | grep "GOOGLE" | sed 's/=/ = /g' >> ./ios/debug.xcconfig
+    cat .env | grep "GOOGLE" | sed 's/=/ = /g' >> ./ios/release.xcconfig
+    cat .env | grep "GOOGLE" | sed 's/=/ = /g' >> ./ios/localrelease.xcconfig
+    cat .env | grep "GOOGLE" | sed 's/=/ = /g' >> ./ios/staging.xcconfig
+
+    echo "✅ .xcconfig files created"
+  fi
+
+  # Export Android env vars for all platforms.
   source .env
   echo "✅ Android ENV vars exported"
 
@@ -39,13 +63,13 @@ if [ -n "$RAINBOW_SCRIPTS_APP_IOS_PREBUILD_HOOK" ]; then
   eval $RAINBOW_SCRIPTS_APP_IOS_PREBUILD_HOOK;
   echo "✅ executed ios prebuild hook"
 fi
+
 if [ -n "$RAINBOW_SCRIPTS_APP_ANDROID_PREBUILD_HOOK" ]; then
   eval $RAINBOW_SCRIPTS_APP_ANDROID_PREBUILD_HOOK;
   echo "✅ executed android prebuild hook"
 fi
 
 # Ignore any potential tracked changes to mutable development files.
-
 git update-index --assume-unchanged "ios/Internals/ios/Internals.h"
 git update-index --assume-unchanged "ios/Internals/ios/Internals.m"
 git update-index --assume-unchanged "ios/Internals/ios/Internals.swift"
@@ -57,16 +81,20 @@ git update-index --assume-unchanged "android/app/src/main/java/me/rainbow/MainAc
 
 
 # Specifying ONLY the node packages that we need to install via browserify
-# (because those aren't available in react native) and some of our deps require them.
-# If we don't specify which packages, rn-nodeify  will look into each package.json
-# and install everything including a bunch devDeps that we don't need like "console"
+# (because those aren't available in react native) and some of our deps require
+# them. If we don't specify which packages, rn-nodeify  will look into each
+# package.json and install everything including a bunch devDeps that we don't
+# need like "console"
 rn-nodeify --install --hack 'crypto,buffer,react-native-randombytes,vm,stream,http,https,os,url,net,fs,process'
 echo "✅ rn-nodeify packages hacked succesfully"
 
+# Apply patches.
 patch-package
 echo "✅ All patches applied"
 
+# Create a debug file from the default if one does not exist.
 DEBUGFILE=src/config/debug.js
+
 if test -f "$DEBUGFILE"; then
     echo "$DEBUGFILE exists."
 else
@@ -74,14 +102,17 @@ else
     cp src/config/defaultDebug.js $DEBUGFILE
 fi
 
-# Remove the incomplete installation
-# of sentry-cli from node_modules
+# Remove the incomplete installation of sentry-cli from node_modules
 rm -rf node_modules/.bin/sentry-cli
 
-if ! command -v sentry-cli &> /dev/null
-then
+if [ ! -x "$(command -v sentry-cli)" ]; then
   echo "Installing sentry-cli..."
-  brew install getsentry/tools/sentry-cli
+  case $PLATFORM in
+    # Mac + Linux environments can use Sentry's automatic install script.
+    Darwin | Linux)
+      curl -sL https://sentry.io/get-cli/ | bash
+      ;;
+  esac
 else
   which sentry-cli
   sentry-cli --version
