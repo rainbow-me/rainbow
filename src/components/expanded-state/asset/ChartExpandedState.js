95652b7b81e87d0046b979ff3fd0dfec2e20f860
Skylar Barrera <skylar.barrera@gmail.com>
2021-05-28 08:34:43 -0600

    fix hidden balance bug (#2063)

--- src/components/expanded-state/asset/ChartExpandedState.js
+++ src/components/expanded-state/asset/ChartExpandedState.js
@@ -316,7 +316,7 @@ export default function ChartExpandedState({ asset }) {
             <TokenInfoItem asset={asset} title="Balance">
               <TokenInfoBalanceValue />
             </TokenInfoItem>
-            {asset?.native?.price.display && (
+            {asset?.native?.balance.display && (
               <TokenInfoItem title="Value" weight="bold">
                 {asset?.native?.balance.display}
               </TokenInfoItem>
