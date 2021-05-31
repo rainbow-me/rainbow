95f89abd34156c9742fcd3fae961c4aa0c20003f
Bruno Barbieri <brunobar79@gmail.com>
2021-05-31 03:06:10 -0400

    better error handling + sync

--- src/handlers/cloudBackup.js
+++ src/handlers/cloudBackup.js
@@ -1,5 +1,6 @@
 import { captureException } from '@sentry/react-native';
 import { sortBy } from 'lodash';
+import { Alert } from 'react-native';
 import RNCloudFs from 'react-native-cloud-fs';
 import { RAINBOW_MASTER_KEY } from 'react-native-dotenv';
 import RNFS from 'react-native-fs';
@@ -112,6 +113,13 @@ function getGoogleDriveDocument(id) {
   return RNCloudFs.getGoogleDriveDocument(id);
 }
 
+export function syncCloud() {
+  if (ios) {
+    return RNCloudFs.syncCloud();
+  }
+  return true;
+}
+
 export async function getDataFromCloud(backupPassword, filename = null) {
   if (android) {
     await RNCloudFs.loginIfNeeded();
@@ -127,6 +135,15 @@ export async function getDataFromCloud(backupPassword, filename = null) {
     const error = new Error(CLOUD_BACKUP_ERRORS.NO_BACKUPS_FOUND);
     captureException(error);
     throw error;
+  }
+  if (!backups.files.find(file => file.uri !== null)) {
+    Alert.alert(
+      'Error syncing with iCloud',
+      `There was an error while fetching your backups information. Please make sure that you are logged into iCloud under Settings => Apple Id .
+      
+      If the problem persists please reach out to support@rainbow.me`
+    );
+    return;
   }
 
   let document;
