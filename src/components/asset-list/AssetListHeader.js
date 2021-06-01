4a3808add50aa4b64c6c08e859582493fdf34472
osdnk <micosa97@gmail.com>
2021-06-01 12:19:00 +0300

    Add truncating

--- src/components/asset-list/AssetListHeader.js
+++ src/components/asset-list/AssetListHeader.js
@@ -1,8 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { IS_TESTING } from 'react-native-dotenv';
 import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components';
-import { abbreviations, magicMemo } from '../../utils';
+import { abbreviations, magicMemo, measureText } from '../../utils';
 import { DividerSize } from '../Divider';
 import { ButtonPressAnimation } from '../animations';
 import { Icon } from '../icons';
@@ -13,7 +13,7 @@ import { useTheme } from '@rainbow-me/context';
 import { useAccountProfile, useDimensions } from '@rainbow-me/hooks';
 import { useNavigation } from '@rainbow-me/navigation';
 import Routes from '@rainbow-me/routes';
-import { position } from '@rainbow-me/styles';
+import { fonts, position } from '@rainbow-me/styles';
 
 export const AssetListHeaderHeight = ListHeaderHeight + DividerSize;
 
@@ -42,6 +42,42 @@ const DropdownArrow = styled(Centered)`
   width: ${dropdownArrowWidth};
 `;
 
+const WalletSelectButton = ({
+  truncatedAccountName,
+  onChangeWallet,
+  deviceWidth,
+  textWidth,
+  totalValue,
+}) => {
+  const { colors } = useTheme();
+  return (
+    <ButtonPressAnimation onPress={onChangeWallet} scaleTo={0.9}>
+      <Row>
+        <AccountName
+          deviceWidth={deviceWidth}
+          textWidth={textWidth}
+          totalValueLength={totalValue.length}
+        >
+          {truncatedAccountName}
+        </AccountName>
+        {truncatedAccountName ? (
+          <DropdownArrow>
+            <LinearGradient
+              borderRadius={15}
+              colors={colors.gradients.lightestGrey}
+              end={{ x: 0.5, y: 1 }}
+              pointerEvents="none"
+              start={{ x: 0.5, y: 0 }}
+              style={[position.coverAsObject, { borderRadius: 15 }]}
+            />
+            <Icon name="walletSwitcherCaret" />
+          </DropdownArrow>
+        ) : null}
+      </Row>
+    </ButtonPressAnimation>
+  );
+};
+
 const AssetListHeader = ({
   contextMenuOptions,
   isCoinListEdited,
@@ -51,13 +87,38 @@ const AssetListHeader = ({
   ...props
 }) => {
   const { width: deviceWidth } = useDimensions();
-  const { colors } = useTheme();
   const { accountName } = useAccountProfile();
   const { navigate } = useNavigation();
 
   const onChangeWallet = useCallback(() => {
     navigate(Routes.CHANGE_WALLET_SHEET);
   }, [navigate]);
+
+  const [textWidth, setTextWidth] = useState(deviceWidth);
+
+  const maxWidth =
+    deviceWidth - dropdownArrowWidth - 32 - totalValue?.length * 15;
+
+  useEffect(() => {
+    async function measure() {
+      const { width } = await measureText(accountName, {
+        fontSize: parseFloat(fonts.size.big),
+        fontWeight: fonts.weight.bold,
+        letterSpacing: fonts.letterSpacing.roundedMedium,
+      });
+      setTextWidth(width);
+    }
+    measure();
+  }, [accountName]);
+
+  const truncated = textWidth > maxWidth - 6;
+
+  const truncatedAccountName = useMemo(() => {
+    if (truncated && accountName?.endsWith('.eth')) {
+      return accountName.slice(0, -4);
+    }
+    return accountName;
+  }, [accountName, truncated]);
 
   return (
     <ListHeader
@@ -69,29 +130,13 @@ const AssetListHeader = ({
       {...props}
     >
       {!title && IS_TESTING !== 'true' && (
-        <ButtonPressAnimation onPress={onChangeWallet} scaleTo={0.9}>
-          <Row>
-            <AccountName
-              deviceWidth={deviceWidth}
-              totalValueLength={totalValue.length}
-            >
-              {accountName}
-            </AccountName>
-            {accountName ? (
-              <DropdownArrow>
-                <LinearGradient
-                  borderRadius={15}
-                  colors={colors.gradients.lightestGrey}
-                  end={{ x: 0.5, y: 1 }}
-                  pointerEvents="none"
-                  start={{ x: 0.5, y: 0 }}
-                  style={[position.coverAsObject, { borderRadius: 15 }]}
-                />
-                <Icon name="walletSwitcherCaret" />
-              </DropdownArrow>
-            ) : null}
-          </Row>
-        </ButtonPressAnimation>
+        <WalletSelectButton
+          deviceWidth={deviceWidth}
+          onChangeWallet={onChangeWallet}
+          textWidth={textWidth}
+          totalValue={totalValue}
+          truncatedAccountName={truncatedAccountName}
+        />
       )}
       {totalValue ? (
         <H1 align="right" letterSpacing="roundedTight" weight="semibold">
