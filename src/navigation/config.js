31faa083b9e8e79705a1d73f3ca1893b19150ba5
Christian Baroni <christianbaroni@me.com>
2021-06-01 02:12:39 -0400

    Clean up ExplainSheet (#2072)

--- src/navigation/config.js
+++ src/navigation/config.js
@@ -7,6 +7,7 @@ import { SheetHandleFixedToTopHeight } from '../components/sheet';
 import { Text } from '../components/text';
 import { useTheme } from '../context/ThemeContext';
 import colors from '../context/currentColors';
+import { ExplainSheetHeight } from '../screens/ExplainSheet';
 import { onWillPop } from './Navigation';
 import WalletBackupStepTypes from '@rainbow-me/helpers/walletBackupStepTypes';
 import { fonts } from '@rainbow-me/styles';
@@ -92,7 +93,7 @@ export const explainSheetConfig = {
   options: ({ route: { params = {} } }) => ({
     ...buildCoolModalConfig({
       ...params,
-      longFormHeight: 400,
+      longFormHeight: ExplainSheetHeight,
     }),
   }),
 };
