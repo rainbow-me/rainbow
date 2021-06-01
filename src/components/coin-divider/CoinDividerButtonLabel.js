65b561f6fb405fcb617a1a94a797275fbd62580f
Christian Baroni <c@baroni.co>
2021-06-01 02:11:50 -0400

    Fix clipped share button

--- src/components/coin-divider/CoinDividerButtonLabel.js
+++ src/components/coin-divider/CoinDividerButtonLabel.js
@@ -4,14 +4,15 @@ import { magicMemo } from '../../utils';
 import { OpacityToggler } from '../animations';
 import { Text } from '../text';
 
-const LabelText = styled(Text).attrs(({ theme: { colors } }) => ({
+const LabelText = styled(Text).attrs(({ shareButton, theme: { colors } }) => ({
   color: colors.alpha(colors.blueGreyDark, 0.6),
   letterSpacing: 'roundedTight',
+  lineHeight: 30,
   size: 'lmedium',
-  weight: 'bold',
+  weight: shareButton ? 'heavy' : 'bold',
 }))`
   position: absolute;
-  top: ${android ? -15.25 : -10.25};
+  top: ${android ? -15 : -16};
   ${({ shareButton }) => shareButton && `width:  100%;`};
 `;
 
