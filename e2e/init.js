450b97dd2861a3d5ac6fdddeb78b262043a12293
Bruno Barbieri <brunobar79@gmail.com>
2021-06-04 19:29:02 -0400

    Unblock detox (#2089)
    * Unblock detox
    
    * Better regex handling
    
    * ignore linter

--- e2e/init.js
+++ e2e/init.js
@@ -1,7 +1,14 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable no-undef */
 // eslint-disable-next-line import/no-commonjs
 require('dotenv').config({ path: '.env' });
 
 beforeAll(async () => {
   await device.launchApp();
+
+  await device.setURLBlacklist([
+    // eslint-disable-next-line prettier/prettier
+    'api.thegraph.com\/subgraphs\/name\/ianlapham\/uniswapv2',
+    'raw.githubusercontent.com',
+  ]);
 });
