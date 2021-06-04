0f8dbbb9e3fbcc959781e12b27079f613915976c
Bruno Barbieri <brunobar79@gmail.com>
2021-06-04 14:15:32 -0400

    ignore linter

--- e2e/init.js
+++ e2e/init.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable no-undef */
 // eslint-disable-next-line import/no-commonjs
 require('dotenv').config({ path: '.env' });
@@ -6,6 +7,7 @@ beforeAll(async () => {
   await device.launchApp();
 
   await device.setURLBlacklist([
+    // eslint-disable-next-line prettier/prettier
     'api.thegraph.com\/subgraphs\/name\/ianlapham\/uniswapv2',
     'raw.githubusercontent.com',
   ]);
