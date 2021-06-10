ff4aa744397128451bef95655cc4105ee1ebdfe3
Bruno Barbieri <brunobar79@gmail.com>
2021-06-10 19:57:03 -0400

    add expanded state l2 explainers

--- src/components/expanded-state/asset/ChartExpandedState.js
+++ src/components/expanded-state/asset/ChartExpandedState.js
@@ -1,4 +1,4 @@
-import { find } from 'lodash';
+import { capitalize, find } from 'lodash';
 import React, {
   useCallback,
   useContext,
@@ -8,6 +8,7 @@ import React, {
 } from 'react';
 import { LayoutAnimation, View } from 'react-native';
 import { getSoftMenuBarHeight } from 'react-native-extra-dimensions-android';
+import RadialGradient from 'react-native-radial-gradient';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import useAdditionalAssetData from '../../../hooks/useAdditionalAssetData';
@@ -15,8 +16,10 @@ import { ModalContext } from '../../../react-native-cool-modals/NativeStackView'
 import { ButtonPressAnimation } from '../../animations';
 import { CoinDividerHeight } from '../../coin-divider';
 import CoinDividerOpenButton from '../../coin-divider/CoinDividerOpenButton';
+import ChainBadge from '../../coin-icon/ChainBadge';
 import EdgeFade from '../../discover-sheet/EdgeFade';
 import UniswapPools from '../../discover-sheet/UniswapPoolsSection';
+import { Column, Row } from '../../layout';
 
 import {
   BuyActionButton,
@@ -45,6 +48,9 @@ import {
   useDimensions,
   useUniswapAssetsInWallet,
 } from '@rainbow-me/hooks';
+import { useNavigation } from '@rainbow-me/navigation';
+import Routes from '@rainbow-me/routes';
+import { lightModeThemeColors, position } from '@rainbow-me/styles';
 import { ethereumUtils, safeAreaInsetValues } from '@rainbow-me/utils';
 
 const defaultCarouselHeight = 60;
@@ -169,6 +175,57 @@ function Description({ text }) {
   );
 }
 
+const L2Explainer = ({ assetType, colors, onPress, symbol }) => {
+  const gradientColors = ['#FFFFFF', '#F0F2F5'];
+  const radialGradientProps = {
+    center: [0, 1],
+    colors: [
+      lightModeThemeColors.alpha(gradientColors[0], 0.1),
+      lightModeThemeColors.alpha(gradientColors[1], 0.08),
+    ],
+    pointerEvents: 'none',
+    style: {
+      ...position.coverAsObject,
+      overflow: 'hidden',
+    },
+  };
+  return (
+    <ButtonPressAnimation onPress={onPress} scaleTo={0.99}>
+      <Row
+        background={colors.red}
+        borderRadius={16}
+        marginBottom={19}
+        marginLeft={15}
+        marginRight={15}
+        padding={10}
+      >
+        <RadialGradient
+          {...radialGradientProps}
+          borderRadius={21}
+          radius={81}
+        />
+        <Column>
+          <ChainBadge
+            assetType={assetType}
+            badgeXPosition={-3}
+            badgeYPosition={-10}
+          />
+        </Column>
+        <Column marginLeft={40}>
+          <Text color={colors.alpha(colors.blueGreyDark, 0.6)} size="smedium">
+            This {symbol} is on the {capitalize(assetType)} network
+          </Text>
+        </Column>
+        <Column align="end" flex={1} justify="end">
+          <Text color={colors.alpha(colors.blueGreyDark, 0.6)} size="smedium">
+            􀅵
+          </Text>
+        </Column>
+      </Row>
+    </ButtonPressAnimation>
+  );
+};
+
 export default function ChartExpandedState({ asset }) {
   const { genericAssets } = useSelector(({ data: { genericAssets } }) => ({
     genericAssets,
@@ -269,6 +326,15 @@ export default function ChartExpandedState({ asset }) {
     ChartExpandedStateSheetHeight -= 60;
   }
 
+  const { navigate } = useNavigation();
+
+  const handleL2ExplainerPress = useCallback(() => {
+    navigate(Routes.EXPLAIN_SHEET, {
+      network: assetWithPrice.type,
+      type: 'l2',
+    });
+  }, [assetWithPrice.type, navigate]);
+
   const { layout } = useContext(ModalContext) || {};
 
   const [morePoolsVisible, setMorePoolsVisible] = useState(false);
@@ -276,6 +342,8 @@ export default function ChartExpandedState({ asset }) {
   const delayedMorePoolsVisible = useDelayedValueWithLayoutAnimation(
     morePoolsVisible
   );
+
+  const { colors } = useTheme();
 
   const MoreButton = useCallback(() => {
     return (
@@ -352,6 +420,15 @@ export default function ChartExpandedState({ asset }) {
           )}
         </SheetActionButtonRow>
       )}
+      {assetWithPrice?.mainnet_address && (
+        <L2Explainer
+          assetType={assetWithPrice.type}
+          colors={colors}
+          onPress={handleL2ExplainerPress}
+          symbol={assetWithPrice.symbol}
+        />
+      )}
+
       <CarouselWrapper
         isAnyItemLoading={
           totalVolumeLoading || totalLiquidityLoading || marketCapLoading
