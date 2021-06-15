0af2e7918c13493472421db2710e339f635db140
Bruno Barbieri <brunobar79@gmail.com>
2021-06-14 22:25:02 -0400

    add delay to show content

--- src/screens/WalletDiagnosticsSheet.js
+++ src/screens/WalletDiagnosticsSheet.js
@@ -222,7 +222,9 @@ const WalletDiagnosticsSheet = () => {
         captureException(customError);
       }
     };
-    init();
+    setTimeout(() => {
+      init();
+    }, 2000);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
