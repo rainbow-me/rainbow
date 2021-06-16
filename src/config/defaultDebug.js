e689d07f0b6ef048dc81df601ea849166f591328
Bruno Barbieri <brunobar79@gmail.com>
2021-06-16 02:14:51 -0400

    turn them all ON

--- src/config/defaultDebug.js
+++ src/config/defaultDebug.js
@@ -14,3 +14,6 @@ export const parseAllTxnsOnReceive = false;
 export const reactNativeDisableYellowBox = true;
 export const showNetworkRequests = false;
 export const showNetworkResponses = false;
+export const arbitrumEnabled = true;
+export const optimismEnabled = true;
+export const polygonEnabled = true;
