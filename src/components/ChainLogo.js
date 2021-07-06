fbdc25b40cbae00234099608485d57f4f20febbc
Esteban Mino <efmino@uc.cl>
2021-07-06 10:33:16 -0400

    use Image

--- src/components/ChainLogo.js
+++ src/components/ChainLogo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image } from 'react-native';
 import styled from 'styled-components';
 import ArbitrumBadge from '../assets/badges/arbitrumBadge.png';
 import ArbitrumBadgeDark from '../assets/badges/arbitrumBadgeDark.png';
@@ -8,10 +9,9 @@ import PolygonBadge from '../assets/badges/polygonBadge.png';
 import PolygonBadgeDark from '../assets/badges/polygonBadgeDark.png';
 import { Centered } from './layout';
 import networkTypes from '@rainbow-me/helpers/networkTypes';
-import { ImgixImage } from '@rainbow-me/images';
 import { position } from '@rainbow-me/styles';
 
-const ChainIcon = styled(ImgixImage)`
+const ChainIcon = styled(Image)`
   height: ${({ size }) => size};
   margin-top: 1;
   width: ${({ size }) => size};
