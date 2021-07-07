8b2bd329583b7f856123886ae3b00eebaabdb37a
Bruno Barbieri <brunobar79@gmail.com>
2021-07-06 21:58:11 -0400

    add recipient checks

--- src/screens/SendSheet.js
+++ src/screens/SendSheet.js
@@ -3,7 +3,7 @@ import analytics from '@segment/analytics-react-native';
 import { captureEvent, captureException } from '@sentry/react-native';
 import { isEmpty, isString, toLower } from 'lodash';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { InteractionManager, Keyboard, StatusBar } from 'react-native';
+import { Alert, InteractionManager, Keyboard, StatusBar } from 'react-native';
 import { getStatusBarHeight, isIphoneX } from 'react-native-iphone-x-helper';
 import { KeyboardArea } from 'react-native-keyboard-area';
 import { useDispatch } from 'react-redux';
@@ -24,6 +24,7 @@ import {
   createSignableTransaction,
   estimateGasLimit,
   getProviderForNetwork,
+  isL2Network,
   resolveNameOrAddress,
   web3Provider,
 } from '@rainbow-me/handlers/web3';
@@ -51,7 +52,7 @@ import {
 } from '@rainbow-me/hooks';
 import { sendTransaction } from '@rainbow-me/model/wallet';
 import { useNavigation } from '@rainbow-me/navigation/Navigation';
-import { ETH_ADDRESS } from '@rainbow-me/references';
+import { ETH_ADDRESS, RAINBOW_TOKEN_LIST } from '@rainbow-me/references';
 import Routes from '@rainbow-me/routes';
 import { borders } from '@rainbow-me/styles';
 import {
@@ -503,11 +504,39 @@ export default function SendSheet(props) {
     [gasPrices, txFees, updateGasPriceOption, currentNetwork]
   );
 
+  const validateReceipient = useCallback(
+    async toAddress => {
+      // Don't allow send to known ERC20 contracts
+      if (RAINBOW_TOKEN_LIST[toLower(toAddress)]) {
+        return false;
+      }
+
+      // Don't allow sending funds directly to contracts on L2
+      if (isL2Network(currentNetwork)) {
+        const code = await currentProvider.getCode(toAddress);
+        if (!code || code === '0x') {
+          return true;
+        }
+        return false;
+      }
+      return true;
+    },
+    [currentNetwork, currentProvider]
+  );
+
   const showConfirmationSheet = useCallback(async () => {
     let toAddress = recipient;
     if (isENSAddressFormat(recipient)) {
       toAddress = await resolveNameOrAddress(recipient);
     }
+
+    const validRecipient = await validateReceipient(toAddress);
+
+    if (!validRecipient) {
+      Alert.alert('STOP!', 'You are sending funds to a contract!');
+      return;
+    }
+
     Keyboard.dismiss();
     navigate(Routes.SEND_CONFIRMATION_SHEET, {
       amountDetails: amountDetails,
