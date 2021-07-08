129248722713dc7ccfc7fbcd35c6055b927f6759
Skylar Barrera <skylar.barrera@gmail.com>
2021-07-08 15:03:57 -0400

    alert util

---
+++ src/utils/watchingAlert.js
@@ -0,0 +1,23 @@
+import { Alert } from 'react-native';
+import { Navigation } from '@rainbow-me/navigation';
+import Routes from '@rainbow-me/routes';
+
+export default async function watchingAlert() {
+  Alert.alert(
+    `You need to import this wallet!`,
+    `This wallet is currently in "Watching" mode.
+
+It looks like you imported this wallet using only its public address. In order to control what's inside, you'll need to import the private key or secret phrase first.`,
+    [
+      {
+        onPress: () => {
+          Navigation.handleAction(Routes.IMPORT_SEED_PHRASE_FLOW);
+        },
+        text: 'Finish Importing',
+      },
+      {
+        text: 'Nevermind',
+      },
+    ]
+  );
+}
