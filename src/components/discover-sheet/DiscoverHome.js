f0250f603d1f0f3f821bcbfa98543bb2c3cc2e85
Esteban Miño <efmino@uc.cl>
2021-07-12 17:03:26 -0400

    Revert "REVERTTHIS"
    This reverts commit ccbe8c96df1a80e1cd2692b22bc34cd4d1647d44.

--- src/components/discover-sheet/DiscoverHome.js
+++ src/components/discover-sheet/DiscoverHome.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ConnectedDapps from '../qrcode-scanner/ConnectedDapps';
 import BottomSpacer from './BottomSpacer';
 import Lists from './ListsSection';
 import PulseIndex from './PulseIndexSection';
@@ -13,8 +12,6 @@ export default function DiscoverHome() {
   return (
     <React.Fragment>
       <TopMoversSection />
-      <ConnectedDapps />
-
       <PulseIndex />
       <Lists />
       {/* <Strategies /> */}
