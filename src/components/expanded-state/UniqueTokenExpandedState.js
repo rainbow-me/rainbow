66972c0a6f4f696519f07f3343a550ded04e0871
Skylar Barrera <skylar.barrera@gmail.com>
2021-07-13 14:33:09 -0400

    add share button

--- src/components/expanded-state/UniqueTokenExpandedState.js
+++ src/components/expanded-state/UniqueTokenExpandedState.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useCallback, useMemo } from 'react';
+import { Share } from 'react-native';
 import styled from 'styled-components';
 import useWallets from '../../hooks/useWallets';
 import Link from '../Link';
@@ -18,8 +19,17 @@ import {
   UniqueTokenExpandedStateContent,
   UniqueTokenExpandedStateHeader,
 } from './unique-token';
-import { useDimensions, useShowcaseTokens } from '@rainbow-me/hooks';
-import { magicMemo, safeAreaInsetValues } from '@rainbow-me/utils';
+import { buildUniqueTokenName } from '@rainbow-me/helpers/assets';
+import {
+  useAccountProfile,
+  useDimensions,
+  useShowcaseTokens,
+} from '@rainbow-me/hooks';
+import {
+  buildRainbowUrl,
+  magicMemo,
+  safeAreaInsetValues,
+} from '@rainbow-me/utils';
 
 const Spacer = styled.View`
   height: ${safeAreaInsetValues.bottom + 20};
@@ -56,6 +66,15 @@ const UniqueTokenExpandedState = ({ asset, external }) => {
     }
   }, [addShowcaseToken, isShowcaseAsset, removeShowcaseToken, uniqueId]);
 
+  const { accountAddress, accountENS } = useAccountProfile();
+
+  const handlePressShare = useCallback(() => {
+    Share.share({
+      title: `Share ${buildUniqueTokenName(asset)} Info`,
+      url: buildRainbowUrl(asset, accountENS, accountAddress),
+    });
+  }, [accountAddress, accountENS, asset]);
+
   const { height: screenHeight } = useDimensions();
   const { colors, isDarkMode } = useTheme();
 
@@ -70,7 +89,7 @@ const UniqueTokenExpandedState = ({ asset, external }) => {
       >
         <UniqueTokenExpandedStateHeader asset={asset} />
         <UniqueTokenExpandedStateContent asset={asset} />
-        {!external && !isReadOnlyWallet && (
+        {!external && !isReadOnlyWallet ? (
           <SheetActionButtonRow>
             <SheetActionButton
               color={isDarkMode ? colors.darkModeDark : colors.dark}
@@ -79,6 +98,15 @@ const UniqueTokenExpandedState = ({ asset, external }) => {
               weight="bold"
             />
             {isSendable && <SendActionButton />}
+          </SheetActionButtonRow>
+        ) : (
+          <SheetActionButtonRow>
+            <SheetActionButton
+              color={isDarkMode ? colors.darkModeDark : colors.dark}
+              label="􀈂 Share"
+              onPress={handlePressShare}
+              weight="bold"
+            />
           </SheetActionButtonRow>
         )}
         <SheetDivider />
