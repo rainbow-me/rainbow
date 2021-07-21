2126ced3a6fb4316a3a50b0f31a14403908be67c
Nico Glennon <nicoglennon@icloud.com>
2021-07-21 12:36:07 -0400

    Log errorInfo as well

--- src/components/error-boundary/ErrorBoundary.tsx
+++ src/components/error-boundary/ErrorBoundary.tsx
@@ -1,6 +1,7 @@
-import { captureException, captureMessage } from '@sentry/react-native';
+import { captureException } from '@sentry/react-native';
 import React from 'react';
 import Fallback from './Fallback';
+import logger from 'logger';
 
 export default class ErrorBoundary extends React.Component {
   static getDerivedStateFromError(_error: any) {
@@ -9,10 +10,14 @@ export default class ErrorBoundary extends React.Component {
 
   state = { hasError: false };
 
-  componentDidCatch(error: any, errorInfo: any) {
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
     // log captured error to Sentry
+    logger.sentry(
+      `Unhandled JS error caught by Error Boundary: ${JSON.stringify(
+        errorInfo
+      )}`
+    );
     captureException(error);
-    captureMessage(errorInfo);
   }
 
   render() {
