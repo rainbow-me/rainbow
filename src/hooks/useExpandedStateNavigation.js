151501f4ee0cabf5518c1ac40fdf9680ef1807bf
Skylar Barrera <skylar.barrera@gmail.com>
2021-07-28 12:17:40 -0400

    add return

--- src/hooks/useExpandedStateNavigation.js
+++ src/hooks/useExpandedStateNavigation.js
@@ -3,6 +3,7 @@ import { useCallback, useMemo } from 'react';
 import { InteractionManager } from 'react-native';
 import useAsset from './useAsset';
 import useWallets from './useWallets';
+import { enableActionsOnReadOnlyWallet } from '@rainbow-me/config/debug';
 import AssetInputTypes from '@rainbow-me/helpers/assetInputTypes';
 import { useNavigation } from '@rainbow-me/navigation';
 import { watchingAlert } from '@rainbow-me/utils';
@@ -29,8 +30,9 @@ export default function useExpandedStateNavigation(inputType) {
 
   return useCallback(
     (routeName, traverseParams) => {
-      if (isReadOnlyWallet) {
+      if (isReadOnlyWallet || enableActionsOnReadOnlyWallet) {
         watchingAlert();
+        return;
       }
 
       InteractionManager.runAfterInteractions(goBack);
