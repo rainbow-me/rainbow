cdbb50ccb15d1eaa30bac9975197a0183a4cab8c
jinchung <jin.chung17@gmail.com>
2021-07-28 12:17:43 -0700

    Rename useUsersPostions hook

--- src/hooks/useTotalFeeEarnedPerAsset.ts
+++ src/hooks/useTotalFeeEarnedPerAsset.ts
@@ -5,14 +5,14 @@ import useNativeCurrencyToUSD from './useNativeCurrencyToUSD';
 import { AppState } from '@rainbow-me/redux/store';
 import { StoredPositions } from '@rainbow-me/redux/usersPositions';
 
-export function useUsersPositions(): StoredPositions[] {
+export default function useTotalFeeEarnedPerAsset(
+  address: string
+): number | undefined {
   const { accountAddress } = useAccountSettings();
+  const positions: StoredPositions[] = useSelector(
+    (state: AppState) => state.usersPositions
+  )[accountAddress];
 
-  return useSelector((state: AppState) => state.usersPositions)[accountAddress];
-}
-
-export function useTotalFeeEarnedPerAsset(address: string): number | undefined {
-  const positions = useUsersPositions();
   const fee = positions?.find(({ pair: { id } }) => id === toLower(address))
     ?.fees?.sum;
   const rate = useNativeCurrencyToUSD();
