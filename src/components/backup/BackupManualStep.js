b1a129d8daa9715bc021b60d230e4232df6dddf9
Nico Glennon <nicoglennon@icloud.com>
2021-07-28 20:13:48 -0400

    Fix layout on seed backup sheets

--- src/components/backup/BackupManualStep.js
+++ src/components/backup/BackupManualStep.js
@@ -4,7 +4,7 @@ import React, { Fragment, useCallback, useEffect, useState } from 'react';
 import { View } from 'react-native';
 import styled from 'styled-components';
 import { useTheme } from '../../context/ThemeContext';
-import { Column } from '../layout';
+import { Column, Row } from '../layout';
 import { SecretDisplaySection } from '../secret-display';
 import { SheetActionButton } from '../sheet';
 import { Nbsp, Text } from '../text';
@@ -23,7 +23,7 @@ const Content = styled(Column).attrs({
 })`
   flex-grow: 1;
   flex-shrink: 0;
-  padding-top: ${({ isTallPhone }) => (android ? 30 : isTallPhone ? 65 : 15)};
+  padding-top: ${({ isTallPhone }) => (android ? 30 : isTallPhone ? 45 : 25)};
 `;
 
 const Footer = styled(Column).attrs({
@@ -38,15 +38,13 @@ const Footer = styled(Column).attrs({
 const Masthead = styled(Column).attrs({
   align: 'center',
   justify: 'start',
-})`
-  padding-top: 18;
-`;
+})``;
 
 const MastheadDescription = styled(Text).attrs(({ theme: { colors } }) => ({
   align: 'center',
   color: colors.alpha(colors.blueGreyDark, 0.6),
   lineHeight: 'looser',
-  size: 'large',
+  size: 'lmedium',
 }))`
   max-width: 291;
 `;
@@ -54,16 +52,23 @@ const MastheadDescription = styled(Text).attrs(({ theme: { colors } }) => ({
 const MastheadIcon = styled(Text).attrs({
   align: 'center',
   color: 'appleBlue',
-  size: 43,
-  weight: 'semibold',
+  size: 21,
+  weight: 'heavy',
 })``;
 
 const MastheadTitle = styled(Text).attrs({
   align: 'center',
-  size: 'big',
+  size: 'larger',
   weight: 'bold',
 })`
-  ${padding(15, 0, 12)};
+  ${padding(8)};
+`;
+
+const MastheadTitleRow = styled(Row).attrs({
+  align: 'center',
+  justify: 'start',
+})`
+  padding-top: 18;
 `;
 
 export default function BackupManualStep() {
@@ -97,8 +102,10 @@ export default function BackupManualStep() {
   return (
     <Fragment>
       <Masthead>
-        <MastheadIcon>􀉆</MastheadIcon>
-        <MastheadTitle>Back up manually</MastheadTitle>
+        <MastheadTitleRow>
+          <MastheadIcon>􀉆</MastheadIcon>
+          <MastheadTitle>Back up manually</MastheadTitle>
+        </MastheadTitleRow>
         <MastheadDescription>
           <MastheadDescription weight="semibold">
             {type === WalletTypes.privateKey
