dbef87746d79e8f8699e3a1cea3bf20dfb168bf3
Bruno Barbieri <brunobar79@gmail.com>
2021-08-14 00:08:10 -0400

    clean up

--- src/redux/walletconnect.js
+++ src/redux/walletconnect.js
@@ -174,7 +174,6 @@ export const walletConnectOnSessionRequest = (uri, callback) => async (
       };
 
       walletConnector?.on('session_request', (error, payload) => {
-        logger.debug('CLEARED TIMED OUT YO 1!');
         clearTimeout(timeout);
         if (error) {
           analytics.track('Error on wc session_request', {
@@ -229,7 +228,6 @@ export const walletConnectOnSessionRequest = (uri, callback) => async (
           if (meta) return;
           timedOut = true;
           routeParams = { ...routeParams, timedOut };
-          logger.debug('TIMED OUT YO!');
           Navigation.handleAction(
             Routes.WALLET_CONNECT_APPROVAL_SHEET,
             routeParams
@@ -247,7 +245,6 @@ export const walletConnectOnSessionRequest = (uri, callback) => async (
         );
       });
     } catch (error) {
-      logger.debug('CLEARED TIMED OUT YO 2!');
       clearTimeout(timeout);
       logger.log('Exception during wc session_request');
       analytics.track('Exception on wc session_request', {
@@ -257,7 +254,6 @@ export const walletConnectOnSessionRequest = (uri, callback) => async (
       Alert.alert(lang.t('wallet.wallet_connect.error'));
     }
   } catch (error) {
-    logger.debug('CLEARED TIMED OUT YO 3!');
     clearTimeout(timeout);
     logger.log('FCM exception during wc session_request');
     analytics.track('FCM exception on wc session_request', {
