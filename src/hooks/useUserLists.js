2ab25b37fad20fbab9c609a9453dca14cffcc8cb
Esteban Miño <efmino@uc.cl>
2021-08-27 14:26:29 -0400

    cleanup

--- src/hooks/useUserLists.js
+++ src/hooks/useUserLists.js
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   userListsClearList,
@@ -9,14 +9,14 @@ import {
 const userListsSelector = state => state.userLists.lists;
 const userListsReadySelector = state => state.userLists.ready;
 const userListsSelectedListSelector = state => state.userLists.selectedList;
-const uniswapfavoritesSelector = state => state.uniswap.favorites;
+const uniswapFavoritesSelector = state => state.uniswap.favorites;
 
 export default function useUserLists() {
   const dispatch = useDispatch();
   const lists = useSelector(userListsSelector);
   const ready = useSelector(userListsReadySelector);
   const selectedList = useSelector(userListsSelectedListSelector);
-  const favorites = useSelector(uniswapfavoritesSelector);
+  const favorites = useSelector(uniswapFavoritesSelector);
 
   const updateList = useCallback(
     (...data) => dispatch(userListsUpdateList(...data)),
