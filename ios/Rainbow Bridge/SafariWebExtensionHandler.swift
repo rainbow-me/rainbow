c2df026a05e53c27d9a969138f54ae15799af3ed
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-09-25 11:25:42 +0200

    Add the extension?? I think
    have no idea to test wether this works but LFG

---
+++ ios/Rainbow Bridge/SafariWebExtensionHandler.swift
@@ -0,0 +1,25 @@
+//
+//  SafariWebExtensionHandler.swift
+//  Rainbow Bridge
+//
+//  Created by Miguel Piedrafita on 25/9/21.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+import SafariServices
+import os.log
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+
+}
