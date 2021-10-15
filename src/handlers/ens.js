84b6e815676cc3940c3201cc1111a280be55a9a5
Skylar Barrera <skylar.barrera@gmail.com>
2021-10-15 13:56:35 -0600

    export

--- src/handlers/ens.js
+++ src/handlers/ens.js
@@ -3,7 +3,7 @@ import { ensClient } from '../apollo/client';
 import { ENS_SUGGESTIONS } from '../apollo/queries';
 import { profileUtils } from '@rainbow-me/utils';
 
-const fetchSuggestions = async (
+export const fetchSuggestions = async (
   recipient,
   setSuggestions,
   // eslint-disable-next-line no-unused-vars
