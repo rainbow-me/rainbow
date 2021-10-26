8906574243476132fd9ce0fbdc2a4883ff31d689
yanniksood <yanniksood7@gmail.com>
2021-10-26 14:31:58 -0400

    added ens resolution hook skeleton

---
+++ src/hooks/useResolveENS.js
@@ -0,0 +1,10 @@
+import { web3Provider } from '../../handlers/web3';
+import { abbreviations } from '../utils';
+
+/**
+ * Pass a users ETH wallet address
+ * Resolve the ens name OR truncate the eth address and return it
+ */
+export default function useResolveENS({ address, truncation }) {
+  
+}
