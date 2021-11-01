9f0d758358959aecf7aab6d32d0870b7d8854644
Marco Burstein <marco@marco.how>
2021-11-01 11:51:43 -0400

    Update `src/redux/imageMetadata` to use TypeScript and create `ImageMetadata` interface.

---
+++ src/redux/imageMetadata.ts
@@ -0,0 +1,110 @@
+import produce from 'immer';
+import {
+  getImageMetadata,
+  saveImageMetadata,
+} from '../handlers/localstorage/globalSettings';
+import { AppDispatch, AppGetState } from './store';
+
+// -- Constants --------------------------------------- //
+const CLEAR = 'imageMetadata/CLEAR';
+const LOAD = 'imageMetadata/LOAD';
+const MERGE = 'imageMetadata/MERGE';
+
+// -- Types ------------------------------------------ //
+
+/**
+ * Represents cached metadata for an image.
+ */
+export interface ImageMetadata {
+  color?: string;
+  dimensions: {
+    height: number;
+    isSquare: boolean;
+    width: number;
+  };
+}
+
+/**
+ * The state of the `imageMetadata` reducer.
+ */
+interface ImageMetadataState {
+  imageMetadata: {
+    [id: string]: ImageMetadata;
+  };
+}
+
+/**
+ * An `imageMetadata` Redux action.
+ */
+type ImageMetadataAction =
+  | ImageMetadataLoadAction
+  | ImageMetadataMergeAction
+  | ImageMetadataClearAction;
+
+interface ImageMetadataLoadAction {
+  type: typeof LOAD;
+  payload: ImageMetadataState['imageMetadata'];
+}
+
+interface ImageMetadataMergeAction {
+  type: typeof MERGE;
+  id: string;
+  metadata: ImageMetadata;
+}
+
+interface ImageMetadataClearAction {
+  type: typeof CLEAR;
+}
+
+interface UpdateImageMetadataFunctionParameter {
+  id: string;
+  metadata: ImageMetadata;
+}
+
+// -- Actions ---------------------------------------- //
+export const clearImageMetadataCache = () => (dispatch: AppDispatch) =>
+  dispatch({ type: CLEAR });
+
+export const imageMetadataCacheLoadState = () => async (
+  dispatch: AppDispatch
+) => {
+  const metadataCache = await getImageMetadata();
+  dispatch({
+    payload: metadataCache,
+    type: LOAD,
+  });
+};
+
+export const updateImageMetadataCache = ({
+  id,
+  metadata,
+}: UpdateImageMetadataFunctionParameter) => (
+  dispatch: AppDispatch,
+  getState: AppGetState
+) => {
+  const { imageMetadata } = getState().imageMetadata;
+  dispatch({ id, metadata, type: MERGE });
+  saveImageMetadata({
+    ...imageMetadata,
+    [id]: metadata,
+  });
+};
+
+// -- Reducer ----------------------------------------- //
+const INITIAL_STATE: ImageMetadataState = {
+  imageMetadata: {},
+};
+
+export default (
+  state: ImageMetadataState = INITIAL_STATE,
+  action: ImageMetadataAction
+) =>
+  produce(state, draft => {
+    if (action.type === LOAD) {
+      draft.imageMetadata = action.payload;
+    } else if (action.type === MERGE) {
+      draft.imageMetadata[action?.id] = action.metadata;
+    } else if (action.type === CLEAR) {
+      return INITIAL_STATE;
+    }
+  });
