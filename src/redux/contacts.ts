999c767da4d5e47dbb80efd92f082c99a550905b
Marco Burstein <marco@marco.how>
2021-11-01 11:51:58 -0400

    Update `src/redux/contacts` to use TypeScript and create `Contact` interface.

--- src/redux/contacts.ts
+++ src/redux/contacts.ts
@@ -1,13 +1,74 @@
 import { omit, toLower } from 'lodash';
 import { getContacts, saveContacts } from '../handlers/localstorage/contacts';
+import { AppDispatch, AppGetState } from './store';
+import { Network } from '@rainbow-me/helpers/networkTypes';
 
 // -- Constants --------------------------------------- //
 const CONTACTS_UPDATE = 'contacts/CONTACTS_UPDATE';
 const CONTACTS_LOAD = 'contacts/CONTACTS_LOAD';
 const CONTACTS_CLEAR_STATE = 'contacts/CONTACTS_CLEAR_STATE';
 
+// -- Types ------------------------------------------ //
+
+/**
+ * A contact's information.
+ */
+export interface Contact {
+  /**
+   * The contact's address.
+   */
+  address: string;
+
+  /**
+   * A color index, based on the values in `avatars` in
+   * `src/utils/profileUtils.ts`.
+   */
+  color: number;
+
+  /**
+   * The network.
+   */
+  network: Network;
+
+  /**
+   * The contact's nickname.
+   */
+  nickname: string;
+}
+
+/**
+ * The `contacts` reducer's state.
+ */
+interface ContactsState {
+  contacts: {
+    [address: string]: Contact;
+  };
+}
+
+/**
+ * An action for the `contacts` reducer.
+ */
+type ContactsAction =
+  | ContactsUpdateAction
+  | ContactsLoadAction
+  | ContactsClearStateAction;
+
+interface ContactsUpdateAction {
+  type: typeof CONTACTS_UPDATE;
+  payload: ContactsState['contacts'];
+}
+
+interface ContactsLoadAction {
+  type: typeof CONTACTS_LOAD;
+  payload: ContactsState['contacts'];
+}
+
+interface ContactsClearStateAction {
+  type: typeof CONTACTS_CLEAR_STATE;
+}
+
 // -- Actions ---------------------------------------- //
-export const contactsLoadState = () => async dispatch => {
+export const contactsLoadState = () => async (dispatch: AppDispatch) => {
   try {
     const contacts = await getContacts();
     dispatch({
@@ -18,10 +79,12 @@ export const contactsLoadState = () => async dispatch => {
   } catch (error) {}
 };
 
-export const contactsAddOrUpdate = (address, nickname, color, network) => (
-  dispatch,
-  getState
-) => {
+export const contactsAddOrUpdate = (
+  address: string,
+  nickname: string,
+  color: number,
+  network: Network
+) => (dispatch: AppDispatch, getState: AppGetState) => {
   const loweredAddress = toLower(address);
   const { contacts } = getState().contacts;
   const updatedContacts = {
@@ -40,7 +103,10 @@ export const contactsAddOrUpdate = (address, nickname, color, network) => (
   });
 };
 
-export const removeContact = address => (dispatch, getState) => {
+export const removeContact = (address: string) => (
+  dispatch: AppDispatch,
+  getState: AppGetState
+) => {
   const { contacts } = getState().contacts;
   const updatedContacts = omit(contacts, toLower(address));
   saveContacts(updatedContacts);
@@ -51,11 +117,11 @@ export const removeContact = address => (dispatch, getState) => {
 };
 
 // -- Reducer ----------------------------------------- //
-const INITIAL_STATE = {
+const INITIAL_STATE: ContactsState = {
   contacts: {},
 };
 
-export default (state = INITIAL_STATE, action) => {
+export default (state = INITIAL_STATE, action: ContactsAction) => {
   switch (action.type) {
     case CONTACTS_UPDATE:
       return { ...state, contacts: action.payload };
