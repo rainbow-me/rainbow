0b764854d521aaa99d15bdc8faefdd50e465d489
Esteban Miño <efmino@uc.cl>
2021-11-02 12:13:11 -0400

    trends

--- src/utils/gas.ts
+++ src/utils/gas.ts
@@ -33,16 +33,14 @@ const GAS_ICONS = {
 };
 
 const getTrendKey = (trend: number) => {
-  // missing rising still
-  switch (trend) {
-    case -1:
-      return 'falling';
-    case 0:
-      return 'stable';
-    case 1:
-      return 'rising';
-    default:
-      return 'stable';
+  if (trend < -0.25) {
+    return 'falling';
+  } else if (trend >= -0.25 && trend <= 0.25) {
+    return 'stable';
+  } else if (trend > 0.25 && trend <= 0.6) {
+    return 'rising';
+  } else if (trend > 0.6) {
+    return 'surging';
   }
 };
 
