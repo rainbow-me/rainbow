c2fcb44171227a31697caf3632cca994841dd479
YannikSood <yanniksood7@gmail.com>
2021-11-03 18:19:29 -0400

    cleanup phase 2

--- src/components/expanded-state/tokenHistory/TokenHistory.js
+++ src/components/expanded-state/tokenHistory/TokenHistory.js
@@ -1,70 +1,58 @@
-import React, { 
-  useEffect, 
-  useMemo, 
-  useState, 
-  useCallback 
-} from 'react';
-import { 
-  FlatList, 
-  View,
-} from "react-native";
-import { ButtonPressAnimation } from '../../animations'; 
-import { 
-  Column, 
-  Row 
-} from '../../layout';
+import MaskedView from '@react-native-community/masked-view';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { FlatList, View } from 'react-native';
+import RadialGradient from 'react-native-radial-gradient';
+import styled from 'styled-components';
+import { ButtonPressAnimation } from '../../animations';
+import { Column, Row } from '../../layout';
 import { Text } from '../../text';
 import TokenHistoryEdgeFade from './TokenHistoryEdgeFade';
 import TokenHistoryLoader from './TokenHistoryLoader';
+import { useTheme } from '@rainbow-me/context/ThemeContext';
 import { apiGetTokenHistory } from '@rainbow-me/handlers/opensea-api';
 import { getHumanReadableDateWithoutOn } from '@rainbow-me/helpers/transactions';
 import { useAccountProfile, useDimensions } from '@rainbow-me/hooks';
 import { useNavigation } from '@rainbow-me/navigation';
-import { useTheme } from '@rainbow-me/context/ThemeContext';
-import MaskedView from '@react-native-community/masked-view';
 import Routes from '@rainbow-me/routes';
-import RadialGradient from 'react-native-radial-gradient';
-import styled from 'styled-components';
 import logger from 'logger';
 
 const EventEnum = {
   DELIST: {
-    type: 'cancelled',
-    label: `Delisted`,
     icon: `􀎩`,
+    label: `Delisted`,
+    type: 'cancelled',
   },
   ENS: {
-    type: 'ens-registration',
-    label: `Registered`,
     icon: `􀈐`,
+    label: `Registered`,
+    type: 'ens-registration',
   },
   LIST: {
-    type: 'created',
-    label: `Listed for `,
     icon: `􀎧`,
+    label: `Listed for `,
+    type: 'created',
   },
   MINT: {
-    type: 'mint',
-    label: `Minted`,
     icon: `􀎛`,
+    label: `Minted`,
+    type: 'mint',
   },
   SALE: {
-    type: 'successful',
-    label: `Sold for `,
     icon: `􀋢`,
+    label: `Sold for `,
+    type: 'successful',
   },
   TRANSFER: {
-    type: 'transfer',
-    label: `Sent to `,
     icon: `􀈠`,
+    label: `Sent to `,
+    type: 'transfer',
   },
-
-}
+};
 
 const Gradient = styled(RadialGradient).attrs(
   ({ color, theme: { colors } }) => ({
     center: [0, 0],
-    colors:  [colors.whiteLabel, color]
+    colors: [colors.whiteLabel, color],
   })
 )`
   position: absolute;
@@ -90,7 +78,7 @@ const DateRow = styled(Row)`
 
 const EmptyView = styled(View)`
   height: 3;
-  marginTop: 4;
+  margin-top: 4;
 `;
 
 const LeftSpacer = styled(View)`
@@ -108,54 +96,46 @@ const LineView = styled(View)`
   right: 6;
 `;
 
-const Container = styled(View)`
-  width: 100%;
-`;
+const AccentText = styled(Text).attrs({
+  size: 'smedium',
+  weight: 'heavy',
+})``;
 
-const ShortListContainer = styled(View)`
-  margin-left: 24;
-`;
-
-const TokenHistory = ({ 
-    contractAndToken,
-    color
-  }) => {
-
+const TokenHistory = ({ contractAndToken, color }) => {
   const [tokenHistory, setTokenHistory] = useState([]);
   const [tokenHistoryShort, setTokenHistoryShort] = useState(false);
-  const [contractAddress, setContractAddress] = useState("");
-  const [tokenID, setTokenID] = useState("");
+  const [contractAddress, setContractAddress] = useState('');
+  const [tokenID, setTokenID] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const { colors } = useTheme();
   const { width } = useDimensions();
   const { accountAddress, accountENS } = useAccountProfile();
   const { navigate } = useNavigation();
 
-  useEffect(async() => {
-    const tokenInfoArray = contractAndToken.split("/")
+  useEffect(async () => {
+    const tokenInfoArray = contractAndToken.split('/');
     setContractAddress(tokenInfoArray[0]);
     setTokenID(tokenInfoArray[1]);
   });
 
   //Query opensea using the contract address + tokenID
-  useEffect(async() => {
+  useEffect(async () => {
     await apiGetTokenHistory(contractAddress, tokenID)
-    .then(results => {
-      setTokenHistory(results);
-      if (results.length <= 2) {
-        setTokenHistoryShort(true);
-      }
-    })
-    .then(() => {
-      setIsLoading(false);
-    })
+      .then(results => {
+        setTokenHistory(results);
+        if (results.length <= 2) {
+          setTokenHistoryShort(true);
+        }
+      })
+      .then(() => {
+        setIsLoading(false);
+      });
   }, [contractAddress, tokenID]);
 
-  const handlePress = useCallback(
-    (address) => {
-      navigate(Routes.SHOWCASE_SHEET, {
-        address: address,
-      });
+  const handlePress = useCallback(address => {
+    navigate(Routes.SHOWCASE_SHEET, {
+      address: address,
+    });
   });
 
   //TODO: Memoize This
@@ -166,7 +146,7 @@ const TokenHistory = ({
     let suffix;
     let suffixSymbol = `􀆊`;
     let isClickable = false;
-    
+
     if (index == 0) {
       isFirst = true;
     }
@@ -176,181 +156,185 @@ const TokenHistory = ({
         symbol = EventEnum.DELIST.icon;
         phrase = EventEnum.DELIST.label;
         suffix = ``;
-        return renderHistoryDescription({ symbol, phrase, item, suffix, isFirst, isClickable, suffixSymbol });
+        return renderHistoryDescription({
+          isClickable,
+          isFirst,
+          item,
+          phrase,
+          suffix,
+          suffixSymbol,
+          symbol,
+        });
 
       case EventEnum.ENS.type:
         symbol = EventEnum.ENS.icon;
         phrase = EventEnum.ENS.label;
         suffix = ``;
-        return renderHistoryDescription({ symbol, phrase, item, suffix, isFirst, isClickable, suffixSymbol });
+        return renderHistoryDescription({
+          isClickable,
+          isFirst,
+          item,
+          phrase,
+          suffix,
+          suffixSymbol,
+          symbol,
+        });
 
       case EventEnum.LIST.type:
         symbol = EventEnum.LIST.icon;
         phrase = EventEnum.LIST.label;
         suffix = `${item.list_amount} ETH`;
-        return renderHistoryDescription({ symbol, phrase, item, suffix, isFirst, isClickable, suffixSymbol });
+        return renderHistoryDescription({
+          isClickable,
+          isFirst,
+          item,
+          phrase,
+          suffix,
+          suffixSymbol,
+          symbol,
+        });
 
       case EventEnum.MINT.type:
-          symbol = EventEnum.MINT.icon;
-          phrase = EventEnum.MINT.label;
-          suffix = ``;
-          return renderHistoryDescription({ symbol, phrase, item, suffix, isFirst, isClickable, suffixSymbol });
-  
+        symbol = EventEnum.MINT.icon;
+        phrase = EventEnum.MINT.label;
+        suffix = ``;
+        return renderHistoryDescription({
+          isClickable,
+          isFirst,
+          item,
+          phrase,
+          suffix,
+          suffixSymbol,
+          symbol,
+        });
+
       case EventEnum.SALE.type:
         symbol = EventEnum.SALE.icon;
         phrase = EventEnum.SALE.label;
         suffix = `${item.sale_amount} ETH`;
-        return renderHistoryDescription({ symbol, phrase, item, suffix, isFirst, isClickable, suffixSymbol });
+        return renderHistoryDescription({
+          isClickable,
+          isFirst,
+          item,
+          phrase,
+          suffix,
+          suffixSymbol,
+          symbol,
+        });
 
       case EventEnum.TRANSFER.type:
         symbol = EventEnum.TRANSFER.icon;
         phrase = EventEnum.TRANSFER.label;
         suffix = `${item.to_account} `;
-        if (accountAddress.toLowerCase() != item.to_account_eth_address && accountENS != item.to_account) {
+        if (
+          accountAddress.toLowerCase() != item.to_account_eth_address &&
+          accountENS != item.to_account
+        ) {
           isClickable = true;
-        }
-        else {
+        } else {
           isClickable = false;
         }
-        return renderHistoryDescription({ symbol, phrase, item, suffix, isFirst, isClickable, suffixSymbol });
+        return renderHistoryDescription({
+          isClickable,
+          isFirst,
+          item,
+          phrase,
+          suffix,
+          suffixSymbol,
+          symbol,
+        });
     }
-  }
+  };
 
-  const renderHistoryDescription = ({ symbol, phrase, item, suffix, isFirst, isClickable, suffixSymbol }) => {
-    const date = getHumanReadableDateWithoutOn(new Date(item.created_date).getTime()/1000);
+  const renderHistoryDescription = ({
+    symbol,
+    phrase,
+    item,
+    suffix,
+    isFirst,
+    isClickable,
+    suffixSymbol,
+  }) => {
+    const date = getHumanReadableDateWithoutOn(
+      new Date(item.created_date).getTime() / 1000
+    );
 
     return (
-        <Column>
-          <GradientRow> 
-            <Gradient color={color} />
-            { isFirst ? <EmptyView /> : <LineView color={color} /> }
-          </GradientRow>
-          
-          <InnerColumn>
-            <DateRow>
-              <Text
-                align="left"
-                color={color}
-                size="smedium"
-                weight="heavy"
-              >
-              {date}     
-              </Text>
-            </DateRow>
+      <Column>
+        <GradientRow>
+          <Gradient color={color} />
+          {isFirst ? <EmptyView /> : <LineView color={color} />}
+        </GradientRow>
 
-            {
-              isClickable ? 
-              <ButtonPressAnimation
-                hapticType={'selection'}
-                onPress={() => handlePress(item.to_account_eth_address)}
-                scaleTo={0.92}
-              >
-                <Row>
-                  <Text
-                    align="left"
-                    color={color}
-                    size="smedium"
-                    weight="heavy"
-                  >
-                    {symbol}
-                  </Text>
+        <InnerColumn>
+          <Row style = {{ marginBottom: 3 }}>
+            <AccentText color={color}>
+              {date}
+            </AccentText>
+          </Row>
 
-                  <Text
-                    align="right"
-                    color={colors.whiteLabel}
-                    size="smedium"
-                    weight="heavy"
-                  >
-                    {' '}{phrase}{suffix}{suffixSymbol}
-                  </Text>
-                </Row>
-              </ButtonPressAnimation>
-              : 
+            <ButtonPressAnimation
+              hapticType="selection"
+              disabled={!isClickable}
+              onPress={() => handlePress(item.to_account_eth_address)}
+              scaleTo={0.92}
+            >
               <Row>
-                <Text
-                  align="left"
-                  color={color}
-                  size="smedium"
-                  weight="heavy"
-                >
+                <AccentText color={color}>
                   {symbol}
-                </Text>
+                </AccentText>
 
-                <Text
-                  align="right"
-                  color={colors.whiteLabel}
-                  size="smedium"
-                  weight="heavy"
-                >
-                  {' '}{phrase}{suffix}
-                </Text>
+
+                <AccentText color={colors.whiteLabel}>
+                  {' '}
+                  {phrase}
+                  {suffix}
+                  {isClickable ? suffixSymbol : ''}
+                </AccentText>
+                  
               </Row>
-            }
-          </InnerColumn>
-        </Column>
-    )
-  }
+            </ButtonPressAnimation>
+          
+        </InnerColumn>
+      </Column>
+    );
+  };
 
   const renderTwoOrLessDataItems = () => {
-    if (tokenHistory.length == 1) {
-      return (
-        <ShortListContainer>
-          <Row>
-            <Column>
-              { renderItem({item: tokenHistory[0], index: 0}) }
-            </Column>
-          </Row>
-      </ShortListContainer>
-        
-      )
-    }
-    else if (tokenHistory.length == 2) {
-      return (
-        <ShortListContainer>
-          <Row>
-            <Column>
-              { renderItem({item: tokenHistory[1], index: 1}) }
-            </Column>
-            <Column>
-              { renderItem({item: tokenHistory[0], index: 0}) }
-            </Column>
-          </Row>
-        </ShortListContainer>
-      )
-    }
+    return (
+      <View style={{ marginLeft: 24 }}>
+        <Row>
+          { tokenHistory.length === 2 ? <Column>{renderItem({ index: 1, item: tokenHistory[1] })} </Column> : <View /> }
+          <Column>{renderItem({ index: 0, item: tokenHistory[0] })} </Column>
+        </Row>
+      </View>
+    );
   };
 
   const renderFlatlist = () => {
     return (
-      <Container>
-        <MaskedView
-          maskElement={<TokenHistoryEdgeFade />}
-        > 
+      <View>
+        <MaskedView maskElement={<TokenHistoryEdgeFade />}>
           <FlatList
+            ListFooterComponent={<LeftSpacer />}
             data={tokenHistory}
-            renderItem={({item, index}) => renderItem({ item, index })}
-            horizontal={true}
-            inverted={true}
-            ListFooterComponent={ <LeftSpacer /> }
+            horizontal
+            inverted
+            renderItem={({ item, index }) => renderItem({ index, item })}
             showsHorizontalScrollIndicator={false}
           />
         </MaskedView>
-      </Container>
-    )
-  }
-  
+      </View>
+    );
+  };
+
   return (
-    <Container>
+    <View>
       {isLoading && <TokenHistoryLoader />}
-      {!isLoading && 
-      (
-        tokenHistoryShort ?
-        renderTwoOrLessDataItems() :
-        renderFlatlist()
-      )}
-    </Container>
-  )
-}
+      {!isLoading &&
+        (tokenHistoryShort ? renderTwoOrLessDataItems() : renderFlatlist())}
+    </View>
+  );
+};
 
 export default TokenHistory;
-
