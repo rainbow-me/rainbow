7fa4d6ddc59be2fc7fcc54af70e71ac055554e35
Marco Burstein <marco@marco.how>
2021-11-04 11:29:10 -0400

    Update `NativeCurrencyKeysMap` to use index signature key as value.
    Co-authored-by: Mark Dalgleish <mark.john.dalgleish@gmail.com>

--- src/entities/nativeCurrencyTypes.ts
+++ src/entities/nativeCurrencyTypes.ts
@@ -1,16 +1,16 @@
+import { mapValues } from 'lodash';
 import nativeCurrencyReference from '../references/native-currencies.json';
 
 type NativeCurrencyKey = keyof typeof nativeCurrencyReference;
-type NativeCurrencyKeysMap = { [key in NativeCurrencyKey]: NativeCurrencyKey };
+type NativeCurrencyKeysMap = { [Key in NativeCurrencyKey]: Key };
 
 // We can't dynamically generate an enum from the JSON data, but we can
 // generate this type-checked object, which is nicer than using strings directly.
-const nativeKeys: Partial<NativeCurrencyKeysMap> = {};
-for (let key of Object.keys(nativeCurrencyReference) as NativeCurrencyKey[]) {
-  nativeKeys[key] = key;
-}
 
 /**
  * An enum of native currencies such as "USD" or "ETH".
  */
-export const NativeCurrencyKeys: NativeCurrencyKeysMap = nativeKeys as NativeCurrencyKeysMap;
+export const NativeCurrencyKeys = mapValues(
+  nativeCurrencyReference,
+  (_value, key) => key
+) as NativeCurrencyKeysMap;
