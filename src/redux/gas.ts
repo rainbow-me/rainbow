de84e2d02c889fff3d86d39e72b7ba01c676083a
Esteban Miño <efmino@uc.cl>
2021-11-04 17:38:40 -0400

    logs

--- src/redux/gas.ts
+++ src/redux/gas.ts
@@ -181,7 +181,6 @@ export const gasUpdateToCustomGasFee = (gasParams: GasFeeParams) => async (
   const { assets } = getState().data;
   const { nativeCurrency } = getState().settings;
   const _gasLimit = gasLimit || defaultGasLimit;
-  console.log('---------------------- gasParams', gasParams);
   const nativeTokenPriceUnit =
     txNetwork !== networkTypes.polygon
       ? ethereumUtils.getEthPriceUnit()
@@ -338,7 +337,6 @@ export const gasPricesStartPolling = (network = networkTypes.mainnet) => async (
               currentBaseFee,
               confirmationTimeByPriorityFee,
             } = await getEIP1559GasParams();
-            console.log('existingGasFees[CUSTOM]', existingGasFees[CUSTOM]);
             if (!isEmpty(existingGasFees[CUSTOM])) {
               // Preserve custom values while updating prices
               gasFeeParamsBySpeed[CUSTOM] = {
