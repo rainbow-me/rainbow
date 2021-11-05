4df46c94302f3a89126cbf98420f8b4c32c2443f
DerHowie <derhowiedesigns@gmail.com>
2021-11-05 13:56:23 -0400

    finish nonceManager localstorage entities

--- src/handlers/localstorage/nonceManager.ts
+++ src/handlers/localstorage/nonceManager.ts
@@ -3,16 +3,22 @@ import { getGlobal, saveGlobal } from './common';
 export const NONCE_MANAGER = 'nonceManager';
 
 type NetworkId = string;
+type AccountId = string;
 interface NetworkNonceInfo {
   nonce: string;
 }
-type AccountNonceInfo = Record<NetworkId, NetworkNonceInfo>;
+interface AccountNonceInfo {
+  [key: NetworkId]: NetworkNonceInfo;
+}
+interface NonceManager {
+  [key: AccountId]: AccountNonceInfo;
+}
 
-export const getNonceManager = async () => {
+export const getNonceManager = async (): Promise<NonceManager> => {
   const nonceManager = await getGlobal(NONCE_MANAGER, []);
 
   return nonceManager;
 };
 
-export const saveNonceManager = (nonceManager: AccountNonceInfo) =>
+export const saveNonceManager = (nonceManager: NonceManager) =>
   saveGlobal(NONCE_MANAGER, nonceManager);
