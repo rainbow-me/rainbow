cfe36d31683ea41bc73617cf9995f6abf03f7f25
Esteban Miño <efmino@uc.cl>
2021-11-15 17:16:05 +0100

    rm unused

--- src/handlers/gasFees.js
+++ src/handlers/gasFees.js
@@ -1,32 +1,4 @@
-import {
-  ETH_GAS_STATION_API_KEY,
-  ETHERSCAN_API_KEY,
-} from 'react-native-dotenv';
 import { RainbowFetchClient } from '../rainbow-fetch';
-
-/**
- * Configuration for defipulse API
- * @type RainbowFetchClient instance
- */
-const ethGasstationApi = new RainbowFetchClient({
-  baseURL: 'https://data-api.defipulse.com',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  },
-  timeout: 30000, // 30 secs
-});
-
-/**
- * @desc get ethereum gas prices
- * @return {Promise}
- */
-export const ethGasStationGetGasPrices = () =>
-  ethGasstationApi.get(`/api/v1/egs/api/ethgasAPI.json`, {
-    params: {
-      'api-key': ETH_GAS_STATION_API_KEY,
-    },
-  });
 
 const rainbowMeteorologyApi = new RainbowFetchClient({
   baseURL: 'https://metadata.p.rainbow.me',
@@ -59,32 +31,6 @@ const polygonGasStationApi = new RainbowFetchClient({
  */
 export const polygonGasStationGetGasPrices = () =>
   polygonGasStationApi.get(`/gasapi.ashx?apikey=key&method=gasoracle`);
-
-/**
- * Configuration for Etherscan API
- * @type RainbowFetchClient instance
- */
-const etherscanAPI = new RainbowFetchClient({
-  baseURL: 'https://api.etherscan.io',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  },
-  timeout: 30000, // 30 secs
-});
-
-/**
- * @desc get ethereum gas prices from Etherscan
- * @return {Promise}
- */
-export const etherscanGetGasPrices = () =>
-  etherscanAPI.get(`/api`, {
-    params: {
-      action: 'gasoracle',
-      apikey: ETHERSCAN_API_KEY,
-      module: 'gastracker',
-    },
-  });
 
 /**
  * @desc get Polygon time estimates
