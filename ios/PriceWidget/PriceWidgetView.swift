0f47479adb4cfbe9c3efaf8f2cb20b98c14f9079
osdnk <micosa97@gmail.com>
2021-11-24 02:23:47 +0200

    restore swift_vibrant

--- ios/PriceWidget/PriceWidgetView.swift
+++ ios/PriceWidget/PriceWidgetView.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SwiftUI
 import UIKit
+import swift_vibrant
 
 @available(iOS 14.0, *)
 struct PriceWidgetView: View {
@@ -78,8 +79,8 @@ struct PriceWidgetView: View {
     if (tokenData.tokenDetails != nil) {
       if let color = tokenData.tokenDetails!.color {
         return hexStringToColor(hex: color)
-      } else if let palette = tokenData.icon?.getColors() {
-        return SwiftUI.Color(palette.primary)
+      } else if let icon = tokenData.icon, let palette = Vibrant.from(icon).getPalette().Vibrant {
+        return SwiftUI.Color(palette.uiColor)
       }
     }
     return Color(red:0.15, green:0.16, blue:0.18)
