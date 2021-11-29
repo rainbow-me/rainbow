9fb82ad7cbc3e0157e094e95bd17444ffc452e17
Terry Sahaidak <s_tasssik_taras@ukr.net>
2021-11-29 19:55:32 +0200

    Temporary remove typings from withReanimatedFallback

--- src/react-native-animated-charts/src/helpers/withReanimatedFallback.tsx
+++ src/react-native-animated-charts/src/helpers/withReanimatedFallback.tsx
@@ -1,14 +1,15 @@
+// @ts-nocheck
 import React from 'react';
 import { Text, TurboModuleRegistry } from 'react-native';
 
 function ChartFallback() {
-  return <Text>Charts</Text>;
+  return <Text>Charts are not available without Reanimated 2</Text>;
 }
 
 export default function withReanimatedFallback<T extends React.ComponentType>(
   ChartComponent: T,
   showText = false
-): T | () => null {
+): T {
   return !TurboModuleRegistry.get('NativeReanimated') &&
     (!global.__reanimatedModuleProxy ||
       global.__reanimatedModuleProxy.__shimmed)
