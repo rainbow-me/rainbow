86ea83a764165b86aa0735ce22a90bc69f6da38a
Esteban Miño <efmino@uc.cl>
2021-12-01 22:44:36 -0300

    Revert "Revert "EIP-1559 (#2422)" (#2605)"
    This reverts commit 3f306961787b0d9853500c0a1f2d079a7bff1cd8.

--- src/entities/gas.ts
+++ src/entities/gas.ts
@@ -1,10 +1,129 @@
-export interface TxFee {
+type Numberish = number | string;
+
+export interface Fee {
   native: { value: { amount: string; display: string } };
   value: { amount: string; display: { amount: string; display: string } };
 }
-export interface SelectedGasPrice {
-  estimatedTime: { amount: string; display: string };
+
+export interface GasFeeParam {
+  amount: string;
+  display: string;
+  gwei: string;
+}
+
+export interface SelectedGasFee {
+  estimatedTime: { amount: number; display: string };
   option: string;
-  txFee: TxFee;
-  value: { amount: string; display: string };
+  gasFee: GasFee;
+  gasFeeParams: GasFeeParams;
+}
+
+export interface LegacySelectedGasFee {
+  estimatedTime: { amount: number; display: string };
+  option: string;
+  gasFee: LegacyGasFee;
+  gasFeeParams: LegacyGasFeeParams;
+}
+
+export interface TransactionGasParams {
+  maxFeePerGas: GasFeeParam;
+  maxPriorityFeePerGas: GasFeeParam;
+}
+
+export interface LegacyTransactionGasParams {
+  gasPrice: GasFeeParam;
+}
+
+export interface GasFeeParams {
+  maxFeePerGas: GasFeeParam;
+  maxPriorityFeePerGas: GasFeeParam;
+  option: string;
+  estimatedTime: { amount: number; display: string };
+}
+
+export interface LegacyGasFeeParams {
+  gasPrice: GasFeeParam;
+  option: string;
+  estimatedTime: { amount: number; display: string };
+}
+
+export interface LegacyGasFee {
+  estimatedFee: Fee;
+}
+
+export interface GasFee extends LegacyGasFee {
+  maxFee: Fee;
+}
+
+export interface GasFeeParamsBySpeed {
+  [key: string]: GasFeeParams;
+}
+
+export interface LegacyGasFeeParamsBySpeed {
+  [key: string]: LegacyGasFeeParams;
+}
+
+export interface GasFeesBySpeed {
+  [key: string]: GasFee;
+}
+
+export interface LegacyGasFeesBySpeed {
+  [key: string]: LegacyGasFee;
+}
+
+export interface CurrentBlockParams {
+  baseFeePerGas: GasFeeParam;
+  trend: number;
+}
+
+// API
+
+export interface GasPricesAPIData {
+  average: Numberish;
+  avgWait: Numberish;
+  fast: Numberish;
+  fastWait: Numberish;
+  safeLow: Numberish;
+  safeLowWait: Numberish;
+  fastest: Numberish;
+  fastestWait: Numberish;
+}
+
+export interface GasFeesPolygonGasStationData {
+  status: string;
+  message: string;
+  result: {
+    LastBlock: string;
+    SafeGasPrice: string;
+    ProposeGasPrice: string;
+    FastGasPrice: string;
+    UsdPrice: string;
+  };
+}
+
+export interface ConfirmationTimeByPriorityFee {
+  15: string;
+  30: string;
+  45: string;
+  60: string;
+}
+
+export interface MaxPriorityFeeSuggestions {
+  fast: string;
+  normal: string;
+  urgent: string;
+}
+
+export interface RainbowMeteorologyData {
+  data: {
+    currentBaseFee: string;
+    baseFeeSuggestion: string;
+    baseFeeTrend: number;
+    confirmationTimeByPriorityFee: ConfirmationTimeByPriorityFee;
+    maxPriorityFeeSuggestions: MaxPriorityFeeSuggestions;
+  };
+  meta: {
+    blockNumber: number;
+    provider: string;
+  };
 }
