226b3850b578bddf6c60f210c8f3e5970d2f53c1
Bruno Barbieri <brunobar79@gmail.com>
2021-12-07 23:16:48 -0500

    [ts-migrate][.] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- src/config/experimentalHooks.ts
+++ src/config/experimentalHooks.ts
@@ -2,11 +2,14 @@ import { useContext } from 'react';
 import { defaultConfig } from './experimental';
 import { RainbowContext } from '@rainbow-me/helpers/RainbowContext';
 
-const useExperimentalFlag = name => {
+const useExperimentalFlag = (name: any) => {
+  // @ts-expect-error ts-migrate(2304) FIXME: Cannot find name 'IS_DEV'.
   if (IS_DEV) {
+    // @ts-expect-error ts-migrate(2339) FIXME: Property 'config' does not exist on type '{}'.
     // eslint-disable-next-line react-hooks/rules-of-hooks
     return useContext(RainbowContext).config[name];
   } else {
+    // @ts-expect-error ts-migrate(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
     return defaultConfig[name];
   }
 };
