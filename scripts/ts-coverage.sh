1f0df285dc4149c9b75d8c10ac9a1cc6c19ecbc1
Bruno Barbieri <brunobar79@gmail.com>
2021-12-08 03:26:32 -0500

    fix ts-coverage config

--- scripts/ts-coverage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-typescript-coverage-report -t 99 --ignore-files 'config/**/*.*' --ignore-files 'android/**/*.*' --ignore-files 'ios/**/*.*' --ignore-files 'e2e/**/*.*'  --ignore-files 'rainbow-scripts/**/*.*'  --ignore-files 'android/**/*.*'
