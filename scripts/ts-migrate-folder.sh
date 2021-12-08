eaa4f9071cd26dd9fe8c5c9c961bf89cbfb052a8
Bruno Barbieri <brunobar79@gmail.com>
2021-12-07 23:00:07 -0500

    add deps and scripts

---
+++ scripts/ts-migrate-folder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eo pipefail
+
+FOLDER=$1
+
+echo "Migrating folder $FOLDER...";
+
+npx ts-migrate-full . / 
+    --sources="$FOLDER/**/*" 
+    --sources="node_modules/**/*.d.ts" 
