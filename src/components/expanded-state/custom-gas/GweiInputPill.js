627f8ca05d609c874eca4477878273eb4aef0081
Esteban Miño <efmino@uc.cl>
2021-12-13 18:30:41 -0300

    fee panels input sanitize numbers and fix focus (#2675)
    * sanitize numbers
    
    * comma
    
    * use TextInputMask instead
    
    * 2026
    
    * update mask
    
    * fix input poill style

--- src/components/expanded-state/custom-gas/GweiInputPill.js
+++ src/components/expanded-state/custom-gas/GweiInputPill.js
@@ -1,42 +1,43 @@
 import React from 'react';
 import LinearGradient from 'react-native-linear-gradient';
+import TextInputMask from 'react-native-text-input-mask';
 import styled from 'styled-components';
 import { ButtonPressAnimation } from '../../animations';
-import { Input } from '../../inputs';
-import { Row } from '../../layout';
 import { Text } from '../../text';
-import { margin, padding } from '@rainbow-me/styles';
+import { buildTextStyles, margin, padding } from '@rainbow-me/styles';
 
 const ANDROID_EXTRA_LINE_HEIGHT = 6;
 
 const GweiPill = styled(LinearGradient).attrs(({ theme: { colors } }) => ({
   colors: colors.gradients.lighterGrey,
   end: { x: 0.5, y: 1 },
+  flexDirection: 'row',
   start: { x: 0, y: 0 },
 }))`
   border-radius: 15;
   ${padding(10, 12)}
   ${margin(0, 6)}
-  min-width: 105;
 `;
 
-const GweiNumberInput = styled(Input).attrs(({ theme: { colors }, value }) => ({
-  color: !value && colors.grey,
-  interval: 1,
-  letterSpacing: 'roundedTight',
-  size: 'lmedium',
-  steps: 1,
-  textAlign: 'center',
-  timing: 'linear',
-  weight: 'heavy',
-}))`
-  ${padding(0, 0, 0, 0)}
+const GweiNumberInput = styled(TextInputMask).attrs(
+  ({ theme: { colors }, value }) => ({
+    allowFontScaling: false,
+    color: !value && colors.grey,
+    keyboardType: 'decimal-pad',
+    letterSpacing: 'roundedTight',
+    size: 'lmedium',
+    textAlign: 'center',
+    weight: 'heavy',
+  })
+)`
+  ${buildTextStyles};
+  ${padding(0, 0, 0, 0)};
   ${margin(
     android ? -ANDROID_EXTRA_LINE_HEIGHT : 0,
     0,
     android ? -ANDROID_EXTRA_LINE_HEIGHT : 0,
     0
-  )}
+  )};
 `;
 
 const GweiLabel = styled(Text).attrs(() => ({
@@ -60,21 +61,20 @@ function GweiInputPill(
   return (
     <ButtonPressAnimation onPress={onPress}>
       <GweiPill>
-        <Row alignSelf="center">
-          <GweiNumberInput
-            keyboardType="numeric"
-            onBlur={onBlur}
-            onChange={onChange}
-            onFocus={onFocus}
-            placeholder="0"
-            placeholderTextColor={colors.alpha(colors.blueGreyDark, 0.4)}
-            ref={ref}
-            selectionColor={color}
-            testID={testID}
-            value={!!value && `${value}`}
-          />
-          <GweiLabel> Gwei</GweiLabel>
-        </Row>
+        <GweiNumberInput
+          keyboardType="numeric"
+          mask="[9999].[999]"
+          onBlur={onBlur}
+          onChange={onChange}
+          onFocus={onFocus}
+          placeholder="0"
+          placeholderTextColor={colors.alpha(colors.blueGreyDark, 0.4)}
+          ref={ref}
+          selectionColor={color}
+          testID={testID}
+          value={!!value && `${value}`}
+        />
+        <GweiLabel> Gwei</GweiLabel>
       </GweiPill>
     </ButtonPressAnimation>
   );
