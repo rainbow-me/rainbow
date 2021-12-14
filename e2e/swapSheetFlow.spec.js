541ea26007f7b2ef388e2d3528e64794de3fd3a0
Esteban Miño <efmino@uc.cl>
2021-12-13 21:58:37 -0300

    e2e

--- e2e/swapSheetFlow.spec.js
+++ e2e/swapSheetFlow.spec.js
@@ -373,10 +373,11 @@ describe('Swap Sheet Interaction Flow', () => {
     await Helpers.checkIfElementByTextToExist('High miner tip!');
     await Helpers.tapByText('Edit Miner Tip');
     await Helpers.waitAndTap('speed-pill-normal');
-    await Helpers.clearField('max-base-fee-input');
-    await Helpers.typeText('max-base-fee-input', '200\n', false);
-    await Helpers.clearField('max-priority-fee-input');
-    await Helpers.typeText('max-priority-fee-input', `2\n`, false);
+
+    // await Helpers.clearField('max-base-fee-input');
+    // await Helpers.typeText('max-base-fee-input', '200\n', false);
+    // await Helpers.clearField('max-priority-fee-input');
+    // await Helpers.typeText('max-priority-fee-input', `2\n`, false);
     await Helpers.waitAndTap('speed-pill-normal');
   });
 
