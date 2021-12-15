805710a1a9e30cf91eb9e73974944172e99a23a5
Terry Sahaidak <tasssik@gmail.com>
2021-12-15 21:39:57 +0200

    Use new charts (#2596)
    * Wip: Add new charts code
    
    * Temporary remove typings from withReanimatedFallback
    
    * Make it work on android
    
    * Fix iOS
    
    * Refactor to move everything to path provider
    
    * Fix extremes labels
    
    * Fixes
    
    * FINALLY I FOUND IT
    
    * Fix ratio flickering
    
    * Fix extreme lable postion
    
    * More fixes
    
    * Address some review comments and bugs
    
    * More fixes
    
    * Remove logs
    
    * Respect nativePoints if exist
    
    * Add new yarn lock
    
    * Fix yarn lock
    
    * Fix spacing issues
    
    * Fix some times to make linter happy
    
    * Fix eslint
    
    * Make CI happy
    
    * Make linter happier
    
    * Fix default values
    
    * Update src/react-native-animated-charts/src/helpers/ChartContext.ts
    
    Co-authored-by: Michał Osadnik <micosa97@gmail.com>
    
    * Update src/react-native-animated-charts/src/charts/linear/ChartPath.tsx
    
    Co-authored-by: Michał Osadnik <micosa97@gmail.com>
    
    * Update src/components/value-chart/Chart.js
    
    Co-authored-by: Michał Osadnik <micosa97@gmail.com>
    
    * Update src/components/expanded-state/chart/chart-data-labels/ChartChangeDirectionArrow.js
    
    * More review feedback fixes
    
    * Fix interpolation animation glitch twiggered by deps change
    
    * Happy linter unhappy devs
    
    * Update src/react-native-animated-charts/src/charts/linear/ChartPath.tsx
    
    Co-authored-by: Michał Osadnik <micosa97@gmail.com>
    
    * Address more feedback
    
    * Even more feedback
    
    * More review fixes
    
    Co-authored-by: Michał Osadnik <micosa97@gmail.com>

---
+++ src/react-native-animated-charts/src/charts/linear/ChartDot.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { ViewProps } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
+import { useChartData } from '../../helpers/useChartData';
+import { FIX_CLIPPED_PATH_MAGIC_NUMBER } from './ChartPath';
+
+interface ChartDotProps extends ViewProps {
+  size?: number;
+  springConfig?: Animated.WithSpringConfig;
+}
+
+const springDefaultConfig = {
+  damping: 15,
+  mass: 1,
+  stiffness: 600,
+};
+
+const ChartDot = React.memo(
+  ({ style, size = 10, springConfig, ...props }: ChartDotProps) => {
+    const { isActive, positionX, positionY } = useChartData();
+
+    const animatedStyle = useAnimatedStyle(() => {
+      const translateX = positionX.value;
+      const translateY = positionY.value + FIX_CLIPPED_PATH_MAGIC_NUMBER / 2;
+      const animation = withSpring(
+        isActive.value ? 1 : 0,
+        springConfig || springDefaultConfig
+      );
+
+      return {
+        opacity: animation,
+        transform: [
+          { translateX },
+          { translateY },
+          {
+            scale: animation,
+          },
+        ],
+      };
+    }, [size]);
+
+    return (
+      <Animated.View
+        {...props}
+        pointerEvents="none"
+        style={[
+          {
+            borderRadius: size / 2,
+            height: size,
+            left: -size / 2,
+            position: 'absolute',
+            top: -size / 2,
+            width: size,
+          },
+          animatedStyle,
+          style,
+        ]}
+      />
+    );
+  }
+);
+
+ChartDot.displayName = 'ChartDot';
+
+export default ChartDot;
