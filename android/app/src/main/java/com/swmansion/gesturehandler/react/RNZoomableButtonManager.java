9ef1d8e67048ae82d9051e60f392bb3ae0919b03
Esteban Miño <efmino@uc.cl>
2021-12-15 18:06:15 -0300

    Update android/app/src/main/java/com/swmansion/gesturehandler/react/RNZoomableButtonManager.java
    Co-authored-by: Terry Sahaidak <tasssik@gmail.com>

--- android/app/src/main/java/com/swmansion/gesturehandler/react/RNZoomableButtonManager.java
+++ android/app/src/main/java/com/swmansion/gesturehandler/react/RNZoomableButtonManager.java
@@ -1,6 +1,5 @@
 package com.swmansion.gesturehandler.react;
 import android.content.Context;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.animation.Animation;
 import android.view.animation.Interpolator;
