6e443a095596eb4e61572932efbf0cfe50b4e6d0
Michał Osadnik <micosa97@gmail.com>
2021-12-16 20:24:03 +0200

    Do not persist open small balances (#2711)
    * Do not persist open small balances
    
    * areOpenSmallBalancesAtom
    
    * remove unrel
    
    * remove unrel

--- src/hooks/useInitializeWallet.js
+++ src/hooks/useInitializeWallet.js
@@ -17,6 +17,7 @@ import useHideSplashScreen from './useHideSplashScreen';
 import useInitializeAccountData from './useInitializeAccountData';
 import useLoadAccountData from './useLoadAccountData';
 import useLoadGlobalEarlyData from './useLoadGlobalEarlyData';
+import useOpenSmallBalances from './useOpenSmallBalances';
 import useResetAccountState from './useResetAccountState';
 import { runKeychainIntegrityChecks } from '@rainbow-me/handlers/walletReadyEvents';
 import { additionalDataCoingeckoIds } from '@rainbow-me/redux/additionalAssetsData';
@@ -31,6 +32,7 @@ export default function useInitializeWallet() {
   const initializeAccountData = useInitializeAccountData();
   const { network } = useAccountSettings();
   const hideSplashScreen = useHideSplashScreen();
+  const { setIsSmallBalancesOpen } = useOpenSmallBalances();
 
   const initializeWallet = useCallback(
     async (
@@ -58,6 +60,8 @@ export default function useInitializeWallet() {
           await runMigrations();
           logger.sentry('done with migrations');
         }
+
+        setIsSmallBalancesOpen(false);
 
         // Load the network first
         await dispatch(settingsLoadNetwork());
