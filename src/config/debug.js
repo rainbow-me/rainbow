9b749996c78a07d1075a2ba672e5fb4072a37b0c
osdnk <micosa97@gmail.com>
2021-12-16 18:35:58 +0200

    Do not persist open small balances

---
+++ src/config/debug.js
@@ -0,0 +1,16 @@
+/**
+ * This fine contains flags for debugging related features that
+ * could be useful during development.
+ */
+
+export const disableCharts = false;
+export const enableActionsOnReadOnlyWallet = false;
+export const debugLayoutAnimations = false;
+export const forceFallbackProvider = false;
+export const alwaysRequireApprove = false;
+export const showReloadButton = false;
+export const showSwitchModeButton = false;
+export const parseAllTxnsOnReceive = false;
+export const reactNativeDisableYellowBox = true;
+export const showNetworkRequests = false;
+export const showNetworkResponses = false;
