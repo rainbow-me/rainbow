025faf7057ab1cff78c12c636faba6a38cf11fcb
osdnk <micosa97@gmail.com>
2021-12-16 20:15:17 +0200

    Reduce renderAheadOffset on ios

--- src/components/asset-list/RecyclerAssetList2/core/RawRecyclerList.tsx
+++ src/components/asset-list/RecyclerAssetList2/core/RawRecyclerList.tsx
@@ -50,7 +50,7 @@ const RawMemoRecyclerAssetList = React.memo(function RawRecyclerAssetList({
       onLayout={onLayout}
       ref={ref as LegacyRef<RecyclerListViewRef>}
       refreshControl={<RefreshControl />}
-      renderAheadOffset={2000}
+      renderAheadOffset={ios ? 700 : 2000}
       rowRenderer={rowRenderer}
     />
   );
