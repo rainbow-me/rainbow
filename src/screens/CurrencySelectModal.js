198608e880672d719cb75e3c00707d34c7b008e0
osdnk <micosa97@gmail.com>
2021-12-17 18:46:06 +0200

    Disable focusing on input on navigating to Currency Selection View from Swap flow

--- src/screens/CurrencySelectModal.js
+++ src/screens/CurrencySelectModal.js
@@ -78,7 +78,7 @@ export default function CurrencySelectModal() {
   const scrollPosition = usePagerPosition();
 
   const searchInputRef = useRef();
-  const { handleFocus } = useMagicAutofocus(searchInputRef, undefined, true);
+  const { handleFocus } = useMagicAutofocus(searchInputRef);
 
   const [assetsToFavoriteQueue, setAssetsToFavoriteQueue] = useState({});
   const [searchQuery, setSearchQuery] = useState('');
@@ -323,9 +323,12 @@ export default function CurrencySelectModal() {
               isFetching={loadingAllTokens}
               isSearching={isSearching}
               onChangeText={setSearchQuery}
-              onFocus={handleFocus}
               ref={searchInputRef}
               searchQuery={searchQuery}
+              {...(ios && {
+                onFocus: handleFocus,
+                ref: searchInputRef,
+              })}
               testID="currency-select-search"
             />
             {type === null || type === undefined ? null : (
