47e7b22eacec2aaf1648ae533acb86306096fe54
DerHowie <derhowiedesigns@gmail.com>
2021-12-18 17:44:45 -0500

    remove useUniswapSearch

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -107,5 +107,4 @@ export { default as useCurrentNonce } from './useCurrentNonce';
 export { default as usePersistentAspectRatio } from './usePersistentAspectRatio';
 export { default as usePersistentDominantColorFromImage } from './usePersistentDominantColorFromImage';
 export { default as useFeesPanelInputRefs } from './useFeesPanelInputRefs';
-export { default as useUniswapSearch } from './useUniswapSearch';
 export { default as useUniswapCurrencyList } from './useUniswapCurrencyList';
