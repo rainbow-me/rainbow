5901cb43f3ff056f4911e45f723e8451f9b7d36f
Christian Baroni <christianbaroni@me.com>
2021-12-20 23:30:01 -0500

    Revert "Remove redundant style"
    This reverts commit b5920c2f4775c7fc65a8e8f2696ee6de1b9c59f4.

--- src/components/coin-divider/CoinDividerOpenButton.js
+++ src/components/coin-divider/CoinDividerOpenButton.js
@@ -44,6 +44,10 @@ const LabelText = styled(Text).attrs(({ theme: { colors } }) => ({
   weight: 'bold',
 }))``;
 
+const buttonPressAnimationStyle = {
+  flexDirection: 'row',
+};
+
 const CoinDividerOpenButton = ({
   coinDividerHeight,
   isSmallBalancesOpen,
@@ -80,7 +84,7 @@ const CoinDividerOpenButton = ({
   }));
 
   return (
-    <ButtonPressAnimation onPress={onPress}>
+    <ButtonPressAnimation onPress={onPress} style={buttonPressAnimationStyle}>
       <Content height={coinDividerHeight} style={wrapperStyle}>
         <LabelText>{isSmallBalancesOpen ? 'Less' : 'All'}</LabelText>
         <Animated.View style={style}>
