e752a97cc7a044fb53daf17aaec364bbd9167101
Ben Goldberg <bengoldberg@rainbow.me>
2021-12-20 18:11:31 -0800

    remove old nonfunctional fix and add descriptive comment

--- src/components/asset-list/RecyclerAssetList2/core/useMemoBriefSectionData.ts
+++ src/components/asset-list/RecyclerAssetList2/core/useMemoBriefSectionData.ts
@@ -41,11 +41,8 @@ export default function useMemoBriefSectionData() {
           return false;
         }
 
+        // removes NFTS_HEADER if wallet doesn't have NFTs
         if (data.type === CellType.NFTS_HEADER && !arr[arrIndex + 2]) {
-          return false;
-        }
-
-        if (data.type === CellType.NFTS_HEADER && !arr[arrIndex + 1]) {
           return false;
         }
 
