cc1e7e23c6b1af3f34e4089773afdcd2ebd78134
Bruno Barbieri <brunobar79@gmail.com>
2021-12-22 15:27:00 -0500

    improvements (#2742)

--- src/handlers/dispersion.ts
+++ src/handlers/dispersion.ts
@@ -1,5 +1,9 @@
+// @ts-ignore
+// import { IS_TESTING } from 'react-native-dotenv';
 import { RainbowFetchClient } from '../rainbow-fetch';
 import { EthereumAddress, RainbowToken } from '@rainbow-me/entities';
+import UniswapAssetsCache from '@rainbow-me/utils/uniswapAssetsCache';
+import logger from 'logger';
 
 const dispersionApi = new RainbowFetchClient({
   // baseURL: 'https://metadata.p.rainbow.me',
@@ -20,8 +24,22 @@ export const getUniswapV2Pools = async (token?: EthereumAddress) => {
 export const getUniswapV2Tokens = async (
   addresses: EthereumAddress[]
 ): Promise<RainbowToken[]> => {
-  const res = await dispersionApi.post('/dispersion/v1/tokens/uniswap/v2', {
-    addresses,
-  });
-  return res?.data?.tokens;
+  logger.debug('====>>> getUniswapV2Tokens', addresses);
+
+  const key = addresses.join(',');
+  // @ts-ignore
+  if (UniswapAssetsCache[key]) {
+    // @ts-ignore
+    return UniswapAssetsCache[key];
+  } else {
+    // if (IS_TESTING === 'true') {
+    //   return [];
+    // }
+    const res = await dispersionApi.post('/dispersion/v1/tokens/uniswap/v2', {
+      addresses,
+    });
+    // @ts-ignore
+    UniswapAssetsCache[key] = [...res?.data?.tokens];
+    return res?.data?.tokens;
+  }
 };
