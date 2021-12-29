ac4d443506f66c287639743465bcff64bf13394f
Terry Sahaidak <s_tasssik_taras@ukr.net>
2021-12-30 00:20:04 +0200

    rm fast compare copy

--- src/styled-thing/index.js
+++ src/styled-thing/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import isEqual from 'react-fast-compare';
 import { css } from 'styled-components';
-import isEqual from './fastCompare';
 
 import hoist from './hoist';
 // eslint-disable-next-line import/no-commonjs
