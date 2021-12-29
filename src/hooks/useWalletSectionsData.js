6ae24d8c613a1b0b2d98a62b55de54c21cbda45c
osdnk <micosa97@gmail.com>
2021-12-29 22:33:45 +0100

    useCoinListEdited

--- src/hooks/useWalletSectionsData.js
+++ src/hooks/useWalletSectionsData.js
@@ -29,7 +29,7 @@ export default function useWalletSectionsData() {
     true
   );
 
-  const isCoinListEdited = useCoinListEdited();
+  const { isCoinListEdited } = useCoinListEdited();
 
   const walletSections = useMemo(() => {
     const accountInfo = {
