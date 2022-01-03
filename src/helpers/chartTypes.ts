3f8b4318ad0d1d34e4571da905062dfc2ae7c3f0
Marco Burstein <marco@marco.how>
2022-01-03 11:02:42 -0800

    Convert `src/redux/charts` and related files to TypeScript.

--- src/helpers/chartTypes.ts
+++ src/helpers/chartTypes.ts
@@ -1,10 +1,14 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
-export default {
+const chartTypes = {
   hour: 'h',
   day: 'd',
   week: 'w',
   month: 'm',
   year: 'y',
   max: 'a',
-};
+} as const;
+
+export default chartTypes;
+export type ChartType = typeof chartTypes[keyof typeof chartTypes];
+
 /* eslint-enable sort-keys-fix/sort-keys-fix */
