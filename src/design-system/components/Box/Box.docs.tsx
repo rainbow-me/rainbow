b91d175bd6fdc8ab55557acd38dd32f945f9a155
Jake Moxey <jakemoxey@gmail.com>
2022-01-05 19:10:04 +1100

    Add dedent to root deps

--- src/design-system/components/Box/Box.docs.tsx
+++ src/design-system/components/Box/Box.docs.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
-import React from 'react';
 import dedent from 'dedent';
+import React from 'react';
 
 import * as Docs from '../../docs/components';
 import { Docs as DocsType } from '../../docs/types';
