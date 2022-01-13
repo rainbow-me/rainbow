9ec4ff5054cfd2e9c55724258decae2c5686ad51
osdnk <micosa97@gmail.com>
2022-01-13 19:15:38 +0200

    Platform

--- src/components/exchange/ExchangeFloatingPanels.js
+++ src/components/exchange/ExchangeFloatingPanels.js
@@ -1,12 +1,13 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components';
 import { FloatingPanels } from '../floating-panels';
 
 const ExchangeFloatingPanels = styled(FloatingPanels).attrs({
   margin: 0,
-  translateY: -16,
+  translateY: ios ? 0 : -16,
 })`
   padding-top: 24;
-  height: 85%;
+  ${Platform.select({ android: 'height: 85%;', ios: '' })}
 `;
 
 export default ExchangeFloatingPanels;
