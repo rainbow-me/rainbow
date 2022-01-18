b9be2c595f5a67110e45b29b575010dd52d142e4
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2022-01-18 17:31:23 +1100

    Align Prettier config with eslint-config-rainbow rules (#2842)

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,8 +26,6 @@ module.exports = {
   extends: 'rainbow',
   settings: {
     'import/resolver': {
-      // eslint wants it to be `'node'` but prettier `node`
-      // prettier-ignore
       'node': {
         extensions: [
           '.js',
