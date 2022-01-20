2e8a77b507b575ac3fe8fbf776176163049b8b3e
Jake Moxey <jakemoxey@gmail.com>
2022-01-20 13:35:41 +1100

    Refactor skeleton

--- src/components/skeleton/Skeleton.tsx
+++ src/components/skeleton/Skeleton.tsx
@@ -1,18 +1,10 @@
 import MaskedView from '@react-native-community/masked-view';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, ViewProps } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-import Animated, {
-  Easing,
-  interpolate,
-  useAnimatedStyle,
-  useSharedValue,
-  withRepeat,
-  withTiming,
-} from 'react-native-reanimated';
 import styled from 'styled-components';
 import { withThemeContext } from '../../context/ThemeContext';
 import { deviceUtils } from '../../utils';
+import { ShimmerAnimation } from '../animations';
 import { CoinRowHeight } from '../coin-row';
 import { Row } from '../layout';
 import { position } from '@rainbow-me/styles';
@@ -34,21 +26,22 @@ export const FakeRow = styled(Row).attrs({
   paddingTop: 5,
 })(Row);
 
-export const FakeText = styled(View)`
+export const FakeText = styled(View).attrs(
+  ({ height = 10 }: { height: number }) => ({
+    height,
+  })
+)`
   background-color: ${({ theme: { colors } }) => colors.skeleton};
-  border-radius: 5;
-  height: ${({ height = 10 }: { height: number }) => height};
-`;
-
-const Gradient = styled(LinearGradient).attrs({
-  end: { x: 1, y: 0.5 },
-  start: { x: 0, y: 0.5 },
-})`
-  ${position.size('100%')};
+  border-radius: ${({ height }: { height: number }) => height / 2};
+  height: ${({ height }: { height: number }) => height};
 `;
 
 const Wrapper = styled(View)`
   ${position.size('100%')};
+`;
+
+const ShimmerWrapper = styled(Wrapper)`
+  background-color: ${({ theme: { colors } }) => colors.skeleton};
 `;
 
 function Skeleton({
@@ -56,66 +49,28 @@ function Skeleton({
   children,
   style,
   colors,
+  width = deviceUtils.dimensions.width,
 }: {
   animated?: boolean;
   children: React.ReactElement;
   style: ViewProps['style'];
   colors: any;
+  width?: number;
 }) {
-  const gradientColors = [
-    colors.skeleton,
-    colors.shimmer,
-    colors.skeleton,
-    colors.skeleton,
-  ];
-
-  const gradientSteps = [0, 0.2, 0.4, 1];
-
-  const progress = useSharedValue(0);
-
-  useEffect(() => {
-    if (!animated) {
-      return;
-    }
-    progress.value = withRepeat(
-      withTiming(1, {
-        duration: 1250,
-        easing: Easing.linear,
-      }),
-      -1
-    );
-  }, [animated, progress]);
-
-  const animatedStyle = useAnimatedStyle(() => {
-    const translateX = interpolate(
-      progress.value,
-      [0, 1],
-      [
-        deviceUtils.dimensions.width * -1.17,
-        deviceUtils.dimensions.width * 1.17,
-      ]
-    );
-    return {
-      backgroundColor: 'red',
-      transform: [
-        {
-          translateX,
-        },
-      ],
-    };
-  }, []);
-
   if (animated) {
     return (
       <MaskedView
         maskElement={<Wrapper style={style}>{children}</Wrapper>}
         style={{ flex: 1 }}
       >
-        <View style={{ backgroundColor: gradientColors[0] }}>
-          <Animated.View style={animatedStyle}>
-            <Gradient colors={gradientColors} locations={gradientSteps} />
-          </Animated.View>
-        </View>
+        <ShimmerWrapper>
+          <ShimmerAnimation
+            color={colors.shimmer}
+            enabled
+            gradientColor={colors.shimmer}
+            width={width}
+          />
+        </ShimmerWrapper>
       </MaskedView>
     );
   }
