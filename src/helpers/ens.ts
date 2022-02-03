81a23a974a05a82f0539189578de3fd21b3aa37a
Esteban Miño <efmino@uc.cl>
2022-02-03 10:53:58 -0300

    Update src/helpers/ens.ts
    Co-authored-by: moxey.eth <jakemoxey@gmail.com>

--- src/helpers/ens.ts
+++ src/helpers/ens.ts
@@ -7,7 +7,7 @@ import {
 } from '@rainbow-me/references';
 
 const ensAddress = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
-// fixed to main regsitrar for now
+// fixed to main registrar for now
 const ensRegistrarAddress = '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5';
 const ensBaseRegistrarImplementationAddress =
   '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85';
