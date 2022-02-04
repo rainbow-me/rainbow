4ada972642cd07f2d99b4fb827d244d19fda9992
Ben Goldberg <bengoldberg@rainbow.me>
2022-02-04 11:19:09 -0700

    fix broken polygonscan link

--- src/components/expanded-state/asset/ChartExpandedState.js
+++ src/components/expanded-state/asset/ChartExpandedState.js
@@ -207,9 +207,11 @@ export default function ChartExpandedState({ asset }) {
   const ogAsset = useMemo(() => {
     return {
       ...assetWithPrice,
-      address: isL2 ? assetWithPrice.l2Address : assetWithPrice.address,
+      address: isL2
+        ? assetWithPrice.l2Address || asset?.address
+        : assetWithPrice.address,
     };
-  }, [assetWithPrice, isL2]);
+  }, [assetWithPrice, isL2, asset]);
 
   const { height: screenHeight } = useDimensions();
   const {
