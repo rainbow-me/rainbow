ae628b5d1ff32089c5c911d73dc0c0ff49f3b5ac
Esteban Miño <efmino@uc.cl>
2022-02-03 21:06:06 -0300

    add dummy txs

--- src/helpers/ens.ts
+++ src/helpers/ens.ts
@@ -7,6 +7,14 @@ import {
   ETHRegistrarControllerABI,
 } from '@rainbow-me/references';
 
+enum ENSRegistrationStepType {
+  COMMIT,
+  REGISTER_WITH_CONFIG,
+  SET_TEXT,
+  SET_NAME,
+  MULTICALL,
+}
+
 const ensAddress = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
 // fixed to main registrar for now
 const ensRegistrarAddress = '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5';
@@ -46,6 +54,59 @@ const getNameExpires = async (name: string): Promise<string> =>
 const getRentPrice = async (name: string, duration: number): Promise<string> =>
   getENSRegistrarContract().rentPrice(name, duration);
 
+const getExecutionDetails = (
+  accountAddress: string,
+  ensRegistrationStepType: ENSRegistrationStepType
+): {
+  methodArguments: (string | string[] | number)[];
+  methodNames: string[];
+  value: string | null;
+} => {
+  // let estimate: Function, method: Function,
+  let methodNames: string[],
+    args: (string | string[] | number)[],
+    value: string | null = null;
+  switch (ensRegistrationStepType) {
+    // to ETHRegistrarController
+    case ENSRegistrationStepType.COMMIT:
+      methodNames = ['commit'];
+      args = ['dummy commitment'];
+      break;
+    // to ETHRegistrarController
+    case ENSRegistrationStepType.REGISTER_WITH_CONFIG:
+      methodNames = ['register'];
+      args = [
+        'name',
+        'owner',
+        'duration',
+        'secret',
+        `{ value: price.toString() }`,
+      ];
+      value = 'eth value';
+      break;
+    // to Resolver
+    case ENSRegistrationStepType.SET_TEXT:
+      methodNames = ['setText'];
+      args = ['node (bytes32)', 'key (string)', 'value (string)'];
+      break;
+    // to Resolver
+    case ENSRegistrationStepType.MULTICALL:
+      methodNames = ['multicall'];
+      args = ['data (bytes[])'];
+      break;
+    // to ReverseRegistrar
+    case ENSRegistrationStepType.SET_NAME:
+      methodNames = ['setName'];
+      args = ['name'];
+      break;
+  }
+  return {
+    methodArguments: args,
+    methodNames,
+    value,
+  };
+};
+
 export {
   getENSContract,
   getENSRegistrarContract,
@@ -54,4 +115,5 @@ export {
   getAvailable,
   getNameExpires,
   getRentPrice,
+  getExecutionDetails,
 };
