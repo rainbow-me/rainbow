fac559ad5fedc9a67aa61c74e0c2b39388ecfc81
Esteban Miño <efmino@uc.cl>
2022-02-08 16:53:12 -0300

    add some redux methods

--- src/redux/ensRegistration.ts
+++ src/redux/ensRegistration.ts
@@ -1,11 +1,24 @@
 import { AnyAction } from 'redux';
 import { AppDispatch } from './store';
 
-export const ENS_REGISTRATION_UPDATE_NAME =
+const ENS_REGISTRATION_UPDATE_NAME =
   'ensRegistration/ENS_REGISTRATION_UPDATE_NAME';
-export const ENS_REGISTRATION_DUMMY_UPDATE_RECORDS =
+const ENS_REGISTRATION_DUMMY_UPDATE_RECORDS =
   'ensRegistration/ENS_REGISTRATION_DUMMY_UPDATE_RECORDS';
+const ENS_REGISTRATION_UPDATE_RECORD =
+  'ensRegistration/ENS_REGISTRATION_UPDATE_RECORD';
+const ENS_REGISTRATION_UPDATE_RECORDS_ADDRESSES =
+  'ensRegistration/ENS_REGISTRATION_UPDATE_ADDRESSES_RECORD';
+const ENS_REGISTRATION_UPDATE_RECORDS_CONTENT =
+  'ensRegistration/ENS_REGISTRATION_UPDATE_RECORDS_CONTENT';
+const ENS_REGISTRATION_UPDATE_RECORDS_TEXT =
+  'ensRegistration/ENS_REGISTRATION_UPDATE_RECORDS_TEXT';
 
+enum RECORD_KEYS {
+  ADDRESSES_KEY,
+  CONTENT_KEY,
+  TEXT_KEY,
+}
 // -- Actions ---------------------------------------- //
 
 interface Records {
@@ -14,7 +27,11 @@ interface Records {
 
 interface ChartsState {
   name: string;
-  records: Records;
+  records: {
+    [RECORD_KEYS.ADDRESSES_KEY]: Records;
+    [RECORD_KEYS.CONTENT_KEY]: Records;
+    [RECORD_KEYS.TEXT_KEY]: Records;
+  };
 }
 
 export const ensRegistrationUpdateName = (name: string) => async (
@@ -33,10 +50,23 @@ export const ensRegistrationUpdateRecords = (records: Records) => async (
     type: ENS_REGISTRATION_DUMMY_UPDATE_RECORDS,
   });
 
+export const ensRegistrationUpdateRecord = (
+  key: string,
+  value: string
+) => async (dispatch: AppDispatch) =>
+  dispatch({
+    payload: { key, value },
+    type: ENS_REGISTRATION_UPDATE_RECORD,
+  });
+
 // -- Reducer ----------------------------------------- //
 const INITIAL_STATE: ChartsState = {
   name: '',
-  records: {} as Records,
+  records: {
+    [RECORD_KEYS.ADDRESSES_KEY]: {} as Records,
+    [RECORD_KEYS.CONTENT_KEY]: {} as Records,
+    [RECORD_KEYS.TEXT_KEY]: {} as Records,
+  },
 };
 
 export default (state = INITIAL_STATE, action: AnyAction) => {
@@ -51,6 +81,43 @@ export default (state = INITIAL_STATE, action: AnyAction) => {
         ...state,
         name: action.payload,
       };
+    case ENS_REGISTRATION_UPDATE_RECORDS_ADDRESSES: {
+      return {
+        ...state,
+        records: {
+          ...state.records,
+          [RECORD_KEYS.ADDRESSES_KEY]: action.payload,
+        },
+      };
+    }
+    case ENS_REGISTRATION_UPDATE_RECORDS_CONTENT: {
+      return {
+        ...state,
+        records: {
+          ...state.records,
+          [RECORD_KEYS.CONTENT_KEY]: action.payload,
+        },
+      };
+    }
+    case ENS_REGISTRATION_UPDATE_RECORDS_TEXT: {
+      return {
+        ...state,
+        records: {
+          ...state.records,
+          [RECORD_KEYS.TEXT_KEY]: action.payload,
+        },
+      };
+    }
+    case ENS_REGISTRATION_UPDATE_RECORD: {
+      const { key, value } = action.payload;
+      return {
+        ...state,
+        records: {
+          ...state.records,
+          [key]: value,
+        },
+      };
+    }
     default:
       return state;
   }
