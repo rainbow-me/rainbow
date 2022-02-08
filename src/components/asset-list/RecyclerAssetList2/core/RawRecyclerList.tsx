044c9184763dfb7e7b0f8d66ba93a553fb510922
osdnk <micosa97@gmail.com>
2022-02-08 19:13:02 +0200

    Fix jumping profile header

--- src/components/asset-list/RecyclerAssetList2/core/RawRecyclerList.tsx
+++ src/components/asset-list/RecyclerAssetList2/core/RawRecyclerList.tsx
@@ -1,7 +1,8 @@
-import React, { LegacyRef, useCallback, useRef } from 'react';
+import React, { LegacyRef, useCallback, useEffect, useRef } from 'react';
 import { LayoutChangeEvent } from 'react-native';
 import { DataProvider, RecyclerListView } from 'recyclerlistview';
 import { useMemoOne } from 'use-memo-one';
+import useAccountSettings from '../../../../hooks/useAccountSettings';
 import ExternalScrollViewWithRef from './ExternalScrollView';
 import RefreshControl from './RefreshControl';
 import rowRenderer from './RowRenderer';
@@ -30,9 +31,22 @@ const RawMemoRecyclerAssetList = React.memo(function RawRecyclerAssetList({
     [briefSectionsData]
   );
 
+  const { accountAddress } = useAccountSettings();
+
   const topMarginRef = useRef<number>(0);
 
   const ref = useRef<RecyclerListViewRef>();
+
+  useEffect(() => {
+    // @ts-ignore
+    ref.current?._virtualRenderer
+      ?.getViewabilityTracker?.()
+      .updateOffset(0, true, 0);
+    // @ts-ignore
+    clearTimeout(ref.current?._processInitialOffsetTimeout);
+    ref.current?.scrollToOffset(0, 0);
+  }, [accountAddress]);
+
   const onLayout = useCallback(
     () => ({ nativeEvent }: LayoutChangeEvent) => {
       topMarginRef.current = nativeEvent.layout.y;
