02c98dc57a6b855ae2f53ed90e6b75d3f5cbbfcd
Ben Goldberg <bengoldberg@rainbow.me>
2022-02-11 14:58:57 -0800

    working on converting edge fade to design system

--- src/components/expanded-state/token-history/TokenHistoryEdgeFade.js
+++ src/components/expanded-state/token-history/TokenHistoryEdgeFade.js
@@ -3,47 +3,21 @@ import { View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components';
 import { useDimensions } from '@rainbow-me/hooks';
+import { Box } from '@rainbow-me/design-system';
 
-const LeftFade = styled(LinearGradient).attrs(({ theme: { colors } }) => ({
-  colors: [
-    colors.black,
-    colors.alpha(colors.black, 0.738),
-    colors.alpha(colors.black, 0.541),
-    colors.alpha(colors.black, 0.382),
-    colors.alpha(colors.black, 0.278),
-    colors.alpha(colors.black, 0.194),
-    colors.alpha(colors.black, 0.126),
-    colors.alpha(colors.black, 0.075),
-    colors.alpha(colors.black, 0.042),
-    colors.alpha(colors.black, 0.021),
-    colors.alpha(colors.black, 0.008),
-    colors.alpha(colors.black, 0.002),
-    colors.alpha(colors.black, 0),
-  ],
-  end: { x: 0, y: 0.5 },
-  locations: [
-    0,
-    0.19,
-    0.34,
-    0.47,
-    0.565,
-    0.65,
-    0.73,
-    0.802,
-    0.861,
-    0.91,
-    0.952,
-    0.982,
-    1,
-  ],
-  pointerEvents: 'none',
-  start: { x: 1, y: 0.5 },
-}))`
-  height: 100%;
-  left: 0;
-  position: absolute;
-  width: 19px;
-`;
+const Fade = ({ color, alignment }) => (
+  <Box
+    as={LinearGradient}
+    colors={['transparent', color]}
+    end={{ x: 1, y: 0 }}
+    start={{ x: 0, y: 1 }}
+    height="full"
+    position="absolute"
+    left={alignment === 'left' ? '0px' : undefined}
+    left={alignment === 'right' ? '0px' : undefined}
+    width={{ custom: 19 }}
+  />
+);
 
 const RightFade = styled(LinearGradient).attrs(({ theme: { colors } }) => ({
   colors: [
@@ -98,9 +72,10 @@ export default function TokenHistoryEdgeFade() {
   const { colors } = useTheme();
   return (
     <View style={{ backgroundColor: 'transparent' }}>
-      <LeftFade />
+      {/* <LeftFade /> */}
+      <Fade color={colors.black} alignment="left" />
       <CenterView colors={colors} width={width} />
-      <RightFade />
+      <Fade color={colors.black} alignment="right" />
     </View>
   );
 }
