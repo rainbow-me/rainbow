d6c664324ba39d915875ffcd3aca983fd61d4a76
Ben Goldberg <bengoldberg@rainbow.me>
2022-02-14 11:37:56 -0800

    fix android sheet handle

--- src/navigation/RegisterENSNavigator.js
+++ src/navigation/RegisterENSNavigator.js
@@ -21,7 +21,7 @@ const renderPager = props => (
 export default function RegisterENSNavigator() {
   const { height: deviceHeight } = useDimensions();
 
-  const extraTopPadding = android ? 10 : 0;
+  const extraTopPadding = android ? 29 : 19;
   const contentHeight =
     deviceHeight -
     SheetHandleFixedToTopHeight -
@@ -34,9 +34,10 @@ export default function RegisterENSNavigator() {
 
   return (
     <SlackSheet
+      removeTopPadding
+      additionalTopPadding
       contentHeight={contentHeight}
       height="100%"
-      removeTopPadding
       scrollEnabled={false}
     >
       <Box
