bf53ce6e5444f797cc94149811f1ad8db9dc5610
Esteban Miño <efmino@uc.cl>
2022-02-22 20:45:40 -0300

    add register hook in handler

--- src/hooks/useENSRegistrationActionHandler.ts
+++ src/hooks/useENSRegistrationActionHandler.ts
@@ -4,8 +4,9 @@ import { useDispatch } from 'react-redux';
 import { RapActionTypes, RegisterENSActionParameters } from '../raps/common';
 import { useAccountSettings, useCurrentNonce, useENSProfile } from '.';
 import { RegistrationParameters } from '@rainbow-me/entities';
-import { web3Provider } from '@rainbow-me/handlers/web3';
-import { generateSalt } from '@rainbow-me/helpers/ens';
+import { toHex, web3Provider } from '@rainbow-me/handlers/web3';
+import { generateSalt, getRentPrice } from '@rainbow-me/helpers/ens';
+import { addBuffer } from '@rainbow-me/helpers/utilities';
 import { loadWallet } from '@rainbow-me/model/wallet';
 import { executeRap } from '@rainbow-me/raps';
 import { updateCommitRegistrationParameters } from '@rainbow-me/redux/ensRegistration';
@@ -18,8 +19,15 @@ enum REGISTRATION_STEPS {
 }
 
 const ENS_SECONDS_WAIT = 60;
+const secsInYear = 31536000;
 
-export default function useENSRegistrationActionHandler() {
+export default function useENSRegistrationActionHandler(
+  {
+    duration,
+  }: {
+    duration: number;
+  } = {} as any
+) {
   const { accountAddress, network } = useAccountSettings();
   const { registrationParameters } = useENSProfile();
   const getNextNonce = useCurrentNonce(accountAddress, network);
@@ -77,27 +85,20 @@ export default function useENSRegistrationActionHandler() {
 
   const commit = useCallback(
     async (callback: () => void) => {
-      const {
-        name,
-        duration,
-        records,
-        rentPrice,
-      } = registrationParameters as RegistrationParameters;
+      const { name } = registrationParameters as RegistrationParameters;
       const wallet = await loadWallet();
       if (!wallet) {
         return;
       }
 
-      const nonce = await getNextNonce();
       const salt = generateSalt();
+      const nonce = await getNextNonce();
 
       const commitEnsRegistrationParameters: RegisterENSActionParameters = {
-        duration,
+        duration: duration * secsInYear,
         name,
         nonce,
         ownerAddress: accountAddress,
-        records,
-        rentPrice,
         salt,
       };
 
@@ -105,6 +106,43 @@ export default function useENSRegistrationActionHandler() {
         wallet,
         RapActionTypes.commitENS,
         commitEnsRegistrationParameters,
+        callback
+      );
+    },
+    [accountAddress, duration, getNextNonce, registrationParameters]
+  );
+
+  const register = useCallback(
+    async (callback: () => void) => {
+      const {
+        name,
+        duration,
+        records,
+        salt,
+      } = registrationParameters as RegistrationParameters;
+      const wallet = await loadWallet();
+      if (!wallet) {
+        return;
+      }
+
+      const nonce = await getNextNonce();
+      const rentPrice = await getRentPrice(name, secsInYear);
+      const rentPriceWithBuffer = toHex(addBuffer(rentPrice.toString(), 1.1));
+
+      const registerEnsRegistrationParameters: RegisterENSActionParameters = {
+        duration,
+        name,
+        nonce,
+        ownerAddress: accountAddress,
+        records,
+        rentPrice: rentPriceWithBuffer,
+        salt,
+      };
+
+      await executeRap(
+        wallet,
+        RapActionTypes.registerWithConfigENS,
+        registerEnsRegistrationParameters,
         callback
       );
     },
@@ -143,10 +181,10 @@ export default function useENSRegistrationActionHandler() {
         return () => null;
       }
       case REGISTRATION_STEPS.REGISTER: {
-        return () => null;
+        return register;
       }
     }
-  }, [commit, registrationStep]);
+  }, [commit, register, registrationStep]);
 
   useEffect(() => {
     if (registrationStep === REGISTRATION_STEPS.WAIT_COMMIT_CONFIRMATION) {
