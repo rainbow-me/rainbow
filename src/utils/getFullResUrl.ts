ef0583d09bd6ecb3f685effae11199f0ee8531a5
Wojtek Witkowski <hi@wojtek.im>
2022-03-04 15:21:48 +0100

    Use proper full size param. Remove magic number

--- src/utils/getFullResUrl.ts
+++ src/utils/getFullResUrl.ts
@@ -1,9 +1,11 @@
 export const GOOGLE_USER_CONTENT_URL = 'https://lh3.googleusercontent.com/';
-const size = 5000;
+
+// List of params for Google Cloud Storage available here
+// https://github.com/albertcht/python-gcs-image
 
 export const getFullResUrl = (url: string | null | undefined) => {
   if (url?.startsWith?.(GOOGLE_USER_CONTENT_URL)) {
-    return `${url}=w${size}`;
+    return `${url}=s0`; // s0 gets the original size from google
   }
   return url;
 };
