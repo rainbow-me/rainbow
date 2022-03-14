09894ecdf00052468d2fe624ec33ce370ea20ef3
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2022-03-14 21:45:05 +0200

    Track unrecognized URLs (#3023)
    * Track unrecognized URLs
    
    * Update code
    
    Co-authored-by: Michał Osadnik <micosa97@gmail.com>
    
    * Remove unused import
    
    * Silence audit
    
    Co-authored-by: Michał Osadnik <micosa97@gmail.com>

--- src/handlers/deeplinks.ts
+++ src/handlers/deeplinks.ts
@@ -1,3 +1,4 @@
+import { captureException } from '@sentry/react-native';
 import { toLower } from 'lodash';
 // @ts-expect-error ts-migrate(7016) FIXME: Could not find a declaration file for module 'qs'.... Remove this comment to see the full error message
 import qs from 'qs';
@@ -97,6 +98,8 @@ export default async function handleDeeplink(
               address: addressOrENS,
             });
           } else {
+            const error = new Error('Invalid deeplink: ' + url);
+            captureException(error);
             Alert.alert('Uh oh! We couldn’t recognize this URL!');
           }
         }
