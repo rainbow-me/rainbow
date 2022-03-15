831399faac6aa6561c651dd9f9f7a0cdafbc98d6
moxey.eth <jakemoxey@gmail.com>
2022-03-16 08:31:29 +1100

    Profiles: ENS call-to-action banner placeholder (#3046)
    * WIP
    
    * Add ENS call-to-action banner

---
+++ src/components/discover-sheet/RegisterENSSection.tsx
@@ -0,0 +1,73 @@
+import lang from 'i18n-js';
+import React, { useCallback } from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import { useNavigation } from '../../navigation/Navigation';
+import { ButtonPressAnimation } from '../animations';
+import ENSIcon from '../icons/svg/ENSIcon';
+import { enableActionsOnReadOnlyWallet } from '@rainbow-me/config/debug';
+import { useTheme } from '@rainbow-me/context';
+import {
+  AccentColorProvider,
+  Box,
+  Inline,
+  Inset,
+  Stack,
+  Text,
+} from '@rainbow-me/design-system';
+import { useWallets } from '@rainbow-me/hooks';
+import Routes from '@rainbow-me/routes';
+import { watchingAlert } from '@rainbow-me/utils';
+
+export default function RegisterENSSection() {
+  const { navigate } = useNavigation();
+  const { colors } = useTheme();
+  const { isReadOnlyWallet } = useWallets();
+
+  const handlePress = useCallback(() => {
+    if (!isReadOnlyWallet || enableActionsOnReadOnlyWallet) {
+      navigate(Routes.REGISTER_ENS_NAVIGATOR);
+    } else {
+      watchingAlert();
+    }
+  }, [isReadOnlyWallet, navigate]);
+
+  return (
+    <ButtonPressAnimation onPress={handlePress} scaleTo={0.92}>
+      <AccentColorProvider color={colors.gradients.ens[1]}>
+        <Inset bottom="19px" horizontal="19px">
+          <Box
+            as={LinearGradient}
+            background="accent"
+            borderRadius={24}
+            colors={colors.gradients.ens}
+            end={{ x: 0.6, y: 0 }}
+            height={{ custom: 70 }}
+            shadow="30px heavy accent"
+            start={{ x: 0, y: 0.6 }}
+          >
+            <Inset space="15px">
+              <Inline alignVertical="center" space="10px" wrap={false}>
+                <Box alignItems="center" width={{ custom: 40 }}>
+                  <ENSIcon height="40" width="40" />
+                </Box>
+                <Stack space="8px">
+                  <Text size="18px" weight="heavy">
+                    {lang.t('profiles.banner.register_name')}
+                  </Text>
+                  <Text color="secondary70" size="16px" weight="semibold">
+                    {lang.t('profiles.banner.and_create_ens_profile')}
+                  </Text>
+                </Stack>
+              </Inline>
+            </Inset>
+            <Box style={{ position: 'absolute', right: 15, top: 18 }}>
+              <Text size="18px" weight="heavy">
+                􀯼
+              </Text>
+            </Box>
+          </Box>
+        </Inset>
+      </AccentColorProvider>
+    </ButtonPressAnimation>
+  );
+}
---
+++ src/components/icons/svg/ENSIcon.js
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import Svg, { Defs, G, LinearGradient, Path, Stop } from 'react-native-svg';
+
+const ENSIcon = props => (
+  <Svg height="80" viewBox="0 0 70 80" width="70" {...props}>
+    <Defs>
+      <LinearGradient
+        gradientUnits="userSpaceOnUse"
+        id="linear-gradient"
+        x1={41.95}
+        x2={12.57}
+        y1={2.57}
+        y2={34.42}
+      >
+        <Stop offset={0.58} stopColor="#a0a8d4" />
+        <Stop offset={0.73} stopColor="#8791c7" />
+        <Stop offset={0.91} stopColor="#6470b4" />
+      </LinearGradient>
+      <LinearGradient
+        id="linear-gradient-2"
+        x1={42.57}
+        x2={71.96}
+        xlinkHref="#linear-gradient"
+        y1={81.66}
+        y2={49.81}
+      />
+      <LinearGradient
+        gradientUnits="userSpaceOnUse"
+        id="linear-gradient-3"
+        x1={42.26}
+        x2={42.26}
+        y1={1.24}
+        y2={82.84}
+      >
+        <Stop offset={0} stopColor="#9994fc" />
+        <Stop offset={0.18} stopColor="#93a6ff" />
+        <Stop offset={0.57} stopColor="#5298ff" />
+        <Stop offset={1} stopColor="#52e5ff" />
+      </LinearGradient>
+    </Defs>
+    <G
+      style={{
+        isolation: 'isolate',
+      }}
+    >
+      <G>
+        <Path
+          d="M15.28 34.39c.8 1.71 2.78 5.09 2.78 5.09L40.95 1.64l-22.34 15.6a9.75 9.75 0 0 0-3.18 3.5 16.19 16.19 0 0 0-.15 13.65z"
+          fill="url(#linear-gradient)"
+          transform="translate(-6 -1.64)"
+        />
+        <Path
+          d="M6.21 46.85a25.47 25.47 0 0 0 10 18.51l24.71 17.23s-15.46-22.28-28.5-44.45a22.39 22.39 0 0 1-2.62-7.56 12.1 12.1 0 0 1 0-3.63c-.34.63-1 1.92-1 1.92a29.35 29.35 0 0 0-2.67 8.55 52.28 52.28 0 0 0 .08 9.43z"
+          fill="#a0a8d4"
+          transform="translate(-6 -1.64)"
+        />
+        <Path
+          d="M69.25 49.84c-.8-1.71-2.78-5.09-2.78-5.09L43.58 82.59 65.92 67a9.75 9.75 0 0 0 3.18-3.5 16.19 16.19 0 0 0 .15-13.66z"
+          fill="url(#linear-gradient-2)"
+          transform="translate(-6 -1.64)"
+        />
+        <Path
+          d="M78.32 37.38a25.47 25.47 0 0 0-10-18.51L43.61 1.64s15.45 22.28 28.5 44.45a22.39 22.39 0 0 1 2.61 7.56 12.1 12.1 0 0 1 0 3.63c.34-.63 1-1.92 1-1.92a29.35 29.35 0 0 0 2.67-8.55 52.28 52.28 0 0 0-.07-9.43z"
+          fill="#a0a8d4"
+          transform="translate(-6 -1.64)"
+        />
+        <Path
+          d="M15.43 20.74a9.75 9.75 0 0 1 3.18-3.5l22.34-15.6-22.89 37.85s-2-3.38-2.78-5.09a16.19 16.19 0 0 1 .15-13.66zM6.21 46.85a25.47 25.47 0 0 0 10 18.51l24.71 17.23s-15.46-22.28-28.5-44.45a22.39 22.39 0 0 1-2.62-7.56 12.1 12.1 0 0 1 0-3.63c-.34.63-1 1.92-1 1.92a29.35 29.35 0 0 0-2.67 8.55 52.28 52.28 0 0 0 .08 9.43zm63 3c-.8-1.71-2.78-5.09-2.78-5.09L43.58 82.59 65.92 67a9.75 9.75 0 0 0 3.18-3.5 16.19 16.19 0 0 0 .15-13.66zm9.07-12.46a25.47 25.47 0 0 0-10-18.51L43.61 1.64s15.45 22.28 28.5 44.45a22.39 22.39 0 0 1 2.61 7.56 12.1 12.1 0 0 1 0 3.63c.34-.63 1-1.92 1-1.92a29.35 29.35 0 0 0 2.67-8.55 52.28 52.28 0 0 0-.07-9.43z"
+          fill="url(#linear-gradient-3)"
+          style={{
+            mixBlendMode: 'color',
+          }}
+          transform="translate(-6 -1.64)"
+        />
+      </G>
+    </G>
+  </Svg>
+);
+
+export default ENSIcon;
