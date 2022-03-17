b1a6f95bb78b381eeec871798ee52f269aed40bc
Esteban Miño <efmino@uc.cl>
2022-03-17 15:21:20 -0300

    uncomment tests

--- e2e/importPrivateKeyFlow.spec.js
+++ e2e/importPrivateKeyFlow.spec.js
@@ -1,65 +1,65 @@
-// /* eslint-disable no-undef */
-// /* eslint-disable jest/expect-expect */
-// import * as Helpers from './helpers';
+/* eslint-disable no-undef */
+/* eslint-disable jest/expect-expect */
+import * as Helpers from './helpers';
 
-// describe('Import from private key flow', () => {
-//   it('without 0x - Should show the welcome screen', async () => {
-//     await Helpers.checkIfVisible('welcome-screen');
-//   });
+describe('Import from private key flow', () => {
+  it('without 0x - Should show the welcome screen', async () => {
+    await Helpers.checkIfVisible('welcome-screen');
+  });
 
-//   it('without 0x - Should show the "Restore Sheet" after tapping on "I already have a wallet"', async () => {
-//     await Helpers.waitAndTap('already-have-wallet-button');
-//     await Helpers.checkIfExists('restore-sheet');
-//   });
+  it('without 0x - Should show the "Restore Sheet" after tapping on "I already have a wallet"', async () => {
+    await Helpers.waitAndTap('already-have-wallet-button');
+    await Helpers.checkIfExists('restore-sheet');
+  });
 
-//   it('without 0x - Show the "Import Sheet" when tapping on "Restore with a recovery phrase or private key"', async () => {
-//     await Helpers.waitAndTap('restore-with-key-button');
-//     await Helpers.checkIfExists('import-sheet');
-//   });
+  it('without 0x - Show the "Import Sheet" when tapping on "Restore with a recovery phrase or private key"', async () => {
+    await Helpers.waitAndTap('restore-with-key-button');
+    await Helpers.checkIfExists('import-sheet');
+  });
 
-//   it('without 0x - Should show the "Add wallet modal" after tapping import with a valid private key"', async () => {
-//     await Helpers.typeText(
-//       'import-sheet-input',
-//       process.env.DEV_PKEY.substring(2),
-//       false
-//     );
-//     await Helpers.checkIfElementHasString(
-//       'import-sheet-button-label',
-//       'Import'
-//     );
-//     await Helpers.waitAndTap('import-sheet-button');
-//     await Helpers.checkIfVisible('wallet-info-modal');
-//   });
+  it('without 0x - Should show the "Add wallet modal" after tapping import with a valid private key"', async () => {
+    await Helpers.typeText(
+      'import-sheet-input',
+      process.env.DEV_PKEY.substring(2),
+      false
+    );
+    await Helpers.checkIfElementHasString(
+      'import-sheet-button-label',
+      'Import'
+    );
+    await Helpers.waitAndTap('import-sheet-button');
+    await Helpers.checkIfVisible('wallet-info-modal');
+  });
 
-//   it('without 0x - Should navigate to the Wallet screen after tapping on "Import Wallet"', async () => {
-//     await Helpers.disableSynchronization();
-//     await Helpers.checkIfVisible('wallet-info-input');
-//     await Helpers.typeText('wallet-info-input', 'TKEY', false);
-//     await Helpers.waitAndTap('wallet-info-submit-button');
-//     if (device.getPlatform() === 'android') {
-//       await Helpers.checkIfVisible('pin-authentication-screen');
-//       // Set the pin
-//       await Helpers.authenticatePin('1234');
-//       // Confirm it
-//       await Helpers.authenticatePin('1234');
-//     }
-//     await Helpers.checkIfVisible('wallet-screen', 40000);
-//     await Helpers.enableSynchronization();
-//   });
+  it('without 0x - Should navigate to the Wallet screen after tapping on "Import Wallet"', async () => {
+    await Helpers.disableSynchronization();
+    await Helpers.checkIfVisible('wallet-info-input');
+    await Helpers.typeText('wallet-info-input', 'TKEY', false);
+    await Helpers.waitAndTap('wallet-info-submit-button');
+    if (device.getPlatform() === 'android') {
+      await Helpers.checkIfVisible('pin-authentication-screen');
+      // Set the pin
+      await Helpers.authenticatePin('1234');
+      // Confirm it
+      await Helpers.authenticatePin('1234');
+    }
+    await Helpers.checkIfVisible('wallet-screen', 40000);
+    await Helpers.enableSynchronization();
+  });
 
-//   // Saving for now in case we want to test iCloud back up sheet
-//   // it('Should show the backup sheet', async () => {
-//   //   await Helpers.checkIfVisible('backup-sheet');
-//   //   await Helpers.waitAndTap('backup-sheet-imported-cancel-button');
-//   // });
+  // Saving for now in case we want to test iCloud back up sheet
+  // it('Should show the backup sheet', async () => {
+  //   await Helpers.checkIfVisible('backup-sheet');
+  //   await Helpers.waitAndTap('backup-sheet-imported-cancel-button');
+  // });
 
-//   it('without 0x - Should say "TKEY" in the Profile Screen header', async () => {
-//     await Helpers.swipe('wallet-screen', 'right');
-//     await Helpers.checkIfExistsByText('TKEY');
-//   });
+  it('without 0x - Should say "TKEY" in the Profile Screen header', async () => {
+    await Helpers.swipe('wallet-screen', 'right');
+    await Helpers.checkIfExistsByText('TKEY');
+  });
 
-//   afterAll(async () => {
-//     // Reset the app state
-//     await device.clearKeychain();
-//   });
-// });
+  afterAll(async () => {
+    // Reset the app state
+    await device.clearKeychain();
+  });
+});
--- e2e/newWalletFlow.spec.js
+++ e2e/newWalletFlow.spec.js
@@ -1,50 +1,50 @@
-// /* eslint-disable no-undef */
-// /* eslint-disable jest/expect-expect */
-// import * as Helpers from './helpers';
+/* eslint-disable no-undef */
+/* eslint-disable jest/expect-expect */
+import * as Helpers from './helpers';
 
-// describe('New Wallet flow', () => {
-//   it('should show the welcome screen', async () => {
-//     await Helpers.checkIfVisible('welcome-screen');
-//   });
+describe('New Wallet flow', () => {
+  it('should show the welcome screen', async () => {
+    await Helpers.checkIfVisible('welcome-screen');
+  });
 
-//   it('go to the wallet screen after pressing "Get a new wallet" button', async () => {
-//     await Helpers.disableSynchronization();
-//     await Helpers.waitAndTap('new-wallet-button');
-//     if (device.getPlatform() === 'android') {
-//       await Helpers.checkIfVisible('pin-authentication-screen');
-//       // Set the pin
-//       await Helpers.authenticatePin('1234');
-//       // Confirm it
-//       await Helpers.authenticatePin('1234');
-//     }
-//     await Helpers.checkIfVisible('wallet-screen', 40000);
-//     await Helpers.enableSynchronization();
-//   });
+  it('go to the wallet screen after pressing "Get a new wallet" button', async () => {
+    await Helpers.disableSynchronization();
+    await Helpers.waitAndTap('new-wallet-button');
+    if (device.getPlatform() === 'android') {
+      await Helpers.checkIfVisible('pin-authentication-screen');
+      // Set the pin
+      await Helpers.authenticatePin('1234');
+      // Confirm it
+      await Helpers.authenticatePin('1234');
+    }
+    await Helpers.checkIfVisible('wallet-screen', 40000);
+    await Helpers.enableSynchronization();
+  });
 
-//   // Saving for now in case we want to test iCloud back up sheet
-//   // it('Should show the backup sheet', async () => {
-//   //   await Helpers.delay(5000);
-//   //   await Helpers.checkIfVisible('backup-sheet');
-//   //   await Helpers.swipe('backup-sheet', 'down');
-//   // });
+  // Saving for now in case we want to test iCloud back up sheet
+  // it('Should show the backup sheet', async () => {
+  //   await Helpers.delay(5000);
+  //   await Helpers.checkIfVisible('backup-sheet');
+  //   await Helpers.swipe('backup-sheet', 'down');
+  // });
 
-//   it('should show the "Add funds" button', async () => {
-//     await Helpers.checkIfVisible('copy-address-button');
-//   });
+  it('should show the "Add funds" button', async () => {
+    await Helpers.checkIfVisible('copy-address-button');
+  });
 
-//   it('should show "No transactions yet" in the activity list', async () => {
-//     if (device.getPlatform() === 'android') {
-//       await Helpers.waitAndTap('navbar-profile-button');
-//       await Helpers.checkIfElementByTextIsVisible('No transactions yet');
-//     } else {
-//       await Helpers.waitAndTap('navbar-profile-button');
-//       await Helpers.checkForElementByLabel('no-transactions-yet-label');
-//     }
-//   });
+  it('should show "No transactions yet" in the activity list', async () => {
+    if (device.getPlatform() === 'android') {
+      await Helpers.waitAndTap('navbar-profile-button');
+      await Helpers.checkIfElementByTextIsVisible('No transactions yet');
+    } else {
+      await Helpers.waitAndTap('navbar-profile-button');
+      await Helpers.checkForElementByLabel('no-transactions-yet-label');
+    }
+  });
 
-//   afterAll(async () => {
-//     // Reset the app state
-//     await device.clearKeychain();
-//     await Helpers.delay(2000);
-//   });
-// });
+  afterAll(async () => {
+    // Reset the app state
+    await device.clearKeychain();
+    await Helpers.delay(2000);
+  });
+});
