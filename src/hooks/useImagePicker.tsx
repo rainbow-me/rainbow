2a6cfd586b581afe8786d9f1b867905b50294c1e
Esteban Miño <efmino@uc.cl>
2022-03-24 18:08:05 -0300

    lang

--- src/hooks/useImagePicker.tsx
+++ src/hooks/useImagePicker.tsx
@@ -1,3 +1,4 @@
+import lang from 'i18n-js';
 import { useCallback } from 'react';
 import { Linking } from 'react-native';
 import ImagePicker, { Options } from 'react-native-image-crop-picker';
@@ -12,11 +13,14 @@ export default function useImagePicker() {
       if (e?.message === 'User did not grant library permission.') {
         Alert({
           buttons: [
-            { style: 'cancel', text: 'Cancel' },
-            { onPress: Linking.openSettings, text: 'Enable library access' },
+            { style: 'cancel', text: lang.t('image_picker.cancel') },
+            {
+              onPress: Linking.openSettings,
+              text: lang.t('image_picker.confirm'),
+            },
           ],
-          message: 'This allows Rainbow to use your photos from your library',
-          title: 'Allow to access your photos',
+          message: lang.t('image_picker.message'),
+          title: lang.t('image_picker.title'),
         });
       }
     }
