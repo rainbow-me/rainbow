c98b980c6835e3650dac939dcac7cc56c57b4cde
Skylar Barrera <skylar.barrera@gmail.com>
2022-04-06 14:43:24 -0700

    new currencyTile

--- src/components/expanded-state/swap-details/SwapDetailsMasthead.js
+++ src/components/expanded-state/swap-details/SwapDetailsMasthead.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Icon } from '../../icons';
 import { RowWithMargins } from '../../layout';
 import CurrencyTile, { CurrencyTileHeight } from './CurrencyTile';
+import { Bleed, Box } from '@rainbow-me/design-system';
 import { useSwapCurrencies } from '@rainbow-me/hooks';
 import styled from '@rainbow-me/styled-components';
 import { padding } from '@rainbow-me/styles';
@@ -12,7 +13,7 @@ export const SwapDetailsMastheadHeight =
 
 const Container = styled(RowWithMargins).attrs({
   align: 'center',
-  margin: 7,
+  margin: 0,
 })({
   ...padding.object(containerPaddingTop, 24, 0),
   width: '100%',
@@ -41,7 +42,17 @@ export default function SwapDetailsMasthead({
         priceValue={inputPriceValue}
         type="input"
       />
-      <Icon color={colors.dark} name="doubleChevron" />
+      <Bleed space="15px">
+        <Box
+          background="body"
+          borderRadius={36}
+          padding="12px"
+          shadow="30px heavy body"
+        >
+          <Icon color={colors.blueGreyDark} name="doubleChevron" />
+        </Box>
+      </Bleed>
+
       <CurrencyTile
         amount={outputAmount}
         amountDisplay={outputAmountDisplay}
@@ -49,6 +60,7 @@ export default function SwapDetailsMasthead({
         isHighPriceImpact={isHighPriceImpact}
         priceImpactColor={priceImpactColor}
         priceValue={outputPriceValue}
+        style={{ zIndex: -1 }}
         type="output"
       />
     </Container>
