58699482262378d6c0e69bef9a457e119c793803
moxey.eth <jakemoxey@gmail.com>
2022-04-06 15:34:37 +1000

    make navigator lazy

--- src/navigation/RegisterENSNavigator.js
+++ src/navigation/RegisterENSNavigator.js
@@ -126,6 +126,7 @@ export default function RegisterENSNavigator() {
           <Swipe.Navigator
             initialLayout={deviceUtils.dimensions}
             initialRouteName={currentRouteName}
+            lazy
             pager={renderPager}
             swipeEnabled={false}
             tabBar={renderTabBar}
