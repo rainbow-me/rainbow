2736d207ab5671d9caff21157a423dbfce98a3bd
Esteban Miño <efmino@uc.cl>
2022-04-07 16:16:53 -0400

    move import fast-text-encoding

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
+import 'fast-text-encoding';
 import './shim';
 import './src/model/config';
-import 'fast-text-encoding';
 
 // eslint-disable-next-line no-unused-vars,import/default
 import App from './src/App';
