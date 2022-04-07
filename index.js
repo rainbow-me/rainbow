e35ee479adf17afd8fb403f012478fa365b981d2
Esteban Miño <efmino@uc.cl>
2022-04-07 14:08:45 -0400

    TextEncoder polyfill

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import './shim';
 import './src/model/config';
+import 'fast-text-encoding';
 
 // eslint-disable-next-line no-unused-vars,import/default
 import App from './src/App';
