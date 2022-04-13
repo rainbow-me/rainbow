5d0da153c1878b4f960ea49d713ebc10b32d7db8
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2022-04-13 17:08:19 +0300

    Apply fix the real cause of the issue

--- src/screens/ExpandedAssetSheet.js
+++ src/screens/ExpandedAssetSheet.js
@@ -57,6 +57,8 @@ export default function ExpandedAssetSheet(props) {
       {ios && !isDarkMode && !params.fromDiscover && (
         <StatusBar barStyle="light-content" />
       )}
+      {ios && <TouchableBackdrop onPress={goBack} />}
+
       {createElement(ScreenTypes[params.type], {
         ...params,
         ...props,
