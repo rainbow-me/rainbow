ab2975d9043ddeef8ffcab5179aa8e9ec095b3f3
osdnk <micosa97@gmail.com>
2022-04-19 15:12:25 +0300

    Fix crash on details sheet

--- src/react-native-cool-modals/ios/UIViewController+slack.swift
+++ src/react-native-cool-modals/ios/UIViewController+slack.swift
@@ -275,9 +275,9 @@ class PanModalViewController: UIViewController, PanModalPresentable, UILayoutSup
     
     let isSlack = self.presentingViewController?.responds(to: NSSelectorFromString("unhackParent")) ?? false
     
-    if (isSlack) {
-      let parentConfig: RNCMScreenView = self.presentingViewController!.value(forKey: "config") as! RNCMScreenView
-      let isHidden: Bool = parentConfig.value(forKey: "_hidden") as? Bool ?? false
+    if isSlack {
+      let parentConfig: RNCMScreenView? = self.presentingViewController?.value(forKey: "config") as? RNCMScreenView
+      let isHidden: Bool = parentConfig?.value(forKey: "_hidden") as? Bool ?? false
       if (isHidden) {
         self.presentingViewController!.dismiss(animated: false)
       }
