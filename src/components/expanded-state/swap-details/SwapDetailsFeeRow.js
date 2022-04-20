a68fa1edc725b537666ccacf19b67c081c62f720
Skylar Barrera <skylar.barrera@gmail.com>
2022-04-20 15:37:07 -0600

    rm fun stuff :(

--- src/components/expanded-state/swap-details/SwapDetailsFeeRow.js
+++ src/components/expanded-state/swap-details/SwapDetailsFeeRow.js
@@ -1,16 +1,8 @@
 import lang from 'i18n-js';
-import { constant, times } from 'lodash';
 import React from 'react';
 import useRainbowFee from '../../../hooks/useRainbowFee';
-
-import { FloatingEmojisTapper } from '../../floating-emojis';
-import SwapDetailsRow, { SwapDetailsValue } from './SwapDetailsRow';
+import SwapDetailsRow from './SwapDetailsRow';
 import { useAccountSettings, useStepper } from '@rainbow-me/hooks';
-
-const emojis = [
-  ...times(3, constant('rainbow')),
-  ...times(2, constant('money_bag')),
-];
 
 export default function SwapDetailsUniswapRow(tradeDetails) {
   const { nativeCurrencySymbol } = useAccountSettings();
@@ -25,21 +17,11 @@ export default function SwapDetailsUniswapRow(tradeDetails) {
   const [step, nextStep] = useStepper(steps.length);
 
   return (
-    <FloatingEmojisTapper
-      activeScale={1.06}
-      disableRainbow
-      distance={150}
-      duration={1500}
-      emojis={emojis}
-      onPress={nextStep}
-      radiusAndroid={30}
-      scaleTo={0}
-      size={50}
-      wiggleFactor={0}
+    <SwapDetailsRow
+      label={`${lang.t('expanded_state.swap_details.rainbow_fee')} 􀅵`}
+      valuePress={nextStep}
     >
-      <SwapDetailsRow label={lang.t('expanded_state.swap_details.rainbow_fee')}>
-        <SwapDetailsValue>{steps[step]}</SwapDetailsValue>
-      </SwapDetailsRow>
-    </FloatingEmojisTapper>
+      {steps[step]}
+    </SwapDetailsRow>
   );
 }
