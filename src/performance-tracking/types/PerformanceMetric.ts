eb1f2b0824be03052ade3d44adb29ede395466fd
Jakub Adamczyk <jakub@rainbow.me>
2022-04-25 12:31:50 +0200

    remove TESt prefix for events

--- src/performance-tracking/types/PerformanceMetric.ts
+++ src/performance-tracking/types/PerformanceMetric.ts
@@ -2,11 +2,11 @@
  * Enum with all performance metrics events strings
  */
 enum PerformanceMetric {
-  loadJSBundle = 'TEST Performance Time To Load JS Bundle',
-  loadRootAppComponent = 'TEST Performance Time To Load Root App Component',
-  timeToInteractive = 'TEST Performance Time To Interactive',
-  useInitializeWallet = 'TEST Performance Wallet Initialize Time',
-  completeStartupTime = 'TEST Performance Complete Startup Time',
+  loadJSBundle = 'Performance Time To Load JS Bundle',
+  loadRootAppComponent = 'Performance Time To Load Root App Component',
+  timeToInteractive = 'Performance Time To Interactive',
+  useInitializeWallet = 'Performance Wallet Initialize Time',
+  completeStartupTime = 'Performance Complete Startup Time',
 }
 
 export default PerformanceMetric;
