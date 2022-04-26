253698a7c4aabc812ba7dea006a7b832540a1819
Jakub Adamczyk <jakub@rainbow.me>
2022-04-26 15:49:50 +0200

    add comment what getProcessStartTime does

--- ios/RNStartTime.m
+++ ios/RNStartTime.m
@@ -9,6 +9,11 @@
 
 RCT_EXPORT_MODULE()
 
+/*
+ function that retrieves the apps process data to get the timestamp the process started
+ timestamps are returned in seconds as a floating point number with fractional second part
+ timestamps are the UNIX Epoch timestamps relative to 00:00 1st Jan 1970 UTC
+ */
 static CFTimeInterval getProcessStartTime() {
   size_t len = 4;
   int mib[len];
