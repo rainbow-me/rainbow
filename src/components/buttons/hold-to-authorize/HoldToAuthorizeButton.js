rainbow-me/rainbow

commit 448d2313437d7b4d5724b471e0dcee1399997277

@benisgold/apply accent color (#2989)
* applied accent color to confirm registration button

* applied accent color to gas speed button

src/components/buttons/hold-to-authorize/HoldToAuthorizeButton.js
@@ -296,6 +296,7 @@ class HoldToAuthorizeButton extends PureComponent {
 const HoldToAuthorizeButtonWithBiometrics = ({
   disableLongPress,
   label,
+  color,
   ...props
 }) => {
   const biometryType = useBiometryType();
@@ -310,6 +311,7 @@ const HoldToAuthorizeButtonWithBiometrics = ({
   return (
     <HoldToAuthorizeButton
       {...props}
+      backgroundColor={color}
       colors={colors}
       deviceDimensions={deviceDimensions}
       enableLongPress={!disableLongPress && isLongPressAvailableForBiometryType}

src/screens/ENSConfirmRegisterSheet.js
@@ -215,6 +215,7 @@ export default function ENSConfirmRegisterSheet() {
             <Box>
               <SheetActionButtonRow paddingBottom={5}>
                 <HoldToAuthorizeButton
+                  color={accentColor}
                   hideInnerBorder
                   isLongPressAvailableForBiometryType
                   label={lang.t('profiles.confirm.commit_button')}
@@ -225,7 +226,11 @@ export default function ENSConfirmRegisterSheet() {
               </SheetActionButtonRow>
             </Box>
             <Box alignItems="center" justifyContent="center">
-              <GasSpeedButton currentNetwork="mainnet" theme="light" />
+              <GasSpeedButton
+                borderColor={accentColor}
+                currentNetwork="mainnet"
+                theme="light"
+              />
             </Box>
           </Box>
         </Box>

src/components/gas/GasSpeedButton.js
@@ -121,6 +121,7 @@ const TransactionTimeLabel = ({ formatter, theme }) => {
 
 const GasSpeedButton = ({
   asset,
+  borderColor = null,
   bottom = 0,
   currentNetwork,
   horizontalPadding = 19,
@@ -413,7 +414,7 @@ const GasSpeedButton = ({
         colorForAsset={
           gasOptionsAvailable
             ? makeColorMoreChill(
-                rawColorForAsset || colors.appleBlue,
+                borderColor || rawColorForAsset || colors.appleBlue,
                 colors.shadowBlack
               )
             : colors.alpha(colors.blueGreyDark, 0.12)
@@ -442,6 +443,7 @@ const GasSpeedButton = ({
       </ContextMenuButton>
     );
   }, [
+    borderColor,
     colors,
     currentNetwork,
     gasIsNotReady,
@@ -562,7 +564,7 @@ const GasSpeedButton = ({
             ) : showGasOptions ? (
               <CustomGasButton
                 borderColor={makeColorMoreChill(
-                  rawColorForAsset || colors.appleBlue,
+                  borderColor || rawColorForAsset || colors.appleBlue,
                   colors.shadowBlack
                 )}
                 onPress={onDonePress}
@@ -573,7 +575,7 @@ const GasSpeedButton = ({
                     theme !== 'light'
                       ? colors.whiteLabel
                       : makeColorMoreChill(
-                          rawColorForAsset || colors.appleBlue,
+                          borderColor || rawColorForAsset || colors.appleBlue,
                           colors.shadowBlack
                         )
                   }

commit 0c1a21ca5ac5e648dd019f527509257c1f647a33

Fix swap button color regression
Caused by https://github.com/rainbow-me/rainbow/pull/2989/files#diff-5408fa5b27b810b3ad4777e8659d9dc65f602f803134286fe1bac59167397c71R314

src/components/buttons/hold-to-authorize/HoldToAuthorizeButton.js
@@ -296,7 +296,6 @@ class HoldToAuthorizeButton extends PureComponent {
 const HoldToAuthorizeButtonWithBiometrics = ({
   disableLongPress,
   label,
-  color,
   ...props
 }) => {
   const biometryType = useBiometryType();
@@ -311,7 +310,6 @@ const HoldToAuthorizeButtonWithBiometrics = ({
   return (
     <HoldToAuthorizeButton
       {...props}
-      backgroundColor={color}
       colors={colors}
       deviceDimensions={deviceDimensions}
       enableLongPress={!disableLongPress && isLongPressAvailableForBiometryType}

src/screens/ENSConfirmRegisterSheet.js
@@ -60,7 +60,7 @@ function TransactionActionRow({
       <Box>
         <SheetActionButtonRow paddingBottom={5}>
           <HoldToAuthorizeButton
-            color={accentColor}
+            backgroundColor={accentColor}
             disabled={!isSufficientGas || !isValidGas}
             hideInnerBorder
             isLongPressAvailableForBiometryType