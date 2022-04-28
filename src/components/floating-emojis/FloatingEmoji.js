c2cca5b671387dc93823ef9875d104e2c3945b5f
Jakub Adamczyk <jakub@rainbow.me>
2022-04-28 12:40:28 +0200

    translate Floating Emoji from Reanimated 1 to Reanimated 2

--- src/components/floating-emojis/FloatingEmoji.js
+++ src/components/floating-emojis/FloatingEmoji.js
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-import Animated, { EasingNode } from 'react-native-reanimated';
-import { useMemoOne } from 'use-memo-one';
-import { interpolate, timing } from '../animations';
+import React, { useEffect } from 'react';
+import Animated, {
+  Easing,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import { Emoji } from '../text';
-
-const { add, concat, multiply, sin } = Animated;
 
 const FloatingEmoji = ({
   centerVertically,
@@ -24,73 +26,81 @@ const FloatingEmoji = ({
   top,
   wiggleFactor,
 }) => {
-  const { opacity, rotate, scale, translateX, translateY } = useMemoOne(() => {
-    const animation = timing({ duration, easing: EasingNode.elastic() });
-    const progress = interpolate(animation, {
-      inputRange: [0, 1],
-      outputRange: [0, distance],
+  const animation = useSharedValue(0);
+
+  useEffect(() => {
+    animation.value = withTiming(1, {
+      duration,
+      easing: Easing.linear,
     });
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const progress = interpolate(animation.value, [0, 1], [0, distance]);
+
+    const opacity = interpolate(
+      progress,
+      [0, distance * (opacityThreshold ?? 0.5), distance - size],
+      [1, fadeOut ? 0.89 : 1, fadeOut ? 0 : 1]
+    );
+
+    const rotate =
+      interpolate(
+        progress,
+        [0, distance / 4, distance / 3, distance / 2, distance],
+        [0, -2, 0, 2, 0]
+      ) + 'deg';
+
+    const scale = interpolate(
+      progress,
+      [0, 15, 30, 50, distance],
+      [0, 1.2, 1.1, 1, scaleTo]
+    );
+
+    const everyThirdEmojiMultiplier = index % 3 === 0 ? 3 : 2;
+    const everySecondEmojiMultiplier = index % 2 === 0 ? -1 : 1;
+    const translateXComponentA =
+      animation.value *
+      size *
+      everySecondEmojiMultiplier *
+      everyThirdEmojiMultiplier;
+
+    const wiggleMultiplierA = Math.sin(progress * (distance / (350 / 15)));
+    const wiggleMultiplierB = interpolate(
+      progress,
+      [0, distance / 10, distance],
+      [10 * wiggleFactor, 6.9 * wiggleFactor, 4.2069 * wiggleFactor]
+    );
+    const translateXComponentB = wiggleMultiplierA * wiggleMultiplierB;
+
+    const translateX = disableHorizontalMovement
+      ? 0
+      : translateXComponentA + translateXComponentB;
+
+    const translateY = disableVerticalMovement ? 0 : -progress;
 
     return {
-      opacity: interpolate(progress, {
-        inputRange: [0, distance * opacityThreshold || 0.5, distance - size],
-        outputRange: [1, fadeOut ? 0.89 : 1, fadeOut ? 0 : 1],
-      }),
-      rotate: concat(
-        interpolate(progress, {
-          inputRange: [0, distance / 4, distance / 3, distance / 2, distance],
-          outputRange: [0, -2, 0, 2, 0],
-        }),
-        'deg'
-      ),
-      scale: interpolate(progress, {
-        inputRange: [0, 15, 30, 50, distance],
-        outputRange: [0, 1.2, 1.1, 1, scaleTo],
-      }),
-      translateX: add(
-        multiply(
-          animation,
-          size,
-          index % 3 === 0 ? 3 : 2,
-          index % 2 === 0 ? -1 : 1
-        ),
-        multiply(
-          sin(multiply(progress, distance / (350 / 15))), // i rly dont understand math plz help
-          interpolate(progress, {
-            inputRange: [0, distance / 10, distance],
-            outputRange: [
-              10 * wiggleFactor,
-              6.9 * wiggleFactor,
-              4.2069 * wiggleFactor,
-            ],
-          })
-        )
-      ),
-      translateY: multiply(animation, distance, -1),
+      opacity,
+      transform: [{ rotate }, { scale }, { translateX }, { translateY }],
     };
-  }, []);
+  });
 
   return (
     <Animated.View
-      style={{
-        left,
-        marginTop,
-        opacity,
-        position: 'absolute',
-        top: centerVertically ? null : top || size * -0.5,
-        transform: [
-          { rotate },
-          { scale },
-          disableHorizontalMovement ? null : { translateX },
-          disableVerticalMovement ? null : { translateY },
-        ],
-      }}
+      style={[
+        {
+          left,
+          marginTop,
+          position: 'absolute',
+          top: centerVertically ? null : top || size * -0.5,
+        },
+        animatedStyle,
+      ]}
     >
       <Emoji name={emoji} size={size} />
     </Animated.View>
   );
 };
-
 FloatingEmoji.propTypes = {
   centerVertically: PropTypes.bool,
   disableHorizontalMovement: PropTypes.bool,
