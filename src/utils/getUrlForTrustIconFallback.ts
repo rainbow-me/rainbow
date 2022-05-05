02fd9ccc043b36db39024db7e047ea70baa7688d
Terry Sahaidak <tasssik@gmail.com>
2022-05-05 13:00:12 +0300

    Revert "Merge remote-tracking branch 'origin/@bruno/lowecase-assets-repo' into @terrysahaidak/test-coinrows"
    This reverts commit 33d3e07316d4ff82b710f698a6cae8ea23612060, reversing
    changes made to 625d8472132c5b8e2ffb94685a4f0c3677ccafee.

--- src/utils/getUrlForTrustIconFallback.ts
+++ src/utils/getUrlForTrustIconFallback.ts
@@ -1,9 +1,10 @@
+import { toChecksumAddress } from '../handlers/web3';
 import { EthereumAddress } from '@rainbow-me/entities';
 
 export default function getUrlForTrustIconFallback(
   address: EthereumAddress
 ): string | null {
   if (!address) return null;
-  const lowercasedAddress = address.toLowerCase();
-  return `https://raw.githubusercontent.com/rainbow-me/assets/develop/blockchains/ethereum/assets/${lowercasedAddress}/logo.png`;
+  const checksummedAddress = toChecksumAddress(address);
+  return `https://raw.githubusercontent.com/rainbow-me/assets/master/blockchains/ethereum/assets/${checksummedAddress}/logo.png`;
 }
