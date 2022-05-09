4f3851d6a377d98d5ac8d47b876d0516d5edf298
Jakub Adamczyk <jakub@rainbow.me>
2022-05-09 09:05:49 +0200

    extract more constants and refactor constant names

--- src/components/buttons/hold-to-authorize/constants.ts
+++ src/components/buttons/hold-to-authorize/constants.ts
@@ -1,3 +1,8 @@
-export const ButtonHeight = 56;
-export const SmallButtonHeight = 46;
-export const TinyButtonHeight = 40;
+// Button Sizes
+export const BUTTON_HEIGHT = 56;
+export const SMALL_BUTTON_HEIGHT = 46;
+export const TINY_BUTTON_HEIGHT = 40;
+
+// Animation Durations
+export const BUTTON_SCALE_DURATION_IN_MS = 150;
+export const LONG_PRESS_DURATION_IN_MS = 500; // 👸 christian approves
