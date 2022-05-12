f34c2ce73b23133daeaa40691f9ee04ac66fa956
Jakub Adamczyk <jakub@rainbow.me>
2022-05-12 20:02:19 +0200

    fix linter unused export

--- src/components/animations/index.js
+++ src/components/animations/index.js
@@ -14,7 +14,6 @@ export {
 } from './ButtonPressAnimation';
 export { default as FlyInAnimation } from './FlyInAnimation';
 export { default as OpacityToggler } from './OpacityToggler';
-export { default as RotationArrow } from './RotationArrow';
 export { default as ScaleInAnimation } from './ScaleInAnimation';
 export { default as ShimmerAnimation } from './ShimmerAnimation';
 export { default as SpinAnimation } from './SpinAnimation';
