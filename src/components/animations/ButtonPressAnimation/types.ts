746bef4e5a9ad4129fecb133d3e74411aa25e20d
Jakub Adamczyk <jakub@rainbow.me>
2022-05-16 17:02:45 +0200

    refine typings to be less lax

--- src/components/animations/ButtonPressAnimation/types.ts
+++ src/components/animations/ButtonPressAnimation/types.ts
@@ -1,13 +1,16 @@
 import {
   NativeSyntheticEvent,
   NativeTouchEvent,
+  PressableProps,
   ViewProps,
 } from 'react-native';
 
 export type TransformOrigin = [number, number];
 export type Direction = 'bottom' | 'left' | 'right' | 'top';
 
-export interface BaseButtonAnimationProps extends Omit<ViewProps, 'hitSlop'> {
+export interface BaseButtonAnimationProps
+  extends Pick<ViewProps, 'onLayout' | 'style' | 'testID'>,
+    Pick<PressableProps, 'onPress'> {
   activeOpacity?: number;
   disabled?: boolean;
   duration?: number;
