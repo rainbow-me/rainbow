0acde3f9c8a1dfe796a41d9e497d5ad58cb8c806
moxey.eth <jakemoxey@gmail.com>
2022-05-17 07:14:48 +1000

    Fix undefined opacity animated value

--- src/components/expanded-state/unique-token/ZoomableWrapper.android.js
+++ src/components/expanded-state/unique-token/ZoomableWrapper.android.js
@@ -150,7 +150,7 @@ export const ZoomableWrapper = ({
 
     const maxWidth = (deviceWidth - containerWidth) / 2;
     return {
-      opacity: opacity.value,
+      opacity: opacity?.value || 1,
       transform: [
         {
           translateY:
