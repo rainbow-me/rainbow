66db886778974447d53c43f049da225cb5457d07
osdnk <micosa97@gmail.com>
2022-05-19 14:53:49 +0300

    useMemo + react.memo

--- src/components/asset-list/RecyclerAssetList2/FastComponents/FastPoolValue.tsx
+++ src/components/asset-list/RecyclerAssetList2/FastComponents/FastPoolValue.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Text } from '@rainbow-me/design-system';
 import { bigNumberFormat } from '@rainbow-me/helpers/bigNumberFormat';
 import { padding } from '@rainbow-me/styles';
 
-const FastPoolValue = ({
+const MemoFastPoolValue = React.memo(function FastPoolValue({
   type,
   value,
   theme,
@@ -14,46 +14,50 @@ const FastPoolValue = ({
   type: string;
   value: number;
   theme: any;
-}) => {
-  let formattedValue: number | string = value;
+}) {
   const { colors } = theme;
-  let color = type === 'oneDayVolumeUSD' ? colors.swapPurple : colors.appleBlue;
+  const { color, formattedValue } = useMemo(() => {
+    let formattedValue: number | string = value;
+    let color =
+      type === 'oneDayVolumeUSD' ? colors.swapPurple : colors.appleBlue;
 
-  if (type === 'annualized_fees' || type === 'profit30d') {
-    let percent: number = value;
-    if (!percent || percent === 0) {
-      formattedValue = '0%';
-    }
-
-    if (percent < 0.0001 && percent > 0) {
-      formattedValue = '< 0.0001%';
-    }
-
-    if (percent < 0 && percent > -0.0001) {
-      formattedValue = '< 0.0001%';
-    }
-
-    let fixedPercent = percent.toFixed(2);
-    if (fixedPercent === '0.00') {
-      formattedValue = '0%';
-    }
-    if (percent > 0) {
-      color = colors.green;
-      if (percent > 100) {
-        formattedValue = `+${percent
-          ?.toFixed(2)
-          .toString()
-          .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}%`;
-      } else {
-        formattedValue = `+${fixedPercent}%`;
+    if (type === 'annualized_fees' || type === 'profit30d') {
+      let percent: number = value;
+      if (!percent || percent === 0) {
+        formattedValue = '0%';
       }
-    } else {
-      formattedValue = `${fixedPercent}%`;
-      color = colors.red;
+
+      if (percent < 0.0001 && percent > 0) {
+        formattedValue = '< 0.0001%';
+      }
+
+      if (percent < 0 && percent > -0.0001) {
+        formattedValue = '< 0.0001%';
+      }
+
+      let fixedPercent = percent.toFixed(2);
+      if (fixedPercent === '0.00') {
+        formattedValue = '0%';
+      }
+      if (percent > 0) {
+        color = colors.green;
+        if (percent > 100) {
+          formattedValue = `+${percent
+            ?.toFixed(2)
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}%`;
+        } else {
+          formattedValue = `+${fixedPercent}%`;
+        }
+      } else {
+        formattedValue = `${fixedPercent}%`;
+        color = colors.red;
+      }
+    } else if (type === 'liquidity' || type === 'oneDayVolumeUSD') {
+      formattedValue = bigNumberFormat(value, nativeCurrency, value >= 10000);
     }
-  } else if (type === 'liquidity' || type === 'oneDayVolumeUSD') {
-    formattedValue = bigNumberFormat(value, nativeCurrency, value >= 10000);
-  }
+    return { color, formattedValue };
+  }, [colors, nativeCurrency, type, value]);
   return (
     <View
       style={[{ backgroundColor: colors.alpha(color, 0.06) }, cx.container]}
@@ -63,9 +67,9 @@ const FastPoolValue = ({
       </Text>
     </View>
   );
-};
+});
 
-export default FastPoolValue;
+export default MemoFastPoolValue;
 
 const cx = StyleSheet.create({
   container: {
