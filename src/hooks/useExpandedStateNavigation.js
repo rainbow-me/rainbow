99b2a7e1d077bc8124e1b3150b892c805a69a2b8
Skylar Barrera <skylar.barrera@gmail.com>
2022-05-24 16:38:00 -0600

    add debug check

--- src/hooks/useExpandedStateNavigation.js
+++ src/hooks/useExpandedStateNavigation.js
@@ -3,11 +3,15 @@ import { useCallback, useMemo } from 'react';
 import { InteractionManager } from 'react-native';
 import useAsset from './useAsset';
 import useWallets from './useWallets';
+import { enableActionsOnReadOnlyWallet } from '@rainbow-me/config/debug';
 import AssetInputTypes from '@rainbow-me/helpers/assetInputTypes';
 import { useNavigation } from '@rainbow-me/navigation';
 import { watchingAlert } from '@rainbow-me/utils';
 
-export default function useExpandedStateNavigation(inputType) {
+export default function useExpandedStateNavigation(
+  inputType,
+  hasBalance = false
+) {
   const { goBack, navigate } = useNavigation();
   const { params } = useRoute();
   const { isReadOnlyWallet } = useWallets();
@@ -18,18 +22,19 @@ export default function useExpandedStateNavigation(inputType) {
     switch (inputType) {
       case AssetInputTypes.in:
         return {
+          hasBalance,
           inputAsset: asset,
         };
       case AssetInputTypes.out:
-        return { outputAsset: asset };
+        return { hasBalance, outputAsset: asset };
       default:
         return { asset };
     }
-  }, [asset, inputType]);
+  }, [asset, hasBalance, inputType]);
 
   return useCallback(
     (routeName, traverseParams) => {
-      if (isReadOnlyWallet) {
+      if (isReadOnlyWallet && !enableActionsOnReadOnlyWallet) {
         watchingAlert();
         return;
       }
