1cc733dd4529c840d6746bc74ae3b28ffde7bd61
Jakub Adamczyk <jakub@rainbow.me>
2022-05-27 13:31:26 +0200

    replace Transitions with Layout Animations in SendEmptyState

--- src/components/send/SendEmptyState.js
+++ src/components/send/SendEmptyState.js
@@ -1,37 +1,11 @@
 import React, { useRef } from 'react';
 import { View } from 'react-native';
-import { Transition, Transitioning } from 'react-native-reanimated';
+import Animated, { Easing, FadeInDown, FadeOut } from 'react-native-reanimated';
 import { sheetVerticalOffset } from '../../navigation/effects';
 import { Icon } from '../icons';
 import { Centered } from '../layout';
 
-const duration = 200;
-const transition = (
-  <Transition.Sequence>
-    <Transition.Out durationMs={duration} interpolation="easeIn" type="fade" />
-    <Transition.Change durationMs={duration} interpolation="easeInOut" />
-    <Transition.Together>
-      <Transition.In
-        delayMs={duration}
-        durationMs={duration}
-        interpolation="easeOut"
-        type="fade"
-      />
-      <Transition.In
-        delayMs={duration}
-        durationMs={duration / 2}
-        interpolation="easeIn"
-        type="scale"
-      />
-      <Transition.In
-        delayMs={duration}
-        durationMs={duration}
-        interpolation="easeInOut"
-        type="slide-bottom"
-      />
-    </Transition.Together>
-  </Transition.Sequence>
-);
+const duration = 100;
 
 const SendEmptyState = () => {
   const ref = useRef();
@@ -66,9 +40,12 @@ const SendEmptyState = () => {
       justify="space-between"
       paddingBottom={sheetVerticalOffset + 19}
     >
-      <Transitioning.View ref={ref} transition={transition}>
+      <Animated.View
+        entering={FadeInDown.duration(duration).easing(Easing.out(Easing.ease))}
+        exiting={FadeOut.duration(duration).easing(Easing.in(Easing.ease))}
+      >
         {icon}
-      </Transitioning.View>
+      </Animated.View>
     </Centered>
   );
 };
