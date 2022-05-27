5b6405d5865f343502c4006247b3ed477cbfdd12
osdnk <micosa97@gmail.com>
2022-05-27 10:56:18 +0300

    Revert "Revert "MainnetAssetDiscovery though the address websocket instead of a Covalent call  (#3368)" (#3403)"
    This reverts commit ee854333043be9d58c04cecd004b52d583c23b1d.

--- src/redux/explorer.js
+++ src/redux/explorer.js
@@ -17,6 +17,7 @@ import {
   fallbackExplorerClearState,
   fallbackExplorerInit,
   fetchOnchainBalances,
+  onMainnetAssetDiscoveryResponse,
 } from './fallbackExplorer';
 // eslint-disable-next-line import/no-cycle
 import { optimismExplorerInit } from './optimismExplorer';
@@ -82,6 +83,7 @@ const messages = {
   CONNECT: 'connect',
   DISCONNECT: 'disconnect',
   ERROR: 'error',
+  MAINNET_ASSET_DISCOVERY: 'received address mainnet-assets-discovery',
   RECONNECT_ATTEMPT: 'reconnect_attempt',
 };
 
@@ -116,6 +118,17 @@ const portfolioSubscription = (address, currency, action = 'get') => [
       portfolio_fields: 'all',
     },
     scope: ['portfolio'],
+  },
+];
+
+const mainnetAssetDisovery = (address, currency, action = 'get') => [
+  action,
+  {
+    payload: {
+      address,
+      currency: toLower(currency),
+    },
+    scope: ['mainnet-assets-discovery'],
   },
 ];
 
@@ -360,6 +373,12 @@ export const explorerInit = () => async (dispatch, getState) => {
       type: EXPLORER_SET_FALLBACK_HANDLER,
     });
   }
+};
+
+export const emitMainnetAssetDiscoveryRequest = (dispatch, getState) => {
+  const { addressSocket } = getState().explorer;
+  const { accountAddress, nativeCurrency } = getState().settings;
+  addressSocket.emit(...mainnetAssetDisovery(accountAddress, nativeCurrency));
 };
 
 export const emitPortfolioRequest = (address, currency) => (
@@ -622,6 +641,10 @@ const listenOnAddressMessages = socket => dispatch => {
     // Fetch balances onchain to override zerion's
     // which is likely behind
     dispatch(fetchOnchainBalances({ keepPolling: false, withPrices: false }));
+  });
+
+  socket.on(messages.MAINNET_ASSET_DISCOVERY, message => {
+    onMainnetAssetDiscoveryResponse(message);
   });
 };
 
