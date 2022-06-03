50f2ee58270462d374adca40592bb986252a1db2
Bruno Barbieri <brunobar79@gmail.com>
2022-06-02 22:38:54 -0400

    kill session only if  we are the ones disconnecting (#3448)

--- src/redux/walletconnect.js
+++ src/redux/walletconnect.js
@@ -467,7 +467,9 @@ const listenOnNewMessages = walletConnector => (dispatch, getState) => {
     if (error) {
       throw error;
     }
-    dispatch(walletConnectDisconnectAllByDappUrl(walletConnector.peerMeta.url));
+    dispatch(
+      walletConnectDisconnectAllByDappUrl(walletConnector.peerMeta.url, false)
+    );
   });
   return walletConnector;
 };
@@ -632,10 +634,10 @@ export const walletConnectRejectSession = (
   dispatch(removePendingRequest(peerId));
 };
 
-export const walletConnectDisconnectAllByDappUrl = dappUrl => async (
-  dispatch,
-  getState
-) => {
+export const walletConnectDisconnectAllByDappUrl = (
+  dappUrl,
+  killSession = true
+) => async (dispatch, getState) => {
   const { walletConnectors } = getState().walletconnect;
   const matchingWalletConnectors = values(
     pickBy(
@@ -651,7 +653,9 @@ export const walletConnectDisconnectAllByDappUrl = dappUrl => async (
       )
     );
     await removeWalletConnectSessions(peerIds);
-    forEach(matchingWalletConnectors, connector => connector?.killSession());
+    if (killSession) {
+      forEach(matchingWalletConnectors, connector => connector?.killSession());
+    }
     dispatch({
       payload: omitBy(
         walletConnectors,
