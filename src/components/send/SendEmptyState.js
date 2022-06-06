ab03f7e7d22398a18dbc90a6a8be75d56a780d51
Jakub Adamczyk <jakub@rainbow.me>
2022-06-06 11:14:47 +0200

    make entering animation more subtle

--- src/components/send/SendEmptyState.js
+++ src/components/send/SendEmptyState.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { Dimensions } from 'react-native';
-import Animated, { Easing, FadeOut, Keyframe } from 'react-native-reanimated';
+import Animated, { Easing, FadeInDown, FadeOut } from 'react-native-reanimated';
 import { sheetVerticalOffset } from '../../navigation/effects';
 import { Icon } from '../icons';
 import { Centered } from '../layout';
 
 const duration = 200;
-
-const screenHeight = Dimensions.get('window').height;
-
-const keyframe = new Keyframe({
-  0: {
-    opacity: 0,
-    transform: [{ translateY: screenHeight }, { scale: 0.0001 }],
-  },
-  100: {
-    easing: Easing.out(Easing.ease),
-    opacity: 1,
-    transform: [{ translateY: 0 }, { scale: 1 }],
-  },
-});
 
 const SendEmptyState = () => {
   const { colors } = useTheme();
@@ -40,7 +25,7 @@ const SendEmptyState = () => {
   if (android) {
     return (
       <Animated.View
-        entering={keyframe.duration(duration)}
+        entering={FadeInDown.duration(duration).easing(Easing.out(Easing.ease))}
         exiting={FadeOut.duration(duration).easing(Easing.in(Easing.ease))}
         style={{ alignItems: 'center', flex: 1 }}
       >
@@ -57,7 +42,7 @@ const SendEmptyState = () => {
       paddingBottom={sheetVerticalOffset + 19}
     >
       <Animated.View
-        entering={keyframe.duration(duration)}
+        entering={FadeInDown.duration(duration).easing(Easing.out(Easing.ease))}
         exiting={FadeOut.duration(duration).easing(Easing.in(Easing.ease))}
       >
         {icon}
