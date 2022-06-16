5a07e260b29055ebdab7ffd076e30e2a049006c3
osdnk <micosa97@gmail.com>
2022-06-16 16:58:50 +0300

    Restrict cloudinary images

--- src/handlers/cloudinary.ts
+++ src/handlers/cloudinary.ts
@@ -1,12 +1,26 @@
 // @ts-ignore
 import { url as cloudinaryURL } from 'cloudinary/lib/cloudinary';
 import { memoFn } from '@rainbow-me/utils/memoFn';
+import logger from 'logger';
 
 type CloudinaryConfig = {
   width: number;
   height: number;
   format: string;
 };
+
+const PixelRatios = [1, 1.5, 2, 3, 3.5];
+const IconsSizes = [40, 36]; // Remove 36 with TopMover
+const allowedIconSizes = PixelRatios.reduce((acc, ratio) => {
+  for (let size of IconsSizes) {
+    acc.push(size * ratio);
+  }
+  return acc;
+}, [] as number[]);
+
+const supportedSizeTransformations = {
+  assets: allowedIconSizes,
+} as { [key: string]: number[] };
 
 export const signUrl = memoFn(
   (url: string, config: Partial<CloudinaryConfig>) => {
@@ -15,6 +29,20 @@ export const signUrl = memoFn(
     if (format) {
       internalAddress = internalAddress.split('.')[0] + '.' + format;
     }
+
+    const directory = internalAddress.split('/')[0];
+
+    if (supportedSizeTransformations[directory] && widthAndHeight.width) {
+      if (
+        !(supportedSizeTransformations[directory] as number[]).includes(
+          widthAndHeight.width
+        )
+      ) {
+        logger.error(url, 'Incorrect size sent to Cloudinary');
+        return null;
+      }
+    }
+
     const cloudinaryImg = cloudinaryURL(internalAddress, {
       sign_url: true,
       ...widthAndHeight,
