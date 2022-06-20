fd423c4b397e3bf40b0de908f5253c394a9fcf53
moxey.eth <jakemoxey@gmail.com>
2022-06-20 10:31:16 +1000

    Fix swap screen keyboard issues

--- src/screens/ExchangeModal.js
+++ src/screens/ExchangeModal.js
@@ -225,11 +225,22 @@ export default function ExchangeModal({
     updateOutputAmount,
   } = useSwapInputHandlers();
 
+  const chainId = useMemo(
+    () => ethereumUtils.getChainIdFromType(inputCurrency?.type),
+    [inputCurrency]
+  );
+
+  const currentNetwork = useMemo(
+    () => ethereumUtils.getNetworkFromChainId(chainId || 1),
+    [chainId]
+  );
+
   const {
     flipCurrencies,
     navigateToSelectInputCurrency,
     navigateToSelectOutputCurrency,
   } = useSwapCurrencyHandlers({
+    currentNetwork,
     defaultInputAsset,
     defaultOutputAsset: defaultOutputAssetOverride,
     fromDiscover,
@@ -241,16 +252,6 @@ export default function ExchangeModal({
     title,
     type,
   });
-
-  const chainId = useMemo(
-    () => ethereumUtils.getChainIdFromType(inputCurrency?.type),
-    [inputCurrency]
-  );
-
-  const currentNetwork = useMemo(
-    () => ethereumUtils.getNetworkFromChainId(chainId || 1),
-    [chainId]
-  );
 
   const basicSwap =
     ChainId.arbitrum === chainId
@@ -700,13 +701,19 @@ export default function ExchangeModal({
   ]);
 
   const handleTapWhileDisabled = useCallback(() => {
-    if (currentNetwork === Network.arbitrum) {
-      navigate(Routes.EXPLAIN_SHEET, {
-        network: currentNetwork,
-        type: 'output_disabled',
-      });
-    }
-  }, [currentNetwork, navigate]);
+    lastFocusedInputHandle?.current?.blur();
+    navigate(Routes.EXPLAIN_SHEET, {
+      network: currentNetwork,
+      onClose: () => {
+        InteractionManager.runAfterInteractions(() => {
+          setTimeout(() => {
+            lastFocusedInputHandle?.current?.focus();
+          }, 250);
+        });
+      },
+      type: 'output_disabled',
+    });
+  }, [currentNetwork, lastFocusedInputHandle, navigate]);
 
   const showConfirmSection = isSavings
     ? !!inputCurrency
@@ -745,15 +752,18 @@ export default function ExchangeModal({
             />
             {showOutputField && (
               <ExchangeOutputField
-                editable={!!outputCurrency}
+                editable={
+                  !!outputCurrency && currentNetwork !== Network.arbitrum
+                }
                 network={currentNetwork}
                 onFocus={handleFocus}
                 onPressSelectOutputCurrency={() =>
                   navigateToSelectOutputCurrency(chainId)
                 }
-                {...(currentNetwork === Network.arbitrum && {
-                  onTapWhileDisabled: handleTapWhileDisabled,
-                })}
+                {...(currentNetwork === Network.arbitrum &&
+                  !!outputCurrency && {
+                    onTapWhileDisabled: handleTapWhileDisabled,
+                  })}
                 outputAmount={outputAmountDisplay}
                 outputCurrencyAddress={outputCurrency?.address}
                 outputCurrencyAssetType={outputCurrency?.type}
