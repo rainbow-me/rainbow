c81cacacc50b967f50f5e4a7a9add0cb0ecf10ed
Marco Burstein <marco@marco.how>
2022-06-27 12:28:45 -0700

    Fix outdated changes.

--- src/hooks/useTimeout.ts
+++ src/hooks/useTimeout.ts
@@ -1,21 +1,21 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { MutableRefObject, useCallback, useEffect, useRef } from 'react';
 
 export default function useTimeout(): [
-  (func: any, ms: any) => void,
+  (func: () => void, ms?: number) => void,
   () => void,
-  React.MutableRefObject<unknown>
+  MutableRefObject<NodeJS.Timeout | null>
 ] {
-  const handle = useRef();
+  const handle = useRef<NodeJS.Timeout | null>(null);
 
-  const start = useCallback((func, ms) => {
-    // @ts-expect-error ts-migrate(2322) FIXME: Type 'number' is not assignable to type 'undefined... Remove this comment to see the full error message
+  const start = useCallback((func: () => void, ms?: number) => {
     handle.current = setTimeout(func, ms);
   }, []);
 
-  const stop = useCallback(
-    () => handle.current && clearTimeout(handle.current),
-    []
-  );
+  const stop = useCallback(() => {
+    if (handle.current) {
+      clearTimeout(handle.current);
+    }
+  }, []);
 
   useEffect(() => () => stop(), [stop]);
 
