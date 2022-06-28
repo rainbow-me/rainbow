e9fdd54a16ebbc6799a7db44d4fb487a4b7aff8c
Esteban Miño <efmino@uc.cl>
2022-06-28 16:05:36 -0400

    Update src/hooks/useSwapDerivedValues.ts
    Co-authored-by: Christopher Howard <derhowiedesigns@gmail.com>

--- src/hooks/useSwapDerivedValues.ts
+++ src/hooks/useSwapDerivedValues.ts
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { AppState } from '@rainbow-me/redux/store';
 import { SwapModalField } from '@rainbow-me/redux/swap';
 
-export default function useSwapCurrencies() {
+export default function useSwapDerivedValues() {
   const derivedValues: { [key in SwapModalField]: string | null } = useSelector(
     (state: AppState) => state.swap.derivedValues
   );
