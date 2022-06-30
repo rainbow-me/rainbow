4b0bf5f30c1ec4ff69e37fb33e7dbc377f6067aa
Terry Sahaidak <tasssik@gmail.com>
2022-06-30 17:47:18 +0300

    Add max wait duration for debounce

--- src/redux/helpers/debouncedUpdateGenericAssets.ts
+++ src/redux/helpers/debouncedUpdateGenericAssets.ts
@@ -1,35 +1,43 @@
+import debounce from 'lodash/debounce';
 import { Dispatch } from 'redux';
 import { DataUpdateGenericAssetsAction } from '../data';
 
-let timeout: undefined | NodeJS.Timeout;
 let payload: DataUpdateGenericAssetsAction['payload'] = {};
 
-// we debounce the dispatching of the action since we receive data from websockets
+// we debounce the dispatching of the action in order to "batch" the events
+// since we receive data from websockets
 // so we might receive lots of events in a small period of time
 // each dispatch will basically trigger random rerenders
 // so in order to not rerender everything and have all the data
 // we store the previous data inside `payload` and dispatch all the data
 // from all the events at once
-export const debouncedUpdateGenericAssets = (
-  action: DataUpdateGenericAssetsAction,
-  dispatch: Dispatch<DataUpdateGenericAssetsAction>
-) => {
-  // this is just a debounce
-  // but since we need some extra params it was easier
-  // to implement debounce with timeouts
-  clearTimeout(timeout!);
-
-  payload = {
-    ...payload,
-    ...action.payload,
-  };
-
-  timeout = setTimeout(() => {
+const debounced = debounce(
+  (
+    action: DataUpdateGenericAssetsAction,
+    dispatch: Dispatch<DataUpdateGenericAssetsAction>
+  ) => {
     dispatch({
       payload: payload,
       type: action.type,
     });
 
     payload = {};
-  }, 500);
+  },
+  500,
+  {
+    // failsafe if zerion decides to bomb us with events
+    maxWait: 5000,
+  }
+);
+
+export const debouncedUpdateGenericAssets = (
+  action: DataUpdateGenericAssetsAction,
+  dispatch: Dispatch<DataUpdateGenericAssetsAction>
+) => {
+  payload = {
+    ...payload,
+    ...action.payload,
+  };
+
+  debounced(action, dispatch);
 };
