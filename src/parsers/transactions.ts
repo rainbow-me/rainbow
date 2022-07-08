571dee41c4a5a3c7109ae6342236a576ea0ce2f3
osdnk <micosa97@gmail.com>
2022-07-08 14:22:43 +0300

    numberWithoutScientificNotation

--- src/parsers/transactions.ts
+++ src/parsers/transactions.ts
@@ -322,22 +322,23 @@ const parseTransactionWithEmptyChanges = async (
 
 // https://rainbowhaus.slack.com/archives/C02C8JQ313N/p1657277922128049
 function numberWithoutScientificNotation(x: number): string | number {
-  let res: string | number = x;
+  let stringValue: string = x.toString();
+  let res = x;
   if (Math.abs(x) < 1.0) {
-    const e = parseInt(x.toString().split('e-')[1]);
+    const e = parseInt(stringValue.split('e-')[1]);
     if (e) {
       res = x * Math.pow(10, e - 1);
       return '0.' + new Array(e).join('0') + res.toString().substring(2);
     }
   } else {
-    let e = parseInt(x.toString().split('+')[1]);
+    let e = parseInt(stringValue.split('+')[1]);
     if (e > 20) {
       e -= 20;
       res = x / Math.pow(10, e);
       return res + new Array(e + 1).join('0');
     }
   }
-  return x;
+  return stringValue;
 }
 
 const parseTransaction = async (
