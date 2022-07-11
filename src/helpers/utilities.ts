d33b9996d6d5afffd68ffce13d591d480d95cf42
perunt <terry.perun@gmail.com>
2022-07-11 22:42:40 +0300

    clean code

--- src/helpers/utilities.ts
+++ src/helpers/utilities.ts
@@ -462,16 +462,6 @@ export const capitalize = (string?: string | undefined) => {
     : '';
 };
 
-/**
- * @desc Converts the first character of string to upper case and the remaining to lower case
- */
-export const startCase = (string?: string | undefined): string => {
-  if (!string) return '';
-  return string
-    ? string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
-    : '';
-};
-
 export const notEmpty = <TValue>(
   value: TValue | null | undefined
 ): value is TValue => {
