87e7a8795d11a6a1d295a11cf6ec208faa9413e4
osdnk <micosa97@gmail.com>
2022-07-12 00:51:32 +0300

    numberWithoutScientificNotation again

--- src/parsers/transactions.ts
+++ src/parsers/transactions.ts
@@ -32,6 +32,7 @@ import {
 import { getTransactionMethodName } from '@rainbow-me/handlers/transactions';
 import { isL2Network } from '@rainbow-me/handlers/web3';
 import { Network } from '@rainbow-me/helpers/networkTypes';
+import { numberWithoutScientificNotation } from '@rainbow-me/helpers/numbers';
 import {
   ETH_ADDRESS,
   savingsAssetsList,
@@ -319,27 +320,6 @@ const parseTransactionWithEmptyChanges = async (
     },
   ];
 };
-
-// https://rainbowhaus.slack.com/archives/C02C8JQ313N/p1657277922128049
-function numberWithoutScientificNotation(x: number): string | number {
-  let stringValue: string = x.toString();
-  let res = x;
-  if (Math.abs(x) < 1.0) {
-    const e = parseInt(stringValue.split('e-')[1]);
-    if (e) {
-      res = x * Math.pow(10, e - 1);
-      return '0.' + new Array(e).join('0') + res.toString().substring(2);
-    }
-  } else {
-    let e = parseInt(stringValue.split('+')[1]);
-    if (e > 20) {
-      e -= 20;
-      res = x / Math.pow(10, e);
-      return res + new Array(e + 1).join('0');
-    }
-  }
-  return stringValue;
-}
 
 const parseTransaction = async (
   transaction: ZerionTransaction,
