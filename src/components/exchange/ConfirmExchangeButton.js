fa485ca6884acdef80a3d0428e03462e1f057b21
Esteban Miño <efmino@uc.cl>
2022-07-19 19:08:11 -0400

    swaps: fix gas loading state (#3755)
    * add is gas ready var
    
    * revert not needed change
    
    * Boolean(gasValue?.amount)

--- src/components/exchange/ConfirmExchangeButton.js
+++ src/components/exchange/ConfirmExchangeButton.js
@@ -32,7 +32,7 @@ export default function ConfirmExchangeButton({
 }) {
   const { inputCurrency, outputCurrency } = useSwapCurrencies();
   const asset = outputCurrency ?? inputCurrency;
-  const { isSufficientGas, isValidGas } = useGas();
+  const { isSufficientGas, isValidGas, isGasReady } = useGas();
   const { name: routeName } = useRoute();
   const { navigate } = useNavigation();
   const [swapSubmitted, setSwapSubmitted] = useReducer(() => true, false);
@@ -101,6 +101,8 @@ export default function ConfirmExchangeButton({
   ]);
 
   let label = '';
+  const loadingQuote = loading || (!loading && !isGasReady);
+
   if (type === ExchangeModalTypes.deposit) {
     label = lang.t('button.confirm_exchange.deposit');
   } else if (type === ExchangeModalTypes.swap) {
@@ -108,8 +110,7 @@ export default function ConfirmExchangeButton({
   } else if (type === ExchangeModalTypes.withdrawal) {
     label = lang.t('button.confirm_exchange.withdraw');
   }
-
-  if (loading) {
+  if (loadingQuote) {
     label = lang.t('button.confirm_exchange.fetching_quote');
   } else if (!isSufficientBalance) {
     label = lang.t('button.confirm_exchange.insufficient_funds');
@@ -163,7 +164,7 @@ export default function ConfirmExchangeButton({
             disabledBackgroundColor={disabledButtonColor}
             hideInnerBorder
             label={label}
-            loading={loading}
+            loading={loadingQuote}
             onLongPress={
               loading || swapSubmitted
                 ? NOOP
