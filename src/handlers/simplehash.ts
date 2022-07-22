f8c6f461174fb2e433929482cf953f588d5a3a6e
jinchung <jin.chung17@gmail.com>
2022-07-22 02:52:55 -0400

    Add simplehash types

--- src/handlers/simplehash.ts
+++ src/handlers/simplehash.ts
@@ -5,6 +5,58 @@ import { RainbowFetchClient } from '../rainbow-fetch';
 import { parseSimplehashNfts } from '@rainbow-me/parsers';
 import { logger } from '@rainbow-me/utils';
 
+interface SimplehashMarketplace {
+  marketplace_name: string;
+  marketplace_collection_id: string;
+  collection_url: string;
+  verified: boolean;
+}
+
+interface SimplehashCollection {
+  collection_id: string;
+  name: string;
+  description: string;
+  image_url: string | null;
+  banner_image_url: string | null;
+  external_url: string | null;
+  twitter_username: string | null;
+  discord_url: string | null;
+  marketplace_pages: SimplehashMarketplace[];
+}
+
+interface SimplehashNft {
+  nft_id: string;
+  chain: string;
+  contract_address: string;
+  token_id: string;
+  name: string | null;
+  description: string | null;
+  image_url: string | null;
+  video_url: string | null;
+  audio_url: string | null;
+  model_url: string | null;
+  previews: {
+    image_small_url: string | null;
+    image_medium_url: string | null;
+    image_large_url: string | null;
+    image_opengraph_url: string | null;
+    blurhash: string | null;
+  };
+  background_color: string | null;
+  external_url: string | null;
+  created_date: string | null;
+  status: string;
+  token_count: number;
+  owner_count: number;
+  contract: {
+    type: string;
+    name: string;
+    symbol: string;
+  };
+  collection: SimplehashCollection;
+  extra_metadata: Record<string, string> | null;
+}
+
 const chains = {
   arbitrum: 'arbitrum',
   ethereum: 'ethereum',
