5d19af61cc8583dcf777570607a01eb2750e45da
Terry Sahaidak <tasssik@gmail.com>
2022-07-22 17:04:34 +0300

    Get rid of uniswap prices entirely

--- src/hooks/useAccountAsset.js
+++ src/hooks/useAccountAsset.js
@@ -1,4 +1,3 @@
-import { isNil } from 'lodash';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
@@ -29,8 +28,6 @@ const getZeroEth = () => {
 };
 
 export const accountAssetsDataSelector = state => state.data.accountAssetsData;
-const assetPricesFromUniswapSelector = state =>
-  state.data.assetPricesFromUniswap;
 const uniqueIdSelector = (_, uniqueId) => uniqueId;
 
 const accountAssetDataSelector = createSelector(
@@ -39,32 +36,10 @@ const accountAssetDataSelector = createSelector(
   (accountAssetsData, uniqueId) => accountAssetsData?.[uniqueId]
 );
 
-const assetPriceFromUniswapSelector = createSelector(
-  assetPricesFromUniswapSelector,
-  uniqueIdSelector,
-  (assetPricesFromUniswap, uniqueId) => assetPricesFromUniswap?.[uniqueId]
-);
-
 const makeAccountAssetSelector = () =>
   createSelector(
     accountAssetDataSelector,
-    assetPriceFromUniswapSelector,
-    (accountAsset, assetPriceFromUniswap) => {
-      if (!accountAsset) return null;
-      const assetUniswapPrice = assetPriceFromUniswap?.price;
-      const assetUniswapRelativeChange =
-        assetPriceFromUniswap?.relativePriceChange;
-      if (isNil(accountAsset?.price) && assetUniswapPrice) {
-        return {
-          ...accountAsset,
-          price: {
-            relative_change_24h: assetUniswapRelativeChange,
-            value: assetUniswapPrice,
-          },
-        };
-      }
-      return accountAsset;
-    }
+    accountAsset => accountAsset ?? null
   );
 
 // this is meant to be used for assets under balances
