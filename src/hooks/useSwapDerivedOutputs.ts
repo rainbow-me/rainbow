04e1272b8c57c95999017efad6ea751f1b8d393d
Esteban Miño <efmino@uc.cl>
2022-07-28 16:48:19 -0400

    cacheTime

--- src/hooks/useSwapDerivedOutputs.ts
+++ src/hooks/useSwapDerivedOutputs.ts
@@ -543,7 +543,7 @@ export default function useSwapDerivedOutputs(chainId: number, type: string) {
     type,
   ]);
   const { data, isLoading } = useQuery({
-    cacheTime: IS_TESTING !== 'true' ? 0 : 10000,
+    cacheTime: IS_TESTING !== 'true' ? undefined : 10000,
     queryFn: getTradeDetails,
     queryKey: [
       'getTradeDetails',
