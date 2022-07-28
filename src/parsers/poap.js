7aa4cf65fa4ca30366eff67b26c396709efb332a
Marco Burstein <marco@marco.how>
2022-07-28 14:54:27 -0700

    Fix POAP parsing to include `fullUniqueId`.

--- src/parsers/poap.js
+++ src/parsers/poap.js
@@ -34,6 +34,12 @@ export const parsePoaps = data => {
       familyImage:
         'https://lh3.googleusercontent.com/FwLriCvKAMBBFHMxcjqvxjTlmROcDIabIFKRp87NS3u_QfSLxcNThgAzOJSbphgQqnyZ_v2fNgMZQkdCYHUliJwH-Q=s60',
       familyName: 'POAP',
+      /**
+       * See also `parseAccountUniqueTokens`. We use "ethereum_" here because
+       * (1) we assume that POAPs are on mainnet and (2) we need to match
+       * the signature used on the web.
+       */
+      fullUniqueId: `ethereum_0x22c1f6050e56d2876009903609a2cc3fef83b415_${event.id}`,
       id: event.id,
       image_original_url: event.image_url,
       image_url: imageToPng(event.image_url, 300),
