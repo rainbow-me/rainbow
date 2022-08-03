bb574ea5436a6e1207824c2e4c43cde5c6c9db0b
Esteban Miño <efmino@uc.cl>
2022-08-03 17:40:21 -0400

    update use memo if image changed

--- src/hooks/useAccountProfile.ts
+++ src/hooks/useAccountProfile.ts
@@ -4,8 +4,7 @@ import useWallets from './useWallets';
 import { getAccountProfileInfo } from '@rainbow-me/helpers/accountInfo';
 
 export default function useAccountProfile() {
-  const wallets = useWallets();
-  const { selectedWallet, walletNames } = wallets;
+  const { selectedWallet, walletNames } = useWallets();
   const {
     accountAddress: accountsettingsAddress,
     network,
