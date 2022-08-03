50421084ec737b91b454f9a07eeaaf9131e7f608
moxey.eth <jakemoxey@gmail.com>
2022-08-04 08:53:56 +1000

    e2e

--- src/components/expanded-state/UniqueTokenExpandedState.tsx
+++ src/components/expanded-state/UniqueTokenExpandedState.tsx
@@ -512,6 +512,8 @@ const UniqueTokenExpandedState = ({
                             )}`}
                             nftShadows
                             onPress={handlePressEdit}
+                            // @ts-expect-error JavaScript component
+                            testID="edit-button"
                             textColor={textColor}
                             weight="heavy"
                           />
