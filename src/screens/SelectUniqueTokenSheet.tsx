29eb6a683467da76393fbd5e8739de2bfe2f8eab
Jakub Adamczyk <jakub@rainbow.me>
2022-08-05 11:36:57 +0200

    move select sheet to profiles feature flag gated section, add option to disable pull down to refresh, add android border radius

--- src/screens/SelectUniqueTokenSheet.tsx
+++ src/screens/SelectUniqueTokenSheet.tsx
@@ -28,12 +28,18 @@ export default function SelectUniqueTokenSheet() {
   });
 
   return (
-    <Box background="body" height="full" paddingTop="34px">
+    <Box
+      background="body"
+      height="full"
+      paddingTop={android ? undefined : '34px'}
+      {...(android && { borderTopRadius: 30 })}
+    >
       <Box alignItems="center" justifyContent="center" paddingVertical="10px">
         {/* @ts-expect-error JavaScript component */}
         <SheetHandle />
       </Box>
       <RecyclerAssetList2
+        disablePullDownToRefresh
         onPressUniqueToken={handlePressUniqueToken}
         type="select-nft"
         walletBriefSectionsData={walletBriefSectionsData}
