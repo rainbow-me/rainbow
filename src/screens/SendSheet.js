85c15e3bc4d180f508f0da27356628c4292a3ced
Esteban Miño <efmino@uc.cl>
2022-08-10 15:12:40 -0400

    fix conf screen height (#3993)

--- src/screens/SendSheet.js
+++ src/screens/SendSheet.js
@@ -22,6 +22,7 @@ import {
   SendHeader,
 } from '../components/send';
 import { SheetActionButton } from '../components/sheet';
+import { getDefaultCheckboxes } from './SendConfirmationSheet';
 import { WrappedAlert as Alert } from '@/helpers/alert';
 import { analytics } from '@rainbow-me/analytics';
 import { PROFILES, useExperimentalFlag } from '@rainbow-me/config';
@@ -769,12 +770,21 @@ export default function SendSheet(props) {
       });
       return;
     }
-
+    const uniqueTokenType = getUniqueTokenType(selected);
+    const isENS = uniqueTokenType === 'ENS';
+    const checkboxes = getDefaultCheckboxes({
+      ensProfile,
+      isENS: true,
+      network,
+      toAddress: recipient,
+    });
     navigate(Routes.SEND_CONFIRMATION_SHEET, {
       amountDetails: amountDetails,
       asset: selected,
       callback: submitTransaction,
+      checkboxes,
       ensProfile,
+      isENS,
       isL2,
       isNft,
       network: currentNetwork,
@@ -792,6 +802,7 @@ export default function SendSheet(props) {
     isNft,
     nativeCurrencyInputRef,
     navigate,
+    network,
     profilesEnabled,
     recipient,
     selected,
