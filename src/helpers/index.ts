089bc9d7559216fd19a701d1d57a3127ee860490
perunt <terry.perun@gmail.com>
2022-08-15 20:58:56 +0300

    create status bar helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,5 @@
+import * as StatusBarHelper from './statusBarHelper';
+
 export { default as BiometryTypes } from './biometryTypes';
 export { default as CurrencySelectionTypes } from './currencySelectionTypes';
 export { default as ExchangeModalTypes } from './exchangeModalTypes';
@@ -5,3 +7,4 @@ export { default as isKeyboardOpen } from './isKeyboardOpen';
 export { default as isReanimatedAvailable } from './isReanimatedAvailable';
 export { default as NetworkTypes, Network } from './networkTypes';
 export { default as TokenSectionTypes } from './tokenSectionTypes';
+export { StatusBarHelper };
