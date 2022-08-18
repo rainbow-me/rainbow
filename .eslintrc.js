f862df0139ca0947a0a1d3db1078434fd4d7bbe3
estrattonbailey <git@esb.lol>
2022-08-18 16:30:48 -0500

    Configure ESLint defaults

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,26 +23,98 @@ const globalVars = parse(babelParse(data, { sourceType: 'module' }))
   );
 
 module.exports = {
-  extends: 'rainbow',
+  env: {
+    es2022: true,
+    browser: true,
+    node: true,
+  },
+
+  parser: "@typescript-eslint/parser",
+
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: ["./tsconfig.json"],
+  },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
+
+    /**
+     * For eslint-plugin-import
+     *
+     * @see https://github.com/import-js/eslint-plugin-import/blob/d45fe21bfa09f61402c68c3d271250d95f9c9ed3/README.md?plain=1#L156
+     */
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+
+    /**
+     * Last, must override thing
+     */
+    'prettier',
+  ],
+
+  globals: globalVars,
+
   settings: {
+    react: {
+      version: 'detect',
+    },
     'import/resolver': {
-      'node': {
-        extensions: [
-          '.js',
-          '.ios.js',
-          '.android.js',
-          '.native.js',
-          '.ts',
-          '.tsx',
-        ],
-      },
-      'babel-module': {
-        alias: {},
-      },
+      typescript: true,
+      node: true,
+      'babel-module': true,
     },
   },
-  plugins: [],
-  globals: globalVars,
-  rules: {},
-  env: { browser: true, node: true },
+
+  rules: {
+    // TODO can fix all of these
+    'no-var': 'warn',
+    'prefer-const': 'warn',
+    'prefer-rest-params': 'warn',
+    'prefer-spread': 'warn',
+    'no-async-promise-executor': 'warn',
+
+    // Some of these are OK while we transition to TS
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'warn',
+    '@typescript-eslint/no-empty-function': 'warn',
+    // TODO could probably fix this
+    '@typescript-eslint/no-var-requires': 'warn',
+    // TODO MUST FIX THIS
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
+
+    'react/prop-types': 'warn', // will be fixed after TS migration
+    'react/display-name': 'warn', // TODO easy fix
+
+    // mimic default eslint
+    'eslint-comments/disable-enable-pair': ["error", {"allowWholeFile": true}],
+
+    /**
+     * @see https://typescript-eslint.io/docs/linting/troubleshooting/#eslint-plugin-import
+     *
+     * From the docs: "We recommend you do not use the following rules, as
+     * TypeScript provides the same checks as part of standard type checking:"
+     */
+    'import/named': 'off',
+    'import/namespace': 'off',
+    'import/default': 'off',
+    'import/no-named-as-default-member': 'off',
+
+    /**
+     * From the docs: "The following rules do not have equivalent checks in TypeScript, so we recommend that you only run them at CI/push time, to lessen the local performance burden."
+     */
+    'import/no-named-as-default': 'off',
+    'import/no-cycle': 'off',
+    'import/no-unused-modules': 'off',
+    'import/no-deprecated': 'off',
+  }
 };
