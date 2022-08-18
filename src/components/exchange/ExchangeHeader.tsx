78785368b3cecd75feaf7f2a0ff1d669b32f39dc
Sam Mason de Caires <sam@rainbow.me>
2022-08-18 12:39:51 +0100

    Convert ExchangeHeader to TS and new DS

---
+++ src/components/exchange/ExchangeHeader.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { SheetHandle } from '../sheet';
+import { Heading, Inset, Stack } from '@rainbow-me/design-system';
+
+interface ExchangeHeaderProps {
+  testID: string;
+  title: string;
+}
+
+export default function ExchangeHeader({ testID, title }: ExchangeHeaderProps) {
+  return (
+    <Inset space="6px">
+      <Stack alignHorizontal="center" space="8px">
+        {/* @ts-expect-error - Javascript Component */}
+        <SheetHandle />
+        {title && (
+          <Heading
+            align="center"
+            size="18px"
+            testID={`${testID}-header`}
+            weight="heavy"
+          >
+            {title}
+          </Heading>
+        )}
+      </Stack>
+    </Inset>
+  );
+}
