21407836e56499981eadd292594e574bb84f8317
Ben Goldberg <bengoldberg@rainbow.me>
2022-08-23 14:23:38 -0700

    small change

--- src/featuresToUnlock/featureUnlockChecks.ts
+++ src/featuresToUnlock/featureUnlockChecks.ts
@@ -2,12 +2,14 @@ import { nftLockedAppIconCheck } from './nftLockedAppIconCheck';
 import { OptimismIcon, SmolIcon } from './unlockableFeatures';
 import { EthereumAddress } from '@/entities';
 
-// a FeatureCheck fn should take in a list of wallet addresses to check for feature unlockability
+// a FeatureUnlockCheck fn should take in a list of wallet addresses to check for feature unlockability
 // and return true if the feature is unlocked, false otherwise
-type FeatureCheck = (walletsToCheck: EthereumAddress[]) => Promise<boolean>;
+type FeatureUnlockCheck = (
+  walletsToCheck: EthereumAddress[]
+) => Promise<boolean>;
 
 // the ordering of this list is IMPORTANT, this is the order that features will be unlocked
-export const featureUnlockChecks: FeatureCheck[] = [
+export const featureUnlockChecks: FeatureUnlockCheck[] = [
   async (walletsToCheck: EthereumAddress[]) =>
     await nftLockedAppIconCheck(OptimismIcon, walletsToCheck),
   async (walletsToCheck: EthereumAddress[]) =>
