afa4d69b6d379c7da99a9965ba2837a8bf72445a
estrattonbailey <git@esb.lol>
2022-08-24 15:04:24 -0500

    feat: add @/env.ts for constants

--- globals.d.ts
+++ globals.d.ts
@@ -3,3 +3,58 @@ declare let ios: boolean;
 // @ts-ignore
 declare let __DEV__: boolean;
 declare let IS_DEV: boolean;
+
+declare module 'react-native-dotenv' {
+  export const IS_TESTING: boolean;
+  export const ENABLE_DEV_MODE: number; // TODO
+  export const SENTRY_ENDPOINT: string;
+  export const SENTRY_ENVIRONMENT: 'production' | 'development'; // TODO
+  export const ARBITRUM_MAINNET_RPC: string;
+  export const DATA_API_KEY: string;
+  export const DATA_ENDPOINT: string;
+  export const DATA_ORIGIN: string;
+  export const ETHEREUM_GOERLI_RPC: string;
+  export const ETHEREUM_GOERLI_RPC_DEV: string;
+  export const ETHEREUM_KOVAN_RPC: string;
+  export const ETHEREUM_KOVAN_RPC_DEV: string;
+  export const ETHEREUM_MAINNET_RPC: string;
+  export const ETHEREUM_MAINNET_RPC_DEV: string;
+  export const ETHEREUM_RINKEBY_RPC: string;
+  export const ETHEREUM_RINKEBY_RPC_DEV: string;
+  export const ETHEREUM_ROPSTEN_RPC: string;
+  export const ETHEREUM_ROPSTEN_RPC_DEV: string;
+  export const OPTIMISM_MAINNET_RPC: string;
+  export const POLYGON_MAINNET_RPC: string;
+  export const RAINBOW_WYRE_MERCHANT_ID: string;
+  export const RAINBOW_WYRE_MERCHANT_ID_TEST: string;
+  export const WYRE_ACCOUNT_ID: string;
+  export const WYRE_ACCOUNT_ID_TEST: string;
+  export const WYRE_ENDPOINT: string;
+  export const WYRE_ENDPOINT_TEST: string;
+  export const WYRE_TOKEN: string;
+  export const WYRE_TOKEN_TEST: string;
+  export const IMGIX_DOMAIN: string;
+  export const IMGIX_TOKEN: string;
+  export const CLOUDINARY_API_KEY: string;
+  export const CLOUDINARY_API_SECRET: string;
+  export const CLOUDINARY_CLOUD_NAME: string;
+  export const PINATA_API_KEY: string;
+  export const PINATA_API_SECRET: string;
+  export const PINATA_API_URL: string;
+  export const PINATA_GATEWAY_URL: string;
+  export const APP_CENTER_READ_ONLY_TOKEN_ANDROID: string;
+  export const APP_CENTER_READ_ONLY_TOKEN_IOS: string;
+  export const CODE_PUSH_DEPLOYMENT_KEY_ANDROID: string;
+  export const CODE_PUSH_DEPLOYMENT_KEY_IOS: string;
+  export const COVALENT_ANDROID_API_KEY: string;
+  export const COVALENT_IOS_API_KEY: string;
+  export const NFT_API_KEY: string;
+  export const NFT_API_URL: string;
+  export const ETHERSCAN_API_KEY: string;
+  export const POAP_API_KEY: string;
+  export const HARDHAT_URL_ANDROID: string;
+  export const HARDHAT_URL_IOS: string;
+  export const SIMPLEHASH_API_KEY: string;
+  export const RAINBOW_MASTER_KEY: string;
+  export const REACT_APP_SEGMENT_API_WRITE_KEY: string;
+}
