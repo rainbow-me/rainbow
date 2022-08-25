0a5ecdc25012d64759bd7e1dde258ddd000175bc
Skylar Barrera <skylar.barrera@gmail.com>
2022-08-24 22:21:57 -0400

    swaps is weird

--- src/screens/SwapsPromoSheet.tsx
+++ src/screens/SwapsPromoSheet.tsx
@@ -1,6 +1,9 @@
+import lang from 'i18n-js';
 import React, { useCallback } from 'react';
 import { useNavigation } from '../navigation/Navigation';
 import { PromoSheet } from '@/components/promos';
+import { CurrencySelectionTypes, ExchangeModalTypes } from '@/helpers';
+import { useSwapCurrencyHandlers } from '@/hooks';
 import SwapsPromoBackground from '@rainbow-me/assets/swapsPromoBackground.png';
 import SwapsPromoHeader from '@rainbow-me/assets/swapsPromoHeader.png';
 import { delay } from '@rainbow-me/helpers/utilities';
@@ -13,13 +16,26 @@ const HEADER_WIDTH = 390;
 export default function SwapsPromoSheet() {
   const { colors } = useTheme();
   const { goBack, navigate } = useNavigation();
+  const { updateInputCurrency } = useSwapCurrencyHandlers({
+    shouldUpdate: false,
+    type: ExchangeModalTypes.swap,
+  });
 
   const navigateToSwaps = useCallback(() => {
     goBack();
     delay(300).then(() =>
-      navigate(Routes.EXCHANGE_MODAL, { fromDiscover: false })
+      navigate(Routes.EXCHANGE_MODAL, {
+        fromDiscover: true,
+        params: {
+          fromDiscover: true,
+          onSelectCurrency: updateInputCurrency,
+          title: lang.t('swap.modal_types.swap'),
+          type: CurrencySelectionTypes.input,
+        },
+        screen: Routes.CURRENCY_SELECT_SCREEN,
+      })
     );
-  }, [goBack, navigate]);
+  }, [goBack, navigate, updateInputCurrency]);
 
   return (
     <PromoSheet
