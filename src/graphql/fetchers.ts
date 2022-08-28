405e5c0c7bc5f06243832a1a57e91523547ab26a
moxey.eth <jakemoxey@gmail.com>
2022-08-29 09:43:19 +1000

    wip: setup codegen

--- src/graphql/fetchers.ts
+++ src/graphql/fetchers.ts
@@ -1,6 +1,5 @@
+import { schemas } from './schemas';
 import { getFetchRequester } from './utils/getFetchRequester';
 import { getSdk as getEnsSdk } from './__generated__/ens';
 
-export const ens = getEnsSdk(
-  getFetchRequester('https://api.thegraph.com/subgraphs/name/ensdomains/ens')
-);
+export const ensFetcher = getEnsSdk(getFetchRequester(schemas.ens.url));
---
+++ src/graphql/schemas.js
@@ -0,0 +1,6 @@
+exports.schemas = {
+  ens: {
+    url: 'https://api.thegraph.com/subgraphs/name/ensdomains/ens',
+    file: './ens.graphql',
+  },
+};
