cb07d2862dce7bb5559790df4cd254b07ff547c6
Ben Goldberg <bengoldberg@rainbow.me>
2022-08-30 14:05:59 -0700

    context menu w/ notifications deeplink

---
+++ src/components/change-wallet/AddressRow.tsx
@@ -0,0 +1,336 @@
+import lang from 'i18n-js';
+import React, { useCallback, useMemo } from 'react';
+import { StyleSheet, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { useTheme } from '../../theme/ThemeContext';
+import { ButtonPressAnimation } from '../animations';
+import { BottomRowText } from '../coin-row';
+import ConditionalWrap from 'conditional-wrap';
+import CoinCheckButton from '../coin-row/CoinCheckButton';
+import { ContactAvatar } from '../contacts';
+import ImageAvatar from '../contacts/ImageAvatar';
+import { Icon } from '../icons';
+import { Centered, Column, ColumnWithMargins, Row } from '../layout';
+import { Text, TruncatedText } from '../text';
+import ContextMenuButton from '@/components/native-context-menu/contextMenu';
+import ContextMenuAndroid from '@/components/native-context-menu/contextMenu.android';
+import useExperimentalFlag, { NOTIFICATIONS } from '@/config/experimentalHooks';
+import {
+  removeFirstEmojiFromString,
+  returnStringFirstEmoji,
+} from '@/helpers/emojiHandler';
+import styled from '@/styled-thing';
+import { fonts, fontWithWidth, getFontSize } from '@/styles';
+import { abbreviations, deviceUtils, profileUtils } from '@/utils';
+import { EditWalletContextMenuActions } from '@/screens/ChangeWalletSheet';
+import { toChecksumAddress } from '@/handlers/web3';
+
+const maxAccountLabelWidth = deviceUtils.dimensions.width - 88;
+const NOOP = () => undefined;
+
+const sx = StyleSheet.create({
+  accountLabel: {
+    fontFamily: fonts.family.SFProRounded,
+    fontSize: getFontSize(fonts.size.lmedium),
+    fontWeight: fonts.weight.medium as '500',
+    letterSpacing: fonts.letterSpacing.roundedMedium,
+    maxWidth: maxAccountLabelWidth,
+  },
+  accountRow: {
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 19,
+  },
+  bottomRowText: {
+    fontWeight: fonts.weight.medium as '500',
+    letterSpacing: fonts.letterSpacing.roundedMedium,
+  },
+  coinCheckIcon: {
+    width: 60,
+  },
+  editIcon: {
+    color: '#0E76FD',
+    fontFamily: fonts.family.SFProRounded,
+    fontSize: getFontSize(fonts.size.large),
+    fontWeight: fonts.weight.heavy as '800',
+    textAlign: 'center',
+  },
+  gradient: {
+    alignSelf: 'center',
+    borderRadius: 24,
+    height: 26,
+    justifyContent: 'center',
+    marginLeft: 19,
+    textAlign: 'center',
+  },
+  rightContent: {
+    flex: 0,
+    flexDirection: 'row',
+    marginLeft: 48,
+  },
+});
+
+const gradientProps = {
+  pointerEvents: 'none',
+  style: sx.gradient,
+};
+
+const StyledTruncatedText = styled(TruncatedText)({
+  ...sx.accountLabel,
+  ...fontWithWidth(sx.accountLabel.fontWeight),
+});
+
+const StyledBottomRowText = styled(BottomRowText)({
+  ...sx.bottomRowText,
+  ...fontWithWidth(sx.bottomRowText.fontWeight),
+});
+
+const ReadOnlyText = styled(Text).attrs({
+  align: 'center',
+  letterSpacing: 'roundedMedium',
+  size: 'smedium',
+  weight: 'semibold',
+})({
+  paddingHorizontal: 8,
+});
+
+const OptionsIcon = ({ onPress }: { onPress: () => void }) => {
+  const { colors } = useTheme();
+  return (
+    <ButtonPressAnimation onPress={onPress} scaleTo={0.9}>
+      <Centered height={40} width={60}>
+        {android ? (
+          <Icon circle color={colors.appleBlue} name="threeDots" tightDots />
+        ) : (
+          <Text style={sx.editIcon}>􀍡</Text>
+        )}
+      </Centered>
+    </ButtonPressAnimation>
+  );
+};
+
+const ContextMenuKeys = {
+  Edit: 'edit',
+  Notifications: 'notifications',
+  Remove: 'remove',
+};
+
+interface AddressRowProps {
+  contextMenuActions: EditWalletContextMenuActions;
+  data: any;
+  editMode: boolean;
+  onPress: () => void;
+}
+
+export default function AddressRow({
+  contextMenuActions,
+  data,
+  editMode,
+  onPress,
+}: AddressRowProps) {
+  const notificationsEnabled = useExperimentalFlag(NOTIFICATIONS);
+
+  const {
+    address,
+    balance,
+    color: accountColor,
+    ens,
+    image: accountImage,
+    isSelected,
+    isReadOnly,
+    label,
+    walletId,
+  } = data;
+
+  const { colors, isDarkMode } = useTheme();
+
+  let cleanedUpBalance = balance;
+  if (balance === '0.00') {
+    cleanedUpBalance = '0';
+  }
+
+  const cleanedUpLabel = useMemo(() => removeFirstEmojiFromString(label), [
+    label,
+  ]);
+
+  const emoji = useMemo(
+    () =>
+      returnStringFirstEmoji(cleanedUpLabel) ||
+      profileUtils.addressHashedEmoji(address),
+    [address, cleanedUpLabel]
+  );
+
+  const displayAddress = useMemo(
+    () => abbreviations.address(toChecksumAddress(address) || '', 4, 6),
+    [address]
+  );
+
+  const walletName = cleanedUpLabel || ens || displayAddress;
+
+  const linearGradientProps = useMemo(
+    () => ({
+      ...gradientProps,
+      colors: [
+        colors.alpha(colors.blueGreyDark, 0.03),
+        colors.alpha(colors.blueGreyDark, isDarkMode ? 0.02 : 0.06),
+      ],
+      end: { x: 1, y: 1 },
+      start: { x: 0, y: 0 },
+    }),
+    [colors, isDarkMode]
+  );
+
+  const menuConfig = useMemo(() => {
+    const menuItems = notificationsEnabled
+      ? [
+          {
+            actionKey: ContextMenuKeys.Remove,
+            actionTitle: lang.t('wallet.action.remove'),
+            icon: {
+              iconType: 'SYSTEM',
+              iconValue: 'trash.fill',
+            },
+            menuAttributes: ['destructive'],
+          },
+          {
+            actionKey: ContextMenuKeys.Notifications,
+            actionTitle: lang.t('wallet.action.notifications'),
+            icon: {
+              iconType: 'SYSTEM',
+              iconValue: 'bell.fill',
+            },
+          },
+          {
+            actionKey: ContextMenuKeys.Edit,
+            actionTitle: lang.t('wallet.action.edit'),
+            icon: {
+              iconType: 'SYSTEM',
+              iconValue: 'pencil',
+            },
+          },
+        ]
+      : [
+          {
+            actionKey: ContextMenuKeys.Remove,
+            actionTitle: lang.t('wallet.action.remove'),
+            icon: {
+              iconType: 'SYSTEM',
+              iconValue: 'trash.fill',
+            },
+            menuAttributes: ['destructive'],
+          },
+          {
+            actionKey: ContextMenuKeys.Edit,
+            actionTitle: lang.t('wallet.action.edit'),
+            icon: {
+              iconType: 'SYSTEM',
+              iconValue: 'pencil',
+            },
+          },
+        ];
+
+    return {
+      menuItems: ios ? menuItems : menuItems.reverse(),
+      menuTitle: walletName,
+    };
+  }, [walletName, notificationsEnabled]);
+
+  const handleSelectMenuItem = useCallback(
+    ({ nativeEvent: { actionKey } }) => {
+      switch (actionKey) {
+        case ContextMenuKeys.Remove:
+          contextMenuActions?.remove(walletId, address);
+          break;
+        case ContextMenuKeys.Notifications:
+          contextMenuActions?.notifications(walletName);
+          break;
+        case ContextMenuKeys.Edit:
+          contextMenuActions?.edit(walletId, address);
+          break;
+        default:
+          break;
+      }
+    },
+    [address, contextMenuActions, walletName, walletId]
+  );
+
+  return (
+    <View style={sx.accountRow}>
+      <ConditionalWrap
+        condition={!editMode}
+        wrap={(children: React.ReactNode) => (
+          <ButtonPressAnimation onPress={onPress} scaleTo={0.98}>
+            {children}
+          </ButtonPressAnimation>
+        )}
+      >
+        <Row align="center">
+          <Row align="center" flex={1} height={59}>
+            {accountImage ? (
+              <ImageAvatar
+                image={accountImage}
+                marginRight={10}
+                size="medium"
+              />
+            ) : (
+              <ContactAvatar
+                color={accountColor}
+                marginRight={10}
+                size="medium"
+                value={emoji}
+              />
+            )}
+            <ColumnWithMargins margin={android ? -6 : 3}>
+              <StyledTruncatedText
+                color={colors.dark}
+                testID={`change-wallet-address-row-label-${walletName}`}
+              >
+                {walletName}
+              </StyledTruncatedText>
+              <StyledBottomRowText
+                color={colors.alpha(colors.blueGreyDark, 0.5)}
+              >
+                {cleanedUpBalance || 0} ETH
+              </StyledBottomRowText>
+            </ColumnWithMargins>
+          </Row>
+          <Column style={sx.rightContent}>
+            {isReadOnly && (
+              <LinearGradient
+                {...linearGradientProps}
+                // @ts-ignore
+                marginRight={editMode || isSelected ? -9 : 19}
+              >
+                <ReadOnlyText color={colors.alpha(colors.blueGreyDark, 0.5)}>
+                  {lang.t('wallet.change_wallet.watching')}
+                </ReadOnlyText>
+              </LinearGradient>
+            )}
+            {!editMode && isSelected && (
+              // @ts-ignore
+              <CoinCheckButton style={sx.coinCheckIcon} toggle={isSelected} />
+            )}
+            {editMode &&
+              (ios ? (
+                <ContextMenuButton
+                  isMenuPrimaryAction
+                  menuConfig={menuConfig}
+                  onPressMenuItem={handleSelectMenuItem}
+                >
+                  <OptionsIcon onPress={NOOP} />
+                </ContextMenuButton>
+              ) : (
+                <ContextMenuAndroid
+                  menuConfig={menuConfig}
+                  isAnchoredToRight
+                  onPressMenuItem={handleSelectMenuItem}
+                >
+                  <OptionsIcon onPress={NOOP} />
+                </ContextMenuAndroid>
+              ))}
+          </Column>
+        </Row>
+      </ConditionalWrap>
+    </View>
+  );
+}
--- src/components/change-wallet/WalletList.tsx
+++ src/components/change-wallet/WalletList.tsx
@@ -7,10 +7,10 @@ import React, {
   useRef,
   useState,
 } from 'react';
-import { StyleSheet } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import Animated, {
   Easing,
+  FadeOut,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
@@ -23,9 +23,11 @@ import { EmptyAssetList } from '../asset-list';
 import { Column } from '../layout';
 import AddressRow from './AddressRow';
 import WalletOption from './WalletOption';
+import { EthereumAddress } from '@rainbow-me/entities';
 import { useAccountSettings } from '@/hooks';
 import styled from '@/styled-thing';
 import { position } from '@/styles';
+import { EditWalletContextMenuActions } from '@/screens/ChangeWalletSheet';
 
 const listTopPadding = 7.5;
 const rowHeight = 59;
@@ -36,7 +38,7 @@ const RowTypes = {
   EMPTY: 2,
 };
 
-const getItemLayout = (data, index) => {
+const getItemLayout = (data: any, index: number) => {
   const { height } = data[index];
   return {
     index,
@@ -45,10 +47,11 @@ const getItemLayout = (data, index) => {
   };
 };
 
-const keyExtractor = item => `${item.walletId}-${item?.id}`;
+const keyExtractor = (item: any) => `${item.walletId}-${item?.id}`;
 
+// @ts-ignore
 const Container = styled.View({
-  height: ({ height }) => height,
+  height: ({ height }: { height: number }) => height,
   marginTop: -2,
 });
 
@@ -61,27 +64,31 @@ const EmptyWalletList = styled(EmptyAssetList).attrs({
   pointerEvents: 'none',
 })({
   ...position.coverAsObject,
-  backgroundColor: ({ theme: { colors } }) => colors.white,
+  backgroundColor: ({ theme: { colors } }: any) => colors.white,
   paddingTop: listTopPadding,
 });
 
-const WalletFlatList = styled(FlatList).attrs(({ showDividers }) => ({
-  contentContainerStyle: {
-    paddingBottom: showDividers ? 9.5 : 0,
-    paddingTop: listTopPadding,
-  },
-  getItemLayout,
-  keyExtractor,
-  removeClippedSubviews: true,
-}))({
+const WalletFlatList = styled(FlatList).attrs(
+  ({ showDividers }: { showDividers: boolean }) => ({
+    contentContainerStyle: {
+      paddingBottom: showDividers ? 9.5 : 0,
+      paddingTop: listTopPadding,
+    },
+    getItemLayout,
+    keyExtractor,
+    removeClippedSubviews: true,
+  })
+)({
   flex: 1,
   minHeight: 1,
 });
 
-const WalletListDivider = styled(Divider).attrs(({ theme: { colors } }) => ({
-  color: colors.rowDividerExtraLight,
-  inset: [0, 15],
-}))({
+const WalletListDivider = styled(Divider).attrs(
+  ({ theme: { colors } }: any) => ({
+    color: colors.rowDividerExtraLight,
+    inset: [0, 15],
+  })
+)({
   marginBottom: 1,
   marginTop: -1,
 });
@@ -91,13 +98,30 @@ const WalletListFooter = styled(Column)({
   paddingTop: 4,
 });
 
+interface Props {
+  accountAddress: EthereumAddress;
+  allWallets: any;
+  contextMenuActions: EditWalletContextMenuActions;
+  currentWallet: any;
+  editMode: boolean;
+  height: number;
+  onChangeAccount: (
+    walletId: string,
+    address: EthereumAddress | undefined
+  ) => void;
+  onPressAddAccount: () => void;
+  onPressImportSeedPhrase: () => void;
+  scrollEnabled: boolean;
+  showDividers: boolean;
+  watchOnly: boolean;
+}
+
 export default function WalletList({
   accountAddress,
   allWallets,
+  contextMenuActions,
   currentWallet,
   editMode,
-  getEditMenuItems,
-  getOnMenuItemPress,
   height,
   onChangeAccount,
   onPressAddAccount,
@@ -105,8 +129,8 @@ export default function WalletList({
   scrollEnabled,
   showDividers,
   watchOnly,
-}) {
-  const [rows, setRows] = useState([]);
+}: Props) {
+  const [rows, setRows] = useState<any[]>([]);
   const [ready, setReady] = useState(false);
   const scrollView = useRef(null);
   const { network } = useAccountSettings();
@@ -115,18 +139,18 @@ export default function WalletList({
 
   // Update the rows when allWallets changes
   useEffect(() => {
-    const seedRows = [];
-    const privateKeyRows = [];
-    const readOnlyRows = [];
+    const seedRows: any[] = [];
+    const privateKeyRows: any[] = [];
+    const readOnlyRows: any[] = [];
 
     if (isEmpty(allWallets)) return;
     const sortedKeys = Object.keys(allWallets).sort();
     sortedKeys.forEach(key => {
       const wallet = allWallets[key];
       const filteredAccounts = wallet.addresses.filter(
-        account => account.visible
+        (account: any) => account.visible
       );
-      filteredAccounts.forEach(account => {
+      filteredAccounts.forEach((account: any) => {
         const row = {
           ...account,
           editMode,
@@ -167,17 +191,16 @@ export default function WalletList({
   }, [
     accountAddress,
     allWallets,
-    currentWallet,
+    currentWallet?.id,
     editMode,
     network,
     onChangeAccount,
-    onPressAddAccount,
     watchOnly,
   ]);
 
   // Update the data provider when rows change
   useEffect(() => {
-    if (rows && rows.length && !ready) {
+    if (rows?.length && !ready) {
       setTimeout(() => {
         setReady(true);
         emptyOpacityAnimation.value = withTiming(0, {
@@ -186,7 +209,7 @@ export default function WalletList({
         });
       }, 50);
     }
-  }, [rows, ready]);
+  }, [rows, ready, emptyOpacityAnimation]);
 
   useLayoutEffect(() => {
     if (ready) {
@@ -203,10 +226,6 @@ export default function WalletList({
     opacity: opacityAnimation.value,
   }));
 
-  const emptyOpacityStyle = useAnimatedStyle(() => ({
-    opacity: emptyOpacityAnimation.value,
-  }));
-
   const renderItem = useCallback(
     ({ item }) => {
       switch (item.rowType) {
@@ -214,12 +233,10 @@ export default function WalletList({
           return (
             <Column height={item.height}>
               <AddressRow
+                contextMenuActions={contextMenuActions}
                 data={item}
                 editMode={editMode}
-                getEditMenuItems={getEditMenuItems}
-                getOnMenuItemPress={getOnMenuItemPress}
                 onPress={item.onPress}
-                watchOnly={watchOnly}
               />
             </Column>
           );
@@ -227,41 +244,46 @@ export default function WalletList({
           return null;
       }
     },
-    [editMode, getEditMenuItems, getOnMenuItemPress, watchOnly]
+    [contextMenuActions, editMode]
   );
 
   return (
     <Container height={height}>
-      <Animated.View style={[StyleSheet.absoluteFill, emptyOpacityStyle]}>
-        <EmptyWalletList />
-      </Animated.View>
-      <WalletsContainer style={opacityStyle}>
-        <WalletFlatList
-          data={rows}
-          initialNumToRender={rows.length}
-          ref={scrollView}
-          renderItem={renderItem}
-          scrollEnabled={scrollEnabled}
-          showDividers={showDividers}
-        />
-        {showDividers && <WalletListDivider />}
-        {!watchOnly && (
-          <WalletListFooter>
-            <WalletOption
-              editMode={editMode}
-              icon="arrowBack"
-              label={`􀁍 ${lang.t('wallet.action.create_new')}`}
-              onPress={onPressAddAccount}
-            />
-            <WalletOption
-              editMode={editMode}
-              icon="arrowBack"
-              label={`􀂍 ${lang.t('wallet.action.add_existing')}`}
-              onPress={onPressImportSeedPhrase}
-            />
-          </WalletListFooter>
-        )}
-      </WalletsContainer>
+      {ready ? (
+        <WalletsContainer style={opacityStyle}>
+          <WalletFlatList
+            data={rows}
+            initialNumToRender={rows.length}
+            ref={scrollView}
+            renderItem={renderItem}
+            scrollEnabled={scrollEnabled}
+            showDividers={showDividers}
+          />
+          {showDividers && <WalletListDivider />}
+          {!watchOnly && (
+            <WalletListFooter>
+              <WalletOption
+                editMode={editMode}
+                label={`􀁍 ${lang.t('wallet.action.create_new')}`}
+                onPress={onPressAddAccount}
+              />
+              <WalletOption
+                editMode={editMode}
+                label={`􀂍 ${lang.t('wallet.action.add_existing')}`}
+                onPress={onPressImportSeedPhrase}
+              />
+            </WalletListFooter>
+          )}
+        </WalletsContainer>
+      ) : (
+        <Animated.View
+          exiting={FadeOut.easing(Easing.out(Easing.ease)).duration(
+            transitionDuration
+          )}
+        >
+          <EmptyWalletList />
+        </Animated.View>
+      )}
     </Container>
   );
 }
