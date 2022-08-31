36327129e7380b68d6f4358bd686502a60c88477
perunt <terry.perun@gmail.com>
2022-08-31 23:00:47 +0300

    fix watchAddressFlow

--- e2e/watchAddressFlow.spec.js
+++ e2e/watchAddressFlow.spec.js
@@ -31,13 +31,6 @@ describe('Watch address flow', () => {
 
   it('Should navigate to the Wallet screen after tapping on "Import Wallet"', async () => {
     await Helpers.waitAndTap('wallet-info-submit-button');
-    if (device.getPlatform() === 'android') {
-      await Helpers.checkIfVisible('pin-authentication-screen');
-      // Set the pin
-      await Helpers.authenticatePin('1234');
-      // Confirm it
-      await Helpers.authenticatePin('1234');
-    }
     await Helpers.checkIfVisible('wallet-screen', 40000);
     await Helpers.enableSynchronization();
   });
