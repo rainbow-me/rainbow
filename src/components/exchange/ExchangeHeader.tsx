ed54b79d2e4cf8c9ae85573d743f1db97493ed3f
Sam Mason de Caires <sam@rainbow.me>
2022-09-01 14:05:26 +0100

    Use correct value from designs

--- src/components/exchange/ExchangeHeader.tsx
+++ src/components/exchange/ExchangeHeader.tsx
@@ -10,7 +10,7 @@ interface ExchangeHeaderProps {
 export default function ExchangeHeader({ testID, title }: ExchangeHeaderProps) {
   return (
     <Inset space="6px">
-      <Stack alignHorizontal="center" space="8px">
+      <Stack alignHorizontal="center" space="10px">
         {/* @ts-expect-error - Javascript Component */}
         <SheetHandle />
         {title && (
