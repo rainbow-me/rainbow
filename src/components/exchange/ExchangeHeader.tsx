c8e4c9577931eda6f90b7f2f83a36c035ac713f3
Sam Mason de Caires <sam@rainbow.me>
2022-09-02 11:52:50 +0100

    Add testId back in before conditional component

--- src/components/exchange/ExchangeHeader.tsx
+++ src/components/exchange/ExchangeHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SheetHandle } from '../sheet';
-import { Heading, Inset, Stack } from '@/design-system';
+import { Box, Heading, Inset, Stack } from '@/design-system';
 
 interface ExchangeHeaderProps {
   testID: string;
@@ -9,21 +9,18 @@ interface ExchangeHeaderProps {
 
 export default function ExchangeHeader({ testID, title }: ExchangeHeaderProps) {
   return (
-    <Inset space="6px">
-      <Stack alignHorizontal="center" space="10px">
-        {/* @ts-expect-error - Javascript Component */}
-        <SheetHandle />
-        {title && (
-          <Heading
-            align="center"
-            size="18px"
-            testID={`${testID}-header`}
-            weight="heavy"
-          >
-            {title}
-          </Heading>
-        )}
-      </Stack>
-    </Inset>
+    <Box testID={`${testID}-header`}>
+      <Inset space="6px">
+        <Stack alignHorizontal="center" space="10px">
+          {/* @ts-expect-error - Javascript Component */}
+          <SheetHandle />
+          {title && (
+            <Heading align="center" size="18px" weight="heavy">
+              {title}
+            </Heading>
+          )}
+        </Stack>
+      </Inset>
+    </Box>
   );
 }
