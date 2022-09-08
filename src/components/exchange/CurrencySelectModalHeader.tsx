6d82dd09b47537f2c246df06248d4dfc1b85eedc
Sam Mason de Caires <sam@rainbow.me>
2022-09-08 17:42:09 +0100

    Update CurrencyModalHeader to TS and new DS

--- src/components/exchange/CurrencySelectModalHeader.tsx
+++ src/components/exchange/CurrencySelectModalHeader.tsx
@@ -3,41 +3,12 @@ import React, { useCallback } from 'react';
 import { delayNext } from '../../hooks/useMagicAutofocus';
 import { CoinIcon } from '../coin-icon';
 import { BackButton } from '../header';
-import { Centered } from '../layout';
 import { SheetHandleFixedToTop } from '../sheet';
-import { TruncatedText } from '../text';
-import { Inset } from '@/design-system';
+import { Box, Inset, Text } from '@/design-system';
 import { useNavigation } from '@/navigation';
 import Routes from '@/navigation/routesNames';
-import styled from '@/styled-thing';
-import { borders, padding } from '@/styles';
-import { ThemeContextProps } from '@/theme';
-
-const BackButtonWrapper = styled(Centered)({
-  bottom: 0,
-  left: 0,
-  position: 'absolute',
-  top: 3,
-});
 
 export const CurrencySelectModalHeaderHeight = 59;
-const HeaderContainer = styled(Centered)({
-  ...borders.buildRadiusAsObject('top', 12),
-  backgroundColor: ({ theme: { colors } }: { theme: ThemeContextProps }) =>
-    colors.white,
-  height: CurrencySelectModalHeaderHeight,
-  width: '100%',
-});
-
-const Title = styled(TruncatedText).attrs({
-  align: 'center',
-  lineHeight: 'loose',
-  size: 'large',
-  weight: 'heavy',
-})({
-  ...padding.object(1, 0, 0),
-  height: 21,
-});
 
 export default function CurrencySelectModalHeader({
   handleBackButton,
@@ -64,11 +35,23 @@ export default function CurrencySelectModalHeader({
   }, [dangerouslyGetState, handleBackButton, navigate]);
 
   return (
-    <HeaderContainer>
+    <Box
+      height={{ custom: CurrencySelectModalHeaderHeight }}
+      justifyContent="center"
+      alignItems="center"
+      width="full"
+    >
       {/** @ts-expect-error JavaScript component */}
       {showHandle && <SheetHandleFixedToTop />}
       {showBackButton && (
-        <BackButtonWrapper>
+        <Box
+          position="absolute"
+          bottom={{ custom: 0 }}
+          left={{ custom: 0 }}
+          top={{ custom: 3 }}
+          justifyContent="center"
+          alignItems="center"
+        >
           {/** @ts-expect-error JavaScript component */}
           <BackButton
             direction="left"
@@ -78,7 +61,7 @@ export default function CurrencySelectModalHeader({
             textChevron={android}
             throttle
           />
-        </BackButtonWrapper>
+        </Box>
       )}
       {showCoinIcon && (
         <Inset right="4px" top={android ? '2px' : '3px'}>
@@ -90,7 +73,15 @@ export default function CurrencySelectModalHeader({
           />
         </Inset>
       )}
-      <Title>{title}</Title>
-    </HeaderContainer>
+
+      <Text
+        size="18px / 27px (Deprecated)"
+        weight="heavy"
+        numberOfLines={1}
+        align="center"
+      >
+        {title}
+      </Text>
+    </Box>
   );
 }
