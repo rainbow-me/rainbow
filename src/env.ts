563aa0abb4eb928d72fc26ff8d35297d4d43330b
estrattonbailey <git@esb.lol>
2022-09-13 12:41:46 -0500

    fix: IS_TEST assignment

--- src/env.ts
+++ src/env.ts
@@ -19,5 +19,5 @@ export const IS_WEB = web;
 
 export const IS_DEV =
   (typeof __DEV__ === 'boolean' && __DEV__) || !!Number(ENABLE_DEV_MODE);
-export const IS_TEST = !!IS_TESTING;
+export const IS_TEST = IS_TESTING === 'true';
 export const IS_PROD = !IS_DEV && !IS_TEST;
