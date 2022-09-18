4b4eec995ed6c1d501f84b8d9a8c91a4e0e091de
moxey.eth <jakemoxey@gmail.com>
2022-09-19 09:53:36 +1000

    migrate to modern insets

--- src/components/expanded-state/CustomGasState.js
+++ src/components/expanded-state/CustomGasState.js
@@ -1,7 +1,6 @@
 import { useIsFocused, useRoute } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { getSoftMenuBarHeight } from 'react-native-extra-dimensions-android';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import Divider from '../Divider';
 import { ExchangeHeader } from '../exchange';
 import { FloatingPanel } from '../floating-panels';
@@ -20,7 +19,7 @@ import {
 import { useNavigation } from '@/navigation';
 import styled from '@/styled-thing';
 import { margin } from '@/styles';
-import { deviceUtils } from '@/utils';
+import { deviceUtils, safeAreaInsetValues } from '@/utils';
 
 const FOOTER_HEIGHT = 76;
 const CONTENT_HEIGHT = 310;
@@ -82,7 +81,9 @@ export default function CustomGasState({ asset }) {
         removeTopPadding: true,
       })}
       backgroundColor={colors.transparent}
-      contentHeight={ios ? longFormHeight : deviceHeight - getStatusBarHeight()}
+      contentHeight={
+        ios ? longFormHeight : deviceHeight - safeAreaInsetValues.top
+      }
       radius={0}
       scrollEnabled={false}
     >
