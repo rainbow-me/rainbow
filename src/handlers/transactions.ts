69bad3cd7f6ece6452e2445e935f2b5ea494f3a6
Esteban Miño <efmino@uc.cl>
2022-09-20 21:36:58 -0300

    get tx status clean up

--- src/handlers/transactions.ts
+++ src/handlers/transactions.ts
@@ -6,6 +6,10 @@ import { CONTRACT_FUNCTION } from '@/apollo/queries';
 import {
   RainbowTransaction,
   TransactionStatusTypes,
+  TransactionDirection,
+  TransactionStatus,
+  TransactionType,
+  TransactionTypes,
   ZerionTransaction,
 } from '@/entities';
 import store from '@/redux/store';
@@ -21,6 +25,7 @@ import {
   abbreviations,
   ethereumUtils,
   showActionSheetWithOptions,
+  isLowerCaseMatch
 } from '@/utils';
 import { getRandomColor } from '@/styles/colors';
 import startCase from 'lodash/startCase';
@@ -29,6 +34,10 @@ import Routes from '@rainbow-me/routes';
 import { Navigation } from '@/navigation';
 import { Contact } from '@/redux/contacts';
 import { Network } from '@/helpers';
+import { TransactionResponse } from '@ethersproject/abstract-provider';
+import { getTitle, getTransactionLabel } from '@/parsers';
+import { isZero } from '@/helpers/utilities';
+import { fetchWalletENSAvatars, fetchWalletNames } from '@/redux/wallets';
 
 const parseSignatureToTitle = (signature: string) => {
   const rawName = signature.match(/^([^)(]*)\((.*)\)([^)(]*)$/u);
@@ -208,4 +217,106 @@ export const showTransactionDetailsSheet = (
       }
     );
   }
+}
+
+/**
+ * Returns the `TransactionStatus` that represents completion for a given
+ * transaction type.
+ *
+ * @param type The transaction type.
+ * @returns The confirmed status.
+ */
+const getConfirmedState = (type?: TransactionType): TransactionStatus => {
+  switch (type) {
+    case TransactionTypes.authorize:
+      return TransactionStatus.approved;
+    case TransactionTypes.deposit:
+      return TransactionStatus.deposited;
+    case TransactionTypes.withdraw:
+      return TransactionStatus.withdrew;
+    case TransactionTypes.receive:
+      return TransactionStatus.received;
+    case TransactionTypes.purchase:
+      return TransactionStatus.purchased;
+    default:
+      return TransactionStatus.sent;
+  }
+};
+
+export const getTransactionReceiptStatus = async (
+  transaction: RainbowTransaction,
+  nonceAlreadyIncluded: boolean,
+  txObj?: TransactionResponse
+): Promise<TransactionStatus> => {
+  let receipt;
+  let status;
+  try {
+    if (txObj) {
+      receipt = await txObj.wait();
+    }
+  } catch (e: any) {
+    // https://docs.ethers.io/v5/api/providers/types/#providers-TransactionResponse
+    if (e.transaction) {
+      // if a transaction field exists, it was confirmed but failed
+      status = TransactionStatus.failed;
+    } else {
+      // cancelled or replaced
+      status = TransactionStatus.cancelled;
+    }
+  }
+  status = receipt?.status || 0;
+
+  if (!isZero(status)) {
+    const isSelf = isLowerCaseMatch(
+      transaction?.from || '',
+      transaction?.to || ''
+    );
+    const transactionDirection = isSelf
+      ? TransactionDirection.self
+      : TransactionDirection.out;
+    const transactionStatus =
+      transaction.status === TransactionStatus.cancelling
+        ? TransactionStatus.cancelled
+        : getConfirmedState(transaction?.type);
+    status = getTransactionLabel({
+      direction: transactionDirection,
+      pending: false,
+      protocol: transaction?.protocol,
+      status: transactionStatus,
+      type: transaction?.type,
+    });
+  } else if (nonceAlreadyIncluded) {
+    status = TransactionStatus.unknown;
+  } else {
+    status = TransactionStatus.failed;
+  }
+  return status;
+};
+
+export const getTransactionFlashbotStatus = async (txHash: string) => {
+  const fbStatus = await fetch(`https://protect.flashbots.net/tx/${txHash}`);
+  const fbResponse = await fbStatus.json();
+  return fbResponse.status;
+};
+
+export const getPendingTransaction = (
+  transaction: RainbowTransaction,
+  status: TransactionStatus
+) => {
+  const updatedPending = { ...transaction };
+  const minedAt = Math.floor(Date.now() / 1000);
+  const title = getTitle({
+    protocol: transaction.protocol,
+    status,
+    type: transaction.type,
+  });
+  updatedPending.title = title;
+  updatedPending.pending = false;
+  updatedPending.minedAt = minedAt;
+  return updatedPending;
+};
+
+export const fetchWalletENSDataAfterRegistration = async () => {
+  await store.dispatch(fetchWalletENSAvatars());
+  store.dispatch(fetchWalletNames());
 };
