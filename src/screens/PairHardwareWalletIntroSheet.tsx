41f66e10414f6a292b1884770bf006ed8e37076f
moxey.eth <jakemoxey@gmail.com>
2022-09-28 13:57:57 +1000

    kill 40px

--- src/screens/PairHardwareWalletIntroSheet.tsx
+++ src/screens/PairHardwareWalletIntroSheet.tsx
@@ -22,7 +22,7 @@ export function PairHardwareWalletIntroSheet() {
   return (
     <Layout
       header={
-        <Inset horizontal="40px">
+        <Inset horizontal="44px">
           <Stack alignHorizontal="center" space="20px">
             <Text align="center" color="label" weight="bold" size="26pt">
               {lang.t('hardware_wallets.pair_your_nano')}
