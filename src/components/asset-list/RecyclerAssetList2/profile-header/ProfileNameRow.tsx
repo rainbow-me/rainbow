57e4cb87ed190eb2ec687bbba4156718ae3ff88c
moxey.eth <jakemoxey@gmail.com>
2022-09-28 11:53:34 +1000

    add long press to copy address

--- src/components/asset-list/RecyclerAssetList2/profile-header/ProfileNameRow.tsx
+++ src/components/asset-list/RecyclerAssetList2/profile-header/ProfileNameRow.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useRecoilState } from 'recoil';
+import Clipboard from '@react-native-community/clipboard';
 import { ButtonPressAnimation } from '@/components/animations';
 import { Icon } from '@/components/icons';
 import { Box, Inline, Text, useForegroundColor } from '@/design-system';
@@ -6,6 +8,9 @@ import { useAccountProfile, useDimensions } from '@/hooks';
 import { useNavigation } from '@/navigation';
 import { abbreviateEnsForDisplay } from '@/utils/abbreviations';
 import Routes from '@rainbow-me/routes';
+import { addressCopiedToastAtom } from '@/screens/WalletScreen';
+import { FloatingEmojis } from '@/components/floating-emojis';
+import { haptics } from '@/utils';
 
 export const ProfileNameRowHeight = 16;
 
@@ -14,14 +19,31 @@ export function ProfileNameRow({ testIDPrefix }: { testIDPrefix?: string }) {
   // Account
   const { accountENS, accountName } = useAccountProfile();
 
+  const onNewEmoji = React.useRef<() => void>();
+
   // ////////////////////////////////////////////////////
   // Name & press handler
 
   const { navigate } = useNavigation();
+  const [isToastActive, setToastActive] = useRecoilState(
+    addressCopiedToastAtom
+  );
+  const { accountAddress } = useAccountProfile();
 
   const onPressName = () => {
     navigate(Routes.CHANGE_WALLET_SHEET);
   };
+  const onLongPressName = React.useCallback(() => {
+    if (!isToastActive) {
+      setToastActive(true);
+      setTimeout(() => {
+        setToastActive(false);
+      }, 2000);
+    }
+    haptics.notificationSuccess();
+    onNewEmoji?.current && onNewEmoji.current();
+    Clipboard.setString(accountAddress);
+  }, [accountAddress, isToastActive, setToastActive]);
 
   const name = accountENS
     ? abbreviateEnsForDisplay(accountENS, 20)
@@ -49,10 +71,24 @@ export function ProfileNameRow({ testIDPrefix }: { testIDPrefix?: string }) {
     <>
       {name && (
         <ButtonPressAnimation
+          onLongPress={onLongPressName}
           onPress={onPressName}
           scale={0.8}
           testID={testIDPrefix ? `${testIDPrefix}-${name}` : undefined}
         >
+          {/* @ts-expect-error – JS component */}
+          <FloatingEmojis
+            distance={150}
+            duration={500}
+            fadeOut={false}
+            scaleTo={0}
+            size={50}
+            wiggleFactor={0}
+            // @ts-expect-error – JS component
+            setOnNewEmoji={newOnNewEmoji =>
+              (onNewEmoji.current = newOnNewEmoji)
+            }
+          />
           <Inline alignVertical="center" space="4px" wrap={false}>
             <Box style={{ maxWidth }}>
               <Text
