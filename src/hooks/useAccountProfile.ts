d3eaa6e4cf5d027d648c972c0a1e3b09c9f8b10f
Esteban Miño <efmino@uc.cl>
2022-10-03 10:40:36 -0300

    useAccountProfile refactor to get updated state from wallets

--- src/hooks/useAccountProfile.ts
+++ src/hooks/useAccountProfile.ts
@@ -3,9 +3,12 @@ import { createSelector } from 'reselect';
 import { EthereumAddress } from '@/entities';
 import { getAccountProfileInfo } from '@/helpers/accountInfo';
 import { AppState } from '@/redux/store';
+import { RainbowWallet } from '@/model/wallet';
+import { findWalletWithAccount } from '@/helpers/findWalletWithAccount';
 
 const walletSelector = (state: AppState) => ({
-  selectedWallet: state.wallets.selected || {},
+  wallets: state.wallets.wallets,
+  selectedWallet: state.wallets.selected || ({} as RainbowWallet),
   walletNames: state.wallets.walletNames,
 });
 const settingsSelector = (state: AppState) => ({
@@ -14,13 +17,18 @@ const settingsSelector = (state: AppState) => ({
 
 const buildAccountProfile = (
   wallet: {
-    selectedWallet: any;
+    wallets: { [id: string]: RainbowWallet } | null;
+    selectedWallet: RainbowWallet;
     walletNames: { [a: EthereumAddress]: string };
   },
   account: { accountAddress: string }
 ) => {
+  const selectedWallet = findWalletWithAccount(
+    wallet.wallets || {},
+    account.accountAddress
+  );
   return getAccountProfileInfo(
-    wallet.selectedWallet,
+    selectedWallet || wallet.selectedWallet,
     wallet.walletNames,
     account.accountAddress
   );
