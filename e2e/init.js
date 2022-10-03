846b53987ce5e275f3f69a19cb67f398212b526f
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2022-10-03 17:07:37 +0300

    Remove uninstall

--- e2e/init.js
+++ e2e/init.js
@@ -15,8 +15,6 @@ beforeAll(async () => {
     await exec(
       'yarn adb-all shell cmd overlay enable com.android.internal.systemui.navbar.threebutton'
     );
-    await exec('/opt/homebrew/share/android-commandlinetools/platform-tools/adb emu kill');
-    await exec('/opt/homebrew/share/android-commandlinetools/emulator/emulator -avd Pixel_5_API_31 -dns-server 8.8.8.8 -no-snapshot-load');
   }
   await device.clearKeychain();
 
