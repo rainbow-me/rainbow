108bd9c8f10934f4b1a48a898e61d230810240b6
Sam Mason de Caires <sam@rainbow.me>
2022-10-06 14:36:09 +0100

    Check for both conditions and file tidy up

--- src/components/expanded-state/swap-details/SwapDetailsContent.js
+++ src/components/expanded-state/swap-details/SwapDetailsContent.js
@@ -70,7 +70,7 @@ export default function SwapDetailsContent({
             </SwapDetailsValue>
           </SwapDetailsRow>
 
-          {isRefuelTx && (
+          {(isRefuelTx || tradeDetails?.refuel) && (
             <SwapDetailsRefuelRow
               testID="swaps-details-refuel-row"
               tradeDetails={tradeDetails}
