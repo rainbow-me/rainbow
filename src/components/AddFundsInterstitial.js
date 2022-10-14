cb2b1d000b702bd6d8095b61232a019e0cbf104d
Skylar Barrera <skylar.barrera@gmail.com>
2022-10-14 10:49:15 -0400

    one more

--- src/components/AddFundsInterstitial.js
+++ src/components/AddFundsInterstitial.js
@@ -258,7 +258,7 @@ const AddFundsInterstitial = ({ network }) => {
             <AmountButton
               amount={200}
               backgroundColor={colors.swapPurple}
-              color={colors.alpha(colors.blueGreyDark, 0.6)}
+              color={colors.neonSkyblue}
               onPress={handlePressAmount}
             />
             <AmountButton
