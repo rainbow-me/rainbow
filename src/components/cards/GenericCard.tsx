9090d7c21b9f691c10468a93e099c0ad8b57d20a
Ben Goldberg <bengoldberg@rainbow.me>
2022-10-24 17:05:12 -0700

    fix GenericCard button

--- src/components/cards/GenericCard.tsx
+++ src/components/cards/GenericCard.tsx
@@ -4,6 +4,7 @@ import { ButtonPressAnimation } from '../animations';
 import LinearGradient from 'react-native-linear-gradient';
 import { globalColors } from '@/design-system/color/palettes';
 import { deviceUtils } from '@/utils';
+import ConditionalWrap from 'conditional-wrap';
 
 // (device width - (horizontal inset * 2 + padding between cards)) / # of cards in row
 export const SquareCardHeight = (deviceUtils.dimensions.width - 60) / 2;
@@ -23,35 +24,40 @@ export const GenericCard = ({
   onPress,
   color,
 }: GenericCardProps) => (
-  <Box width="full">
-    <ButtonPressAnimation onPress={onPress} disabled={!onPress} scaleTo={0.92}>
-      <AccentColorProvider color={color ?? globalColors.grey100}>
-        <Box
-          background={color ? 'accent' : 'surfacePrimaryElevated'}
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          {...(gradient && {
-            as: LinearGradient,
-            colors: gradient,
-            end: { x: 1, y: 0.5 },
-            start: { x: 0, y: 0.5 },
-          })}
-          width={type === 'square' ? { custom: SquareCardHeight } : 'full'}
-          height={
-            type === 'square'
-              ? {
-                  custom: SquareCardHeight,
-                }
-              : undefined
-          }
-          borderRadius={24}
-          // @ts-expect-error - Box will take flex prop it just doesn't want to
-          flex={0}
-          shadow={color ? '18px accent' : '18px'}
-          padding="20px"
-        >
-          {children}
-        </Box>
-      </AccentColorProvider>
-    </ButtonPressAnimation>
-  </Box>
+  <ConditionalWrap
+    condition={onPress}
+    wrap={(children: React.ReactNode) => (
+      <ButtonPressAnimation onPress={onPress} scaleTo={0.92}>
+        {children}
+      </ButtonPressAnimation>
+    )}
+  >
+    <AccentColorProvider color={color ?? globalColors.grey100}>
+      <Box
+        background={color ? 'accent' : 'surfacePrimaryElevated'}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...(gradient && {
+          as: LinearGradient,
+          colors: gradient,
+          end: { x: 1, y: 0.5 },
+          start: { x: 0, y: 0.5 },
+        })}
+        width={type === 'square' ? { custom: SquareCardHeight } : 'full'}
+        height={
+          type === 'square'
+            ? {
+                custom: SquareCardHeight,
+              }
+            : undefined
+        }
+        borderRadius={24}
+        // @ts-expect-error - Box will take flex prop it just doesn't want to
+        flex={0}
+        shadow={color ? '18px accent' : '18px'}
+        padding="20px"
+      >
+        {children}
+      </Box>
+    </AccentColorProvider>
+  </ConditionalWrap>
 );
