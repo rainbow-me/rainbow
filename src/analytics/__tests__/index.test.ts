2e3e0102c5d66edd840537f1bb007158fea7100a
estrattonbailey <git@esb.lol>
2022-10-25 12:14:14 -0500

    fix: update test

--- src/analytics/__tests__/index.test.ts
+++ src/analytics/__tests__/index.test.ts
@@ -79,7 +79,8 @@ describe('@/analytics', () => {
     analytics.identify({ currency: 'USD' });
     analytics.screen(Routes.BACKUP_SHEET);
 
-    expect(analytics.client.track).not.toHaveBeenCalled();
+    // called once to let us know it was disabled
+    expect(analytics.client.track).toHaveBeenCalledTimes(1);
     expect(analytics.client.identify).not.toHaveBeenCalled();
     expect(analytics.client.screen).not.toHaveBeenCalled();
   });
