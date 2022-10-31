c1511b44d83e044eab8d3655018d41f68c53839f
Eric Bailey <git@esb.lol>
2022-10-31 16:22:50 -0500

    feat: swap in new PromoSheet, update copy and images
    (cherry picked from commit 80ade5e632cf38db467f5a997a0d3a1183da5a36)

--- src/screens/NotificationsPromoSheet.tsx
+++ src/screens/NotificationsPromoSheet.tsx
@@ -1,19 +1,24 @@
 import React, { useCallback } from 'react';
-import { useNavigation } from '../navigation/Navigation';
+
+import { useNavigation } from '@/navigation/Navigation';
 import { CampaignKey } from '@/campaigns/campaignChecks';
-import { PromoSheet } from '@/components/promos';
-import SwapsPromoBackground from '@/assets/swapsPromoBackground.png';
-import SwapsPromoHeader from '@/assets/swapsPromoHeader.png';
+import { PromoSheet } from '@/components/PromoSheet';
+import backgroundImage from '@/assets/notificationsPromoSheetBackground.png';
+import headerImageIOS from '@/assets/notificationsPromoSheetHeaderIOS.png';
+import headerImageAndroid from '@/assets/notificationsPromoSheetHeaderAndroid.png';
 import { delay } from '@/helpers/utilities';
 import Routes from '@/navigation/routesNames';
 import { useTheme } from '@/theme';
+import * as i18n from '@/languages';
+import { IS_IOS } from '@/env';
 
-const HEADER_HEIGHT = 285;
+const HEADER_HEIGHT = 255;
 const HEADER_WIDTH = 390;
 
 export default function NotificationsPromoSheet() {
   const { colors } = useTheme();
   const { goBack, navigate } = useNavigation();
+  const translations = i18n.l.promos.notifications_launch;
 
   const navigateToNotifications = useCallback(() => {
     goBack();
@@ -31,16 +36,41 @@ export default function NotificationsPromoSheet() {
     <PromoSheet
       accentColor={colors.whiteLabel}
       backgroundColor={colors.trueBlack}
-      backgroundImage={SwapsPromoBackground}
+      backgroundImage={backgroundImage}
       campaignKey={CampaignKey.notificationsLaunch}
-      headerImage={SwapsPromoHeader}
+      headerImage={IS_IOS ? headerImageIOS : headerImageAndroid}
       headerImageAspectRatio={HEADER_WIDTH / HEADER_HEIGHT}
-      icon1="􀖅"
-      icon2="􀯮"
-      icon3="􀙨"
-      iconGradient={colors.gradients.swapPurpleTintToSwapPurple}
-      onPress={navigateToNotifications}
       sheetHandleColor={colors.whiteLabel}
+      header={i18n.t(translations.header)}
+      subHeader={i18n.t(translations.subheader)}
+      primaryButtonProps={{
+        label: i18n.t(translations.primary_button),
+        onPress: navigateToNotifications,
+      }}
+      secondaryButtonProps={{
+        label: i18n.t(translations.secondary_button),
+        onPress: goBack,
+      }}
+      items={[
+        {
+          title: i18n.t(translations.info_row_1.title),
+          description: i18n.t(translations.info_row_1.title),
+          icon: '􀖅',
+          gradient: colors.gradients.swapPurpleTintToSwapPurple,
+        },
+        {
+          title: i18n.t(translations.info_row_2.title),
+          description: i18n.t(translations.info_row_2.description),
+          icon: '􀯮',
+          gradient: colors.gradients.swapPurpleTintToSwapPurple,
+        },
+        {
+          title: i18n.t(translations.info_row_3.title),
+          description: i18n.t(translations.info_row_3.description),
+          icon: '􀙨',
+          gradient: colors.gradients.swapPurpleTintToSwapPurple,
+        },
+      ]}
     />
   );
 }
