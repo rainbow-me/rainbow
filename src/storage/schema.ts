ff4ba6417a7c68f7bbb80279cadc9ef8d25cdb27
Eric Bailey <git@esb.lol>
2022-11-01 11:16:02 -0500

    feat: new Segment interface (#4202)
    * feat: initSentry
    
    * init
    
    * better constructor
    
    * updates
    
    * migrate off keychain for analytics
    
    * updates
    
    * chore: move hashing to utils
    
    * feat: add setDeviceId
    
    * feat: remove old deviceId handling
    
    * feat: update setCurrentWalletAddressHash
    
    * feat: update wallet address hash handling
    
    * fix: cleanup
    
    * fix: polish events
    
    * feat: add tests, update some things
    
    * feat: add disablement, debug
    
    * fix: use real env
    
    * fix: remove unused imports
    
    * feat: remove route types for now
    
    * feat: set default values, identify user in Segment
    
    * fix: harden app init
    
    * fix: remove public keywords, needed for testing
    
    * docs: add some docs, events, debug
    
    * fix: update test
    
    * fix: migrate initial load events to new setup
    
    * fix: remove unused imports
    
    * feat: consistent naming, replace in App.js
    
    (cherry picked from commit 0702c9f5abfaf0b651d6a493d7dcf9e933223965)
    
    * feat: enable dev-only instances
    
    * fix: tighten types
    
    * fix: update getOrCreateDeviceId, handle first app open event properly
    
    * fix: handle failure in App better
    
    * fix: cast to string, add important comments
    
    Co-authored-by: Skylar Barrera <skylar.barrera@gmail.com>

--- src/storage/schema.ts
+++ src/storage/schema.ts
@@ -4,4 +4,11 @@
 export type Device = {
   id: string;
   doNotTrack: boolean;
+
+  /**
+   * Undefined on first load of the app, meaning they're a new user. We set
+   * this to `true` immediately, so that on subsequent loads we knew they've
+   * already opened the app at least once.
+   */
+  isReturningUser: boolean;
 };
