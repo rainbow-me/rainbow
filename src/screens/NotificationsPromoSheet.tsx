6c334124d61fff698492b5499f65e361ef226213
Eric Bailey <git@esb.lol>
2022-11-02 15:48:17 -0500

    feat: refactor, is functional

--- src/screens/NotificationsPromoSheet.tsx
+++ src/screens/NotificationsPromoSheet.tsx
@@ -1,5 +1,7 @@
 import React, { useCallback } from 'react';
+import * as perms from 'react-native-permissions';
 
+import useAppState from '@/hooks/useAppState';
 import { useNavigation } from '@/navigation/Navigation';
 import { CampaignKey } from '@/campaigns/campaignChecks';
 import { PromoSheet } from '@/components/PromoSheet';
@@ -11,14 +13,29 @@ import Routes from '@/navigation/routesNames';
 import { useTheme } from '@/theme';
 import * as i18n from '@/languages';
 import { IS_IOS } from '@/env';
+import { logger, RainbowError } from '@/logger';
 
 const HEADER_HEIGHT = 255;
 const HEADER_WIDTH = 390;
+const TRANSLATIONS = i18n.l.promos.notifications_launch;
 
-export default function NotificationsPromoSheet() {
+export function NotificationsPromoSheetInner({
+  permissions,
+  requestNotificationPermissions,
+}: {
+  permissions: perms.NotificationsResponse;
+  requestNotificationPermissions: () => Promise<void>;
+}) {
   const { colors } = useTheme();
   const { goBack, navigate } = useNavigation();
-  const translations = i18n.l.promos.notifications_launch;
+
+  const { status, settings } = permissions;
+  const hasSettingsEnabled = Boolean(
+    Object.values(settings).find(s => Boolean(s))
+  );
+  const notificationsEnabled = status === perms.RESULTS.GRANTED;
+  const notificationsDenied = status === perms.RESULTS.DENIED;
+  const notificationsBlocked = status === perms.RESULTS.BLOCKED;
 
   const navigateToNotifications = useCallback(() => {
     goBack();
@@ -32,6 +49,39 @@ export default function NotificationsPromoSheet() {
     });
   }, [goBack, navigate]);
 
+  const primaryButtonOnPress = useCallback(async () => {
+    if (notificationsDenied) {
+      logger.debug(
+        `NotificationsPromoSheet: notifications permissions denied (could be default state)`
+      );
+      await requestNotificationPermissions();
+    } else if (!hasSettingsEnabled || notificationsBlocked) {
+      logger.debug(
+        `NotificationsPromoSheet: notifications permissions either blocked or all settings are disabled`
+      );
+      await perms.openSettings();
+    } else if (notificationsEnabled) {
+      logger.debug(
+        `NotificationsPromoSheet: notifications permissions enabled`
+      );
+      navigateToNotifications();
+    } else {
+      logger.error(
+        new RainbowError(`NotificationsPromoSheet: reached invalid state`),
+        {
+          permissions,
+        }
+      );
+    }
+  }, [
+    permissions,
+    hasSettingsEnabled,
+    notificationsDenied,
+    notificationsEnabled,
+    notificationsBlocked,
+    navigateToNotifications,
+  ]);
+
   return (
     <PromoSheet
       accentColor={colors.whiteLabel}
@@ -41,36 +91,70 @@ export default function NotificationsPromoSheet() {
       headerImage={IS_IOS ? headerImageIOS : headerImageAndroid}
       headerImageAspectRatio={HEADER_WIDTH / HEADER_HEIGHT}
       sheetHandleColor={colors.whiteLabel}
-      header={i18n.t(translations.header)}
-      subHeader={i18n.t(translations.subheader)}
+      header={i18n.t(TRANSLATIONS.header)}
+      subHeader={i18n.t(TRANSLATIONS.subheader)}
       primaryButtonProps={{
-        label: i18n.t(translations.primary_button),
-        onPress: navigateToNotifications,
+        label:
+          notificationsEnabled && hasSettingsEnabled
+            ? i18n.t(TRANSLATIONS.primary_button.permissions_enabled)
+            : i18n.t(TRANSLATIONS.primary_button.permissions_not_enabled),
+        onPress: primaryButtonOnPress,
       }}
       secondaryButtonProps={{
-        label: i18n.t(translations.secondary_button),
+        label: i18n.t(TRANSLATIONS.secondary_button),
         onPress: goBack,
       }}
       items={[
         {
-          title: i18n.t(translations.info_row_1.title),
-          description: i18n.t(translations.info_row_1.title),
+          title: i18n.t(TRANSLATIONS.info_row_1.title),
+          description: i18n.t(TRANSLATIONS.info_row_1.title),
           icon: '􀖅',
           gradient: colors.gradients.swapPurpleTintToSwapPurple,
         },
         {
-          title: i18n.t(translations.info_row_2.title),
-          description: i18n.t(translations.info_row_2.description),
+          title: i18n.t(TRANSLATIONS.info_row_2.title),
+          description: i18n.t(TRANSLATIONS.info_row_2.description),
           icon: '􀯮',
           gradient: colors.gradients.swapPurpleTintToSwapPurple,
         },
         {
-          title: i18n.t(translations.info_row_3.title),
-          description: i18n.t(translations.info_row_3.description),
+          title: i18n.t(TRANSLATIONS.info_row_3.title),
+          description: i18n.t(TRANSLATIONS.info_row_3.description),
           icon: '􀙨',
           gradient: colors.gradients.swapPurpleTintToSwapPurple,
         },
       ]}
     />
   );
+}
+
+export default function NotificationsPromoSheet() {
+  const { justBecameActive } = useAppState();
+  const [
+    permissionsCheckResult,
+    setPermissionsCheckResult,
+  ] = React.useState<perms.NotificationsResponse>();
+
+  const checkPermissions = React.useCallback(async () => {
+    const result = await perms.checkNotifications();
+    setPermissionsCheckResult(result);
+  }, [setPermissionsCheckResult]);
+
+  const requestNotificationPermissions = React.useCallback(async () => {
+    // TODO what perms
+    const result = await perms.requestNotifications(['alert', 'badge']);
+    setPermissionsCheckResult(result);
+  }, [setPermissionsCheckResult]);
+
+  // checks initially, then each time after app state becomes active
+  React.useEffect(() => {
+    checkPermissions();
+  }, [justBecameActive, checkPermissions]);
+
+  return permissionsCheckResult !== undefined ? (
+    <NotificationsPromoSheetInner
+      permissions={permissionsCheckResult}
+      requestNotificationPermissions={requestNotificationPermissions}
+    />
+  ) : null;
 }
