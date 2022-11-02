4d9b1fc4d56f99bb68dd31be4a9acc26beb27c4a
Eric Bailey <git@esb.lol>
2022-11-02 12:28:42 -0500

    fix: properly mock MMKV, add tests

--- src/migrations/__tests__/index.test.ts
+++ src/migrations/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect, beforeEach, afterEach } from '@jest/globals';
+import { describe, test, expect } from '@jest/globals';
 import { MMKV } from 'react-native-mmkv';
 
 import { runMigrations } from '@/migrations';
@@ -8,27 +8,44 @@ import {
   MIGRATIONS_STORAGE_ID,
 } from '@/migrations/types';
 
-beforeEach(() => {
-  jest.useFakeTimers();
-});
-
-afterEach(() => {
-  jest.useRealTimers();
-});
-
 describe(`@/migrations`, () => {
   const storage = new MMKV({ id: MIGRATIONS_STORAGE_ID });
 
-  // TODO not working, wrong MMKV instance
-  test.skip(`runMigrations for new migration`, async () => {
-    const name = 'migration_foo' as MigrationName;
-    const migration: Migration = {
-      name,
-      async migrate() {},
+  const fooMigrationName = 'migration_foo' as MigrationName;
+  const fooMigration: Migration = {
+    name: fooMigrationName,
+    async migrate() {},
+  };
+
+  test(`new migration runs once`, async () => {
+    jest.useFakeTimers();
+    const currDate = new Date().toUTCString();
+    await runMigrations([fooMigration]);
+    jest.useRealTimers();
+
+    expect(storage.getString(fooMigrationName)).toEqual(
+      JSON.stringify({ data: currDate })
+    );
+
+    await runMigrations([fooMigration]);
+
+    // not run again
+    expect(storage.getString(fooMigrationName)).toEqual(
+      JSON.stringify({ data: currDate })
+    );
+  });
+
+  test(`migration that throws exits and does not mark as complete`, async () => {
+    const throwMigrationName = 'migration_throw' as MigrationName;
+    const throwMigration: Migration = {
+      name: throwMigrationName,
+      async migrate() {
+        throw new Error('throws');
+      },
     };
 
-    await runMigrations([migration]);
+    await runMigrations([throwMigration]);
 
-    expect(storage.getString(name)).toEqual(new Date().toUTCString());
+    expect(storage.getString(throwMigrationName)).toBeFalsy();
   });
 });
