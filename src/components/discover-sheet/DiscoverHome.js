9dbc351dc35b593229759c8c44d741427de11ab1
Ben Goldberg <bengoldberg@rainbow.me>
2022-11-03 15:24:50 -0700

    arrange discover home

--- src/components/discover-sheet/DiscoverHome.js
+++ src/components/discover-sheet/DiscoverHome.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import useExperimentalFlag, { PROFILES } from '../../config/experimentalHooks';
 import BottomSpacer from './BottomSpacer';
-import Lists from './ListsSection';
-import TopMoversSection from './TopMoversSection';
-import UniswapPools from './UniswapPoolsSection';
-import { isTestnetNetwork } from '@/handlers/web3';
-import { Columns, Inline, Inset, Stack } from '@/design-system';
-import { useAccountAsset, useAccountSettings } from '@/hooks';
-import { ETH_ADDRESS } from '@/references';
-import { isZero } from '@/helpers/utilities';
+import { Inline, Inset, Stack } from '@/design-system';
 import { LearnCard } from '../cards/LearnCard';
-import { getCardColorways, learnCards } from '../cards/utils/constants';
+import { learnCards } from '../cards/utils/constants';
 import {
   ActionCard,
   DPICard,
@@ -20,57 +12,26 @@ import {
 } from '../cards';
 
 export default function DiscoverHome() {
-  const { accountAddress, network } = useAccountSettings();
-  const accountAsset = useAccountAsset(ETH_ADDRESS);
-  const profilesEnabled = useExperimentalFlag(PROFILES);
-  const testNetwork = isTestnetNetwork(network);
   return (
     <React.Fragment>
-      <Inset top={{ custom: 16 }}>
-        <Stack space="30px (Deprecated)">
-          {false ? (
-            <Inset horizontal={ios && '19px (Deprecated)'}>
-              <Stack space={ios && '19px (Deprecated)'}>
-                <Columns space={ios && '19px (Deprecated)'}>
-                  <GasCard />
-                  <ENSSearchCard />
-                </Columns>
-                <ENSCreateProfileCard />
-                <LearnCard cardDetails={learnCards[0]} type="stretch" />
-                <Inline space="20px">
-                  <LearnCard cardDetails={learnCards[1]} type="square" />
-                  <LearnCard cardDetails={learnCards[2]} type="square" />
-                </Inline>
-                <DPICard />
-              </Stack>
-            </Inset>
-          ) : (
-            <Inset horizontal="20px">
-              <Stack space="20px">
-                <TopMoversSection />
-                <ENSCreateProfileCard />
-                <Inline space="20px">
-                  <GasCard />
-                  <ENSSearchCard />
-                </Inline>
-                <DPICard />
-                <LearnCard cardDetails={learnCards[0]} type="stretch" />
-                <Inline space="20px">
-                  <ActionCard
-                    colorway="green"
-                    sfSymbolIcon="􀅼"
-                    title="Buy Crypto with Cash"
-                    onPress={() => {}}
-                  />
-                  <LearnCard cardDetails={learnCards[2]} type="square" />
-                </Inline>
-              </Stack>
-            </Inset>
-          )}
-          <Stack space="30px (Deprecated)">
-            <Lists />
-            {accountAddress ? <UniswapPools /> : null}
-          </Stack>
+      <Inset space="20px">
+        <Stack space="20px">
+          <ENSCreateProfileCard />
+          <Inline space="20px">
+            <GasCard />
+            <ENSSearchCard />
+          </Inline>
+          <DPICard />
+          <LearnCard cardDetails={learnCards[0]} type="stretch" />
+          <Inline space="20px">
+            <ActionCard
+              colorway="green"
+              sfSymbolIcon="􀅼"
+              title="Buy Crypto with Cash"
+              onPress={() => {}}
+            />
+            <LearnCard cardDetails={learnCards[2]} type="square" />
+          </Inline>
         </Stack>
       </Inset>
       <BottomSpacer />
