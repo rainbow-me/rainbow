2b0a98851a4cb14225954a52d73e81d1509fed07
Skylar Barrera <skylar.barrera@gmail.com>
2022-11-21 12:51:42 -0500

    use meteorology

--- src/handlers/gasFees.ts
+++ src/handlers/gasFees.ts
@@ -1,7 +1,4 @@
-import {
-  GasFeesBscGasStationData,
-  GasFeesPolygonGasStationData,
-} from '@/entities/gas';
+import { Network } from '@/helpers';
 import { RainbowFetchClient } from '../rainbow-fetch';
 
 const rainbowMeteorologyApi = new RainbowFetchClient({
@@ -13,50 +10,5 @@ const rainbowMeteorologyApi = new RainbowFetchClient({
   timeout: 30000, // 30 secs
 });
 
-export const rainbowMeteorologyGetData = () =>
-  rainbowMeteorologyApi.get(`/meteorology/v1/gas/mainnet`, {});
-
-/**
- * Configuration for Polygon GAS Station API
- * @type RainbowFetchClient instance
- */
-const polygonGasStationApi = new RainbowFetchClient({
-  baseURL: 'https://gpoly.blockscan.com',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  },
-  timeout: 30000, // 30 secs
-});
-
-/**
- * @desc get Polygon gas prices
- * @return {Promise}
- */
-export const polygonGasStationGetGasPrices = (): Promise<{
-  data: GasFeesPolygonGasStationData;
-}> => polygonGasStationApi.get(`/gasapi.ashx?apikey=key&method=gasoracle`);
-
-/**
- * Configuration for BSC GAS Station API
- * @type RainbowFetchClient instance
- */
-const bscGasStationApi = new RainbowFetchClient({
-  baseURL: 'https://api.bscscan.com/api',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  },
-  timeout: 30000, // 30 secs
-});
-
-/**
- * @desc get BSC gas prices
- * @return {Promise}
- */
-export const bscGasStationGetGasPrices = (): Promise<{
-  data: GasFeesBscGasStationData;
-}> =>
-  bscGasStationApi.get(
-    `?module=gastracker&action=gasoracle&apikey=YourApiKeyToken`
-  );
+export const rainbowMeteorologyGetData = (network: Network) =>
+  rainbowMeteorologyApi.get(`/meteorology/v1/gas/${network}`, {});
