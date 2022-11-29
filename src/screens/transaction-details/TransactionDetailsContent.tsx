326c89fc98baf6fe5c5ff525bcc9ab318749007f
Jakub Adamczyk <jakub@rainbow.me>
2022-11-29 13:01:27 +0100

    add missing separator

--- src/screens/transaction-details/TransactionDetailsContent.tsx
+++ src/screens/transaction-details/TransactionDetailsContent.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box } from '@/design-system';
+import { Bleed, Box, Separator } from '@/design-system';
 import { RainbowTransaction } from '@/entities';
 import { SingleLineTransactionDetailsRow } from '@/screens/transaction-details/SingleLineTransactionDetailsRow';
 import { shortenTxHashString } from '@/screens/transaction-details/shortenTxHashString';
@@ -8,6 +8,12 @@ import { ethereumUtils } from '@/utils';
 type Props = {
   transaction: RainbowTransaction;
 };
+
+const Divider = () => (
+  <Bleed horizontal="20px">
+    <Separator color="separatorTertiary" />
+  </Bleed>
+);
 
 export const TransactionDetailsContent: React.FC<Props> = ({ transaction }) => {
   const hash = ethereumUtils.getHash(transaction);
@@ -20,6 +26,7 @@ export const TransactionDetailsContent: React.FC<Props> = ({ transaction }) => {
       paddingHorizontal="20px"
       paddingBottom="20px"
     >
+      <Divider />
       <SingleLineTransactionDetailsRow
         icon={'􀆃'}
         title={'Tx Hash'}
