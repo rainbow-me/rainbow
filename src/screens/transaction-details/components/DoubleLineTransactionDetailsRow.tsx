b0e9729d88be363ccf980bed2970a7618adc4095
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2022-12-01 13:46:14 +0100

    self review

--- src/screens/transaction-details/components/DoubleLineTransactionDetailsRow.tsx
+++ src/screens/transaction-details/components/DoubleLineTransactionDetailsRow.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Column, Columns, Inline, Stack, Text } from '@/design-system';
+import React from "react";
+import { Box, Column, Columns, Inline, Stack, Text } from "@/design-system";
 
 type Props = {
   leftComponent: React.ReactNode;
@@ -13,41 +13,39 @@ export const DoubleLineTransactionDetailsRow: React.FC<Props> = ({
   secondaryValue,
   value,
   title,
-}) => {
-  return (
-    <Box paddingVertical="20px">
-      <Columns space="10px" alignVertical="center">
-        <Column width="content">{leftComponent}</Column>
-        <Stack space="10px">
-          <Inline>
-            <Text
-              color="labelTertiary"
-              size="13pt"
-              numberOfLines={1}
-              weight="semibold"
-            >
-              {title}
-            </Text>
-          </Inline>
-          <Columns>
-            <Text color="label" size="17pt" weight="semibold">
-              {value}
-            </Text>
-            {secondaryValue !== undefined && (
-              <Column width="content">
-                <Text
-                  color="labelTertiary"
-                  size="17pt"
-                  weight="medium"
-                  numberOfLines={1}
-                >
-                  {secondaryValue}
-                </Text>
-              </Column>
-            )}
-          </Columns>
-        </Stack>
-      </Columns>
-    </Box>
-  );
-};
+}) => (
+  <Box paddingVertical="20px">
+    <Columns space="10px" alignVertical="center">
+      <Column width="content">{leftComponent}</Column>
+      <Stack space="10px">
+        <Inline>
+          <Text
+            color="labelTertiary"
+            size="13pt"
+            numberOfLines={1}
+            weight="semibold"
+          >
+            {title}
+          </Text>
+        </Inline>
+        <Columns>
+          <Text color="label" size="17pt" weight="semibold">
+            {value}
+          </Text>
+          {secondaryValue !== undefined && (
+            <Column width="content">
+              <Text
+                color="labelTertiary"
+                size="17pt"
+                weight="medium"
+                numberOfLines={1}
+              >
+                {secondaryValue}
+              </Text>
+            </Column>
+          )}
+        </Columns>
+      </Stack>
+    </Columns>
+  </Box>
+);
