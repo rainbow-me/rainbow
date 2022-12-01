2415a4ce809473c8c60da7264b5501ba92c55014
Ben Goldberg <bengoldberg@rainbow.me>
2022-11-30 16:24:03 -0800

    lots of secret input work

--- src/screens/ImportWalletSheet.tsx
+++ src/screens/ImportWalletSheet.tsx
@@ -1,16 +1,38 @@
 import { Input } from '@/components/inputs';
 import { SlackSheet } from '@/components/sheet';
-import { Box, globalColors, Text, useTextStyle } from '@/design-system';
+import {
+  Box,
+  DebugLayout,
+  globalColors,
+  Text,
+  useForegroundColor,
+  useTextStyle,
+} from '@/design-system';
 import { IS_ANDROID } from '@/env';
+import { useImportingWallet } from '@/hooks';
 import { colors } from '@/styles';
 import React, { useMemo } from 'react';
+import * as i18n from '@/languages';
 
 export const ImportWalletSheet = () => {
-  const headingStyle = useTextStyle({
-    color: 'label',
-    size: '17pt',
+  const {
+    busy,
+    handleFocus,
+    handlePressImportButton,
+    handleSetSeedPhrase,
+    inputRef,
+    isSecretValid,
+    seedPhrase,
+  } = useImportingWallet();
+
+  const textStyle = useTextStyle({
+    align: 'center',
+    color: isSecretValid ? { custom: globalColors.purple60 } : 'label',
+    size: '17pt / 135%',
     weight: 'semibold',
   });
+
+  const labelTertiary = useForegroundColor('labelTertiary');
 
   return (
     <SlackSheet
@@ -22,27 +44,40 @@ export const ImportWalletSheet = () => {
       testID="import-sheet"
     >
       <Box alignItems="center" paddingTop={{ custom: 38 }}>
-        <Text color="label" size="26pt" weight="bold">
-          Watch an address
-        </Text>
+        <Box height={{ custom: 68 }} justifyContent="space-between">
+          <Text color="label" size="26pt" weight="bold">
+            Watch an address
+          </Text>
+        </Box>
         <Input
           autoCorrect={false}
-          keyboardType={android ? 'visible-password' : 'default'}
-          // onChangeText={onChangeText}
-          // onFocus={handleFocus}
-          // ref={inputRef}
+          autoComplete={false}
+          autoFocus
+          autoCapitalize="none"
+          textContentType="none"
+          enablesReturnKeyAutomatically
+          keyboardType={IS_ANDROID ? 'visible-password' : 'default'}
+          onChangeText={handleSetSeedPhrase}
+          onFocus={handleFocus}
+          multiline
+          numberOfLines={3}
+          onSubmitEditing={handlePressImportButton}
+          placeholder={i18n.t(i18n.l.wallet.new.enter_seeds_placeholder)}
+          placeholderTextColor={labelTertiary}
+          ref={inputRef}
           selectionColor={colors.alpha(globalColors.purple60, 10)}
           spellCheck={false}
+          width={232}
+          returnKeyType="done"
           style={useMemo(
             () => ({
-              ...headingStyle,
-              // height,
-              // top: ios ? 1.5 : 8,
+              ...textStyle,
+              paddingTop: 99,
             }),
-            [headingStyle]
+            [textStyle]
           )}
-          // testID={testID}
-          // value={value}
+          testID="import-sheet-input"
+          value={seedPhrase}
         />
       </Box>
     </SlackSheet>
