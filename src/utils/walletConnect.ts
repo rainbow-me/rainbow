ff19165259a3e9694af0d82ec9565bfa621a1b56
Eric Bailey <git@esb.lol>
2022-12-07 15:56:45 -0600

    feat: handle signing requests
    (cherry picked from commit 36fc32fb1b451cd1d0aad6a82fa7cbd595359127)

--- src/utils/walletConnect.ts
+++ src/utils/walletConnect.ts
@@ -4,6 +4,8 @@ import { getSdkError, parseUri } from '@walletconnect/utils';
 import { WC_PROJECT_ID } from 'react-native-dotenv';
 import { NavigationContainerRef } from '@react-navigation/native';
 import Minimizer from 'react-native-minimizer';
+import { utils as ethersUtils } from 'ethers';
+import { formatJsonRpcResult, formatJsonRpcError } from '@json-rpc-tools/utils';
 
 import { logger, RainbowError } from '@/logger';
 import { WalletconnectApprovalSheetRouteParams } from '@/redux/walletconnect';
@@ -15,6 +17,21 @@ import { maybeSignUri } from '@/handlers/imgix';
 import { dappLogoOverride, dappNameOverride } from '@/helpers/dappNameHandler';
 import { Alert } from '@/components/alerts';
 import * as lang from '@/languages';
+import {
+  isSigningMethod,
+  isTransactionDisplayType,
+} from '@/utils/signingMethods';
+import store from '@/redux/store';
+import { findWalletWithAccount } from '@/helpers/findWalletWithAccount';
+import WalletTypes from '@/helpers/walletTypes';
+import { ethereumUtils } from '@/utils';
+import { getRequestDisplayDetails } from '@/parsers';
+import {
+  RequestData,
+  REQUESTS_UPDATE_REQUESTS_TO_APPROVE,
+  removeRequest,
+} from '@/redux/requests';
+import { saveLocalRequests } from '@/handlers/localstorage/walletconnectRequests';
 
 /**
  * Indicates that the app should redirect or go back after the next action
@@ -49,6 +66,48 @@ const signClient = Promise.resolve(
     },
   })
 );
+
+/**
+ * For RPC requests that have [address, message] tuples (order may change),
+ * return { address, message } and JSON.parse the value if it's from a typed
+ * data request
+ */
+function parseRPCParams({
+  method,
+  params,
+}: {
+  method: string;
+  params: string[];
+}) {
+  if (method === 'eth_sign' || method === 'personal_sign') {
+    const [address, message] = params.sort(a =>
+      ethersUtils.isAddress(a) ? -1 : 1
+    );
+    const isHexString = ethersUtils.isHexString(message);
+
+    const decodedMessage = isHexString
+      ? ethersUtils.toUtf8String(message)
+      : message;
+
+    return {
+      address,
+      message: decodedMessage,
+    };
+  }
+
+  if (method === 'eth_signTypedData' || method === 'eth_signTypedData_v4') {
+    const [address, message] = params.sort(a =>
+      ethersUtils.isAddress(a) ? -1 : 1
+    );
+
+    return {
+      address,
+      message: JSON.parse(message),
+    };
+  }
+
+  return {};
+}
 
 export async function pair({ uri }: { uri: string }) {
   logger.debug(`WC v2: pair`, { uri });
@@ -108,7 +167,10 @@ export async function pair({ uri }: { uri: string }) {
 export async function initListeners() {
   const client = await signClient;
 
+  logger.debug(`WC v2: signClient initialized, initListeners`);
+
   client.on('session_proposal', onSessionProposal);
+  client.on('session_request', onSessionRequest);
 }
 
 export function onSessionProposal(
@@ -118,10 +180,8 @@ export function onSessionProposal(
 
   const receivedTimestamp = Date.now();
   const {
-    id,
     proposer,
-    expiry,
-    pairingTopic,
+    expiry, // TODO do we need to do anything with this?
     requiredNamespaces,
   } = proposal.params;
 
@@ -144,7 +204,7 @@ export function onSessionProposal(
     meta: {
       chainId,
       dappName,
-      dappScheme: peerMeta.url || 'Unknown URL', // TODO
+      dappScheme: 'unused in WC v2', // only used for deeplinks from WC v1
       dappUrl: peerMeta.url || 'Unknown URL',
       imageUrl: maybeSignUri(
         dappLogoOverride(peerMeta?.url) || peerMeta?.icons?.[0]
@@ -260,4 +320,170 @@ export function onSessionProposal(
     routeParams,
     getActiveRoute()?.name === Routes.WALLET_CONNECT_APPROVAL_SHEET
   );
+}
+
+export async function onSessionRequest(
+  event: SignClientTypes.EventArguments['session_request']
+) {
+  const client = await signClient;
+
+  logger.debug(`WC v2: session_request`, { event });
+
+  const { id, topic } = event;
+  const { method, params } = event.params.request;
+
+  if (isSigningMethod(method)) {
+    // transactions aren't a `[address, message]` tuple
+    const isTransactionMethod = isTransactionDisplayType(method);
+    let { address, message } = parseRPCParams({ method, params });
+    const allWallets = store.getState().wallets.wallets;
+
+    if (!isTransactionMethod) {
+      if (!address || !message) {
+        logger.error(
+          new RainbowError(
+            `WC v2: session_request exited, no address or messsage`
+          ),
+          {
+            address,
+            message,
+          }
+        );
+
+        await client.respond({
+          topic,
+          response: formatJsonRpcError(id, `Invalid RPC params`),
+        });
+
+        return;
+      }
+
+      // for TS only, should never happen
+      if (!allWallets) {
+        logger.error(new RainbowError(`WC v2: allWallets is null`));
+        return;
+      }
+
+      const selectedWallet = findWalletWithAccount(allWallets, address);
+
+      if (!selectedWallet || selectedWallet?.type === WalletTypes.readOnly) {
+        logger.debug(
+          `WC v2: session_request exited, selectedWallet was falsy or read only`
+        );
+
+        await client.respond({
+          topic,
+          response: formatJsonRpcError(id, `Wallet is read-only`),
+        });
+
+        return;
+      }
+    } else {
+      address = params[0].from;
+    }
+
+    const session = client.session.get(topic);
+    const { nativeCurrency, network } = store.getState().settings;
+    const chainId = Number(event.params.chainId.split(':')[1]);
+    const dappNetwork = ethereumUtils.getNetworkFromChainId(chainId);
+    const displayDetails = getRequestDisplayDetails(
+      event.params.request,
+      nativeCurrency,
+      dappNetwork
+    );
+    const peerMeta = session.peer.metadata;
+    const request: RequestData = {
+      clientId: session.topic, // I don't think this is used
+      peerId: session.topic, // I don't think this is used
+      requestId: event.id,
+      dappName:
+        dappNameOverride(peerMeta.name) || peerMeta.name || 'Unknown Dapp',
+      dappScheme: 'unused in WC v2', // only used for deeplinks from WC v1
+      dappUrl: peerMeta.url || 'Unknown URL',
+      displayDetails,
+      imageUrl: maybeSignUri(
+        dappLogoOverride(peerMeta.url) || peerMeta.icons[0]
+      ),
+      payload: event.params.request,
+      walletConnectV2RequestValues: {
+        sessionRequestEvent: event,
+        // @ts-ignore we assign address above
+        address, // required by screen
+        chainId, // required by screen
+      },
+    };
+
+    logger.debug(`request`, { request });
+
+    const { requests: pendingRequests } = store.getState().requests;
+
+    if (!pendingRequests[request.requestId]) {
+      const updatedRequests = {
+        ...pendingRequests,
+        [request.requestId]: request,
+      };
+      store.dispatch({
+        payload: updatedRequests,
+        type: REQUESTS_UPDATE_REQUESTS_TO_APPROVE,
+      });
+      saveLocalRequests(updatedRequests, address, network);
+
+      logger.debug(`WC v2: navigating to CONFIRM_REQUEST sheet`);
+
+      Navigation.handleAction(Routes.CONFIRM_REQUEST, {
+        openAutomatically: true,
+        transactionDetails: request,
+      });
+
+      analytics.track('Showing Walletconnect signing request', {
+        dappName: request.dappName,
+        dappUrl: request.dappUrl,
+      });
+    }
+  } else {
+    logger.info(`utils/walletConnectV2: received unsupported session_request`);
+
+    await client.respond({
+      topic,
+      response: formatJsonRpcError(id, `Unsupported RPC method`),
+    });
+  }
+}
+
+/**
+ * Handles the result created on our confirmation screen and sends it along to the dapp via WC
+ */
+export async function handleSessionRequestResponse(
+  {
+    sessionRequestEvent,
+  }: {
+    sessionRequestEvent: SignClientTypes.EventArguments['session_request'];
+  },
+  { result, error }: { result: string; error: any }
+) {
+  logger.debug(`WC v2: handleSessionRequestResponse`, { result, error });
+  logger.info(`WC v2: handleSessionRequestResponse`, {
+    success: Boolean(result),
+  });
+
+  const client = await signClient;
+  const { topic, id } = sessionRequestEvent;
+
+  if (result) {
+    const payload = {
+      topic,
+      response: formatJsonRpcResult(id, result),
+    };
+    logger.debug(`WC v2: handleSessionRequestResponse success`, { payload });
+    await client.respond(payload);
+  } else {
+    const payload = {
+      topic,
+      response: formatJsonRpcError(id, error),
+    };
+    logger.debug(`WC v2: handleSessionRequestResponse reject`, { payload });
+    await client.respond(payload);
+  }
+
+  store.dispatch(removeRequest(sessionRequestEvent.id));
 }
