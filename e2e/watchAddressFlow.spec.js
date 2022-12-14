9e19129d26f9be1a9b9f295302a4c16c777f2468
Ben Goldberg <bengoldberg@rainbow.me>
2022-12-14 14:35:43 -0800

    e2e fix

--- e2e/watchAddressFlow.spec.js
+++ e2e/watchAddressFlow.spec.js
@@ -21,7 +21,6 @@ describe('Watch address flow', () => {
 
   it('Should show the "Add wallet modal" after tapping import with a valid address', async () => {
     await Helpers.clearField('import-sheet-input');
-    await Helpers.checkIfElementHasString('import-sheet-button-label', 'Paste');
     await Helpers.typeText('import-sheet-input', 'test.eth', false);
     await Helpers.checkIfElementHasString(
       'import-sheet-button-label',
