bf841f4dd6c5d7b2b1570afcfd9a50bc5b98eb6d
Skylar Barrera <skylar.barrera@gmail.com>
2022-12-14 13:14:42 -0500

    1 more

--- src/hooks/useImportingWallet.ts
+++ src/hooks/useImportingWallet.ts
@@ -30,8 +30,9 @@ import { walletInit } from '@/model/wallet';
 import { Navigation, useNavigation } from '@/navigation';
 import { walletsLoadState } from '@/redux/wallets';
 import Routes from '@/navigation/routesNames';
-import { ethereumUtils, sanitizeSeedPhrase } from '@/utils';
+import { sanitizeSeedPhrase } from '@/utils';
 import logger from '@/utils/logger';
+import { deriveAccountFromWalletInput } from '@/utils/wallet';
 
 export default function useImportingWallet({ showImportModal = true } = {}) {
   const { accountAddress } = useAccountSettings();
@@ -209,12 +210,12 @@ export default function useImportingWallet({ showImportModal = true } = {}) {
         try {
           setBusy(true);
           setTimeout(async () => {
-            const walletResult = await ethereumUtils.deriveAccountFromWalletInput(
-              input
-            );
+            const walletResult = await deriveAccountFromWalletInput(input);
             // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '{ address: string; isHDWallet: b... Remove this comment to see the full error message
             setCheckedWallet(walletResult);
-            const ens = await fetchReverseRecord(walletResult.address);
+            const ens = await fetchReverseRecord(
+              walletResult.address as string
+            );
             if (ens && ens !== input) {
               name = forceEmoji ? `${forceEmoji} ${ens}` : ens;
               if (!avatarUrl && profilesEnabled) {
