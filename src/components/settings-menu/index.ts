0bdb65c5d17fe81616ccabd3c293fdc5ead7d08e
Jakub Adamczyk <jakub@rainbow.me>
2023-01-09 14:26:28 +0100

    Remove transaction details playground from dev menu (#4519)

--- src/components/settings-menu/index.ts
+++ src/components/settings-menu/index.ts
@@ -6,6 +6,4 @@ export { default as NetworkSection } from './NetworkSection';
 export { default as NotificationsSection } from './NotificationsSection';
 export { default as PrivacySection } from './PrivacySection';
 export { default as SettingsSection } from './SettingsSection';
-// TODO: Remove below after finishing work on APP-27
-export { default as TransactionDetailsPlayground } from './TransactionDetailsPlayground';
 export { default as WalletNotificationsSettings } from './WalletNotificationsSettings';
