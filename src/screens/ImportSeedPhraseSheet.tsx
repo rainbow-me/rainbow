1376e2b1104ae982a0a5c63516669ac35a8acf17
Skylar Barrera <skylar.barrera@gmail.com>
2023-01-11 18:13:22 -0400

    Revert "fix APP-312 again"
    This reverts commit 5ab8f1474bbc9e06eb5938b79ec497486168d519.

--- src/screens/ImportSeedPhraseSheet.tsx
+++ src/screens/ImportSeedPhraseSheet.tsx
@@ -20,9 +20,9 @@ import { RouteProp, useFocusEffect, useRoute } from '@react-navigation/core';
 import Clipboard from '@react-native-community/clipboard';
 import { LoadingOverlay } from '@/components/modal';
 import { StatusBar } from 'react-native';
-import { safeAreaInsetValues } from '@/utils';
 
 const TRANSLATIONS = i18n.l.wallet.new.import_seed_phrase_sheet;
+const IOS_STATUS_BAR_HEIGHT = 20;
 
 type RouteParams = {
   ImportSeedPhraseSheetParams: {
@@ -58,137 +58,129 @@ export const ImportSeedPhraseSheet: React.FC = () => {
   useFocusEffect(useCallback(() => inputRef.current?.focus(), [inputRef]));
 
   const buttonDisabled = seedPhrase && !isSecretValid;
-  const safeAreaTopPadding = IS_ANDROID
+  const statusBarHeight = IS_ANDROID
     ? StatusBar?.currentHeight ?? 0
-    : safeAreaInsetValues.top;
+    : IOS_STATUS_BAR_HEIGHT;
 
   return (
-    <>
+    <Box
+      height={{
+        custom: deviceHeight - SheetHandleFixedToTopHeight - statusBarHeight,
+      }}
+      background="surfaceSecondary"
+    >
       <Box
-        height={{
-          custom:
-            deviceHeight - SheetHandleFixedToTopHeight - safeAreaTopPadding,
-        }}
-        background="surfaceSecondary"
+        alignItems="center"
+        justifyContent="space-between"
+        paddingTop={{ custom: 38 }}
+        paddingHorizontal="20px"
+        testID="import-sheet"
       >
-        <Box
-          alignItems="center"
-          justifyContent="space-between"
-          paddingTop={{ custom: 38 }}
-          paddingHorizontal="20px"
-          testID="import-sheet"
-        >
-          <Stack space="20px">
-            <Text align="center" color="label" size="26pt" weight="bold">
-              {i18n.t(TRANSLATIONS[type].title)}
-            </Text>
-            {type === 'import' && (
-              <Text
-                align="center"
-                color="labelTertiary"
-                size="15pt / 135%"
-                weight="semibold"
-              >
-                {i18n.t(TRANSLATIONS.import.description)}
-              </Text>
-            )}
-          </Stack>
-        </Box>
-        <Box
-          alignItems="center"
-          bottom={{ custom: keyboardHeight }}
-          justifyContent="center"
-          position="absolute"
-          top={{ custom: -SheetHandleFixedToTopHeight }}
-          width="full"
-        >
-          <Input
-            autoCorrect={false}
-            autoCompleteType={false}
-            autoFocus={false}
-            autoCapitalize="none"
-            textContentType="none"
-            enablesReturnKeyAutomatically
-            keyboardType={IS_ANDROID ? 'visible-password' : 'default'}
-            onChangeText={handleSetSeedPhrase}
-            onFocus={handleFocus}
-            multiline
-            numberOfLines={3}
-            onSubmitEditing={() => {
-              // @ts-expect-error callback needs refactor
-              if (isSecretValid) handlePressImportButton();
-            }}
-            placeholder={i18n.t(TRANSLATIONS[type].placeholder)}
-            placeholderTextColor={labelTertiary}
-            ref={inputRef}
-            selectionColor={globalColors.purple60}
-            // scrollEnabled={false}
-            spellCheck={false}
-            width={232}
-            returnKeyType="done"
-            style={textStyle}
-            testID="import-sheet-input"
-            value={seedPhrase}
-          />
-        </Box>
-        <Box
-          position="absolute"
-          right="0px"
-          bottom={{ custom: keyboardHeight }}
-        >
-          <Inset bottom="20px" right="20px">
-            <AccentColorProvider
-              color={colors.alpha(globalColors.purple60, seedPhrase ? 1 : 0.1)}
+        <Stack space="20px">
+          <Text align="center" color="label" size="26pt" weight="bold">
+            {i18n.t(TRANSLATIONS[type].title)}
+          </Text>
+          {type === 'import' && (
+            <Text
+              align="center"
+              color="labelTertiary"
+              size="15pt / 135%"
+              weight="semibold"
             >
-              <ButtonPressAnimation
-                disabled={buttonDisabled}
-                onPress={
-                  seedPhrase
-                    ? handlePressImportButton
-                    : () =>
-                        Clipboard.getString().then((text: string) =>
-                          handleSetSeedPhrase(text)
-                        )
-                }
-                overflowMargin={50}
-                testID="import-sheet-button"
-              >
-                <Box
-                  alignItems="center"
-                  background={buttonDisabled ? 'fillSecondary' : 'accent'}
-                  borderRadius={99}
-                  height="36px"
-                  justifyContent="center"
-                  shadow={
-                    seedPhrase && !buttonDisabled ? '12px accent' : undefined
-                  }
-                  width={{ custom: 88 }}
-                >
-                  <Text
-                    align="center"
-                    color={
-                      // eslint-disable-next-line no-nested-ternary
-                      buttonDisabled
-                        ? 'labelSecondary'
-                        : seedPhrase
-                        ? 'label'
-                        : { custom: globalColors.purple60 }
-                    }
-                    size="15pt"
-                    testID="import-sheet-button-label"
-                    weight="bold"
-                  >
-                    {seedPhrase
-                      ? i18n.t(TRANSLATIONS.continue)
-                      : `􀉃 ${i18n.t(TRANSLATIONS.paste)}`}
-                  </Text>
-                </Box>
-              </ButtonPressAnimation>
-            </AccentColorProvider>
-          </Inset>
-        </Box>
-        {busy && <LoadingOverlay />}
+              {i18n.t(TRANSLATIONS.import.description)}
+            </Text>
+          )}
+        </Stack>
       </Box>
-    </>
+      <Box
+        alignItems="center"
+        bottom={{ custom: keyboardHeight }}
+        justifyContent="center"
+        position="absolute"
+        top={{ custom: -SheetHandleFixedToTopHeight }}
+        width="full"
+      >
+        <Input
+          autoCorrect={false}
+          autoCompleteType={false}
+          autoFocus={false}
+          autoCapitalize="none"
+          textContentType="none"
+          enablesReturnKeyAutomatically
+          keyboardType={IS_ANDROID ? 'visible-password' : 'default'}
+          onChangeText={handleSetSeedPhrase}
+          onFocus={handleFocus}
+          multiline
+          numberOfLines={3}
+          onSubmitEditing={() => {
+            // @ts-expect-error callback needs refactor
+            if (isSecretValid) handlePressImportButton();
+          }}
+          placeholder={i18n.t(TRANSLATIONS[type].placeholder)}
+          placeholderTextColor={labelTertiary}
+          ref={inputRef}
+          selectionColor={globalColors.purple60}
+          spellCheck={false}
+          width={232}
+          returnKeyType="done"
+          style={textStyle}
+          testID="import-sheet-input"
+          value={seedPhrase}
+        />
+      </Box>
+      <Box position="absolute" right="0px" bottom={{ custom: keyboardHeight }}>
+        <Inset bottom="20px" right="20px">
+          <AccentColorProvider
+            color={colors.alpha(globalColors.purple60, seedPhrase ? 1 : 0.1)}
+          >
+            <ButtonPressAnimation
+              disabled={buttonDisabled}
+              onPress={
+                seedPhrase
+                  ? handlePressImportButton
+                  : () =>
+                      Clipboard.getString().then((text: string) =>
+                        handleSetSeedPhrase(text)
+                      )
+              }
+              overflowMargin={50}
+              testID="import-sheet-button"
+            >
+              <Box
+                alignItems="center"
+                background={buttonDisabled ? 'fillSecondary' : 'accent'}
+                borderRadius={99}
+                height="36px"
+                justifyContent="center"
+                shadow={
+                  seedPhrase && !buttonDisabled ? '12px accent' : undefined
+                }
+                width={{ custom: 88 }}
+              >
+                <Text
+                  align="center"
+                  color={
+                    // eslint-disable-next-line no-nested-ternary
+                    buttonDisabled
+                      ? 'labelSecondary'
+                      : seedPhrase
+                      ? 'label'
+                      : { custom: globalColors.purple60 }
+                  }
+                  size="15pt"
+                  testID="import-sheet-button-label"
+                  weight="bold"
+                >
+                  {seedPhrase
+                    ? i18n.t(TRANSLATIONS.continue)
+                    : `􀉃 ${i18n.t(TRANSLATIONS.paste)}`}
+                </Text>
+              </Box>
+            </ButtonPressAnimation>
+          </AccentColorProvider>
+        </Inset>
+      </Box>
+      {busy && <LoadingOverlay />}
+    </Box>
   );
 };
