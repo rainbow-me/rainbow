b2aa9dc8d36293c686eb7393374eaf6065902f8f
Ben Goldberg <bengoldberg@rainbow.me>
2023-01-11 12:44:18 -0800

    nav to pair flow

--- src/screens/AddWalletSheet.tsx
+++ src/screens/AddWalletSheet.tsx
@@ -195,6 +195,17 @@ export const AddWalletSheet = () => {
     });
   };
 
+  const onPressConnectHardwareWallet = () => {
+    analyticsV2.track(analyticsV2.event.addWalletFlowStarted, {
+      isFirstWallet: false,
+      type: 'ledger_nano_x',
+    });
+    goBack();
+    InteractionManager.runAfterInteractions(() => {
+      navigate(Routes.PAIR_HARDWARE_WALLET_NAVIGATOR);
+    });
+  };
+
   const create: AddWalletItem = {
     title: i18n.t(TRANSLATIONS.create_new.title),
     description: i18n.t(TRANSLATIONS.create_new.description),
@@ -228,12 +239,7 @@ export const AddWalletSheet = () => {
     icon: '􀕹',
     iconColor: globalColors.blue60,
     testID: 'connect-hardware-wallet-button',
-    onPress: () => {
-      analyticsV2.track(analyticsV2.event.addWalletFlowStarted, {
-        isFirstWallet: false,
-        type: 'ledger_nano_x',
-      });
-    },
+    onPress: onPressConnectHardwareWallet,
   };
 
   return (
