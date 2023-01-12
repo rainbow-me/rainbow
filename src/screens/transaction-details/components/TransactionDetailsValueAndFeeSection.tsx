db772febff4184a47be53842c321362839c3cd07
Jakub Adamczyk <jakub@rainbow.me>
2023-01-12 16:18:45 +0100

    fix L2 transaction badges and coin icons

--- src/screens/transaction-details/components/TransactionDetailsValueAndFeeSection.tsx
+++ src/screens/transaction-details/components/TransactionDetailsValueAndFeeSection.tsx
@@ -9,7 +9,7 @@ import { CoinIcon } from '@/components/coin-icon';
 import { Box, Stack } from '@/design-system';
 import { TransactionDetailsDivider } from '@/screens/transaction-details/components/TransactionDetailsDivider';
 import * as i18n from '@/languages';
-import { TransactionType } from '@/entities';
+import { AssetTypes, TransactionType } from '@/entities';
 import { ethereumUtils } from '@/utils';
 import { Network } from '@/helpers';
 import { useSelector } from 'react-redux';
@@ -26,18 +26,20 @@ export const TransactionDetailsValueAndFeeSection: React.FC<Props> = ({
   transaction,
 }) => {
   const { network, symbol, type, fee } = transaction;
-  // FIXME: Doesn't fully support Layer 2 Networks yet and not all coin icons work properly.
-  const coinSymbol =
-    type === TransactionType.contract_interaction
-      ? ethereumUtils.getNetworkNativeAsset(network ?? Network.mainnet)?.symbol
-      : symbol ?? undefined;
-  const mainnetCoinAddress = useSelector(
+  const assetData = useSelector(
     (state: AppState) =>
       state.data.accountAssetsData?.[
         `${transaction.address}_${transaction.network}`
-      ]?.mainnet_address
+      ]
   );
-  const coinAddress = mainnetCoinAddress ?? transaction.address ?? undefined;
+  const coinAddress = assetData?.address ?? transaction.address;
+  const mainnetCoinAddress = assetData?.mainnet_address;
+  const coinSymbol =
+    type === TransactionType.contract_interaction
+      ? ethereumUtils.getNetworkNativeAsset(network ?? Network.mainnet)?.symbol
+      : assetData?.symbol ?? symbol ?? undefined;
+  const coinType =
+    assetData?.type ?? network !== Network.mainnet ? network : undefined;
 
   const value = transaction.balance?.display;
   const nativeCurrencyValue = transaction.native?.display;
@@ -57,7 +59,12 @@ export const TransactionDetailsValueAndFeeSection: React.FC<Props> = ({
           {value && (
             <DoubleLineTransactionDetailsRow
               leftComponent={
-                <CoinIcon address={coinAddress} symbol={coinSymbol} />
+                <CoinIcon
+                  mainnet_address={mainnetCoinAddress}
+                  address={coinAddress}
+                  symbol={coinSymbol}
+                  type={coinType}
+                />
               }
               title={i18n.t(i18n.l.transaction_details.value)}
               value={value}
