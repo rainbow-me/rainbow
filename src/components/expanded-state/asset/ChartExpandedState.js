590803703fcdf85699ad23e77c43b74d30f8d5e7
Jin <jin.chung17@gmail.com>
2023-01-13 12:54:12 -0500

    Update original asset address (#4540)

--- src/components/expanded-state/asset/ChartExpandedState.js
+++ src/components/expanded-state/asset/ChartExpandedState.js
@@ -206,7 +206,7 @@ export default function ChartExpandedState({ asset }) {
   }, [asset, genericAsset, hasBalance, nativeCurrency]);
 
   if (assetWithPrice?.mainnet_address) {
-    assetWithPrice.l2Address = assetWithPrice.address;
+    assetWithPrice.l2Address = asset?.address;
     assetWithPrice.address = assetWithPrice.mainnet_address;
   }
 
