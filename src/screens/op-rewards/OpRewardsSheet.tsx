7d1b9d77755fb8073e9b598aa1e3da392b81dff7
Jakub Adamczyk <jakub@rainbow.me>
2023-01-17 13:54:10 +0100

    implement data loading mock

--- src/screens/op-rewards/OpRewardsSheet.tsx
+++ src/screens/op-rewards/OpRewardsSheet.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { SlackSheet } from '@/components/sheet';
 import { useDimensions } from '@/hooks';
 import { Box, Text } from '@/design-system';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useFocusEffect } from '@react-navigation/native';
+import { getMockOpData } from '@/screens/op-rewards/mocks/getMockOpData';
 
 export const OpRewardsSheet: React.FC = () => {
   const { height } = useDimensions();
   const { top } = useSafeAreaInsets();
+  const [rewardsData, setRewardsData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useFocusEffect(
+    useCallback(() => {
+      setLoading(true);
+      getMockOpData().then(data => {
+        setRewardsData(data);
+        setLoading(false);
+      });
+    }, [])
+  );
 
   return (
     // @ts-ignore
     <SlackSheet height="100%" contentHeight={height - top} scrollEnabled>
       <Box flexGrow={1}>
         <Text size="15pt" color="label">
-          Temporary
+          {loading ? 'Loading...' : JSON.stringify(rewardsData)}
         </Text>
       </Box>
     </SlackSheet>
