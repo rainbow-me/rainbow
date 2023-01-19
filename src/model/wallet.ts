be4e31d13edf0c9e24c57751356d0e4f7a2ba2e2
Skylar Barrera <skylar.barrera@gmail.com>
2023-01-19 08:48:53 -0400

    typo

--- src/model/wallet.ts
+++ src/model/wallet.ts
@@ -1207,11 +1207,9 @@ export const getHardwareKey = async (
 ): Promise<null | PrivateKeyData> => {
   try {
     const key = `${address}_${privateKeyKey}`;
-    const harwarePrivateKey = (await keychain.loadObject(
-      key
-    )) as PrivateKeyData;
+    const hardwareKey = (await keychain.loadObject(key)) as PrivateKeyData;
 
-    return harwarePrivateKey || null;
+    return hardwareKey || null;
   } catch (error) {
     logger.error(new RainbowError('Error in getHardwareKey'), { error });
     return null;
