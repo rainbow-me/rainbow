fff6bb9185cc327d6f30e40644adf4ddfb550959
Eric Bailey <git@esb.lol>
2023-01-23 11:47:27 -0600

    fix: bump package, handle errors

--- src/screens/AddCashSheet.tsx
+++ src/screens/AddCashSheet.tsx
@@ -14,6 +14,9 @@ import { Box, Text, Stack, Inline } from '@/design-system';
 import { AppState } from '../redux/store';
 import { loadWallet, signPersonalMessage } from '@/model/wallet';
 import { Ratio } from '@/components/icons/svg/Ratio';
+import { WrappedAlert } from '@/helpers/alert';
+import { logger, RainbowError } from '@/logger';
+import * as lang from '@/languages';
 
 const deviceHeight = deviceUtils.dimensions.height;
 const statusBarHeight = getStatusBarHeight(true);
@@ -29,6 +32,8 @@ function RatioButton({ accountAddress }: { accountAddress: string }) {
   return (
     <RatioComponent
       fetchSessionToken={async () => {
+        logger.debug(`Ratio: fetchSessionToken`, {}, logger.DebugContext.f2c);
+
         const { data, error } = await gretch<{ id: string }>(
           'https://f2c.rainbow.me/v1/ratio/client-session',
           {
@@ -42,17 +47,17 @@ function RatioButton({ accountAddress }: { accountAddress: string }) {
         ).json();
 
         if (!data || error) {
-          // TODO
           throw new Error(error);
         }
 
         return data?.id;
       }}
       signingCallback={async challenge => {
+        logger.debug(`Ratio: signingCallback`, {}, logger.DebugContext.f2c);
+
         const existingWallet = await loadWallet(accountAddress, true);
 
         if (!existingWallet) {
-          // TODO
           throw new Error('No wallet found');
         }
 
@@ -60,30 +65,52 @@ function RatioButton({ accountAddress }: { accountAddress: string }) {
           (await signPersonalMessage(challenge, existingWallet)) || {};
 
         if (!result || error) {
-          // TODO
           throw new Error('Signature failed');
         }
 
         return { signature: result };
       }}
-      onPress={() => setIsLoading(true)}
+      onPress={() => {
+        logger.debug(`Ratio: clicked`, {}, logger.DebugContext.f2c);
+        setIsLoading(true);
+      }}
       onOpen={() => {
-        console.log('Opened');
+        logger.debug(`Ratio: opened`, {}, logger.DebugContext.f2c);
       }}
       onTransactionComplete={order => {
-        console.log('Transaction complete', order);
+        logger.debug(
+          `Ratio: transaction complete`,
+          { order },
+          logger.DebugContext.f2c
+        );
       }}
       onHelp={() => {
-        console.log('Help');
+        logger.debug(`Ratio: help clicked`, {}, logger.DebugContext.f2c);
       }}
       onAccountRecovery={() => {
-        console.log('Account recovery');
+        logger.debug(
+          `Ratio: account recovery clicked`,
+          {},
+          logger.DebugContext.f2c
+        );
       }}
       onError={error => {
-        console.log('Error', error);
+        logger.error(
+          new RainbowError(`Ratio component threw an error: ${error}`),
+          { error }
+        );
+        WrappedAlert.alert(
+          lang.t(lang.l.wallet.add_cash_v2.generic_error.title),
+          lang.t(lang.l.wallet.add_cash_v2.generic_error.message),
+          [
+            {
+              text: lang.t(lang.l.wallet.add_cash_v2.generic_error.button),
+            },
+          ]
+        );
       }}
       onClose={() => {
-        console.log('Closed');
+        logger.debug(`Ratio: closed`, {}, logger.DebugContext.f2c);
       }}
     >
       <Box
