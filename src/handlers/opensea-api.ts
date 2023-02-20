fc2d685a3e45c67225f0afde0bd007702f9c8da4
Ben Goldberg <bengoldberg@rainbow.me>
2023-02-20 14:56:33 -0700

    remove obsolete

--- src/handlers/opensea-api.ts
+++ src/handlers/opensea-api.ts
@@ -7,111 +7,12 @@ import {
   NFT_API_URL,
 } from 'react-native-dotenv';
 import { rainbowFetch } from '../rainbow-fetch';
-import NetworkTypes, { Network } from '@/helpers/networkTypes';
-import {
-  parseAccountUniqueTokens,
-  parseAccountUniqueTokensPolygon,
-} from '@/parsers';
+import NetworkTypes from '@/helpers/networkTypes';
 import { handleSignificantDecimals } from '@/helpers/utilities';
 import logger from '@/utils/logger';
 
-export const UNIQUE_TOKENS_LIMIT_PER_PAGE = 50;
-export const UNIQUE_TOKENS_LIMIT_TOTAL = 2000;
-
 // limiting our opensea api requests to 10/sec so we don't max out
 const queue = new PQueue({ interval: 1000, intervalCap: 10 });
-
-export const apiGetAccountUniqueTokens = async (
-  network: Network,
-  address: string,
-  page: number
-) => {
-  try {
-    const isPolygon = network === NetworkTypes.polygon;
-    const networkPrefix = network === NetworkTypes.mainnet ? '' : `${network}-`;
-    const offset = page * UNIQUE_TOKENS_LIMIT_PER_PAGE;
-    const url = `https://${networkPrefix}${NFT_API_URL}/api/v1/assets`;
-    const urlV2 = `https://${NFT_API_URL}/api/v2/beta/assets`;
-    const data = await queue.add(
-      async () =>
-        await rainbowFetch(isPolygon ? urlV2 : url, {
-          headers: {
-            'Accept': 'application/json',
-            'X-Api-Key': NFT_API_KEY,
-          },
-          method: 'get',
-          params: {
-            include_orders: 'true',
-            // @ts-expect-error ts-migrate(2322) FIXME: Type '{ limit: number; offset: number; owner: any;... Remove this comment to see the full error message
-            limit: UNIQUE_TOKENS_LIMIT_PER_PAGE,
-            // @ts-expect-error ts-migrate(2322) FIXME: Type '{ limit: number; offset: number; owner: any;... Remove this comment to see the full error message
-            offset: offset,
-            ...(isPolygon
-              ? {
-                  chain_identifier: 'matic',
-                  owner_address: address,
-                }
-              : {
-                  owner: address,
-                }),
-          },
-          timeout: 10000, // 10 secs
-        })
-    );
-
-    return parseAccountUniqueTokens(data);
-  } catch (error: any) {
-    // opensea gives us an error if the account has no tokens on the network, we want to ignore this error
-    if (
-      error.responseBody[0]?.includes('does not exist for chain identifier')
-    ) {
-      return [];
-    } else {
-      logger.sentry('Error getting unique tokens', error);
-      captureException(new Error('Opensea: Error getting unique tokens'));
-      throw error;
-    }
-  }
-};
-
-export const apiGetAccountUniqueToken = async (
-  network: Network,
-  contractAddress: string,
-  tokenId: string,
-  // The `forceUpdate` param triggers an NFT metadata refresh
-  { forceUpdate = false }: { forceUpdate?: boolean } = {}
-) => {
-  try {
-    const isPolygon = network === NetworkTypes.polygon;
-    const networkPrefix = network === NetworkTypes.mainnet ? '' : `${network}-`;
-    const url = `https://${networkPrefix}${NFT_API_URL}/api/v1/asset/${contractAddress}/${tokenId}${
-      forceUpdate ? '?force_update=true' : ''
-    }`;
-    const urlV2 = `https://${NFT_API_URL}/api/v2/beta/asset/${contractAddress}/${tokenId}${
-      forceUpdate ? '?force_update=true' : ''
-    }`;
-    const { data } = await queue.add(
-      async () =>
-        await rainbowFetch(isPolygon ? urlV2 : url, {
-          headers: {
-            'Accept': 'application/json',
-            'X-Api-Key': NFT_API_KEY,
-          },
-          method: 'get',
-          timeout: 10000, // 10 secs
-        })
-    );
-    return isPolygon
-      ? parseAccountUniqueTokensPolygon({
-          data: { results: [data] },
-        })?.[0]
-      : parseAccountUniqueTokens({ data: { assets: [data] } })?.[0];
-  } catch (error) {
-    logger.sentry('Error getting unique token', error);
-    captureException(new Error('Opensea: Error getting unique token'));
-    throw error;
-  }
-};
 
 export const apiGetUniqueTokenFloorPrice = async (
   network: any,
