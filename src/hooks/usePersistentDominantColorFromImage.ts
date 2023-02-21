16590f943e967166cbac656812d1236b5d6be238
Eric Bailey <git@esb.lol>
2023-02-21 12:00:25 -0600

    feat: slight refactor of persistent color hook

--- src/hooks/usePersistentDominantColorFromImage.ts
+++ src/hooks/usePersistentDominantColorFromImage.ts
@@ -1,96 +1,71 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React from 'react';
 import { MMKV } from 'react-native-mmkv';
+
 import { STORAGE_IDS } from '@/model/mmkv';
 import { getDominantColorFromImage } from '@/utils';
 import { maybeSignUri } from '@/handlers/imgix';
-import usePrevious from './usePrevious';
-
-const DEFAULT_MMKV_KEY = 'DEFAULT_MMKV_KEY';
-const NOT_DEFINED_MMKV_VALUE = 'NOT_DEFINED';
-const storage = new MMKV({
-  id: STORAGE_IDS.DOMINANT_COLOR,
-});
-// settings a default value to easily check against it instead of just undefined and to make the value be always a string
-storage.set(DEFAULT_MMKV_KEY, NOT_DEFINED_MMKV_VALUE);
 
 const DEFAULT_COLOR_TO_MEASURE_AGAINST = '#333333';
 const DEFAULT_SIZE_FOR_COLOR_PROBING = 80;
+const storage = new MMKV({
+  id: STORAGE_IDS.DOMINANT_COLOR,
+});
 
 export function usePersistentDominantColorFromImage(
-  url: string | undefined | null,
+  url?: string | null,
   colorToMeasureAgainst = DEFAULT_COLOR_TO_MEASURE_AGAINST
 ) {
-  const previousUrl = usePrevious(url);
-  const didInitialize = useRef(false);
-  const externalUrl = useMemo(() => {
-    if (url) {
-      return maybeSignUri(url, { w: DEFAULT_SIZE_FOR_COLOR_PROBING });
-    }
-    return undefined;
-  }, [url]);
-  const currentCachedValue = storage.getString(url || DEFAULT_MMKV_KEY);
-  const [dominantColor, setDominantColor] = useState(
-    currentCachedValue !== NOT_DEFINED_MMKV_VALUE
-      ? currentCachedValue
-      : undefined
+  /**
+   * Returns the cached color or undefined.
+   *
+   * Will update if `url` changes.
+   */
+  const cachedColor = React.useMemo(
+    () => (url ? storage.getString(url) : undefined),
+    [url]
   );
-  const [loading, setLoading] = useState(currentCachedValue === undefined);
-
-  const checkIfCacheExistsAndSetColor = useCallback((): boolean => {
-    if (currentCachedValue && currentCachedValue !== NOT_DEFINED_MMKV_VALUE) {
-      setDominantColor(currentCachedValue);
-      setLoading(false);
-      return true;
-    }
-    return false;
-  }, [currentCachedValue]);
-
-  const fetchAndSetColor = useCallback(() => {
-    if (!url || !externalUrl) {
-      setDominantColor(undefined);
-      setLoading(false);
-      return;
-    }
-    setLoading(true);
-    getDominantColorFromImage(externalUrl, colorToMeasureAgainst)
-      .then(color => {
-        setDominantColor(color);
-        setLoading(false);
-        // We use original URL as a cache key
-        storage.set(url, color);
-      })
-      .catch(() => {
-        setDominantColor(undefined);
-        setLoading(false);
-      });
-  }, [colorToMeasureAgainst, externalUrl, url]);
-
-  useEffect(() => {
-    if (!url || !externalUrl) {
-      setDominantColor(undefined);
-      setLoading(false);
-      return;
-    }
-    if (!didInitialize.current || url !== previousUrl) {
-      didInitialize.current = true;
-      const alreadyCached = checkIfCacheExistsAndSetColor();
-      if (!alreadyCached) {
-        fetchAndSetColor();
-      }
-    }
-  }, [
-    checkIfCacheExistsAndSetColor,
-    externalUrl,
-    fetchAndSetColor,
-    previousUrl,
-    url,
-  ]);
-
-  return useMemo(
-    () => ({
-      dominantColor,
-      loading,
-    }),
-    [dominantColor, loading]
+  /**
+   * Prefill state with cached color, if available
+   */
+  const [color, setColor] = React.useState(cachedColor);
+  /**
+   * Potentially signs the `url` and returns it, or falls back to the raw
+   * `url`, which may already be signed.
+   */
+  const externalUrl = React.useMemo(
+    () =>
+      url
+        ? maybeSignUri(url, { w: DEFAULT_SIZE_FOR_COLOR_PROBING }) || url
+        : url,
+    [url]
   );
+
+  /**
+   * Only Runs if `url`/`externalUrl` or `colorToMeasureAgainst` changes
+   */
+  React.useEffect(() => {
+    /**
+     * If we have a `cachedColor`, always skip, since that value will update as
+     * the `url` prop passed in changes.
+     *
+     * `externalUrl` check here is for TypeScript and to ensure that `url` must
+     * also be defined.
+     */
+    if (!cachedColor && externalUrl) {
+      getDominantColorFromImage(externalUrl, colorToMeasureAgainst)
+        .then(color => {
+          setColor(color);
+          // @ts-ignore If `externalUrl` is defined, `url` is defined
+          storage.set(url, color);
+        })
+        .catch(() => {
+          setColor(undefined);
+        });
+    }
+  }, [setColor, cachedColor, externalUrl, colorToMeasureAgainst]);
+
+  return {
+    dominantColor: color,
+    loading: !color,
+  };
 }
