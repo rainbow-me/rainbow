be3bf89e2711378d3de8b9f8e76b6ed282c03ee9
Jakub Adamczyk <jakub@rainbow.me>
2023-02-22 11:11:18 +0100

    Move WalletScreen to a separate direcotry, refactor it to TS and extract shared elements to separate files (#4642)

--- src/components/cards/ReceiveAssetsCard.tsx
+++ src/components/cards/ReceiveAssetsCard.tsx
@@ -5,7 +5,6 @@ import { useAccountProfile, useClipboard } from '@/hooks';
 import { ButtonPressAnimation } from '../animations';
 import { FloatingEmojis } from '@/components/floating-emojis';
 import { useRecoilState } from 'recoil';
-import { addressCopiedToastAtom } from '@/screens/WalletScreen';
 import { haptics } from '@/utils';
 import { useNavigation } from '@/navigation';
 import Routes from '@/navigation/routesNames';
@@ -15,6 +14,7 @@ import { useAccountAccentColor } from '@/hooks/useAccountAccentColor';
 import { TintButton } from './reusables/TintButton';
 import Skeleton, { FakeText } from '../skeleton/Skeleton';
 import * as i18n from '@/languages';
+import { addressCopiedToastAtom } from '@/recoil/addressCopiedToastAtom';
 
 export const RECEIVE_CARD_HEIGHT = 174;
 const TRANSLATIONS = i18n.l.cards.receive;
