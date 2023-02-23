8bd5e510497d75d8dd8492e6d105079ee3d4de69
Jakub Adamczyk <jakub@rainbow.me>
2023-02-23 11:38:39 +0100

    remove stray console logs

--- src/hooks/usePersistentDominantColorFromImage.ts
+++ src/hooks/usePersistentDominantColorFromImage.ts
@@ -12,32 +12,25 @@ const COLOR_TO_MEASURE_AGAINST = '#333333';
 const SIZE_FOR_COLOR_CALCULATION = 40;
 
 export function usePersistentDominantColorFromImage(url?: string | null) {
-  // Will update if `url` changes
   const cachedColor = useMemo(
     () => (url ? storage.getString(url) : undefined),
     [url]
   );
   const [color, setColor] = useState(cachedColor);
-  // Will update if `url` changes
   const externalUrl = useMemo(
     () =>
       url ? maybeSignUri(url, { w: SIZE_FOR_COLOR_CALCULATION }) || url : url,
     [url]
   );
 
-  // console.log(externalUrl, cachedColor);
-
-  // Only Runs if `url`/`externalUrl` changes
   useEffect(() => {
     if (!cachedColor && externalUrl && url) {
       getDominantColorFromImage(externalUrl, COLOR_TO_MEASURE_AGAINST)
         .then(color => {
-          console.log('FINISHED GETTING COLOR: ', color);
           setColor(color);
           storage.set(url, color);
         })
         .catch(() => {
-          console.log('ERROR SETTING TO UNDEFINED');
           setColor(undefined);
         });
     } else {
