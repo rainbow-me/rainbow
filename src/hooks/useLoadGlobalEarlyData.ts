3c8898ee02ea6e1a68bd308e739c792abdab2ea6
Skylar Barrera <skylar.barrera@gmail.com>
2023-03-03 09:22:28 -0700

    adjust staged data loading  (#4677)
    * reprioritize early + late data loading
    
    * note
    
    * move user lists
    
    * oop

--- src/hooks/useLoadGlobalEarlyData.ts
+++ src/hooks/useLoadGlobalEarlyData.ts
@@ -1,10 +1,7 @@
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { contactsLoadState } from '@/redux/contacts';
-import { imageMetadataCacheLoadState } from '@/redux/imageMetadata';
-import { keyboardHeightsLoadState } from '@/redux/keyboardHeight';
 import { settingsLoadLanguage, settingsLoadState } from '@/redux/settings';
-import { transactionSignaturesLoadState } from '@/redux/transactionSignatures';
+
 import { promiseUtils } from '@/utils';
 import logger from '@/utils/logger';
 
@@ -14,14 +11,13 @@ export default function useLoadGlobalEarlyData() {
   const loadGlobalData = useCallback(async () => {
     logger.sentry('Load wallet global early data');
     const promises = [];
-    const p1 = dispatch(settingsLoadState());
-    const p2 = dispatch(imageMetadataCacheLoadState());
-    const p3 = dispatch(keyboardHeightsLoadState());
-    const p4 = dispatch(transactionSignaturesLoadState());
-    const p5 = dispatch(contactsLoadState());
-    const p6 = dispatch(settingsLoadLanguage());
 
-    promises.push(p1, p2, p3, p4, p5, p6);
+    // native currency, app icon, testnetsEnabled, flashbotsEnabled
+    const p1 = dispatch(settingsLoadState());
+    // language
+    const p2 = dispatch(settingsLoadLanguage());
+
+    promises.push(p1, p2);
 
     // @ts-expect-error ts-migrate(2345) FIXME: Argument of type '((dispatch: Dispatch<any>) => Pr... Remove this comment to see the full error message
     return promiseUtils.PromiseAllWithFails(promises);
