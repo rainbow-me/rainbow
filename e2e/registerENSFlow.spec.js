dd6e5293b5ad670da05f04100f08be6dd5343c1a
Skylar Barrera <skylar.barrera@gmail.com>
2023-03-09 15:12:37 -0500

    scroll down a bit

--- e2e/registerENSFlow.spec.js
+++ e2e/registerENSFlow.spec.js
@@ -224,6 +224,7 @@ describe('Register ENS Flow', () => {
 
   it('Should go to ENS flow pressing the ENS banner', async () => {
     device.getPlatform() === 'android' && (await Helpers.delay(2000));
+    await Helpers.swipe('discover-sheet', 'up', 'slow', 0.2);
     await Helpers.waitAndTap('ens-create-profile-card');
     await Helpers.checkIfVisible('ens-intro-sheet');
   });
