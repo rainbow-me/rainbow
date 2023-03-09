b9b5965719d9f85c1396c0ab6beaa2d66234e741
Eric Bailey <git@esb.lol>
2023-03-09 15:37:45 -0600

    fix: remove hook

--- src/resources/nfts/index.ts
+++ src/resources/nfts/index.ts
@@ -3,8 +3,7 @@ import { createQueryKey, queryClient, QueryFunctionArgs } from '@/react-query';
 import { NFT } from '@/resources/nfts/types';
 import { UniqueAsset } from '@/entities/uniqueAssets';
 import { useIsMounted } from '@/hooks';
-import { fetchSimplehashNfts, START_CURSOR } from '@/resources/nfts/simplehash';
-import { SimplehashNFT } from '@/resources/nfts/simplehash/types';
+import { fetchSimplehashNfts } from '@/resources/nfts/simplehash';
 import { useEffect, useReducer, useState } from 'react';
 import { uniqBy } from 'lodash';
 import { simplehashNFTToUniqueAsset } from '@/resources/nfts/simplehash/utils';
@@ -69,37 +68,4 @@ export function useLegacyNFTs(address: string): UniqueAsset[] {
   }, [address, cursor, isFinished, mounted, nfts.length, queryClient]);
 
   return nfts;
-}
-
-export function useLegacyNFTsV2({
-  accountAddress,
-}: {
-  accountAddress: string;
-}) {
-  return useQuery(
-    ['nfts', accountAddress],
-    async () => {
-      const then = Date.now();
-      let cursor = START_CURSOR;
-      let nfts: SimplehashNFT[] = [];
-
-      await (async function fetch() {
-        const res = await fetchSimplehashNfts(accountAddress, cursor);
-
-        nfts.push(...res.nfts);
-
-        if (res.nextCursor) {
-          cursor = res.nextCursor;
-          await fetch();
-        }
-      })();
-
-      logger.debug(`Legacy NFTs loaded in ${Date.now() - then}ms`);
-
-      return nfts;
-    },
-    {
-      enabled: !!accountAddress,
-    }
-  );
 }
