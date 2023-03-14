7d7afa2483a05ba194372dd40b0fa3e45918ed18
Skylar Barrera <skylar.barrera@gmail.com>
2023-03-14 12:52:03 -0400

    lint

--- src/handlers/deeplinks.ts
+++ src/handlers/deeplinks.ts
@@ -45,7 +45,7 @@ export default async function handleDeeplink(
    * We need to wait till the wallet is ready to handle any deeplink
    */
   while (store.getState().data.isLoadingAssets) {
-      logger.info(`handleDeeplink: Waiting for wallet to be ready`);
+    logger.info(`handleDeeplink: Waiting for wallet to be ready`);
     await delay(50);
   }
 
