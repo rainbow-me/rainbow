80acb47608b9c4b1617105ce998f32e699229f8a
Ben Goldberg <bengoldberg@rainbow.me>
2023-03-14 18:44:41 -0600

    profiles fix

--- src/hooks/useCollectible.ts
+++ src/hooks/useCollectible.ts
@@ -1,7 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
 import { useMemo } from 'react';
 import { ParsedAddressAsset, UniqueAsset } from '@/entities';
-import { nftsQueryKey, useLegacyNFTs } from '@/resources/nfts';
+import { useLegacyNFTs } from '@/resources/nfts';
 import { useAccountSettings } from '.';
 
 export default function useCollectible(
@@ -10,29 +9,24 @@ export default function useCollectible(
 ) {
   const { accountAddress } = useAccountSettings();
   const { data: selfNFTs } = useLegacyNFTs({ address: accountAddress });
-  const { data: externalNFTs } = useQuery<UniqueAsset[]>(
-    nftsQueryKey({ address: externalAddress ?? '' }),
-    // We just want to watch for changes in the query key,
-    // so just supplying a noop function & staleTime of Infinity.
-    async () => [],
-    { staleTime: Infinity }
-  );
+  const { data: externalNFTs } = useLegacyNFTs({
+    address: externalAddress ?? '',
+  });
   const isExternal = Boolean(externalAddress);
-  // Use the appropriate tokens based on if the user is viewing the
-  // current accounts tokens, or external tokens (e.g. ProfileSheet)
-  const uniqueTokens = useMemo(() => (isExternal ? externalNFTs : selfNFTs), [
+  // Use the appropriate nfts based on if the user is viewing the
+  // current account's nfts, or external nfts (e.g. ProfileSheet)
+  const nfts = useMemo(() => (isExternal ? externalNFTs : selfNFTs), [
     externalNFTs,
     isExternal,
     selfNFTs,
   ]);
 
   const asset = useMemo(() => {
-    let matched = uniqueTokens!.find(
-      (uniqueToken: UniqueAsset) =>
-        uniqueToken.uniqueId === initialAsset?.uniqueId
+    const matched = nfts?.find(
+      (nft: UniqueAsset) => nft.uniqueId === initialAsset?.uniqueId
     );
     return matched || initialAsset;
-  }, [initialAsset, uniqueTokens]);
+  }, [initialAsset, nfts]);
 
   return { ...asset, isExternal };
 }
