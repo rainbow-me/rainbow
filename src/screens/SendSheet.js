9712889b2e2dee027438130645d1ee645b691dc5
Skylar Barrera <skylar.barrera@gmail.com>
2023-03-14 20:53:30 -0400

    fix send colors (#4718)

--- src/screens/SendSheet.js
+++ src/screens/SendSheet.js
@@ -187,17 +187,7 @@ export default function SendSheet(props) {
 
   const isNft = selected?.type === AssetTypes.nft;
 
-  const address = selected?.mainnet_address || selected?.address;
-  const type = selected?.mainnet_address ? AssetTypes.token : selected?.type;
-  let colorForAsset = useColorForAsset(
-    {
-      address,
-      type,
-    },
-    null,
-    false,
-    true
-  );
+  let colorForAsset = useColorForAsset(selected, null, false, true);
   if (isNft) {
     colorForAsset = colors.appleBlue;
   }
@@ -939,6 +929,7 @@ export default function SendSheet(props) {
     <Container testID="send-sheet">
       <SheetContainer>
         <SendHeader
+          colorForAsset={colorForAsset}
           contacts={contacts}
           fromProfile={params?.fromProfile}
           hideDivider={showAssetForm}
@@ -1019,6 +1010,7 @@ export default function SendSheet(props) {
                 weight="heavy"
               />
             }
+            colorForAsset={colorForAsset}
             nativeAmount={amountDetails.nativeAmount}
             nativeCurrency={nativeCurrency}
             nativeCurrencyInputRef={nativeCurrencyInputRef}
