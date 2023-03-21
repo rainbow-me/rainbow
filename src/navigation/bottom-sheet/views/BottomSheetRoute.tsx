364323e05f82fa341495748934415cb28bb009a0
Jakub Adamczyk <jakub@rainbow.me>
2023-03-21 12:26:33 +0100

    prettier

--- src/navigation/bottom-sheet/views/BottomSheetRoute.tsx
+++ src/navigation/bottom-sheet/views/BottomSheetRoute.tsx
@@ -27,7 +27,7 @@ const BottomSheetRoute = ({
   onDismiss,
   removing = false,
 }: Props) => {
-  //#region extract options
+  // #region extract options
   const {
     enableContentPanningGesture,
     enableHandlePanningGesture,
@@ -38,18 +38,18 @@ const BottomSheetRoute = ({
     height = DEFAULT_HEIGHT,
     offsetY = android ? 20 : 3,
   } = options || {};
-  //#endregion
+  // #endregion
 
-  //#region refs
+  // #region refs
   const ref = useRef<BottomSheet>(null);
 
   const removingRef = useRef(false);
   removingRef.current = removing;
 
   // const
-  //#endregion
+  // #endregion
 
-  //#region styles
+  // #region styles
   const screenContainerStyle: ViewStyle = useMemo(
     () => ({
       bottom: 0,
@@ -66,9 +66,9 @@ const BottomSheetRoute = ({
     }),
     [backdropColor]
   );
-  //#endregion
+  // #endregion
 
-  //#region context methods
+  // #region context methods
   const handleSettingSnapPoints = useCallback(
     (_snapPoints: (string | number)[]) => {
       navigation.setOptions({ snapPoints: _snapPoints });
@@ -105,16 +105,16 @@ const BottomSheetRoute = ({
       handleSettingSnapPoints,
     ]
   );
-  //#endregion
+  // #endregion
 
-  //#region callbacks
+  // #region callbacks
   const handleOnClose = useCallback(() => {
-      onDismiss(routeKey, removingRef.current);
+    onDismiss(routeKey, removingRef.current);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  //#endregion
+  // #endregion
 
-  //#region effects
+  // #region effects
   useEffect(() => {
     if (removing === true && ref.current) {
       // close keyboard before closing the modal
@@ -127,9 +127,9 @@ const BottomSheetRoute = ({
       }
     }
   }, [removing]);
-  //#endregion
+  // #endregion
 
-  //#region renders
+  // #region renders
   const renderBackdropComponent = useCallback(
     (props: BottomSheetBackdropProps) => (
       <BottomSheetBackdrop
