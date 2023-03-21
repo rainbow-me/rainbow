24e107967e2bf23c8f8f43a29d2a356698c95d0b
Eric Bailey <git@esb.lol>
2023-03-21 12:01:36 -0500

    feat: use new method of switching accounts

--- src/utils/walletConnect.ts
+++ src/utils/walletConnect.ts
@@ -872,44 +872,126 @@ export function getAllActiveSessionsSync() {
 }
 
 /**
- * Updates an existing session with new values
+ * Adds an account to an existing session
  */
-export async function updateSession(
+export async function addAccountToSession(
   session: SessionTypes.Struct,
   { address }: { address?: string }
 ) {
-  const client = await web3WalletClient;
+  try {
+    const client = await web3WalletClient;
 
-  const namespaces: Parameters<
-    typeof client.updateSession
-  >[0]['namespaces'] = {};
+    const namespaces: Parameters<
+      typeof client.updateSession
+    >[0]['namespaces'] = {};
 
-  for (const [key, value] of Object.entries(session.requiredNamespaces)) {
-    namespaces[key] = {
-      accounts: [],
-      methods: value.methods,
-      events: value.events,
-    };
+    for (const [key, value] of Object.entries(session.requiredNamespaces)) {
+      /**
+       * The `namespace` that corresponds to the `requiredNamespace` that was
+       * requested when connecting the session. The `requiredNamespace` does
+       * not have `accounts`, since it merely requests chains and methods, so
+       * we need to referernce the connected `namespace` for existing `account`
+       * values.
+       */
+      const ns = session.namespaces[key];
 
-    if (value.chains) {
-      for (const chain of value.chains) {
-        const chainId = parseInt(chain.split(`${key}:`)[1]);
-        namespaces[key].accounts.push(`${key}:${chainId}:${address}`);
+      namespaces[key] = {
+        accounts: ns.accounts || [],
+        methods: value.methods,
+        events: value.events,
+      };
+
+      if (value.chains) {
+        for (const chain of value.chains) {
+          const chainId = parseInt(chain.split(`${key}:`)[1]);
+          const account = `${key}:${chainId}:${address}`;
+
+          /*
+           * Add latest selected accounts to beginning of array so that our
+           * switch wallet sheet shows that one as selected
+           */
+          if (!namespaces[key].accounts.includes(account)) {
+            namespaces[key].accounts.unshift(account);
+          } else {
+            // remove and re-add to start of array
+            namespaces[key].accounts.splice(
+              namespaces[key].accounts.indexOf(account),
+              1
+            );
+            namespaces[key].accounts.unshift(account);
+          }
+        }
+      } else {
+        logger.error(
+          new RainbowError(
+            `WC v2: namespace is missing chains prop when updating`
+          ),
+          { requiredNamespaces: session.requiredNamespaces }
+        );
       }
-    } else {
-      logger.error(
-        new RainbowError(
-          `WC v2: namespace is missing chains prop when updating`
-        ),
-        { requiredNamespaces: session.requiredNamespaces }
-      );
     }
-  }
 
-  await client.updateSession({
-    topic: session.topic,
-    namespaces,
-  });
+    logger.debug(`WC v2: updating session`, {
+      namespaces,
+    });
+
+    await client.updateSession({
+      topic: session.topic,
+      namespaces,
+    });
+  } catch (e: any) {
+    logger.error(new RainbowError(`WC v2: error adding account to session`), {
+      message: e.message,
+    });
+  }
+}
+
+export async function changeAccount(
+  session: SessionTypes.Struct,
+  { address }: { address?: string }
+) {
+  try {
+    const client = await web3WalletClient;
+
+    /*
+     * Before we can effectively switch accounts, we need to add the account to
+     * the session. Only then can we tell the dapp to use the new account.
+     */
+    await addAccountToSession(session, { address });
+
+    for (const value of Object.values(session.requiredNamespaces)) {
+      if (!value.chains) {
+        logger.debug(`WC v2: changeAccount, no chains found for namespace`);
+        continue;
+      }
+
+      for (const chainId of value.chains) {
+        logger.debug(`WC v2: changeAccount, updating accounts for chainId`, {
+          chainId,
+        });
+
+        // tell the dapp to use the new account
+        await client.emitSessionEvent({
+          topic: session.topic,
+          event: {
+            name: 'accountsChanged',
+            data: [address],
+          },
+          chainId,
+        });
+
+        logger.debug(`WC v2: changeAccount, updated accounts for chainId`, {
+          chainId,
+        });
+      }
+    }
+
+    logger.debug(`WC v2: changeAccount complete`);
+  } catch (e: any) {
+    logger.error(new RainbowError(`WC v2: error changing account`), {
+      message: e.message,
+    });
+  }
 }
 
 /**
