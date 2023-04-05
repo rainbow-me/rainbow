951293f1cebdcf7ff5cd79d674eb7538232b451b
Eric Bailey <git@esb.lol>
2023-04-05 12:57:35 -0500

    feat: use extension, document

---
+++ src/screens/Portal/README.md
@@ -0,0 +1,87 @@
+# Portal
+
+A low-level primitive for building new sheets that appear on top of other
+content.
+
+## Usage
+
+```tsx
+import * as portal from '@/screens/Portal';
+
+export function Comp() {
+  const { open } = portal.useOpen();
+
+  const handlePress = () => {
+    open(() => (
+      <>
+        <Box>
+          <Text>Hello world</Text>
+        </Box>
+      </>
+    ), { sheetHeight: 400 });
+  }
+
+  return (
+    <Button onPress={handlePress}>
+      Show some sheet
+    </Button>
+  )
+}
+```
+
+There's also an API for use outside a React component, but try to use the hook
+where possible.
+
+```tsx
+import * as portal from '@/screens/Portal';
+
+portal.open(() => <>...</>, { ...options });
+```
+
+## Extension
+
+We extend this primitive in some cases, like our `@/screens/Explain` sheet.
+There, we re-export `useOpen` and `open`, as well as provide some pre-built
+components that can be used to make consistent explainer sheets.
+
+```tsx
+import * as explain from '@/screens/Explain'
+
+explain.open(() => (
+  <>
+    <explain.Emoji>💰</explain.Emoji>
+    <explain.Title>This is a title</explain.Title>
+    <explain.Body>And this is body text</explain.Body>
+  </>
+), { sheetHeight: 640 });
+```
+
+## Creating navigable routes
+
+**All portals are rendered on the `Routes.PORTAL` route.** If you need to create
+a separate route, maybe to deeplink to, then you'll need to create a new
+`@/screens/<screen>`, assign it a separate route name in
+`@/navigation/routeNames`, and add the new route to `Routes.ios.js` and
+`Routes.android.js`.
+
+You can use the `Portal` sheet wrapper as a starting point, though (it just uses
+our `SimpleSheet` under the hood:
+
+```tsx
+export function Portal() {
+  const { params } = useRoute<RouteProp<NavigationRouteParams, 'MyRoute'>>();
+
+  return (
+    <SimpleSheet backgroundColor="white" scrollEnabled={false}>
+      <Box
+        paddingVertical="44px"
+        paddingHorizontal="32px"
+        height="full"
+        background="surfaceSecondary"
+      >
+        ...
+      </Box>
+    </SimpleSheet>
+  );
+}
+```
--- src/screens/Portal/index.tsx
+++ src/screens/Portal/index.tsx
@@ -3,16 +3,8 @@ import { useRoute, useNavigation, RouteProp } from '@react-navigation/native';
 
 import { Navigation } from '@/navigation';
 import Routes from '@/navigation/routesNames';
-import {
-  Box,
-  Text,
-  TextProps,
-  AccentColorProvider,
-  Stack,
-} from '@/design-system';
+import { Box } from '@/design-system';
 import { SimpleSheet } from '@/components/sheet/SimpleSheet';
-import { ImgixImage } from '@/components/images';
-import SheetActionButton from '@/components/sheet/sheet-action-buttons/SheetActionButton';
 
 type PortalSheetProps = {
   sheetHeight?: number;
@@ -22,137 +14,6 @@ type PortalSheetProps = {
 type NavigationRouteParams = {
   Portal: PortalSheetProps;
 };
-
-/**
- * Proxy for `SheetActionButton` with no changes to API.
- *
- * TODO This is proxied because eventually I'd like to replace this.
- */
-export const Button = SheetActionButton;
-
-/**
- * Image icon component for use within the Portal sheet.
- *
- *   `import * as p from '@/screens/Portal';`
- *
- *   `<p.Logo accentColor={...} source={require('...')} />`
- */
-export function Logo({
-  accentColor,
-  source,
-  size = 64,
-}: {
-  accentColor: string;
-  source: StaticImageData;
-  size?: number;
-}) {
-  return (
-    <Box
-      flexDirection="row"
-      alignItems="center"
-      justifyContent="center"
-      paddingBottom="20px"
-    >
-      <AccentColorProvider color={accentColor}>
-        {/* @ts-expect-error Box doesn't like ImgixImage */}
-        <Box
-          as={ImgixImage}
-          source={source}
-          size={size}
-          width={{ custom: size }}
-          height={{ custom: size }}
-          shadow="18px accent"
-        />
-      </AccentColorProvider>
-    </Box>
-  );
-}
-
-/**
- * Emoji "logo" for use within the Portal sheet.
- */
-export function Emoji({ children }: { children: string }) {
-  return (
-    <Box paddingBottom="20px">
-      <Text
-        containsEmoji
-        color="accent"
-        align="center"
-        size="44pt"
-        weight="bold"
-      >
-        {children}
-      </Text>
-    </Box>
-  );
-}
-
-/**
- * Title text for use within the Portal sheet.
- */
-export function Title({
-  children,
-  color,
-  size,
-  weight,
-  ...props
-}: Omit<Partial<TextProps>, 'children'> & { children: string }) {
-  return (
-    <Box paddingBottom="36px">
-      <Text
-        color={color || 'label'}
-        weight={weight || 'heavy'}
-        size={size || '26pt'}
-        align="center"
-        {...props}
-      >
-        {children}
-      </Text>
-    </Box>
-  );
-}
-
-/**
- * Body text for use within the Portal sheet.
- */
-export function Body({
-  children,
-  size,
-  color,
-  ...props
-}: Omit<Partial<TextProps>, 'children'> & { children: string }) {
-  return (
-    <Box paddingBottom="20px">
-      <Text
-        color={color || 'labelSecondary'}
-        size={size || '17pt / 135%'}
-        align="center"
-        {...props}
-      >
-        {children}
-      </Text>
-    </Box>
-  );
-}
-
-/**
- * A wrapper for any buttons you might want to add to the Portal sheet.
- * Multiple buttons are automatically spaced apart.
- *
- * TODO eventually this should be sticky to the bottom of the sheet to match
- * designs.
- */
-export function Footer({
-  children,
-}: {
-  children: React.ReactNode | React.ReactNodeArray;
-}) {
-  return (
-    <Box paddingTop="12px">
-      <Stack space="16px">{children}</Stack>
-    </Box>
-  );
-}
 
 /**
  * The core Portal sheet
