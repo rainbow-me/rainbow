6f6d1b24cec2f447839f707cc3ed46667288b2cd
Eric Bailey <git@esb.lol>
2023-04-28 15:19:20 -0500

    feat: add tests for deeplinks

---
+++ src/handlers/__tests__/deeplinks.test.ts
@@ -0,0 +1,233 @@
+import { jest, test, expect, beforeEach, afterEach } from '@jest/globals';
+import { mocked } from '@/testing/utils';
+
+import store from '@/redux/store';
+import { delay } from '@/utils/delay';
+import ethereumUtils from '@/utils/ethereumUtils';
+import {
+  setHasPendingDeeplinkPendingRedirect,
+  pair,
+} from '@/utils/walletConnect';
+import Routes from '@/navigation/routesNames';
+import { Navigation } from '@/navigation';
+import { scheduleActionOnAssetReceived } from '@/redux/data';
+import { analyticsV2 } from '@/analytics';
+import {
+  checkIsValidAddressOrDomain,
+  isENSAddressFormat,
+} from '@/helpers/validators';
+import { fetchReverseRecordWithRetry } from '@/utils/profileUtils';
+
+jest.mock('@/redux/store');
+jest.mock('@/redux/walletconnect', () => ({
+  walletConnectOnSessionRequest: jest.fn(),
+  walletConnectRemovePendingRedirect: jest.fn(),
+  walletConnectSetPendingRedirect: jest.fn(),
+}));
+jest.mock('@/redux/data', () => ({
+  scheduleActionOnAssetReceived: jest.fn(),
+}));
+jest.mock('@/redux/explorer', () => ({
+  emitAssetRequest: jest.fn(),
+  emitChartsRequest: jest.fn(),
+}));
+jest.mock('@/utils/profileUtils', () => ({
+  fetchReverseRecordWithRetry: jest.fn(),
+}));
+jest.mock('@/config/experimentalHooks', () => ({
+  PROFILES: 'ENS Profiles',
+}));
+jest.mock('@/helpers/validators', () => ({
+  checkIsValidAddressOrDomain: jest.fn(),
+  isENSAddressFormat: jest.fn(),
+}));
+jest.mock('@/navigation', () => ({
+  Navigation: {
+    handleAction: jest.fn(),
+  },
+}));
+jest.mock('@/utils/ethereumUtils', () => ({
+  parseEthereumUrl: jest.fn(),
+  getAssetFromAllAssets: jest.fn(),
+}));
+jest.mock('@/utils/walletConnect', () => ({
+  pair: jest.fn(),
+  setHasPendingDeeplinkPendingRedirect: jest.fn(),
+}));
+jest.mock('@/utils/delay');
+jest.mock('@/analytics');
+
+import handleDeepLink from '../deeplinks';
+
+beforeEach(() => {
+  jest.useFakeTimers();
+
+  mocked(store.getState).mockReturnValue({
+    // @ts-ignore
+    data: {
+      isLoadingAssets: false,
+    },
+  });
+});
+
+afterEach(() => {
+  jest.useRealTimers();
+  jest.resetAllMocks();
+});
+
+test(`runs but does nothing`, async () => {
+  await handleDeepLink('https://example.com');
+});
+
+test(`waits for isLoadingAssets to be falsy`, async () => {
+  mocked(store.getState).mockReturnValue({
+    // @ts-ignore
+    data: {
+      isLoadingAssets: true,
+    },
+  });
+
+  // don't await this, it will never resolve in this test
+  handleDeepLink('https://example.com');
+
+  jest.advanceTimersByTime(50);
+
+  mocked(store.getState).mockReturnValue({
+    // @ts-ignore
+    data: {
+      isLoadingAssets: false,
+    },
+  });
+
+  jest.advanceTimersByTime(50);
+
+  expect(delay).toHaveBeenCalledTimes(1);
+});
+
+test(`handles ethereum:// protocol`, async () => {
+  await handleDeepLink('ethereum:payment-brunobarbieri.eth@1?value=1e2');
+  expect(ethereumUtils.parseEthereumUrl).toHaveBeenCalledTimes(1);
+});
+
+test(`handles wc:// protocol`, async () => {
+  await handleDeepLink(`wc:topic@2?relay-protocol=protocol&symKey=symKey`);
+  expect(setHasPendingDeeplinkPendingRedirect).toHaveBeenCalledTimes(1);
+  expect(pair).toHaveBeenCalledTimes(1);
+});
+
+test(`handles https:// protocol for WalletConnect`, async () => {
+  await handleDeepLink(
+    `https://rnbwapp.com/wc?uri=wc:topic@2?relay-protocol=protocol&symKey=symKey`
+  );
+  expect(setHasPendingDeeplinkPendingRedirect).toHaveBeenCalledTimes(1);
+  expect(pair).toHaveBeenCalledTimes(1);
+});
+
+test(`handles rainbow:// protocol for WalletConnect`, async () => {
+  await handleDeepLink(
+    `rainbow://wc?uri=wc:topic@2?relay-protocol=protocol&symKey=symKey`
+  );
+  expect(setHasPendingDeeplinkPendingRedirect).toHaveBeenCalledTimes(1);
+  expect(pair).toHaveBeenCalledTimes(1);
+});
+
+test(`handles https:// protocol for tokens, found asset`, async () => {
+  // @ts-ignore just need a truthy value
+  mocked(ethereumUtils.getAssetFromAllAssets).mockReturnValueOnce({});
+
+  await handleDeepLink(
+    `https://rnbwapp.com/token?addr=0x123`,
+    Routes.ADD_CASH_SHEET
+  );
+
+  expect(Navigation.handleAction).toHaveBeenCalledTimes(1);
+
+  jest.advanceTimersByTime(50);
+
+  expect(Navigation.handleAction).toHaveBeenCalledWith(
+    Routes.EXPANDED_ASSET_SHEET,
+    {
+      asset: {},
+      fromDiscover: true,
+      type: 'token',
+    }
+  );
+});
+
+test(`handles https:// protocol for tokens, no asset found`, async () => {
+  // @ts-ignore just need a truthy value
+  mocked(ethereumUtils.getAssetFromAllAssets).mockReturnValueOnce(undefined);
+
+  await handleDeepLink(
+    `https://rnbwapp.com/token?addr=0x123`,
+    Routes.WELCOME_SCREEN
+  );
+
+  expect(Navigation.handleAction).toHaveBeenCalledTimes(0);
+
+  jest.advanceTimersByTime(50);
+
+  expect(store.dispatch).toHaveBeenCalledTimes(2);
+  expect(scheduleActionOnAssetReceived).toHaveBeenCalledTimes(1);
+});
+
+test(`handles https:// protocol for f2c, no query params`, async () => {
+  await handleDeepLink(`https://rnbwapp.com/f2c`);
+  expect(analyticsV2.track).not.toHaveBeenCalled();
+});
+
+test(`handles https:// protocol for f2c, ramp`, async () => {
+  const provider = 'ramp';
+  const sessionId = '123';
+
+  await handleDeepLink(
+    `https://rnbwapp.com/f2c?provider=${provider}&sessionId=${sessionId}`
+  );
+
+  expect(analyticsV2.track).toHaveBeenCalledWith(
+    analyticsV2.event.f2cProviderFlowCompleted,
+    {
+      provider,
+      sessionId,
+      success: true,
+    }
+  );
+});
+
+test(`handles https:// protocol for f2c, other provider`, async () => {
+  const provider = 'other';
+  const sessionId = '123';
+
+  await handleDeepLink(
+    `https://rnbwapp.com/f2c?provider=${provider}&sessionId=${sessionId}`
+  );
+
+  expect(analyticsV2.track).toHaveBeenCalledWith(
+    analyticsV2.event.f2cProviderFlowCompleted,
+    {
+      provider,
+      sessionId,
+    }
+  );
+});
+
+test(`handles https:// protocol for ens profiles`, async () => {
+  mocked(checkIsValidAddressOrDomain).mockResolvedValueOnce(true);
+  mocked(isENSAddressFormat).mockReturnValueOnce(true);
+
+  await handleDeepLink(`https://rainbow.me/estrattonbailey.eth`);
+
+  expect(fetchReverseRecordWithRetry).toHaveBeenCalledTimes(0);
+  expect(Navigation.handleAction).toHaveBeenCalledTimes(1);
+});
+
+test(`handles https:// protocol for profile addresses`, async () => {
+  mocked(checkIsValidAddressOrDomain).mockResolvedValueOnce(true);
+  mocked(isENSAddressFormat).mockReturnValueOnce(false);
+  mocked(fetchReverseRecordWithRetry).mockResolvedValue('estrattonbailey.eth');
+
+  await handleDeepLink(`https://rainbow.me/0x123`);
+
+  expect(fetchReverseRecordWithRetry).toHaveBeenCalledTimes(1);
+  expect(Navigation.handleAction).toHaveBeenCalledTimes(1);
+});
---
+++ src/utils/__mocks__/delay.ts
@@ -0,0 +1,8 @@
+/**
+ * @desc Promise that will resolve after the ms interval
+ */
+export const delay = jest.fn().mockImplementation(
+  (ms: number): Promise<void> => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+);
