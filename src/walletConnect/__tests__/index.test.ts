3767c0ba79816e42cd08759531973f71d53cf159
Eric Bailey <git@esb.lol>
2023-05-09 11:44:24 -0500

    feat: add parseRPCParams test

--- src/walletConnect/__tests__/index.test.ts
+++ src/walletConnect/__tests__/index.test.ts
@@ -1,4 +1,5 @@
 import { jest, test } from '@jest/globals';
+import { isAddress } from '@ethersproject/address';
 
 jest.mock('@walletconnect/core');
 
@@ -22,7 +23,7 @@ jest.mock('@/walletConnect/sheets/AuthRequest', () => ({
 import { parseRPCParams } from '@/walletConnect';
 import { RPCMethod } from '@/walletConnect/types';
 
-test(`works`, () => {
+test(`parseRPCParams`, () => {
   const send_transaction = {
     method: RPCMethod.SendTransaction,
     params: [
@@ -51,49 +52,63 @@ test(`works`, () => {
       '0x4d7920656d61696c206973206a6f686e40646f652e636f6d202d2031363833353832363433383539',
     ],
   };
-  const eth_signedTypedData = {
+  const eth_signTypedData = {
     method: RPCMethod.SignTypedData,
     params: [
       '0xA2Eaa7BAe79F0F9FfB23667cdAc9CE285b30aE0E',
-      {
-        types: {
-          EIP712Domain: [
-            { name: 'name', type: 'string' },
-            { name: 'version', type: 'string' },
-            { name: 'chainId', type: 'uint256' },
-            { name: 'verifyingContract', type: 'address' },
+      `{
+        "types": {
+          "EIP712Domain": [
+            { "name": "name", "type": "string" },
+            { "name": "version", "type": "string" },
+            { "name": "chainId", "type": "uint256" },
+            { "name": "verifyingContract", "type": "address" }
           ],
-          Person: [
-            { name: 'name', type: 'string' },
-            { name: 'wallet', type: 'address' },
+          "Person": [
+            { "name": "name", "type": "string" },
+            { "name": "wallet", "type": "address" }
           ],
-          Mail: [
-            { name: 'from', type: 'Person' },
-            { name: 'to', type: 'Person' },
-            { name: 'contents', type: 'string' },
-          ],
+          "Mail": [
+            { "name": "from", "type": "Person" },
+            { "name": "to", "type": "Person" },
+            { "name": "contents", "type": "string" }
+          ]
         },
-        primaryType: 'Mail',
-        domain: {
-          name: 'Ether Mail',
-          version: '1',
-          chainId: 1,
-          verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+        "primaryType": "Mail",
+        "domain": {
+          "name": "Ether Mail",
+          "version": "1",
+          "chainId": 1,
+          "verifyingContract": "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"
         },
-        message: {
-          from: {
-            name: 'Cow',
-            wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+        "message": {
+          "from": {
+            "name": "Cow",
+            "wallet": "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826"
           },
-          to: {
-            name: 'Bob',
-            wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+          "to": {
+            "name": "Bob",
+            "wallet": "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"
           },
-          contents: 'Hello, Bob!',
-        },
-      },
+          "contents": "Hello, Bob!"
+        }
+      }`,
     ],
   };
 
-  const params = parseRPCParams(send_transaction);
+  const sendTransactionResult = parseRPCParams(send_transaction);
+  expect(sendTransactionResult).toMatchSnapshot();
+  expect(isAddress(sendTransactionResult.address!)).toBeTruthy();
+
+  const personalSignResult = parseRPCParams(personal_sign);
+  expect(personalSignResult).toMatchSnapshot();
+  expect(isAddress(personalSignResult.address!)).toBeTruthy();
+
+  const ethSignResult = parseRPCParams(eth_sign);
+  expect(ethSignResult).toMatchSnapshot();
+  expect(isAddress(ethSignResult.address!)).toBeTruthy();
+
+  const ethSignTypedDataResult = parseRPCParams(eth_signTypedData);
+  expect(ethSignTypedDataResult).toMatchSnapshot();
+  expect(isAddress(ethSignTypedDataResult.address!)).toBeTruthy();
 });
