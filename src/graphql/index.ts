fe8636408fceadf1dcbc36dc86f344f91f76c8b0
Ben Goldberg <bengoldberg@rainbow.me>
2023-05-18 15:42:23 -0700

    fix

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -3,7 +3,7 @@ import { getFetchRequester } from './utils/getFetchRequester';
 import { getSdk as getEnsSdk } from './__generated__/ens';
 import { getSdk as getMetadataSdk } from './__generated__/metadata';
 import { getSdk as getUniswapSdk } from './__generated__/uniswap';
-import { getSdk as getNftsSdk } from './__generated__/nfts';
+import { getSdk as getArcSdk } from './__generated__/arc';
 
 export const ensClient = getEnsSdk(getFetchRequester(config.ens.schema));
 export const metadataClient = getMetadataSdk(
@@ -12,4 +12,4 @@ export const metadataClient = getMetadataSdk(
 export const uniswapClient = getUniswapSdk(
   getFetchRequester(config.uniswap.schema)
 );
-export const arcClient = getNftsSdk(getFetchRequester(config.arc.schema));
+export const arcClient = getArcSdk(getFetchRequester(config.arc.schema));
