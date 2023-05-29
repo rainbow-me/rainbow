800ef656477fe39b42671262d286aa2229b6f95b
Jakub Adamczyk <jakub@rainbow.me>
2023-05-29 13:26:55 +0200

    Diagnostics sheet changes (#4778)

---
+++ src/screens/Diagnostics/DiagnosticsSecretInput.tsx
@@ -0,0 +1,65 @@
+import { useTheme } from '@/theme';
+import React, { useCallback } from 'react';
+import { WrappedAlert as Alert } from '@/helpers/alert';
+import lang from 'i18n-js';
+import Clipboard from '@react-native-community/clipboard';
+import { haptics } from '@/utils';
+import { Row } from '@/components/layout';
+import { TextInput } from 'react-native';
+import { ButtonPressAnimation } from '@/components/animations';
+import { Text } from '@/components/text';
+
+export const DiagnosticsSecretInput = ({
+  value,
+  color,
+}: {
+  value: string;
+  color: string;
+}) => {
+  const { colors } = useTheme();
+  const handleCopy = useCallback(() => {
+    Alert.alert(
+      lang.t('wallet.diagnostics.secret.reminder_title'),
+      lang.t('wallet.diagnostics.secret.these_words_are_for_your_eyes_only'),
+      [
+        {
+          onPress: () => {
+            Clipboard.setString(value);
+            haptics.notificationSuccess();
+          },
+          text: lang.t('wallet.diagnostics.secret.okay_i_understand'),
+        },
+        {
+          onPress: undefined,
+          style: 'cancel',
+          text: lang.t('button.cancel'),
+        },
+      ]
+    );
+  }, [value]);
+  return (
+    <Row justify="space-between" width="100%">
+      <TextInput
+        // @ts-expect-error probably a valid prop but not typed properly
+        disabled
+        editable={false}
+        secureTextEntry
+        selectTextOnFocus
+        style={{ width: '65%', color }}
+        value={value}
+      />
+      <ButtonPressAnimation onPress={handleCopy}>
+        <Row
+          backgroundColor={colors.appleBlue}
+          borderRadius={15}
+          style={{ paddingHorizontal: android ? 10 : 15, paddingVertical: 10 }}
+          width="100%"
+        >
+          <Text align="center" color={colors.whiteLabel} weight="bold">
+            {lang.t('wallet.diagnostics.secret.copy_secret')}
+          </Text>
+        </Row>
+      </ButtonPressAnimation>
+    </Row>
+  );
+};
---
+++ src/screens/Diagnostics/helpers/createAndShareStateDumpFile.ts
@@ -0,0 +1,39 @@
+import store from '@/redux/store';
+import { getCircularReferenceReplacer } from '@/screens/Diagnostics/helpers/getCircularReferenceReplacer';
+import RNFS from 'react-native-fs';
+import { APP_STATE_DUMP_FILE_NAME } from '@/screens/Diagnostics/constants';
+import RNShare from 'react-native-share';
+import { IS_ANDROID } from '@/env';
+import { logger, RainbowError } from '@/logger';
+
+export async function createAndShareStateDumpFile() {
+  const appState = store.getState();
+  const stringifiedState = JSON.stringify(
+    appState,
+    getCircularReferenceReplacer()
+  );
+
+  const documentsFilePath = `${RNFS.DocumentDirectoryPath}/${APP_STATE_DUMP_FILE_NAME}`;
+  try {
+    // first remove the old file in case the immediate removal failed
+    const fileExists = await RNFS.exists(documentsFilePath);
+    if (fileExists) {
+      await RNFS.unlink(documentsFilePath);
+    }
+    await RNFS.writeFile(documentsFilePath, stringifiedState, 'utf8');
+    if (IS_ANDROID) {
+      await RNFS.writeFile(
+        `${RNFS.DownloadDirectoryPath}/app_state_dump_${Date.now()}.json`,
+        stringifiedState,
+        'utf8'
+      );
+    }
+    await RNShare.open({
+      url: `file://${documentsFilePath}`,
+    });
+    // clean up the file since we don't need it anymore
+    await RNFS.unlink(documentsFilePath);
+  } catch (error) {
+    logger.error(new RainbowError('Saving app state dump data failed'));
+  }
+}
