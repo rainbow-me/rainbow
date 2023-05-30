96bcadbf3bfcc9f49790239626d22a7b2c0f0fad
Eric Bailey <git@esb.lol>
2023-05-30 11:07:00 -0500

    feat: clean up SVG file, allow for fill, update color values

--- src/components/icons/svg/Ratio.tsx
+++ src/components/icons/svg/Ratio.tsx
@@ -1,46 +1,16 @@
 import React from 'react';
-import {
-  G,
-  Path,
-  Mask,
-  Defs,
-  ClipPath,
-  SvgProps,
-  EMaskUnits,
-} from 'react-native-svg';
+import { Path, SvgProps } from 'react-native-svg';
 import Svg from '../Svg';
 
-export function Ratio(props: SvgProps) {
+export function Ratio({ color, ...props }: SvgProps) {
   return (
-    <Svg viewBox="0 0 16 16" {...props}>
-      <G clipPath="url(#clip0_1_2)">
-        <Path d="M16 0H0v16h16V0z" fill="#7EFDCF" />
-        <Mask
-          id="a"
-          maskUnits={EMaskUnits.USER_SPACE_ON_USE}
-          x={2}
-          y={3}
-          width={12}
-          height={10}
-        >
-          <Path d="M13.13 3.154H2.87v9.66h10.26v-9.66z" fill="#fff" />
-        </Mask>
-        <G mask="url(#a)">
-          <Path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M8.917 4.331L4.03 8.046a.479.479 0 00.055.79l.85.462 4.33 2.352c.372.203.819.173 1.164-.077l.965-.698.277-.2c.448-.325.608-.941.383-1.46l-.005-.012-2.042-4.573c-.195-.438-.717-.58-1.089-.299zm-.569-.837c.866-.658 2.085-.325 2.54.695l2.035 4.56.007.013c.438.98.137 2.151-.709 2.763l-.277.201-.965.698c-.641.464-1.47.52-2.161.144l-5.18-2.814c-.94-.51-1.038-1.893-.179-2.546l4.89-3.714z"
-            fill="#1E2435"
-          />
-        </G>
-      </G>
-      <Defs>
-        <ClipPath id="clip0_1_2">
-          <Path fill="#fff" d="M0 0H16V16H0z" />
-        </ClipPath>
-      </Defs>
+    <Svg viewBox="0 0 165 155" fill="none" {...props}>
+      <Path
+        fill={color}
+        fillRule="evenodd"
+        d="m96.754 18.837-78.216 59.43c-4.27 3.244-3.788 10.115.888 12.657h.001l82.873 45.018c5.951 3.235 13.1 2.76 18.627-1.236l.001-.001 15.434-11.168.002-.001 4.442-3.213.002-.001c7.154-5.179 9.726-15.044 6.12-23.352l-.087-.188-32.664-73.173c-3.124-6.997-11.479-9.286-17.423-4.772ZM87.65 5.434C101.51-5.095 121.004.242 128.29 16.564l32.565 72.95c.035.073.07.147.103.222 7.002 15.681 2.194 34.414-11.338 44.207l-.002.001-4.442 3.213-.002.001-15.433 11.168-.002.001c-10.251 7.414-23.521 8.301-34.572 2.294h-.002l-82.87-45.018C-2.756 97.426-4.317 75.309 9.43 64.864l78.218-59.43Z"
+        clipRule="evenodd"
+      />
     </Svg>
   );
 }
-
-export default Ratio;
