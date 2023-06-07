bfa0d36d8b19bf83486e7a3ccaa6f6e24c768dcf
skylarbarrera <skylar.barrera@gmail.com>
2023-06-07 13:47:23 -0400

    BE + missed config

--- src/redux/explorer.ts
+++ src/redux/explorer.ts
@@ -75,6 +75,7 @@ const messages = {
     RECEIVED_OPTIMISM: 'received address optimism-assets',
     RECEIVED_POLYGON: 'received address polygon-assets',
     RECEIVED_BSC: 'received address bsc-assets',
+    RECEIVED_ZORA: 'received address zora-assets',
   },
   ADDRESS_PORTFOLIO: {
     RECEIVED: 'received address portfolio',
@@ -86,6 +87,7 @@ const messages = {
     RECEIVED_OPTIMISM: 'received address optimism-transactions',
     RECEIVED_POLYGON: 'received address polygon-transactions',
     RECEIVED_BSC: 'received address bsc-transactions',
+    RECEIVED_ZORA: 'received address zora-transactions',
   },
   ASSET_CHARTS: {
     RECEIVED: 'received assets charts',
@@ -384,6 +386,7 @@ const l2AddressTransactionHistoryRequest = (
       `${Network.optimism}-transactions`,
       `${Network.polygon}-transactions`,
       `${Network.bsc}-transactions`,
+      `${Network.zora}-transactions`,
     ],
   },
 ];
@@ -939,6 +942,13 @@ const listenOnAddressMessages = (socket: Socket) => (
       dispatch(transactionsReceived(message));
     }
   );
+  socket.on(
+    messages.ADDRESS_TRANSACTIONS.RECEIVED_ZORA,
+    (message: TransactionsReceivedMessage) => {
+      // logger.log('zora txns received', message?.payload?.transactions);
+      dispatch(transactionsReceived(message));
+    }
+  );
 
   socket.on(
     messages.ADDRESS_TRANSACTIONS.APPENDED,
@@ -968,10 +978,18 @@ const listenOnAddressMessages = (socket: Socket) => (
       dispatch(l2AddressAssetsReceived(message, Network.polygon));
     }
   );
+
   socket.on(
     messages.ADDRESS_ASSETS.RECEIVED_BSC,
     (message: L2AddressAssetsReceivedMessage) => {
       dispatch(l2AddressAssetsReceived(message, Network.bsc));
+    }
+  );
+
+  socket.on(
+    messages.ADDRESS_ASSETS.RECEIVED_ZORA,
+    (message: L2AddressAssetsReceivedMessage) => {
+      dispatch(l2AddressAssetsReceived(message, Network.zora));
     }
   );
 
