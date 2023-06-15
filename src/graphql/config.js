9b0e0104dcfe55dbdd244168dd6fd56398a3af7d
Ben Goldberg <bengoldberg@rainbow.me>
2023-06-15 15:14:59 -0700

    endpoint

--- src/graphql/config.js
+++ src/graphql/config.js
@@ -21,8 +21,7 @@ exports.config = {
     document: './queries/arc.graphql',
     schema: {
       method: 'GET',
-      // url: 'https://arc-graphql.rainbow.me/graphql',
-      url: 'http://127.0.0.1:8787/graphql',
+      url: 'https://arc-graphql.rainbow.me/graphql',
     },
   },
 };
