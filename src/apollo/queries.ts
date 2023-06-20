14ccc1fbe99f89d806d95e9e6532f6d2621b935f
skylarbarrera <skylar.barrera@gmail.com>
2023-06-20 16:18:21 -0400

    Revert "🏛 Migrate Uniswap GraphQL from Apollo to new GraphQL client (#4143)"
    This reverts commit 1d896b384be5c2e059745f589a6c8838d677237e.

--- src/apollo/queries.ts
+++ src/apollo/queries.ts
@@ -48,6 +48,16 @@ export const COMPOUND_ACCOUNT_AND_MARKET_QUERY = gql`
   }
 `;
 
+export const UNISWAP_ADDITIONAL_POOL_DATA = gql`
+  query pairs($address: String!) {
+    pairs(where: { id: $address }) {
+      volumeUSD
+      reserveUSD
+      trackedReserveETH
+    }
+  }
+`;
+
 export const GET_BLOCKS_QUERY = (timestamps: any) => {
   let queryString = 'query blocks {';
   queryString += timestamps.map((timestamp: any) => {
@@ -62,6 +72,99 @@ export const GET_BLOCKS_QUERY = (timestamps: any) => {
     ${queryString}
   `;
 };
+
+export const USER_POSITIONS = gql`
+  query liquidityPositions($user: Bytes!) {
+    liquidityPositions(where: { user: $user, liquidityTokenBalance_gt: 0 }) {
+      pair {
+        id
+        reserve0
+        reserve1
+        reserveUSD
+        token0 {
+          decimals
+          id
+          name
+          symbol
+          derivedETH
+        }
+        token1 {
+          decimals
+          id
+          name
+          symbol
+          derivedETH
+        }
+        totalSupply
+      }
+      liquidityTokenBalance
+    }
+  }
+`;
+
+export const USER_MINTS_BURNS_PER_PAIR = gql`
+  query events($user: Bytes!, $pair: Bytes!) {
+    mints(where: { to: $user, pair: $pair }) {
+      amountUSD
+      amount0
+      amount1
+      timestamp
+      pair {
+        token0 {
+          id
+        }
+        token1 {
+          id
+        }
+      }
+    }
+    burns(where: { sender: $user, pair: $pair }) {
+      amountUSD
+      amount0
+      amount1
+      timestamp
+      pair {
+        token0 {
+          id
+        }
+        token1 {
+          id
+        }
+      }
+    }
+  }
+`;
+
+export const USER_HISTORY = gql`
+  query snapshots($user: Bytes!, $skip: Int!) {
+    liquidityPositionSnapshots(
+      first: 1000
+      skip: $skip
+      where: { user: $user }
+    ) {
+      timestamp
+      reserveUSD
+      liquidityTokenBalance
+      liquidityTokenTotalSupply
+      reserve0
+      reserve1
+      token0PriceUSD
+      token1PriceUSD
+      pair {
+        id
+        reserve0
+        reserve1
+        reserveUSD
+        token0 {
+          id
+        }
+        token1 {
+          id
+        }
+      }
+    }
+  }
+`;
 
 export const CONTRACT_FUNCTION = gql`
   query contractFunction($chainID: Int!, $hex: String!) {
--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,12 +2,8 @@ import { config } from './config';
 import { getFetchRequester } from './utils/getFetchRequester';
 import { getSdk as getEnsSdk } from './__generated__/ens';
 import { getSdk as getMetadataSdk } from './__generated__/metadata';
-import { getSdk as getUniswapSdk } from './__generated__/uniswap';
 
 export const ensClient = getEnsSdk(getFetchRequester(config.ens.schema));
 export const metadataClient = getMetadataSdk(
   getFetchRequester(config.metadata.schema)
-);
-export const uniswapClient = getUniswapSdk(
-  getFetchRequester(config.uniswap.schema)
 );
