10d19161a65016a9fdf4093ce61c47f0a637852a
Eric Bailey <git@esb.lol>
2023-06-21 13:13:33 -0500

    feat: add api key header to arc clients

--- src/graphql/config.js
+++ src/graphql/config.js
@@ -1,5 +1,6 @@
 exports.config = {
   ens: {
+    __name: 'ens',
     document: './queries/ens.graphql',
     schema: {
       method: 'POST',
@@ -7,21 +8,28 @@ exports.config = {
     },
   },
   metadata: {
+    __name: 'metadata',
     document: './queries/metadata.graphql',
     schema: { method: 'GET', url: 'https://metadata.p.rainbow.me/v1/graph' },
   },
   arc: {
+    __name: 'arc',
     document: './queries/arc.graphql',
     schema: {
       method: 'GET',
       url: 'https://arc-graphql.rainbow.me/graphql',
+      headers: {
+        'x-api-key': 'ARC_GRAPHQL_API_KEY',
+      },
     },
   },
   arcDev: {
+    __name: 'arcDev',
     document: './queries/arc.graphql',
     schema: {
       method: 'GET',
       url: 'https://arc-graphql.rainbowdotme.workers.dev/graphql',
+      headers: {},
     },
   },
 };
