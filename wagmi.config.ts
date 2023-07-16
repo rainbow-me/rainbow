708643a0094b7b2f127f2534e375412875fd7b78
skylarbarrera <skylar.barrera@gmail.com>
2023-07-16 13:05:03 -0400

    add zoraDrop contract

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,8 +1,5 @@
-import { defineConfig } from '@wagmi/cli'
-import { etherscan, react } from "@wagmi/cli/plugins";
-
-
-
+import { defineConfig } from '@wagmi/cli';
+import { etherscan, react } from '@wagmi/cli/plugins';
 
 export default defineConfig({
   out: 'src/generated.ts',
@@ -11,13 +8,16 @@ export default defineConfig({
     etherscan({
       apiKey: 'A8EIEGG7EVRK1WITUQBRHRA71FFGXUPPGB',
       chainId: 1,
-      contracts: [{
-
+      contracts: [
+        {
           name: 'zoraNFTCreator',
           address: { [1]: '0xa6a2956fa075d50b021385478A99f3642dAfCc2C' },
-          
-      }],
+        },
+        {
+          name: 'zoraDrop',
+          address: { [1]: '0x419074d73Cf0852e46b8531b430B1230C348C291' },
+        },
+      ],
     }),
-    react(),
   ],
-})
+});
