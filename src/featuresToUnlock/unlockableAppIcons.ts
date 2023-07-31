9da534236eb0e73400715286c904363105785d34
Ben Goldberg <bengoldberg@rainbow.me>
2023-07-31 15:28:23 -0600

    icons

--- src/featuresToUnlock/unlockableAppIcons.ts
+++ src/featuresToUnlock/unlockableAppIcons.ts
@@ -41,6 +41,10 @@ const FINILIAR_NFT_ADDRESS: EthereumAddress =
 const ZORB_NFT_ADDRESS: EthereumAddress =
   '0x12e4527e2807978a49469f8d757abf5e07b32b8f';
 
+// poolboy app icon unlocking NFTs
+const POOLBOY_NFT_ADDRESS: EthereumAddress =
+  '0xf25298fa62a2eb94fc06626966f6f21399b4c508';
+
 export interface UnlockableAppIcon {
   key: string; // string used for analytics
   network: Network; // network that the unlockingNfts exist on
@@ -116,4 +120,12 @@ export const ZorbIcon: UnlockableAppIcon = {
   network: Network.zora,
   unlockingNfts: [ZORB_NFT_ADDRESS],
   unlockKey: 'zorb_nft_app_icon',
+};
+
+export const PoolboyIcon: UnlockableAppIcon = {
+  key: 'poolboy',
+  explainSheetType: 'poolboy_app_icon',
+  network: Network.mainnet,
+  unlockingNfts: [POOLBOY_NFT_ADDRESS],
+  unlockKey: 'poolboy_nft_app_icon',
 };
