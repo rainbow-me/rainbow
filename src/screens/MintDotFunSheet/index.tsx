72e5b1596deb61633e6d7da8e637f5c419c22109
Ben Goldberg <bengoldberg@rainbow.me>
2023-08-25 12:33:49 -0600

    stuff

--- src/screens/MintDotFunSheet/index.tsx
+++ src/screens/MintDotFunSheet/index.tsx
@@ -7,9 +7,34 @@ import {
   Stack,
   Text,
 } from '@/design-system';
-import { MintDotFunCard } from '@/components/cards/MintDotFunCard';
 import { CarouselCard } from '@/components/cards/CarouselCard';
 import { Cell, NFT_IMAGE_SIZE, Placeholder } from './Cell';
+
+const Card = ({
+  collectionName,
+  mints,
+}: {
+  collectionName: string;
+  mints: any[];
+}) => (
+  <CarouselCard
+    title={collectionName}
+    data={mints}
+    carouselItem={{
+      renderItem: () => <Cell />,
+      keyExtractor: (item: any) => item.id,
+      placeholder: <Placeholder />,
+      width: NFT_IMAGE_SIZE,
+      height: 167,
+      padding: 10,
+    }}
+    button={{
+      text: 'Mint',
+      style: 'fill',
+      onPress: () => {},
+    }}
+  />
+);
 
 export function MintDotFunSheet() {
   return (
@@ -26,56 +51,17 @@ export function MintDotFunSheet() {
                 space="24px"
                 separator={<Separator color="separatorTertiary" />}
               >
-                <CarouselCard
-                  title="Collection Name"
-                  data={[]}
-                  carouselItem={{
-                    renderItem: () => <Cell />,
-                    keyExtractor: (item: any) => item.id,
-                    placeholder: <Placeholder />,
-                    width: NFT_IMAGE_SIZE,
-                    height: 167,
-                    padding: 10,
-                  }}
-                  button={{
-                    text: 'View All Mints',
-                    style: 'fill',
-                    onPress: () => {},
-                  }}
+                <Card
+                  collectionName="Collection Name"
+                  mints={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
                 />
-                <CarouselCard
-                  title="Collection Name"
-                  data={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
-                  carouselItem={{
-                    renderItem: () => <Placeholder />,
-                    keyExtractor: (item: any) => item.id,
-                    placeholder: <></>,
-                    width: NFT_IMAGE_SIZE,
-                    height: 167,
-                    padding: 10,
-                  }}
-                  button={{
-                    text: 'View All Mints',
-                    style: 'fill',
-                    onPress: () => {},
-                  }}
+                <Card
+                  collectionName="Collection Name"
+                  mints={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
                 />
-                <CarouselCard
-                  title="Collection Name"
-                  data={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
-                  carouselItem={{
-                    renderItem: () => <Placeholder />,
-                    keyExtractor: (item: any) => item.id,
-                    placeholder: <></>,
-                    width: NFT_IMAGE_SIZE,
-                    height: 167,
-                    padding: 10,
-                  }}
-                  button={{
-                    text: 'View All Mints',
-                    style: 'fill',
-                    onPress: () => {},
-                  }}
+                <Card
+                  collectionName="Collection Name"
+                  mints={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
                 />
               </Stack>
             </Stack>
