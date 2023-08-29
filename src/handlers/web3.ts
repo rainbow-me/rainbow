7ac72967f832009b8bb7a9061c89199616ee14b6
Wayne Cheng <677680+welps@users.noreply.github.com>
2023-08-29 12:37:28 -0400

    chore: Update flashbots protect RPC to include all builders

--- src/handlers/web3.ts
+++ src/handlers/web3.ts
@@ -174,7 +174,7 @@ export const isTestnetNetwork = (network: Network): boolean => {
 // shoudl figure out better way to include this in networks
 export const getFlashbotsProvider = async () => {
   return new StaticJsonRpcProvider(
-    'https://rpc.flashbots.net',
+    'https://rpc.flashbots.net/?hint=hash&builder=flashbots&builder=f1b.io&builder=rsync&builder=beaverbuild.org&builder=builder0x69&builder=titan&builder=eigenphi&builder=boba-builder',
     Network.mainnet
   );
 };
