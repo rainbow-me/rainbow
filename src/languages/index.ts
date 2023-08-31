ad8c5cd5e0cd44824af3279a11b9fa93150e29f5
Daniel Sinclair <d@niel.nyc>
2023-08-31 18:08:12 -0400

    chore: add es_419, pt_BR, zh_CN, hi_IN, tr_TR, ru_RU

--- src/languages/index.ts
+++ src/languages/index.ts
@@ -1,8 +1,14 @@
 import { I18n } from 'i18n-js';
 
 import en_US from './en_US.json';
+import es_419 from './es_419.json';
 import fr_FR from './fr_FR.json';
+import hi_IN from './hi_IN.json';
 import ja_JP from './ja_JP.json';
+import pt_BR from './pt_BR.json';
+import ru_RU from './ru_RU.json';
+import tr_TR from './tr_TR.json';
+import zh_CN from './zh_CN.json';
 
 import { simpleObjectProxy } from '@/languages/utils';
 
@@ -14,14 +20,26 @@ export type Translation = typeof en_US;
 
 export enum Language {
   EN_US = 'en_US',
+  ES_419 = 'es_419',
   FR_FR = 'fr_FR',
   JA_JP = 'ja_JP',
+  PT_BR = 'pt_BR',
+  ZH_CN = 'zh_CN',
+  HI_IN = 'hi_IN',
+  TR_TR = 'tr_TR',
+  RU_RU = 'ru_RU',
 }
 
 export const lang = new I18n({
   en_US,
+  es_419,
   fr_FR,
   ja_JP,
+  pt_BR,
+  zh_CN,
+  hi_IN,
+  tr_TR,
+  ru_RU,
 });
 
 // Configure languages
@@ -33,11 +51,29 @@ export const supportedLanguages = {
   [Language.EN_US]: {
     label: 'English',
   },
+  [Language.ES_419]: {
+    label: 'Español',
+  },
   [Language.FR_FR]: {
     label: 'Français',
   },
   [Language.JA_JP]: {
     label: '日本語',
+  },
+  [Language.PT_BR]: {
+    label: 'Português',
+  },
+  [Language.ZH_CN]: {
+    label: '中文',
+  },
+  [Language.HI_IN]: {
+    label: 'हिंदी',
+  },
+  [Language.TR_TR]: {
+    label: 'Türkçe',
+  },
+  [Language.RU_RU]: {
+    label: 'Русский',
   },
 };
 
