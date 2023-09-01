4218120863882fca40cb3c6f9aef3112b55cf934
skylarbarrera <skylar.barrera@gmail.com>
2023-09-01 14:31:24 -0400

    swaps: fee on transfer

--- src/screens/ExplainSheet.js
+++ src/screens/ExplainSheet.js
@@ -934,6 +934,42 @@ export const explainers = (params, colors) => ({
       </Text>
     ),
   },
+  feeOnTransfer: {
+    extraHeight: -70,
+    logo: (
+      <RowWithMargins justify="center" margin={35} marginBottom={10}>
+        <CoinIcon size={40} {...params?.inputCurrency} />
+      </RowWithMargins>
+    ),
+    title: lang.t('explain.fee_on_transfer.title'),
+    stillCurious: (
+      <Text {...getBodyTextPropsWithColor(colors)}>
+        {lang.t('explain.fee_on_transfer.fragment1', {
+          tokenName: params?.inputCurrency?.symbol,
+        })}
+        <Text
+          color={colors?.appleBlue}
+          onPress={() =>
+            Linking.openURL(
+              buildRainbowLearnUrl({
+                url:
+                  'https://support.rainbow.me/en/articles/8324868-fee-on-transfer-tokens',
+                query: {
+                  campaign: 'explain',
+                },
+              })
+            )
+          }
+          size="large"
+          suppressHighlighting
+          weight="semibold"
+        >
+          {lang.t('explain.fee_on_transfer.fragment2')}
+        </Text>
+        {lang.t('explain.fee_on_transfer.fragment3')}
+      </Text>
+    ),
+  },
   noRouteFound: {
     extraHeight: -90,
     emoji: '🚧',
