67062cfb28be63eb5ce76f4f9f216bbb4818de61
Derek Nelson <6868432+dereknelson@users.noreply.github.com>
2023-09-12 11:35:48 -0400

    fixes animations for bottom tab bar (#5046)
    * Fix @tanstack/query-core subdependency to v4.2.1
    
    * fixed tab bar animation
    
    * removed QRCodeScanner from swiping bc of janky tabbar stuff
    
    ---------
    
    Co-authored-by: jinchung <jin.chung17@gmail.com>

--- src/navigation/SwipeNavigator.js
+++ src/navigation/SwipeNavigator.js
@@ -1,9 +1,11 @@
 import { BlurView } from '@react-native-community/blur';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import React from 'react';
+import React, { useState } from 'react';
 import Animated, {
-  interpolate,
   useAnimatedStyle,
+  useSharedValue,
+  interpolate,
+  Extrapolate,
 } from 'react-native-reanimated';
 import { TabBarIcon } from '@/components/icons/TabBarIcon';
 import { FlexItem } from '@/components/layout';
@@ -50,221 +52,217 @@ const config = {
 
 const Swipe = createMaterialTopTabNavigator();
 
-export function SwipeNavigator() {
-  const { isCoinListEdited } = useCoinListEdited();
-  const { network } = useAccountSettings();
+const TabBar = ({ state, descriptors, navigation, position }) => {
+  const { width: deviceWidth } = useDimensions();
+  const reanimatedPosition = useSharedValue(2);
+  const tabWidth = deviceWidth / NUMBER_OF_TABS;
+  const tabPillStartPosition = (tabWidth - 72) / 2;
 
   const { accentColor } = useAccountAccentColor();
-
+  useEffect(() => {
+    reanimatedPosition.value = state.index;
+  }, [state.index]);
   // ////////////////////////////////////////////////////
   // Colors
 
   const { colors, isDarkMode } = useTheme();
-
-  // ////////////////////////////////////////////////////
-  // Animations
-
-  const TabBar = ({ state, descriptors, navigation, position }) => {
-    const { width: deviceWidth } = useDimensions();
-
-
-    const tabWidth = deviceWidth / NUMBER_OF_TABS;
-    const tabPillStartPosition = (tabWidth - 72) / 2;
-
-    const tabStyle = useAnimatedStyle(() => {
-      return {
-        transform: [
-          {
-            translateX:
-              tabPillStartPosition +
-              (position
-                ? (Math.max(position.value, 1) - 1) * tabWidth
-                : 0),
-          },
-        ],
-        width: 72,
-      };
-    });
-
-    const offScreenTabBar = useAnimatedStyle(() => {
-      const translateX = interpolate(
-        position.value,
-        [0, 1, 10],
-        [deviceWidth, 0, 0]
-      );
-
-      return {
-        transform: [
-          {
-            translateX,
-          },
-        ],
-      };
-    });
-
-    return (
+  const tabStyle = useAnimatedStyle(() => {
+    const pos1 = tabPillStartPosition;
+    const pos2 = tabPillStartPosition + tabWidth;
+    const pos3 = tabPillStartPosition + tabWidth * 2;
+    const translateX = interpolate(
+      reanimatedPosition.value,
+      [0, 1, 2],
+      [pos1, pos2, pos3],
+      Extrapolate.CLAMP
+    );
+    return {
+      transform: [{ translateX }],
+      width: 72,
+    };
+  });
+  // for when QRScannerScreen is re-added
+  // const offScreenTabBar = useAnimatedStyle(() => {
+  //   const translateX = interpolate(
+  //     reanimatedPosition.value,
+  //     [0, 1, 2],
+  //     [deviceWidth, 0, 0]
+  //   );
+  //   return {
+  //     transform: [
+  //       {
+  //         translateX,
+  //       },
+  //     ],
+  //   };
+  // });
+  return (
+    <Box
+      as={Animated.View}
+      style={[
+        // offScreenTabBar,
+        {
+          shadowColor: colors.shadowBlack,
+          shadowOffset: { width: 0, height: -4 },
+          shadowOpacity: isDarkMode ? 0.2 : 0.04,
+          shadowRadius: 20,
+        },
+      ]}
+    >
       <Box
-        as={Animated.View}
-        style={[
-          offScreenTabBar,
-          {
-            shadowColor: colors.shadowBlack,
-            shadowOffset: { width: 0, height: -4 },
-            shadowOpacity: isDarkMode ? 0.2 : 0.04,
-            shadowRadius: 20,
-          },
-        ]}
+        style={{
+          shadowColor: colors.shadowBlack,
+          shadowOffset: { width: 0, height: -1 },
+          shadowOpacity: isDarkMode ? 0.2 : 0.04,
+          shadowRadius: 3,
+        }}
       >
         <Box
+          height={{ custom: 82 }}
+          position="absolute"
           style={{
-            shadowColor: colors.shadowBlack,
-            shadowOffset: { width: 0, height: -1 },
-            shadowOpacity: isDarkMode ? 0.2 : 0.04,
-            shadowRadius: 3,
+            bottom: 0,
+            overflow: 'hidden',
           }}
+          width="full"
         >
           <Box
-            height={{ custom: 82 }}
-            position="absolute"
-            style={{
-              bottom: 0,
-              overflow: 'hidden',
-            }}
+            as={BlurView}
+            blurAmount={40}
+            blurType={isDarkMode ? 'chromeMaterialDark' : 'chromeMaterialLight'}
             width="full"
+            height={{ custom: 82 }}
           >
             <Box
-              as={BlurView}
-              blurAmount={40}
-              blurType={
-                isDarkMode ? 'chromeMaterialDark' : 'chromeMaterialLight'
-              }
+              height="full"
+              position="absolute"
+              style={{
+                backgroundColor: isDarkMode
+                  ? colors.alpha('#191A1C', 0.7)
+                  : colors.alpha(colors.white, 0.7),
+              }}
               width="full"
-              height={{ custom: 82 }}
-            >
-              <Box
-                height="full"
-                position="absolute"
-                style={{
-                  backgroundColor: isDarkMode
-                    ? colors.alpha('#191A1C', 0.7)
-                    : colors.alpha(colors.white, 0.7),
-                }}
-                width="full"
-              />
-              <Box
-                alignItems="center"
-                as={Animated.View}
-                style={[
-                  tabStyle,
-                  {
-                    backgroundColor: colors.alpha(
-                      accentColor,
-                      isDarkMode ? 0.25 : 0.1
-                    ),
-                    top: 6,
-                  },
-                ]}
-                justifyContent="center"
-                height="36px"
-                borderRadius={18}
-                position="absolute"
-                width="72px"
-              />
-              <Box
-                height="1px"
-                position="absolute"
-                style={{
-                  backgroundColor: isDarkMode
-                    ? colors.alpha('#ffffff', 0.4)
-                    : colors.alpha(colors.white, 0.5),
-                  top: 0,
-                }}
-                width="full"
-              />
-              <Columns alignVertical="center">
-                {state.routes.map((route, index) => {
-                  const { options } = descriptors[route.key];
-                  logger.log('routeKey = ' + route.key);
+            />
+            <Box
+              alignItems="center"
+              as={Animated.View}
+              style={[
+                tabStyle,
+                {
+                  backgroundColor: colors.alpha(
+                    accentColor,
+                    isDarkMode ? 0.25 : 0.1
+                  ),
+                  top: 6,
+                },
+              ]}
+              justifyContent="center"
+              height="36px"
+              borderRadius={18}
+              position="absolute"
+              width="72px"
+            />
+            <Box
+              height="1px"
+              position="absolute"
+              style={{
+                backgroundColor: isDarkMode
+                  ? colors.alpha('#ffffff', 0.4)
+                  : colors.alpha(colors.white, 0.5),
+                top: 0,
+              }}
+              width="full"
+            />
+            <Columns alignVertical="center">
+              {state.routes.map((route, index) => {
+                const { options } = descriptors[route.key];
+                // logger.log('routeKey = ' + route.key);
 
-                  const isFocused = state.index === index;
+                const isFocused = state.index === index;
 
-                  const onPress = () => {
-                    const event = navigation.emit({
-                      type: 'tabPress',
-                      target: route.key,
+                const onPress = () => {
+                  const event = navigation.emit({
+                    type: 'tabPress',
+                    target: route.key,
+                  });
+
+                  if (!isFocused && !event.defaultPrevented) {
+                    navigation.navigate(route.name);
+                    reanimatedPosition.value = index;
+                  } else if (
+                    isFocused &&
+                    options.tabBarIcon === 'tabDiscover'
+                  ) {
+                    discoverScrollToTopFnRef?.();
+                  }
+                };
+
+                const onLongPress = async () => {
+                  navigation.emit({
+                    type: 'tabLongPress',
+                    target: route.key,
+                  });
+                  reanimatedPosition.value = index;
+
+                  if (options.tabBarIcon === 'tabHome') {
+                    navigation.navigate(Routes.CHANGE_WALLET_SHEET);
+                  }
+                  if (options.tabBarIcon === 'tabDiscover') {
+                    navigation.navigate(Routes.DISCOVER_SCREEN);
+                    InteractionManager.runAfterInteractions(() => {
+                      discoverOpenSearchFnRef?.();
                     });
+                  }
+                };
 
-                    if (!isFocused && !event.defaultPrevented) {
-                      navigation.navigate(route.name);
-                    } else if (
-                      isFocused &&
-                      options.tabBarIcon === 'tabDiscover'
-                    ) {
-                      discoverScrollToTopFnRef?.();
-                    }
-                  };
-
-                  const onLongPress = async () => {
-                    navigation.emit({
-                      type: 'tabLongPress',
-                      target: route.key,
-                    });
-
-                    if (options.tabBarIcon === 'tabHome') {
-                      navigation.navigate(Routes.CHANGE_WALLET_SHEET);
-                    }
-                    if (options.tabBarIcon === 'tabDiscover') {
-                      navigation.navigate(Routes.DISCOVER_SCREEN);
-                      InteractionManager.runAfterInteractions(() => {
-                        discoverOpenSearchFnRef?.();
-                      });
-                    }
-                  };
-
-                  return (
-                    options.tabBarIcon !== 'none' && (
-                      <Box
-                        key={route.key}
-                        height="full"
-                        width="full"
-                        justifyContent="flex-start"
-                        paddingTop="6px"
+                return (
+                  options.tabBarIcon !== 'none' && (
+                    <Box
+                      key={route.key}
+                      height="full"
+                      width="full"
+                      justifyContent="flex-start"
+                      paddingTop="6px"
+                    >
+                      <ButtonPressAnimation
+                        onPress={onPress}
+                        onLongPress={onLongPress}
+                        scaleTo={0.75}
                       >
-                        <ButtonPressAnimation
-                          onPress={onPress}
-                          onLongPress={onLongPress}
-                          scaleTo={0.75}
-                        >
-                          <Stack
-                            alignVertical="center"
-                            alignHorizontal="center"
+                        <Stack alignVertical="center" alignHorizontal="center">
+                          <Box
+                            alignItems="center"
+                            justifyContent="center"
+                            height="36px"
+                            borderRadius={20}
                           >
-                            <Box
-                              alignItems="center"
-                              justifyContent="center"
-                              height="36px"
-                              borderRadius={20}
-                            >
-                              <TabBarIcon
-                                accentColor={accentColor}
-                                icon={options.tabBarIcon}
-                                index={index}
-                                rawScrollPosition={position}
-                              />
-                            </Box>
-                          </Stack>
-                        </ButtonPressAnimation>
-                      </Box>
-                    )
-                  );
-                })}
-              </Columns>
-            </Box>
+                            <TabBarIcon
+                              accentColor={accentColor}
+                              icon={options.tabBarIcon}
+                              index={index}
+                              rawScrollPosition={position}
+                              reanimatedPosition={reanimatedPosition}
+                            />
+                          </Box>
+                        </Stack>
+                      </ButtonPressAnimation>
+                    </Box>
+                  )
+                );
+              })}
+            </Columns>
           </Box>
         </Box>
       </Box>
-    );
-  };
+    </Box>
+  );
+};
+export function SwipeNavigator() {
+  const { isCoinListEdited } = useCoinListEdited();
+  const { network } = useAccountSettings();
+
+  // ////////////////////////////////////////////////////
+  // Animations
 
   return (
     <FlexItem>
@@ -272,17 +270,21 @@ export function SwipeNavigator() {
         <Swipe.Navigator
           initialLayout={deviceUtils.dimensions}
           initialRouteName={Routes.WALLET_SCREEN}
-
           swipeEnabled={!isCoinListEdited}
           tabBar={props => <TabBar {...props} />}
           tabBarPosition="bottom"
         >
-          <Swipe.Screen
+          {/* <Swipe.Screen
             component={QRScannerScreen}
             name={Routes.QR_SCANNER_SCREEN}
             options={{
               tabBarIcon: 'none',
             }}
+          /> */}
+          <Swipe.Screen
+            component={ProfileScreen}
+            name={Routes.PROFILE_SCREEN}
+            options={{ tabBarIcon: 'tabActivity' }}
           />
           <Swipe.Screen
             component={WalletScreen}
@@ -294,11 +296,6 @@ export function SwipeNavigator() {
                 close: config,
               },
             }}
-          />
-          <Swipe.Screen
-            component={ProfileScreen}
-            name={Routes.PROFILE_SCREEN}
-            options={{ tabBarIcon: 'tabActivity' }}
           />
           <Swipe.Screen
             component={DiscoverScreen}
