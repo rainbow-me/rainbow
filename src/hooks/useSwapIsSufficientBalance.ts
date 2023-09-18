8c5d9db045d22eef903bfeb2d2e578aa0ca55de4
jinchung <jin.chung17@gmail.com>
2023-09-18 12:26:57 -0400

    Removing logic related to ExchangeModalType deposit or withdraw

--- src/hooks/useSwapIsSufficientBalance.ts
+++ src/hooks/useSwapIsSufficientBalance.ts
@@ -1,6 +1,5 @@
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { ExchangeModalTypes } from '@/helpers';
 import { AppState } from '@/redux/store';
 import { greaterThanOrEqualTo } from '@/helpers/utilities';
 import { ethereumUtils } from '@/utils';
@@ -10,22 +9,14 @@ export default function useSwapIsSufficientBalance(inputAmount: string | null) {
     (state: AppState) => state.swap.inputCurrency?.uniqueId
   );
   const type = useSelector((state: AppState) => state.swap.type);
-  const supplyBalanceUnderlying = useSelector(
-    (state: AppState) =>
-      state.swap.typeSpecificParameters?.supplyBalanceUnderlying
-  );
 
   const isSufficientBalance = useMemo(() => {
     if (!inputAmount) return true;
     const maxInputBalance =
       ethereumUtils.getAccountAsset(inputCurrencyUniqueId)?.balance?.amount ??
       0;
-    const isWithdrawal = type === ExchangeModalTypes.withdrawal;
-
-    return isWithdrawal
-      ? greaterThanOrEqualTo(supplyBalanceUnderlying, inputAmount)
-      : greaterThanOrEqualTo(maxInputBalance, inputAmount);
-  }, [inputAmount, inputCurrencyUniqueId, supplyBalanceUnderlying, type]);
+    return greaterThanOrEqualTo(maxInputBalance, inputAmount);
+  }, [inputAmount, inputCurrencyUniqueId, type]);
 
   return isSufficientBalance;
 }
