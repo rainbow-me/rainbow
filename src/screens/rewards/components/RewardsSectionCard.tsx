959626a8d20a30a075c7f81505667539ebb91dfd
Matthew Wall <matthew.wallt@gmail.com>
2023-10-11 10:34:14 -0600

    fix: android shadows not appearing

--- src/screens/rewards/components/RewardsSectionCard.tsx
+++ src/screens/rewards/components/RewardsSectionCard.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react';
 import { View } from 'react-native';
-import { Box, Space, globalColors } from '@/design-system';
+import { Box, Space, globalColors, useBackgroundColor } from '@/design-system';
 import { IS_ANDROID, IS_IOS } from '@/env';
 import { useTheme } from '@/theme';
 
@@ -15,6 +15,8 @@ export function RewardsSectionCard({
   paddingHorizontal = '20px',
 }: PropsWithChildren<Props>) {
   const { isDarkMode } = useTheme();
+
+  const bg = useBackgroundColor('surfaceSecondaryElevated');
 
   return (
     <View
@@ -42,6 +44,8 @@ export function RewardsSectionCard({
                 shadowColor: globalColors.grey100,
                 elevation: 8,
                 shadowOpacity: isDarkMode ? 1 : 0.55,
+                backgroundColor: bg,
+                borderRadius: 18,
               }
         }
       >
