f8711ed59a877039784399f948ec1723e4384cf0
skylarbarrera <skylar.barrera@gmail.com>
2023-10-18 11:33:04 -0400

    last fixes

--- e2e/watchAddressFlow.spec.js
+++ e2e/watchAddressFlow.spec.js
@@ -32,8 +32,8 @@ describe('Watch address flow', () => {
 
   it('Should navigate to the Wallet screen after tapping on "Import Wallet"', async () => {
     await Helpers.waitAndTap('wallet-info-submit-button');
-    await Helpers.checkIfVisible('wallet-screen', 40000);
     await Helpers.enableSynchronization();
+    await Helpers.checkIfVisible('wallet-screen', 200000);
   });
 
   afterAll(async () => {
