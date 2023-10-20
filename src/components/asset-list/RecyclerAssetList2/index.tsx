9edef6ba3e26efd3a0998e0ed43c9480c835b1a4
Christian Baroni <christianbaroni@me.com>
2023-10-20 16:46:15 -0400

    Points cleanup (#5139)
    * Add gravity emojis, fix parachute icon
    
    * Fix wallet screen scroll indicator
    
    ---------
    
    Co-authored-by: Christian Baroni <7061887+christianbaroni@users.noreply.github.com>

--- src/components/asset-list/RecyclerAssetList2/index.tsx
+++ src/components/asset-list/RecyclerAssetList2/index.tsx
@@ -70,7 +70,7 @@ function RecyclerAssetList({
           briefSectionsData={briefSectionsData}
           disablePullDownToRefresh={!!disablePullDownToRefresh}
           extendedState={extendedState}
-          scrollIndicatorInsets={{ bottom: 40, top: 40 }}
+          scrollIndicatorInsets={{ bottom: 40, top: 132 }}
           type={type}
         />
       </StickyHeaderManager>
