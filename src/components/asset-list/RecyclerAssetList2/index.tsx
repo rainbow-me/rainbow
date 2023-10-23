86591ea51cac59cc09d355f3c5e43732104f3789
skylarbarrera <skylar.barrera@gmail.com>
2023-10-23 13:16:33 -0400

    wc: always show connected dapps

--- src/components/asset-list/RecyclerAssetList2/index.tsx
+++ src/components/asset-list/RecyclerAssetList2/index.tsx
@@ -177,13 +177,12 @@ function NavbarOverlay({
           actionTitle: lang.t('button.my_qr_code'),
           icon: { iconType: 'SYSTEM', iconValue: 'qrcode' },
         },
-        mostRecentWalletConnectors.length > 0 || activeWCV2Sessions.length > 0
-          ? {
-              actionKey: 'connectedApps',
-              actionTitle: lang.t('wallet.connected_apps'),
-              icon: { iconType: 'SYSTEM', iconValue: 'app.badge.checkmark' },
-            }
-          : null,
+
+        {
+          actionKey: 'connectedApps',
+          actionTitle: lang.t('wallet.connected_apps'),
+          icon: { iconType: 'SYSTEM', iconValue: 'app.badge.checkmark' },
+        },
       ].filter(Boolean),
       ...(ios ? { menuTitle: '' } : {}),
     }),
