4e95a2ccf18225d31c7c2da232cc88aa78f82376
jinchung <jin.chung17@gmail.com>
2023-10-31 08:48:35 -0400

    Add support for new notification subscription endpoint
    Includes retries if the current FCM token is out of date.

--- src/notifications/settings/firebase.ts
+++ src/notifications/settings/firebase.ts
@@ -1,11 +1,97 @@
+import { NOTIFICATIONS_API_KEY } from 'react-native-dotenv';
+import { logger, RainbowError } from '@/logger';
+import { RainbowNetworks } from '@/networks';
 import {
   NotificationTopicType,
+  NotificationSubscriptionWalletsType,
   WalletNotificationSettings,
 } from '@/notifications/settings/types';
+import { getFCMToken, saveFCMToken } from '@/notifications/tokens';
 import messaging from '@react-native-firebase/messaging';
 import { trackChangedNotificationSettings } from '@/notifications/analytics';
 import { NotificationTopic } from '@/notifications/settings/constants';
-import { logger } from '@/logger';
+import { rainbowFetch } from '@/rainbow-fetch';
+
+const NOTIFICATION_SUBSCRIPTIONS_URL =
+  'https://notifications.p.rainbow.me/api/v1/subscriptions';
+
+const INVALID_FCM_TOKEN_ERROR =
+  'failed to validate FCM token: invalid or expired FCM token';
+
+type NotificationsSubscriptionResponse = {
+  error: boolean;
+  shouldRetry: boolean;
+};
+
+const updateNotificationSubscription = async (
+  firebaseToken: string,
+  wallets: NotificationSubscriptionWalletsType[]
+): Promise<NotificationsSubscriptionResponse> => {
+  try {
+    const options = {
+      firebase_token: firebaseToken,
+      wallets: wallets,
+    };
+    await rainbowFetch(NOTIFICATION_SUBSCRIPTIONS_URL, {
+      method: 'put',
+      body: JSON.stringify(options),
+      headers: {
+        Authorization: `Bearer ${NOTIFICATIONS_API_KEY}`,
+      },
+    });
+
+    // success
+    return {
+      error: false,
+      shouldRetry: false,
+    };
+  } catch (error: any) {
+    // if INVALID_FCM_TOKEN_ERROR message, retry with updated FCM token
+    if (error.message === INVALID_FCM_TOKEN_ERROR) {
+      return {
+        error: true,
+        shouldRetry: true,
+      };
+    }
+
+    logger.error(new RainbowError('Failed to subscribe to notifications'), {
+      message: (error as Error).message,
+    });
+
+    return {
+      error: true,
+      shouldRetry: false,
+    };
+  }
+};
+
+const updateNotificationSubscriptionWithRetry = async (
+  firebaseToken: string,
+  wallets: NotificationSubscriptionWalletsType[]
+): Promise<boolean> => {
+  const subscriptionResponse = await updateNotificationSubscription(
+    firebaseToken,
+    wallets
+  );
+
+  if (!subscriptionResponse.error) {
+    // success
+    return true;
+  } else if (subscriptionResponse.shouldRetry) {
+    // retry with an updated FCM token
+    await saveFCMToken();
+    const refreshedFirebaseToken = await getFCMToken();
+    if (!refreshedFirebaseToken) return false;
+
+    const subscriptionRetryResponse = await updateNotificationSubscription(
+      refreshedFirebaseToken,
+      wallets
+    );
+    return !subscriptionRetryResponse.error;
+  } else {
+    return false;
+  }
+};
 
 /**
  Firebase functions for subscribing/unsubscribing to topics.
@@ -28,6 +114,69 @@ export const subscribeWalletToAllEnabledTopics = (
       }
     })
   );
+};
+
+// returns updated wallet settings on success, undefined otherwise
+export const publishWalletSettings = async (
+  walletSettings: WalletNotificationSettings[]
+): Promise<WalletNotificationSettings[] | undefined> => {
+  try {
+    const subscriptionPayload = parseWalletSettings(walletSettings);
+    let firebaseToken = await getFCMToken();
+
+    // refresh the FCM token if not found
+    if (!firebaseToken) {
+      await saveFCMToken();
+      firebaseToken = await getFCMToken();
+      if (!firebaseToken) return;
+    }
+
+    const success = await updateNotificationSubscriptionWithRetry(
+      firebaseToken,
+      subscriptionPayload
+    );
+    if (success) {
+      return walletSettings.map(setting => {
+        return {
+          ...setting,
+          successfullyFinishedInitialSubscription: true,
+        };
+      });
+    } else {
+      return;
+    }
+  } catch (e: any) {
+    logger.error(
+      new RainbowError('Failed to publish wallet notification settings'),
+      {
+        message: (e as Error).message,
+      }
+    );
+
+    return;
+  }
+};
+
+const parseWalletSettings = (
+  walletSettings: WalletNotificationSettings[]
+): NotificationSubscriptionWalletsType[] => {
+  return walletSettings.flatMap(setting => {
+    const topics = Object.keys(setting.topics).filter(
+      topic => !!setting.topics[topic]
+    );
+    const notificationChainIds = RainbowNetworks.filter(
+      network => network.enabled && network.features.notifications
+    ).map(network => network.id);
+
+    return notificationChainIds.map(chainId => {
+      return {
+        type: setting.type,
+        chain_id: chainId,
+        address: setting.address.toLowerCase(),
+        transaction_action_types: topics,
+      };
+    });
+  });
 };
 
 export const unsubscribeWalletFromAllNotificationTopics = (
