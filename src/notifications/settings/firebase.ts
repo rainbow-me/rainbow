67223792511fea3968286f9b398c2d6995e7663e
jinchung <jin.chung17@gmail.com>
2023-10-31 08:48:35 -0400

    Placeholder: basic function for new notification subscription with types

--- src/notifications/settings/firebase.ts
+++ src/notifications/settings/firebase.ts
@@ -1,11 +1,36 @@
+import { NOTIFICATIONS_API_KEY } from 'react-native-dotenv';
 import {
   NotificationTopicType,
+  NotificationSubscriptionWalletsType,
   WalletNotificationSettings,
 } from '@/notifications/settings/types';
 import messaging from '@react-native-firebase/messaging';
 import { trackChangedNotificationSettings } from '@/notifications/analytics';
 import { NotificationTopic } from '@/notifications/settings/constants';
 import { logger } from '@/logger';
+import { rainbowFetch } from '@/rainbow-fetch';
+
+const NOTIFICATION_SUBSCRIPTIONS_URL =
+  'https://notifications.p.rainbow.me/api/v1/subscriptions';
+
+const updateNotificationSubscription = async (
+  firebaseToken: string,
+  wallets: NotificationSubscriptionWalletsType[]
+) => {
+  const options = {
+    firebase_token: firebaseToken,
+    wallets: wallets,
+  };
+  const response = await rainbowFetch(NOTIFICATION_SUBSCRIPTIONS_URL, {
+    method: 'put',
+    body: JSON.stringify(options),
+    headers: {
+      Authorization: `Bearer ${NOTIFICATIONS_API_KEY}`,
+    },
+  });
+
+  return response.data;
+};
 
 /**
  Firebase functions for subscribing/unsubscribing to topics.
