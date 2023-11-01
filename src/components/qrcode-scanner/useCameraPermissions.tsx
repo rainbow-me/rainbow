d397edb7dd31583975beaef915ef3512c8fd36b6
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2023-11-01 11:51:42 -0500

    init

---
+++ src/components/qrcode-scanner/useCameraPermissions.tsx
@@ -0,0 +1,57 @@
+import { useState, useCallback, useEffect } from 'react';
+import { Platform } from 'react-native';
+import {
+  check as checkForPermissions,
+  PERMISSIONS,
+  request as requestPermission,
+  RESULTS,
+} from 'react-native-permissions';
+
+export enum CameraState {
+  Unauthorized = 'Unauthorized',
+  Scanning = 'Scanning',
+  Error = 'Error',
+}
+
+export function useCameraPermission() {
+  const [cameraState, setCameraState] = useState<CameraState>(
+    CameraState.Scanning
+  );
+
+  const askForPermissions = useCallback(async () => {
+    try {
+      const permission =
+        Platform.OS === 'ios'
+          ? PERMISSIONS.IOS.CAMERA
+          : PERMISSIONS.ANDROID.CAMERA;
+
+      const res = await checkForPermissions(permission);
+
+      if (res === RESULTS.DENIED || res === RESULTS.BLOCKED) {
+        const askResult = await requestPermission(permission);
+
+        if (askResult !== RESULTS.GRANTED) {
+          setCameraState(CameraState.Unauthorized);
+        } else {
+          setCameraState(CameraState.Scanning);
+        }
+      } else if (res === RESULTS.UNAVAILABLE) {
+        setCameraState(CameraState.Unauthorized);
+      } else if (res === RESULTS.GRANTED) {
+        setCameraState(CameraState.Scanning);
+      }
+    } catch (err) {
+      setCameraState(CameraState.Error);
+      throw err;
+    }
+  }, []);
+
+  useEffect(() => {
+    askForPermissions();
+  }, [askForPermissions]);
+
+  return {
+    cameraState,
+    askForPermissions,
+  };
+}
