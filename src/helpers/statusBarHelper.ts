8a1a0adfb4f3c9535b08f4fabb46b22b3b074437
Matthew Wall <matthew.wallt@gmail.com>
2023-11-20 18:36:56 -0700

    [Android]: Random bug fixes (#5195)
    * fix gradient on bottom of nft send sheet
    
    * fix zora eth to weth swap decimal place
    
    * adjust detection for button navigation
    
    * Update src/hooks/useSwapAdjustedAmounts.ts
    
    * fix overflow issue with language sheet
    
    * fix lint

--- src/helpers/statusBarHelper.ts
+++ src/helpers/statusBarHelper.ts
@@ -45,5 +45,5 @@ export const isUsingButtonNavigation = () => {
   const deviceHeight = Dimensions.get('screen').height;
   const windowHeight = Dimensions.get('window').height;
   const bottomNavBarHeight = deviceHeight - windowHeight;
-  return bottomNavBarHeight > 80;
+  return bottomNavBarHeight > 24;
 };
--- src/hooks/useSwapAdjustedAmounts.ts
+++ src/hooks/useSwapAdjustedAmounts.ts
@@ -8,6 +8,7 @@ import {
   WMATIC_POLYGON_ADDRESS,
   WBNB_BSC_ADDRESS,
   WETH_ARBITRUM_ADDRESS,
+  WETH_ZORA_ADDRESS,
 } from '@/references';
 import { fromWei, updatePrecisionToDisplay } from '@/helpers/utilities';
 import { ethereumUtils } from '@/utils';
@@ -63,7 +64,12 @@ export default function useSwapAdjustedAmounts(tradeDetails: Quote) {
     (tradeDetails.buyTokenAddress === ETH_ADDRESS &&
       tradeDetails.sellTokenAddress === WETH_ARBITRUM_ADDRESS) ||
     (tradeDetails.sellTokenAddress === ETH_ADDRESS &&
-      tradeDetails.buyTokenAddress === WETH_ARBITRUM_ADDRESS)
+      tradeDetails.buyTokenAddress === WETH_ARBITRUM_ADDRESS) ||
+    // zora eth <-> weth swap
+    (tradeDetails.buyTokenAddress === ETH_ADDRESS &&
+      tradeDetails.sellTokenAddress === WETH_ZORA_ADDRESS) ||
+    (tradeDetails.sellTokenAddress === ETH_ADDRESS &&
+      tradeDetails.buyTokenAddress === WETH_ZORA_ADDRESS)
   ) {
     amountReceivedSold = fromWei(amountReceivedSold.toString());
   }
