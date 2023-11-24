517043f916ca93009fc45a2d860d57d3e95722e4
jinchung <jin.chung17@gmail.com>
2023-11-24 17:04:03 -0300

    Revert back to using imports from notifications files directly

--- src/notifications/settings/index.ts
+++ src/notifications/settings/index.ts
@@ -1,7 +1,7 @@
-export { DEFAULT_ENABLED_TOPIC_SETTINGS, WalletNotificationRelationship, WalletNotificationTopic, WALLET_GROUPS_STORAGE_KEY, WALLET_TOPICS_STORAGE_KEY } from './constants';
+export { WalletNotificationRelationship, WalletNotificationTopic, WALLET_GROUPS_STORAGE_KEY, WALLET_TOPICS_STORAGE_KEY } from './constants';
 export { addDefaultNotificationGroupSettings, initializeNotificationSettingsForAddresses } from './initialization';
 export { useAllNotificationSettingsFromStorage, useWalletGroupNotificationSettings } from './hooks';
-export { publishAndSaveWalletSettings, removeNotificationSettingsForWallet, toggleGroupNotifications, toggleTopicForWallet } from './settings';
+export { removeNotificationSettingsForWallet } from './settings';
 export type {
   AddressWithRelationship,
   GlobalNotificationTopicType,
@@ -10,4 +10,4 @@ export type {
   WalletNotificationTopicType,
   WalletNotificationSettings,
 } from './types';
-export { getAllWalletNotificationSettingsFromStorage, setAllWalletNotificationSettingsToStorage, notificationSettingsStorage, updateGroupSettings } from './storage';
+export { notificationSettingsStorage, updateGroupSettings } from './storage';
