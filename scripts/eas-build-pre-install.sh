e389189afd512b8b6fa9fe115744ada5e1228cf9
Matthew Wall <matthew.wallt@gmail.com>
2023-12-01 10:07:20 -0700

    fix: prebuild.sh files not working with eval (#5215)

--- scripts/eas-build-pre-install.sh
+++ scripts/eas-build-pre-install.sh
@@ -2,7 +2,6 @@
 
 echo "PRE INSTALLING"
 
-
 if [ -n "$RAINBOW_SCRIPTS_INTERNALS" ]; then
 	git clone https://$RAINBOW_SCRIPTS_TOKEN$RAINBOW_SCRIPTS_INTERNALS
     cd rainbow-scripts
@@ -11,12 +10,12 @@ if [ -n "$RAINBOW_SCRIPTS_INTERNALS" ]; then
 fi
 
 if [ "$EAS_BUILD_PLATFORM" = "ios" ]; then
-    eval $RAINBOW_SCRIPTS_APP_IOS_PREBUILD_HOOK
+    bash $RAINBOW_SCRIPTS_APP_IOS_PREBUILD_HOOK;
     echo "✅ executed ios prebuild hook"
 fi
 
 if [ "$EAS_BUILD_PLATFORM" = "android" ]; then
-    eval $RAINBOW_SCRIPTS_APP_ANDROID_PREBUILD_HOOK > /dev/null 2>&1;
+    bash $RAINBOW_SCRIPTS_APP_ANDROID_PREBUILD_HOOK;
     echo "✅ executed android prebuild hook"
 fi
 
