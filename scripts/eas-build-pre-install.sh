b7fa33ce623d945550e9258e92e2961d3108025b
Matthew Wall <matthew.wallt@gmail.com>
2023-12-01 10:27:40 -0700

    add .easignore step

--- scripts/eas-build-pre-install.sh
+++ scripts/eas-build-pre-install.sh
@@ -3,20 +3,30 @@
 echo "PRE INSTALLING"
 
 if [ -n "$RAINBOW_SCRIPTS_INTERNALS" ]; then
+    echo "🌈 Pulling latest rainbow-scripts code from github"
 	git clone https://$RAINBOW_SCRIPTS_TOKEN$RAINBOW_SCRIPTS_INTERNALS
     cd rainbow-scripts
     git checkout prebuild
     cd ..
 fi
 
+echo "🌈 Creating .easignore"
+bash $RAINBOW_SCRIPTS_APP_EASIGNORE_HOOK;
+
 if [ "$EAS_BUILD_PLATFORM" = "ios" ]; then
+    echo "🌈 Executing iOS prebuild.sh script"
     bash $RAINBOW_SCRIPTS_APP_IOS_PREBUILD_HOOK;
     echo "✅ executed ios prebuild hook"
 fi
 
 if [ "$EAS_BUILD_PLATFORM" = "android" ]; then
+    echo "🌈 Executing Android prebuild.sh script"
     bash $RAINBOW_SCRIPTS_APP_ANDROID_PREBUILD_HOOK;
     echo "✅ executed android prebuild hook"
 fi
+
+
+echo "🌈 Finished preinstallation..."
+echo "🌈 Running yarn setup..."
 
 yarn setup
