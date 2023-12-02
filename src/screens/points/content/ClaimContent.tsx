b86f302fff28e0fde3aa2707d733b079783db5c8
Ben Goldberg <bengoldberg@rainbow.me>
2023-12-01 20:54:08 -0800

    restrict read only wallets

--- src/screens/points/content/ClaimContent.tsx
+++ src/screens/points/content/ClaimContent.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { navbarHeight } from '@/components/navbar/Navbar';
 import { Box, Stack, Text } from '@/design-system';
-import { useAccountAccentColor, useDimensions } from '@/hooks';
+import { useAccountAccentColor, useDimensions, useWallets } from '@/hooks';
 import { useNavigation } from '@/navigation';
 import { TAB_BAR_HEIGHT } from '@/navigation/SwipeNavigator';
 import * as i18n from '@/languages';
 import Routes from '@/navigation/routesNames';
 import { ActionButton } from '@/screens/points/components/ActionButton';
 import { PointsIconAnimation } from '../components/PointsIconAnimation';
+import { watchingAlert } from '@/utils';
 
 export default function ClaimContent() {
   const { accentColor } = useAccountAccentColor();
   const { navigate } = useNavigation();
   const { height: deviceHeight } = useDimensions();
+  const { isReadOnlyWallet } = useWallets();
 
   const contentBottom =
     TAB_BAR_HEIGHT + (deviceHeight - navbarHeight - 218) / 2;
@@ -47,14 +49,20 @@ export default function ClaimContent() {
           <ActionButton
             color={accentColor}
             label={i18n.t(i18n.l.points.claim.get_started)}
-            onPress={() => navigate(Routes.CONSOLE_SHEET)}
+            onPress={() =>
+              isReadOnlyWallet
+                ? watchingAlert()
+                : navigate(Routes.CONSOLE_SHEET)
+            }
           />
         </Stack>
       </Box>
       <ActionButton
         color={accentColor}
         label={i18n.t(i18n.l.points.claim.use_referral_code)}
-        onPress={() => navigate('ReferralContent')}
+        onPress={() =>
+          isReadOnlyWallet ? watchingAlert() : navigate('ReferralContent')
+        }
         outline
         small
       />
