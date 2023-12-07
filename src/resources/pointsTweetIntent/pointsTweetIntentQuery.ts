9a377343feab01d54143acee13d33a1c834e8f5e
Matthew Wall <matthew.wallt@gmail.com>
2023-12-07 13:24:54 -0700

    add contentful points tweet intent schema and queries (#5228)

---
+++ src/resources/pointsTweetIntent/pointsTweetIntentQuery.ts
@@ -0,0 +1,87 @@
+import { useQuery } from '@tanstack/react-query';
+
+import {
+  createQueryKey,
+  queryClient,
+  QueryConfig,
+  QueryFunctionArgs,
+  QueryFunctionResult,
+} from '@/react-query';
+
+import { arcDevClient } from '@/graphql';
+
+// Set a default stale time of 10 seconds so we don't over-fetch
+// (query will serve cached data & invalidate after 10s).
+const defaultStaleTime = 60_000;
+
+export type PointsTweetIntentArgs = {
+  id: string;
+};
+
+// ///////////////////////////////////////////////
+// Query Key
+
+const pointsTweetIntentQueryKey = ({ id }: PointsTweetIntentArgs) =>
+  createQueryKey('pointsTweetIntent', { id }, { persisterVersion: 1 });
+
+type PointsTweetIntentQueryKey = ReturnType<typeof pointsTweetIntentQueryKey>;
+
+// ///////////////////////////////////////////////
+// Query Function
+
+async function pointsTweetIntentQueryFunction({
+  queryKey: [{ id }],
+}: QueryFunctionArgs<typeof pointsTweetIntentQueryKey>) {
+  const data = await arcDevClient.getPointsTweetIntent({ id });
+  return data;
+}
+
+export type PointsTweetIntentResult = QueryFunctionResult<
+  typeof pointsTweetIntentQueryFunction
+>;
+
+// ///////////////////////////////////////////////
+// Query Prefetcher
+
+export async function prefetchPointsTweetIntent(
+  { id }: PointsTweetIntentArgs,
+  config: QueryConfig<
+    PointsTweetIntentResult,
+    Error,
+    PointsTweetIntentQueryKey
+  > = {}
+) {
+  return await queryClient.prefetchQuery(
+    pointsTweetIntentQueryKey({ id }),
+    pointsTweetIntentQueryFunction,
+    config
+  );
+}
+
+// ///////////////////////////////////////////////
+// Query Fetcher
+
+export async function fetchPointsTweetIntent({ id }: PointsTweetIntentArgs) {
+  return await queryClient.fetchQuery(
+    pointsTweetIntentQueryKey({ id }),
+    pointsTweetIntentQueryFunction,
+    { staleTime: defaultStaleTime }
+  );
+}
+
+// ///////////////////////////////////////////////
+// Query Hook
+
+export function usePointsTweetIntentQuery(
+  { id }: PointsTweetIntentArgs,
+  { enabled }: { enabled?: boolean } = {}
+) {
+  return useQuery(
+    pointsTweetIntentQueryKey({ id }),
+    pointsTweetIntentQueryFunction,
+    {
+      enabled,
+      staleTime: defaultStaleTime,
+    }
+  );
+}
