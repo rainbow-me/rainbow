bac66652e248bef772f9f0e8d4de7fa7d4df2052
skylarbarrera <skylar.barrera@gmail.com>
2023-12-11 12:57:19 -0500

    rm delay

--- src/utils/points.ts
+++ src/utils/points.ts
@@ -1,17 +1,14 @@
 import { Navigation } from '@/navigation';
 import Routes from '@/navigation/routesNames';
 import { POINTS_ROUTES } from '@/screens/points/PointsScreen';
-import { delay } from '@/utils/delay';
 
 export const handlePointsReferralCodeDeeplink = (code: string) => {
-  delay(3000).then(() => {
-    Navigation.handleAction(Routes.POINTS_SCREEN, {
+  Navigation.handleAction(Routes.POINTS_SCREEN, {
+    externalReferralCode: code,
+    deeplinkId: Math.random().toString(36).slice(2),
+    screen: POINTS_ROUTES.REFERRAL_CONTENT,
+    params: {
       externalReferralCode: code,
-      deeplinkId: Math.random().toString(36).slice(2),
-      screen: POINTS_ROUTES.REFERRAL_CONTENT,
-      params: {
-        externalReferralCode: code,
-      },
-    });
+    },
   });
 };
