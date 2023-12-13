cea5ba24f3c6c19fe278e35940938794fce8f772
Ben Goldberg <bengoldberg@rainbow.me>
2023-12-13 12:55:54 -0800

    fix refetch on next drop

--- src/resources/points.ts
+++ src/resources/points.ts
@@ -24,6 +24,7 @@ export function usePointsReferralCode() {
   return query;
 }
 
+let nextDropTimeout: NodeJS.Timeout | undefined;
 export function usePoints({ walletAddress }: { walletAddress: string }) {
   const pointsEnabled =
     (useExperimentalFlag(POINTS) || config.points_fully_enabled) &&
@@ -45,10 +46,18 @@ export function usePoints({ walletAddress }: { walletAddress: string }) {
   );
 
   useEffect(() => {
-    const nextDistribution = query?.data?.points?.meta?.distribution?.next;
-    if (nextDistribution && Date.now() >= nextDistribution * 1000) {
-      query.refetch();
-    }
+    const nextDistribution = query.data?.points?.meta.distribution.next;
+    if (!nextDistribution) return;
+    const nextDistributionIn = nextDistribution * 1000 - Date.now();
+
+    nextDropTimeout ??= setTimeout(() => query.refetch(), nextDistributionIn);
+
+    return () => {
+      if (nextDropTimeout) {
+        clearTimeout(nextDropTimeout);
+        nextDropTimeout = undefined;
+      }
+    };
   }, [query]);
 
   return query;
