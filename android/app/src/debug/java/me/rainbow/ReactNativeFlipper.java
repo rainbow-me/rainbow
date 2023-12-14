42272ce10a763c7cf55d827b6f5995bb899b7a60
Elias Rad <146735585+nnsW3@users.noreply.github.com>
2023-12-15 00:06:05 +0200

    fix typos ReactNativeFlipper.java

--- android/app/src/debug/java/me/rainbow/ReactNativeFlipper.java
+++ android/app/src/debug/java/me/rainbow/ReactNativeFlipper.java
@@ -47,7 +47,7 @@ public class ReactNativeFlipper {
       client.start();
 
       // Fresco Plugin needs to ensure that ImagePipelineFactory is initialized
-      // Hence we run if after all native modules have been initialized
+      // Hence we run after all native modules have been initialized
       ReactContext reactContext = reactInstanceManager.getCurrentReactContext();
       if (reactContext == null) {
         reactInstanceManager.addReactInstanceEventListener(
