31d3442ace18dade5018160225e1c74c284a726c
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2023-12-18 14:33:04 -0500

    Clean up NFT sort UI, list header heights

--- src/components/list/ListHeaderMenu.tsx
+++ src/components/list/ListHeaderMenu.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ContextMenuButton from '@/components/native-context-menu/contextMenu';
 import { ButtonPressAnimation } from '@/components/animations';
-import { Inline, Inset, Text } from '@/design-system';
+import { Bleed, Box, Inline, Text, useForegroundColor } from '@/design-system';
 import { haptics } from '@/utils';
 import { CollectibleSortByOptions } from '@/hooks/useNFTsSortBy';
 
@@ -15,14 +15,18 @@ type ListHeaderMenuProps = {
   selected: CollectibleSortByOptions;
   menuItems: MenuItem[];
   selectItem: (item: string) => void;
+  icon: string;
   text: string;
 };
 
 export function ListHeaderMenu({
   menuItems,
   selectItem,
+  icon,
   text,
 }: ListHeaderMenuProps) {
+  const separatorTertiary = useForegroundColor('separatorTertiary');
+
   const menuConfig = {
     menuTitle: '',
     menuItems: menuItems,
@@ -38,24 +42,51 @@ export function ListHeaderMenu({
   };
 
   return (
-    <ContextMenuButton
-      menuConfig={menuConfig}
-      onPressMenuItem={onPressMenuItem}
-    >
-      <ButtonPressAnimation>
-        <Inset top="2px">
-          <Inline alignVertical="center" space={{ custom: 5 }}>
-            <Inline alignVertical="center" horizontalSpace={'2px'}>
-              <Text color="label" size="17pt" weight="semibold">
-                {text}
-              </Text>
-              <Text color="label" size="15pt" weight="semibold">
-                􀆈
-              </Text>
-            </Inline>
-          </Inline>
-        </Inset>
-      </ButtonPressAnimation>
-    </ContextMenuButton>
+    <Bleed space="10px">
+      <ContextMenuButton
+        menuConfig={menuConfig}
+        onPressMenuItem={onPressMenuItem}
+      >
+        <ButtonPressAnimation style={{ padding: 10 }}>
+          <Bleed bottom="2px">
+            <Box
+              alignItems="center"
+              borderRadius={15}
+              height={{ custom: 30 }}
+              justifyContent="center"
+              paddingHorizontal="8px"
+              style={{ borderColor: separatorTertiary, borderWidth: 1.5 }}
+            >
+              <Inline alignVertical="center" space={{ custom: 5 }} wrap={false}>
+                <Text
+                  align="center"
+                  color="labelQuaternary"
+                  size="icon 13px"
+                  weight="bold"
+                >
+                  {icon}
+                </Text>
+                <Text
+                  align="center"
+                  color="labelTertiary"
+                  size="15pt"
+                  weight="bold"
+                >
+                  {text}
+                </Text>
+                <Text
+                  align="center"
+                  color="labelQuaternary"
+                  size="icon 13px"
+                  weight="bold"
+                >
+                  􀆏
+                </Text>
+              </Inline>
+            </Box>
+          </Bleed>
+        </ButtonPressAnimation>
+      </ContextMenuButton>
+    </Bleed>
   );
 }
