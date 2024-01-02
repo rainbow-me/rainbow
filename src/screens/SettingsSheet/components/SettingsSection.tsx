91b0fb267094a06fcbbc2268d4d7f7bfd7f6c296
skylarbarrera <skylar.barrera@gmail.com>
2024-01-02 13:02:18 -0500

    remote testnet settings

--- src/screens/SettingsSheet/components/SettingsSection.tsx
+++ src/screens/SettingsSheet/components/SettingsSection.tsx
@@ -290,7 +290,7 @@ const SettingsSection = ({
           testID="currency-section"
           titleComponent={<MenuItem.Title text={lang.t('settings.currency')} />}
         />
-        {(testnetsEnabled || IS_DEV) && (
+        {/* {(testnetsEnabled || IS_DEV) && (
           <MenuItem
             hasRightArrow
             leftComponent={
@@ -310,7 +310,7 @@ const SettingsSection = ({
               <MenuItem.Title text={lang.t('settings.network')} />
             }
           />
-        )}
+        )} */}
         <ContextMenuButton
           menuConfig={themeMenuConfig}
           {...(android ? { onPress: onPressThemeAndroidActions } : {})}
