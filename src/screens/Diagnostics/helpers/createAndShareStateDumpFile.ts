b8953c74dd310f830fbe404ee4b784cb3ce7034c
Ben Goldberg <bengoldberg@rainbow.me>
2024-02-05 14:24:01 -0700

    progress

--- src/screens/Diagnostics/helpers/createAndShareStateDumpFile.ts
+++ src/screens/Diagnostics/helpers/createAndShareStateDumpFile.ts
@@ -5,10 +5,51 @@ import RNShare from 'react-native-share';
 import { IS_ANDROID } from '@/env';
 import { logger, RainbowError } from '@/logger';
 import { stringify } from 'flatted';
+import { getAllActiveSessions } from '@/walletConnect';
+
+function cyclicReplacer() {
+  const seenObjects = new Map<object, string[]>(); // Tracks objects and their paths
+
+  function replacer(
+    key: string | undefined,
+    value: any,
+    path: string[] = []
+  ): any {
+    if (typeof value === 'object' && value !== null) {
+      // Determine the new path
+      const newPath = key !== undefined ? path.concat(key) : path;
+
+      if (seenObjects.has(value)) {
+        // Construct a string representation of the path to the cyclic reference
+        return `Cyclic reference to ${seenObjects
+          .get(value)
+          ?.filter(key => !!key)
+          ?.join('.')}`;
+      }
+
+      seenObjects.set(value, newPath);
+
+      // Recursively handle nested objects and arrays
+      const valueCopy: { [key: string]: any } = Array.isArray(value) ? [] : {};
+      for (const k of Object.keys(value)) {
+        valueCopy[k] = replacer(k, value[k], newPath);
+      }
+      return valueCopy;
+    }
+    return value;
+  }
+
+  return (key: string, value: any): any => replacer(key, value);
+}
 
 export async function createAndShareStateDumpFile() {
-  const appState = store.getState();
-  const stringifiedState = stringify(appState);
+  const reduxState = store.getState();
+  const walletConnectV2Sessions = await getAllActiveSessions();
+  const appState = {
+    ...reduxState,
+    walletConnectV2: { sessions: walletConnectV2Sessions },
+  };
+  const stringifiedState = JSON.stringify(appState, cyclicReplacer());
 
   const documentsFilePath = `${RNFS.DocumentDirectoryPath}/${APP_STATE_DUMP_FILE_NAME}`;
   try {
