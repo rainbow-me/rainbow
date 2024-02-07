5a676c0c61684c815f1dae28d3a337993fa68231
skylarbarrera <skylar.barrera@gmail.com>
2024-02-07 13:16:19 -0500

    fixes

--- src/screens/CurrencySelectModal.tsx
+++ src/screens/CurrencySelectModal.tsx
@@ -345,12 +345,7 @@ export default function CurrencySelectModal() {
     (item: any) => {
       if (!crosschainSwapsEnabled && checkForRequiredAssets(item)) return;
 
-      const assetWithType = {
-        ...item,
-        decimals: item?.networks?.[currentChainId]?.decimals || item.decimals,
-        address: item?.networks?.[currentChainId]?.address,
-        network: getNetworkFromChainId(currentChainId),
-      };
+      let newAsset = item;
 
       const selectAsset = () => {
         if (!item?.balance) {
@@ -360,15 +355,22 @@ export default function CurrencySelectModal() {
             network,
             currency: nativeCurrency,
           });
+          // if the asset is external we need to add the network specific information
+          newAsset = {
+            ...newAsset,
+            decimals: item?.networks?.[currentChainId]?.decimals || item.decimals,
+            address: item?.networks?.[currentChainId]?.address,
+            network: getNetworkFromChainId(currentChainId),
+          };
         }
         setIsTransitioning(true); // continue to display list during transition
         callback?.();
-        onSelectCurrency(assetWithType, handleNavigate);
+        onSelectCurrency(newAsset, handleNavigate);
       };
       if (
         !crosschainSwapsEnabled &&
         checkForSameNetwork(
-          assetWithType,
+          newAsset,
           selectAsset,
           type === CurrencySelectionTypes.output ? CurrencySelectionTypes.output : CurrencySelectionTypes.input
         )
@@ -384,7 +386,6 @@ export default function CurrencySelectModal() {
       type,
       checkForSameNetwork,
       nativeCurrency,
-      dispatch,
       callback,
       onSelectCurrency,
       handleNavigate,
