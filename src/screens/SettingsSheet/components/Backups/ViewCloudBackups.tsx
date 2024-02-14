266ec57087417126320646f95ca239113fe299d8
Matthew Wall <matthew.wallt@gmail.com>
2024-02-14 13:13:42 -0700

    stuff

--- src/screens/SettingsSheet/components/Backups/ViewCloudBackups.tsx
+++ src/screens/SettingsSheet/components/Backups/ViewCloudBackups.tsx
@@ -3,19 +3,27 @@ import React, { useCallback } from 'react';
 import Menu from '../Menu';
 import MenuContainer from '../MenuContainer';
 import MenuItem from '../MenuItem';
-import { parseTimestampFromFilename } from '@/model/backup';
+import { BackupUserData } from '@/model/backup';
 import { format } from 'date-fns';
 import { Stack } from '@/design-system';
 import { useNavigation } from '@/navigation';
 import Routes from '@/navigation/routesNames';
 import walletBackupStepTypes from '@/helpers/walletBackupStepTypes';
-import useCloudBackups from '@/hooks/useCloudBackups';
 import walletBackupTypes from '@/helpers/walletBackupTypes';
 import { RainbowWallet } from '@/model/wallet';
+import { RouteProp, useRoute } from '@react-navigation/native';
+
+type ViewCloudBackupsParams = {
+  ViewCloudBackups: {
+    backups: BackupUserData;
+  };
+};
 
 const ViewCloudBackups = () => {
-  const { backups } = useCloudBackups();
+  const { params } = useRoute<RouteProp<ViewCloudBackupsParams, 'ViewCloudBackups'>>();
   const { navigate } = useNavigation();
+
+  const { backups } = params;
 
   const cloudBackups = Object.values(backups.wallets || {})
     .filter(backup => backup.backedUp && backup.backupType === walletBackupTypes.cloud)
@@ -55,7 +63,7 @@ const ViewCloudBackups = () => {
         {mostRecentBackup && (
           <Menu
             description={i18n.t(i18n.l.back_up.cloud.latest_backup, {
-              date: format(parseTimestampFromFilename(mostRecentBackup.name), "M/d/yy 'at' h:mm a"),
+              date: format(new Date(mostRecentBackup.backupDate!), "M/d/yy 'at' h:mm a"),
             })}
           >
             <MenuItem
@@ -71,17 +79,17 @@ const ViewCloudBackups = () => {
         <Menu header={i18n.t(i18n.l.back_up.cloud.older_backups)}>
           {cloudBackups.map(
             backup =>
-              backup.name !== mostRecentBackup?.name && (
+              backup.id !== mostRecentBackup?.id && (
                 <MenuItem
-                  key={backup.name}
+                  key={backup.id}
                   size={52}
                   onPress={() => onSelectCloudBackup(backup)}
                   titleComponent={
                     <MenuItem.Title
                       isLink
                       text={i18n.t(i18n.l.back_up.cloud.older_backups_title, {
-                        date: format(parseTimestampFromFilename(backup.name), 'M/d/yy'),
-                        time: format(parseTimestampFromFilename(backup.name), 'p'),
+                        date: format(new Date(backup.backupDate!), 'M/d/yy'),
+                        time: format(new Date(backup.backupDate!), 'p'),
                       })}
                     />
                   }
