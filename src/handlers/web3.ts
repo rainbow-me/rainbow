aa7bf440405970f2d6be772d85d68df87e77a555
Derek <dereknelsony0@gmail.com>
2024-02-21 21:35:43 -0500

    added proxy url and contract addys

--- src/handlers/web3.ts
+++ src/handlers/web3.ts
@@ -61,6 +61,7 @@ export const proxyRpcEndpoint = (chainId: number, customEndpoint?: string) => {
     base_mainnet_rpc,
     bsc_mainnet_rpc,
     zora_mainnet_rpc,
+    avalanche_mainnet_rpc,
     ethereum_mainnet_rpc,
   } = getRemoteConfig();
   if (rpc_proxy_enabled) {
@@ -85,6 +86,8 @@ export const proxyRpcEndpoint = (chainId: number, customEndpoint?: string) => {
         return bsc_mainnet_rpc;
       case Network.zora:
         return zora_mainnet_rpc;
+      case Network.avalanche:
+        return avalanche_mainnet_rpc;
       case Network.gnosis:
       case Network.mainnet:
       default:
@@ -138,7 +141,7 @@ type NewTransactionNonNullable = {
 /**
  * @desc web3 http instance
  */
-export let web3Provider: StaticJsonRpcProvider = (null as unknown) as StaticJsonRpcProvider;
+export let web3Provider: StaticJsonRpcProvider = null as unknown as StaticJsonRpcProvider;
 
 /**
  * @desc Checks whether or not a `Network | string` union type should be
