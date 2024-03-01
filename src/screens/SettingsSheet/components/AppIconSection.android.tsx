3a409e4f9cf7ebac9c37f31586a97eea83ee23ce
Ben Goldberg <bengoldberg@rainbow.me>
2024-03-01 10:54:57 -0800

    app icon refactor (#5444)
    * refactored unlocking functionality
    
    * done i think
    
    * mmkv id
    
    * refactor
    
    * types
    
    * types
    
    * type
    
    * fix android

--- src/screens/SettingsSheet/components/AppIconSection.android.tsx
+++ src/screens/SettingsSheet/components/AppIconSection.android.tsx
@@ -1,152 +1,18 @@
 import * as i18n from '@/languages';
 import React, { useCallback, useMemo } from 'react';
 import { Source } from 'react-native-fast-image';
-import { MMKV } from 'react-native-mmkv';
 import Menu from './Menu';
 import MenuContainer from './MenuContainer';
 import MenuItem from './MenuItem';
-import AppIconFiniliar from '@/assets/appIconFiniliar.png';
-import AppIconGoldDoge from '@/assets/appIconGoldDoge.png';
-import AppIconRainDoge from '@/assets/appIconRainDoge.png';
-import AppIconOg from '@/assets/appIconOg.png';
-import AppIconOptimism from '@/assets/appIconOptimism.png';
-import AppIconPixel from '@/assets/appIconPixel.png';
-import AppIconPooly from '@/assets/appIconPooly.png';
-import AppIconSmol from '@/assets/appIconSmol.png';
-import AppIconZora from '@/assets/appIconZora.png';
-import AppIconZorb from '@/assets/appIconZorb.png';
-import AppIconPoolboy from '@/assets/appIconPoolboy.png';
-import AppIconAdworld from '@/assets/appIconAdworld.png';
-import AppIconFarcaster from '@/assets/appIconFarcaster.png';
 import { BackgroundProvider, Box, Inline, Inset, Text } from '@/design-system';
 import { useAccountSettings } from '@/hooks';
 import { ImgixImage } from '@/components/images';
 import { useTheme } from '@/theme';
 import Logger from '@/utils/logger';
-import {
-  AdworldIcon,
-  FarcasterIcon,
-  FiniliarIcon,
-  GoldDogeIcon,
-  OptimismIcon,
-  PoolboyIcon,
-  PoolyIcon,
-  RainDogeIcon,
-  SmolIcon,
-  ZoraIcon,
-  ZorbIcon,
-} from '@/featuresToUnlock/unlockableAppIcons';
 import { analytics } from '@/analytics';
-import { ImageSourcePropType } from 'react-native';
 import { SimpleSheet } from '@/components/sheet/SimpleSheet';
-
-type AppIcon = {
-  accentColor?: string;
-  key: string;
-  name: string;
-  source: ImageSourcePropType;
-};
-
-const supportedAppIcons: { [key: string]: AppIcon } = {
-  og: {
-    accentColor: 'rainbowBlue',
-    key: 'og',
-    name: 'OG',
-    source: AppIconOg,
-  },
-  pixel: {
-    accentColor: 'rainbowBlue',
-    key: 'pixel',
-    name: 'Pixel',
-    source: AppIconPixel,
-  },
-};
-
-type LockedAppIcon = AppIcon & {
-  unlock_key: string;
-};
-
-export const tokenGatedIcons: { [key: string]: LockedAppIcon } = {
-  optimism: {
-    accentColor: 'optimismRed',
-    key: OptimismIcon.key,
-    name: 'Optimism',
-    source: AppIconOptimism,
-    unlock_key: OptimismIcon.unlockKey,
-  },
-  smol: {
-    accentColor: 'smolPurple',
-    key: SmolIcon.key,
-    name: 'SMOL',
-    source: AppIconSmol,
-    unlock_key: SmolIcon.unlockKey,
-  },
-  zora: {
-    accentColor: 'rainbowBlue',
-    key: ZoraIcon.key,
-    name: 'Zora',
-    source: AppIconZora,
-    unlock_key: ZoraIcon.unlockKey,
-  },
-  golddoge: {
-    accentColor: 'dogeGold',
-    key: GoldDogeIcon.key,
-    name: 'GOLDDOGE',
-    source: AppIconGoldDoge,
-    unlock_key: GoldDogeIcon.unlockKey,
-  },
-  raindoge: {
-    accentColor: 'dogeGold',
-    key: RainDogeIcon.key,
-    name: 'RAINDOGE',
-    source: AppIconRainDoge,
-    unlock_key: RainDogeIcon.unlockKey,
-  },
-  pooly: {
-    accentColor: 'poolyPurple',
-    key: PoolyIcon.key,
-    name: 'Rainbow Pooly',
-    source: AppIconPooly,
-    unlock_key: PoolyIcon.unlockKey,
-  },
-  finiliar: {
-    accentColor: 'finiliarPink',
-    key: FiniliarIcon.key,
-    name: 'Rainbow Finiliar',
-    source: AppIconFiniliar,
-    unlock_key: FiniliarIcon.unlockKey,
-  },
-  zorb: {
-    accentColor: 'zorbPink',
-    key: ZorbIcon.key,
-    name: 'Rainbow Zorb Energy',
-    source: AppIconZorb,
-    unlock_key: ZorbIcon.unlockKey,
-  },
-  poolboy: {
-    accentColor: 'poolboyPink',
-    key: PoolboyIcon.key,
-    name: 'Rainbow Poolboy',
-    source: AppIconPoolboy,
-    unlock_key: PoolboyIcon.unlockKey,
-  },
-  adworld: {
-    accentColor: 'adworldRed',
-    key: AdworldIcon.key,
-    name: 'Rainbow World',
-    source: AppIconAdworld,
-    unlock_key: AdworldIcon.unlockKey,
-  },
-  farcaster: {
-    accentColor: 'farcasterPurple',
-    key: FarcasterIcon.key,
-    name: 'Rainbowcast',
-    source: AppIconFarcaster,
-    unlock_key: FarcasterIcon.unlockKey,
-  },
-};
-
-const mmkv = new MMKV();
+import { AppIcon, AppIconKey, UnlockableAppIcon, UnlockableAppIconKey, freeAppIcons, unlockableAppIcons } from '@/appIcons/appIcons';
+import { unlockableAppIconStorage } from '@/featuresToUnlock/unlockableAppIconCheck';
 
 const AppIconSection = () => {
   const { appIcon, settingsChangeAppIcon } = useAccountSettings();
@@ -161,20 +27,25 @@ const AppIconSection = () => {
     [settingsChangeAppIcon]
   );
 
-  const appIconListItemsWithUnlocked = useMemo(() => {
-    // Here we gotta check if each additional icon is unlocked and add it to the list
-    const list = supportedAppIcons;
-    Object.keys(tokenGatedIcons).forEach(key => {
-      const icon = tokenGatedIcons[key];
-      const unlocked = mmkv.getBoolean(icon.unlock_key);
-      Logger.log('checking if unlocked', icon.name, unlocked, icon.unlock_key);
-      if (unlocked) {
-        Logger.log('unlocked', icon.name);
-        list[key] = icon;
-      }
-    });
-    return Object.values(list);
-  }, []);
+  const unlockedAppIcons: Record<AppIconKey, AppIcon> = useMemo(
+    () => ({
+      ...freeAppIcons,
+      ...(Object.keys(unlockableAppIcons) as UnlockableAppIconKey[]).reduce(
+        (unlockedAppIcons, appIconKey) => {
+          const appIcon = unlockableAppIcons[appIconKey];
+          const unlocked = unlockableAppIconStorage.getBoolean(appIconKey);
+          Logger.log('checking if unlocked', appIcon.displayName, unlocked, appIconKey);
+          if (unlocked) {
+            Logger.log('unlocked', appIcon.displayName);
+            unlockedAppIcons[appIconKey] = appIcon;
+          }
+          return unlockedAppIcons;
+        },
+        {} as Record<UnlockableAppIconKey, UnlockableAppIcon>
+      ),
+    }),
+    []
+  );
 
   return (
     <BackgroundProvider color="surfaceSecondary">
@@ -190,7 +61,7 @@ const AppIconSection = () => {
             </Inline>
             <MenuContainer>
               <Menu>
-                {appIconListItemsWithUnlocked.map(({ key, name, accentColor, source }) => (
+                {Object.entries(unlockedAppIcons).map(([key, { displayName, image, accentColor }]) => (
                   <MenuItem
                     key={key}
                     leftComponent={
@@ -205,7 +76,7 @@ const AppIconSection = () => {
                         }}
                       >
                         <ImgixImage
-                          source={source as Source}
+                          source={image as Source}
                           style={{
                             height: 36,
                             width: 36,
@@ -218,7 +89,7 @@ const AppIconSection = () => {
                     onPress={() => onSelectIcon(key)}
                     rightComponent={key === appIcon && <MenuItem.StatusIcon status="selected" />}
                     size={60}
-                    titleComponent={<MenuItem.Title text={name} />}
+                    titleComponent={<MenuItem.Title text={displayName} />}
                   />
                 ))}
               </Menu>
