7c2fde26c998a14b5ad065bde1a9b1ff11cb3286
jinchung <jin.chung17@gmail.com>
2024-03-07 00:08:08 -0500

    Disable and remove Flipper which is officially deprecated

--- android/app/src/main/java/me/rainbow/MainApplication.java
+++ android/app/src/main/java/me/rainbow/MainApplication.java
@@ -101,40 +101,9 @@ public class MainApplication extends Application implements ReactApplication {
     RNBranchModule.enableLogging();
     
     RNBranchModule.getAutoInstance(this);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
    public static Context getAppContext() {
         return MainApplication.context;
     }
-
- /**
-  * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-  * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-  *
-  * @param context
-  * @param reactInstanceManager
-  */
-  private static void initializeFlipper(Context context,
-                                       ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("me.rainbow.ReactNativeFlipper");
-        aClass.getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-              .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
-    }
-  }
 }
