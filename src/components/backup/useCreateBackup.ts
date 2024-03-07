d00ed23200b829b1424eb90deaa014e47718076e
Matthew Wall <matthew.wallt@gmail.com>
2024-03-07 10:48:38 -0500

    return success

--- src/components/backup/useCreateBackup.ts
+++ src/components/backup/useCreateBackup.ts
@@ -134,9 +134,10 @@ export const useCreateBackup = ({ walletId }: UseCreateBackupProps) => {
       const password = await getPassword();
       if (password) {
         onConfirmBackup(password, type);
-        return;
+        return true;
       }
       setLoadingStateWithTimeout('error');
+      return false;
     },
     [getPassword, onConfirmBackup, setLoadingStateWithTimeout]
   );
