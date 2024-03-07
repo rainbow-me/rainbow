1308b32d9dd3800246d42ea4c4fc2a19db2a89e1
Derek <dereknelsony0@gmail.com>
2024-03-07 16:36:34 -0500

    bumped viem & used it for blast chain

--- src/networks/blast.ts
+++ src/networks/blast.ts
@@ -1,50 +1,31 @@
 import { getProviderForNetwork, proxyRpcEndpoint } from '@/handlers/web3';
 import { Network, NetworkProperties } from './types';
 import { gasUtils } from '@/utils';
-import type { Chain } from '@wagmi/chains';
-// import { blast } from '@wagmi/chains';
-import { getAvalancheGasPrices, getBlastGasPrices } from '@/redux/gas';
+import { blast } from 'viem/chains';
+import { getBlastGasPrices } from '@/redux/gas';
 import { getRemoteConfig } from '@/model/remoteConfig';
 import { BLAST_MAINNET_RPC } from 'react-native-dotenv';
 import { BLAST_ETH_ADDRESS } from '@/references';
 
 const BLAST_CHAIN_ID = 81457;
-export const chainBlast: Chain = {
-  id: BLAST_CHAIN_ID,
-  name: 'Blast',
-  network: 'blast',
-  rpcUrls: {
-    public: { http: [BLAST_MAINNET_RPC as string] },
-    default: {
-      http: [BLAST_MAINNET_RPC as string],
-    },
-  },
-  blockExplorers: {
-    default: { name: 'Blastscan', url: 'https://blastscan.io/' },
-  },
-  nativeCurrency: {
-    name: 'Blast',
-    symbol: 'BLAST',
-    decimals: 18,
-  },
-};
 
 export const getBlastNetworkObject = (): NetworkProperties => {
   const { blast_enabled, blast_tx_enabled } = getRemoteConfig();
   return {
     // where wagmi chain data usually is
-    ...chainBlast,
+    ...blast,
 
     // network related data
     enabled: blast_enabled,
     name: 'Blast',
+    network: 'blast',
     longName: 'Blast',
     value: Network.blast,
     networkType: 'layer2',
     blockTimeInMs: 5_000,
 
     nativeCurrency: {
-      ...chainBlast.nativeCurrency,
+      ...blast.nativeCurrency,
       address: BLAST_ETH_ADDRESS,
     },
 
@@ -85,6 +66,13 @@ export const getBlastNetworkObject = (): NetworkProperties => {
     colors: {
       light: '#25292E',
       dark: '#FCFC03',
+    },
+
+    rpcUrls: {
+      public: { http: [BLAST_MAINNET_RPC as string] },
+      default: {
+        http: [BLAST_MAINNET_RPC as string],
+      },
     },
   };
 };
