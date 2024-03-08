fa1dcd5319d0fe6f50d8d799e8e2aead230c9b2c
Derek <dereknelsony0@gmail.com>
2024-03-08 12:50:12 -0500

    favorites query migration

--- src/migrations/migrations/migratePersistedQueriesToMMKV.ts
+++ src/migrations/migrations/migratePersistedQueriesToMMKV.ts
@@ -1,13 +1,15 @@
 import { Migration, MigrationName } from '@/migrations/types';
 import { queryClient } from '@/react-query';
+import { favoritesQueryKey } from '@/resources/favorites';
 import { queryStorage } from '@/storage/legacy';
 
 export function migratePersistedQueriesToMMKV(): Migration {
   return {
     name: MigrationName.migratePersistedQueriesToMMKV,
-    async defer() {
-      // query storage will retreive the whole query state from asyncstorage and replace it in MMKV
-      await queryStorage.get(['rainbow.react-query']);
+    async migrate() {
+      const queryState = await queryStorage.get(['rainbow.react-query']);
+      const favoritesState = queryState?.clientState?.queries[favoritesQueryKey[1]];
+      await queryClient.setQueryData(favoritesQueryKey, favoritesState);
       return;
     },
   };
