aa3d1e728c25bff5ac140fb9d8924832590acd5f
Ben Goldberg <bengoldberg@rainbow.me>
2024-03-08 09:55:11 -0800

    some progress on dapp listings ui

--- src/screens/dapp-browser/DappBrowserScreen.tsx
+++ src/screens/dapp-browser/DappBrowserScreen.tsx
@@ -1,24 +1,85 @@
 import { ButtonPressAnimation } from '@/components/animations';
 import { Page } from '@/components/layout';
-import { Box, Text } from '@/design-system';
+import { Bleed, Box, Inline, Inset, Stack, Text } from '@/design-system';
 import { useNavigation } from '@/navigation';
+import { getHeaderHeight } from '@/navigation/SwipeNavigator';
+import { safeAreaInsetValues } from '@/utils';
 import React from 'react';
+import { ScrollView } from 'react-native';
+
+const Card = () => {
+  return (
+    <ButtonPressAnimation>
+      <Box
+        borderRadius={24}
+        shadow="18px"
+        background="surfacePrimary"
+        width={{ custom: 166.5 }}
+        height={{ custom: 137 }}
+        padding="20px"
+        justifyContent="space-between"
+        style={{ borderWidth: 1.33, borderColor: 'rgba(255, 255, 255, 0.12)' }}
+      >
+        <Box
+          background="blue"
+          shadow="18px"
+          width={{ custom: 48 }}
+          height={{ custom: 48 }}
+          top={{ custom: -8 }}
+          left={{ custom: -8 }}
+          borderRadius={12}
+        />
+        <Stack space="10px">
+          <Text size="17pt" weight="heavy" color="label">
+            Rainbowcast
+          </Text>
+          <Text size="13pt" weight="bold" color="labelTertiary">
+            zora.co
+          </Text>
+        </Stack>
+      </Box>
+    </ButtonPressAnimation>
+  );
+};
 
 export default function DappBrowserScreen() {
   const { navigate } = useNavigation();
 
   return (
-    <Box as={Page} flex={1} height="full" width="full" alignItems="center" justifyContent="center">
-      <ButtonPressAnimation
-        onPress={() => {
-          // navigate(Routes.DAPP_BROWSER);
+    <Box as={Page} flex={1} height="full" width="full" justifyContent="center">
+      <ScrollView
+        scrollIndicatorInsets={{
+          bottom: getHeaderHeight() - safeAreaInsetValues.bottom,
         }}
-        style={{ backgroundColor: 'blue', width: 100, height: 50, alignItems: 'center', justifyContent: 'center' }}
+        contentContainerStyle={{
+          paddingBottom: getHeaderHeight() + 32,
+          paddingTop: 100, // change this
+          paddingHorizontal: 24,
+        }}
+        showsVerticalScrollIndicator
       >
-        <Text size="20pt" color="label">
-          Dapp Browser
-        </Text>
-      </ButtonPressAnimation>
+        <Stack space="20px">
+          <Inline alignVertical="center" space="6px">
+            <Text color="red" size="15pt" align="center" weight="heavy">
+              􀙭
+            </Text>
+            <Text color="label" size="20pt" weight="heavy">
+              Trending
+            </Text>
+          </Inline>
+          <Bleed space="20px">
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <Inset space="20px">
+                <Inline space="12px">
+                  <Card />
+                  <Card />
+                  <Card />
+                </Inline>
+              </Inset>
+            </ScrollView>
+          </Bleed>
+        </Stack>
+      </ScrollView>
     </Box>
   );
 }
