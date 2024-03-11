07242ba80bbd501218d12d3cfa4ef2aad55e873d
Derek <dereknelsony0@gmail.com>
2024-03-11 12:43:35 -0400

    migrate data from async storage

--- src/migrations/migrations/migratePersistedQueriesToMMKV.ts
+++ src/migrations/migrations/migratePersistedQueriesToMMKV.ts
@@ -2,12 +2,13 @@ import { Migration, MigrationName } from '@/migrations/types';
 import { queryClient } from '@/react-query';
 import { favoritesQueryKey } from '@/resources/favorites';
 import { queryStorage } from '@/storage/legacy';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function migratePersistedQueriesToMMKV(): Migration {
   return {
     name: MigrationName.migratePersistedQueriesToMMKV,
     async migrate() {
-      const queryState = JSON.parse(await queryStorage.get(['rainbow.react-query']));
+      const queryState = JSON.parse((await AsyncStorage.getItem('rainbow.react-query')) || '');
       const favoritesState = queryState?.clientState?.queries?.find?.(
         (query: { queryKey: string[] }) => query?.queryKey[1] == favoritesQueryKey[1]
       );
