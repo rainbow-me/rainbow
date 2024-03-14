2cd3ca55ac6923816068895f927dea17581625de
Skylar Barrera <skylar.barrera@gmail.com>
2024-03-14 14:40:53 -0400

    dapp browser: address bar (#5506)
    * clean up address bar to spec
    
    * is loading indicator rendering
    
    * mr. clean

--- src/components/DappBrowser/BrowserTab.tsx
+++ src/components/DappBrowser/BrowserTab.tsx
@@ -483,7 +483,7 @@ export const BrowserTab = React.memo(function BrowserTab({ tabIndex, injectedJS 
           </AnimatePresence>
         </Animated.View>
       </TouchableWithoutFeedback>
-      {isActiveTab && <Box as={Animated.View} background="blue" style={[styles.progressBar, progressBarStyle]} />}
+      {isActiveTab && !tabViewVisible && <Box as={Animated.View} background="blue" style={[styles.progressBar, progressBarStyle]} />}
     </>
   );
 });
