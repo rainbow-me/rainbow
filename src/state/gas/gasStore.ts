fa109b9dc01c1a2b82e96c4d5b89f79bf4c334c3
Derek <dereknelsony0@gmail.com>
2024-03-20 12:20:55 -0400

    yall mind if i open a gas store?

---
+++ src/state/gas/gasStore.ts
@@ -0,0 +1,55 @@
+import create from 'zustand';
+
+// todo - absolute imports
+import { GasFeeLegacyParams, GasFeeLegacyParamsBySpeed, GasFeeParams, GasFeeParamsBySpeed, GasSpeed } from '../../__swaps__/types/gas';
+
+import { createStore } from '../internal/createStore';
+
+export interface GasStore {
+  selectedGas: GasFeeParams | GasFeeLegacyParams;
+  gasFeeParamsBySpeed: GasFeeParamsBySpeed | GasFeeLegacyParamsBySpeed;
+  customGasModified: boolean;
+  setCustomSpeed: (speed: GasFeeParams) => void;
+  setSelectedGas: ({ selectedGas }: { selectedGas: GasFeeParams | GasFeeLegacyParams }) => void;
+  setGasFeeParamsBySpeed: ({ gasFeeParamsBySpeed }: { gasFeeParamsBySpeed: GasFeeParamsBySpeed | GasFeeLegacyParamsBySpeed }) => void;
+  clearCustomGasModified: () => void;
+}
+
+export const gasStore = createStore<GasStore>(
+  (set, get) => ({
+    selectedGas: {} as GasFeeParams | GasFeeLegacyParams,
+    gasFeeParamsBySpeed: {} as GasFeeParamsBySpeed | GasFeeLegacyParamsBySpeed,
+    customGasModified: false,
+    setSelectedGas: ({ selectedGas }) => {
+      set({
+        selectedGas,
+      });
+    },
+    setGasFeeParamsBySpeed: ({ gasFeeParamsBySpeed }) => {
+      set({
+        gasFeeParamsBySpeed,
+      });
+    },
+    setCustomSpeed: (speed: GasFeeParams) => {
+      const { gasFeeParamsBySpeed } = get();
+      set({
+        gasFeeParamsBySpeed: {
+          ...gasFeeParamsBySpeed,
+          [GasSpeed.CUSTOM]: speed,
+        } as GasFeeParamsBySpeed,
+        customGasModified: true,
+      });
+    },
+    clearCustomGasModified: () => {
+      set({ customGasModified: false });
+    },
+  }),
+  {
+    persist: {
+      name: 'gas',
+      version: 0,
+    },
+  }
+);
+
+export const useGasStore = create(gasStore);
