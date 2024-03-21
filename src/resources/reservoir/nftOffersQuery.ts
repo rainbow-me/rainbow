93285d3c6819ec486beb2b9b8ee7ef74be23f2fc
Matthew Wall <matthew.wallt@gmail.com>
2024-03-21 15:45:13 -0400

    undo commit to nfts query

--- src/resources/reservoir/nftOffersQuery.ts
+++ src/resources/reservoir/nftOffersQuery.ts
@@ -92,31 +92,31 @@ export function useNFTOffers({ walletAddress }: { walletAddress: string }) {
 
   const sortedOffers = sortNftOffers(query.data?.nftOffers || [], sortCriterion);
 
-  // useEffect(() => {
-  //   const nftOffers = query.data?.nftOffers ?? [];
-  //   const totalUSDValue = nftOffers.reduce((acc: number, offer: NftOffer) => acc + offer.grossAmount.usd, 0);
-  //   const offerVarianceArray = nftOffers.map(offer => offer.floorDifferencePercentage / 100);
-  //   offerVarianceArray.sort((a, b) => a - b);
+  useEffect(() => {
+    const nftOffers = query.data?.nftOffers ?? [];
+    const totalUSDValue = nftOffers.reduce((acc: number, offer: NftOffer) => acc + offer.grossAmount.usd, 0);
+    const offerVarianceArray = nftOffers.map(offer => offer.floorDifferencePercentage / 100);
+    offerVarianceArray.sort((a, b) => a - b);
 
-  //   // calculate median offer variance
-  //   const middleIndex = Math.floor(offerVarianceArray.length / 2);
-  //   let medianVariance;
-  //   if (offerVarianceArray.length % 2 === 0) {
-  //     medianVariance = (offerVarianceArray[middleIndex - 1] + offerVarianceArray[middleIndex]) / 2;
-  //   } else {
-  //     medianVariance = offerVarianceArray[middleIndex];
-  //   }
+    // calculate median offer variance
+    const middleIndex = Math.floor(offerVarianceArray.length / 2);
+    let medianVariance;
+    if (offerVarianceArray.length % 2 === 0) {
+      medianVariance = (offerVarianceArray[middleIndex - 1] + offerVarianceArray[middleIndex]) / 2;
+    } else {
+      medianVariance = offerVarianceArray[middleIndex];
+    }
 
-  //   // calculate mean offer variance
-  //   const meanVariance = offerVarianceArray.reduce((acc, cur) => acc + cur, 0) / offerVarianceArray.length;
+    // calculate mean offer variance
+    const meanVariance = offerVarianceArray.reduce((acc, cur) => acc + cur, 0) / offerVarianceArray.length;
 
-  //   analyticsV2.identify({
-  //     nftOffersAmount: nftOffers.length,
-  //     nftOffersUSDValue: totalUSDValue,
-  //     nftOffersMedianOfferVariance: medianVariance,
-  //     nftOffersMeanOfferVariance: meanVariance,
-  //   });
-  // }, [query.data?.nftOffers]);
+    analyticsV2.identify({
+      nftOffersAmount: nftOffers.length,
+      nftOffersUSDValue: totalUSDValue,
+      nftOffersMedianOfferVariance: medianVariance,
+      nftOffersMeanOfferVariance: meanVariance,
+    });
+  }, [query.data?.nftOffers]);
 
   // every 1 min check for invalid offers and remove them from the cache
   useEffect(() => {
