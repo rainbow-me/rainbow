e603f95eb92bcae887718961b49434b52dc8fcf5
Matthew Wall <matthew.wallt@gmail.com>
2024-03-26 11:31:23 -0600

    [CHORE]: Remove Apollo client in favor of `@/graphql` (#5555)
    * rm apollo
    
    * rm apollo dir
    
    * remove apollo client dep and tsconfig paths

--- src/apollo/queries.ts
+++
@@ -1,9 +0,0 @@
-import gql from 'graphql-tag';
-
-export const CONTRACT_FUNCTION = gql`
-  query contractFunction($chainID: Int!, $hex: String!) {
-    contractFunction(chainID: $chainID, hex: $hex) {
-      text
-    }
-  }
-`;
