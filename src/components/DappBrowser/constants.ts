66b3ae78ebb38e2924dd2a2fb5951b29fd5e2c7d
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2024-03-26 15:34:52 -0400

    Add browser utils, constants, consoleLogWorklet util

---
+++ src/components/DappBrowser/constants.ts
@@ -0,0 +1,3 @@
+export const GOOGLE_SEARCH_URL = 'https://www.google.com/search?q=';
+export const HTTP = 'http://';
+export const HTTPS = 'https://';
---
+++ src/components/DappBrowser/utils.ts
@@ -0,0 +1,33 @@
+import { Share } from 'react-native';
+import { HTTP, HTTPS } from './constants';
+
+// ---------------------------------------------------------------------------- //
+// URL validation regex breakdown here: https://mathiasbynens.be/demo/url-regex
+//
+// This is the @diegoperini version, which is a bit long but accurate
+// Details on its validation logic: https://gist.github.com/dperini/729294
+// ---------------------------------------------------------------------------- //
+const urlPattern =
+  /^(?:(?:(?:https?):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
+export function isValidURL(url: string): boolean {
+  let urlForValidation = url.trim();
+  if (!urlForValidation.startsWith(HTTP) && !urlForValidation.startsWith(HTTPS)) {
+    urlForValidation = HTTPS + urlForValidation;
+  }
+  return urlPattern.test(urlForValidation);
+}
+
+export const generateUniqueId = (): string => {
+  const timestamp = Date.now().toString(36);
+  const randomString = Math.random().toString(36).slice(2, 7);
+  return `${timestamp}${randomString}`;
+};
+
+export async function handleShareUrl(url: string): Promise<void> {
+  try {
+    await Share.share({ message: url });
+  } catch (error) {
+    console.error('Error sharing browser URL', error);
+  }
+}
