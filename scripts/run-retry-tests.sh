8912eedc3e1bdeabb0e8cf92144adc136a6fb945
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2024-03-27 15:54:35 -0500

    test

--- scripts/run-retry-tests.sh
+++ scripts/run-retry-tests.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-max_retries=$1
+max_retries="$1"
 count=0
 
-until [ $count -ge $max_retries ]
+until (( count >= max_retries ))
 do
-  ./node_modules/.bin/detox test -R 5 --configuration ios.sim.release --forceExit --bail && exit 0
-  count=$[$count+1]
+  ./node_modules/.bin/detox test -R 5 --configuration ios.sim.release --forceExit --bail
+  ret_val=$?
+  if [ $ret_val -eq 0 ]; then
+    exit 0
+  fi
+  ((count++))
   echo "Test failed, attempt $count/$max_retries..."
 done
 
