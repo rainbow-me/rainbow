7905f5fa5635148ffeca3ef9ce3d716b4469f829
Derek <dereknelsony0@gmail.com>
2024-04-04 16:32:54 -0400

    added warning for unknown price impact

--- src/components/exchange/PriceImpactWarning.tsx
+++ src/components/exchange/PriceImpactWarning.tsx
@@ -25,6 +25,8 @@ export default function PriceImpactWarning({
   ...props
 }: PriceImpactWarningProps) {
   const headingValue = priceImpactNativeAmount ?? priceImpactPercentDisplay;
+  const hasPriceData = priceImpactPercentDisplay !== '100.00%';
+  const impactMsg = !hasPriceData ? 'exchange.price_impact.no_data' : 'exchange.price_impact.small_market';
   return (
     <ColorModeProvider value="dark">
       <Animated.View {...props} style={[style, position.coverAsObject]}>
@@ -34,16 +36,20 @@ export default function PriceImpactWarning({
               <Inline alignHorizontal="center">
                 <Text weight="bold" size="17pt" color={{ custom: priceImpactColor }}>{`􀇿 `}</Text>
                 <Text weight="bold" size="17pt" color="primary (Deprecated)">
-                  {lang.t('exchange.price_impact.small_market')}
+                  {lang.t(impactMsg)}
                 </Text>
-                <Text
-                  weight="bold"
-                  size="17pt"
-                  color={{ custom: priceImpactColor }}
-                >{` • ${lang.t('exchange.price_impact.losing_prefix')} `}</Text>
-                <Text weight="bold" size="17pt" color={{ custom: priceImpactColor }}>
-                  {headingValue}
-                </Text>
+                {hasPriceData && (
+                  <Text
+                    weight="bold"
+                    size="17pt"
+                    color={{ custom: priceImpactColor }}
+                  >{` • ${lang.t('exchange.price_impact.losing_prefix')} `}</Text>
+                )}
+                {hasPriceData && (
+                  <Text weight="bold" size="17pt" color={{ custom: priceImpactColor }}>
+                    {headingValue}
+                  </Text>
+                )}
               </Inline>
             </Box>
           </ButtonPressAnimation>
