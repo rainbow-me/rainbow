2741a95ac55a26cd3bc5608a29d6a900bf7c826a
Derek <dereknelsony0@gmail.com>
2024-04-09 11:42:28 -0400

    more concise price impact warning

--- src/components/exchange/PriceImpactWarning.tsx
+++ src/components/exchange/PriceImpactWarning.tsx
@@ -12,8 +12,11 @@ interface PriceImpactWarningProps extends ViewProps {
   priceImpactColor?: string;
   priceImpactNativeAmount?: string | null;
   priceImpactPercentDisplay?: string | null;
+  outputCurrencySymbol?: string | null;
   style?: StyleProp<ViewStyle>;
 }
+
+const NO_PRICE_DATA_PERCENTAGE = '100.00%';
 
 export default function PriceImpactWarning({
   onPress,
@@ -21,22 +24,25 @@ export default function PriceImpactWarning({
   priceImpactColor = 'primary',
   priceImpactNativeAmount,
   priceImpactPercentDisplay,
+  outputCurrencySymbol,
   style,
   ...props
 }: PriceImpactWarningProps) {
   const headingValue = priceImpactNativeAmount ?? priceImpactPercentDisplay;
-  const hasPriceData = priceImpactPercentDisplay !== '100.00%';
-  const impactMsg = !hasPriceData ? 'exchange.price_impact.no_data' : 'exchange.price_impact.small_market';
+  const hasPriceData = priceImpactPercentDisplay !== NO_PRICE_DATA_PERCENTAGE;
+  const impactMsg = !hasPriceData
+    ? `${outputCurrencySymbol} ${lang.t('exchange.price_impact.no_data')}`
+    : lang.t('exchange.price_impact.small_market');
   return (
     <ColorModeProvider value="dark">
       <Animated.View {...props} style={[style, position.coverAsObject]}>
-        {isHighPriceImpact && headingValue && (
+        {!isHighPriceImpact && headingValue && (
           <ButtonPressAnimation onPress={onPress} scaleTo={0.94}>
             <Box paddingHorizontal="19px (Deprecated)" paddingTop="19px (Deprecated)">
               <Inline alignHorizontal="center">
                 <Text weight="bold" size="17pt" color={{ custom: priceImpactColor }}>{`􀇿 `}</Text>
                 <Text weight="bold" size="17pt" color="primary (Deprecated)">
-                  {lang.t(impactMsg)}
+                  {impactMsg}
                 </Text>
                 {hasPriceData && (
                   <Text
