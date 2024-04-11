f73d37b28457702b514b0d3f4e461bb304621f1c
Ben Goldberg <bengoldberg@rainbow.me>
2024-04-11 10:35:59 -0700

    Dapp Browser: Search (#5617)

--- src/components/DappBrowser/search-input/SearchInput.tsx
+++ src/components/DappBrowser/search-input/SearchInput.tsx
@@ -4,7 +4,7 @@ import { AnimatedText, Box, Cover, globalColors, useColorMode, useForegroundColo
 import Animated, { SharedValue, useAnimatedStyle, useDerivedValue, withSpring, withTiming } from 'react-native-reanimated';
 import Input from '@/components/inputs/Input';
 import * as i18n from '@/languages';
-import { NativeSyntheticEvent, StyleSheet, TextInput, TextInputFocusEventData, TextInputSubmitEditingEventData } from 'react-native';
+import { NativeSyntheticEvent, StyleSheet, TextInput, TextInputChangeEventData, TextInputSubmitEditingEventData } from 'react-native';
 import { ToolbarIcon } from '../ToolbarIcon';
 import { IS_IOS } from '@/env';
 import { FadeMask } from '@/__swaps__/screens/Swap/components/FadeMask';
@@ -29,6 +29,7 @@ export const SearchInput = ({
   inputRef,
   formattedInputValue,
   inputValue,
+  searchValue,
   isGoogleSearch,
   isHome,
   onPressWorklet,
@@ -39,20 +40,23 @@ export const SearchInput = ({
   logoUrl,
   canGoBack,
   canGoForward,
+  onChange,
 }: {
   inputRef: RefObject<TextInput>;
   formattedInputValue: { url: string; tabIndex: number };
   inputValue: string | undefined;
+  searchValue: string;
   isGoogleSearch: boolean;
   isHome: boolean;
   onPressWorklet: () => void;
-  onBlur: (event: NativeSyntheticEvent<TextInputFocusEventData>) => void;
+  onBlur: () => void;
   onSubmitEditing: (event: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => void;
   isFocused: boolean;
   isFocusedValue: SharedValue<boolean>;
   logoUrl: string | undefined | null;
   canGoBack: boolean;
   canGoForward: boolean;
+  onChange: (event: NativeSyntheticEvent<TextInputChangeEventData>) => void;
 }) => {
   const { animatedActiveTabIndex, goBack, goForward, onRefresh, tabViewProgress } = useBrowserContext();
   const { isFavorite, addFavorite, removeFavorite } = useFavoriteDappsStore();
@@ -223,6 +227,7 @@ export const SearchInput = ({
               placeholder={i18n.t(i18n.l.dapp_browser.address_bar.input_placeholder)}
               placeholderTextColor={labelQuaternary}
               onBlur={onBlur}
+              onChange={onChange}
               onSubmitEditing={onSubmitEditing}
               ref={inputRef}
               returnKeyType="go"
@@ -237,6 +242,7 @@ export const SearchInput = ({
               ]}
               textAlign="left"
               textAlignVertical="center"
+              value={searchValue}
               defaultValue={inputValue}
             />
             <Cover alignHorizontal="center" alignVertical="center" pointerEvents="none">
--- src/components/DappBrowser/utils.ts
+++ src/components/DappBrowser/utils.ts
@@ -31,6 +31,26 @@ export const normalizeUrl = (url: string): string => {
   return url;
 };
 
+export const formatUrl = (url: string): string => {
+  let formattedValue = '';
+  let isGoogleSearch = false;
+  try {
+    const { hostname, pathname, search } = new URL(url);
+    isGoogleSearch = hostname === 'www.google.com' && pathname === '/search';
+    if (isGoogleSearch) {
+      const params = new URLSearchParams(search);
+      formattedValue = params.get('q') || '';
+    } else {
+      formattedValue = hostname.startsWith('www.') ? hostname.slice(4) : hostname;
+    }
+  } catch {
+    if (!isGoogleSearch) {
+      formattedValue = url;
+    }
+  }
+  return formattedValue;
+};
+
 export const generateUniqueId = (): string => {
   const timestamp = Date.now().toString(36);
   const randomString = Math.random().toString(36).slice(2, 7);
