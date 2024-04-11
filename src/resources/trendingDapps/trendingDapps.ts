dd63d7931239231171358bdd16e5548a1cae8ee1
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2024-04-11 03:31:47 -0400

    Homepage UI improvements, adjust placeholders

--- src/resources/trendingDapps/trendingDapps.ts
+++ src/resources/trendingDapps/trendingDapps.ts
@@ -7,6 +7,30 @@ export type TrendingSite = {
 
 export const trendingDapps: TrendingSite[] = [
   {
+    name: 'Zora',
+    url: 'zora.co',
+    image: 'https://rainbowme-res.cloudinary.com/image/upload/v1692144154/dapps/ingested_zora.co.png',
+    screenshot: 'https://rainbowme-res.cloudinary.com/image/upload/v1692144154/dapps/ingested_zora.co.png',
+  },
+  {
+    name: 'Party',
+    url: 'party.app',
+    image: 'https://rainbowme-res.cloudinary.com/image/upload/v1694735726/dapps/Party.app.png',
+    screenshot: 'https://rainbowme-res.cloudinary.com/image/upload/v1694735726/dapps/Party.app.png',
+  },
+  {
+    name: 'BasePaint',
+    url: 'basepaint.xyz',
+    image: 'https://pbs.twimg.com/profile_images/1704878803611447296/6HniqSfx_400x400.jpg',
+    screenshot: 'https://i.seadn.io/s/raw/files/7c38b0c9b5903d08817102466e9fff27.png?auto=format&dpr=1&w=600',
+  },
+  {
+    name: 'Uniswap',
+    url: 'app.uniswap.org',
+    image: 'https://rainbowme-res.cloudinary.com/image/upload/v1668565116/dapps/uniswap.org.png',
+    screenshot: 'https://rainbowme-res.cloudinary.com/image/upload/v1668565116/dapps/uniswap.org.png',
+  },
+  {
     name: 'Aave',
     url: 'app.aave.com',
     image: 'https://rainbowme-res.cloudinary.com/image/upload/v1668565116/dapps/aave.com.png',
@@ -23,23 +47,5 @@ export const trendingDapps: TrendingSite[] = [
     url: 'opensea.io',
     image: 'https://rainbowme-res.cloudinary.com/image/upload/v1688068601/dapps/ingested_opensea.io.png',
     screenshot: 'https://rainbowme-res.cloudinary.com/image/upload/v1688068601/dapps/ingested_opensea.io.png',
-  },
-  {
-    name: 'Party',
-    url: 'party.app',
-    image: 'https://rainbowme-res.cloudinary.com/image/upload/v1694735726/dapps/Party.app.png',
-    screenshot: 'https://rainbowme-res.cloudinary.com/image/upload/v1694735726/dapps/Party.app.png',
-  },
-  {
-    name: 'Uniswap',
-    url: 'app.uniswap.org',
-    image: 'https://rainbowme-res.cloudinary.com/image/upload/v1668565116/dapps/uniswap.org.png',
-    screenshot: 'https://rainbowme-res.cloudinary.com/image/upload/v1668565116/dapps/uniswap.org.png',
-  },
-  {
-    name: 'Zora',
-    url: 'zora.co',
-    image: 'https://rainbowme-res.cloudinary.com/image/upload/v1692144154/dapps/ingested_zora.co.png',
-    screenshot: 'https://rainbowme-res.cloudinary.com/image/upload/v1692144154/dapps/ingested_zora.co.png',
   },
 ];
