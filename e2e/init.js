4ddcfbee18807b48570c000a155fe35a8dca4da7
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2024-04-16 13:44:56 -0500

    .

--- e2e/init.js
+++ e2e/init.js
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { exec } from 'child_process';
+// import { exec } from 'child_process';
 import { device } from 'detox';
 
 require('dotenv').config({ path: '.env' });
 
 beforeAll(async () => {
-  if (device.getPlatform() === 'android') {
-    // connecting to metro
-    await device.reverseTcpPort(8081);
-    // connecting to hardhat
-    await device.reverseTcpPort(8545); // TODO: WIP for android connecting in dev
+  // if (device.getPlatform() === 'android') {
+  //   // connecting to metro
+  //   await device.reverseTcpPort(8081);
+  //   // connecting to hardhat
+  //   await device.reverseTcpPort(8545); // TODO: WIP for android connecting in dev
 
-    // make sure we don't have gesture navigation what might cause collisions
-    exec('yarn adb-all shell cmd overlay enable com.android.internal.systemui.navbar.threebutton');
-  }
+  //   // make sure we don't have gesture navigation what might cause collisions
+  //   exec('yarn adb-all shell cmd overlay enable com.android.internal.systemui.navbar.threebutton');
+  // }
   await device.clearKeychain();
   await device.launchApp({ newInstance: true, delete: true });
   await device.setURLBlacklist([
