503ae90d44d69c2b610210a571078a05ae607eab
Bruno Barbieri <brunobar79@gmail.com>
2024-04-23 16:39:52 -0400

    delay tab rendering till injectedJS is in memory

--- src/components/DappBrowser/BrowserTab.tsx
+++ src/components/DappBrowser/BrowserTab.tsx
@@ -693,7 +693,7 @@ export const BrowserTab = React.memo(
                 <Freeze freeze={!isActiveTab}>
                   <DappBrowserWebview
                     webviewDebuggingEnabled={IS_DEV}
-                    injectedJavaScriptBeforeContentLoaded={injectedJS.current || ''}
+                    injectedJavaScriptBeforeContentLoaded={injectedJS || ''}
                     allowsInlineMediaPlayback
                     fraudulentWebsiteWarningEnabled
                     allowsBackForwardNavigationGestures
