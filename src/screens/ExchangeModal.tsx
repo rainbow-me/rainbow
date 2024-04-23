96037a1b378b0c9ac2d27317804ee0e2dea4cd87
Matthew Wall <matthew.wallt@gmail.com>
2024-04-22 22:16:13 -0400

    fix lint

--- src/screens/ExchangeModal.tsx
+++ src/screens/ExchangeModal.tsx
@@ -70,7 +70,7 @@ import { handleReviewPromptAction } from '@/utils/reviewAlert';
 import { ReviewPromptAction } from '@/storage/schema';
 import { SwapPriceImpactType } from '@/hooks/usePriceImpactDetails';
 import { getNextNonce } from '@/state/nonces';
-import { getChainName } from '@/__swaps__/screens/Swap/utils/chains';
+import { getChainName } from '@/__swaps__/utils/chains';
 import { ChainName } from '@/__swaps__/types/chains';
 import { AddressOrEth, ParsedAsset } from '@/__swaps__/types/assets';
 import { TokenColors } from '@/graphql/__generated__/metadata';
