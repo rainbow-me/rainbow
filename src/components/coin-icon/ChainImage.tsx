bae3ece21961559772c586da6c12544e483d9d64
skylarbarrera <skylar.barrera@gmail.com>
2024-05-03 12:22:45 -0400

    fix badge imports

--- src/components/coin-icon/ChainImage.tsx
+++ src/components/coin-icon/ChainImage.tsx
@@ -4,16 +4,16 @@ import { Image, ImageSourcePropType } from 'react-native';
 import { IS_IOS } from '@/env';
 import { Network } from '@/helpers';
 
-import ArbitrumBadge from '../../assets/badges/arbitrum.png';
-import BaseBadge from '../../assets/badges/base.png';
-import BscBadge from '../../assets/badges/bsc.png';
-import EthereumBadge from '../../assets/badges/ethereum.png';
-import OptimismBadge from '../../assets/badges/optimism.png';
-import PolygonBadge from '../../assets/badges/polygon.png';
-import ZoraBadge from '../../assets/badges/zora.png';
-import AvalancheBadge from '@/assets/badges/avalancheBadge.png';
-import BlastBadge from '@/assets/badges/blastBadge.png';
-import DegenBadge from '@/assets/badges/degenBadge.png';
+import ArbitrumBadge from '@/assets/badges/arbitrum.png';
+import BaseBadge from '@/assets/badges/base.png';
+import BscBadge from '@/assets/badges/bsc.png';
+import EthereumBadge from '@/assets/badges/ethereum.png';
+import OptimismBadge from '@/assets/badges/optimism.png';
+import PolygonBadge from '@/assets/badges/polygon.png';
+import ZoraBadge from '@/assets/badges/zora.png';
+import AvalancheBadge from '@/assets/badges/avalanche.png';
+import BlastBadge from '@/assets/badges/blast.png';
+import DegenBadge from '@/assets/badges/degen.png';
 
 export function ChainImage({ chain, size = 20 }: { chain: Network | null | undefined; size?: number }) {
   const source = useMemo(() => {
