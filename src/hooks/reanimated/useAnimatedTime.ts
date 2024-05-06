3ea6c5cac84f1bbe1485e635fc641f02b24ea220
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2024-05-06 15:20:07 -0400

    Add documentation (#5701)

--- src/hooks/reanimated/useAnimatedTime.ts
+++ src/hooks/reanimated/useAnimatedTime.ts
@@ -2,19 +2,56 @@ import { useCallback, useEffect } from 'react';
 import { Easing, SharedValue, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
 
 interface TimerConfig {
+  /** Whether the timer should start automatically. @default false */
   autoStart?: boolean;
+  /** The duration of the timer in milliseconds. @default 1000 */
   durationMs?: number;
+  /** A worklet function to be called when the timer ends. */
   onEndWorklet?: () => void;
+  /** Whether the timer should repeat after completion. @default false */
   shouldRepeat?: boolean;
 }
 
 interface TimerResult {
+  /** A worklet function that resets the timer. */
   reset: () => void;
+  /** A worklet function that starts the timer. */
   start: () => void;
+  /** A worklet function that stops the timer. */
   stop: () => void;
+  /** A read-only shared value representing the timer clock in seconds. */
   timeInSeconds: Readonly<SharedValue<number>>;
 }
 
+/**
+ * ### useAnimatedTime
+ *
+ * Creates a shared value that represents a timer with a specified duration.
+ * It provides controls to start, stop, and reset the timer, as well as a read-only shared value representing the timer clock in seconds.
+ *
+ * @param config {@link TimerConfig} – Configuration options for the timer:
+ *   - `autoStart` – Whether the timer should start automatically.
+ *   - `durationMs` – The duration of the timer in milliseconds.
+ *   - `onEndWorklet` – A worklet function to be called when the timer ends.
+ *   - `shouldRepeat` – Whether the timer should repeat after completion.
+ *
+ * @returns {TimerResult} {@link TimerResult} – An object containing:
+ *   - `reset` – A worklet function that resets the timer.
+ *   - `start` – A worklet function that starts the timer.
+ *   - `stop` – A worklet function that stops the timer.
+ *   - `timeInSeconds` – A read-only shared value representing the timer clock in seconds.
+ *
+ * @example
+ * const { reset, start, stop, timeInSeconds } = useAnimatedTime({
+ *   autoStart: true,
+ *   durationMs: 3000,
+ *   onEndWorklet: () => {
+ *     'worklet';
+ *     console.log('Timer ended');
+ *   },
+ *   shouldRepeat: true,
+ * });
+ */
 export function useAnimatedTime(config: TimerConfig = {}): TimerResult {
   const { autoStart = false, durationMs = 1000, onEndWorklet, shouldRepeat = false } = config;
 
