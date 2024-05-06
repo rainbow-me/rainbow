a78b736e7ec67f9e92eaf9f85d3e2a0637eb2174
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2024-05-06 17:50:19 +0000

    Add documentation

--- src/hooks/reanimated/useAnimatedInterval.ts
+++ src/hooks/reanimated/useAnimatedInterval.ts
@@ -1,11 +1,37 @@
 import { useAnimatedTime } from './useAnimatedTime';
-
 interface IntervalConfig {
+  /** Whether the interval clock should start automatically. @default true */
   autoStart?: boolean;
+  /** The interval duration in milliseconds. */
   intervalMs: number;
+  /** The worklet function to be executed at each interval. */
   onIntervalWorklet: () => void;
 }
 
+/**
+ * ### useAnimatedInterval
+ *
+ * Creates an interval that repeatedly executes a worklet function at the specified interval.
+ *
+ * @param config - Configuration options for the interval timer:
+ *   - `autoStart` - Whether the interval clock should start automatically. Default is true.
+ *   - `intervalMs` - The interval duration in milliseconds.
+ *   - `onIntervalWorklet` - The worklet function to be executed at each interval.
+ *
+ * @returns An object containing:
+ *   - `reset` - A worklet function to restart the interval clock.
+ *   - `start` - A worklet function to start the interval clock.
+ *   - `stop` - A worklet function to stop the interval clock.
+ *
+ * @example
+ * const { reset } = useAnimatedInterval({
+ *   intervalMs: 10000,
+ *   onIntervalWorklet: () => {
+ *     'worklet';
+ *     console.log('Logging once every 10 seconds');
+ *   },
+ * });
+ */
 export function useAnimatedInterval(config: IntervalConfig) {
   const { autoStart = true, intervalMs, onIntervalWorklet } = config;
 
