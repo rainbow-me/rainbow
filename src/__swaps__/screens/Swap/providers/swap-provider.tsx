5a0e882ee316d0661836af3f28375b8a416c7f34
Matthew Wall <matthew.wallt@gmail.com>
2024-05-16 14:26:53 -0400

    Swap Warning / Price Impact component (#5722)
    * bring back swap warning
    
    * bring back swap warning

--- src/__swaps__/screens/Swap/providers/swap-provider.tsx
+++ src/__swaps__/screens/Swap/providers/swap-provider.tsx
@@ -119,6 +119,9 @@ export const SwapProvider = ({ children }: SwapProviderProps) => {
 
   const SwapWarning = useSwapWarning({
     SwapInputController,
+    inputAsset: internalSelectedInputAsset,
+    outputAsset: internalSelectedOutputAsset,
+    quote,
     sliderXPosition,
     isFetching,
   });
