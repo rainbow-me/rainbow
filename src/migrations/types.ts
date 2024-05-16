04befa60a826c8904954bcd10b892035118ccc5a
Ben Goldberg <bengoldberg@rainbow.me>
2024-05-16 08:14:47 -0700

    migration

--- src/migrations/types.ts
+++ src/migrations/types.ts
@@ -17,6 +17,7 @@ export enum MigrationName {
   migratePinnedAndHiddenTokenUniqueIds = 'migration_migratePinnedAndHiddenTokenUniqueIds',
   migrateUnlockableAppIconStorage = 'migration_migrateUnlockableAppIconStorage',
   migratePersistedQueriesToMMKV = 'migration_migratePersistedQueriesToMMKV',
+  migrateFavoritesToZustand = 'migration_migrateFavoritesToZustand',
 }
 
 export type Migration = {
