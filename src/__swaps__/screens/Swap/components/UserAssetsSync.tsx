f45e15361a89b5effc62f47af7b3fdf51a53cd9d
Matthew Wall <matthew.wallt@gmail.com>
2024-05-23 17:06:29 -0400

    keep inputAsset in sync with user assets in background

--- src/__swaps__/screens/Swap/components/UserAssetsSync.tsx
+++ src/__swaps__/screens/Swap/components/UserAssetsSync.tsx
@@ -5,8 +5,16 @@ import { selectUserAssetsList, selectorFilterByUserChains } from '@/__swaps__/sc
 import { Hex } from 'viem';
 import { userAssetsStore } from '@/state/assets/userAssets';
 import { ParsedSearchAsset } from '@/__swaps__/types/assets';
+import { useRoute } from '@react-navigation/native';
+import Routes from '@/navigation/routesNames';
+import { parseSearchAsset } from '@/__swaps__/utils/assets';
+import { SearchAsset } from '@/__swaps__/types/search';
+import { useSwapContext } from '../providers/swap-provider';
+import { SwapAssetType } from '@/__swaps__/types/swap';
 
 export const UserAssetsSync = () => {
+  const { name } = useRoute();
+  const { setAsset } = useSwapContext();
   const { accountAddress: currentAddress, nativeCurrency: currentCurrency } = useAccountSettings();
 
   // TODO: Should we setAsset here as well?
@@ -27,6 +35,17 @@ export const UserAssetsSync = () => {
           userAssetsById: new Set(data.map(d => d.uniqueId)),
           userAssets: new Map(data.map(d => [d.uniqueId, d as ParsedSearchAsset])),
         });
+
+        if (name !== Routes.SWAP) {
+          const [firstAsset] = data;
+          const parsedAsset = parseSearchAsset({
+            assetWithPrice: undefined,
+            searchAsset: firstAsset as unknown as SearchAsset, // NOTE: We don't really care about this since it's a userAsset
+            userAsset: firstAsset,
+          });
+
+          setAsset({ asset: parsedAsset, type: SwapAssetType.inputAsset });
+        }
       },
       enabled: !!currentAddress,
     }
