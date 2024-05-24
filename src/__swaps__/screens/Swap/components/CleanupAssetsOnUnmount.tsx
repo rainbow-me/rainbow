ba06ff3ec51272bff2a0645d7d36bbbaf2a67835
Matthew Wall <matthew.wallt@gmail.com>
2024-05-24 12:02:32 -0400

    cleanup

--- src/__swaps__/screens/Swap/components/CleanupAssetsOnUnmount.tsx
+++ src/__swaps__/screens/Swap/components/CleanupAssetsOnUnmount.tsx
@@ -1,16 +1,6 @@
 import { useCleanupOnExit } from '../hooks/useCleanupOnExit';
-import { useSwapContext } from '../providers/swap-provider';
 
 export const CleanupAssetsOnUnmount = () => {
-  const { SwapInputsController, inputProgress, outputProgress, internalSelectedInputAsset, internalSelectedOutputAsset } = useSwapContext();
-
-  useCleanupOnExit({
-    SwapInputsController,
-    inputProgress,
-    outputProgress,
-    internalSelectedInputAsset,
-    internalSelectedOutputAsset,
-  });
-
+  useCleanupOnExit();
   return null;
 };
