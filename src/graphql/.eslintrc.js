fa49a3ab87043fa6aed23ffdfe36b504c29a055c
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2024-05-31 19:57:56 -0500

    .

--- src/graphql/.eslintrc.js
+++ src/graphql/.eslintrc.js
@@ -1,26 +1,26 @@
-module.exports = {
-  extends: ['../../.eslintrc.js'],
-  overrides: [
-    {
-      files: ['*.graphql'],
-      parser: '@graphql-eslint/eslint-plugin',
-      plugins: ['@graphql-eslint'],
-      rules: {
-        '@graphql-eslint/description-style': 'error',
-        '@graphql-eslint/naming-convention': [
-          'error',
-          {
-            OperationDefinition: {
-              style: 'camelCase',
-            },
-          },
-        ],
-        '@graphql-eslint/no-case-insensitive-enum-values-duplicates': 'error',
-        '@graphql-eslint/no-hashtag-description': 'error',
-        '@graphql-eslint/no-typename-prefix': 'error',
-        '@graphql-eslint/require-deprecation-reason': 'error',
-        '@graphql-eslint/require-description': ['error', { types: true, DirectiveDefinition: true }],
-      },
-    },
-  ],
-};
+// module.exports = {
+//   extends: ['../../.eslintrc.js'],
+//   overrides: [
+//     {
+//       files: ['*.graphql'],
+//       parser: '@graphql-eslint/eslint-plugin',
+//       plugins: ['@graphql-eslint'],
+//       rules: {
+//         '@graphql-eslint/description-style': 'error',
+//         '@graphql-eslint/naming-convention': [
+//           'error',
+//           {
+//             OperationDefinition: {
+//               style: 'camelCase',
+//             },
+//           },
+//         ],
+//         '@graphql-eslint/no-case-insensitive-enum-values-duplicates': 'error',
+//         '@graphql-eslint/no-hashtag-description': 'error',
+//         '@graphql-eslint/no-typename-prefix': 'error',
+//         '@graphql-eslint/require-deprecation-reason': 'error',
+//         '@graphql-eslint/require-description': ['error', { types: true, DirectiveDefinition: true }],
+//       },
+//     },
+//   ],
+// };
