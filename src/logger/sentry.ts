92753144b60799750b45f3436035c7928b24ca07
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2024-06-06 06:54:14 +0000

    Remove unused routing instrumentation

--- src/logger/sentry.ts
+++ src/logger/sentry.ts
@@ -6,13 +6,6 @@ import { IS_PROD, IS_TEST } from '@/env';
 import { logger, RainbowError } from '@/logger';
 import isTestFlight from '@/helpers/isTestFlight';
 
-/**
- * We need to disable React Navigation instrumentation for E2E tests because
- * detox doesn't like setTimeout calls that are used inside When enabled detox
- * hangs and timeouts on all test cases
- */
-export const sentryRoutingInstrumentation = IS_PROD ? new Sentry.ReactNavigationInstrumentation() : undefined;
-
 export const defaultOptions: Sentry.ReactNativeOptions = {
   attachStacktrace: true,
   defaultIntegrations: false,
