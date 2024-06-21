f367ff92cbbe14e946979473ae318ccf0d17fdeb
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2024-06-21 18:07:27 +0000

    Fix swap button press animations, haptic feedback, review button states

--- src/__swaps__/screens/Swap/components/SwapActionButton.tsx
+++ src/__swaps__/screens/Swap/components/SwapActionButton.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable no-nested-ternary */
 import React from 'react';
-import { StyleProp, StyleSheet, TextStyle, ViewProps, ViewStyle } from 'react-native';
-import Animated, { DerivedValue, useAnimatedProps, useAnimatedStyle, useDerivedValue } from 'react-native-reanimated';
+import { StyleProp, StyleSheet, TextStyle, ViewStyle } from 'react-native';
+import Animated, { DerivedValue, useAnimatedStyle, useDerivedValue } from 'react-native-reanimated';
 
 import { ExtendedAnimatedAssetWithColors } from '@/__swaps__/types/assets';
 import { getColorValueForThemeWorklet } from '@/__swaps__/utils/swaps';
 import { AnimatedText, Box, Column, Columns, globalColors, useColorMode, useForegroundColor } from '@/design-system';
 import { GestureHandlerV1Button } from './GestureHandlerV1Button';
-
-const AnimatedGestureHandlerV1Button = Animated.createAnimatedComponent(GestureHandlerV1Button);
 
 export const SwapActionButton = ({
   asset,
@@ -90,6 +88,12 @@ export const SwapActionButton = ({
     };
   });
 
+  const disabledWrapper = useAnimatedStyle(() => {
+    return {
+      pointerEvents: disabled && disabled?.value ? 'none' : 'auto',
+    };
+  });
+
   const iconValue = useDerivedValue(() => {
     if (typeof icon === 'string') return icon;
     return icon?.value || '';
@@ -104,53 +108,47 @@ export const SwapActionButton = ({
     return rightIcon;
   });
 
-  const buttonAnimatedProps = useAnimatedProps(() => {
-    return {
-      pointerEvents: (disabled?.value ? 'none' : 'box-only') as ViewProps['pointerEvents'],
-      disableButtonPressWrapper: disabled?.value,
-      scaleTo: scaleTo || (hugContent ? undefined : 0.925),
-    };
-  });
-
   return (
-    <AnimatedGestureHandlerV1Button
-      animatedProps={buttonAnimatedProps}
-      onPressWorklet={onPressWorklet}
-      onPressJS={onPressJS}
-      style={[hugContent && feedActionButtonStyles.buttonWrapper, style]}
-    >
-      <Box
-        as={Animated.View}
-        paddingHorizontal={{ custom: small ? 14 : 20 - (outline ? 2 : 0) }}
-        paddingLeft={small && icon ? '10px' : undefined}
-        paddingRight={small && rightIcon ? '10px' : undefined}
-        style={[feedActionButtonStyles.button, outline && feedActionButtonStyles.outlineButton, buttonWrapperStyles]}
+    <Animated.View style={disabledWrapper}>
+      <GestureHandlerV1Button
+        onPressWorklet={onPressWorklet}
+        onPressJS={onPressJS}
+        scaleTo={scaleTo || (hugContent ? undefined : 0.925)}
+        style={[hugContent && feedActionButtonStyles.buttonWrapper, style]}
       >
-        <Columns alignHorizontal="center" alignVertical="center" space="6px">
-          {icon && (
-            <Column width="content">
-              <AnimatedText align="center" size={small ? '15pt' : '17pt'} style={[iconStyle, textStyles]} weight="heavy">
-                {iconValue}
-              </AnimatedText>
-            </Column>
-          )}
-          {typeof label !== 'undefined' && (
-            <Column width="content">
-              <AnimatedText align="center" style={textStyles} numberOfLines={1} size={small ? '17pt' : '20pt'} weight="heavy">
-                {labelValue}
-              </AnimatedText>
-            </Column>
-          )}
-          {rightIcon && (
-            <Column width="content">
-              <AnimatedText align="center" style={[textStyles, secondaryTextStyles]} size={small ? '15pt' : '17pt'} weight="bold">
-                {rightIconValue}
-              </AnimatedText>
-            </Column>
-          )}
-        </Columns>
-      </Box>
-    </AnimatedGestureHandlerV1Button>
+        <Box
+          as={Animated.View}
+          paddingHorizontal={{ custom: small ? 14 : 20 - (outline ? 2 : 0) }}
+          paddingLeft={small && icon ? '10px' : undefined}
+          paddingRight={small && rightIcon ? '10px' : undefined}
+          style={[feedActionButtonStyles.button, outline && feedActionButtonStyles.outlineButton, buttonWrapperStyles]}
+        >
+          <Columns alignHorizontal="center" alignVertical="center" space="6px">
+            {icon && (
+              <Column width="content">
+                <AnimatedText align="center" size={small ? '15pt' : '17pt'} style={[iconStyle, textStyles]} weight="heavy">
+                  {iconValue}
+                </AnimatedText>
+              </Column>
+            )}
+            {typeof label !== 'undefined' && (
+              <Column width="content">
+                <AnimatedText align="center" style={textStyles} numberOfLines={1} size={small ? '17pt' : '20pt'} weight="heavy">
+                  {labelValue}
+                </AnimatedText>
+              </Column>
+            )}
+            {rightIcon && (
+              <Column width="content">
+                <AnimatedText align="center" style={[textStyles, secondaryTextStyles]} size={small ? '15pt' : '17pt'} weight="bold">
+                  {rightIconValue}
+                </AnimatedText>
+              </Column>
+            )}
+          </Columns>
+        </Box>
+      </GestureHandlerV1Button>
+    </Animated.View>
   );
 };
 
