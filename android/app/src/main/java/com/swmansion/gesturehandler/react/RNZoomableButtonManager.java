94b9017d03e895cf78711596c1f8e86cf3d4c4a1
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2024-07-09 19:19:13 -0400

    Fix native Android ZoomableButton
    Will follow up with some code cleanup but everything should be working.
    
    This also removes a bunch of unnecessary wrapper views and layout recalculations in the Android ButtonPressAnimation component.

--- android/app/src/main/java/com/swmansion/gesturehandler/react/RNZoomableButtonManager.java
+++ android/app/src/main/java/com/swmansion/gesturehandler/react/RNZoomableButtonManager.java
@@ -1,146 +1,170 @@
 package com.swmansion.gesturehandler.react;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.MotionEvent;
 import android.view.animation.Animation;
 import android.view.animation.Interpolator;
 import android.view.animation.ScaleAnimation;
+import androidx.annotation.NonNull;
 import androidx.core.view.animation.PathInterpolatorCompat;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.common.MapBuilder;
+import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ViewGroupManager;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
-import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 import javax.annotation.Nullable;
+import java.util.Map;
 
-public class RNZoomableButtonManager extends
-        RNGestureHandlerButtonViewManager {
-    public static class ZoomableButtonViewGroup extends ButtonViewGroup {
-        int mMinLongPressDuration = 500;
-        float mScaleTo = 0.86f;
-        int mDuration = 160;
-        float pivotX = 0.5f;
-        float pivotY = 0.5f;
-        boolean isLongPress = false;
-        boolean shouldLongPressHoldPress = false;
+@ReactModule(name = "RNZoomableButton")
+public class RNZoomableButtonManager extends ViewGroupManager<RNGestureHandlerButtonViewManager.ButtonViewGroup> {
 
+    public static class ZoomableButtonViewGroup extends RNGestureHandlerButtonViewManager.ButtonViewGroup {
+        private float mScaleTo = 0.86f;
+        private int mDuration = 160;
+        private float pivotX = 0.5f;
+        private float pivotY = 0.5f;
+        private static final Interpolator bezierInterpolator = PathInterpolatorCompat.create(0.25f, 0.46f, 0.45f, 0.94f);
+
+        private boolean isLongPress = false;
+        private boolean shouldLongPressHoldPress = false;
+        private int mMinLongPressDuration = 500;
+
+        private boolean mIsActive = false;
+        private boolean mIsTaskScheduled = false;
+        private boolean didLongPressFire = false;
+        private boolean mIsLongTaskScheduled = false;
+
+        private final Handler mHandler = new Handler(Looper.getMainLooper());
+        private Runnable mLongPressRunnable;
 
         public ZoomableButtonViewGroup(Context context) {
             super(context);
         }
 
-        static Interpolator bezierInterpolator = PathInterpolatorCompat.create(0.25f, 0.46f, 0.45f, 0.94f);
-
-        private boolean mIsActive = false;
-        private Timer mLongPressTimer = new Timer();
-        private boolean mIsTaskScheduled = false;
-        private boolean mIsLongTaskScheduled = false;
         private void animate(boolean in) {
             if (mIsActive == in) {
                 return;
             }
             mIsActive = in;
-            this.clearAnimation();
+            clearAnimation();
+            float fromScale = in ? 1f : mScaleTo;
+            float toScale = in ? mScaleTo : 1f;
             Animation anim = new ScaleAnimation(
-                    in ? 1f : mScaleTo, !in ? 1f : mScaleTo,
-                    in ? 1f : mScaleTo, !in ? 1f : mScaleTo,
-                    this.getMeasuredWidth() * pivotX,
-                    this.getMeasuredHeight() * pivotY);
+                    fromScale, toScale,
+                    fromScale, toScale,
+                    Animation.RELATIVE_TO_SELF, pivotX,
+                    Animation.RELATIVE_TO_SELF, pivotY);
             anim.setFillAfter(true);
             anim.setDuration(mDuration);
             anim.setInterpolator(bezierInterpolator);
             this.startAnimation(anim);
         }
 
-
+        @SuppressLint("ClickableViewAccessibility")
         @Override
-        public boolean performClick() {
-            if (!mIsLongTaskScheduled) {
-                onReceivePressEvent(false);
-            }
-            return super.performClick();
-        }
+        public boolean onTouchEvent(@NonNull MotionEvent event) {
+            int action = event.getActionMasked();
 
-        @Override
-        public boolean onTouchEvent(MotionEvent ev){
-            if (ev.getAction() == MotionEvent.ACTION_UP) {
-                mIsTaskScheduled = false;
-                if (mIsLongTaskScheduled && isLongPress && shouldLongPressHoldPress) {
-                    onReceivePressEndedEvent();
-                }
-                mLongPressTimer.cancel();
-                mLongPressTimer = new Timer();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                    animate(true);
+                    startLongPressTimer();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    mIsTaskScheduled = false;
+                    cancelLongPress();
+                    if (didLongPressFire && shouldLongPressHoldPress) {
+                        didLongPressFire = false;
+                        onLongPressEnded();
+                        return true;
+                    }
+                    break;
+                case MotionEvent.ACTION_CANCEL:
+                    cancelLongPress();
+                    if (didLongPressFire) {
+                        didLongPressFire = false;
+                    }
+                    break;
             }
-            return super.onTouchEvent(ev);
-        }
 
-        static ButtonViewGroup sButtonResponder;
+            return super.onTouchEvent(event);
+        }
 
         @Override
         public void setPressed(boolean pressed) {
-            if (pressed && sButtonResponder == null) {
-                // first button to be pressed grabs button responder
-                sButtonResponder = this;
-            }
-            if (!pressed || sButtonResponder == this) {
-                // we set pressed state only for current responder
-                setPressedInternal(pressed);
-            }
-            if (!pressed && sButtonResponder == this) {
-                // if the responder is no longer pressed we release button responder
-                sButtonResponder = null;
-            }
+            animate(pressed);
+            super.setPressed(pressed);
         }
 
-        private void setPressedInternal(boolean pressed) {
-            this.animate(pressed);
-            super.setPressed(pressed);
+        @Override
+        public void cancelLongPress() {
+            if (mLongPressRunnable != null) {
+                mHandler.removeCallbacks(mLongPressRunnable);
+            }
+            mIsLongTaskScheduled = false;
+            mIsTaskScheduled = false;
+        }
 
-            if (pressed && !mIsTaskScheduled) {
+        private void startLongPressTimer() {
+            if (!mIsTaskScheduled) {
                 mIsTaskScheduled = true;
                 mIsLongTaskScheduled = false;
                 if (isLongPress) {
-                    mLongPressTimer.schedule(new TimerTask() {
-                        @Override
-                        public void run() {
-                            mIsTaskScheduled = false;
-                            mIsLongTaskScheduled = true;
-
-                            onReceivePressEvent(true);
-                            if (!shouldLongPressHoldPress) {
-                                animate(false);
-                            }
+                    mLongPressRunnable = () -> {
+                        mIsTaskScheduled = false;
+                        mIsLongTaskScheduled = true;
+                        onLongPress();
+                        if (!shouldLongPressHoldPress) {
+                            animate(false);
+                            setPressed(false);
+                            didLongPressFire = false;
                         }
-                    }, mMinLongPressDuration);
+                    };
+                    mHandler.postDelayed(mLongPressRunnable, mMinLongPressDuration);
                 }
-            }
-
-            if (!pressed && mIsTaskScheduled) {
-                mLongPressTimer.cancel();
-                mLongPressTimer = new Timer();
-                mIsTaskScheduled = false;
             }
         }
 
-        public void onReceivePressEvent(boolean longPress) {
+        @Override
+        public boolean performClick() {
+            if (!mIsTaskScheduled && !mIsLongTaskScheduled && !didLongPressFire) {
+                boolean result = super.performClick();
+                ReactContext reactContext = (ReactContext) getContext();
+                WritableMap event = Arguments.createMap();
+                event.putString("type", "press");
+                reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+                        getId(),
+                        "topPress",
+                        event);
+                return result;
+            }
+            return false;
+        }
+
+        private void onLongPress() {
+            didLongPressFire = true;
+            mIsLongTaskScheduled = false;
+            ReactContext reactContext = (ReactContext) getContext();
             WritableMap event = Arguments.createMap();
-            event.putString("type", longPress ? "longPress" : "press");
-            ReactContext reactContext = (ReactContext)getContext();
+            event.putString("type", "longPress");
             reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
                     getId(),
                     "topPress",
                     event);
         }
 
-        public void onReceivePressEndedEvent() {
+        private void onLongPressEnded() {
             WritableMap event = Arguments.createMap();
             event.putString("type", "longPressEnded");
-            ReactContext reactContext = (ReactContext)getContext();
+            ReactContext reactContext = (ReactContext) getContext();
             reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
                     getId(),
                     "topPress",
@@ -148,6 +172,7 @@ public class RNZoomableButtonManager extends
         }
     }
 
+    @Override
     public Map getExportedCustomBubblingEventTypeConstants() {
         return MapBuilder.builder()
                 .put(
@@ -158,24 +183,26 @@ public class RNZoomableButtonManager extends
                 .build();
     }
 
+    @NonNull
     @Override
-    public ButtonViewGroup createViewInstance(ThemedReactContext context) {
+    public RNGestureHandlerButtonViewManager.ButtonViewGroup createViewInstance(@NonNull ThemedReactContext context) {
         return new ZoomableButtonViewGroup(context);
     }
 
+    @NonNull
     @Override
     public String getName() {
         return "RNZoomableButton";
     }
 
-    @ReactProp(name = "minLongPressDuration")
-    public void setMinLongPressDuration(ZoomableButtonViewGroup view, Integer minLongPressDuration) {
-        view.mMinLongPressDuration = minLongPressDuration;
-    }
-
     @ReactProp(name = "scaleTo")
     public void setScaleTo(ZoomableButtonViewGroup view, float scaleTo) {
         view.mScaleTo = scaleTo;
+    }
+
+    @ReactProp(name = "minLongPressDuration")
+    public void setMinLongPressDuration(ZoomableButtonViewGroup view, Integer minLongPressDuration) {
+        view.mMinLongPressDuration = minLongPressDuration;
     }
 
     @ReactProp(name = "isLongPress")
@@ -202,13 +229,5 @@ public class RNZoomableButtonManager extends
             view.pivotX = (float) transformOrigin.getDouble(0);
             view.pivotY = (float) transformOrigin.getDouble(1);
         }
-    }
-
-    @Override
-    protected void onAfterUpdateTransaction(ButtonViewGroup view) {
-        // Ripple effect is broken in GH 1.10.3 on Android 13
-        // It appears all the time on press even when the color is transparent
-        // So we disable any feedback completely since we have our own animation
-        // So this is NOOP
     }
 }
