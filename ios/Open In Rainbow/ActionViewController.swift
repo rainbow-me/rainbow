5426d09ede2baf02af337617d1cb6d850f3cdc67
Matthew Wall <matthew.wallt@gmail.com>
2024-07-17 19:00:34 -0400

    idk

--- ios/Open In Rainbow/ActionViewController.swift
+++ ios/Open In Rainbow/ActionViewController.swift
@@ -1,56 +1,65 @@
+//
+//  ActionViewController.swift
+//  Open In Rainbow
+//
+//  Created by Matthew Wall on 7/17/24.
+//  Copyright © 2024 Rainbow. All rights reserved.
+//
+
 import UIKit
 import MobileCoreServices
+import UniformTypeIdentifiers
 
 class ActionViewController: UIViewController {
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        print("in viewWillAppear")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
         for item in extensionContext?.inputItems as? [NSExtensionItem] ?? [] {
             for provider in item.attachments ?? [] {
 
-                if provider.hasItemConformingToTypeIdentifier(kUTTypeText as String) {
-                    provider.loadItem(forTypeIdentifier: kUTTypeText as String, options: nil) { text, _ in
+            // URL in the form of a text
+            if provider.hasItemConformingToTypeIdentifier(UTType.text.identifier) {
+                    provider.loadItem(forTypeIdentifier: UTType.text.identifier, options: nil) { text, _ in
                         guard let text = text as? String else { return }
                         guard let url = URL(string: text) else {
                             return
                         }
-                        self.launchBrowser(withUrl: url)
-                        self.done()
+                        self.openApp(url: url)
                     }
                     break
                 }
 
-                if provider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
-                    provider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil) { url, _ in
-                        guard let url = url as? URL else { return }
-                        self.launchBrowser(withUrl: url)
-                        self.done()
-                    }
-                    break
+            // URL in URL form
+            if provider.hasItemConformingToTypeIdentifier(UTType.url.identifier) {
+                provider.loadItem(forTypeIdentifier: UTType.text.identifier, options: nil) { url, _ in
+                    guard let url = url as? URL else { return }
+                    self.openApp(url: url)
                 }
+                break
+            }
 
             }
         }
     }
 
-    func launchBrowser(withUrl url: URL) {
-        // Open the main app using a custom URL scheme
-        let urlScheme = "rainbow://open?url=\(url.absoluteString)"
-      
-        print("URL: \(urlScheme)")
-
-        if let appURL = URL(string: urlScheme) {
-            print("Opening URL scheme: \(urlScheme)")
-            self.extensionContext?.open(appURL, completionHandler: nil)
-        } else {
-            print("Failed to create URL from scheme: \(urlScheme)")
+    func openApp(url: URL) {
+        var responder = self as UIResponder?
+        responder = (responder as? UIViewController)?.parent
+        while (responder != nil && !(responder is UIApplication)) {
+            responder = responder?.next
         }
+        if responder != nil{
+            let selectorOpenURL = sel_registerName("openURL:")
+            if responder!.responds(to: selectorOpenURL) {
+                responder!.perform(selectorOpenURL, with: url)
+            }
+        }
+    
+        self.done()
     }
 
-    func done() {
-        self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+    @IBAction func done() {
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
     }
-    
+
 }
