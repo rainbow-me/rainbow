25a511d492fa918c7d9c600544fa08c74b27dd5b
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2024-07-26 13:46:22 -0500

    oop

--- scripts/run-retry-tests.sh
+++ scripts/run-retry-tests.sh
@@ -2,24 +2,36 @@
 
 max_retries="$1"
 count=0
+failed_specs=""
+
+run_tests() {
+    if [ -z "$failed_specs" ]; then
+        ./node_modules/.bin/detox test -c ios.sim.release --cleanup --maxWorkers 2 -- --forceExit
+    else
+        ./node_modules/.bin/detox test -c ios.sim.release --cleanup --maxWorkers 2 -f "$failed_specs" -- --forceExit
+    fi
+}
 
 until (( count >= max_retries ))
 do
-    if [ $count -eq 0 ]; then
-        ./node_modules/.bin/detox test -c ios.sim.release --maxWorkers 2 -- --forceExit
-    else
-        ./node_modules/.bin/detox test -c ios.sim.release --maxWorkers 2 -- --forceExit --onlyFailures
-    fi
-    
-    ret_val=$?
+    run_tests | tee output.log
+    ret_val=${PIPESTATUS[0]}
+
     if [ $ret_val -eq 0 ]; then
         echo "All tests passed."
         exit 0
     fi
-    
+
+    # Update failed_specs based on Jest output
+    failed_specs=$(grep "FAIL" output.log | grep -oE "e2e/.*\.spec\.[jt]s" | sort -u | tr '\n' ' ')
+
     ((count++))
-    echo "Some tests failed, attempt $count/$max_retries..."
+    if [ $count -lt $max_retries ]; then
+        echo "Some tests failed. Retrying failed tests (Attempt $count/$max_retries)..."
+        echo "Failed specs: $failed_specs"
+    fi
 done
 
 echo "Tests failed after $max_retries attempts."
+echo "Failed specs: $failed_specs"
 exit 1
