8b7b03770e46c1428232713de5b71e5bf7ca3bb0
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2024-07-27 18:11:39 -0400

    Add preferredNetwork to swapsStore, persist degenMode

--- src/state/assets/userAssets.ts
+++ src/state/assets/userAssets.ts
@@ -1,10 +1,11 @@
-import { ParsedSearchAsset, UniqueId, UserAssetFilter } from '@/__swaps__/types/assets';
-import { ChainId } from '@/__swaps__/types/chains';
+import { Address } from 'viem';
 import { RainbowError, logger } from '@/logger';
 import store from '@/redux/store';
 import { SUPPORTED_CHAIN_IDS, supportedNativeCurrencies } from '@/references';
 import { createRainbowStore } from '@/state/internal/createRainbowStore';
-import { Address } from 'viem';
+import { ParsedSearchAsset, UniqueId, UserAssetFilter } from '@/__swaps__/types/assets';
+import { ChainId } from '@/__swaps__/types/chains';
+import { swapsStore } from '../swaps/swapsStore';
 
 const SEARCH_CACHE_MAX_ENTRIES = 50;
 
@@ -34,7 +35,7 @@ export interface UserAssetsState {
   getBalanceSortedChainList: () => ChainId[];
   getChainsWithBalance: () => ChainId[];
   getFilteredUserAssetIds: () => UniqueId[];
-  getHighestValueAsset: () => ParsedSearchAsset | null;
+  getHighestValueAsset: (usePreferredNetwork?: boolean) => ParsedSearchAsset | null;
   getUserAsset: (uniqueId: UniqueId) => ParsedSearchAsset | null;
   getUserAssets: () => ParsedSearchAsset[];
   selectUserAssetIds: (selector: (asset: ParsedSearchAsset) => boolean, filter?: UserAssetFilter) => Generator<UniqueId, void, unknown>;
@@ -179,7 +180,22 @@ export const userAssetsStore = createRainbowStore<UserAssetsState>(
       }
     },
 
-    getHighestValueAsset: () => get().userAssets.values().next().value || null,
+    getHighestValueAsset: (usePreferredNetwork = true) => {
+      const preferredNetwork = usePreferredNetwork ? swapsStore.getState().preferredNetwork : undefined;
+      const assets = get().userAssets;
+
+      if (preferredNetwork && get().getChainsWithBalance().includes(preferredNetwork)) {
+        // Find the highest-value asset on the preferred network
+        for (const [, asset] of assets) {
+          if (asset.chainId === preferredNetwork) {
+            return asset;
+          }
+        }
+      }
+
+      // If no preferred network asset, return the highest-value asset
+      return assets.values().next().value || null;
+    },
 
     getUserAsset: (uniqueId: UniqueId) => get().userAssets.get(uniqueId) || null,
 
