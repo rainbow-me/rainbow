8d7dcf76b256f337484753af5059b60e55aed44e
gregs <ukfaeku@gmail.com>
2024-07-27 18:23:11 -0300

    Hold to swap button (#5920)
    * hold to swap
    
    * fix button shadow
    
    * a
    
    * idk
    
    * Update src/__swaps__/screens/Swap/components/GestureHandlerHoldButton.tsx
    
    Co-authored-by: Matthew Wall <matthew.wallt@gmail.com>
    
    * Add long press handling to GestureHandlerButton
    
    * Refactor hold to press to remove state reliance
    
    Also polishes the hold to press interaction
    
    * Fix dapp browser GH buttons
    
    * Fix lint errors, fix Podfile.lock
    
    * Fix initial HoldProgress color
    
    ---------
    
    Co-authored-by: Matthew Wall <matthew.wallt@gmail.com>
    Co-authored-by: Christian Baroni <7061887+christianbaroni@users.noreply.github.com>

--- src/__swaps__/screens/Swap/components/SwapBottomPanel.tsx
+++ src/__swaps__/screens/Swap/components/SwapBottomPanel.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { PanGestureHandler } from 'react-native-gesture-handler';
-import Animated, { useAnimatedStyle, useDerivedValue } from 'react-native-reanimated';
-import { Box, Separator, globalColors, useColorMode } from '@/design-system';
 import { LIGHT_SEPARATOR_COLOR, SEPARATOR_COLOR, THICK_BORDER_WIDTH } from '@/__swaps__/screens/Swap/constants';
 import { NavigationSteps, useSwapContext } from '@/__swaps__/screens/Swap/providers/swap-provider';
 import { opacity } from '@/__swaps__/utils/swaps';
+import { Box, Separator, globalColors, useColorMode } from '@/design-system';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import Animated, {
+  Easing,
+  runOnJS,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 import { useBottomPanelGestureHandler } from '../hooks/useBottomPanelGestureHandler';
 import { GasButton } from './GasButton';
 import { GasPanel } from './GasPanel';
 import { ReviewPanel } from './ReviewPanel';
 import { SwapActionButton } from './SwapActionButton';
+import { SPRING_CONFIGS } from '@/components/animations/animationConfigs';
+import { triggerHapticFeedback } from '@/screens/points/constants';
+import { LONG_PRESS_DURATION_IN_MS } from '@/components/buttons/hold-to-authorize/constants';
 
 export function SwapBottomPanel() {
   const { isDarkMode } = useColorMode();
-  const { AnimatedSwapStyles, SwapNavigation, configProgress, confirmButtonIconStyle, confirmButtonProps, internalSelectedOutputAsset } =
-    useSwapContext();
+  const {
+    AnimatedSwapStyles,
+    SwapNavigation,
+    configProgress,
+    confirmButtonIconStyle,
+    confirmButtonProps,
+    internalSelectedOutputAsset,
+    quoteFetchingInterval,
+  } = useSwapContext();
 
   const { swipeToDismissGestureHandler, gestureY } = useBottomPanelGestureHandler();
+
+  const holdProgress = useSharedValue(0);
 
   const gestureHandlerStyles = useAnimatedStyle(() => {
     return {
@@ -35,6 +55,7 @@ export function SwapBottomPanel() {
   const label = useDerivedValue(() => confirmButtonProps.value.label);
   const disabled = useDerivedValue(() => confirmButtonProps.value.disabled);
   const opacity = useDerivedValue(() => confirmButtonProps.value.opacity);
+  const type = useDerivedValue(() => confirmButtonProps.value.type);
 
   return (
     <PanGestureHandler maxPointers={1} onGestureEvent={swipeToDismissGestureHandler}>
@@ -71,11 +92,47 @@ export function SwapBottomPanel() {
           <Box style={{ flex: 1 }}>
             <SwapActionButton
               asset={internalSelectedOutputAsset}
+              holdProgress={holdProgress}
               icon={icon}
               iconStyle={confirmButtonIconStyle}
               label={label}
               disabled={disabled}
-              onPressWorklet={SwapNavigation.handleSwapAction}
+              onPressWorklet={() => {
+                'worklet';
+                if (type.value !== 'hold') {
+                  SwapNavigation.handleSwapAction();
+                }
+              }}
+              onLongPressEndWorklet={success => {
+                'worklet';
+                if (!success) {
+                  quoteFetchingInterval.start();
+                  holdProgress.value = withSpring(0, SPRING_CONFIGS.slowSpring);
+                }
+              }}
+              onLongPressWorklet={() => {
+                'worklet';
+                if (type.value === 'hold') {
+                  runOnJS(triggerHapticFeedback)('notificationSuccess');
+                  SwapNavigation.handleSwapAction();
+                }
+              }}
+              onPressStartWorklet={() => {
+                'worklet';
+                if (type.value === 'hold') {
+                  quoteFetchingInterval.stop();
+                  holdProgress.value = 0;
+                  holdProgress.value = withTiming(
+                    100,
+                    { duration: LONG_PRESS_DURATION_IN_MS, easing: Easing.inOut(Easing.sin) },
+                    isFinished => {
+                      if (isFinished) {
+                        holdProgress.value = 0;
+                      }
+                    }
+                  );
+                }
+              }}
               opacity={opacity}
               scaleTo={0.9}
             />
