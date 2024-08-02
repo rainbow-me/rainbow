05aac864d35a7406ac868f0aee7176834fa84887
Esteban Miño <efmino@uc.cl>
2024-08-02 16:09:31 -0400

    use chainid in <GasSpeedButton

--- src/screens/ExchangeModal.tsx
+++ src/screens/ExchangeModal.tsx
@@ -848,6 +848,10 @@ export function ExchangeModal({ fromDiscover, ignoreInitialTypeCheck, testID, ty
                   nativeAmount={nativeAmountDisplay}
                   nativeCurrency={nativeCurrency}
                   nativeFieldRef={nativeFieldRef}
+<<<<<<< HEAD
+=======
+                  network={ethereumUtils.getNetworkFromChainId(inputNetwork)}
+>>>>>>> de7ba31b9 (use chainid in <GasSpeedButton)
                   onFocus={handleFocus}
                   onPressMaxBalance={updateMaxInputAmount}
                   onPressSelectInputCurrency={chainId => {
@@ -919,7 +923,11 @@ export function ExchangeModal({ fromDiscover, ignoreInitialTypeCheck, testID, ty
               {/* @ts-expect-error - Javascript Component */}
               <GasSpeedButton
                 asset={outputCurrency}
+<<<<<<< HEAD
                 chainId={currentChainId}
+=======
+                currentNetwork={ethereumUtils.getNetworkFromChainId(currentChainId)}
+>>>>>>> de7ba31b9 (use chainid in <GasSpeedButton)
                 flashbotTransaction={flashbots}
                 marginBottom={0}
                 marginTop={0}
