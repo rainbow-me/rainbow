404a7ecb02a551109fbde329909cdc39c64378c1
Bruno Barbieri <brunobar79@gmail.com>
2024-08-05 16:31:09 -0400

    fix build

--- metro.config.js
+++ metro.config.js
@@ -30,6 +30,50 @@ if (process.env.CI) {
 const rainbowConfig = {
   resolver: {
     blacklistRE,
+    resolveRequest: (context, moduleName, platform) => {
+      try {
+        return context.resolveRequest(context, moduleName, platform);
+      } catch (error) {
+        console.warn('\n1️⃣ context.resolveRequest cannot resolve: ', moduleName);
+      }
+
+      try {
+        const resolution = require.resolve(moduleName, {
+          paths: [path.dirname(context.originModulePath), ...config.resolver.nodeModulesPaths],
+        });
+
+        if (path.isAbsolute(resolution)) {
+          return {
+            filePath: resolution,
+            type: 'sourceFile',
+          };
+        }
+      } catch (error) {
+        console.warn('\n2️⃣ require.resolve cannot resolve: ', moduleName);
+      }
+
+      try {
+        return defaultModuleResolver(context, moduleName, platform);
+      } catch (error) {
+        console.warn('\n3️⃣ defaultModuleResolver cannot resolve: ', moduleName);
+      }
+
+      try {
+        return {
+          filePath: require.resolve(moduleName),
+          type: 'sourceFile',
+        };
+      } catch (error) {
+        console.warn('\n4️⃣ require.resolve cannot resolve: ', moduleName);
+      }
+
+      try {
+        const resolution = getDefaultConfig(require.resolve(moduleName)).resolver?.resolveRequest;
+        return resolution(context, moduleName, platform);
+      } catch (error) {
+        console.warn('\n5️⃣ getDefaultConfig cannot resolve: ', moduleName);
+      }
+    },
   },
   transformer,
 };
