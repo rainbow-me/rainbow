b0db622ede6f6d83b048bff52d9242b51106bdd1
Matthew Wall <matthew.wallt@gmail.com>
2024-08-05 19:55:19 -0400

    fix

--- src/navigation/Routes.tsx
+++ src/navigation/Routes.tsx
@@ -2,5 +2,5 @@ import { Platform } from 'react-native';
 
 export default Platform.select({
   ios: require('./Routes.ios'),
-  android: require('./Routes.ios'),
+  android: require('./Routes.android'),
 });
