b9e278f2f087ed9b8effb154dd3951e429872aea
gregs <ukfaeku@gmail.com>
2024-08-06 17:21:35 -0300

    👍

--- src/migrations/migrations/migrateFavorites.ts
+++ src/migrations/migrations/migrateFavorites.ts
@@ -1,9 +1,10 @@
 import { AddressOrEth, UniqueId } from '@/__swaps__/types/assets';
 import { getStandardizedUniqueIdWorklet } from '@/__swaps__/utils/swaps';
 import { EthereumAddress, RainbowToken } from '@/entities';
-import { createQueryKey, queryClient } from '@/react-query';
+import { createQueryKey, persistOptions, queryClient } from '@/react-query';
 import { favoritesQueryKey } from '@/resources/favorites';
 import { ethereumUtils } from '@/utils';
+import { persistQueryClientRestore, persistQueryClientSave } from '@tanstack/react-query-persist-client';
 import { Migration, MigrationName } from '../types';
 
 const favoritesV1QueryKey = createQueryKey('favorites', {}, { persisterVersion: 1 });
@@ -13,8 +14,11 @@ export function migrateFavoritesV2(): Migration {
   return {
     name: MigrationName.migrateFavoritesV2,
     async migrate() {
+      await persistQueryClientRestore({ queryClient, persister: persistOptions.persister }); // first restore persisted data
+
       // v1 used just the address as key, v2 uses uniqueId as key and builds this uniqueId with ChainId instead of Network
       const v1Data = queryClient.getQueryData<Record<EthereumAddress, RainbowToken>>(favoritesV1QueryKey);
+
       if (!v1Data) return;
 
       const migratedFavorites: Record<UniqueId, RainbowToken> = {};
@@ -26,8 +30,9 @@ export function migrateFavoritesV2(): Migration {
         favorite.uniqueId = uniqueId; // v2 unique uses chainId instead of Network
         migratedFavorites[uniqueId] = favorite;
       }
-      queryClient.setQueryData(favoritesV2QueryKey, migratedFavorites, { updatedAt: 0 });
-      queryClient.setQueryData(favoritesV1QueryKey, undefined); // clear v1 store data
+      queryClient.setQueryData(favoritesQueryKey, migratedFavorites);
+
+      await persistQueryClientSave({ queryClient, persister: persistOptions.persister });
     },
   };
 }
@@ -36,11 +41,14 @@ export function migrateFavoritesV3(): Migration {
   return {
     name: MigrationName.migrateFavoritesV3,
     async migrate() {
-      console.log('MIGRATE V2 TO V3');
+      await persistQueryClientRestore({ queryClient, persister: persistOptions.persister }); // first restore persisted data
+
       const v2Data = queryClient.getQueryData<Record<UniqueId, RainbowToken>>(favoritesV2QueryKey);
-      if (v2Data) {
-        queryClient.setQueryData(favoritesQueryKey, v2Data, { updatedAt: 0 });
-      }
+      if (!v2Data) return;
+
+      queryClient.setQueryData(favoritesQueryKey, v2Data, { updatedAt: 0 });
+
+      await persistQueryClientSave({ queryClient, persister: persistOptions.persister });
     },
   };
 }
