c05f854a6ea94cf448be11f661c012473fc449ea
Ben Goldberg <bengoldberg@rainbow.me>
2024-08-08 15:03:44 -0600

    useMemo -> useEffect

--- src/hooks/useWallets.ts
+++ src/hooks/useWallets.ts
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
 import WalletTypes from '@/helpers/walletTypes';
@@ -24,15 +24,15 @@ const walletSelector = createSelector(
 export default function useWallets() {
   const { isWalletLoading, selectedWallet, walletNames, wallets } = useSelector(walletSelector);
 
-  const isDamaged = useMemo(() => {
-    const bool = selectedWallet?.damaged;
-    if (bool) {
+  const isDamaged = selectedWallet?.damaged;
+
+  useEffect(() => {
+    if (isDamaged) {
       logger.sentry('Wallet is damaged. Check values below:');
       logger.sentry('selectedWallet: ', selectedWallet);
       logger.sentry('wallets: ', wallets);
     }
-    return bool;
-  }, [selectedWallet, wallets]);
+  }, [isDamaged, selectedWallet, wallets]);
 
   return {
     isDamaged,
