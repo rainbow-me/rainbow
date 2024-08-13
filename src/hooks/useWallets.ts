6acff71c9c9dd03446c0d1ea8c90b10b9220f66d
Ben Goldberg <bengoldberg@rainbow.me>
2024-08-13 09:15:47 -0700

    remove spammy isDamaged logging

--- src/hooks/useWallets.ts
+++ src/hooks/useWallets.ts
@@ -1,10 +1,8 @@
-import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
 import WalletTypes from '@/helpers/walletTypes';
 import { RainbowWallet } from '@/model/wallet';
 import { AppState } from '@/redux/store';
-import { logger } from '@/logger';
 
 const walletSelector = createSelector(
   ({ wallets: { isWalletLoading, selected = {} as RainbowWallet, walletNames, wallets } }: AppState) => ({
@@ -24,16 +22,8 @@ const walletSelector = createSelector(
 export default function useWallets() {
   const { isWalletLoading, selectedWallet, walletNames, wallets } = useSelector(walletSelector);
 
-  const isDamaged = selectedWallet?.damaged;
-
-  useEffect(() => {
-    if (isDamaged) {
-      logger.warn('Wallet is damaged. Check attached metadata.', { selectedWallet, wallets });
-    }
-  }, [isDamaged, selectedWallet, wallets]);
-
   return {
-    isDamaged,
+    isDamaged: selectedWallet?.damaged,
     isReadOnlyWallet: selectedWallet.type === WalletTypes.readOnly,
     isHardwareWallet: !!selectedWallet.deviceId,
     isWalletLoading,
