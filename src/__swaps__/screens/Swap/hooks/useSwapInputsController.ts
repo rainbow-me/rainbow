c106f220e1a0bf76e283edf71d6fa8d475711521
Jin <jin.chung17@gmail.com>
2024-08-20 17:02:30 -0700

    WETH to ETH endpoints do not always return the same asset info (#6022)

--- src/__swaps__/screens/Swap/hooks/useSwapInputsController.ts
+++ src/__swaps__/screens/Swap/hooks/useSwapInputsController.ts
@@ -424,9 +424,9 @@ export function useSwapInputsController({
         setQuote({
           data: quoteResponse,
           inputAmount: quotedInputAmount,
-          inputPrice: quoteResponse.sellTokenAsset.price.value,
+          inputPrice: quoteResponse?.sellTokenAsset?.price?.value,
           outputAmount: quotedOutputAmount,
-          outputPrice: quoteResponse.buyTokenAsset.price.value,
+          outputPrice: quoteResponse?.buyTokenAsset?.price?.value,
           originalQuoteParams,
           quoteFetchingInterval,
         });
