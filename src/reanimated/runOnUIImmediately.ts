cdc7b0fb7b8b5dc17047f5db2ca909def9497c1b
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2024-08-27 16:55:20 -0600

    Expose runOnUIImmediately
    This is an alternative to runOnUI that allows bypassing reanimated's internal update batching and the accompanying possibility of a delay before code gets executed on the UI thread

---
+++ src/reanimated/runOnUIImmediately.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { runOnUIImmediately } = require('react-native-reanimated/src/threads') as typeof import('react-native-reanimated/src/threads');
+
+export { runOnUIImmediately };
