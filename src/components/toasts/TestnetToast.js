4ec453495b9d509d0120708a86f6a67fe93fe313
jinchung <jin.chung17@gmail.com>
2024-08-28 11:47:44 -0700

    Ignore providerUrl for hardhat check and rely on connectedToHardhat flag

--- src/components/toasts/TestnetToast.js
+++ src/components/toasts/TestnetToast.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Icon } from '../icons';
 import { Nbsp, Text } from '../text';
 import Toast from './Toast';
-import { isHardHat } from '@/handlers/web3';
 import { useInternetStatus } from '@/hooks';
 import { getNetworkObject } from '@/networks';
 import { ChainId } from '@/networks/types';
@@ -16,17 +15,15 @@ const TestnetToast = ({ chainId }) => {
   const [networkName, setNetworkName] = useState(name);
 
   useEffect(() => {
-    if (connectedToHardhat && chainId === ChainId.mainnet) {
-      const networkObject = getNetworkObject({ chainId });
-      const providerUrl = networkObject.rpc();
-      if (isHardHat(providerUrl)) {
+    if (chainId === ChainId.mainnet) {
+      if (connectedToHardhat) {
         setVisible(true);
         setNetworkName('Hardhat');
       } else {
         setVisible(false);
       }
     } else {
-      setVisible(false);
+      setVisible(true);
       setNetworkName(name + (isConnected ? '' : ' (offline)'));
     }
   }, [name, isConnected, chainId, connectedToHardhat]);
