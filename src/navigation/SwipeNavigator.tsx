808d25ecf622e8df66f70772a43a53f555932dd8
Matthew Wall <matthew.wallt@gmail.com>
2024-08-29 11:39:14 -0400

    rm unused import

--- src/navigation/SwipeNavigator.tsx
+++ src/navigation/SwipeNavigator.tsx
@@ -7,7 +7,6 @@ import { TestnetToast } from '@/components/toasts';
 import { DAPP_BROWSER, POINTS, useExperimentalFlag } from '@/config';
 import { Box, Columns, globalColors, Stack, useForegroundColor, Text, Cover, useColorMode } from '@/design-system';
 import { IS_ANDROID, IS_IOS, IS_TEST } from '@/env';
-import { web3Provider } from '@/handlers/web3';
 import { isUsingButtonNavigation } from '@/utils/deviceUtils';
 import { useAccountAccentColor, useAccountSettings, useCoinListEdited, useDimensions, usePendingTransactions } from '@/hooks';
 import { useRemoteConfig } from '@/model/remoteConfig';
