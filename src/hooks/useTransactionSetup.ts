35d3e524073ea635ef66d8ff74ce8536f284d28a
Matthew Wall <matthew.wallt@gmail.com>
2024-08-30 15:01:04 -0400

    Mobile Wallet Protocol / Txn cleanup (#6061)
    * lots of sign transaction sheet cleanup including original ticket fix
    
    * fix toHex ref
    
    * mwp
    
    * create mwp secure store and wrap app in provider
    
    * add android intents and clena up app.tsx
    
    * idk
    
    * changes
    
    * handshake request
    
    * basic impl
    
    * update some comments
    
    * some progress today at least
    
    * move business logic off App.tsx
    
    * move backup check to wallet screen
    
    * latest changes
    
    * use suspense and lazy to fix oop issue
    
    * expose config
    
    * recursively handle incoming actions
    
    * only process one incoming message at once
    
    * UNDO THIS PROBABLY
    
    * progress on personal sign
    
    * personal sign working
    
    * send transactions working
    
    * cleanup
    
    * fix lint and other calls to getRequestDisplayDetails
    
    * Update src/handlers/deeplinks.ts
    
    * fml
    
    * add switch eth chain
    
    * lint
    
    * fix lint
    
    * code review changes
    
    * cleanup App.tsx
    
    * fixes
    
    * code review changes
    
    * code review

---
+++ src/hooks/useTransactionSetup.ts
@@ -0,0 +1,66 @@
+import * as i18n from '@/languages';
+import { useCallback, useEffect, useState } from 'react';
+import { InteractionManager } from 'react-native';
+import useGas from '@/hooks/useGas';
+import { methodRegistryLookupAndParse } from '@/utils/methodRegistry';
+import { analytics } from '@/analytics';
+import { event } from '@/analytics/event';
+import { RequestSource } from '@/utils/requestNavigationHandlers';
+import { ChainId } from '@/networks/types';
+
+type TransactionSetupParams = {
+  chainId: ChainId;
+  startPollingGasFees: ReturnType<typeof useGas>['startPollingGasFees'];
+  stopPollingGasFees: ReturnType<typeof useGas>['stopPollingGasFees'];
+  isMessageRequest: boolean;
+  transactionDetails: any;
+  source: RequestSource;
+};
+
+export const useTransactionSetup = ({
+  chainId,
+  startPollingGasFees,
+  stopPollingGasFees,
+  isMessageRequest,
+  transactionDetails,
+  source,
+}: TransactionSetupParams) => {
+  const [methodName, setMethodName] = useState<string | null>(null);
+
+  const fetchMethodName = useCallback(
+    async (data: string) => {
+      const methodSignaturePrefix = data.substr(0, 10);
+      try {
+        const { name } = await methodRegistryLookupAndParse(methodSignaturePrefix, chainId);
+        if (name) {
+          setMethodName(name);
+        }
+      } catch (e) {
+        setMethodName(data);
+      }
+    },
+    [chainId]
+  );
+
+  useEffect(() => {
+    InteractionManager.runAfterInteractions(() => {
+      if (chainId) {
+        if (!isMessageRequest) {
+          startPollingGasFees(chainId);
+          fetchMethodName(transactionDetails?.payload?.params?.[0].data);
+        } else {
+          setMethodName(i18n.t(i18n.l.wallet.message_signing.request));
+        }
+        analytics.track(event.txRequestShownSheet, { source });
+      }
+    });
+
+    return () => {
+      if (!isMessageRequest) {
+        stopPollingGasFees();
+      }
+    };
+  }, [isMessageRequest, chainId, transactionDetails?.payload?.params, source, fetchMethodName, startPollingGasFees, stopPollingGasFees]);
+
+  return { methodName };
+};
