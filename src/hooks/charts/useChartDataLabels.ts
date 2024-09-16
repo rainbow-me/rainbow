2aa7baeb4f5a7d9b688ad4243cc346b6261d5274
gregs <ukfaeku@gmail.com>
2024-09-16 12:39:19 -0300

    token details chart - cannot read property 'y' of undefined  (#6009)
    * swallow error
    
    * uhu
    
    * imports

--- src/hooks/charts/useChartDataLabels.ts
+++ src/hooks/charts/useChartDataLabels.ts
@@ -1,15 +1,17 @@
 import { useCallback, useMemo } from 'react';
-import ChartTypes from '@/helpers/chartTypes';
+import ChartTypes, { ChartType } from '@/helpers/chartTypes';
 import { toFixedDecimals } from '@/helpers/utilities';
+import { ChartData } from './useChartInfo';
 
 const formatPercentChange = (change = 0) => toFixedDecimals(change, 2);
 
-export default function useChartDataLabels({ asset, chartType, points }: any) {
+export default function useChartDataLabels({ asset, chartType, points }: { asset: any; chartType: ChartType; points: ChartData[] }) {
   const latestPrice = asset?.price?.value;
 
   const getPercentChangeForPrice = useCallback(
     (startPrice: number) => {
-      const endPrice = points?.[points.length - 1].y;
+      const endPrice = points[points.length - 1]?.y;
+      if (!endPrice) return;
       const percent = ((endPrice - startPrice) / startPrice) * 100;
       return formatPercentChange(percent);
     },
