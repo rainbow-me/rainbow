21a4c7689fb176c3edd9b0b0518c16de220abb80
Matthew Wall <matthew.wallt@gmail.com>
2024-09-19 15:39:56 -0400

    fix lint

--- src/__swaps__/screens/Swap/components/SearchInputButton.tsx
+++ src/__swaps__/screens/Swap/components/SearchInputButton.tsx
@@ -74,7 +74,7 @@ export const SearchInputButton = ({
     const isInputSearchFocused = inputProgress.value === NavigationSteps.SEARCH_FOCUSED;
     const isOutputSearchFocused = outputProgress.value === NavigationSteps.SEARCH_FOCUSED;
     const isOutputTokenListFocused = outputProgress.value === NavigationSteps.TOKEN_LIST_FOCUSED;
-    
+
     const clipboardDataAvailable = hasClipboardData || IS_ANDROID;
 
     const isPasteDisabled = output && !internalSelectedOutputAsset.value && isOutputTokenListFocused && !clipboardDataAvailable;
