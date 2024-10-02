a647a10d8fc730d7010d649d8b74a9c6d3adccfc
Ben Goldberg <bengoldberg@rainbow.me>
2024-10-02 13:06:53 -0400

    fix (#6159)

--- src/components/asset-list/RecyclerAssetList2/Claimable.tsx
+++ src/components/asset-list/RecyclerAssetList2/Claimable.tsx
@@ -40,14 +40,9 @@ export const Claimable = React.memo(function Claimable({ uniqueId, extendedState
       flexDirection="row"
     >
       <Inline alignVertical="center" space="12px">
-        <Box
-          as={FasterImageView}
-          source={{ url: claimable.iconUrl }}
-          style={{ height: 40, width: 40 }}
-          borderRadius={11}
-          borderWidth={1}
-          borderColor={{ custom: 'rgba(0, 0, 0, 0.03)' }}
-        />
+        <Box borderRadius={11} borderWidth={1} borderColor={{ custom: 'rgba(0, 0, 0, 0.03)' }}>
+          <FasterImageView source={{ url: claimable.iconUrl }} style={{ height: 40, width: 40 }} />
+        </Box>
         <Stack space={{ custom: 11 }}>
           <Text
             weight="semibold"
