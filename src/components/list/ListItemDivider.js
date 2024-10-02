57e12df6fa57cf63f0fe9058bdbf5de10c5976dd
Matthew Wall <matthew.wallt@gmail.com>
2024-10-02 16:32:23 -0400

    convert sends to typescript (#6120)
    * convert sends to typescript
    
    * more conversions
    
    * fix lint
    
    * fix optimism security fee
    
    * fix file renaming weirdness
    
    * fix rotation from max asset to nft not working
    
    * fix some typescript bs
    
    * fix lint
    
    * fix nft sends

--- src/components/list/ListItemDivider.js
+++ src/components/list/ListItemDivider.js
@@ -1,5 +1,5 @@
 import { PropTypes } from 'prop-types';
-import Divider from '../Divider';
+import Divider from '@/components/Divider';
 import styled from '@/styled-thing';
 import { neverRerender } from '@/utils';
 
