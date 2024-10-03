c024cf8a453a48e1c4ee21455edec9f8773475dd
Matthew Wall <matthew.wallt@gmail.com>
2024-10-03 14:59:51 -0400

    wc ts conversion

--- src/screens/WalletScreen/index.tsx
+++ src/screens/WalletScreen/index.tsx
@@ -1,4 +1,3 @@
-import { View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { AssetList } from '../../components/asset-list';
@@ -14,31 +13,26 @@ import {
   useLoadGlobalLateData,
   useWalletSectionsData,
 } from '@/hooks';
-import Routes from '@rainbow-me/routes';
-import { position } from '@/styles';
 import { Toast, ToastPositionContainer } from '@/components/toasts';
 import { useRecoilValue } from 'recoil';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { analyticsV2 } from '@/analytics';
 import { AppState } from '@/redux/store';
 import { addressCopiedToastAtom } from '@/recoil/addressCopiedToastAtom';
-import styled from '@/styled-thing';
 import { IS_ANDROID } from '@/env';
 import { RemoteCardsSync } from '@/state/sync/RemoteCardsSync';
 import { RemotePromoSheetSync } from '@/state/sync/RemotePromoSheetSync';
 import { UserAssetsSync } from '@/state/sync/UserAssetsSync';
 import { MobileWalletProtocolListener } from '@/components/MobileWalletProtocolListener';
 import { runWalletBackupStatusChecks } from '@/handlers/walletReadyEvents';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { RootStackParamList } from '@/navigation/types';
+import { useNavigation } from '@/navigation';
+import Routes from '@/navigation/Routes';
 
-const WalletPage = styled(Page)({
-  ...position.sizeAsObject('100%'),
-  flex: 1,
-});
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const WalletScreen: React.FC<any> = ({ navigation, route }) => {
-  const { params } = route;
-  const { setParams, getState: dangerouslyGetState, getParent: dangerouslyGetParent } = navigation;
+function WalletScreen() {
+  const { params } = useRoute<RouteProp<RootStackParamList, 'WalletScreen'>>();
+  const { setParams, getState: dangerouslyGetState, getParent: dangerouslyGetParent } = useNavigation();
   const removeFirst = useRemoveFirst();
   const [initialized, setInitialized] = useState(!!params?.initialized);
   const initializeWallet = useInitializeWallet();
@@ -97,38 +91,31 @@ const WalletScreen: React.FC<any> = ({ navigation, route }) => {
   const { highContrastAccentColor } = useAccountAccentColor();
 
   return (
-    <View
-      style={{
-        height: '100%',
-        width: '100%',
-      }}
-    >
-      <WalletPage testID="wallet-screen">
-        <Box style={{ flex: 1, marginTop: -(navbarHeight + insets.top) }}>
-          {/* @ts-expect-error JavaScript component */}
-          <AssetList
-            accentColor={highContrastAccentColor}
-            disableRefreshControl={isLoadingUserAssetsAndAddress || isLoadingBalance}
-            isLoading={IS_ANDROID && (isLoadingUserAssetsAndAddress || isLoadingBalance)}
-            isWalletEthZero={isWalletEthZero}
-            network={currentNetwork}
-            walletBriefSectionsData={walletBriefSectionsData}
-          />
-        </Box>
-        <ToastPositionContainer>
-          <Toast isVisible={isAddressCopiedToastActive} text="􀁣 Address Copied" testID="address-copied-toast" />
-        </ToastPositionContainer>
+    <Box as={Page} flex={1} testID="wallet-screen">
+      <Box style={{ flex: 1, marginTop: -(navbarHeight + insets.top) }}>
+        {/* @ts-expect-error JavaScript component */}
+        <AssetList
+          accentColor={highContrastAccentColor}
+          disableRefreshControl={isLoadingUserAssetsAndAddress || isLoadingBalance}
+          isLoading={IS_ANDROID && (isLoadingUserAssetsAndAddress || isLoadingBalance)}
+          isWalletEthZero={isWalletEthZero}
+          network={currentNetwork}
+          walletBriefSectionsData={walletBriefSectionsData}
+        />
+      </Box>
+      <ToastPositionContainer>
+        <Toast isVisible={isAddressCopiedToastActive} text="􀁣 Address Copied" testID="address-copied-toast" />
+      </ToastPositionContainer>
 
-        {/* NOTE: The components below render null and are solely for keeping react-query and Zustand in sync */}
-        <UserAssetsSync />
-        <RemoteCardsSync />
-        <RemotePromoSheetSync />
+      {/* NOTE: The components below render null and are solely for keeping react-query and Zustand in sync */}
+      <UserAssetsSync />
+      <RemoteCardsSync />
+      <RemotePromoSheetSync />
 
-        {/* NOTE: This component listens for Mobile Wallet Protocol requests and handles them */}
-        <MobileWalletProtocolListener />
-      </WalletPage>
-    </View>
+      {/* NOTE: This component listens for Mobile Wallet Protocol requests and handles them */}
+      <MobileWalletProtocolListener />
+    </Box>
   );
-};
+}
 
-export default WalletScreen;
+export default React.memo(WalletScreen);
