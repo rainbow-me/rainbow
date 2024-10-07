d052a282b26265c77fb410c648f2b0c1976d5193
Matthew Wall <matthew.wallt@gmail.com>
2024-10-06 22:01:04 -0400

    more removal

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,6 @@
 import * as StatusBarHelper from './statusBarHelper';
 
 export { default as BiometryTypes } from './biometryTypes';
-export { default as CurrencySelectionTypes } from './currencySelectionTypes';
 export { default as isKeyboardOpen } from './isKeyboardOpen';
 export { default as isReanimatedAvailable } from './isReanimatedAvailable';
-export { default as TokenSectionTypes } from './tokenSectionTypes';
 export { StatusBarHelper };
