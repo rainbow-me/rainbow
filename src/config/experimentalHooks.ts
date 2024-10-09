b879d0c0e3d607ba9e8f7b6b68f291fdc7a36498
Ben Goldberg <bengoldberg@rainbow.me>
2024-10-09 15:19:47 -0400

    defaultConfig fix

--- src/config/experimentalHooks.ts
+++ src/config/experimentalHooks.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { defaultConfig } from './experimental';
+import { defaultConfig, defaultConfigValues } from './experimental';
 import { RainbowContext } from '@/helpers/RainbowContext';
 import { IS_DEV } from '@/env';
 import isTestFlight from '@/helpers/isTestFlight';
@@ -15,9 +15,10 @@ const useExperimentalFlag = (name: any) => {
 
 export const useExperimentalConfig = () => {
   if (IS_DEV || isTestFlight) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     return useContext(RainbowContext).config;
   } else {
-    return defaultConfig;
+    return defaultConfigValues;
   }
 };
 
