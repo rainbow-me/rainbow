cb2b8a07514cc50aea985a6e456c0afc9b9fbf40
Jin <jin.chung17@gmail.com>
2024-11-01 10:32:03 -0400

    Fix userAssets query enabled config (#6243)
    * Remove unused useUserAssetsByChain hook
    
    * Only trigger retry logic if there are chainIdsWithErrorsInResponse
    
    * Remove unnecessary export of userAssetsByChainQueryFunction
    
    * Update default for empty cache for userAssetsByChainQueryFunction
    
    * Results from retry can be undefined, so check before using Object.values on the results
    
    * Fix endpoint we were hitting on retry logic which was causing NO AUTH error
    
    * Fix userAssets query to only be enabled when both address and currency are available
    
    * Remove isSwapsOpen check when keeping user assets store in sync
    
    * Current search cache can sometimes be an empty object instead of a Map and fails when trying to index get function
    
    * Ignore persistence serialization and deserialization of searchCache in userAssets store
    
    * Only update the user assets store when swaps is not open or if it is open but missing user assets data
    
    * Update enabled logic for userAssetsQuery from userAssetsSync
    
    * Use partialize for userAssets store
    
    * Re-add address check in user assets query

--- src/__swaps__/screens/Swap/resources/assets/index.ts
+++ src/__swaps__/screens/Swap/resources/assets/index.ts
@@ -1,4 +1,3 @@
 export { useUserAssets } from './userAssets';
 export type { UserAssetsArgs } from './userAssets';
-export { useUserAssetsByChain } from './userAssetsByChain';
 export type { UserAssetsByChainArgs } from './userAssetsByChain';
