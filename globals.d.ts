da74692602ddfd634c37997d1983a64de43dab1f
jinchung <jin.chung17@gmail.com>
2024-11-04 16:32:24 -0500

    Remove unused app center read only tokens

--- globals.d.ts
+++ globals.d.ts
@@ -42,8 +42,6 @@ declare module 'react-native-dotenv' {
   export const PINATA_API_SECRET: string;
   export const PINATA_API_URL: string;
   export const PINATA_GATEWAY_URL: string;
-  export const APP_CENTER_READ_ONLY_TOKEN_ANDROID: string;
-  export const APP_CENTER_READ_ONLY_TOKEN_IOS: string;
   export const CODE_PUSH_DEPLOYMENT_KEY_IOS: string;
   export const NFT_API_KEY: string;
   export const NFT_API_URL: string;
