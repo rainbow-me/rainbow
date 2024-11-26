aab142aa12a674feb7fc2fbf4a2fe461ac52a721
Matthew Wall <matthew.wallt@gmail.com>
2024-11-26 09:44:37 -0600

    add syncing timeout 10s

--- src/state/backups/backups.ts
+++ src/state/backups/backups.ts
@@ -27,6 +27,8 @@ export enum CloudBackupState {
   Success = 'success',
 }
 
+const DEFAULT_TIMEOUT = 10_000;
+
 export const LoadingStates = [CloudBackupState.Initializing, CloudBackupState.Syncing, CloudBackupState.Fetching];
 
 interface BackupsStore {
@@ -70,94 +72,124 @@ export const backupsStore = createRainbowStore<BackupsStore>((set, get) => ({
   setPassword: password => set({ password }),
 
   syncAndFetchBackups: async (retryOnFailure = true) => {
-    const { status } = get();
-    const syncAndPullFiles = async (): Promise<{ success: boolean; retry?: boolean }> => {
-      try {
-        const isAvailable = await isCloudBackupAvailable();
-        if (!isAvailable) {
-          logger.debug('[backupsStore]: Cloud backup is not available');
-          set({ backupProvider: undefined, status: CloudBackupState.NotAvailable, backups: { files: [] }, mostRecentBackup: undefined });
-          return {
-            success: false,
-            retry: false,
-          };
-        }
+    const timeoutPromise = new Promise<{
+      success: boolean;
+      retry?: boolean;
+    }>(resolve => {
+      setTimeout(() => {
+        logger.error(new RainbowError('[backupsStore]: syncAndFetchBackups timed out'));
+        resolve({ success: false, retry: false });
+      }, DEFAULT_TIMEOUT);
+    });
 
-        if (IS_ANDROID) {
-          const gdata = await getGoogleAccountUserData();
-          if (!gdata) {
-            logger.debug('[backupsStore]: Google account is not available');
-            set({ backupProvider: undefined, status: CloudBackupState.NotAvailable, backups: { files: [] }, mostRecentBackup: undefined });
+    const syncPromise = (async (): Promise<{
+      success: boolean;
+      retry?: boolean;
+    }> => {
+      const { status } = get();
+      const syncAndPullFiles = async (): Promise<{
+        success: boolean;
+        retry?: boolean;
+      }> => {
+        try {
+          const isAvailable = await isCloudBackupAvailable();
+          if (!isAvailable) {
+            logger.debug('[backupsStore]: Cloud backup is not available');
+            set({
+              backupProvider: undefined,
+              status: CloudBackupState.NotAvailable,
+              backups: { files: [] },
+              mostRecentBackup: undefined,
+            });
             return {
               success: false,
               retry: false,
             };
           }
-        }
 
-        set({ status: CloudBackupState.Syncing });
-        logger.debug('[backupsStore]: Syncing with cloud');
-        await syncCloud();
+          if (IS_ANDROID) {
+            const gdata = await getGoogleAccountUserData();
+            if (!gdata) {
+              logger.debug('[backupsStore]: Google account is not available');
+              set({
+                backupProvider: undefined,
+                status: CloudBackupState.NotAvailable,
+                backups: { files: [] },
+                mostRecentBackup: undefined,
+              });
+              return {
+                success: false,
+                retry: false,
+              };
+            }
+          }
 
-        set({ status: CloudBackupState.Fetching });
-        logger.debug('[backupsStore]: Fetching backups');
-        const backups = await fetchAllBackups();
+          set({ status: CloudBackupState.Syncing });
+          logger.debug('[backupsStore]: Syncing with cloud');
+          await syncCloud();
 
-        set({ backups });
+          set({ status: CloudBackupState.Fetching });
+          logger.debug('[backupsStore]: Fetching backups');
+          const backups = await fetchAllBackups();
 
-        const { wallets } = store.getState().wallets;
+          set({ backups });
 
-        // if the user has any cloud backups, set the provider to cloud
-        if (backups.files.length > 0) {
-          set({
-            backupProvider: walletBackupTypes.cloud,
-            mostRecentBackup: getMostRecentCloudBackup(backups.files),
+          const { wallets } = store.getState().wallets;
+
+          // if the user has any cloud backups, set the provider to cloud
+          if (backups.files.length > 0) {
+            set({
+              backupProvider: walletBackupTypes.cloud,
+              mostRecentBackup: getMostRecentCloudBackup(backups.files),
+            });
+          } else if (hasManuallyBackedUpWallet(wallets)) {
+            set({ backupProvider: walletBackupTypes.manual });
+          } else {
+            set({ backupProvider: undefined });
+          }
+
+          logger.debug(`[backupsStore]: Retrieved ${backups.files.length} backup files`);
+
+          set({ status: CloudBackupState.Ready });
+          return {
+            success: true,
+            retry: false,
+          };
+        } catch (e) {
+          logger.error(new RainbowError('[backupsStore]: Failed to fetch all backups'), {
+            error: e,
           });
-        } else if (hasManuallyBackedUpWallet(wallets)) {
-          set({ backupProvider: walletBackupTypes.manual });
-        } else {
-          set({ backupProvider: undefined });
+          set({ status: CloudBackupState.FailedToInitialize });
         }
 
-        logger.debug(`[backupsStore]: Retrieved ${backups.files.length} backup files`);
-
-        set({ status: CloudBackupState.Ready });
         return {
-          success: true,
+          success: false,
+          retry: retryOnFailure,
+        };
+      };
+
+      if (mutex.isLocked() || returnEarlyIfLockedStates.includes(status)) {
+        logger.debug('[backupsStore]: Mutex is locked or returnEarlyIfLockedStates includes status', {
+          status,
+        });
+        return {
+          success: false,
           retry: false,
         };
-      } catch (e) {
-        logger.error(new RainbowError('[backupsStore]: Failed to fetch all backups'), {
-          error: e,
-        });
-        set({ status: CloudBackupState.FailedToInitialize });
       }
 
-      return {
-        success: false,
-        retry: retryOnFailure,
-      };
-    };
+      const releaser = await mutex.acquire();
+      logger.debug('[backupsStore]: Acquired mutex');
+      const { success, retry } = await syncAndPullFiles();
+      releaser();
+      logger.debug('[backupsStore]: Released mutex');
+      if (retry) {
+        await sleep(5_000);
+        return get().syncAndFetchBackups(retryOnFailure);
+      }
+      return { success, retry };
+    })();
 
-    if (mutex.isLocked() || returnEarlyIfLockedStates.includes(status)) {
-      logger.debug('[backupsStore]: Mutex is locked or returnEarlyIfLockedStates includes status', {
-        status,
-      });
-      return {
-        success: false,
-        retry: false,
-      };
-    }
-
-    const releaser = await mutex.acquire();
-    logger.debug('[backupsStore]: Acquired mutex');
-    const { success, retry } = await syncAndPullFiles();
-    releaser();
-    logger.debug('[backupsStore]: Released mutex');
-    if (retry) {
-      await sleep(5_000);
-      return get().syncAndFetchBackups(retryOnFailure);
-    }
-    return { success, retry };
+    return Promise.race([syncPromise, timeoutPromise]);
   },
 }));
