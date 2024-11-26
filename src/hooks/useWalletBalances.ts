3078d6da5d6588c7ef8d3b5c7d84ca099de356ca
Kane Thomas <kmt8vn@virginia.edu>
2024-11-26 12:52:34 -0500

    fix double locked position subtraction

--- src/hooks/useWalletBalances.ts
+++ src/hooks/useWalletBalances.ts
@@ -71,7 +71,7 @@ const useWalletBalances = (wallets: AllRainbowWallets): WalletBalanceResult => {
       const lowerCaseAddress = address.toLowerCase() as Address;
       const assetBalance = summaryData?.data?.addresses?.[lowerCaseAddress]?.summary?.asset_value?.toString() || '0';
       const positionData = queryClient.getQueryData<RainbowPositions | undefined>(positionsQueryKey({ address, currency: nativeCurrency }));
-      const positionsBalance = positionData ? subtract(positionData.totals.total.amount, positionData.totals.totalLocked) : '0';
+      const positionsBalance = positionData ? positionData.totals.total.amount : '0';
       const totalAccountBalance = add(assetBalance, positionsBalance);
 
       result[lowerCaseAddress] = {
