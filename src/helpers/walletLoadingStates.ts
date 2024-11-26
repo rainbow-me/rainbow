664a03576f6d52fcd22ea38a371c5f7220cc91ca
Matthew Wall <matthew.wallt@gmail.com>
2024-11-26 09:02:51 -0600

    i18n wallet loading states

--- src/helpers/walletLoadingStates.ts
+++ src/helpers/walletLoadingStates.ts
@@ -1,10 +1,10 @@
+import * as i18n from '@/languages';
+
 export const WalletLoadingStates = {
-  BACKING_UP_WALLET: 'Backing up...',
-  CREATING_WALLET: 'Creating wallet...',
-  FETCHING_PASSWORD: 'Fetching Password...',
-  IMPORTING_WALLET: 'Importing...',
-  IMPORTING_WALLET_SILENTLY: '',
-  RESTORING_WALLET: 'Restoring...',
+  BACKING_UP_WALLET: i18n.t('loading.backing_up'),
+  CREATING_WALLET: i18n.t('loading.creating_wallet'),
+  IMPORTING_WALLET: i18n.t('loading.importing_wallet'),
+  RESTORING_WALLET: i18n.t('loading.restoring'),
 } as const;
 
 export type WalletLoadingState = (typeof WalletLoadingStates)[keyof typeof WalletLoadingStates];
