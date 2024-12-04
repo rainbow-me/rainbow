03bfb03941acf7202acfef33b49ecbf914dea7f9
gregs <gregs@rainbow.me>
2024-12-04 18:50:37 -0300

    Swaps deeplink (#6178)
    * fix branch
    
    branch is blocking some vpns, when trying to open deeplinks while connected to a vpn it would fail to connect to branch servers and return an empty params object, not undefined, empty `params: {}`, in turn making the app ignore the deeplink and not open it
    
    * aaa
    
    * aaaaaa
    
    * remove unused imports
    
    * fix type
    
    * ops
    
    * better validations
    
    * opsi inverted the conditions
    
    * revert pbxproj changes
    
    * setFromWallet
    
    * fix typo & code style
    
    * remove uncessary stuff
    
    ---------
    
    Co-authored-by: Matthew Wall <matthew.wallt@gmail.com>

--- src/__swaps__/screens/Swap/hooks/useSwapInputsController.ts
+++ src/__swaps__/screens/Swap/hooks/useSwapInputsController.ts
@@ -44,10 +44,37 @@ import { analyticsTrackQuoteFailed } from './analyticsTrackQuoteFailed';
 
 const REMOTE_CONFIG = getRemoteConfig();
 
+function getInitialInputValues({
+  inputAsset,
+  inputAmount,
+  outputAsset,
+  outputAmount,
+  percentageToSwap,
+  sliderXPosition,
+}: {
+  inputAsset: ExtendedAnimatedAssetWithColors | null;
+  inputAmount: string | undefined;
+  outputAsset: ExtendedAnimatedAssetWithColors | null;
+  outputAmount: string | undefined;
+  percentageToSwap: number;
+  sliderXPosition: number;
+}) {
+  if (inputAsset && inputAmount) {
+    const inputNativeValue = mulWorklet(inputAmount, inputAsset?.price?.value ?? 0);
+    return { inputAmount, inputNativeValue, outputAmount: 0, outputNativeValue: 0 };
+  }
+  if (outputAsset && outputAmount) {
+    const outputNativeValue = mulWorklet(outputAmount, outputAsset?.price?.value ?? 0);
+    return { inputAmount: 0, inputNativeValue: 0, outputAmount, outputNativeValue };
+  }
+
+  const slider = getInputValuesForSliderPositionWorklet({ selectedInputAsset: inputAsset, percentageToSwap, sliderXPosition });
+  return { inputAmount: slider.inputAmount, inputNativeValue: slider.inputNativeValue, outputAmount: 0, outputNativeValue: 0 };
+}
+
 export function useSwapInputsController({
   focusedInput,
   inputProgress,
-  initialSelectedInputAsset,
   internalSelectedInputAsset,
   internalSelectedOutputAsset,
   isFetching,
@@ -57,10 +84,10 @@ export function useSwapInputsController({
   quote,
   sliderXPosition,
   slippage,
+  initialValues,
 }: {
   focusedInput: SharedValue<inputKeys>;
   inputProgress: SharedValue<number>;
-  initialSelectedInputAsset: ExtendedAnimatedAssetWithColors | null;
   internalSelectedInputAsset: SharedValue<ExtendedAnimatedAssetWithColors | null>;
   internalSelectedOutputAsset: SharedValue<ExtendedAnimatedAssetWithColors | null>;
   isFetching: SharedValue<boolean>;
@@ -70,27 +97,30 @@ export function useSwapInputsController({
   quote: SharedValue<Quote | CrosschainQuote | QuoteError | null>;
   sliderXPosition: SharedValue<number>;
   slippage: SharedValue<string>;
+  initialValues: {
+    inputAmount?: string;
+    outputAmount?: string;
+    inputMethod?: inputMethods;
+  };
 }) {
   const percentageToSwap = useDerivedValue(() => {
     return Math.round(clamp((sliderXPosition.value - SCRUBBER_WIDTH / SLIDER_WIDTH) / SLIDER_WIDTH, 0, 1) * 100) / 100;
   });
 
-  const { inputAmount: initialInputAmount, inputNativeValue: initialInputNativeValue } = getInputValuesForSliderPositionWorklet({
-    selectedInputAsset: initialSelectedInputAsset,
-    percentageToSwap: percentageToSwap.value,
-    sliderXPosition: sliderXPosition.value,
-  });
-
   const { nativeCurrency: currentCurrency } = useAccountSettings();
   const setSlippage = swapsStore(state => state.setSlippage);
 
-  const inputValues = useSharedValue<inputValuesType>({
-    inputAmount: initialInputAmount,
-    inputNativeValue: initialInputNativeValue,
-    outputAmount: 0,
-    outputNativeValue: 0,
-  });
-  const inputMethod = useSharedValue<inputMethods>('slider');
+  const inputValues = useSharedValue<inputValuesType>(
+    getInitialInputValues({
+      inputAsset: internalSelectedInputAsset.value,
+      inputAmount: initialValues.inputAmount,
+      outputAsset: internalSelectedOutputAsset.value,
+      outputAmount: initialValues.outputAmount,
+      percentageToSwap: percentageToSwap.value,
+      sliderXPosition: sliderXPosition.value,
+    })
+  );
+  const inputMethod = useSharedValue<inputMethods>(initialValues.inputMethod || 'slider');
 
   const inputNativePrice = useDerivedValue(() => {
     return internalSelectedInputAsset.value?.nativePrice || internalSelectedInputAsset.value?.price?.value || 0;
