e755afde172542dc74ae984f3a1d9589d0cc59c9
Kane Thomas <kmt8vn@virginia.edu>
2024-12-07 22:59:31 -0500

    derive positions value from summary field instead of fetching each address position endpoint

--- src/resources/summary/summary.ts
+++ src/resources/summary/summary.ts
@@ -27,6 +27,8 @@ interface AddysSummary {
           num_erc20s: number;
           last_activity: number;
           asset_value: number | null;
+          claimables_value: number | null;
+          positions_value: number | null;
         };
       };
       summary_by_chain: {
@@ -39,6 +41,8 @@ interface AddysSummary {
           num_erc20s: number;
           last_activity: number;
           asset_value: number | null;
+          claimables_value: number | null;
+          positions_value: number | null;
         };
       };
     };
