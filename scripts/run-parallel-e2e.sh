9b431b530e752809dbfb8ea32e974346d95f314b
Bruno Barbieri <brunobar79@gmail.com>
2024-12-10 18:38:51 -0500

    downgrade to 3 runners

--- scripts/run-parallel-e2e.sh
+++ scripts/run-parallel-e2e.sh
@@ -5,7 +5,7 @@ count=0
 
 until (( count >= max_retries ))
 do
-  ./node_modules/.bin/detox test parallel/ -c ios.sim.release --maxWorkers 4 -- --forceExit --bail 1
+  ./node_modules/.bin/detox test parallel/ -c ios.sim.release --maxWorkers 3 -- --forceExit --bail 1
   ret_val=$?
   if [ $ret_val -eq 0 ]; then
     exit 0
