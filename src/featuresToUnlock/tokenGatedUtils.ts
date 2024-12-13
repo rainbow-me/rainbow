2f81bceac917149d6b212f16318e9e65b572c973
Bruno Barbieri <1247834+brunobar79@users.noreply.github.com>
2024-12-13 15:25:58 -0500

    NFT checker 2.0 (#6293)

--- src/featuresToUnlock/tokenGatedUtils.ts
+++ src/featuresToUnlock/tokenGatedUtils.ts
@@ -1,9 +1,9 @@
-import { Contract } from '@ethersproject/contracts';
 import { EthereumAddress } from '@/entities';
 import { getProvider } from '@/handlers/web3';
 import { tokenGateCheckerAbi } from '@/references';
 import { Network } from '@/state/backendNetworks/types';
 import { useBackendNetworksStore } from '@/state/backendNetworks/backendNetworks';
+import { Interface } from '@ethersproject/abi';
 
 export type TokenGateCheckerNetwork =
   | Network.arbitrum
@@ -12,29 +12,64 @@ export type TokenGateCheckerNetwork =
   | Network.mainnet
   | Network.polygon
   | Network.bsc
-  | Network.base;
-
+  | Network.base
+  | Network.gnosis
+  | Network.avalanche;
 const TOKEN_GATE_CHECKER_ADDRESS: Record<TokenGateCheckerNetwork, EthereumAddress> = {
-  [Network.arbitrum]: '0x2a0332e28913a06fa924d40a3e2160f763010417',
-  [Network.mainnet]: '0x47c9c137fc9aa5ccdbea707b0b27d52780565476',
-  [Network.optimism]: '0x400a9f1bb1db80643c33710c2232a0d74ef5cff1',
-  [Network.polygon]: '0x400a9f1bb1db80643c33710c2232a0d74ef5cff1',
-  [Network.zora]: '0x12a39421c23f4d3f788c33f0f9281652ac4f909a',
-  [Network.bsc]: '0x5a9d9ffbd5a22f2790af726550920b845c3a6b35',
-  [Network.base]: '0x7edddf0b8e7471e0ebf0df67ad179598c0bef695',
+  [Network.arbitrum]: '0x5A9D9FFBd5a22f2790AF726550920B845c3A6B35',
+  [Network.mainnet]: '0xc4A8619B3980d84F6d59d416d415007A1217fEc8',
+  [Network.optimism]: '0x5A9D9FFBd5a22f2790AF726550920B845c3A6B35',
+  [Network.polygon]: '0x3e3937C119BD854059844D3D03A8116a18Afa409',
+  [Network.zora]: '0x75efed6B8AF6B0490c2899e489c58EF26E3D0898',
+  [Network.bsc]: '0x50a42aB85A09e24229e42dAd31Cb44B42E83b2De',
+  [Network.base]: '0xa5d7b264ad7039F54A670c27Fe5A64CAd0FE0cCe',
+  [Network.gnosis]: '0x2488F7B6FD1A0949391fE6a533D7E5c4704173E2',
+  [Network.avalanche]: '0x2a0332E28913A06Fa924d40A3E2160f763010417',
 };
 
+/***
+ * Check if wallets own NFTs of type ERC-721
+ * We only need the NFT address to check for ownership
+ */
+
 export const checkIfWalletsOwnNft = async (
-  tokenAddress: EthereumAddress[],
+  tokenAddresses: EthereumAddress[],
   network: TokenGateCheckerNetwork,
   walletsToCheck: EthereumAddress[]
 ) => {
-  const p = getProvider({ chainId: useBackendNetworksStore.getState().getChainsIdByName()[network] });
-
-  const contractInstance = new Contract(TOKEN_GATE_CHECKER_ADDRESS[network], tokenGateCheckerAbi, p);
-
   try {
-    const found = contractInstance.areOwners(tokenAddress, walletsToCheck);
+    const p = getProvider({ chainId: useBackendNetworksStore.getState().getChainsIdByName()[network] });
+    const iface = new Interface(tokenGateCheckerAbi);
+
+    const data = iface.encodeFunctionData('areOwners(address[], address[])', [tokenAddresses, walletsToCheck]);
+    const found = await p.call({ to: TOKEN_GATE_CHECKER_ADDRESS[network], data });
+    return found;
+  } catch (e) {
+    return false;
+  }
+};
+
+export interface TokenInfo {
+  account: EthereumAddress;
+  id: number;
+}
+
+/***
+ * Check if wallets own NFTs of type ERC-1155
+ * In this case we need to also check the id of the NFTs
+ */
+
+export const checkIfWalletsOwnNft1155 = async (
+  tokenInfo: TokenInfo[],
+  network: TokenGateCheckerNetwork,
+  walletsToCheck: EthereumAddress[]
+) => {
+  try {
+    const p = getProvider({ chainId: useBackendNetworksStore.getState().getChainsIdByName()[network] });
+    const iface = new Interface(tokenGateCheckerAbi);
+    const data = iface.encodeFunctionData('areOwners(TokenInfo[], address[])', [tokenInfo, walletsToCheck]);
+    const found = await p.call({ to: TOKEN_GATE_CHECKER_ADDRESS[network], data });
+
     return found;
   } catch (e) {
     return false;
