c2b49d15952b24aaed3c32c6dd6e896262147da0
Bruno Barbieri <1247834+brunobar79@users.noreply.github.com>
2024-12-16 18:27:57 -0500

    TT fixes (#6338)
    * remove arrows for price change
    
    * fix friend holders display
    
    * fix dupes
    
    * align and make friends looks better
    
    * remove spread
    
    * ops
    
    ---------
    
    Co-authored-by: gregs <ukfaeku@gmail.com>

--- src/components/Discover/TrendingTokens.tsx
+++ src/components/Discover/TrendingTokens.tsx
@@ -222,6 +222,9 @@ function FriendPfp({ pfp_url }: { pfp_url: string }) {
 }
 function FriendHolders({ friends }: { friends: FarcasterUser[] }) {
   if (friends.length === 0) return null;
+  const howManyOthers = Math.max(1, friends.length - 2);
+  const separator = howManyOthers === 1 && friends.length === 2 ? ` ${i18n.t(t.and)} ` : ', ';
+
   return (
     <View style={{ flexDirection: 'row', gap: 5.67, alignItems: 'center', marginTop: -2 }}>
       <View style={{ flexDirection: 'row-reverse', alignItems: 'center', paddingLeft: 6 }}>
@@ -230,12 +233,21 @@ function FriendHolders({ friends }: { friends: FarcasterUser[] }) {
       </View>
 
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text color="labelSecondary" size="11pt" weight="bold" numberOfLines={1} style={{ maxWidth: 148 }}>
-          {friends[0].username}{' '}
+        <Text color="labelSecondary" size="11pt" weight="bold" numberOfLines={1}>
+          {friends[0].username}
+          {friends[1] && (
+            <>
+              <Text color="labelTertiary" size="11pt" weight="bold">
+                {separator}
+              </Text>
+              {friends[1].username}
+            </>
+          )}
         </Text>
-        {friends.length > 1 && (
+        {friends.length > 2 && (
           <Text color="labelTertiary" size="11pt" weight="bold">
-            {i18n.t(t.and_others, { count: friends.length - 1 })}
+            {' '}
+            {i18n.t('trending_tokens.and_others', { count: howManyOthers })}
           </Text>
         )}
       </View>
@@ -409,12 +421,7 @@ function TrendingTokenRow({ token }: { token: TrendingToken }) {
             </View>
 
             <View style={{ gap: 12, marginLeft: 'auto' }}>
-              <View style={{ flexDirection: 'row', gap: 2, alignItems: 'center' }}>
-                {token.priceChange.day !== 0 && (
-                  <Text color={getPriceChangeColor(token.priceChange.day)} size="11pt" weight="bold">
-                    {token.priceChange.day > 0 ? '􀄨' : '􀄩'}
-                  </Text>
-                )}
+              <View style={{ flexDirection: 'row', gap: 2, alignItems: 'center', justifyContent: 'flex-end' }}>
                 <Text color={getPriceChangeColor(token.priceChange.day)} size="15pt" weight="bold">
                   {formatNumber(token.priceChange.day, { decimals: 2, useOrderSuffix: true })}%
                 </Text>
