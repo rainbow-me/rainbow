1ec444605a7469e01cbbb6b5a5071ea86058e9f3
Kane Thomas <kmt8vn@virginia.edu>
2024-12-17 22:38:09 -0500

    refactor wallet list loading animation transition

--- src/components/change-wallet/WalletList.tsx
+++ src/components/change-wallet/WalletList.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { StyleSheet, View } from 'react-native';
-import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
 import * as i18n from '@/languages';
-import { EmptyAssetList } from '../asset-list';
+import { EmptyAssetList } from '@/components/asset-list';
 import { AddressRow } from './AddressRow';
 import { EthereumAddress } from '@rainbow-me/entities';
 import styled from '@/styled-thing';
@@ -17,15 +17,14 @@ import {
   PANEL_INSET_HORIZONTAL,
 } from '@/screens/change-wallet/ChangeWalletSheet';
 import { Box, Separator, Text } from '@/design-system';
-import { DndProvider, Draggable, DraggableFlatListProps, UniqueIdentifier } from '../drag-and-drop';
+import { DndProvider, Draggable, DraggableFlatListProps, UniqueIdentifier } from '@/components/drag-and-drop';
 import { PinnedWalletsGrid } from '@/screens/change-wallet/PinnedWalletsGrid';
 import { usePinnedWalletsStore } from '@/state/wallets/pinnedWalletsStore';
 import { MenuItem } from '@/components/DropdownMenu';
 import { DraggableScrollView } from '@/components/drag-and-drop/components/DraggableScrollView';
 
 const DRAG_ACTIVATION_DELAY = 150;
-const LIST_TOP_PADDING = 7.5;
-const TRANSITION_DURATION = 75;
+const FADE_TRANSITION_DURATION = 75;
 const LIST_MAX_HEIGHT = MAX_PANEL_HEIGHT - PANEL_HEADER_HEIGHT;
 
 const EmptyWalletList = styled(EmptyAssetList).attrs({
@@ -33,8 +32,7 @@ const EmptyWalletList = styled(EmptyAssetList).attrs({
   pointerEvents: 'none',
 })({
   ...position.coverAsObject,
-  backgroundColor: ({ theme: { colors } }: any) => colors.white,
-  paddingTop: LIST_TOP_PADDING,
+  paddingTop: 7.5,
 });
 
 interface Props {
@@ -62,41 +60,7 @@ export function WalletList({ walletItems, menuItems, onPressMenuItem, onPressAcc
       .sort((a, b) => unpinnedAddresses.indexOf(a.id) - unpinnedAddresses.indexOf(b.id));
   }, [walletItems, pinnedAddresses, unpinnedAddresses]);
 
-  const [ready, setReady] = useState(false);
-  const opacityAnimation = useSharedValue(walletItems.length ? 1 : 0);
-  const emptyOpacityAnimation = useSharedValue(walletItems.length ? 0 : 1);
-
   const reorderUnpinnedAddresses = usePinnedWalletsStore(state => state.setUnpinnedAddresses);
-
-  // TODO: convert the effect below into an animated reaction
-  useEffect(() => {
-    if (walletItems.length && !ready) {
-      setTimeout(() => {
-        setReady(true);
-        emptyOpacityAnimation.value = withTiming(0, {
-          duration: TRANSITION_DURATION,
-          easing: Easing.out(Easing.ease),
-        });
-      }, 50);
-    }
-  }, [walletItems, ready, emptyOpacityAnimation]);
-
-  useLayoutEffect(() => {
-    if (walletItems.length) {
-      opacityAnimation.value = withTiming(1, {
-        duration: TRANSITION_DURATION,
-        easing: Easing.in(Easing.ease),
-      });
-    }
-  }, [walletItems, opacityAnimation]);
-
-  const opacityStyle = useAnimatedStyle(() => ({
-    opacity: opacityAnimation.value,
-  }));
-
-  const emptyOpacityStyle = useAnimatedStyle(() => ({
-    opacity: emptyOpacityAnimation.value,
-  }));
 
   const onOrderChange: DraggableFlatListProps<AddressItem>['onOrderChange'] = useCallback(
     (value: UniqueIdentifier[]) => {
@@ -158,22 +122,26 @@ export function WalletList({ walletItems, menuItems, onPressMenuItem, onPressAcc
 
   return (
     <Box>
-      <Animated.View style={[StyleSheet.absoluteFill, emptyOpacityStyle]}>
-        <EmptyWalletList />
-      </Animated.View>
-      <Animated.View style={opacityStyle}>
-        <DndProvider activationDelay={DRAG_ACTIVATION_DELAY} disabled={!editMode}>
-          <DraggableScrollView
-            onOrderChange={onOrderChange}
-            style={{ maxHeight: LIST_MAX_HEIGHT, marginHorizontal: -PANEL_INSET_HORIZONTAL, paddingHorizontal: PANEL_INSET_HORIZONTAL }}
-            autoScrollInsets={{ bottom: FOOTER_HEIGHT - 24 }}
-            contentContainerStyle={{ paddingBottom: FOOTER_HEIGHT - 24 }}
-          >
-            {renderHeader()}
-            {draggableUnpinnedWalletItems.map(item => renderScrollItem(item))}
-          </DraggableScrollView>
-        </DndProvider>
-      </Animated.View>
+      {walletItems.length === 0 && (
+        <Animated.View exiting={FadeOut.duration(FADE_TRANSITION_DURATION)} style={StyleSheet.absoluteFill}>
+          <EmptyWalletList />
+        </Animated.View>
+      )}
+      {walletItems.length > 0 && (
+        <Animated.View entering={FadeIn.duration(FADE_TRANSITION_DURATION)}>
+          <DndProvider activationDelay={DRAG_ACTIVATION_DELAY} disabled={!editMode}>
+            <DraggableScrollView
+              onOrderChange={onOrderChange}
+              style={{ maxHeight: LIST_MAX_HEIGHT, marginHorizontal: -PANEL_INSET_HORIZONTAL, paddingHorizontal: PANEL_INSET_HORIZONTAL }}
+              autoScrollInsets={{ bottom: FOOTER_HEIGHT - 24 }}
+              contentContainerStyle={{ paddingBottom: FOOTER_HEIGHT - 24 }}
+            >
+              {renderHeader()}
+              {draggableUnpinnedWalletItems.map(item => renderScrollItem(item))}
+            </DraggableScrollView>
+          </DndProvider>
+        </Animated.View>
+      )}
     </Box>
   );
 }
