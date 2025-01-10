b8161a7d44cc7c1ea0f9ada3328e1609ac675fda
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2025-01-10 01:31:51 -0500

    Migrate dapps query to createQueryStore (#6385)
    * Migrate dapps query to `createQueryStore`, clean up types
    
    * Replace old haptics with turbo-haptics
    
    * Bump up `cacheTime` and `staleTime`
    
    * Fix merge conflicts, enable `keepPreviousData`

--- src/resources/metadata/dapps.tsx
+++ src/resources/metadata/dapps.tsx
@@ -1,100 +1,87 @@
 import { formatUrl } from '@/components/DappBrowser/utils';
 import { metadataClient } from '@/graphql';
 import { RainbowError, logger } from '@/logger';
-import { createQueryKey } from '@/react-query';
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
-import React, { createContext, useContext } from 'react';
+import { createQueryStore } from '@/state/internal/createQueryStore';
+import { time } from '@/utils/time';
 
 export type Dapp = {
-  name: string;
-  shortName: string;
-  description: string;
-  url: string;
-  urlDisplay: string;
-  iconUrl: string;
-  status: string;
-  trending: boolean;
   colors: {
-    primary: string;
     fallback?: string | null;
+    primary: string;
     shadow?: string | null;
   };
-  report: { url: string };
+  iconUrl: string;
   isDirect?: boolean;
+  name: string;
+  report: { url: string };
   search: {
     normalizedName: string;
     normalizedNameTokens: string[];
     normalizedUrlTokens: string[];
   };
+  shortName: string;
+  status: string;
+  trending: boolean;
+  url: string;
+  urlDisplay: string;
 };
 
-const QUERY_KEY = createQueryKey('dApps', {}, { persisterVersion: 2 });
-
-export function useDapps(config?: UseQueryOptions<Dapp[]>): { dapps: Dapp[] } {
-  const query = useQuery<Dapp[]>(
-    QUERY_KEY,
-    async () => {
-      try {
-        const response = await metadataClient.getdApps();
-        if (!response || !response.dApps) {
-          return [];
-        }
-
-        return response.dApps
-          .filter(dapp => dapp && dapp.status !== 'SCAM')
-          .map(dapp => ({
-            name: dapp!.name,
-            shortName: dapp!.shortName,
-            description: '', // Remove to cut down on total object size
-            trending: dapp!.trending || false,
-            url: dapp!.url,
-            urlDisplay: formatUrl(dapp!.url),
-            iconUrl: dapp!.iconURL,
-            status: dapp!.status,
-            colors: { primary: dapp!.colors.primary, fallback: dapp!.colors.fallback, shadow: dapp!.colors.shadow },
-            report: { url: dapp!.report.url },
-            search: {
-              normalizedName: dapp!.name.toLowerCase().split(' ').filter(Boolean).join(' '),
-              normalizedNameTokens: dapp!.name.toLowerCase().split(' ').filter(Boolean),
-              normalizedUrlTokens: dapp!.url
-                .toLowerCase()
-                .replace(/(^\w+:|^)\/\//, '') // Remove protocol from URL
-                .split(/\/|\?|&|=|\./) // Split the URL into tokens
-                .filter(Boolean),
-            },
-          }));
-      } catch (e: any) {
-        logger.error(new RainbowError('[dapps]: Failed to fetch dApps'), { message: e.message });
-        return [];
-      }
-    },
-    {
-      staleTime: 1000 * 60 * 20, // 20 minutes
-      cacheTime: 1000 * 60 * 60 * 24 * 2, // 2 days
-      retry: 3,
-      keepPreviousData: true,
-      ...config,
-    }
-  );
-
-  return { dapps: query.data ?? [] };
-}
-
-interface DappsContextType {
+type DappsState = {
   dapps: Dapp[];
-}
+  findDappByHostname: (hostname: string) => Dapp | undefined;
+};
 
-const DappsContext = createContext<DappsContextType | undefined>(undefined);
+export const useBrowserDappsStore = createQueryStore<Dapp[], never, DappsState>(
+  {
+    fetcher: fetchDapps,
+    setData: ({ data, set }) => set({ dapps: data }),
+    cacheTime: time.weeks(1),
+    keepPreviousData: true,
+    staleTime: time.minutes(30),
+  },
 
-export const useDappsContext = () => {
-  const context = useContext(DappsContext);
-  if (!context) {
-    throw new Error('useDappsContext must be used within a DappsContextProvider');
+  (_, get) => ({
+    dapps: [],
+    findDappByHostname: (hostname: string) => get().dapps.find(dapp => dapp.urlDisplay === hostname),
+  }),
+
+  { storageKey: 'browserDapps' }
+);
+
+async function fetchDapps(): Promise<Dapp[]> {
+  try {
+    const response = await metadataClient.getdApps();
+
+    if (!response || !response.dApps) return [];
+
+    return response.dApps
+      .filter(dapp => dapp && dapp.status !== 'SCAM')
+      .map(dapp =>
+        dapp
+          ? {
+              colors: { primary: dapp.colors.primary, fallback: dapp.colors.fallback, shadow: dapp.colors.shadow },
+              iconUrl: dapp.iconURL,
+              name: dapp.name,
+              report: { url: dapp.report.url },
+              search: {
+                normalizedName: dapp.name.toLowerCase().split(' ').filter(Boolean).join(' '),
+                normalizedNameTokens: dapp.name.toLowerCase().split(' ').filter(Boolean),
+                normalizedUrlTokens: dapp.url
+                  .toLowerCase()
+                  .replace(/(^\w+:|^)\/\//, '') // Remove protocol from URL
+                  .split(/\/|\?|&|=|\./) // Split the URL into tokens
+                  .filter(Boolean),
+              },
+              shortName: dapp.shortName,
+              status: dapp.status,
+              trending: dapp.trending || false,
+              url: dapp.url,
+              urlDisplay: formatUrl(dapp.url),
+            }
+          : ({} as Dapp)
+      );
+  } catch (e: unknown) {
+    logger.error(new RainbowError('[dapps]: Failed to fetch dApps'), { message: e instanceof Error ? e.message : 'Unknown error' });
+    return [];
   }
-  return context;
-};
-
-export const DappsContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const { dapps } = useDapps();
-  return <DappsContext.Provider value={{ dapps }}>{children}</DappsContext.Provider>;
-};
+}
