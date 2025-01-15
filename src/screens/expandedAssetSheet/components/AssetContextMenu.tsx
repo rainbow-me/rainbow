e2b2da89d27a1f48eed99df63cbc0ae76dea5307
Kane Thomas <kmt8vn@virginia.edu>
2025-01-15 16:57:10 -0500

    separate out context menu button, add pin & hide functionality

---
+++ src/screens/expandedAssetSheet/components/AssetContextMenu.tsx
@@ -0,0 +1,154 @@
+import React, { useMemo, useEffect } from 'react';
+import * as i18n from '@/languages';
+import { DropdownMenu, MenuConfig, MenuItem } from '@/components/DropdownMenu';
+import { SheetActionButton } from '@/components/sheet';
+import { useCoinListFinishEditingOptions, useCoinListEditOptions } from '@/hooks';
+import { useExpandedAssetSheetContext } from '../context/ExpandedAssetSheetContext';
+import { useUserAssetsStore } from '@/state/assets/userAssets';
+import { ethereumUtils } from '@/utils';
+import EditAction from '@/helpers/EditAction';
+import Clipboard from '@react-native-clipboard/clipboard';
+
+const ContextMenuActions = {
+  BlockExplorer: 'block_explorer',
+  Share: 'share',
+  Copy: 'copy',
+  Pin: 'pin',
+  Unpin: 'unpin',
+  Hide: 'hide',
+  Unhide: 'unhide',
+} as const;
+
+type ContextMenuAction = (typeof ContextMenuActions)[keyof typeof ContextMenuActions];
+
+export function AssetContextMenu() {
+  const { accentColors, basicAsset: asset } = useExpandedAssetSheetContext();
+
+  const { clearSelectedCoins, pushSelectedCoin } = useCoinListEditOptions();
+  const setHiddenAssets = useUserAssetsStore(state => state.setHiddenAssets);
+
+  const { currentAction, setPinnedCoins } = useCoinListFinishEditingOptions();
+
+  useEffect(() => {
+    // Ensure this expanded state's asset is always actively inside
+    // the CoinListEditOptions selection queue
+    pushSelectedCoin(asset.uniqueId);
+
+    // Clear CoinListEditOptions selection queue on unmount.
+    return () => clearSelectedCoins();
+  }, [asset, clearSelectedCoins, currentAction, pushSelectedCoin]);
+
+  const menuConfig = useMemo<MenuConfig<ContextMenuAction>>(() => {
+    const menuItems = [] as MenuItem<ContextMenuAction>[];
+
+    if (asset.address && !asset.isNativeAsset) {
+      menuItems.push({
+        actionKey: ContextMenuActions.BlockExplorer,
+        actionTitle: i18n.t('expanded_state.asset.menu.view_on', {
+          blockExplorerName: ethereumUtils.getBlockExplorer({ chainId: asset.chainId }),
+        }),
+        icon: {
+          iconType: 'SYSTEM',
+          iconValue: 'network',
+        },
+      });
+    }
+
+    if (asset.address && !asset.isNativeAsset) {
+      menuItems.push({
+        actionKey: ContextMenuActions.Copy,
+        actionTitle: i18n.t('expanded_state.asset.menu.copy_contract_address'),
+        actionSubtitle: asset.address.slice(0, 6) + '...' + asset.address.slice(-4),
+        icon: {
+          iconType: 'SYSTEM',
+          iconValue: 'square.on.square',
+        },
+      });
+    }
+
+    menuItems.push({
+      actionKey: ContextMenuActions.Share,
+      actionTitle: i18n.t('expanded_state.asset.menu.share'),
+      icon: {
+        iconType: 'SYSTEM',
+        iconValue: 'square.and.arrow.up',
+      },
+    });
+
+    if (currentAction === EditAction.unhide) {
+      menuItems.push({
+        actionKey: ContextMenuActions.Unhide,
+        actionTitle: i18n.t('expanded_state.asset.menu.unhide'),
+        icon: {
+          iconType: 'SYSTEM',
+          iconValue: 'eye.slash',
+        },
+      });
+    } else {
+      menuItems.push({
+        actionKey: ContextMenuActions.Hide,
+        actionTitle: i18n.t('expanded_state.asset.menu.hide'),
+        icon: {
+          iconType: 'SYSTEM',
+          iconValue: 'eye.slash',
+        },
+      });
+    }
+
+    if (currentAction === EditAction.unpin) {
+      menuItems.push({
+        actionKey: ContextMenuActions.Unpin,
+        actionTitle: i18n.t('expanded_state.asset.menu.unpin'),
+        icon: {
+          iconType: 'SYSTEM',
+          iconValue: 'pin',
+        },
+      });
+    } else {
+      menuItems.push({
+        actionKey: ContextMenuActions.Pin,
+        actionTitle: i18n.t('expanded_state.asset.menu.pin'),
+        icon: {
+          iconType: 'SYSTEM',
+          iconValue: 'pin',
+        },
+      });
+    }
+
+    return {
+      menuItems,
+    };
+  }, [asset, currentAction]);
+
+  const handlePressMenuItem = (actionKey: ContextMenuAction) => {
+    switch (actionKey) {
+      case ContextMenuActions.Copy:
+        Clipboard.setString(asset.address);
+        break;
+      case ContextMenuActions.Share:
+        // TODO:
+        break;
+      case ContextMenuActions.BlockExplorer:
+        ethereumUtils.openTokenEtherscanURL({ address: asset.address, chainId: asset.chainId });
+        break;
+      case ContextMenuActions.Pin:
+        setPinnedCoins();
+        break;
+      case ContextMenuActions.Unpin:
+        setPinnedCoins();
+        break;
+      case ContextMenuActions.Hide:
+        setHiddenAssets([asset.uniqueId]);
+        break;
+      case ContextMenuActions.Unhide:
+        setHiddenAssets([asset.uniqueId]);
+        break;
+    }
+  };
+
+  return (
+    <DropdownMenu<ContextMenuAction> menuConfig={menuConfig} onPressMenuItem={handlePressMenuItem}>
+      <SheetActionButton color={accentColors.opacity100} isSquare label={'􀍠'} />
+    </DropdownMenu>
+  );
+}
--- src/screens/expandedAssetSheet/components/SheetFooter.tsx
+++ src/screens/expandedAssetSheet/components/SheetFooter.tsx
@@ -1,26 +1,16 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { EasingGradient } from '@/components/easing-gradient/EasingGradient';
 import { Box, Column, Columns } from '@/design-system';
 import { useExpandedAssetSheetContext } from '../context/ExpandedAssetSheetContext';
 import * as i18n from '@/languages';
 import { SwapAssetType } from '@/__swaps__/types/swap';
-import { BuyActionButton, SendActionButton, SheetActionButton, SwapActionButton } from '@/components/sheet';
-import { DropdownMenu, MenuConfig, MenuItem } from '@/components/DropdownMenu';
-import Clipboard from '@react-native-clipboard/clipboard';
-import { ethereumUtils } from '@/utils';
+import { BuyActionButton, SendActionButton, SwapActionButton } from '@/components/sheet';
 import { useRemoteConfig } from '@/model/remoteConfig';
 import { useBackendNetworksStore } from '@/state/backendNetworks/backendNetworks';
+import { AssetContextMenu } from './AssetContextMenu';
 
 // 32px for the gradient + 46px for the buttons + 44px for the bottom padding
 export const SHEET_FOOTER_HEIGHT = 32 + 46 + 44;
-
-export enum ContextMenuActions {
-  BlockExplorer = 'block_explorer',
-  Share = 'share',
-  Copy = 'copy',
-}
-
-type ContextMenuAction = ContextMenuActions.BlockExplorer | ContextMenuActions.Share | ContextMenuActions.Copy;
 
 export function SheetFooter() {
   const { accentColors, basicAsset: asset, isOwnedAsset } = useExpandedAssetSheetContext();
@@ -34,62 +24,6 @@ export function SheetFooter() {
   const hasEth = true;
   const isSwappable = isOwnedAsset && swapEnabled;
 
-  const menuConfig = useMemo<MenuConfig<ContextMenuAction>>(() => {
-    const menuItems = [] as MenuItem<ContextMenuAction>[];
-
-    if (asset.address && !asset.isNativeAsset) {
-      menuItems.push({
-        actionKey: ContextMenuActions.BlockExplorer,
-        actionTitle: i18n.t('expanded_state.asset.menu.view_on', {
-          blockExplorerName: ethereumUtils.getBlockExplorer({ chainId: asset.chainId }),
-        }),
-        icon: {
-          iconType: 'SYSTEM',
-          iconValue: 'network',
-        },
-      });
-    }
-
-    if (asset.address && !asset.isNativeAsset) {
-      menuItems.push({
-        actionKey: ContextMenuActions.Copy,
-        actionTitle: i18n.t('expanded_state.asset.menu.copy_contract_address'),
-        actionSubtitle: asset.address.slice(0, 6) + '...' + asset.address.slice(-4),
-        icon: {
-          iconType: 'SYSTEM',
-          iconValue: 'square.on.square',
-        },
-      });
-    }
-
-    menuItems.push({
-      actionKey: ContextMenuActions.Share,
-      actionTitle: i18n.t('expanded_state.asset.menu.share'),
-      icon: {
-        iconType: 'SYSTEM',
-        iconValue: 'square.and.arrow.up',
-      },
-    });
-
-    return {
-      menuItems,
-    };
-  }, [asset]);
-
-  const handlePressMenuItem = (actionKey: ContextMenuAction) => {
-    switch (actionKey) {
-      case ContextMenuActions.Copy:
-        Clipboard.setString(asset.address);
-        break;
-      case ContextMenuActions.Share:
-        // TODO:
-        break;
-      case ContextMenuActions.BlockExplorer:
-        ethereumUtils.openTokenEtherscanURL({ address: asset.address, chainId: asset.chainId });
-        break;
-    }
-  };
-
   return (
     <Box pointerEvents="box-none" position="absolute" bottom="0px" width="full">
       <EasingGradient
@@ -102,9 +36,7 @@ export function SheetFooter() {
       <Box backgroundColor={accentColors.background} width="full" paddingVertical="2px" paddingHorizontal="24px" paddingBottom="44px">
         <Columns space="16px">
           <Column width="content">
-            <DropdownMenu<ContextMenuAction> menuConfig={menuConfig} onPressMenuItem={handlePressMenuItem}>
-              <SheetActionButton color={accentColors.opacity100} isSquare label={'􀍠'} />
-            </DropdownMenu>
+            <AssetContextMenu />
           </Column>
           {isSwappable && <SwapActionButton asset={asset} color={accentColors.opacity100} inputType={SwapAssetType.inputAsset} />}
           {isTransferable && <SendActionButton asset={asset} color={accentColors.opacity100} />}
