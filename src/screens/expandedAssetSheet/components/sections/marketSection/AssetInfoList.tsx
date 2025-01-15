f2d9c7ae0f330c3131d501f297896be16ba77fd1
Kane Thomas <kmt8vn@virginia.edu>
2025-01-15 16:04:07 -0500

    fix market stat info list formatting

--- src/screens/expandedAssetSheet/components/sections/marketSection/AssetInfoList.tsx
+++ src/screens/expandedAssetSheet/components/sections/marketSection/AssetInfoList.tsx
@@ -2,22 +2,13 @@ import React, { useMemo } from 'react';
 import { Bleed, AnimatedText, Box, Stack, Text, TextIcon, TextShadow } from '@/design-system';
 import { bigNumberFormat } from '@/helpers/bigNumberFormat';
 import { Row } from '../../shared/Row';
-import { greaterThanOrEqualTo } from '@/helpers/utilities';
+import { abbreviateNumber, greaterThanOrEqualTo } from '@/helpers/utilities';
 import Animated, { useAnimatedStyle, useDerivedValue, useSharedValue, withSpring } from 'react-native-reanimated';
 import { GestureHandlerButton } from '@/__swaps__/screens/Swap/components/GestureHandlerButton';
 import { useExpandedAssetSheetContext } from '@/screens/expandedAssetSheet/context/ExpandedAssetSheetContext';
 import { SPRING_CONFIGS } from '@/components/animations/animationConfigs';
 
 const DEFAULT_VISIBLE_ITEMS = 2;
-
-// eslint-disable-next-line react-hooks/exhaustive-deps
-const assetInfo = {
-  marketCap: 987620000,
-  fullyDilutedValuation: 1350000000,
-  rank: 3,
-  circulatingSupply: 868471,
-  maxSupply: 100000577,
-};
 
 function AssetInfoItem({ title, value, icon, highlighted }: { title: string; value: string; icon: string; highlighted: boolean }) {
   return (
@@ -40,7 +31,7 @@ function AssetInfoItem({ title, value, icon, highlighted }: { title: string; val
 }
 
 export function AssetInfoList() {
-  const { accentColors } = useExpandedAssetSheetContext();
+  const { accentColors, assetMetadata: metadata } = useExpandedAssetSheetContext();
   const isExpanded = useSharedValue(false);
 
   const expandedText = useDerivedValue(() => {
@@ -61,44 +52,47 @@ export function AssetInfoList() {
   const assetInfoItems = useMemo(() => {
     const items: { title: string; value: string; icon: string }[] = [];
 
-    if (assetInfo.marketCap) {
+    if (!metadata) return items;
+
+    if (metadata.marketCap) {
       items.push({
         title: 'Market Cap',
-        value: bigNumberFormat(assetInfo.marketCap, 'USD', greaterThanOrEqualTo(assetInfo.marketCap, 1000000000)),
+        value: bigNumberFormat(metadata.marketCap, 'USD', greaterThanOrEqualTo(metadata.marketCap, 1000000000)),
         icon: '􁎢',
       });
     }
-    if (assetInfo.fullyDilutedValuation) {
+    if (metadata.fullyDilutedValuation) {
       items.push({
         title: 'Fully Diluted Valuation',
-        value: bigNumberFormat(assetInfo.fullyDilutedValuation, 'USD', greaterThanOrEqualTo(assetInfo.fullyDilutedValuation, 10000)),
+        value: bigNumberFormat(metadata.fullyDilutedValuation, 'USD', greaterThanOrEqualTo(metadata.fullyDilutedValuation, 10000)),
         icon: '􀠏',
       });
     }
-    if (assetInfo.rank) {
-      items.push({
-        title: 'Rank',
-        value: '#' + assetInfo.rank.toString(),
-        icon: '􀄯',
-      });
-    }
-    if (assetInfo.circulatingSupply) {
+    // BLOCKED: Do not currently have rank data
+    // if (metadata.rank) {
+    //   items.push({
+    //     title: 'Rank',
+    //     value: `#${metadata.rank}`,
+    //     icon: '􀄯',
+    //   });
+    // }
+    if (metadata.circulatingSupply) {
       items.push({
         title: 'Circulating Supply',
-        value: '0',
+        value: abbreviateNumber(metadata.circulatingSupply),
         icon: '􂣽',
       });
     }
-    if (assetInfo.maxSupply) {
+    if (metadata.totalSupply) {
       items.push({
         title: 'Max Supply',
-        value: '0',
+        value: abbreviateNumber(metadata.totalSupply),
         icon: '􀅃',
       });
     }
 
     return items;
-  }, [assetInfo]);
+  }, [metadata]);
 
   return (
     <Stack space="4px">
@@ -106,44 +100,48 @@ export function AssetInfoList() {
         <AssetInfoItem key={item.title} title={item.title} value={item.value} icon={item.icon} highlighted={index % 2 === 0} />
       ))}
       <Animated.View style={expandedItemsContainerStyle}>
-        {assetInfoItems.slice(DEFAULT_VISIBLE_ITEMS).map((item, index) => (
-          <AssetInfoItem key={item.title} title={item.title} value={item.value} icon={item.icon} highlighted={index % 2 !== 0} />
-        ))}
+        {assetInfoItems.slice(DEFAULT_VISIBLE_ITEMS).map(item => {
+          const index = assetInfoItems.indexOf(item);
+          return <AssetInfoItem key={item.title} title={item.title} value={item.value} icon={item.icon} highlighted={index % 2 === 0} />;
+        })}
       </Animated.View>
-      <GestureHandlerButton
-        scaleTo={0.96}
-        hapticTrigger="tap-end"
-        onPressWorklet={() => {
-          'worklet';
-          isExpanded.value = !isExpanded.value;
-        }}
-      >
-        <Row highlighted={true}>
-          <Bleed vertical="4px" horizontal="2px">
-            <Box width="full" flexDirection="row" alignItems="center" gap={8}>
-              <Box
-                width={{ custom: 20 }}
-                height={{ custom: 20 }}
-                borderRadius={40}
-                style={{ backgroundColor: accentColors.opacity6 }}
-                borderWidth={1.33}
-                borderColor={{ custom: accentColors.opacity2 }}
-                alignItems="center"
-                justifyContent="center"
-              >
-                <AnimatedText weight="black" align="center" size="icon 10px" color="labelQuaternary">
-                  {expandedTextIcon}
-                </AnimatedText>
+      {assetInfoItems.length > DEFAULT_VISIBLE_ITEMS && (
+        <GestureHandlerButton
+          scaleTo={0.96}
+          hapticTrigger="tap-end"
+          onPressWorklet={() => {
+            'worklet';
+            isExpanded.value = !isExpanded.value;
+          }}
+        >
+          {/* TODO: Allow Row to accept animated value for highlighted prop */}
+          <Row highlighted={true}>
+            <Bleed vertical="4px" horizontal="2px">
+              <Box width="full" flexDirection="row" alignItems="center" gap={8}>
+                <Box
+                  width={{ custom: 20 }}
+                  height={{ custom: 20 }}
+                  borderRadius={40}
+                  style={{ backgroundColor: accentColors.opacity6 }}
+                  borderWidth={1.33}
+                  borderColor={{ custom: accentColors.opacity2 }}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <AnimatedText weight="black" align="center" size="icon 10px" color="labelQuaternary">
+                    {expandedTextIcon}
+                  </AnimatedText>
+                </Box>
+                <TextShadow blur={12} shadowOpacity={0.24}>
+                  <AnimatedText weight="semibold" size="17pt" align="center" color="labelTertiary">
+                    {expandedText}
+                  </AnimatedText>
+                </TextShadow>
               </Box>
-              <TextShadow blur={12} shadowOpacity={0.24}>
-                <AnimatedText weight="semibold" size="17pt" align="center" color="labelTertiary">
-                  {expandedText}
-                </AnimatedText>
-              </TextShadow>
-            </Box>
-          </Bleed>
-        </Row>
-      </GestureHandlerButton>
+            </Bleed>
+          </Row>
+        </GestureHandlerButton>
+      )}
     </Stack>
   );
 }
