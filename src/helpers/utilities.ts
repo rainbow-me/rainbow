dc3704b7eba7a2bf3399032d9798bbe3f1cee836
Kane Thomas <kmt8vn@virginia.edu>
2025-01-21 12:47:55 -0500

    add number rounding utility

--- src/helpers/utilities.ts
+++ src/helpers/utilities.ts
@@ -504,6 +504,28 @@ export const flattenDeep = (arr: unknown[]): unknown[] =>
 export const times = (n: number, fn: (i: number) => unknown) => Array.from({ length: n }, (_, i) => fn(i));
 
 /**
+ * @desc Round a number's significant digits to the nearest significant 1 or 5, e.g 1000 -> 1000, 1300 -> 1500, 1800 -> 2000
+ */
+export function roundToSignificant1or5(number: number): number {
+  if (number === 0) return 0;
+
+  // Find the magnitude (power of 10) of the number
+  const magnitude = Math.floor(Math.log10(number));
+  const scale = Math.pow(10, magnitude);
+
+  // Get the first digit
+  const firstDigit = number / scale;
+
+  // Round to nearest 1 or 5
+  let roundedFirstDigit: number;
+  if (firstDigit < 3) roundedFirstDigit = 1;
+  else if (firstDigit < 7.5) roundedFirstDigit = 5;
+  else roundedFirstDigit = 10;
+
+  return roundedFirstDigit * scale;
+}
+
+/**
  * @desc Creates an object composed of the omitted object properties by some predicate function.
  */
 export const omitBy = <T>(obj: Dictionary<T>, predicate: ValueKeyIteratee<T>): Dictionary<T> => {
