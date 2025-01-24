81ec118c34b1102de54e22a9830a11c776aa4c70
Kane Thomas <kmt8vn@virginia.edu>
2025-01-24 12:01:13 -0500

    comment out market stats card

--- src/screens/expandedAssetSheet/components/sections/marketSection/MarketStatsSection.tsx
+++ src/screens/expandedAssetSheet/components/sections/marketSection/MarketStatsSection.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Stack } from '@/design-system';
-import { MarketStatsCard } from './MarketStatsCard';
+// import { MarketStatsCard } from './MarketStatsCard';
 import { AssetInfoList } from './AssetInfoList';
 
 export function MarketStatsSection() {
   return (
     <Stack space="24px">
       {/* BLOCKED: blocked by backend for data */}
-      <MarketStatsCard />
+      {/* <MarketStatsCard /> */}
       <AssetInfoList />
     </Stack>
   );
