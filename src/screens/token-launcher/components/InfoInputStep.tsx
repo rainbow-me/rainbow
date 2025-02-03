5671cd9d544128089cb34b45ed27482d472f2c5d
Kane Thomas <kmt8vn@virginia.edu>
2025-02-03 14:20:51 -0500

    common ui abstractions, network field, image uploading / ui adapt, other misc.

--- src/screens/token-launcher/components/InfoInputStep.tsx
+++ src/screens/token-launcher/components/InfoInputStep.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
-import { Box } from '@/design-system';
+import { Bleed, Box, Text } from '@/design-system';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
 import { TOKEN_PREVIEW_BAR_HEIGHT } from './TokenPreviewBar';
 import { safeAreaInsetValues } from '@/utils';
 import { TOKEN_LAUNCHER_HEADER_HEIGHT } from './TokenLauncherHeader';
 import { SingleFieldInput } from './SingleFieldInput';
+import { TokenLogo } from './TokenLogo';
+import { useTokenLauncherStore } from '../state/tokenLauncherStore';
+import { CollapsableField } from './CollapsableField';
+import Animated, { Layout } from 'react-native-reanimated';
+import { NetworkField } from './NetworkField';
+import { LinksSection } from './LinksSection';
+
+function TotalSupplyInput() {
+  const setTotalSupply = useTokenLauncherStore(state => state.setTotalSupply);
+  const formattedTotalSupply = useTokenLauncherStore(state => state.formattedTotalSupply());
+
+  return (
+    <SingleFieldInput
+      onInputChange={text => {
+        setTotalSupply(parseInt(text));
+      }}
+      inputMode="numeric"
+      title="Total Supply"
+      subtitle={formattedTotalSupply}
+      placeholder="1,000,000"
+    />
+  );
+}
+
+function DescriptionSection() {
+  return (
+    <CollapsableField title="Description">
+      <SingleFieldInput title="" placeholder="Enter coin description" />
+    </CollapsableField>
+  );
+}
 
 export function InfoInputStep() {
+  const setSymbol = useTokenLauncherStore(state => state.setSymbol);
+
   return (
     <KeyboardAwareScrollView
       contentOffset={{ x: 0, y: -TOKEN_LAUNCHER_HEADER_HEIGHT }}
@@ -19,32 +52,62 @@ export function InfoInputStep() {
       }}
       keyboardDismissMode="interactive"
       bottomOffset={TOKEN_PREVIEW_BAR_HEIGHT + 36}
+      extraKeyboardSpace={TOKEN_PREVIEW_BAR_HEIGHT}
       // disableScrollOnKeyboardHide={true}
       // extraKeyboardSpace={-(TOKEN_PREVIEW_BAR_HEIGHT + safeAreaInsetValues.bottom)}
     >
+      {/* <Animated.View style={{ width: '100%', flex: 1 }} layout={Layout.springify()}> */}
       <Box width="full" gap={8} alignItems="center" paddingHorizontal="20px">
-        <SingleFieldInput
-          validationWorklet={text => {
-            'worklet';
-            if (text.trim().length > 4) {
-              return 'Too long, friend';
-            }
-            return '';
-          }}
-          onInputChange={text => {
-            console.log('text', text);
-          }}
-          style={{ textTransform: 'uppercase' }}
-          autoCapitalize="characters"
-          title="Ticker"
-          placeholder="$NAME"
-        />
-        <SingleFieldInput title="Name" placeholder="Enter coin name" />
-        <SingleFieldInput inputMode="numeric" title="Total Supply" defaultValue={'1,000,000'} placeholder="1,000,000" />
-        <Box borderRadius={16} backgroundColor="blue" width="full" height={150} />
-        <SingleFieldInput title="Test" placeholder="Testing" />
-        <Box borderRadius={16} backgroundColor="purple" width="full" height={250} />
+        <Box paddingBottom={'16px'}>
+          <TokenLogo />
+        </Box>
+        <Box gap={16} width="full" paddingVertical="20px">
+          <Text color="labelSecondary" size="13pt" weight="heavy">
+            Required Info
+          </Text>
+          <Box gap={8}>
+            <SingleFieldInput
+              validationWorklet={text => {
+                'worklet';
+                if (text.trim().length > 4) {
+                  return 'Too long, friend';
+                }
+                return '';
+              }}
+              onInputChange={text => {
+                setSymbol(text);
+              }}
+              style={{ textTransform: 'uppercase' }}
+              autoCapitalize="characters"
+              title="Ticker"
+              placeholder="$NAME"
+            />
+            <SingleFieldInput title="Name" placeholder="Enter coin name" />
+            <TotalSupplyInput />
+            <NetworkField />
+          </Box>
+        </Box>
+        {/* <Bleed horizontal={'20px'}> */}
+        <Box height={1} width="full" backgroundColor="rgba(245, 248, 255, 0.06)" />
+        {/* </Bleed> */}
+        <Box gap={16} width="full" paddingVertical="20px">
+          <Text color="labelSecondary" size="13pt" weight="heavy">
+            Optional
+          </Text>
+          <Box gap={8} width={'full'}>
+            <DescriptionSection />
+            <LinksSection />
+          </Box>
+        </Box>
+        <Box height={1} width="full" backgroundColor="rgba(245, 248, 255, 0.06)" />
+        <Box gap={16} width="full" paddingVertical="20px">
+          <Text color="labelSecondary" size="13pt" weight="heavy">
+            Token Allocation
+          </Text>
+          <Box gap={8} width={'full'}></Box>
+        </Box>
       </Box>
+      {/* </Animated.View> */}
     </KeyboardAwareScrollView>
   );
 }
---
+++ src/screens/token-launcher/state/tokenLauncherStore.ts
@@ -0,0 +1,54 @@
+import { abbreviateNumber } from '@/helpers/utilities';
+import { createRainbowStore } from '@/state/internal/createRainbowStore';
+import getDominantColorFromImage from '@/utils/getDominantColorFromImage';
+import { DEFAULT_TOKEN_IMAGE_PRIMARY_COLOR, DEFAULT_CHAIN_ID, DEFAULT_TOTAL_SUPPLY } from '../constants';
+
+interface TokenLauncherStore {
+  // base state
+  imageUri: string;
+  imageUrl: string;
+  imagePrimaryColor: string;
+  name: string;
+  symbol: string;
+  chainId: number;
+  totalSupply: number;
+  links: Record<string, string>;
+  // derived state
+  formattedTotalSupply: () => string;
+  // actions
+  setImageUri: (uri: string) => void;
+  setImageUrl: (url: string) => void;
+  setName: (name: string) => void;
+  setSymbol: (symbol: string) => void;
+  setChainId: (chainId: number) => void;
+  setTotalSupply: (totalSupply: number) => void;
+  setLink: (link: string, url: string) => void;
+}
+
+export const useTokenLauncherStore = createRainbowStore<TokenLauncherStore>((set, get) => ({
+  imageUri: '',
+  imageUrl: '',
+  imagePrimaryColor: DEFAULT_TOKEN_IMAGE_PRIMARY_COLOR,
+  name: '',
+  symbol: '',
+  chainId: DEFAULT_CHAIN_ID,
+  totalSupply: DEFAULT_TOTAL_SUPPLY,
+  links: {},
+  formattedTotalSupply: () => abbreviateNumber(get().totalSupply),
+  setImageUri: (uri: string) => {
+    set({ imageUri: uri });
+  },
+  setImageUrl: async (url: string) => {
+    try {
+      // TODO: ideally this would be done in the setImageUri function, but the native library doesn't support it
+      set({ imageUrl: url, imagePrimaryColor: await getDominantColorFromImage(url, '#333333') });
+    } catch (e) {
+      console.log('error extracting color', e);
+    }
+  },
+  setName: (name: string) => set({ name }),
+  setSymbol: (symbol: string) => set({ symbol }),
+  setChainId: (chainId: number) => set({ chainId }),
+  setTotalSupply: (totalSupply: number) => set({ totalSupply }),
+  setLink: (link: string, url: string) => set({ links: { ...get().links, [link]: url } }),
+}));
