3df649805a05b90e682991a252993e61f20712b2
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2025-02-04 18:54:49 +0000

    Disable `tokenMetadata` persistence while the store is unused

--- src/resources/tokenMetadata/tokenMetadata.ts
+++ src/resources/tokenMetadata/tokenMetadata.ts
@@ -22,8 +22,9 @@ export const useTokenMetadataStore = createQueryStore<TokenMetadata | null, Toke
   {
     fetcher: (params: TokenMetadataParams) => fetchTokenMetadata(params),
     cacheTime: time.weeks(1),
-    enabled: false,
+    enabled: false, // TODO: Remove once this store is ready to be used
     keepPreviousData: true,
+    //
     params: {
       address: '',
       chainId: ChainId.mainnet,
@@ -34,7 +35,7 @@ export const useTokenMetadataStore = createQueryStore<TokenMetadata | null, Toke
   () => ({
     metadata: null,
   }),
-  { storageKey: 'tokenMetadata' }
+  // { storageKey: 'tokenMetadata' } // TODO: Uncomment once this store is ready to be used
 );
 
 async function fetchTokenMetadata({ address, chainId, currency }: TokenMetadataParams): Promise<TokenMetadata | null> {
