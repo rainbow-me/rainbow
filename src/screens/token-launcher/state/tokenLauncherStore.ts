9b0602ee2c975f8504042a1a276c089bb92cba35
Kane Thomas <kmt8vn@virginia.edu>
2025-02-04 12:32:58 -0500

    link section separate input & url

--- src/screens/token-launcher/state/tokenLauncherStore.ts
+++ src/screens/token-launcher/state/tokenLauncherStore.ts
@@ -4,7 +4,7 @@ import getDominantColorFromImage from '@/utils/getDominantColorFromImage';
 import { DEFAULT_TOKEN_IMAGE_PRIMARY_COLOR, DEFAULT_CHAIN_ID, DEFAULT_TOTAL_SUPPLY } from '../constants';
 
 type LinkType = 'website' | 'x' | 'telegram' | 'farcaster' | 'discord' | 'other';
-export type Link = { input: string; type: LinkType };
+export type Link = { input: string; type: LinkType; url: string };
 interface TokenLauncherStore {
   // base state
   imageUri: string;
@@ -29,7 +29,7 @@ interface TokenLauncherStore {
   setChainId: (chainId: number) => void;
   setTotalSupply: (totalSupply: number) => void;
   addLink: (type: LinkType) => void;
-  editLink: (index: number, input: string) => void;
+  editLink: ({ index, input, url }: { index: number; input: string; url: string }) => void;
   deleteLink: (index: number) => void;
   setDescription: (description: string) => void;
   // actions
@@ -48,12 +48,12 @@ export const useTokenLauncherStore = createRainbowStore<TokenLauncherStore>((set
   chainId: DEFAULT_CHAIN_ID,
   totalSupply: DEFAULT_TOTAL_SUPPLY,
   links: [
-    { input: '', type: 'website' },
-    { input: '', type: 'x' },
-    { input: '', type: 'telegram' },
-    { input: '', type: 'farcaster' },
-    // { input: '', type: 'discord' },
-    // { input: '', type: 'other' },
+    { input: '', type: 'website', url: '' },
+    { input: '', type: 'x', url: '' },
+    { input: '', type: 'telegram', url: '' },
+    { input: '', type: 'farcaster', url: '' },
+    // { input: '', type: 'discord', url: '' },
+    // { input: '', type: 'other', url: '' },
   ],
   creatorBuyInEth: 0,
   formattedTotalSupply: () => abbreviateNumber(get().totalSupply),
@@ -73,8 +73,9 @@ export const useTokenLauncherStore = createRainbowStore<TokenLauncherStore>((set
   setDescription: (description: string) => set({ description }),
   setChainId: (chainId: number) => set({ chainId }),
   setTotalSupply: (totalSupply: number) => set({ totalSupply }),
-  addLink: (type: LinkType) => set({ links: [...get().links, { input: '', type }] }),
-  editLink: (index: number, input: string) => set({ links: get().links.map((link, i) => (i === index ? { ...link, input } : link)) }),
+  addLink: (type: LinkType) => set({ links: [...get().links, { input: '', type, url: '' }] }),
+  editLink: ({ index, input, url }: { index: number; input: string; url: string }) =>
+    set({ links: get().links.map((link, i) => (i === index ? { ...link, input, url } : link)) }),
   deleteLink: (index: number) => set({ links: get().links.filter((_, i) => i !== index) }),
   validateForm: () => {
     // TODO: validate all field values before submission to sdk for creation
