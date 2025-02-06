182ab91af4b23efed973188795947bcf038b5073
Kane Thomas <kmt8vn@virginia.edu>
2025-02-06 11:56:03 -0500

    link section add links section

--- src/screens/token-launcher/components/LinksSection.tsx
+++ src/screens/token-launcher/components/LinksSection.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { CollapsableField } from './CollapsableField';
-import { Bleed, Box, IconContainer, Text, TextIcon, TextShadow } from '@/design-system';
-import { useTokenLauncherStore, Link } from '../state/tokenLauncherStore';
+import { Bleed, Box, Column, Columns, IconContainer, Separator, Text, TextIcon, TextShadow } from '@/design-system';
+import { useTokenLauncherStore, Link, LinkType } from '../state/tokenLauncherStore';
 import Animated, { LinearTransition } from 'react-native-reanimated';
 import { SingleFieldInput } from './SingleFieldInput';
 import { ButtonPressAnimation } from '@/components/animations';
 import { SPRING_CONFIGS } from '@/components/animations/animationConfigs';
 import { Icon } from '@/components/icons';
 import { FIELD_INNER_BORDER_RADIUS, INNER_FIELD_BACKGROUND_COLOR } from '../constants';
+import { chunk } from 'lodash';
+import { useTheme } from '@/theme';
 
 const ANIMATION_CONFIG = SPRING_CONFIGS.slowSpring;
 
@@ -15,8 +17,10 @@ const ANIMATION_CONFIG = SPRING_CONFIGS.slowSpring;
 const LINK_SETTINGS = {
   x: {
     Icon: () => <Icon name="x" color={'white'} size={26} />,
-    iconBackgroundColor: 'black',
+    iconBackgroundColor: '#000000',
+    primaryColor: '#000000',
     placeholder: 'Username',
+    displayName: 'X/Twitter',
   },
   telegram: {
     Icon: () => (
@@ -27,7 +31,9 @@ const LINK_SETTINGS = {
       </IconContainer>
     ),
     iconBackgroundColor: '#24A1DE',
+    primaryColor: '#24A1DE',
     placeholder: 'Channel',
+    displayName: 'Telegram',
   },
   // TODO: get warpcast W svg
   farcaster: {
@@ -37,7 +43,9 @@ const LINK_SETTINGS = {
       </TextIcon>
     ),
     iconBackgroundColor: '#855DCD',
+    primaryColor: '#855DCD',
     placeholder: 'Username',
+    displayName: 'Farcaster',
   },
   website: {
     Icon: () => (
@@ -46,9 +54,16 @@ const LINK_SETTINGS = {
       </TextIcon>
     ),
     iconBackgroundColor: '#000000',
+    primaryColor: '#000000',
     placeholder: 'https://token.com',
+    displayName: 'Website',
   },
 };
+
+// hack around not having a grid component
+const LINK_ROWS = chunk(Object.keys(LINK_SETTINGS), 2);
+
+console.log(LINK_ROWS);
 
 export const LAYOUT_ANIMATION = LinearTransition.springify()
   .mass(ANIMATION_CONFIG.mass as number)
@@ -97,6 +112,7 @@ function LinkField({ link, index }: { link: Link; index: number }) {
 }
 
 export function LinksSection() {
+  const { colors } = useTheme();
   const links = useTokenLauncherStore(state => state.links);
   const addLink = useTokenLauncherStore(state => state.addLink);
 
@@ -107,6 +123,59 @@ export function LinksSection() {
           {links.map((link, index) => (
             <LinkField key={`${link.type}-${index}`} link={link} index={index} />
           ))}
+        </Box>
+        {links.length > 0 && (
+          <Box paddingVertical="16px">
+            <Separator color="separatorSecondary" />
+            <Text style={{ paddingTop: 16 }} color="labelSecondary" size="17pt" weight="heavy">
+              Add more
+            </Text>
+          </Box>
+        )}
+        <Box gap={8}>
+          {LINK_ROWS.map((linkTypes, index) => {
+            return (
+              <Columns key={index} space="8px">
+                {linkTypes.map(linkType => {
+                  const { Icon, displayName, iconBackgroundColor, primaryColor } = LINK_SETTINGS[linkType as keyof typeof LINK_SETTINGS];
+                  const backgroundColor = colors.alpha(primaryColor, 0.05);
+                  return (
+                    <Column key={`${linkType}-${index}`}>
+                      <ButtonPressAnimation onPress={() => addLink(linkType as LinkType)}>
+                        <Box
+                          paddingLeft="10px"
+                          paddingRight="16px"
+                          paddingVertical="10px"
+                          borderWidth={2.5}
+                          borderRadius={28}
+                          borderColor="fillTertiary"
+                          backgroundColor={backgroundColor}
+                        >
+                          <Box flexDirection="row" alignItems="center" gap={8}>
+                            <Box
+                              width={20}
+                              height={20}
+                              borderRadius={10}
+                              backgroundColor={iconBackgroundColor}
+                              justifyContent="center"
+                              alignItems="center"
+                            >
+                              <Icon />
+                            </Box>
+                            <TextShadow containerStyle={{ flex: 1 }} blur={12} shadowOpacity={0.24}>
+                              <Text align="center" color="labelSecondary" size="17pt" weight="heavy">
+                                {displayName}
+                              </Text>
+                            </TextShadow>
+                          </Box>
+                        </Box>
+                      </ButtonPressAnimation>
+                    </Column>
+                  );
+                })}
+              </Columns>
+            );
+          })}
         </Box>
       </Animated.View>
     </CollapsableField>
