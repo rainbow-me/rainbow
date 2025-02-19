21a1b5296588cac578f2db6bf63bd4de204f2a6f
Kane Thomas <kmt8vn@virginia.edu>
2025-02-19 11:42:28 -0500

    fix shadow outline effect on review step

--- src/screens/token-launcher/components/SkiaBackground.tsx
+++ src/screens/token-launcher/components/SkiaBackground.tsx
@@ -4,20 +4,17 @@ import {
   Canvas,
   RoundedRect,
   Paint,
-  vec,
-  Blur,
-  Group,
   Shadow,
   useImage,
   Image,
   Fill,
-  ColorMatrix,
-  interpolate,
   BackdropBlur,
+  // BoxShadow,
+  // Box,
+  // rrect,
+  // rect,
 } from '@shopify/react-native-skia';
 import { useTokenLauncherStore } from '../state/tokenLauncherStore';
-import { useTheme } from '@/theme';
-import { Extrapolation } from 'react-native-reanimated';
 import { THICK_BORDER_WIDTH } from '@/__swaps__/screens/Swap/constants';
 import { useTokenLauncherContext } from '../context/TokenLauncherContext';
 
@@ -28,24 +25,26 @@ export function SkiaBackground({ width, height }: { width: number; height: numbe
   const imageUri = useTokenLauncherStore(state => state.imageUri);
   const image = useImage(imageUri);
 
-  const shadowColor = accentColors.opacity12;
-  const shadowOpacity = interpolate(stepIndex.value, [0, 1], [0, 1], Extrapolation.CLAMP);
+  const shadowColor = accentColors.opacity20;
 
   if (!imageUri) return null;
 
   return (
     <Canvas style={{ width, height }}>
-      {/* <RoundedRect opacity={stepIndex} x={0} y={0} width={width} height={height} r={42}>
+      <Image x={0} y={0} width={width} height={height} image={image} fit="cover" />
+      <Fill color="rgba(26, 26, 26, 0.75)" />
+      <BackdropBlur blur={200} />
+      <RoundedRect opacity={stepIndex} x={0} y={0} width={width} height={height} r={42}>
         <Paint color={'rgba(245, 248, 255, 0.06)'} style="stroke" strokeWidth={THICK_BORDER_WIDTH} />
-        <Shadow dx={0} dy={0} blur={40} color={shadowColor} inner />
-        <Shadow dx={0} dy={0} blur={4} color={shadowColor} inner />
-      </RoundedRect> */}
-      <Image x={0} y={0} width={width} height={height} image={image} fit="cover">
-        {/* <Blur blur={200} /> */}
-      </Image>
-      <BackdropBlur blur={200}>
-        <Fill color="rgba(26, 26, 26, 0.75)" />
-      </BackdropBlur>
+        <Shadow dx={0} dy={0} blur={40} color={shadowColor} inner shadowOnly />
+        <Shadow dx={0} dy={0} blur={4} color={shadowColor} inner shadowOnly />
+      </RoundedRect>
+      {/* More efficient, but opacity prop does not apply to children */}
+      {/* <Box opacity={stepIndex} box={rrect(rect(0, 0, width, height), 42, 42)}>
+        <Paint color={'rgba(245, 248, 255, 0.06)'} style="stroke" strokeWidth={THICK_BORDER_WIDTH} />
+        <BoxShadow dx={0} dy={0} blur={40} spread={-2} color={shadowColor} inner />
+        <BoxShadow dx={0} dy={0} blur={4} spread={2} color={shadowColor} inner />
+      </Box> */}
     </Canvas>
   );
 }
