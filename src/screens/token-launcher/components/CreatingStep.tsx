6ab5d53c814bf294c9932e705c4c4361b176d018
Kane Thomas <kmt8vn@virginia.edu>
2025-02-28 12:21:15 -0500

    replace creating spinning token with variant of christian's rainbow token anim

--- src/screens/token-launcher/components/CreatingStep.tsx
+++ src/screens/token-launcher/components/CreatingStep.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Box, Text } from '@/design-system';
-// import Animated from 'react-native-reanimated';
-import { CreatingAnimatedTokenLogo } from './CreatingAnimatedTokenLogo';
 import { useTokenLauncherStore } from '../state/tokenLauncherStore';
+import { RainbowTokenFlip } from './RainbowTokenFlip';
+import { useTokenLauncherContext } from '../context/TokenLauncherContext';
 
 export function CreatingStep() {
+  const { tokenSkiaImage, accentColors } = useTokenLauncherContext();
   const symbol = useTokenLauncherStore(state => state.symbol);
+
+  if (!tokenSkiaImage) return null;
 
   return (
     <Box style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Box gap={16} alignItems="center">
-        {/* <Animated.View sharedTransitionTag="tokenImage"> */}
-        <CreatingAnimatedTokenLogo />
-        {/* </Animated.View> */}
-        <Text size="20pt" weight="bold" color={'labelSecondary'}>
+      <Box gap={32} alignItems="center">
+        <RainbowTokenFlip color={accentColors.opacity100} image={tokenSkiaImage} size={100} />
+        <Text size="20pt" weight="bold" color={'label'}>
           {`Deploying $${symbol}...`}
         </Text>
       </Box>
