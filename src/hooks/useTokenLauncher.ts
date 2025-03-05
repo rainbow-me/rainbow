3291a306c1209ae94c157a50aae77dd27c39b1cc
derHowie <derhowiedesigns@gmail.com>
2025-03-05 18:28:28 -0500

    chore: simplify updating applicable networks in useTokenLauncher

--- src/hooks/useTokenLauncher.ts
+++ src/hooks/useTokenLauncher.ts
@@ -1,6 +1,6 @@
 import { useBackendNetworksStore } from '@/state/backendNetworks/backendNetworks';
 import { TokenLauncher as TL, SDKConfig as TokenLauncherConfig } from '@rainbow-me/token-launcher';
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { TOKEN_LAUNCHER_URL, TOKEN_LAUNCHER_API_KEY } from 'react-native-dotenv';
 
 const config: TokenLauncherConfig = {
@@ -15,16 +15,10 @@ TL.configure(config);
 export function useTokenLauncher(): typeof TokenLauncher {
   const { getTokenLauncherSupportedChainInfo } = useBackendNetworksStore();
   const launcherSupportedChains = getTokenLauncherSupportedChainInfo();
-
-  const currentConfig = useMemo(() => TokenLauncher.getConfig(), []);
-
   useEffect(() => {
-    console.log('launcherSupportedNetworks', launcherSupportedChains);
-    if (currentConfig.SUPPORTED_NETWORKS?.length !== launcherSupportedChains.length) {
-      TL.configure({
-        SUPPORTED_NETWORKS: launcherSupportedChains,
-      });
-    }
+    TL.configure({
+      SUPPORTED_NETWORKS: launcherSupportedChains,
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [launcherSupportedChains]);
 
