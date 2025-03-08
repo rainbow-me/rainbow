f1cd3d5daa612a4688fabfbde972c3504c5139a0
Janic Duplessis <janicduplessis@gmail.com>
2025-03-07 23:31:59 -0500

    Test

--- scripts/e2e-ci.sh
+++ scripts/e2e-ci.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export PATH="$PATH":"$HOME/.maestro/bin"
+export MAESTRO_CLI_NO_ANALYTICS=1
 
-adb shell install -r ./android/app/build/outputs/apk/release/app-release.apk
+$ANDROID_HOME/platform-tools/adb shell install -r ./android/app/build/outputs/apk/release/app-release.apk
 
 ./scripts/e2e.sh
