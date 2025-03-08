298819904be1bfafbda80f3e52e7d6a1efcb84ba
Janic Duplessis <janicduplessis@gmail.com>
2025-03-07 23:53:08 -0500

    Fix

--- scripts/e2e-ci.sh
+++ scripts/e2e-ci.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export PATH="$PATH":"$HOME/.maestro/bin"
-export MAESTRO_CLI_NO_ANALYTICS=1
+export MAESTRO_CLI_NO_ANALYTICS=true
+export MAESTRO_CLI_ANALYSIS_NOTIFICATION_DISABLED=true
 
 $ANDROID_HOME/platform-tools/adb shell install -r ./android/app/build/outputs/apk/release/app-release.apk
 
