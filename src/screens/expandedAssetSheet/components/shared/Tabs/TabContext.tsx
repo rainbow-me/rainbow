5c1e7d20c38bb9652b6d8ef175e2b31be7d7bb4a
walmat <matthew.wallt@gmail.com>
2025-03-31 15:19:48 -0400

    cleanup tab context wrapper

--- src/screens/expandedAssetSheet/components/shared/Tabs/TabContext.tsx
+++ src/screens/expandedAssetSheet/components/shared/Tabs/TabContext.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@/design-system';
+import { Box, BoxProps } from '@/design-system';
 import React, { createContext, useContext } from 'react';
 import { clamp, makeMutable, SharedValue, useSharedValue } from 'react-native-reanimated';
 
@@ -18,11 +18,13 @@ export const TabProvider = ({
   tabs,
   accentColor,
   initialActiveTabIndex = 0,
+  wrapperStyles,
   children,
 }: {
   tabs: string[];
   accentColor: string;
   initialActiveTabIndex?: number;
+  wrapperStyles?: BoxProps;
   children: React.ReactNode;
 }) => {
   const activeTabIndex = useSharedValue(clamp(initialActiveTabIndex, 0, tabs.length - 1));
@@ -31,7 +33,7 @@ export const TabProvider = ({
 
   return (
     <TabContext.Provider value={{ tabs, activeTabIndex, accentColor, isExpanded }}>
-      <Box style={{ position: 'relative' }} gap={24}>
+      <Box gap={24} {...wrapperStyles}>
         {children}
       </Box>
     </TabContext.Provider>
