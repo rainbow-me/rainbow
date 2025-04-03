e1790d97d760d8e1511e2bc5b6a1ae9dba65c0cf
walmat <matthew.wallt@gmail.com>
2025-04-03 13:00:59 -0400

    improve safety for untyped files by converting options to string if needed

--- src/utils/actionsheet.ts
+++ src/utils/actionsheet.ts
@@ -15,16 +15,38 @@ const determineUserInterfaceStyle = async () => {
   return currentTheme;
 };
 
+/**
+ * @desc Safely convert options to strings.
+ * This should technically never do anything since we have proper type checking, but
+ * it's a good sanity check for javascript files that are not typed.
+ * @param options - The options to convert (string[])
+ * @returns The options as strings
+ */
+const safeOptions = (options: ActionSheetIOSOptions['options']) => {
+  return options
+    .map(option => {
+      if (typeof option === 'undefined') return null;
+      if (typeof option === 'string') {
+        return option;
+      }
+      return `${option}`;
+    })
+    .filter(Boolean);
+};
+
 export default async function showActionSheetWithOptions(
-  options: ActionSheetIOSOptions,
+  { options, ...props }: ActionSheetIOSOptions,
   callback: (buttonIndex: number | undefined) => void
 ) {
   const userInterfaceStyle = await determineUserInterfaceStyle();
+  const sheetOptions = safeOptions(options);
+
   if (IS_IOS) {
     ActionSheetIOS.showActionSheetWithOptions(
       {
         userInterfaceStyle,
-        ...options,
+        options: sheetOptions,
+        ...props,
       },
       callback
     );
@@ -32,7 +54,8 @@ export default async function showActionSheetWithOptions(
     ActionSheet.showActionSheetWithOptions(
       {
         userInterfaceStyle,
-        ...options,
+        options: sheetOptions,
+        ...props,
       },
       callback
     );
