2ebccf2b54aa858147158c1b6bb9a001d62038fd
walmat <matthew.wallt@gmail.com>
2025-04-03 12:49:44 -0400

    add  actionsheet user interface style based on theme

--- src/utils/actionsheet.ts
+++ src/utils/actionsheet.ts
@@ -1,11 +1,40 @@
 import { IS_IOS } from '@/env';
-import { ActionSheetIOS, ActionSheetIOSOptions } from 'react-native';
+import { getTheme } from '@/handlers/localstorage/theme';
+import { ActionSheetIOS, ActionSheetIOSOptions, Appearance } from 'react-native';
 import ActionSheet from 'react-native-action-sheet';
+import { Themes } from '@/theme/ThemeContext';
 
-export default function showActionSheetWithOptions(options: ActionSheetIOSOptions, callback: (buttonIndex: number | undefined) => void) {
+const determineUserInterfaceStyle = async () => {
+  let currentTheme = await getTheme();
+
+  if (currentTheme === Themes.SYSTEM) {
+    const isSystemDarkMode = Appearance.getColorScheme() === Themes.DARK;
+    currentTheme = isSystemDarkMode ? Themes.DARK : Themes.LIGHT;
+  }
+
+  return currentTheme;
+};
+
+export default async function showActionSheetWithOptions(
+  options: ActionSheetIOSOptions,
+  callback: (buttonIndex: number | undefined) => void
+) {
+  const userInterfaceStyle = await determineUserInterfaceStyle();
   if (IS_IOS) {
-    ActionSheetIOS.showActionSheetWithOptions(options, callback);
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        userInterfaceStyle,
+        ...options,
+      },
+      callback
+    );
   } else {
-    ActionSheet.showActionSheetWithOptions(options, callback);
+    ActionSheet.showActionSheetWithOptions(
+      {
+        userInterfaceStyle,
+        ...options,
+      },
+      callback
+    );
   }
 }
