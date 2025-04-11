24a875d881dbbda03dbb36247b32bc0cdede5370
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2025-04-11 01:46:39 -0500

    get anvil working + more mods to tests

--- src/helpers/RainbowContext.tsx
+++ src/helpers/RainbowContext.tsx
@@ -11,7 +11,8 @@ import { logger, RainbowError } from '@/logger';
 import { Navigation } from '@/navigation';
 import Routes from '@rainbow-me/routes';
 import { useConnectedToAnvilStore } from '@/state/connectedToAnvil';
-import { IS_DEV, IS_TEST } from '@/env';
+import { IS_ANDROID, IS_DEV, IS_TEST } from '@/env';
+import { ethers } from 'ethers';
 
 export type RainbowContextType = {
   config: Record<keyof typeof defaultConfig, boolean> | Record<string, never>;
@@ -85,15 +86,43 @@ export default function RainbowContextWrapper({ children }: PropsWithChildren) {
     Navigation.handleAction(Routes.WALLET_SCREEN, {});
   }, [setConnectedToAnvil]);
 
+  const fundTestWallet = useCallback(async () => {
+    if (!IS_TEST) return;
+    const RPC_URL = IS_ANDROID ? 'http://10.0.2.2:8545' : 'http://127.0.0.1:8545';
+    try {
+      const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+      const wallet = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider);
+      const testWalletAddress = '0x4d14289265eb7c166cF111A76B6D742e3b85dF85';
+      console.log('provider', provider);
+      console.log('wallet', wallet);
+      console.log('testWalletAddress', testWalletAddress);
+      await wallet.sendTransaction({
+        to: testWalletAddress,
+        value: ethers.utils.parseEther('20'),
+      });
+      console.log('transaction sent');
+    } catch (e) {
+      console.log('error', e);
+      logger.error(new RainbowError('error funding test wallet'), {
+        message: e instanceof Error ? e.message : String(e),
+      });
+    }
+  }, []);
+
   return (
     <RainbowContext.Provider value={initialValue}>
       {children}
       {/* @ts-expect-error ts-migrate(2741) FIXME: Property 'color' is missing in type... Remove this comment to see the full error message */}
       {showReloadButton && IS_DEV && <DevButton initialDisplacement={200} />}
       {((showConnectToAnvilButton && IS_DEV) || IS_TEST) && (
-        <DevButton color={colors.purple} onPress={connectToAnvil} initialDisplacement={150} testID={'dev-button-anvil'} size={20}>
-          <Emoji>👷</Emoji>
-        </DevButton>
+        <>
+          <DevButton color={colors.purple} onPress={connectToAnvil} initialDisplacement={150} testID={'dev-button-anvil'} size={20}>
+            <Emoji>👷</Emoji>
+          </DevButton>
+          <DevButton color={colors.green} onPress={fundTestWallet} initialDisplacement={100} testID={'fund-test-wallet-button'} size={20}>
+            <Emoji>💰</Emoji>
+          </DevButton>
+        </>
       )}
       {showSwitchModeButton && IS_DEV && (
         <DevButton color={colors.dark} onPress={() => setTheme(isDarkMode ? 'light' : 'dark')}>
