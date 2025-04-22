64889f62b014c2e867bf405972b2832065afbb81
BrodyHughes <41711440+BrodyHughes@users.noreply.github.com>
2025-04-22 17:00:33 -0500

    Revert "Fix: Expanded State History spacing off on Android (#6579)"
    This reverts commit 2ddcffe50648e5e6585c9dbcf896bcc339fcf2aa.

--- src/screens/expandedAssetSheet/components/sections/HistorySection.tsx
+++ src/screens/expandedAssetSheet/components/sections/HistorySection.tsx
@@ -63,10 +63,6 @@ const moreButtonStyles = {
   justifyContent: 'center' as const,
 };
 
-const buttonWrapperStyles = {
-  gap: LIST_ITEM_GAP,
-};
-
 interface MoreButtonProps {
   tokenInteractions: TokenInteraction[];
 }
@@ -237,7 +233,7 @@ export const ListItem = memo(function ListItem({ index, item, nativeCurrency, li
 
   return (
     <Box as={Animated.View} style={isVisibleStyles} height={ROW_HEIGHT}>
-      <Box as={ButtonPressAnimation} scaleTo={0.94} onPress={navigateToTransaction} style={buttonWrapperStyles}>
+      <Box as={ButtonPressAnimation} scaleTo={0.94} onPress={navigateToTransaction} gap={LIST_ITEM_GAP}>
         <Box flexDirection="row" justifyContent="space-between" alignItems="center">
           <Box flexDirection="row" alignItems="center" gap={ICON_TEXT_GAP}>
             <Text size="icon 11px" color={{ custom: iconColor }} weight="bold">
