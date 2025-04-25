554a56a84dafc6249029e5b36e773ce093720fef
walmat <matthew.wallt@gmail.com>
2025-04-25 17:09:13 -0400

    wire up market stats minus relative change

--- src/screens/expandedAssetSheet/components/sections/marketSection/MarketStatsSection.tsx
+++ src/screens/expandedAssetSheet/components/sections/marketSection/MarketStatsSection.tsx
@@ -1,24 +1,29 @@
 import React, { memo } from 'react';
 import * as i18n from '@/languages';
 import { Box } from '@/design-system';
-// import { MarketStatsCard } from './MarketStatsCard';
-import { AssetInfoList } from './AssetInfoList';
+import { MarketStatsCard } from './MarketStatsCard';
 import { CollapsibleSection, LAYOUT_ANIMATION } from '../../shared/CollapsibleSection';
 import { SheetSeparator } from '../../shared/Separator';
-import { SectionId } from '@/screens/expandedAssetSheet/context/ExpandedAssetSheetContext';
+import { SectionId, useExpandedAssetSheetContext } from '@/screens/expandedAssetSheet/context/ExpandedAssetSheetContext';
 import Animated from 'react-native-reanimated';
+import { TokenMarketStatsQueryKey, useTokenMarketStats } from '@/resources/metadata/tokenStats';
+import { queryClient } from '@/react-query';
 
 export const MarketStatsSection = memo(function MarketStatsSection() {
-  {
-    /* BLOCKED: blocked by backend for data */
-  }
-  {
-    /*content={<MarketStatsCard />} */
-  }
+  const { basicAsset: asset } = useExpandedAssetSheetContext();
+
+  queryClient.invalidateQueries(TokenMarketStatsQueryKey({ chainID: asset.chainId, address: asset.address }));
+
+  const { data: marketData } = useTokenMarketStats({ chainID: asset.chainId, address: asset.address });
+
+  if (!marketData || Object.keys(marketData).length === 0) return null;
+
+  console.log('marketData', marketData);
+
   return (
     <Box as={Animated.View} layout={LAYOUT_ANIMATION} gap={28}>
       <CollapsibleSection
-        content={<AssetInfoList />}
+        content={<MarketStatsCard marketData={marketData} />}
         icon="􀑃"
         id={SectionId.MARKET_STATS}
         primaryText={i18n.t(i18n.l.expanded_state.sections.market_stats.title)}
