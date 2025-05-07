75011877f23a06a8555a89d6165337e1d9890e54
EQuimper <quimperemanuel@gmail.com>
2025-05-07 14:23:40 -0400

    Fix build time

--- scripts/e2e-ios-build.sh
+++ scripts/e2e-ios-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xcodebuild -workspace ios/Rainbow.xcworkspace -scheme Rainbow -configuration Release -sdk iphonesimulator -derivedDataPath ios/build
+xcodebuild -workspace ios/Rainbow.xcworkspace -scheme Rainbow -configuration Release -sdk iphonesimulator -derivedDataPath ios/build -parallelizeTargets -quiet BUILD_LIBRARY_FOR_DISTRIBUTION=YES COMPILER_INDEX_STORE_ENABLE=NO
