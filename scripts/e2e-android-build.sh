f16e4f7aceb294d103154b953e927c228739ad8c
Emanuel Quimper <quimperemanuel@gmail.com>
2025-05-20 09:22:29 -0400

    E2E EditContact flow (#6624)
    * add edit contact test
    
    * bump to 16
    
    * fix telemetry
    
    * add flag to script
    
    * add a check for sim booted
    
    * Add bigger timeout
    
    * remove telemetry
    
    * Update EditContact.yaml
    
    ---------
    
    Co-authored-by: Janic Duplessis <janicduplessis@gmail.com>

--- scripts/e2e-android-build.sh
+++ scripts/e2e-android-build.sh
@@ -6,4 +6,4 @@ if [ "$DEBUG" != "true" ] && [ -n "$ARCH" ]; then
   ARCHS_FLAG="-PreactNativeArchitectures=$ARCH"
 fi
 
-yarn gradle assembleRelease $ARCHS_FLAG -PisE2E=true
+yarn gradle assembleRelease $ARCHS_FLAG -PisE2E=true --no-daemon --build-cache --no-scan
