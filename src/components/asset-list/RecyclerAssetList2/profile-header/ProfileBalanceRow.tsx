1d34c274af3cf1a51d85b1e51ad2a17c359f74fa
Kane Thomas <kanethomas1997@gmail.com>
2025-05-20 13:01:49 -0400

    use animated number on total wallet balance

--- src/components/asset-list/RecyclerAssetList2/profile-header/ProfileBalanceRow.tsx
+++ src/components/asset-list/RecyclerAssetList2/profile-header/ProfileBalanceRow.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Skeleton, { FakeText } from '@/components/skeleton/Skeleton';
-import { Box, Text } from '@/design-system';
+import { Box } from '@/design-system';
+import { AnimatedNumber } from '@/components/live-token-text/AnimatedNumber';
 
 export const ProfileBalanceRowHeight = 24;
 const placeholderHeight = ProfileBalanceRowHeight;
@@ -21,9 +22,10 @@ export function ProfileBalanceRow({ totalValue, isLoadingBalance }: ProfileBalan
           </Skeleton>
         </Box>
       ) : (
-        <Text testID="balance-text" numberOfLines={1} color="label" size={totalValue?.length > 14 ? '26pt' : '34pt'} weight="heavy">
-          {totalValue}
-        </Text>
+        // TODO: Arbitrary negative margin to account for the padding in the animated number, which is font size dependent
+        <Box height={ProfileBalanceRowHeight} marginTop={{ custom: -12 }}>
+          <AnimatedNumber value={totalValue} tabularNumbers color="label" size={totalValue?.length > 14 ? '26pt' : '34pt'} weight="heavy" />
+        </Box>
       )}
     </>
   );
