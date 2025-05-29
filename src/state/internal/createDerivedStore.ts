c4514b20732ea8ef98f092fa7e0e7f7f78171e87
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2025-05-28 23:22:58 -0400

    Remove automatic `getState` flush
    This interferes with debouncing, since `getState` can be called during normal use by store subscribers. Will need to expose as an explicit option on `getState`, or somehow make it explicit.
    
    In the meantime, where this matters, consumers can call `flushUpdates` before calling `getState` on debounced stores to ensure output is current.

--- src/state/internal/createDerivedStore.ts
+++ src/state/internal/createDerivedStore.ts
@@ -273,7 +273,6 @@ function derive<DerivedState>(
       if (watchers.size) return derive();
       else return deriveFunction($);
     }
-    debouncedDerive?.flush();
     return derivedState;
   }
 
