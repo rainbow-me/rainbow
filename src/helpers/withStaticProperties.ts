66337b735434685a640abdf625087c0ca8905351
Nate Wienert <natewienert@gmail.com>
2025-06-20 06:22:13 -1000

    RainbowImage - unified image component for faster image loads (#6653)
    * add RainbowImage to enable faster image rendering with fallbacks
    
    * add containerStyle
    
    * revert version

---
+++ src/helpers/withStaticProperties.ts
@@ -0,0 +1,30 @@
+/**
+ * Helper for easily adding static properties onto functions
+ *
+ * eg:
+ *
+ * export const DecoratedComponent = styled(Component, {
+ *   helperFunction() {}
+ * })
+ *
+ */
+
+const Decorated = Symbol();
+
+type Combined<A, B> = A & B;
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const withStaticProperties = <A extends Function, B extends Record<string, any>>(component: A, staticProps: B): Combined<A, B> => {
+  // if already wrapped once, error, its an edge case we don't use:
+  // @ts-expect-error its ok to attach symbol to function
+  if (component[Decorated]) {
+    throw new Error(`Already called withStaticProperties on component`);
+  }
+
+  // add new things
+  Object.assign(component, staticProps);
+  // @ts-expect-error its ok to attach symbol to function
+  component[Decorated] = true;
+
+  return component as Combined<A, B>;
+};
