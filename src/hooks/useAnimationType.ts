e7a96c21cb6b8da20ff06ebe37bbbbd10cb13e67
Matthew Wall <matthew.wallt@gmail.com>
2025-06-24 09:18:14 -0400

    fallback to image on audio for now

--- src/hooks/useAnimationType.ts
+++ src/hooks/useAnimationType.ts
@@ -7,7 +7,10 @@ export default function useAnimationType(uniqueAsset: UniqueAsset) {
   return React.useMemo((): AnimationType | undefined => {
     if (!uniqueAsset.images.animatedMimeType || !uniqueAsset.images.animatedUrl) return undefined;
     if (uniqueAsset.images.animatedMimeType.includes('video')) return 'video';
-    if (uniqueAsset.images.animatedMimeType.includes('audio')) return 'audio';
     if (uniqueAsset.images.animatedMimeType.includes('model')) return '3d';
+    // Current support for Audio in prod is not working
+    // ticketed here: https://linear.app/rainbow/issue/APP-2784/follow-up-audio-nfts-are-sometimes-displayed-in-a-broken-state
+    // FIXME: Uncomment below to support audio in prod
+    // if (uniqueAsset.images.animatedMimeType.includes('audio')) return 'audio';
   }, [uniqueAsset.images.animatedMimeType, uniqueAsset.images.animatedUrl]);
 }
