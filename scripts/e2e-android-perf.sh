f820255c67065cff5b874c47037c95ceadc92576
Janic Duplessis <janicduplessis@gmail.com>
2025-07-07 11:41:49 -0400

    Test

--- scripts/e2e-android-perf.sh
+++ scripts/e2e-android-perf.sh
@@ -4,26 +4,26 @@ set -euo pipefail
 
 APP_ID="me.rainbow"
 
-cleanup() {
-  echo "Cleaning up..."
+# cleanup() {
+#   echo "Cleaning up..."
 
-  if [ -n "${SESSION_PID:-}" ]; then
-    kill "$SESSION_PID" 2>/dev/null || true
-  fi
-}
+#   if [ -n "${SESSION_PID:-}" ]; then
+#     kill "$SESSION_PID" 2>/dev/null || true
+#   fi
+# }
 
-trap cleanup EXIT INT TERM
+# trap cleanup EXIT INT TERM
 
-npx @perf-profiler/maestro@latest start-session &
-SESSION_PID=$!
+# npx @perf-profiler/maestro@latest start-session &
+# SESSION_PID=$!
 
 sleep 5
 
-npx @perf-profiler/maestro@latest test -e APP_ID="$APP_ID" e2e/utils/PreparePerf.yaml
+maestro test -e APP_ID="$APP_ID" e2e/utils/PreparePerf.yaml
 
 mkdir -p e2e-artifacts/perf/results
 flashlight test --bundleId "$APP_ID" \
-  --testCommand "npx @perf-profiler/maestro@latest test -e APP_ID=\"$APP_ID\" e2e/perf/TTI.yaml" \
+  --testCommand "maestro test -e APP_ID=\"$APP_ID\" e2e/perf/TTI.yaml" \
   --duration 10000 \
   --resultsFilePath e2e-artifacts/perf/results/tti.json \
   "$@"
