3968706f181ac630e4fa6f97b2a954ce9ca9e7d2
EQuimper <quimperemanuel@gmail.com>
2025-07-30 14:24:34 -0400

    Add E2E test flow for L2 transactions and network switching logic

--- src/components/TestDeeplinkHandler.tsx
+++ src/components/TestDeeplinkHandler.tsx
@@ -5,11 +5,16 @@ import Routes from '@/navigation/routesNames';
 import { useEffect } from 'react';
 import { Linking } from 'react-native';
 import URL from 'url-parse';
+import { useDispatch } from 'react-redux';
+import { settingsUpdateNetwork } from '@/redux/settings';
+import { ChainId } from '@/state/backendNetworks/types';
 
 /**
  * Handles E2E test commands. See e2e/README.md:31 for usage.
  */
 export function TestDeeplinkHandler() {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     const listener = Linking.addListener('url', async ({ url }) => {
       const { protocol, host, pathname, query } = new URL(url, true);
@@ -35,6 +40,40 @@ export function TestDeeplinkHandler() {
             true
           );
           break;
+        case 'switchNetwork': {
+          const networkName = query.network as string;
+          let chainId: ChainId | undefined;
+          // Map network names to chain IDs
+          switch (networkName) {
+            case 'base':
+              chainId = ChainId.base;
+              break;
+            case 'optimism':
+              chainId = ChainId.optimism;
+              break;
+            case 'arbitrum':
+              chainId = ChainId.arbitrum;
+              break;
+            case 'polygon':
+              chainId = ChainId.polygon;
+              break;
+            case 'mainnet':
+              chainId = ChainId.mainnet;
+              break;
+            case 'anvil':
+              chainId = ChainId.anvil;
+              break;
+            default:
+              logger.debug(`[TestDeeplinkHandler]: unsupported network`, { networkName });
+              return;
+          }
+
+          if (chainId) {
+            dispatch(settingsUpdateNetwork(chainId));
+            logger.debug(`[TestDeeplinkHandler]: switched to network`, { networkName, chainId });
+          }
+          break;
+        }
         default:
           logger.debug(`[TestDeeplinkHandler]: unknown path`, { url });
           break;
@@ -42,7 +81,7 @@ export function TestDeeplinkHandler() {
     });
     return listener.remove;
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [dispatch]);
 
   return null;
 }
