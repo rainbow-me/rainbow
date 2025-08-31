880f6f41b9c70e967b6c5df1ec4b9024bc61cb6c
jinchung <jin.chung17@gmail.com>
2025-08-31 13:38:14 -0400

    Revert "TEMP: override for tenderly on base"
    This reverts commit 732c7452b33b61118dd0bf0ad8b139725cde470d.

--- src/state/backendNetworks/utils.ts
+++ src/state/backendNetworks/utils.ts
@@ -1,5 +1,5 @@
 import { Chain } from 'viem';
-import { base, mainnet } from 'viem/chains';
+import { mainnet } from 'viem/chains';
 import { IS_DEV, RPC_PROXY_API_KEY } from '@/env';
 import { BackendNetwork } from './types';
 
@@ -7,16 +7,11 @@ const proxyBackendNetworkRpcEndpoint = (endpoint: string) => {
   return `${endpoint}${RPC_PROXY_API_KEY}`;
 };
 
-const TENDERLY_BASE_RPC = 'https://virtual.base.us-east.rpc.tenderly.co/8bd88a16-7aed-467c-bfd1-34f39a2ac536';
 export function transformBackendNetworkToChain(network: BackendNetwork): Chain {
   if (!network) {
     throw new Error('Invalid network data');
   }
-  let defaultRpcUrl = proxyBackendNetworkRpcEndpoint(network.defaultRPC.url);
-  if (Number(network.id) === base.id) {
-    console.log('OVERRIDING base RPC, using', TENDERLY_BASE_RPC);
-    defaultRpcUrl = TENDERLY_BASE_RPC;
-  }
+  const defaultRpcUrl = proxyBackendNetworkRpcEndpoint(network.defaultRPC.url);
 
   return {
     id: parseInt(network.id, 10),
