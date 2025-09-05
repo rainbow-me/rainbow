05871bea766f68f378225b73a0da6851666b17eb
Emanuel Quimper <quimperemanuel@gmail.com>
2025-09-04 20:53:21 -0400

    Fix issue with e2e (#6838)
    * Fix issue with discover in e2e
    
    * Fix toast issue
    
    * Fix send
    
    * Fix
    
    * Fix
    
    * Fix toast issue in test

--- src/components/rainbow-toast/useRainbowToastEnabled.ts
+++ src/components/rainbow-toast/useRainbowToastEnabled.ts
@@ -1,7 +1,9 @@
 import { RAINBOW_TOASTS, useExperimentalFlag } from '@/config';
+import { IS_TEST } from '@/env';
 import { useRemoteConfig } from '@/model/remoteConfig';
 
 export const useRainbowToastEnabled = () => {
   const { rainbow_toasts_enabled } = useRemoteConfig('rainbow_toasts_enabled');
-  return useExperimentalFlag(RAINBOW_TOASTS) || rainbow_toasts_enabled;
+  const isEnabled = useExperimentalFlag(RAINBOW_TOASTS);
+  return IS_TEST || isEnabled || rainbow_toasts_enabled;
 };
