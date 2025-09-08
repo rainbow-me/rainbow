80f9f25649948bc4d5417456c22661bef62c9bb7
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2025-09-08 17:57:59 -0400

    Remove env vars dev menu

--- src/screens/SettingsSheet/components/DevSection.tsx
+++ src/screens/SettingsSheet/components/DevSection.tsx
@@ -33,7 +33,6 @@ import { analyzeReactQueryStore, clearReactQueryCache } from '@/react-query/reac
 import { useConnectedToAnvilStore } from '@/state/connectedToAnvil';
 import { nonceStore } from '@/state/nonces';
 import { pendingTransactionsStore } from '@/state/pendingTransactions';
-import { analyzeEnvVariables } from '@/utils/analyzeEnvVariables';
 import FastImage from 'react-native-fast-image';
 import { analyzeUserAssets } from '@/state/debug/analyzeUserAssets';
 
@@ -253,12 +252,6 @@ const DevSection = () => {
               onPress={() => Restart.Restart()}
               size={52}
               titleComponent={<MenuItem.Title text={i18n.t(i18n.l.developer_settings.restart_app)} />}
-            />
-            <MenuItem
-              leftComponent={<MenuItem.TextIcon icon="🔍" isEmoji />}
-              onPress={analyzeEnvVariables}
-              size={52}
-              titleComponent={<MenuItem.Title text={lang.t('developer_settings.analyze_env_variables')} />}
             />
             {/* TEMP: Removal for public TF */}
             {/* <MenuItem
