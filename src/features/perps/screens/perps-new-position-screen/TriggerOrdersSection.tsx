4f2946e75d2442a91704b348d0e3dcee35d8900b
Kane Thomas <kanethomas1997@gmail.com>
2025-09-23 10:24:03 -0400

    lint fix

--- src/features/perps/screens/perps-new-position-screen/TriggerOrdersSection.tsx
+++ src/features/perps/screens/perps-new-position-screen/TriggerOrdersSection.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from '@/design-system';
 import React from 'react';
 import { hlNewPositionStoreActions, useHlNewPositionStore } from '@/features/perps/stores/hlNewPositionStore';
 import { TriggerOrderCard } from '@/features/perps/components/TriggerOrderCard';
-import { TriggerOrderType } from '@/features/perps/types';
+import { TriggerOrderSource, TriggerOrderType } from '@/features/perps/types';
 import { AddTriggerOrderButton } from '@/features/perps/components/AddTriggerOrderButton';
 
 export const TriggerOrdersSection = function TriggerOrdersSection() {
@@ -20,8 +20,8 @@ export const TriggerOrdersSection = function TriggerOrdersSection() {
           </Text>
         </Box>
         <Box gap={12}>
-          <AddTriggerOrderButton symbol={market.symbol} type={TriggerOrderType.TAKE_PROFIT} source="newPosition" />
-          <AddTriggerOrderButton symbol={market.symbol} type={TriggerOrderType.STOP_LOSS} source="newPosition" />
+          <AddTriggerOrderButton symbol={market.symbol} type={TriggerOrderType.TAKE_PROFIT} source={TriggerOrderSource.NEW} />
+          <AddTriggerOrderButton symbol={market.symbol} type={TriggerOrderType.STOP_LOSS} source={TriggerOrderSource.NEW} />
         </Box>
       </Box>
       <Box gap={12}>
