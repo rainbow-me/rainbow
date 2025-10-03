e995de30d8f0fbd0e830bd91dffd6b180d305b16
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2025-10-02 22:10:56 -0400

    Polish explain sheet, adjust copy

--- src/features/perps/screens/perps-explain-sheet/PerpsExplainSheet.tsx
+++ src/features/perps/screens/perps-explain-sheet/PerpsExplainSheet.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ViewStyle } from 'react-native';
 import { AnimatedText, Box, ColorModeProvider, Text, TextShadow } from '@/design-system';
 import { ExplainerSheet, ExplainerSheetStep, PANEL_INNER_WIDTH } from '@/components/explainer-sheet/ExplainerSheet';
 import FastImage from 'react-native-fast-image';
@@ -14,30 +14,46 @@ import { useRoute } from '@/navigation/Navigation';
 import { HyperliquidLogo } from '@/features/perps/components/HyperliquidLogo';
 import { EasingGradient } from '@/components/easing-gradient/EasingGradient';
 import { opacityWorklet } from '@/__swaps__/utils/swaps';
+import { easing } from '@/components/animations/animationConfigs';
 
 const translations = i18n.l.perps.explain_sheet;
 
-const titleShadowConfig = {
-  blur: 10,
-  shadowOpacity: 0.1,
-  color: '#25292E',
+const config = {
+  titleShadow: {
+    blur: 10,
+    color: '#25292E',
+    shadowOpacity: 0.1,
+  },
+  titleStyle: {
+    bottom: 8,
+  },
+  subtitleStyle: {
+    alignSelf: 'center',
+    bottom: 4,
+    maxWidth: 266,
+  } satisfies ViewStyle,
 };
 
 const STEPS: ExplainerSheetStep[] = [
   {
     id: 'step-1',
     titleComponent: () => (
-      <TextShadow color={titleShadowConfig.color} blur={titleShadowConfig.blur} shadowOpacity={titleShadowConfig.shadowOpacity}>
+      <TextShadow
+        color={config.titleShadow.color}
+        blur={config.titleShadow.blur}
+        containerStyle={config.titleStyle}
+        shadowOpacity={config.titleShadow.shadowOpacity}
+      >
         <Text color={'label'} align="center" size="44pt" weight="heavy">
           {i18n.t(translations.steps.step_1.title_parts[0])}
         </Text>
       </TextShadow>
     ),
     graphicComponent: () => (
-      <View style={{ width: PANEL_INNER_WIDTH, height: PANEL_INNER_WIDTH, marginTop: 24 }}>
+      <View style={{ width: PANEL_INNER_WIDTH, height: PANEL_INNER_WIDTH, marginTop: 32 }}>
         <FastImage
           source={magicOrbImage}
-          style={{ width: PANEL_INNER_WIDTH, height: PANEL_INNER_WIDTH }}
+          style={{ width: PANEL_INNER_WIDTH, height: PANEL_INNER_WIDTH - 28 }}
           resizeMode={FastImage.resizeMode.contain}
         />
         <EasingGradient
@@ -50,7 +66,7 @@ const STEPS: ExplainerSheetStep[] = [
       </View>
     ),
     subtitleComponent: () => (
-      <Text align="center" size="17pt / 150%" weight="medium" color="labelTertiary" style={{ paddingHorizontal: 24 }}>
+      <Text align="center" size="17pt / 135%" weight="semibold" color="labelTertiary" style={config.subtitleStyle}>
         {i18n.t(translations.steps.step_1.subtitle_parts[0])}
       </Text>
     ),
@@ -58,7 +74,12 @@ const STEPS: ExplainerSheetStep[] = [
   {
     id: 'step-2',
     titleComponent: () => (
-      <TextShadow color={titleShadowConfig.color} blur={titleShadowConfig.blur} shadowOpacity={titleShadowConfig.shadowOpacity}>
+      <TextShadow
+        color={config.titleShadow.color}
+        blur={config.titleShadow.blur}
+        containerStyle={config.titleStyle}
+        shadowOpacity={config.titleShadow.shadowOpacity}
+      >
         <Text align="center" size="44pt" weight="heavy" color="label" style={{ maxWidth: 200, alignSelf: 'center' }}>
           {`${i18n.t(translations.steps.step_2.title_parts[0])} `}
           <Text size="44pt" weight="heavy" color="green">
@@ -79,23 +100,26 @@ const STEPS: ExplainerSheetStep[] = [
           overflow: 'visible',
           justifyContent: 'center',
           alignItems: 'center',
-          marginTop: 48,
+          marginTop: 36,
         }}
       >
         <FastImage
           source={longShortImage}
-          style={{ width: PANEL_INNER_WIDTH + 24 + 200, height: 350 }}
+          style={{ width: PANEL_INNER_WIDTH + 24 + 200, height: 320 }}
           resizeMode={FastImage.resizeMode.contain}
         />
         <EasingGradient
-          startColor={opacityWorklet('#171E20', 0)}
+          easing={easing.inOut.ease}
           endColor={'#171E20'}
-          style={{ position: 'absolute', height: 100, left: 0, right: 0, bottom: 0 }}
+          endPosition={{ x: 0.5, y: 0.64 }}
+          startColor={'#171E20'}
+          startPosition={{ x: 0.5, y: 0 }}
+          style={{ position: 'absolute', height: 150, left: 0, right: 0, bottom: 0 }}
         />
       </Box>
     ),
     subtitleComponent: () => (
-      <Text align="center" size="17pt / 150%" weight="medium" color="labelTertiary" style={{ paddingHorizontal: 24 }}>
+      <Text align="center" size="17pt / 135%" weight="semibold" color="labelTertiary" style={[config.subtitleStyle, { maxWidth: 260 }]}>
         {i18n.t(translations.steps.step_2.subtitle_parts[0])}
       </Text>
     ),
@@ -103,17 +127,22 @@ const STEPS: ExplainerSheetStep[] = [
   {
     id: 'step-3',
     titleComponent: () => (
-      <TextShadow color={titleShadowConfig.color} blur={titleShadowConfig.blur} shadowOpacity={titleShadowConfig.shadowOpacity}>
+      <TextShadow
+        color={config.titleShadow.color}
+        containerStyle={config.titleStyle}
+        blur={config.titleShadow.blur}
+        shadowOpacity={config.titleShadow.shadowOpacity}
+      >
         <Text align="center" size="44pt" weight="heavy" color="label">
           {i18n.t(translations.steps.step_3.title_parts[0])}
         </Text>
       </TextShadow>
     ),
     graphicComponent: () => (
-      <FastImage source={leverageImage} style={{ width: PANEL_INNER_WIDTH, height: '100%' }} resizeMode={FastImage.resizeMode.contain} />
+      <FastImage source={leverageImage} style={{ width: PANEL_INNER_WIDTH, height: 240 }} resizeMode={FastImage.resizeMode.contain} />
     ),
     subtitleComponent: () => (
-      <Text align="center" size="17pt / 150%" weight="medium" color="labelTertiary" style={{ paddingHorizontal: 24 }}>
+      <Text align="center" size="17pt / 135%" weight="semibold" color="labelTertiary" style={config.subtitleStyle}>
         {i18n.t(translations.steps.step_3.subtitle_parts[0])}
       </Text>
     ),
