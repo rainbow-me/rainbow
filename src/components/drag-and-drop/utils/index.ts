f496d10605e6d4c241b3119406d2c656271a3b71
Kane Thomas <kanethomas1997@gmail.com>
2025-10-21 15:43:47 -0400

    mv getRandomInt to utils, rm unused version

--- src/components/drag-and-drop/utils/index.ts
+++ src/components/drag-and-drop/utils/index.ts
@@ -1,7 +1,6 @@
 export * from './array';
 export * from './assert';
 export * from './geometry';
-export * from './random';
 export * from './reanimated';
 export * from './swap';
 export * from './collision';
