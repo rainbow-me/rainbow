434d6b69cc89a2156d86324d569d007213f67879
Daniel Sinclair <d@niel.nyc>
2025-10-24 23:56:34 -0400

    tweak: keep dev section in dev build

--- src/model/remoteConfig.ts
+++ src/model/remoteConfig.ts
@@ -225,7 +225,7 @@ export const DEFAULT_CONFIG = {
   candlestick_charts_enabled: IS_DEV || isTestFlight || false,
   rainbow_toasts_enabled: IS_DEV || isTestFlight || false,
   perps_enabled: false,
-  dev_section_enabled: false,
+  dev_section_enabled: IS_DEV || false,
 } as const satisfies Readonly<RainbowConfig>;
 
 type RemoteConfigKey = keyof typeof DEFAULT_CONFIG;
