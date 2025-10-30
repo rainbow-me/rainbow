4b0a574e8f1b0896b788b0cd0917425e26d66738
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2025-10-30 01:17:00 +0000

    Reorder args

--- src/features/charts/candlestick/hyperliquid/hyperliquidCharts.ts
+++ src/features/charts/candlestick/hyperliquid/hyperliquidCharts.ts
@@ -54,7 +54,7 @@ export async function fetchHyperliquidChart(
     abortController?.signal
   );
 
-  const bars = convertCandlesToBars(candles, requestedCandles, candleResolution);
+  const bars = convertCandlesToBars(candles, candleResolution, requestedCandles);
 
   return {
     candleResolution,
@@ -67,7 +67,7 @@ export async function fetchHyperliquidChart(
 /**
  * Converts a Hyperliquid `Candle` array to `Bar` format.
  */
-function convertCandlesToBars(candles: Candle[], maxBars: number, candleResolution: CandleResolution): Bar[] {
+function convertCandlesToBars(candles: Candle[],  candleResolution: CandleResolution, maxBars: number): Bar[] {
   const length = candles.length;
   if (!length) return [];
 
