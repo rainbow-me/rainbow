67e0c41a8288273ac293b37ba8e30032d8152244
Daniel Sinclair <d@niel.nyc>
2026-01-13 14:39:34 -0500

    fix: pass nonce for transaction consistency

--- src/raps/execute.ts
+++ src/raps/execute.ts
@@ -230,6 +230,7 @@ export const walletExecuteRap = async <T extends RapTypes>(
           maxPriorityFeePerGas: BigInt(eip1559.maxPriorityFeePerGas),
           gasLimit: null,
         },
+        nonce: swapParams.nonce!,
       });
 
       if (!result.hash) {
