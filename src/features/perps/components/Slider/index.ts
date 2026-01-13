bc2abdcccd1643a474c0c233e18227dbb2e706f9
Ivan Sorokin <ivan@sorokin.io>
2026-01-13 18:09:10 +0100

    chore: remove src/features/perps/components/Slider/index.ts barrel file

--- src/features/perps/components/Slider/index.ts
+++
@@ -1,5 +0,0 @@
-export { Slider } from './Slider';
-export type { SliderProps, SliderColors, SliderChangeSource, SliderVisualState, SliderGestureState } from './Slider';
-
-export { SliderWithLabels } from './SliderWithLabels';
-export type { SliderLabels } from './SliderWithLabels';
