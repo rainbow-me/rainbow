acea0a7ebf60aa14bddd7628180ae1355b8a4daa
Janic Duplessis <janicduplessis@gmail.com>
2026-02-02 20:12:41 -0500

    fix wallet connect test

--- src/components/DappBrowser/search/Search.tsx
+++ src/components/DappBrowser/search/Search.tsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useMemo } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import Animated, { dispatchCommand, runOnJS, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import Animated, { runOnJS, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import { scheduleOnRN } from 'react-native-worklets';
 import { SPRING_CONFIGS } from '@/components/animations/animationConfigs';
 import { useColorMode } from '@/design-system';
-import { IS_IOS } from '@/env';
 import { useKeyboardHeight } from '@/hooks';
 import { useSharedValueState } from '@/hooks/reanimated/useSharedValueState';
 import { useSyncSharedValue } from '@/hooks/reanimated/useSyncSharedValue';
@@ -56,8 +56,7 @@ export const Search = () => {
   }));
 
   const bottomBarStyle = useAnimatedStyle(() => {
-    const translateY =
-      _WORKLET && isFocused.value ? -(keyboardHeight.value - (IS_IOS ? TAB_BAR_HEIGHT : 46) + extraWebViewHeight.value) : 0;
+    const translateY = _WORKLET && isFocused.value ? -(keyboardHeight.value - TAB_BAR_HEIGHT + extraWebViewHeight.value) : 0;
     return {
       transform: [
         {
@@ -118,12 +117,16 @@ export const Search = () => {
     [goToUrl, inputRef, searchQuery, searchResults]
   );
 
+  const focusInput = useCallback(() => {
+    inputRef.current?.focus();
+  }, [inputRef]);
+
   const onAddressInputPressWorklet = useCallback(() => {
     'worklet';
     isFocused.value = true;
     searchViewProgress.value = withSpring(100, SPRING_CONFIGS.snappierSpringConfig);
-    dispatchCommand(inputRef, 'focus');
-  }, [inputRef, isFocused, searchViewProgress]);
+    scheduleOnRN(focusInput);
+  }, [focusInput, isFocused, searchViewProgress]);
 
   const onBlurWorklet = useCallback(() => {
     'worklet';
