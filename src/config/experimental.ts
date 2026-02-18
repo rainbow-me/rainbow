dcf0d7cecd6df16ea07f5341a136141675ea4d21
Daniel Sinclair <4412473+DanielSinclair@users.noreply.github.com>
2026-02-18 00:33:30 -0500

    chore: delegation sdk, service injection (#7017)
    * chore: update viem to 2.38.0
    
    * chore: install delegation, stores sdk
    
    chore: install storesjs/stores
    
    chore: delegation sdk upgrade
    
    chore: upgrade delegation sdk, storesjs
    
    chore: swaps, delegation sdk
    
    chore: upgrade sdk
    
    chore: upgrade delegation sdk to 0.2.2
    
    chore: upgrade stores to 0.8.7
    
    remove
    
    * feat: add delegation experimental flag
    
    Co-Authored-By: derHowie <derhowiedesigns@gmail.com>
    
    * feat: delegation remote flag
    
    * feat:  configure delegation client with dependencies
    
    logger service
    
    fix: add selected current address, chains
    
    * note: chains issue
    
    * fix: RainbowError safe logger proxy
    
    fix: RainbowError safe logger proxy
    
    chore: logger DebugContext for `delegation`
    
    ---------
    
    Co-authored-by: derHowie <derhowiedesigns@gmail.com>

--- src/config/experimental.ts
+++ src/config/experimental.ts
@@ -42,6 +42,7 @@ export const PERPS = 'Perps';
 export const POLYMARKET = 'Polymarket';
 export const DEFI_POSITIONS_THRESHOLD_FILTER = 'DeFi Minimum Value Filter';
 export const RNBW_REWARDS = 'RNBW Rewards';
+export const DELEGATION = '7702 Delegation';
 
 /**
  * A developer setting that pushes log lines to an array in-memory so that
@@ -93,6 +94,7 @@ const config = {
   [DEFI_POSITIONS_THRESHOLD_FILTER]: { settings: true, value: true },
   [POLYMARKET]: { settings: true, value: false },
   [RNBW_REWARDS]: { settings: true, value: false },
+  [DELEGATION]: { settings: true, value: false },
 } as const;
 
 /** This flag is not reactive. We use this in a static context. */
