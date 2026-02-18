767133ec64cfed980bf172873d436da524cf21aa
Daniel Sinclair <d@niel.nyc>
2026-02-17 23:29:00 -0500

    Revert "refactor: prepareSimulationTransaction to early break if delegation disable"
    This reverts commit ef05dff949b638530dc850ab15b14d686e2fe6e7.

--- src/resources/transactions/transactionSimulation.ts
+++ src/resources/transactions/transactionSimulation.ts
@@ -1,7 +1,5 @@
 import type { Address } from 'viem';
-import { prepareBatchedTransaction, supportsDelegation, type BatchCall } from '@rainbow-me/delegation';
-import { DELEGATION, getExperimentalFlag } from '@/config/experimental';
-import { getRemoteConfig } from '@/model/remoteConfig';
+import { prepareBatchedTransaction, type BatchCall } from '@rainbow-me/delegation';
 import { createQueryKey, QueryConfig, QueryFunctionArgs } from '@/react-query';
 import { useQuery } from '@tanstack/react-query';
 import { RainbowError, logger } from '@/logger';
@@ -44,38 +42,21 @@ type SimulationResult = {
 // ///////////////////////////////////////////////
 // Simulations
 
-const prepareSimulationTransaction = async ({
-  from,
-  chainId,
-  transactions,
-}: {
-  from: Address;
-  chainId: number;
-  transactions: Transaction[];
-}) => {
-  const delegationEnabled = getRemoteConfig().delegation_enabled || getExperimentalFlag(DELEGATION);
-  if (!delegationEnabled) return transactions;
-
-  const { supported } = await supportsDelegation({ address: from, chainId });
-  if (!supported) return transactions;
-
-  const nonce = await getNextNonce({ address: from, chainId });
-
-  // Converts transaction representation to delegation formats if applicable
-  // Casting is safe as we are casting string to Hex string type
-  return prepareBatchedTransaction({ from, chainId, calls: transactions as BatchCall[], nonce });
-};
-
 export const simulateTransactions = async (args: SimulateTransactionsQueryVariables) => {
   if (!args.transactions) return [];
 
   const transactions = Array.isArray(args.transactions) ? args.transactions : [args.transactions];
   if (transactions.length === 0) return [];
 
-  const preparedTransaction = await prepareSimulationTransaction({
+  const nonce = await getNextNonce({ address: transactions[0].from as Address, chainId: args.chainId });
+
+  // Converts transaction representation to delegation formats if applicable
+  // Casting is safe as we are casting string to Hex string type
+  const preparedTransaction = await prepareBatchedTransaction({
     from: transactions[0].from as Address,
     chainId: args.chainId,
-    transactions,
+    calls: transactions as BatchCall[],
+    nonce,
   });
 
   const response = await metadataPOSTClient.simulateTransactions({ ...args, transactions: preparedTransaction });
