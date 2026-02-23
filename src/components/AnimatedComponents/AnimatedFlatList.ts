707cd9e741202290d13d9ca01d875960c89ff806
Ole Birk Rasmussen <1476509+olerass@users.noreply.github.com>
2026-02-23 23:19:51 +0100

    chore: remove unused DraggableFlatList and AnimatedFlatList components

--- src/components/AnimatedComponents/AnimatedFlatList.ts
+++
@@ -1,4 +0,0 @@
-import { FlatList } from 'react-native-gesture-handler';
-import Animated from 'react-native-reanimated';
-
-export const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
