a92415d2a60d7aabb0fd277282168d6c7e3ddf0e
Janic Duplessis <janicduplessis@gmail.com>
2026-03-10 13:04:44 -0400

    fix: use absolute paths for Metro exclusion patterns
    Metro tests absolute file paths against the exclusionList regex.
    Relative patterns with ^ don't match. Use escaped __dirname to build
    absolute patterns that only match the root ios/ and android/ dirs.

--- metro.config.js
+++ metro.config.js
@@ -4,13 +4,15 @@ const blacklist = require('metro-config/src/defaults/exclusionList');
 const { mergeConfig, getDefaultConfig } = require('@react-native/metro-config');
 const { withSentryConfig } = require('@sentry/react-native/metro');
 const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+const escapeRegExp = require('lodash/escapeRegExp');
 
 // Deny list is a function that takes an array of regexes and combines
 // them with the default blacklist to return a single regex.
+const makeExclusionRE = re => new RegExp(`${escapeRegExp(__dirname)}\\/${re}`);
 const blacklistRE = blacklist([
   // Ignore native build directories to prevent Metro fast refresh during builds
-  /^ios\/.*/,
-  /^android\/.*/,
+  makeExclusionRE('ios\\/.*'),
+  makeExclusionRE('android\\/.*'),
   // react-native-animated-charts
   /src\/react-native-animated-charts\/Example\/.*/,
   /src\/react-native-animated-charts\/node_modules\/.*/,
