3ac79bbeb32743d4cec542002a99d2ad72afeb41
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2026-04-22 16:35:11 -0400

    Remove unused constant

--- src/components/pending-transaction-watcher/PendingTransactionWatcher.tsx
+++ src/components/pending-transaction-watcher/PendingTransactionWatcher.tsx
@@ -1,13 +1,10 @@
 import { memo } from 'react';
 
-import { type PendingTransaction } from '@/entities/transactions';
 import { useTransactionWatcher } from '@/hooks/useTransactionWatcher';
 import { useWatchPendingTransactions } from '@/hooks/useWatchPendingTxs';
 import { usePendingTransactionsStore } from '@/state/pendingTransactions';
 import { useAccountAddress } from '@/state/wallets/walletsStore';
 import { shallowEqual } from '@/worklets/comparisons';
-
-const EMPTY_PENDING_TRANSACTIONS: PendingTransaction[] = [];
 
 export const PendingTransactionWatcher = memo(function PendingTransactionWatcher() {
   const address = useAccountAddress();
