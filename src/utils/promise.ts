096cfb81305c12de6dc6f4ac1b5cbaf264797cd6
Daniel Sinclair <d@niel.nyc>
2026-05-07 19:45:30 -0400

    feat(utils): extract withTimeout helper to shared utils
    Lifts the withTimeout<T>(promise, duration, message) function out of
    LedgerSigner.ts into src/utils/promise.ts so it can be reused. No
    behavioral change to LedgerSigner; subsequent commits in this stack will
    also use the shared helper.

--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -1,5 +1,18 @@
+import { RainbowError } from '@/logger';
+
 const PromiseAllWithFails = async (promises: Promise<any>[]) =>
   Promise.all(promises.map((promise: any) => (promise?.catch ? promise.catch((error: any) => error) : promise)));
+
+export function withTimeout<T>(promise: Promise<T>, duration: number, message: string): Promise<T> {
+  let timeoutId: ReturnType<typeof setTimeout> | undefined;
+  const timeout = new Promise<never>((_, reject) => {
+    timeoutId = setTimeout(() => reject(new RainbowError(message)), duration);
+  });
+
+  return Promise.race([promise, timeout]).finally(() => {
+    if (timeoutId) clearTimeout(timeoutId);
+  });
+}
 
 export default {
   PromiseAllWithFails,
