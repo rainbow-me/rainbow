fccb3ee7f7a2be930b78afc0ff44bba8d23c7eef
Daniel Sinclair <d@niel.nyc>
2026-05-22 19:05:57 -0400

    dev: default to Discover tab on launch

--- src/navigation/SwipeNavigator.tsx
+++ src/navigation/SwipeNavigator.tsx
@@ -741,7 +741,7 @@ function SwipeNavigatorScreens() {
       // required to force re-render when showKingOfTheHillTab, showRnbwRewardsTab or language changes
       key={key}
       initialLayout={deviceUtils.dimensions}
-      initialRouteName={Routes.WALLET_SCREEN}
+      initialRouteName={Routes.DISCOVER_SCREEN}
       screenOptions={getScreenOptions}
       tabBar={TabBarContainer}
       tabBarPosition="bottom"
