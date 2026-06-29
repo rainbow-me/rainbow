3e006bc52d0c7064f28d167717db57d97dcb116e
Ole Birk Rasmussen <1476509+olerass@users.noreply.github.com>
2026-06-30 00:13:50 +0200

    refactor: consolidate dapp browser referrals into one registry

--- src/features/lighter/constants.ts
+++
@@ -1,3 +0,0 @@
-export const LIGHTER_HOSTNAMES: ReadonlySet<string> = new Set(['app.lighter.xyz', 'lighter.xyz', 'www.lighter.xyz']);
-export const LIGHTER_REFERRAL_PARAM = 'referral';
-export const LIGHTER_REFERRAL_CODE = '7228476M';
