8a7c5dc97472baf0df8afa745a1a3008897531e6
Ole Birk Rasmussen <1476509+olerass@users.noreply.github.com>
2026-06-30 00:46:16 +0200

    fix lint

--- src/features/dapp-browser/components/DappBrowserWebview.tsx
+++ src/features/dapp-browser/components/DappBrowserWebview.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+
 import WebView, { type WebViewProps } from 'react-native-webview';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
