d4e88624a529f4f4279ac97146adcae96967a419
Ole Birk Rasmussen <1476509+olerass@users.noreply.github.com>
2026-07-01 11:32:29 +0200

    chore: remove the disabled audio NFT player

--- src/hooks/useAnimationType.ts
+++ src/hooks/useAnimationType.ts
@@ -2,16 +2,12 @@ import React from 'react';
 
 import type { UniqueAsset } from '@/entities/uniqueAssets';
 
-type AnimationType = '3d' | 'video' | 'audio';
+type AnimationType = '3d' | 'video';
 
 export default function useAnimationType(uniqueAsset: UniqueAsset) {
   return React.useMemo((): AnimationType | undefined => {
     if (!uniqueAsset.images.animatedMimeType || !uniqueAsset.images.animatedUrl) return undefined;
     if (uniqueAsset.images.animatedMimeType.includes('video')) return 'video';
     if (uniqueAsset.images.animatedMimeType.includes('model')) return '3d';
-    // Current support for Audio in prod is not working
-    // ticketed here: https://linear.app/rainbow/issue/APP-2784/follow-up-audio-nfts-are-sometimes-displayed-in-a-broken-state
-    // FIXME: Uncomment below to support audio in prod
-    // if (uniqueAsset.images.animatedMimeType.includes('audio')) return 'audio';
   }, [uniqueAsset.images.animatedMimeType, uniqueAsset.images.animatedUrl]);
 }
