89cdc118030e00e09e5018b3f7451a01168e3d40
Ole Birk Rasmussen <1476509+olerass@users.noreply.github.com>
2026-07-24 22:53:29 +0200

    chore: remove dead exports from vendored animated-charts

--- src/react-native-animated-charts/src/index.ts
+++ src/react-native-animated-charts/src/index.ts
@@ -1,9 +1,5 @@
 export { ChartPathProvider } from './charts/linear/ChartPathProvider';
 export { default as ChartDot } from './charts/linear/ChartDot';
-export { ChartYLabel, ChartXLabel } from './charts/linear/ChartLabels';
 export { ChartPath } from './charts/linear/ChartPath';
 export { useChartData } from './helpers/useChartData';
-export { default as simplifyData } from './simplification/simplifyData';
 export { default as monotoneCubicInterpolation } from './interpolations/monotoneCubicInterpolation';
-export { default as bSplineInterpolation } from './interpolations/bSplineInterpolation';
-export { CurrentPositionVerticalLine, OpeningPositionHorizontalLine } from './charts/linear/ChartLines';
