bf788adecd100dbcd3caea682f9d94fca136a2ad
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2026-08-16 05:28:25 -0400

    Fix Reanimated `processColor` crash
    Opacity value of `0.00000012567359165993606` was causing a Reanimated crash in `backgroundPillStyle`

--- src/framework/ui/utils/opacity.ts
+++ src/framework/ui/utils/opacity.ts
@@ -5,7 +5,8 @@ export const opacity = (color: string, opacity: number) => {
 
   if (isColor(color)) {
     const rgbaColor = convertToRGBA(color);
-    return `rgba(${rgbaColor[0] * 255}, ${rgbaColor[1] * 255}, ${rgbaColor[2] * 255}, ${opacity})`;
+    const safeOpacity = opacity < 0.000001 ? 0 : opacity;
+    return `rgba(${rgbaColor[0] * 255}, ${rgbaColor[1] * 255}, ${rgbaColor[2] * 255}, ${safeOpacity})`;
   } else {
     return color;
   }
